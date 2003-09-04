355e84bc1cfb7bb5f6619aa1c768340477a21546
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:29:46 +0000

    rev 1.1 at 2000/02/08 15:25:23 GMT  - ajout du sous-repertoire `net' a la toolbox
    (Logical change 1.70)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@698 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/ntbx/include/ntbx_tcp.h
@@ -0,0 +1,72 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: ntbx_tcp.h,v $
+Revision 1.1  2000/02/08 15:25:23  oaumage
+- ajout du sous-repertoire `net' a la toolbox
+
+
+______________________________________________________________________________
+*/
+
+/*
+ * NTbx_tcp.h
+ * ==========
+ */
+
+#ifndef NTBX_TCP_H
+#define NTBX_TCP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <errno.h>
+
+typedef int                ntbx_tcp_port_t,    *p_ntbx_tcp_port_t;
+typedef struct sockaddr_in ntbx_tcp_address_t, *p_ntbx_tcp_address_t;
+typedef int                ntbx_tcp_socket_t,  *p_ntbx_tcp_socket_t;
+
+#endif /* NTBX_TCP_H */

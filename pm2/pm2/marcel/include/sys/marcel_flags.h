cd01ffecb79aa321dcb9844c13ebb0c5b630f05a
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:19:28 +0000

    rev 1.1 at 2000/03/06 14:56:04 GMT  Modified to include "marcel_flags.h".
    (Logical change 1.91)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1168 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/sys/marcel_flags.h
@@ -0,0 +1,58 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: marcel_flags.h,v $
+Revision 1.1  2000/03/06 14:56:04  rnamyst
+Modified to include "marcel_flags.h".
+
+______________________________________________________________________________
+*/
+
+#ifndef MARCEL_FLAGS_EST_DEF
+#define MARCEL_FLAGS_EST_DEF
+
+#ifdef SMP
+#define ACT_OR_SMP
+#endif
+
+#ifdef __ACT__
+#include <linux/config.h>
+#ifdef CONFIG_SMP
+#define ACT_OR_SMP
+#define __SMP__
+#endif
+#endif
+
+#endif

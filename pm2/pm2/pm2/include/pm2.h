3d255a01ec2436fc7ef1577e71dd44ec4e068d69
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:47:46 +0000

    rev 1.10 at 2000/01/03 09:21:50 GMT  Added pm2_rpc.h & pm2_thread.h.
    (Logical change 1.24)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@217 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/pm2.h
+++ pm2/pm2/pm2/include/pm2.h
@@ -40,10 +40,12 @@
 #include <safe_malloc.h>
 #include <madeleine.h>
 #include <pm2_attr.h>
+#include <pm2_thread.h>
 #include <block_alloc.h>
 #include <sys/slot_distrib.h>
 #include <pm2_timing.h>
 #include <sys/debug.h>
+#include <pm2_rpc.h>
 
 /* A startup function may be specified. If so, it will be called after
  * all modules will be spawned but before the current module will
@@ -51,8 +53,6 @@
  */
 typedef void (*pm2_startup_func_t)(int *modules, int nb);
 void pm2_set_startup_func(pm2_startup_func_t f);
-
-void pm2_init_rpc(void);
 
 #define ASK_USER	-1
 #define ONE_PER_NODE	0
@@ -77,265 +77,25 @@ static __inline__ int pm2_self(void)
   return __pm2_self;
 }
 
-void pm2_enter_critical_section(void);
-void pm2_leave_critical_section(void);
-
 void pm2_printf(char *format, ...);
 
-extern exception	LRPC_ERROR;
+void pm2_channel_alloc(pm2_channel_t *channel);
 
-#define LRPC_DECL_REQ(name, decl) typedef struct { decl } \
-	_##name##_req; \
-	void name##_pack_req(any_t _arg); \
-	void name##_unpack_req(any_t _arg);
+/****************** "RAW" RPC : *******************/
 
-#define LRPC_DECL_RES(name, decl) typedef struct { decl } \
-	_##name##_res; \
-	void name##_pack_res(any_t _arg); \
-	void name##_unpack_res(any_t _arg);
+typedef void (*pm2_rawrpc_func_t)(void);
 
+void pm2_rawrpc_register(int *num, pm2_rawrpc_func_t func);
 
-#define BEGIN_LRPC_LIST unsigned int
-#define END_LRPC_LIST	;
+void pm2_rawrpc_begin(int module, int num, pm2_attr_t *pm2_attr);
 
-#include <sys/console.h>
-#include <sys/isomalloc_rpc.h>
-#ifdef DSM
-#include <dsm_rpc.h>
-#endif
+void pm2_rawrpc_end(void);
 
-#define LRPC_REQ(name) _##name##_req
-#define LRPC_RES(name) _##name##_res
-
-#define PACK_REQ_STUB(name) \
-	void name##_pack_req(any_t _arg) \
-	{ _##name##_req *arg = (_##name##_req *)_arg;
-
-#define UNPACK_REQ_STUB(name) \
-	void name##_unpack_req(any_t _arg) \
-	{ _##name##_req *arg = (_##name##_req *)_arg;
-
-#define PACK_RES_STUB(name) \
-	void name##_pack_res(any_t _arg) \
-	{ _##name##_res *arg = (_##name##_res *)_arg;
-
-#define UNPACK_RES_STUB(name) \
-	void name##_unpack_res(any_t _arg) \
-	{ _##name##_res *arg = (_##name##_res *)_arg;
-
-#define END_STUB	arg=_arg; }
-
-
-typedef void (*pack_func_t)(any_t arg);
-typedef void (*unpack_func_t)(any_t arg);
-typedef void (*rpc_func_t)(any_t arg);
-
-enum {
-  DO_NOT_OPTIMIZE,
-  OPTIMIZE_IF_LOCAL
-};
-
-void pm2_register_service(int *num, rpc_func_t func,
-			  int req_size, pack_func_t pack_req, unpack_func_t unpack_req,
-			  int res_size, pack_func_t pack_res, unpack_func_t unpack_res,
-			  char *name, int optimize_if_local);
-
-#define DECLARE_LRPC(name) \
-	pm2_register_service(&name, (rpc_func_t) _##name##_func, \
-	sizeof(LRPC_REQ(name)), name##_pack_req, name##_unpack_req, \
-	sizeof(LRPC_RES(name)), name##_pack_res, name##_unpack_res, \
-	#name, OPTIMIZE_IF_LOCAL)
-
-#define DECLARE_LRPC_WITH_NAME(name, string, optimize_if_local) \
-	pm2_register_service(&name, (rpc_func_t) _##name##_func, \
-	sizeof(LRPC_REQ(name)), name##_pack_req, name##_unpack_req, \
-	sizeof(LRPC_RES(name)), name##_pack_res, name##_unpack_res, \
-	string, optimize_if_local)
-
-#define DECLARE_RAW_RPC(name) \
-	pm2_register_service(&name, (rpc_func_t) _##name##_func, \
-	0, NULL, NULL, \
-	0, NULL, NULL, \
-	#name, DO_NOT_OPTIMIZE)
-
-
-_PRIVATE_ int pm2_lrpc_runned_by(marcel_t pid);
-_PRIVATE_ char *pm2_lrpc_name(int num);
-
-#ifdef MAD2
-_PRIVATE_ struct pm2_channel_struct_t;
-_PRIVATE_ typedef struct pm2_channel_struct_t *pm2_channel_t;
-#endif
-
-/************* LRPC avec attente differee : **************/
-
-/*
- * LRP_CALL(module,
- *          service_name,
- *          priority,
- *          stack_size,
- *          request_ptr,
- *          result_ptr,
- *          wait_struct_ptr)
- *
- * QUICK_LRP_CALL(module,
- *                service_name,
- *                request_ptr,
- *                result_ptr,
- *                wait_struct_ptr)
- *
- * LRP_WAIT(wait_struct_ptr)
- */
-
-_PRIVATE_ typedef struct _pm2_rpc_wait {
-   marcel_sem_t sem;
-   any_t result;
-   unpack_func_t unpack;
-   int tid;
-   pointer ptr_att;
-} pm2_rpc_wait_t;
-
-void pm2_rpc_call(int module, int num, pm2_attr_t *pm2_attr,
-		  any_t args, any_t results, pm2_rpc_wait_t *att);
-
-#define LRP_CALL(module, name, prio, stack, args, results, att) \
-	pm2_rpc_call(module, name, NULL, args, results, att)
-
-void pm2_quick_rpc_call(int module, int num, pm2_attr_t *pm2_attr,
-			any_t args, any_t results, pm2_rpc_wait_t *att);
-
-#define QUICK_LRP_CALL(module, name, args, results, att) \
-	pm2_quick_rpc_call(module, name, NULL, args, results, att)
-
-void pm2_rpc_wait(pm2_rpc_wait_t *att);
-
-#define LRP_WAIT(att) \
-	pm2_rpc_wait(att)
-
-void pm2_cancel_wait(pm2_rpc_wait_t *att);
-
-
-_PRIVATE_ void pm2_rpc_call_begin(int module, int num,
-				  pm2_attr_t *pm2_attr,
-				  any_t args, any_t results,
-				  pm2_rpc_wait_t *att);
-_PRIVATE_ void pm2_rpc_call_end(void);
-
-/****************** LRPC synchrones : ********************/
-
-/*
- * LRPC(module,
- *      service_name,
- *      priority,
- *      stack_size,
- *      request_ptr,
- *      result_ptr)
- *
- * QUICK_LRPC(module,
- *            service_name,
- *            request_ptr,
- *            result_ptr)
- *
- */
-
-static __inline__ void pm2_rpc(int module, int num, pm2_attr_t *pm2_attr,
-			       any_t args, any_t results)
-{
-  pm2_rpc_wait_t att;
-
-  pm2_rpc_call(module, num, pm2_attr, args, results, &att);
-  pm2_rpc_wait(&att);
-}
-
-static __inline__ void pm2_quick_rpc(int module, int num, pm2_attr_t *pm2_attr,
-				     any_t args, any_t results)
-{
-  pm2_rpc_wait_t att;
-
-  pm2_quick_rpc_call(module, num, pm2_attr, args, results, &att);
-  pm2_rpc_wait(&att);
-}
-
-#define LRPC(module, name, prio, stack, args, results) \
-	pm2_rpc(module, name, NULL, args, results)
-
-#define QUICK_LRPC(module, name, args, results) \
-	pm2_quick_rpc(module, name, NULL, args, results)
-
-
-#define pm2_rpc_begin(module, num, pm2_attr, args, results) \
-{ \
-  pm2_rpc_wait_t att; \
-  pm2_rpc_call_begin(module, num, pm2_attr, args, results, &att);
-
-#define pm2_rpc_end() \
-  pm2_rpc_call_end(); \
-  pm2_rpc_wait(&att); \
-}
-
-/****************** LRPC asynchrones : *******************/
-
-/*
- * ASYNC_LRPC(module,
- *            service_name,
- *            priority,
- *            stack_size,
- *            request_ptr)
- *
- * QUICK_ASYNC_LRPC(module,
- *                  service_name,
- *                  request_ptr)
- */
-
-void pm2_async_rpc(int module, int num, pm2_attr_t *pm2_attr,
-		   any_t args);
-
-#define ASYNC_LRPC(module, name, prio, stack, args) \
-	pm2_async_rpc(module, name, NULL, args)
-
-void pm2_quick_async_rpc(int module, int num, pm2_attr_t *pm2_attr,
-			 any_t args);
-
-#define QUICK_ASYNC_LRPC(module, name, args) \
-	pm2_quick_async_rpc(module, name, NULL, args)
-
-
-_PRIVATE_ void pm2_async_rpc_begin(int module, int num,
-				   pm2_attr_t *pm2_attr,
-				   any_t args);
-_PRIVATE_ void pm2_async_rpc_end(void);
-
-_PRIVATE_ void pm2_rawrpc_begin(int module, int num,
-				pm2_attr_t *pm2_attr);
-_PRIVATE_ void pm2_rawrpc_end(void);
-
-/************ LRPC asynchrones en multicast : ************/
-
-/*
- * MULTI_ASYNC_LRPC(tab_modules,
- *                  nb_modules,
- *                  service_name,
- *                  priority,
- *                  stack_size,
- *                  request_ptr)
- *
- * MULTI_QUICK_ASYNC_LRPC(tab_modules,
- *                        nb_modules,
- *                        service_name,
- *                        request_ptr)
- */
-
-void pm2_multi_async_rpc(int *modules, int nb, int num, pm2_attr_t *pm2_attr,
-			 any_t args);
-
-#define MULTI_ASYNC_LRPC(modules, nb, name, prio, stack, args) \
-	pm2_multi_async_rpc(modules, nb, name, NULL, args)
-
-void pm2_multi_quick_async_rpc(int *modules, int nb, int num, pm2_attr_t *pm2_attr,
-			       any_t args);
-
-#define MULTI_QUICK_ASYNC_LRPC(modules, nb, name, args) \
-	pm2_multi_quick_async_rpc(modules, nb, name, NULL, args)
+#define pm2_rawrpc_waitdata() \
+  { \
+    mad_recvbuf_receive(); \
+    marcel_sem_V((marcel_sem_t *)marcel_getspecific(_pm2_mad_key)); \
+  }
 
 
 /******************** Migration **************************/
@@ -356,11 +116,16 @@ static __inline__ void pm2_freeze(void)
   lock_task();
 }
 
-
 static __inline__ void pm2_unfreeze(void)
 {
   unlock_task();
 }
+
+/* Les valeurs possibles pour *which* sont :
+		ALL_THREADS
+	ou	MIGRATABLE_ONLY
+*/
+void pm2_threads_list(int max, marcel_t *pids, int *nb, int which);
 
 void pm2_migrate_group(marcel_t *pids, int nb, int module);
 void pm2_migrate(marcel_t pid, int module);
@@ -372,10 +137,6 @@ static __inline__ void pm2_migrate_self(int module)
   pm2_freeze();
   pm2_migrate_group(&self, 1, module);
 }
-
-void pm2_backup_group(marcel_t *pids, int nb, char *file);
-
-void pm2_restore_group(char *file);
 
 _PRIVATE_ typedef struct {
   void *stack_base;
@@ -403,163 +164,15 @@ typedef block_descr_t isomalloc_dataset_t;
 #define pm2_dataset_attach(descr) \
    block_merge_lists(descr, \
                      (block_descr_t *)marcel_getspecific(_pm2_block_key))
-
 #define pm2_dataset_attach_other(task,descr) \
    block_merge_lists(descr, (block_descr_t *) task->key[_pm2_block_key])
 
 /*********************************************************/
 
-/*
-	Les valeurs possibles pour *which* sont :
-		ALL_THREADS
-	ou	MIGRATABLE_ONLY
-*/
-void pm2_threads_list(int max, marcel_t *pids, int *nb, int which);
 
+_PRIVATE_ extern marcel_key_t _pm2_lrpc_num_key,
+  _pm2_block_key, _pm2_mad_key, _pm2_isomalloc_nego_key;
 
-/*********************************************************/
-
-_PRIVATE_ typedef struct {
-  int tid, num, not_migratable;
-  boolean sync, quick;
-  any_t req;
-  marcel_sem_t *sem;
-  marcel_t initiator;
-  pointer ptr_att;
-  pm2_rpc_wait_t *true_ptr_att;
-  block_descr_t sd;
-} rpc_args;
-
-#define EXTERN_LRPC_SERVICE(name) \
-	void _##name##_func(rpc_args *arg)
-
-#ifdef MINIMAL_PREEMPTION
-#define RELEASE_CALLER(arg) if(!_quick) marcel_explicityield((arg)->initiator)
-#else
-#define RELEASE_CALLER(arg) marcel_sem_V((arg)->sem)
-#endif
-
-_PRIVATE_ extern int _pm2_optimize[];
-
-_PRIVATE_ extern marcel_key_t _pm2_lrpc_num_key, _pm2_block_key, _pm2_isomalloc_nego_key;
-
-_PRIVATE_ extern void _pm2_term_func(void *arg);
-
-#define BEGIN_SERVICE(name) \
-	void _##name##_func(rpc_args *_arg) { \
-	boolean _sync, _quick, _local = FALSE; \
-	LRPC_REQ(name) req; \
-	LRPC_RES(name) res; \
-        _sync = _arg->sync; \
-	_quick = _arg->quick; \
-	if(!_quick) { \
-		marcel_setspecific(_pm2_lrpc_num_key, (any_t)((long)_arg->num)); \
-		marcel_setspecific(_pm2_block_key, (any_t)&_arg->sd); \
-		marcel_setspecific(_pm2_isomalloc_nego_key, (any_t) 0);\
-		block_init_list(&_arg->sd); \
-		marcel_cleanup_push(_pm2_term_func, marcel_self()); \
-	} \
-	if(_arg->tid == pm2_self() && _pm2_optimize[name]) { \
-		if(_quick) {\
-			_arg->not_migratable = marcel_self()->not_migratable; \
-			_local = TRUE; \
-		} \
-		memcpy(&req, _arg->req, sizeof(LRPC_REQ(name))); \
-	} else { \
-		name##_unpack_req(&req); \
-		mad_recvbuf_receive(); \
-	} \
-        RELEASE_CALLER(_arg); \
-	{
-
-#define BEGIN_STUBLESS_SERVICE(name) \
-	void _##name##_func(rpc_args *_arg) { \
-	boolean _sync, _quick, _local = FALSE; \
-	LRPC_RES(name) res; \
-        _sync = _arg->sync; \
-	_quick = _arg->quick; \
-	if(!_quick) { \
-		marcel_setspecific(_pm2_lrpc_num_key, (any_t)((long)_arg->num)); \
-		marcel_setspecific(_pm2_block_key, (any_t)&_arg->sd); \
-		marcel_setspecific(_pm2_isomalloc_nego_key, (any_t) 0);\
-		block_init_list(&_arg->sd); \
-		marcel_cleanup_push(_pm2_term_func, marcel_self()); \
-	} \
-	if(_arg->tid == pm2_self() && _pm2_optimize[name]) { \
-		RAISE(NOT_IMPLEMENTED); \
-	} \
-	{
-
-#define BEGIN_RAW_SERVICE(name) \
-  void _##name##_func(rpc_args *_arg) { \
-
-#define pm2_raw_waitdata() mad_recvbuf_receive()
-
-#define pm2_wait_for_data() \
-  { \
-    mad_recvbuf_receive(); \
-    RELEASE_CALLER((rpc_args *)(marcel_self()->user_space_ptr)); \
-  }
-
-_PRIVATE_ void _end_service(rpc_args *args, any_t res, int local);
-
-#define END_RAW_SERVICE(name) \
-    }
-
-#define END_SERVICE(name) \
-	} \
-	{ rpc_args *args; \
-        	if(_sync) { \
-		if(_quick) { \
-			args = _arg; \
-			if(_local) \
-				marcel_self()->not_migratable = _arg->not_migratable; \
-		} else { \
-			marcel_disablemigration(marcel_self()); \
-			marcel_getuserspace(marcel_self(), (void **)&args); \
-		} \
-		_end_service(args, &res, _local); \
-		} else if(_local && _quick) \
-			marcel_self()->not_migratable = _arg->not_migratable; \
-	} \
-	}
-
-
-
-/*********************************************************/
-
-
-#define SPLIT(nb, tab, type) \
-	{ unsigned _total = (nb)+1, my_rank; \
-	  char *_dest; \
-	  marcel_sem_t _wait, *_ptr_wait; \
-	  type *_contrib; \
-	  int _module; \
-	pm2_disable_migration(); \
-	_module = pm2_self(); \
-	_contrib = (type *)mad_aligned_malloc(sizeof(type) * _total); \
-        _ptr_wait = &_wait; \
-	marcel_sem_init(&_wait, 0); \
-	my_rank = _pm2_fork(tab, nb); \
-	_dest = (char *)&_contrib[my_rank]; \
-	if(my_rank) pm2_enable_migration(); 
-
-#define MERGE(my_contrib, tab) \
-	_pm2_merge(_module, _ptr_wait, _dest, (char *)my_contrib, sizeof(my_contrib)); \
-	if(my_rank != 0) \
-		marcel_exit(NULL); \
-	*(tab) = _contrib; \
-	{ int _i; for(_i=0; _i<_total; _i++) marcel_sem_P(&_wait); }
-
-#define END_SPLIT \
-	mad_aligned_free(_contrib); \
-	pm2_enable_migration(); \
-	}
-
-
-_PRIVATE_ extern marcel_key_t _pm2_cloning_key;
-_PRIVATE_ int _pm2_fork(int *modules, int nb);
-_PRIVATE_ void _pm2_merge(int module, marcel_sem_t *sem, char *dest, char *src, unsigned size);
 
 #define pm2_main marcel_main
 

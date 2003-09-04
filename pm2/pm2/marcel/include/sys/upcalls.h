1717f0d54286e707beaacb2ffc91cb02a17d30a8
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:18:11 +0000

    rev 1.7 at 2000/03/06 09:24:58 GMT  Nouvelle version des activations
    (Logical change 1.90)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1154 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/upcalls.h
+++ pm2/pm2/marcel/include/sys/upcalls.h
@@ -19,34 +19,12 @@
 
 #include "marcel.h"
 
-#define RETURN_FROM_UPCALL 1
-
-typedef enum {
-  ACT_UNUSED,
-  ACT_RUNNING,
-} act_state_t;
-
-typedef struct {
-  marcel_t current_thread;
-  act_state_t state;
-  marcel_t start_thread;
-} act_info_t;
-
-extern volatile marcel_t thread_to_schedule;
-extern volatile int schedule_new_thread;
-
 void act_lock(marcel_t self);
 void act_unlock(marcel_t self);
-marcel_t act_update(marcel_t new);
 
 void restart_thread(task_desc *desc);
-void act_new(act_id_t new, act_id_t stopped_aid);
-void act_preempt(act_id_t cur_aid, act_id_t stopped_aid, int fast_restart);
-void act_block(act_id_t cur_aid, act_id_t stopped_aid);
-void act_unblock(act_id_t cur_aid, act_id_t stopped_aid, int fast_restart);
-void act_restart(act_id_t cur_aid);
 
 void init_upcalls(int nb_act);
-void launch_upcalls(int __nb_act_wanted);
+#define launch_upcalls(__nb_act_wanted)
 
 #endif /* _UPCALLS_H */

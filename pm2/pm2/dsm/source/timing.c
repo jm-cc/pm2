62b8b50448bd64f63b2c8791d3c2ce210fdb3302
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:08:56 +0000

    rev 1.2 at 2000/01/21 13:28:49 GMT  Changed safe_malloc and timing to links. Modified dsm_rpc to match the new interface.
    (Logical change 1.53)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@482 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/timing.c
+++ pm2/pm2/dsm/source/timing.c
@@ -1,13 +1,13 @@
 /*
                       PM2 HIGH-PERF/ISOMALLOC
            High Performance Parallel Multithreaded Machine
-                           version 2.0
+                           version 3.0
 
-             Gabriel Antoniu, Luc Bouge, Christian Perez,
-                Jean-Francois Mehaut, Raymond Namyst
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
 
             Laboratoire de l'Informatique du Parallelisme
-                        UMR 8512 CNRS-INRIA
+                        UMR 5668 CNRS-INRIA
                  Ecole Normale Superieure de Lyon
 
                       External Contributors:
@@ -42,26 +42,13 @@
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
-#ifdef PENTIUM_TIMING
-
-#define TICK_RAW_DIFF(t1, t2) ((t2).tick - (t1).tick)
-
-#else
-
-#define TICK_RAW_DIFF(t1, t2) ((t2.timev.tv_sec * 1000000L + t2.timev.tv_usec) - \
-			       (t1.timev.tv_sec * 1000000L + t1.timev.tv_usec))
-
-#endif
-
-#define TICK_DIFF(t1, t2)     (TICK_RAW_DIFF(t1, t2) - _timing_residual)
-
 static double scale = 0.0;
-double microsec_time;
+
 long long _timing_residual;
 
 static Tick t1, t2;
 
-Tick _last_timing_event;
+Tick _new_timing_event, _last_timing_event;
 
 void timing_init()
 {
@@ -74,17 +61,18 @@ void timing_init()
     _timing_residual = min(_timing_residual, TICK_RAW_DIFF(t1, t2));
   }
 
-#ifdef PENTIUM_TIMING
+#ifdef X86_ARCH
   {
     struct timeval tv1,tv2;
 
     GET_TICK(t1);
     gettimeofday(&tv1,0);
-    usleep(100000);
+    usleep(50000);
     GET_TICK(t2);
     gettimeofday(&tv2,0);
-    scale = ((tv2.tv_sec*1e6 + tv2.tv_usec) - (tv1.tv_sec*1e6 + tv1.tv_usec))
-      / (double)(TICK_DIFF(t1, t2));
+    scale = ((tv2.tv_sec*1e6 + tv2.tv_usec) -
+	     (tv1.tv_sec*1e6 + tv1.tv_usec)) / 
+             (double)(TICK_DIFF(t1, t2));
   }
 #else
   scale = 1.0;
@@ -98,10 +86,12 @@ double timing_tick2usec(long long t)
   return (double)(t)*scale;
 }
 
-void timing_stat(char *name, Tick t)
+void timing_stat(char *name)
 {
   fprintf(stderr, "EVT<%s> at +%lld ticks (+%f usecs)\n",
-	  name, TICK_DIFF(_last_timing_event, t),
-	  microsec_time = timing_tick2usec(TICK_DIFF(_last_timing_event, t)));
+	  name,
+	  TICK_DIFF(_last_timing_event, _new_timing_event),
+	  timing_tick2usec(TICK_DIFF(_last_timing_event,
+				     _new_timing_event)));
 }
 

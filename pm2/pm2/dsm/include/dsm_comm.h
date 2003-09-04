e03c2e3030f7dfa0d2dc770adbf38a636a688c53
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:39:12 +0000

    rev 1.1 at 1999/12/01 14:44:45 GMT  Rajoute DSM (version 30/11: interface protocol rajoute au niveau utilisateur).
    (Logical change 1.9)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@94 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/dsm/include/dsm_comm.h
@@ -0,0 +1,59 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 2.0
+
+             Gabriel Antoniu, Luc Bouge, Christian Perez,
+                Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 8512 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+*/
+
+#ifndef DSM_COMM_IS_DEF
+#define DSM_COMM_IS_DEF
+
+#include "dsm_const.h" // dsm_access_t, dsm_node_t
+#include "dsm_page_manager.h"
+
+void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_access_t req_access);
+
+void dsm_invalidate_copyset(unsigned long index, dsm_node_t new_owner);
+
+void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_node_t reply_node, dsm_access_t access);
+
+void dsm_send_invalidate_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_node_t new_owner);
+
+void dsm_send_invalidate_ack(dsm_node_t dest_node, unsigned long index);
+
+/***********************  Hyperion stuff: ****************************/
+
+void dsm_send_diffs(unsigned long index, dsm_node_t dest_node);
+
+#endif
+
+
+

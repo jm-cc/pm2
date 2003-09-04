d564cd30efc20d39eda60543871f18c67d9b444b
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:04:33 +0000

    rev 1.2 at 2000/01/16 11:50:18 GMT  Modofied to match the new pm2_init/pm2_halt interface.
    (Logical change 1.47)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@425 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/examples/X/regul.c
+++ pm2/pm2/pm2/examples/X/regul.c
@@ -35,26 +35,21 @@
 
 #include <pm2.h>
 #include "regul.h"
-#include "rpc_defs.h"
 
 #define MAX_THREADS	  1024
-
 #define FREQ_OBS          100
-
 #define GROSSE_VARIATION  1
 
 #define abs(x)  (((x) < 0) ? -(x) : (x))
 
 static unsigned local_load = 0, other_load = 0;
 static int module_precedent, module_suivant;
-static int *les_mod, nb_mod;
 
-static marcel_t spy_pid;
-static marcel_sem_t spy_ok;
+static boolean do_regulate = FALSE, finished = FALSE;
 
-static boolean do_regulate = FALSE;
+static unsigned LOAD;
 
-static unsigned charge_locale()
+static unsigned charge_locale(void)
 {
   unsigned nb;
 
@@ -65,16 +60,14 @@ static unsigned charge_locale()
   return nb;
 }
 
-static void informer()
+static void informer(void)
 {
-  LRPC_REQ(LOAD) req;
-
-  req.load = local_load;
-  
-  QUICK_ASYNC_LRPC(module_precedent, LOAD, &req);
+  pm2_rawrpc_begin(module_precedent, LOAD, NULL);
+  mad_pack_int(MAD_IN_HEADER, &local_load, 1);
+  pm2_rawrpc_end();
 }
 
-static void reguler()
+static void reguler(void)
 { 
   /* liste de threads par ordre de priorité décroissante : */
   static marcel_t pids[MAX_THREADS], to_migrate[MAX_THREADS];
@@ -107,24 +100,17 @@ static void reguler()
    }
 }
 
-/* *** SERVICES *** */
+void load_service(void)
+{
+  mad_unpack_int(MAD_IN_HEADER, &other_load, 1);
+  pm2_rawrpc_waitdata();
+}
 
-BEGIN_SERVICE(LOAD)
-  other_load = req.load;
-#ifdef DEBUG
-  printf("Charge locale : %d, charge du suivant : %d\n", local_load, other_load);
-#endif
-END_SERVICE(LOAD)
-
-
-BEGIN_SERVICE(SPY)
-
+void spy(void *arg)
+{
   int charge;
 
-  spy_pid = marcel_self();
-  marcel_sem_V(&spy_ok);
-
-  while(1) {
+  while(!finished) {
     marcel_delay(FREQ_OBS);
 
     if(do_regulate) {
@@ -137,44 +123,33 @@ BEGIN_SERVICE(SPY)
       reguler();
     }
   }
-END_SERVICE(SPY)
-
-
-void regul_init_rpc()
-{
-  DECLARE_LRPC_WITH_NAME(SPY, "spy", OPTIMIZE_IF_LOCAL);
-  DECLARE_LRPC(LOAD);
 }
 
-void regul_init(int *les_modules, int nb_modules)
+void regul_rpc_init(void)
 {
-  int i;
-
-  for(i=0; les_modules[i] != pm2_self(); i++) ;
-  module_precedent = les_modules[(i+nb_modules-1)%nb_modules];
-  module_suivant = les_modules[(i+1)%nb_modules];
-
-  marcel_sem_init(&spy_ok, 0);
-
-  ASYNC_LRPC(pm2_self(), SPY, STD_PRIO, DEFAULT_STACK, NULL);
-
-  marcel_sem_P(&spy_ok);
-
-  les_mod = les_modules; nb_mod = nb_modules;
+  pm2_rawrpc_register(&LOAD, load_service);
 }
 
-void regul_start()
+void regul_init(void)
+{
+  module_precedent = (pm2_self() + pm2_config_size() - 1) % pm2_config_size();
+  module_suivant = (pm2_self() + 1) % pm2_config_size();
+
+  pm2_thread_create(spy, NULL);
+}
+
+void regul_start(void)
 {
   do_regulate = TRUE;
 }
 
-void regul_stop()
+void regul_stop(void)
 {
   do_regulate = FALSE;
 }
 
 void regul_exit()
 {
-  marcel_cancel(spy_pid);
+  finished = TRUE;
 }
 

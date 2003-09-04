63afcfbe059fd3c2a48fcefd2ade37008d5ca95e
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:08:24 +0000

    rev 1.3 at 2000/01/21 09:50:14 GMT  Added pm2_types.h to share the pm2_completion_t definition.
    (Logical change 1.51)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@463 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/pm2_attr.h
+++ pm2/pm2/pm2/include/pm2_attr.h
@@ -38,6 +38,7 @@
 
 #include <marcel.h>
 #include <madeleine.h>
+#include <pm2_types.h>
 
 typedef unsigned pm2_channel_t;
 
@@ -45,6 +46,7 @@ _PRIVATE_ typedef struct {
   unsigned priority;
   int sched_policy;
   pm2_channel_t channel;
+  pm2_completion_t *completion;
 } pm2_attr_t;
 
 int pm2_attr_init(pm2_attr_t *attr);
@@ -57,6 +59,9 @@ int pm2_attr_getschedpolicy(pm2_attr_t *attr, int *policy);
 
 int pm2_attr_setchannel(pm2_attr_t *attr, unsigned channel);
 int pm2_attr_getchannel(pm2_attr_t *attr, unsigned *channel);
+
+int pm2_attr_setcompletion(pm2_attr_t *attr, pm2_completion_t *completion);
+int pm2_attr_getcompletion(pm2_attr_t *attr, pm2_completion_t **completion);
 
 _PRIVATE_ extern pm2_attr_t pm2_attr_default;
 

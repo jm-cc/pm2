75bdee469fbcf331d6720bfb541c150eba7d5d0b
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:08:24 +0000

    rev 1.2 at 2000/01/21 09:52:48 GMT  Changed old_mad_pack functions to pm2_pack ones.
    (Logical change 1.51)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@466 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/console.c
+++ pm2/pm2/pm2/source/console.c
@@ -55,19 +55,19 @@ static void snap(marcel_t p)
   int temp = 1;
   char buf[16];
 
-   mad_pack_int(MAD_IN_HEADER, &temp, 1);
+   old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
    sprintf(buf, "%lx", (unsigned long)p);
-   mad_pack_str(MAD_IN_HEADER, buf);
+   old_mad_pack_str(MAD_IN_HEADER, buf);
    tamp=p->number;
-   mad_pack_long(MAD_IN_HEADER, &tamp, 1);
-   mad_pack_int(MAD_IN_HEADER, &p->prio, 1);
+   old_mad_pack_long(MAD_IN_HEADER, &tamp, 1);
+   old_mad_pack_int(MAD_IN_HEADER, &p->prio, 1);
    temp=p->state;
-   mad_pack_int(MAD_IN_HEADER, &temp, 1);
+   old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
    temp=64*1024;
-   mad_pack_int(MAD_IN_HEADER, &temp, 1);
-   mad_pack_str(MAD_IN_HEADER, pm2_lrpc_name(pm2_lrpc_runned_by(p)));
+   old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
+   old_mad_pack_str(MAD_IN_HEADER, pm2_lrpc_name(pm2_lrpc_runned_by(p)));
    temp = p->not_migratable;
-   mad_pack_int(MAD_IN_HEADER, &temp, 1);
+   old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
 }
 
 
@@ -85,7 +85,7 @@ PACK_RES_STUB(LRPC_CONSOLE_THREADS)
  int zero=0;
   
   marcel_snapshot(snap);
-  mad_pack_int(MAD_IN_HEADER, &zero, 1);
+  old_mad_pack_int(MAD_IN_HEADER, &zero, 1);
 END_STUB
 
 UNPACK_RES_STUB(LRPC_CONSOLE_THREADS)
@@ -94,16 +94,16 @@ UNPACK_RES_STUB(LRPC_CONSOLE_THREADS)
  int encore, prio_threads, state_threads, pile_threads, not_migratable;
 
   do {
-      mad_unpack_int(MAD_IN_HEADER, &encore, 1);
+      old_mad_unpack_int(MAD_IN_HEADER, &encore, 1);
       if(encore==0)
 		break;
-      mad_unpack_str(MAD_IN_HEADER, thread_id);
-      mad_unpack_long(MAD_IN_HEADER, &num_threads, 1);
-      mad_unpack_int(MAD_IN_HEADER, &prio_threads, 1);
-      mad_unpack_int(MAD_IN_HEADER, &state_threads, 1);
-      mad_unpack_int(MAD_IN_HEADER, &pile_threads, 1);
-      mad_unpack_str(MAD_IN_HEADER, rpc_name);
-      mad_unpack_int(MAD_IN_HEADER, &not_migratable, 1);
+      old_mad_unpack_str(MAD_IN_HEADER, thread_id);
+      old_mad_unpack_long(MAD_IN_HEADER, &num_threads, 1);
+      old_mad_unpack_int(MAD_IN_HEADER, &prio_threads, 1);
+      old_mad_unpack_int(MAD_IN_HEADER, &state_threads, 1);
+      old_mad_unpack_int(MAD_IN_HEADER, &pile_threads, 1);
+      old_mad_unpack_str(MAD_IN_HEADER, rpc_name);
+      old_mad_unpack_int(MAD_IN_HEADER, &not_migratable, 1);
       if(state_threads == RUNNING)
 		sprintf(s1, "run");
       else if(state_threads == BLOCKED)
@@ -128,24 +128,24 @@ END_SERVICE(LRPC_CONSOLE_PING)
 
 
 PACK_REQ_STUB(LRPC_CONSOLE_PING)
-   mad_pack_int(MAD_IN_HEADER, &arg->version, 1);
+   old_mad_pack_int(MAD_IN_HEADER, &arg->version, 1);
 END_STUB
 
 UNPACK_REQ_STUB(LRPC_CONSOLE_PING)
-	mad_unpack_int(MAD_IN_HEADER, &arg->version, 1);
+	old_mad_unpack_int(MAD_IN_HEADER, &arg->version, 1);
 END_STUB
 
 PACK_RES_STUB(LRPC_CONSOLE_PING)
-	mad_pack_int(MAD_IN_HEADER, &arg->version, 1);
-	mad_pack_int(MAD_IN_HEADER, &arg->tid, 1);
+	old_mad_pack_int(MAD_IN_HEADER, &arg->version, 1);
+	old_mad_pack_int(MAD_IN_HEADER, &arg->tid, 1);
 END_STUB
 
 marcel_sem_t _ping_sem;
 LRPC_REQ(LRPC_CONSOLE_PING) _ping_req = { 0 };
 
 UNPACK_RES_STUB(LRPC_CONSOLE_PING)
-   mad_unpack_int(MAD_IN_HEADER, &arg->version, 1);
-   mad_unpack_int(MAD_IN_HEADER, &arg->tid, 1);
+   old_mad_unpack_int(MAD_IN_HEADER, &arg->version, 1);
+   old_mad_unpack_int(MAD_IN_HEADER, &arg->tid, 1);
    if(arg->version == _ping_req.version)
       marcel_sem_V(&_ping_sem);
    else
@@ -160,18 +160,18 @@ static BEGIN_SERVICE(LRPC_CONSOLE_USER)
 END_SERVICE(LRPC_CONSOLE_USER)
 
 PACK_REQ_STUB(LRPC_CONSOLE_USER)
-   mad_pack_byte(MAD_BY_COPY, arg->tab, sizeof(_user_args_tab));
+   old_mad_pack_byte(MAD_BY_COPY, arg->tab, sizeof(_user_args_tab));
 END_STUB
 
 UNPACK_REQ_STUB(LRPC_CONSOLE_USER)
-   mad_unpack_byte(MAD_BY_COPY, arg->tab, sizeof(_user_args_tab));
+   old_mad_unpack_byte(MAD_BY_COPY, arg->tab, sizeof(_user_args_tab));
 END_STUB
 
 PACK_RES_STUB(LRPC_CONSOLE_USER)
    int i = 0, argc = 0;
    char *argv[128];
 
-	mad_pack_int(MAD_IN_HEADER, &arg->tid, 1);
+	old_mad_pack_int(MAD_IN_HEADER, &arg->tid, 1);
 	if(user_function) {
                 while(arg->tab[i] != 0) {
                         argv[argc++] = &arg->tab[i];
@@ -182,21 +182,21 @@ PACK_RES_STUB(LRPC_CONSOLE_USER)
 		}
 		(*user_function)(argc, argv);
 	} else
-		mad_pack_str(MAD_IN_HEADER, "<User function not defined>");
-	mad_pack_str(MAD_IN_HEADER, "");
+		old_mad_pack_str(MAD_IN_HEADER, "<User function not defined>");
+	old_mad_pack_str(MAD_IN_HEADER, "");
 END_STUB
 
 UNPACK_RES_STUB(LRPC_CONSOLE_USER)
    char str[255];
    int tid;
 
-   mad_unpack_int(MAD_IN_HEADER, &tid, 1);
-   mad_unpack_str(MAD_IN_HEADER, str);
+   old_mad_unpack_int(MAD_IN_HEADER, &tid, 1);
+   old_mad_unpack_str(MAD_IN_HEADER, str);
    if(strcmp(str, "")) {
       tprintf("Task [%lx]\n", tid);
       do {
 	tprintf("\t%s\n",str);
-	mad_unpack_str(MAD_IN_HEADER, str);
+	old_mad_unpack_str(MAD_IN_HEADER, str);
       } while (strcmp(str,""));
    }
 END_STUB
@@ -207,15 +207,15 @@ static BEGIN_SERVICE(LRPC_CONSOLE_MIGRATE)
 END_SERVICE(LRPC_CONSOLE_MIGRATE)
 
 PACK_REQ_STUB(LRPC_CONSOLE_MIGRATE)
-	mad_pack_int(MAD_BY_COPY, &arg->destination, 1);
-        mad_pack_str(MAD_IN_HEADER, arg->thread_id);
+	old_mad_pack_int(MAD_BY_COPY, &arg->destination, 1);
+        old_mad_pack_str(MAD_IN_HEADER, arg->thread_id);
 END_STUB
 
 UNPACK_REQ_STUB(LRPC_CONSOLE_MIGRATE)
 	char thread_id[16];
 
-	mad_unpack_int(MAD_BY_COPY, &arg->destination, 1);
-        mad_unpack_str(MAD_IN_HEADER, thread_id);
+	old_mad_unpack_int(MAD_BY_COPY, &arg->destination, 1);
+        old_mad_unpack_str(MAD_IN_HEADER, thread_id);
 	arg->thread = xtoi(thread_id);
 END_STUB
 
@@ -230,13 +230,13 @@ static BEGIN_SERVICE(LRPC_CONSOLE_FREEZE)
 END_SERVICE(LRPC_CONSOLE_FREEZE)
 
 PACK_REQ_STUB(LRPC_CONSOLE_FREEZE)
-        mad_pack_str(MAD_IN_HEADER, arg->thread_id);
+        old_mad_pack_str(MAD_IN_HEADER, arg->thread_id);
 END_STUB
 
 UNPACK_REQ_STUB(LRPC_CONSOLE_FREEZE)
 	char thread_id[16];
 
-        mad_unpack_str(MAD_IN_HEADER, thread_id);
+        old_mad_unpack_str(MAD_IN_HEADER, thread_id);
 	arg->thread = xtoi(thread_id);
 END_STUB
 

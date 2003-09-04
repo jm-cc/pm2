43435ef54e7dd8b413ee10f4d1270c1319749ea3
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:08:24 +0000

    rev 1.3 at 2000/01/21 09:53:13 GMT  Changed old_mad_pack functions to pm2_pack ones.
    (Logical change 1.51)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@472 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/pm2_attr.c
+++ pm2/pm2/pm2/source/pm2_attr.c
@@ -38,7 +38,8 @@
 pm2_attr_t pm2_attr_default = {
   STD_PRIO,           /* priority */
   MARCEL_SCHED_OTHER, /* scheduling policy */
-  0                   /* channel number */
+  0,                  /* channel number */
+  NULL                /* completion ptr */
 };
 
 int pm2_attr_init(pm2_attr_t *attr)
@@ -84,3 +85,16 @@ int pm2_attr_getchannel(pm2_attr_t *attr, pm2_channel_t *channel)
    *channel = attr->channel;
    return 0;
 }
+
+int pm2_attr_setcompletion(pm2_attr_t *attr, pm2_completion_t *completion)
+{
+  attr->completion = completion;
+  return 0;
+}
+
+int pm2_attr_getcompletion(pm2_attr_t *attr, pm2_completion_t **completion)
+{
+  *completion = attr->completion;
+  return 0;
+}
+

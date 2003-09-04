495f3a5cb6186bcb94d878b73f025afc8ab5dfbb
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:42:08 +0000

    rev 1.3 at 2000/02/14 10:00:37 GMT  Modified to propose a new interface to the "pm2_completion" facilities.
    (Logical change 1.76)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@818 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/pm2_types.h
+++ pm2/pm2/pm2/include/pm2_types.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: pm2_types.h,v $
+Revision 1.3  2000/02/14 10:00:37  rnamyst
+Modified to propose a new interface to the "pm2_completion" facilities.
+
 Revision 1.2  2000/01/31 15:49:47  oaumage
 - ajout du Log CVS
 
@@ -46,10 +49,23 @@ ______________________________________________________________________________
 
 #include <marcel.h>
 
-_PRIVATE_ typedef struct {
+typedef void (*pm2_startup_func_t)(void);
+
+typedef void (*pm2_rawrpc_func_t)(void);
+
+typedef void (*pm2_completion_handler_t)(void *);
+
+typedef void (*pm2_pre_migration_hook)(marcel_t pid);
+typedef any_t (*pm2_post_migration_hook)(marcel_t pid);
+typedef void (*pm2_post_post_migration_hook)(any_t key);
+
+
+_PRIVATE_ typedef struct pm2_completion_struct_t {
   marcel_sem_t sem;
   unsigned proc;
-  marcel_sem_t *sem_ptr;
+  struct pm2_completion_struct_t *c_ptr;
+  pm2_completion_handler_t handler;
+  void *arg;
 } pm2_completion_t;
 
 _PRIVATE_ typedef struct {

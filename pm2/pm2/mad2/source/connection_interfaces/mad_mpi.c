31d3837755344064381fb576c649669d6c604465
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:52:56 +0000

    rev 1.5 at 2000/01/05 15:52:43 GMT  - ajout du support PM2 avec polling optimise
    (Logical change 1.31)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@290 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/source/connection_interfaces/mad_mpi.c
+++ pm2/pm2/mad2/source/connection_interfaces/mad_mpi.c
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: mad_mpi.c,v $
+Revision 1.5  2000/01/05 15:52:43  oaumage
+- ajout du support PM2 avec polling optimise
+
 Revision 1.4  2000/01/05 09:43:58  oaumage
 - initialisation du nouveau champs `group_mode' dans link_init
 - mad_sbp.c: suppression des fonctions vides
@@ -73,8 +76,8 @@ ______________________________________________________________________________
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h> /* for `gethostname' */
-#include <netdb.h>  /* for `MAXHOSTNAMELEN' */
+#include <unistd.h> 
+#include <netdb.h>
 
 /* protocol specific header files */
 #include <mpi.h>
@@ -83,9 +86,15 @@ ______________________________________________________________________________
  * macros and constants definition
  * -------------------------------
  */
-#define MAD_MPI_SERVICE_TAG  0
-#define MAD_MPI_TRANSFER_TAG 1
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN 256
+#endif /* MAXHOSTNAMELEN */
+#define MAD_MPI_SERVICE_TAG      0
+#define MAD_MPI_TRANSFER_TAG     1
 #define MAD_MPI_FLOW_CONTROL_TAG 2
+#ifdef PM2
+#define MAX_MPI_REQ 64
+#endif /* PM2 */
 
 /*
  * type definition
@@ -97,9 +106,25 @@ ______________________________________________________________________________
  * local structures
  * ----------------
  */
+#ifdef PM2
+typedef struct 
+{
+  marcel_pollinst_t pollinst;
+  MPI_Request       request;
+  MPI_Status        status;
+} mad_mpi_poll_arg_t, *p_mad_mpi_poll_arg_t;
+#endif /* PM2 */
+
 typedef struct
 {
+  PM2_SHARED;
   int nb_adapter;
+#ifdef PM2
+  marcel_pollid_t      poll_id;
+  unsigned             poll_nb_req;
+  MPI_Request          poll_req[MAX_MPI_REQ];
+  p_mad_mpi_poll_arg_t poll_arg[MAX_MPI_REQ];
+#endif /* PM2 */
 } mad_mpi_driver_specific_t, *p_mad_mpi_driver_specific_t;
 
 typedef struct
@@ -123,7 +148,103 @@ typedef struct
  * static functions
  * ----------------
  */
+#ifdef PM2
+static p_mad_mpi_driver_specific_t mad_mpi_driver_specific;
 
+static void mpi_io_group(marcel_pollid_t id)
+{
+  p_mad_mpi_poll_arg_t arg;
+  unsigned             count = 0;
+  
+  LOG_IN();
+  FOREACH_POLL(id, arg) 
+    {
+      arg->pollinst           = GET_CURRENT_POLLINST(id);
+      mad_mpi_driver_specific->poll_req[count] = arg->request;
+      mad_mpi_driver_specific->poll_arg[count] = arg;
+      count++;
+    }
+  mad_mpi_driver_specific->poll_nb_req = count;
+  LOG_OUT();
+}
+
+static void *mpi_io_poll(marcel_pollid_t id,
+			 unsigned        active,
+			 unsigned        sleeping,
+			 unsigned        blocked)
+{
+  LOG_IN();
+  if(PM2_TRYLOCK_SHARED(mad_mpi_driver_specific))
+    {
+      int flag;
+      int index;
+      MPI_Status status;
+  
+      MPI_Testany(mad_mpi_driver_specific->poll_nb_req,
+		  mad_mpi_driver_specific->poll_req,
+		  &index,
+		  &flag,
+		  &status);      
+      PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+      
+      if(flag) 
+	{
+	  mad_mpi_driver_specific->poll_arg[index]->status = status;
+	  LOG_OUT();
+	  return MARCEL_POLL_SUCCESS_FOR(mad_mpi_driver_specific->
+					 poll_arg[index]->pollinst);
+	}
+    }
+  LOG_OUT();
+  return MARCEL_POLL_FAILED;
+}
+
+static void mad_mpi_send(void         *buffer,
+			 int           count,
+			 MPI_Datatype  type,
+			 int           destination,
+			 int           tag,
+			 MPI_Comm      communicator,
+			 MPI_Status   *status)
+{
+  mad_mpi_poll_arg_t arg;
+
+  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+  MPI_Isend(buffer,
+	    count, 
+	    type, 
+	    destination,
+	    tag,
+	    communicator,
+	    &arg.request);
+  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+  marcel_poll(mad_mpi_driver_specific->poll_id, (any_t)&arg);
+  *status = arg.status;
+}
+
+static void mad_mpi_recv(void         *buffer,
+			 int           count,
+			 MPI_Datatype  type,
+			 int           source,
+			 int           tag,
+			 MPI_Comm      communicator,
+			 MPI_Status   *status)
+{
+  mad_mpi_poll_arg_t arg;
+
+  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+  MPI_Irecv(buffer,
+	    count,
+	    type, 
+	    source,
+	    tag,
+	    communicator, 
+	    &arg.request);
+  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+  marcel_poll(mad_mpi_driver_specific->poll_id, (any_t)&arg);
+  *status = arg.status;
+}
+#endif /* PM2 */
 
 /*
  * exported functions
@@ -192,8 +313,14 @@ mad_mpi_driver_init(p_mad_driver_t driver)
   driver_specific = malloc(sizeof(mad_mpi_driver_specific_t));
   CTRL_ALLOC(driver_specific);
   driver->specific = driver_specific;
+  PM2_INIT_SHARED(driver_specific);
   driver_specific->nb_adapter = 0;
 
+#ifdef PM2
+  mad_mpi_driver_specific = driver_specific;
+  mad_mpi_driver_specific->poll_id =
+    marcel_pollid_create(mpi_io_group, mpi_io_poll, 1);
+#endif /* PM2 */
   LOG_OUT();
 }
 
@@ -241,7 +368,9 @@ mad_mpi_channel_init(p_mad_channel_t channel)
   channel_specific = malloc(sizeof(mad_mpi_channel_specific_t));
   CTRL_ALLOC(channel_specific);
 
+  PM2_LOCK_SHARED(mad_mpi_driver_specific);
   MPI_Comm_dup(MPI_COMM_WORLD, &channel_specific->communicator);
+  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
   channel->specific = channel_specific;
   LOG_OUT();
 }
@@ -264,7 +393,9 @@ mad_mpi_channel_exit(p_mad_channel_t channel)
   
   LOG_IN();
   /* Code to execute to clean up a channel */
+  PM2_LOCK_SHARED(mad_mpi_driver_specific);
   MPI_Comm_free(&channel_specific->communicator);
+  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
   free(channel_specific);
   channel->specific = NULL;
   LOG_OUT();
@@ -273,8 +404,6 @@ mad_mpi_channel_exit(p_mad_channel_t channel)
 void
 mad_mpi_adapter_exit(p_mad_adapter_t adapter)
 {
-  p_mad_mpi_adapter_specific_t adapter_specific = adapter->specific;
-  
   LOG_IN();
   /* Code to execute to clean up an adapter */
   free(adapter->parameter);
@@ -286,10 +415,20 @@ mad_mpi_adapter_exit(p_mad_adapter_t adapter)
 void
 mad_mpi_driver_exit(p_mad_driver_t driver)
 {
-  p_mad_mpi_driver_specific_t driver_specific = driver->specific;
-  
+#ifdef PM2
+  unsigned count;
+#endif /* PM2 */  
   LOG_IN();
   /* Code to execute to clean up a driver */
+  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+#ifdef PM2
+  for (count = 0;
+       count < mad_mpi_driver_specific->poll_nb_req;
+       count++)
+    {
+      MPI_Cancel(&(mad_mpi_driver_specific->poll_req[count]));
+    }
+#endif /* PM2 */  
   MPI_Finalize();
   LOG_OUT();
 }
@@ -302,18 +441,29 @@ mad_mpi_new_message(p_mad_connection_t connection)
   p_mad_mpi_channel_specific_t channel_specific = channel->specific;
   p_mad_configuration_t        configuration    = 
     &(channel->adapter->driver->madeleine->configuration);
+#ifdef PM2
+  MPI_Status status;
+#endif /* PM2 */
   LOG_IN();
   /* Code to prepare a new message */
-#ifndef PM2
+#ifdef PM2
+  mad_mpi_send(&configuration->local_host_id,
+	       1,
+	       MPI_INT,
+	       connection->remote_host_id,
+	       MAD_MPI_SERVICE_TAG,
+	       channel_specific->communicator,
+	       &status);
+#else
   if (configuration->size == 2)
     return;
-#endif /* PM2 */
   MPI_Send(&configuration->local_host_id,
 	   1,
 	   MPI_INT,
 	   connection->remote_host_id,
 	   MAD_MPI_SERVICE_TAG,
 	   channel_specific->communicator);
+#endif /* PM2 */
   LOG_OUT();
 }
 
@@ -321,24 +471,33 @@ p_mad_connection_t
 mad_mpi_receive_message(p_mad_channel_t channel)
 {
   p_mad_mpi_channel_specific_t channel_specific = channel->specific;
+#ifndef PM2
   p_mad_configuration_t configuration    = 
     &(channel->adapter->driver->madeleine->configuration);
+#endif /* PM2 */
   p_mad_connection_t    connection       = NULL;
+  int                   remote_host_id;
+  MPI_Status            status;
 
   LOG_IN();
-#ifndef PM2
+#ifdef PM2
+  mad_mpi_recv(&remote_host_id,
+	       1,
+	       MPI_INT,
+	       MPI_ANY_SOURCE,
+	       MAD_MPI_SERVICE_TAG,
+	       channel_specific->communicator,
+	       &status);
+  connection = &(channel->input_connection[remote_host_id]);
+#else /* PM2 */
   if (configuration->size == 2)
     {
       connection =
 	&(channel->input_connection[1 - configuration->local_host_id]);
     }
   else
-#endif /* PM2 */
     {
       /* Incoming communication detection code */      
-      int remote_host_id;
-      MPI_Status status;
-      
       MPI_Recv(&remote_host_id,
 	       1,
 	       MPI_INT,
@@ -348,6 +507,7 @@ mad_mpi_receive_message(p_mad_channel_t channel)
 	       &status);
       connection = &(channel->input_connection[remote_host_id]);
     }
+#endif /* PM2 */
   LOG_OUT();
   return connection;
 }
@@ -359,16 +519,28 @@ mad_mpi_send_buffer(p_mad_link_t     lnk,
   p_mad_connection_t           connection       = lnk->connection;
   p_mad_channel_t              channel          = connection->channel;
   p_mad_mpi_channel_specific_t channel_specific = channel->specific;
+#ifdef PM2
+  MPI_Status status;
+#endif /* PM2 */
   
   LOG_IN();
-  /* Code to send one buffer */
-
+/* Code to send one buffer */
+#ifdef PM2
+  mad_mpi_send(buffer->buffer,
+	       buffer->length,
+	       MPI_CHAR,
+	       connection->remote_host_id,
+	       MAD_MPI_TRANSFER_TAG,
+	       channel_specific->communicator,
+	       &status);
+#else /* PM2 */
   MPI_Send(buffer->buffer,
 	   buffer->length,
 	   MPI_CHAR,
 	   connection->remote_host_id,
 	   MAD_MPI_TRANSFER_TAG,
 	   channel_specific->communicator);
+#endif /* PM2 */
   buffer->bytes_read = buffer->length;
   LOG_OUT();
 }
@@ -385,6 +557,15 @@ mad_mpi_receive_buffer(p_mad_link_t     lnk,
   
   LOG_IN();
   /* Code to receive one buffer */
+#ifdef PM2
+  mad_mpi_recv(buffer->buffer,
+	       buffer->length,
+	       MPI_CHAR,
+	       connection->remote_host_id,
+	       MAD_MPI_TRANSFER_TAG,
+	       channel_specific->communicator,
+	       &status);
+#else /* PM2 */
   MPI_Recv(buffer->buffer,
 	   buffer->length,
 	   MPI_CHAR,
@@ -392,6 +573,7 @@ mad_mpi_receive_buffer(p_mad_link_t     lnk,
 	   MAD_MPI_TRANSFER_TAG,
 	   channel_specific->communicator,
 	   &status);
+#endif /* PM2 */
   buffer->bytes_written = buffer->length;
   LOG_OUT();
 }
@@ -405,13 +587,66 @@ mad_mpi_send_buffer_group(p_mad_link_t           lnk,
   if (!mad_empty_list(&(buffer_group->buffer_list)))
     {
       mad_list_reference_t ref;
+      int                  buffer_count = buffer_group->buffer_list.length;
 
       mad_list_reference_init(&ref, &(buffer_group->buffer_list));
-      do
+      
+      if (buffer_count == 1)
 	{
 	  mad_mpi_send_buffer(lnk, mad_get_list_reference_object(&ref));
 	}
-      while(mad_forward_list_reference(&ref));
+      else
+	{
+	  p_mad_connection_t           connection       = lnk->connection;
+	  p_mad_channel_t              channel          = connection->channel;
+	  p_mad_mpi_channel_specific_t channel_specific = channel->specific;
+	  p_mad_buffer_t               buffer;
+	  int                          length[buffer_count];
+	  MPI_Aint                     displacement[buffer_count];
+	  MPI_Datatype                 hindexed;
+	  int                          count = 0;
+#ifdef PM2
+	  MPI_Status status;
+#endif /* PM2 */
+
+	  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+	  do
+	    {
+	      buffer = mad_get_list_reference_object(&ref);
+
+	      MPI_Address(buffer->buffer, displacement + count);	      
+	      length[count] = buffer->length;
+	      count ++;
+	    }
+	  while(mad_forward_list_reference(&ref));
+
+	  MPI_Type_hindexed(buffer_count,
+			    length,
+			    displacement,
+			    MPI_BYTE,
+			    &hindexed);
+	  MPI_Type_commit(&hindexed);
+	  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+#ifdef PM2
+	  mad_mpi_send(MPI_BOTTOM,
+		       1,
+		       hindexed,
+		       connection->remote_host_id,
+		       MAD_MPI_TRANSFER_TAG,
+		       channel_specific->communicator,
+		       &status);
+#else /* PM2 */
+	  MPI_Send(MPI_BOTTOM,
+		   1,
+		   hindexed,
+		   connection->remote_host_id,
+		   MAD_MPI_TRANSFER_TAG,
+		   channel_specific->communicator);
+#endif /* PM2 */
+	  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+	  MPI_Type_free(&hindexed);
+	  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+	}
     }
   LOG_OUT();
 }
@@ -421,23 +656,78 @@ mad_mpi_receive_sub_buffer_group(p_mad_link_t           lnk,
 				 mad_bool_t             first_sub_group,
 				 p_mad_buffer_group_t   buffer_group)
 {
+  /* Code to receive a group of buffers */
   LOG_IN();
-  /*
-   *  Code to receive a part of a group of buffers
-   *                    ====
-   */
+
+  /* The selected group mode is `split', hence the groups should be
+     splitted correctly*/
+  if (!first_sub_group)
+    FAILURE("group split error");
 
   if (!mad_empty_list(&(buffer_group->buffer_list)))
     {
       mad_list_reference_t ref;
+      int                  buffer_count = buffer_group->buffer_list.length;
 
       mad_list_reference_init(&ref, &(buffer_group->buffer_list));
-      do
+      
+      if (buffer_count == 1)
 	{
 	  p_mad_buffer_t buffer = mad_get_list_reference_object(&ref);
+
 	  mad_mpi_receive_buffer(lnk, &buffer);
 	}
-      while(mad_forward_list_reference(&ref));
+      else
+	{
+	  p_mad_connection_t           connection       = lnk->connection;
+	  p_mad_channel_t              channel          = connection->channel;
+	  p_mad_mpi_channel_specific_t channel_specific = channel->specific;
+	  p_mad_buffer_t               buffer;
+	  int                          length[buffer_count];
+	  MPI_Aint                     displacement[buffer_count];
+	  MPI_Datatype                 hindexed;
+	  MPI_Status                   status;
+	  int                          count = 0;
+	  
+	  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+	  do
+	    {
+	      buffer = mad_get_list_reference_object(&ref);
+
+	      MPI_Address(buffer->buffer, displacement + count);	      
+	      length[count] = buffer->length;
+	      count ++;
+	    }
+	  while(mad_forward_list_reference(&ref));
+
+	  MPI_Type_hindexed(buffer_count,
+			    length,
+			    displacement,
+			    MPI_BYTE,
+			    &hindexed);
+	  MPI_Type_commit(&hindexed);
+	  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+#ifdef PM2
+	  mad_mpi_recv(MPI_BOTTOM,
+		       1,
+		       hindexed,
+		       connection->remote_host_id,
+		       MAD_MPI_TRANSFER_TAG,
+		       channel_specific->communicator,
+		       &status);
+#else /* PM2 */
+	  MPI_Recv(MPI_BOTTOM,
+		   1,
+		   hindexed,
+		   connection->remote_host_id,
+		   MAD_MPI_TRANSFER_TAG,
+		   channel_specific->communicator,
+		   &status);	  
+#endif /* PM2 */
+	  PM2_LOCK_SHARED(mad_mpi_driver_specific);
+	  MPI_Type_free(&hindexed);
+	  PM2_UNLOCK_SHARED(mad_mpi_driver_specific);
+	}
     }
   LOG_OUT();
 }
@@ -458,8 +748,6 @@ void
 mad_mpi_configuration_init(p_mad_adapter_t       spawn_adapter,
 			   p_mad_configuration_t configuration)
 {
-  p_mad_mpi_adapter_specific_t spawn_adapter_specific =
-    spawn_adapter->specific;
   mad_host_id_t                host_id;
   int                          rank;
   int                          size;

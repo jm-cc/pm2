5b9334f56290c75ec8142b9bdba8f2d2a6e37d7a
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:42:49 +0000

    rev 1.3 at 2000/03/27 12:54:05 GMT  - progression des fonctionnalites:   * support reseau   * support execution   * extension du support fichier
    (Logical change 1.109)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1358 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/swann/source/swann.c
+++ pm2/pm2/swann/source/swann.c
@@ -34,6 +34,12 @@
 
 ______________________________________________________________________________
 $Log: swann.c,v $
+Revision 1.3  2000/03/27 12:54:05  oaumage
+- progression des fonctionnalites:
+  * support reseau
+  * support execution
+  * extension du support fichier
+
 Revision 1.2  2000/02/17 09:26:29  oaumage
 - Fichier principal de Swann
 
@@ -50,14 +56,87 @@ ______________________________________________________________________________
 #include <stdio.h>
 #include <string.h>
 
-#include <swann.h>
+#include "swann.h"
 
 int 
 main(int   argc   __attribute__((unused)),
      char *argv[] __attribute__((unused)))
 {
+  p_swann_net_server_t   net_server         = NULL;
+  int                    param_id           = 0;
+  swann_net_server_id_t  id                 = -1;
+  int                    param_master       = 0;
+  char                  *master_host_name   = NULL;
+  int                    param_cnx          = 0;
+  ntbx_connection_data_t connection_data    = {0};
+
+  /* Toolboxes initialization */
   tbx_init();
   ntbx_init();
+
+  /* Command line parsing */
+  {
+    int i;
+
+    i = 1;
+    
+    while (i < argc)
+      {
+	if(!strcmp(argv[i], "-id"))
+	{
+	  if (param_id)
+	    FAILURE("id parameter already specified");
+
+	  param_id++;
+
+	  if (i == (argc - 1))
+	    FAILURE("-id option must be followed "
+		    "by the id of the process");
+
+	  id = atoi(argv[i + 1]);
+	  i++;
+	}
+      else if (!strcmp(argv[i], "-cnx"))
+	{
+	  if (param_cnx)
+	    FAILURE("cnx parameter already specified");
+	  
+	  param_cnx++;
+
+	  if (i == (argc - 1))
+	    FAILURE("-cnx must be followed "
+		    "by the master node connection parameter");
+
+	  strcpy(connection_data, argv[i + 1]);
+	  i++;
+	}
+      else if (!strcmp(argv[i], "-master"))
+	{
+	  if (param_master)
+	    FAILURE("master parameter already specified");
+
+	  param_master++;
+	  if(i == (argc - 1))
+	    FAILURE(" -master must be followed by the master host name");
+
+	  master_host_name = TBX_MALLOC(strlen(argv[i + 1]) + 1);
+	  CTRL_ALLOC(master_host_name);
+	    
+	  strcpy(master_host_name,
+		 argv[i + 1]);
+	  i++;
+	}
+      else
+	FAILURE("invalid parameter");
+	
+      i++;
+    }
+  }
+
+  /* Net server initialization */
+  net_server = swann_net_server_init(id,
+				     master_host_name,
+				     &connection_data);
   
   return 0;
 }

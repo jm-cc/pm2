770cba11ca224b94dd34819e70704dcb2c197447
ens-lyon.fr!jfmehaut <ens-lyon.fr!jfmehaut@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:12:58 +0000

    rev 1.4 at 2000/03/02 09:52:21 GMT  pilote Madeleine II/BIP
    (Logical change 1.87)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1118 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/examples/mad_ping.c
+++ pm2/pm2/mad2/examples/mad_ping.c
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: mad_ping.c,v $
+Revision 1.4  2000/03/02 09:52:21  jfmehaut
+pilote Madeleine II/BIP
+
 Revision 1.3  2000/03/01 14:20:07  oaumage
 - restauration de mad_ping (remplissage du buffer de test, pour sisci)
 
@@ -95,12 +98,12 @@ ______________________________________________________________________________
 static int param_control_receive = 0;
 static int param_send_mode       = mad_send_CHEAPER;
 static int param_receive_mode    = mad_receive_CHEAPER;
-static int param_nb_echantillons = 1000;
+static int param_nb_echantillons = 1001;
 static int param_min_size        = 16;
 static int param_max_size        = 256;
 static int param_step            = 16; /* 0 = progression log. */
 static int param_nb_tests        = 5;
-static int param_bandwidth       = 1;
+static int param_bandwidth       = 0;
 static int param_no_zero         = 1;
 static int param_simul_migr      = 0;
 static int param_fill_buffer     = 1;
@@ -495,13 +498,15 @@ int main(int argc, char **argv)
   /* VIA - ethernet 
      adapter_list = mad_adapter_list_init(1, mad_VIA, "/dev/via_eth0"); */
   /* TCP */
-  adapter_set = mad_adapter_set_init(1, mad_TCP, NULL); 
+  /* adapter_set = mad_adapter_set_init(1, mad_TCP, NULL); */
   /* SISCI 
      adapter_set = mad_adapter_set_init(1, mad_SISCI, NULL); */
   /* SBP 
      adapter_set = mad_adapter_set_init(1, mad_SBP, NULL); */
   /* MPI 
      adapter_set = mad_adapter_set_init(1, mad_MPI, NULL);  */
+  /* BIP */
+     adapter_set = mad_adapter_set_init(1, mad_BIP, NULL);  
 
   madeleine = mad_init(&argc, argv, NULL, adapter_set);
 

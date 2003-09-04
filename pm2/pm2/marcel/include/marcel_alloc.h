8e385f1e614d4aa4e3dac492978e0c9f181dc71a
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:21:19 +0000

    rev 1.2 at 2000/01/31 15:56:19 GMT  - ajout du Log CVS
    (Logical change 1.62)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@560 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_alloc.h
+++ pm2/pm2/marcel/include/marcel_alloc.h
@@ -31,6 +31,14 @@
  Fondamentale de Lille), nor the Authors make any representations
  about the suitability of this software for any purpose. This
  software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: marcel_alloc.h,v $
+Revision 1.2  2000/01/31 15:56:19  oaumage
+- ajout du Log CVS
+
+
+______________________________________________________________________________
 */
 
 #ifndef MARCEL_ALLOC_EST_DEF

2375e0b000329d7be61f7052987e41124fb66634
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:10:58 +0000

    rev 1.3 at 2000/01/23 19:19:05 GMT  - dsm_comm has been upgraded to the new-style RPC - optimizations in page_manager
    (Logical change 1.56)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@510 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_comm.c
+++ pm2/pm2/dsm/source/dsm_comm.c
@@ -43,28 +43,29 @@
 
 
 //#define DEBUG1
-#define DEBUG_HYP
+//#define DEBUG_HYP
+#define ASSERT
 
 void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_access_t req_access)
 {
+#ifdef ASSERT
   assert(dest_node != dsm_self());
+#endif
   switch(req_access){
   case READ_ACCESS: 
     {
-      LRPC_REQ(DSM_LRPC_READ_PAGE_REQ) req;
-
-      req.index = index;
-      req.req_node = req_node;
-      pm2_async_rpc((int)dest_node, DSM_LRPC_READ_PAGE_REQ, NULL, &req);
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_READ_PAGE_REQ, NULL);
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
+      pm2_rawrpc_end();
       break;
     }
   case WRITE_ACCESS: 
     {
-      LRPC_REQ(DSM_LRPC_WRITE_PAGE_REQ) req;
-
-      req.index = index;
-      req.req_node = req_node;
-      pm2_async_rpc((int)dest_node, DSM_LRPC_WRITE_PAGE_REQ, NULL, &req);
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_WRITE_PAGE_REQ, NULL);
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
+      pm2_rawrpc_end();
       break;
     }
   default:
@@ -77,86 +78,169 @@ void dsm_invalidate_copyset(unsigned long index, dsm_node_t new_owner)
   (*dsm_get_invalidate_server(index))(index, dsm_self(), new_owner);
 }
 
-void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_node_t reply_node, dsm_access_t access)
+void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_access_t access)
 {
-  LRPC_REQ(DSM_LRPC_SEND_PAGE) req;
+  void *addr = dsm_get_page_addr(index);
+  unsigned long page_size = dsm_get_page_size(index);
+  dsm_node_t reply_node = dsm_self();
 
-  req.addr = dsm_get_page_addr(index);
-  req.page_size = dsm_get_page_size(index);
-  req.reply_node = dsm_self();
-  req.access = access;
-
-  pm2_async_rpc((int)dest_node, DSM_LRPC_SEND_PAGE, NULL, &req);
+  pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_PAGE, NULL);
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size, sizeof(unsigned long));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&reply_node, sizeof(dsm_node_t));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&access, sizeof(dsm_access_t));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, page_size); 
+  pm2_rawrpc_end();
 #ifdef DEBUG3
-     fprintf(stderr, "Page %d sent -> %d for %s, a = %d \n", index, dest_node, (access == 1)?"read":"write", ((atomic_t *)req.addr)->counter);
+  fprintf(stderr, "Page %d sent -> %d for %s, a = %d \n", index, dest_node, (access == 1)?"read":"write", ((atomic_t *)addr)->counter);
 #endif
 }
 
 void dsm_send_invalidate_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_node_t new_owner)
 {
-  LRPC_REQ(DSM_LRPC_INVALIDATE_REQ) req;
-
-  req.index = index;
-  req.req_node = req_node;
-  req.new_owner = new_owner;
-  pm2_async_rpc((int)dest_node, DSM_LRPC_INVALIDATE_REQ, NULL, &req);
+  pm2_rawrpc_begin((int)dest_node, DSM_LRPC_INVALIDATE_REQ, NULL);
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&new_owner, sizeof(dsm_node_t));
+  pm2_rawrpc_end();
 }
 
 void dsm_send_invalidate_ack(dsm_node_t dest_node, unsigned long index)
 {
-  LRPC_REQ(DSM_LRPC_INVALIDATE_ACK) req;
-
-  req.index = index;
-  pm2_quick_async_rpc((int)dest_node, DSM_LRPC_INVALIDATE_ACK, NULL, &req);
+  pm2_rawrpc_begin((int)dest_node, DSM_LRPC_INVALIDATE_ACK, NULL);
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_rawrpc_end();
 }
 
 
-BEGIN_SERVICE(DSM_LRPC_READ_PAGE_REQ)
-#ifdef DEBUG1
-     fprintf(stderr,"will enter RS...(I am %p)\n", marcel_self());
-#endif
-     (*dsm_get_read_server(req.index))(req.index, req.req_node);
-END_SERVICE(DSM_LRPC_READ_PAGE_REQ)
+static void DSM_LRPC_READ_PAGE_REQ_threaded_func(void)
+{
+  dsm_node_t node;
+  unsigned long index;
 
-BEGIN_SERVICE(DSM_LRPC_WRITE_PAGE_REQ)
-#ifdef DEBUG1
-     fprintf(stderr,"will enter WS...(I am %p)\n", marcel_self());
-#endif
-     (*dsm_get_write_server(req.index))(req.index, req.req_node);
-END_SERVICE(DSM_LRPC_WRITE_PAGE_REQ)
-
-BEGIN_SERVICE(DSM_LRPC_SEND_PAGE)
-     (*dsm_get_receive_page_server(dsm_page_index(req.addr)))(req.addr, req.access, req.reply_node);
-END_SERVICE(DSM_LRPC_SEND_PAGE)
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&node, sizeof(dsm_node_t));
+  pm2_rawrpc_waitdata(); 
+  (*dsm_get_read_server(index))(index, node);
+}
 
 
-BEGIN_SERVICE(DSM_LRPC_INVALIDATE_REQ)
-     dsm_lock_page(req.index);
-     (*dsm_get_invalidate_server(req.index))(req.index, req.req_node, req.new_owner);
-     dsm_unlock_page(req.index);
-END_SERVICE(DSM_LRPC_INVALIDATE_REQ)
+void DSM_LRPC_READ_PAGE_REQ_func(void)
+{
+  pm2_thread_create(DSM_LRPC_READ_PAGE_REQ_threaded_func, NULL);
+}
 
 
-BEGIN_SERVICE(DSM_LRPC_INVALIDATE_ACK)
+static void DSM_LRPC_WRITE_PAGE_REQ_threaded_func(void)
+{
+  dsm_node_t node;
+  unsigned long index;
+
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&node, sizeof(dsm_node_t));
+  pm2_rawrpc_waitdata(); 
+  (*dsm_get_write_server(index))(index, node);
+}
+
+
+void DSM_LRPC_WRITE_PAGE_REQ_func(void)
+{
+  pm2_thread_create(DSM_LRPC_WRITE_PAGE_REQ_threaded_func, NULL);
+}
+
+
+static void DSM_LRPC_SEND_PAGE_threaded_func(void)
+{
+  dsm_node_t reply_node;
+  unsigned long page_size;
+  void * addr;
+  dsm_access_t access, old_access;
+  unsigned long index;
+
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size, sizeof(unsigned long));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&reply_node, sizeof(dsm_node_t));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&access, sizeof(dsm_access_t));
+
+  index = dsm_page_index(addr);
+  old_access = dsm_get_access(index);
+
+  //     fprintf(stderr, "lock_task: I am %p \n", marcel_self());  
+  lock_task();
+  dsm_protect_page(addr, WRITE_ACCESS); // to enable the page to be copied
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, page_size); 
+  if (old_access != access)
+    dsm_protect_page(addr, old_access);
+  unlock_task();
+  pm2_rawrpc_waitdata(); 
+  (*dsm_get_receive_page_server(index))(addr, access, reply_node);
+}
+
+
+void DSM_LRPC_SEND_PAGE_func(void)
+{
+  pm2_thread_create(DSM_LRPC_SEND_PAGE_threaded_func, NULL);
+}
+ 
+
+static void DSM_LRPC_INVALIDATE_REQ_threaded_func(void)
+{
+  dsm_node_t req_node, new_owner;
+  unsigned long index;
+
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&new_owner, sizeof(dsm_node_t));
+  pm2_rawrpc_waitdata(); 
+  dsm_lock_page(index);
+  (*dsm_get_invalidate_server(index))(index, req_node, new_owner);
+  dsm_unlock_page(index);
+}
+
+
+void DSM_LRPC_INVALIDATE_REQ_func(void)
+{
+  pm2_thread_create(DSM_LRPC_INVALIDATE_REQ_threaded_func, NULL);
+}
+
+
+void DSM_LRPC_INVALIDATE_ACK_func(void)
+{
+  unsigned long index;
+  
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
+  pm2_rawrpc_waitdata(); 
 #ifdef DEBUG2
-    tfprintf(stderr, "Received ack(%d)\n", req.index);
+  tfprintf(stderr, "Received ack(%d)\n", index);
 #endif
-    dsm_signal_ack(req.index, 1);
-END_SERVICE(DSM_LRPC_INVALIDATE_ACK)
+  dsm_signal_ack(index, 1);
+}
 
-
-BEGIN_SERVICE(DSM_LRPC_SEND_DIFFS)
-#ifdef DEBUG_HYP
-    tfprintf(stderr, "Received diffs(%d)\n", req.index);
-#endif
-END_SERVICE(DSM_LRPC_SEND_DIFFS)
 
 /***********************  Hyperion stuff: ****************************/
 
 void dsm_send_diffs(unsigned long index, dsm_node_t dest_node)
 {
-   LRPC_REQ(DSM_LRPC_SEND_DIFFS) req;
+  void *addr;
+  int size;
+  
+  pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_DIFFS, NULL);
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index, sizeof(unsigned long));
+  while ((addr = dsm_get_next_modified_data(index, &size)) != NULL)
+    {
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&size, sizeof(int));
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, size);
+    }
+  // send the NULL value to terminate 
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));   
+  pm2_rawrpc_end();
+}
 
-  req.index = index;
-  pm2_async_rpc((int)dest_node, DSM_LRPC_SEND_DIFFS, NULL, &req);
+
+void DSM_LRPC_SEND_DIFFS_func(void)
+{
+#ifdef DEBUG_HYP
+  tfprintf(stderr, "Received diffs(%d)\n", req.index);
+#endif
 }

1063a989272dbeefe46b6503686dbf1db32055a9
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:21:19 +0000

    rev 1.4 at 2000/01/31 15:58:27 GMT  - ajout du Log CVS
    (Logical change 1.62)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@557 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/pm2_attr.c
+++ pm2/pm2/pm2/source/pm2_attr.c
@@ -31,6 +31,14 @@
  Fondamentale de Lille), nor the Authors make any representations
  about the suitability of this software for any purpose. This
  software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: pm2_attr.c,v $
+Revision 1.4  2000/01/31 15:58:27  oaumage
+- ajout du Log CVS
+
+
+______________________________________________________________________________
 */
 
 #include <pm2_attr.h>

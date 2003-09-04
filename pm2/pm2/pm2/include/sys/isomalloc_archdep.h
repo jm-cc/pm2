907fd1cfce300b11ca51b09ead5953eb81c19499
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:21:19 +0000

    rev 1.2 at 2000/01/31 15:50:19 GMT  - ajout du Log CVS
    (Logical change 1.62)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@587 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/sys/isomalloc_archdep.h
+++ pm2/pm2/pm2/include/sys/isomalloc_archdep.h
@@ -31,6 +31,14 @@
  Fondamentale de Lille), nor the Authors make any representations
  about the suitability of this software for any purpose. This
  software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: isomalloc_archdep.h,v $
+Revision 1.2  2000/01/31 15:50:19  oaumage
+- ajout du Log CVS
+
+
+______________________________________________________________________________
 */
 
 #ifndef ISOMALLOC_ARCHDEP_IS_DEF

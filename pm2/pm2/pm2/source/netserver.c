b35d94d7a1e8363430420b46d0b9a998ebc5f80e
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:47:46 +0000

    rev 1.5 at 2000/01/03 09:24:28 GMT  Added pm2_migr.c, pm2_printf.c, pm2_rpc.c & pm2_thread.c files. This is a major release of PM2 that separates the "pm2 micro-kernel" (only providing a raw rpc functionality + thread creation + thread migration) and the "old RPC" (with stubs).
    (Logical change 1.24)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@188 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/netserver.c
+++ pm2/pm2/pm2/source/netserver.c
@@ -40,9 +40,6 @@
 
 #include <pm2_timing.h>
 
-/* Here, it's possible to define USE_DYN_ARRAYS */
-#define USE_DYN_ARRAYS
-
 #define MAX_NETSERVERS   128
 
 static marcel_t _recv_pid[MAX_NETSERVERS];
@@ -50,301 +47,18 @@ static unsigned nb_netservers = 0;
 
 static boolean finished = FALSE;
 
-#define SYSTEM_SERVICE	-1L
+extern pm2_rawrpc_func_t pm2_rawrpc_func[];
 
-extern marcel_key_t _pm2_lrpc_mode_key, _pm2_lrpc_num_key, _pm2_cloning_key;
-extern marcel_sem_t _pm2_critical[];
-
-extern rpc_func_t _pm2_rpc_funcs[];
-extern int _pm2_req_sizes[];
-extern int _pm2_res_sizes[];
-extern marcel_attr_t _pm2_lrpc_attr[];
-
-extern pm2_post_migration_hook _pm2_post_migr_func;
-extern pm2_post_post_migration_hook _pm2_post_post_migr_func;
-
-extern volatile unsigned _pm2_imported_threads;
-
-extern marcel_key_t mad2_send_key, mad2_recv_key;
-
-static void netserver_printf()
-{ 
-  static char buf[2048] __MAD_ALIGNED__;
-  unsigned len;
-
-  mad_unpack_int(MAD_IN_HEADER, &len, 1);
-  mad_unpack_byte(MAD_IN_HEADER, buf, len);
-
-  mad_recvbuf_receive();
-
-  tfprintf(stderr, "%s", buf);
-}
-
-#ifdef MIGRATE_IN_HEADER
-#define MIGR_MODE  MAD_IN_HEADER
-#else
-#define MIGR_MODE  MAD_IN_PLACE
-#endif
-
-#ifdef MAD2
-#define MAD2_TRANSMIT_RECV_CONNECTION(pid) \
-  *(marcel_specificdatalocation((pid), mad2_recv_key)) = marcel_getspecific(mad2_recv_key)
-#else
-#define MAD2_TRANSMIT_RECV_CONNECTION(pid) (void)0
-#endif
-
-
-static void netserver_migration()
+static void netserver_raw_rpc(int num)
 {
-  int nb, i;
-  pm2_migr_ctl mctl;
-
-  mad_unpack_int(MAD_IN_HEADER, &nb, 1);
-
-  {
-#ifdef USE_DYN_ARRAYS
-    marcel_t pids[nb];
-    any_t keys[nb];
-#else
-    marcel_t *pids = tmalloc(nb*sizeof(marcel_t));
-    any_t *keys = tmalloc(nb*sizeof(any_t));
-#endif
-
-    for(i=0; i<nb; i++) {
-
-      mad_unpack_byte(MAD_IN_HEADER, (char *)&mctl, sizeof(mctl));
-      pids[i] = mctl.task;
-
-      slot_general_alloc(NULL, 0, NULL, mctl.stack_base);
-
-      mad_unpack_byte(MIGR_MODE,
-		      (char *)mctl.stack_base + mctl.depl,
-		      mctl.blk);
-
-      block_unpack_all();
-
-      if(_pm2_post_migr_func != NULL)
-	keys[i] = (*_pm2_post_migr_func)((marcel_t)mctl.task);
-    }
-
-    mad_recvbuf_receive();
-
-    for(i=0; i<nb; i++)
-      marcel_end_hibernation(pids[i],
-			     (post_migration_func_t)_pm2_post_post_migr_func,
-			     keys[i]);
-
-#ifndef USE_DYN_ARRAYS
-    tfree(pids);
-    tfree(keys);
-#endif
-  }
-  _pm2_imported_threads += nb;
-}
-
-static void netserver_cloning()
-{
-  marcel_t pid;
-  int rank;
-  unsigned long depl, taille;
-  void **old, **stack_base;
-  any_t key = NULL;
-
-  mad_unpack_int(MAD_IN_HEADER, &rank, 1);
-  mad_unpack_byte(MAD_IN_HEADER, (char *)&old, sizeof(void *));
-
-  stack_base = slot_general_alloc(NULL, 0, NULL, old);
-
-  mad_unpack_byte(MAD_IN_HEADER, (char *)&pid, sizeof(marcel_t));
-
-  mad_unpack_long(MAD_IN_HEADER, &depl, 1);
-  mad_unpack_long(MAD_IN_HEADER, &taille, 1);
-
-  mad_unpack_byte(MIGR_MODE, (char *)stack_base + depl, taille);
-
-  block_unpack_all();
-
-  if(_pm2_post_migr_func != NULL)
-    key = (*_pm2_post_migr_func)((marcel_t)pid);
-
-  mad_recvbuf_receive();
-
-  *((int *)(marcel_specificdatalocation((marcel_t)pid,
-					_pm2_cloning_key))) = rank;
-  marcel_end_hibernation(pid,
-			 (post_migration_func_t)_pm2_post_post_migr_func,
-			 key);
-  _pm2_imported_threads++;
-}
-
-static void netserver_lrpc()
-{
-  int num, tid, priority, sched_policy;
-  rpc_args *args;
-  marcel_attr_t attr;
-  marcel_t pid;
-#ifndef MINIMAL_PREEMPTION
   marcel_sem_t sem;
-#endif
-  unsigned granted;
 
-  mad_unpack_int(MAD_IN_HEADER, &tid, 1);
-  mad_unpack_int(MAD_IN_HEADER, &num, 1);
-  mad_unpack_int(MAD_IN_HEADER, &priority, 1);
-  mad_unpack_int(MAD_IN_HEADER, &sched_policy, 1);
-
-  attr = _pm2_lrpc_attr[num];
-  marcel_attr_setstackaddr(&attr,
-			   slot_general_alloc(NULL, 0, &granted, NULL));
-  marcel_attr_setstacksize(&attr, granted);
-  marcel_attr_setprio(&attr, priority);
-  marcel_attr_setschedpolicy(&attr, sched_policy);
-
-  marcel_create(&pid, &attr, (marcel_func_t)_pm2_rpc_funcs[num], NULL);
-
-  marcel_getuserspace(pid, (void **)&args);
-  mad_unpack_pointer(MAD_IN_HEADER, &args->ptr_att, 1);
-
-  args->num = num;
-  args->tid = tid;
-  args->quick = FALSE;
-  args->sync = TRUE;
-
-#ifndef MINIMAL_PREEMPTION
-  args->sem = &sem;
   marcel_sem_init(&sem, 0);
-#else
-  args->initiator = marcel_self();
-#endif
+  marcel_setspecific(_pm2_mad_key, &sem);
 
-  MAD2_TRANSMIT_RECV_CONNECTION(pid);
+  (*pm2_rawrpc_func[num])();
 
-  marcel_run(pid, args);
-
-#ifdef MINIMAL_PREEMPTION
-  marcel_explicityield(pid);
-#else
   marcel_sem_P(&sem);
-#endif
-}
-
-static void netserver_lrpc_done()
-{
-  pm2_rpc_wait_t *ptr_att;
-  pointer p;
-
-  mad_unpack_pointer(MAD_IN_HEADER, &p, 1);
-  ptr_att = (pm2_rpc_wait_t *)to_any_t(&p);
-  (*ptr_att->unpack)(ptr_att->result);
-  mad_recvbuf_receive();
-  marcel_sem_V(&ptr_att->sem);
-}
-
-static void netserver_async_lrpc()
-{
-  int num, tid, priority, sched_policy;
-  rpc_args *args;
-  marcel_attr_t attr;
-  marcel_t pid;
-#ifndef MINIMAL_PREEMPTION
-  marcel_sem_t sem;
-#endif
-  unsigned granted;
-
-  mad_unpack_int(MAD_IN_HEADER, &tid, 1);
-  mad_unpack_int(MAD_IN_HEADER, &num, 1);
-  mad_unpack_int(MAD_IN_HEADER, &priority, 1);
-  mad_unpack_int(MAD_IN_HEADER, &sched_policy, 1);
-
-  attr = _pm2_lrpc_attr[num];
-  marcel_attr_setstackaddr(&attr,
-			   slot_general_alloc(NULL, 0, &granted, NULL));
-  marcel_attr_setstacksize(&attr, granted);
-  marcel_attr_setprio(&attr, priority);
-  marcel_attr_setschedpolicy(&attr, sched_policy);
-
-  marcel_create(&pid, &attr, (marcel_func_t)_pm2_rpc_funcs[num], NULL);
-
-  marcel_getuserspace(pid, (void **)&args);
-  args->num = num;
-  args->tid = tid;
-  args->quick = FALSE;
-  args->sync = FALSE;
-
-#ifndef MINIMAL_PREEMPTION
-  args->sem = &sem;
-  marcel_sem_init(&sem, 0);
-#else
-  args->initiator = marcel_self();
-#endif
-
-  MAD2_TRANSMIT_RECV_CONNECTION(pid);
-
-  marcel_run(pid, args);
-
-#ifdef MINIMAL_PREEMPTION
-  marcel_explicityield(pid);
-#else
-  marcel_sem_P(&sem);
-#endif
-}
-
-static void netserver_raw_rpc()
-{
-  int num;
-
-  mad_unpack_int(MAD_IN_HEADER, &num, 1);
-
-  (*_pm2_rpc_funcs[num])(NULL);
-}
-
-static void netserver_quick_lrpc()
-{
-  rpc_args args;
-  marcel_sem_t sem;
-
-  mad_unpack_int(MAD_IN_HEADER, &args.tid, 1);
-  mad_unpack_int(MAD_IN_HEADER, &args.num, 1);
-  mad_unpack_pointer(MAD_IN_HEADER, &args.ptr_att, 1);
-
-  args.sem = &sem;
-  args.sync = TRUE;
-  args.quick = TRUE;
-  marcel_sem_init(&sem, 0);
-
-  (*_pm2_rpc_funcs[args.num])(&args);
-}
-
-static void netserver_quick_async_lrpc()
-{
-  rpc_args args;
-  marcel_sem_t sem;
-
-  mad_unpack_int(MAD_IN_HEADER, &args.tid, 1);
-  mad_unpack_int(MAD_IN_HEADER, &args.num, 1);
-
-  args.sem = &sem;
-  args.sync = FALSE;
-  args.quick = TRUE;
-  marcel_sem_init(&sem, 0);
-
-  (*_pm2_rpc_funcs[args.num])(&args);
-}
-
-static void netserver_merge()
-{
-  unsigned size;
-  marcel_sem_t *sem;
-  char *dest;
-
-  mad_unpack_byte(MAD_IN_HEADER, (char *)&sem, sizeof(marcel_sem_t *));
-  mad_unpack_byte(MAD_IN_HEADER, (char *)&dest, sizeof(char *));
-  mad_unpack_int(MAD_IN_HEADER, &size, 1);
-  mad_unpack_byte(MAD_IN_HEADER, dest, size);
-
-  mad_recvbuf_receive();
-
-  marcel_sem_V(sem);
 }
 
 void _netserver_term_func(void *arg)
@@ -360,9 +74,6 @@ static any_t netserver(any_t arg)
 {
   unsigned tag;
 
-  marcel_setspecific(_pm2_lrpc_mode_key, (any_t)1);
-  marcel_setspecific(_pm2_lrpc_num_key, (any_t)((long)SYSTEM_SERVICE));
-
   marcel_cleanup_push(_netserver_term_func, marcel_self());
 
   while(!finished) {
@@ -372,62 +83,20 @@ static any_t netserver(any_t arg)
     mad_receive();
 #endif
     mad_unpack_int(MAD_IN_HEADER, &tag, 1);
-    switch(tag) {
-    case NETSERVER_RAW_RPC : {
-      netserver_raw_rpc();
-      break;
-    }
-    case NETSERVER_LRPC : {
-      netserver_lrpc();
-      break;
-    }
-    case NETSERVER_LRPC_DONE : {
-      netserver_lrpc_done();
-      break;
-    }
-    case NETSERVER_ASYNC_LRPC : {
-      netserver_async_lrpc();
-      break;
-    }
-    case NETSERVER_QUICK_LRPC : {
-      netserver_quick_lrpc();
-      break;
-    }
-    case NETSERVER_QUICK_ASYNC_LRPC : {
-      netserver_quick_async_lrpc();
-      break;
-    }
-    case NETSERVER_MIGRATION : {
-      netserver_migration();
-      break;
-    }
-    case NETSERVER_END : {
-      finished = TRUE;
-      break;
-    }
-    case NETSERVER_CRITICAL_SECTION : {
-      marcel_sem_V(&_pm2_critical[0]);
-      marcel_sem_P(&_pm2_critical[1]);
-      break;
-    }
-    case NETSERVER_CLONING : {
-      netserver_cloning();
-      break;
-    }
-    case NETSERVER_PRINTF : {
-      netserver_printf();
-      break;
-    }
-    case NETSERVER_MERGE : {
-      netserver_merge();
-      break;
-    }
-    default : {
-      fprintf(stderr, "NETSERVER ERROR: %d is not a valid tag.\n", tag);
-    }
+    if(tag >= NETSERVER_RAW_RPC)
+      netserver_raw_rpc(tag - NETSERVER_RAW_RPC);
+    else {
+      switch(tag) {
+      case NETSERVER_END : {
+	finished = TRUE;
+	break;
+      }
+      default : {
+	fprintf(stderr, "NETSERVER ERROR: %d is not a valid tag.\n", tag);
+      }
+      }
     }
   }
-
 #ifdef MINIMAL_PREEMPTION
    marcel_setspecialthread(NULL);
 #endif

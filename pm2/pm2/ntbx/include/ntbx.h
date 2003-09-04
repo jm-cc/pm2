d3af1e2aca78c1da42b5845edff0b9a215b38e3c
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:29:46 +0000

    rev 1.1 at 2000/02/08 15:25:20 GMT  - ajout du sous-repertoire `net' a la toolbox
    (Logical change 1.70)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@686 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/ntbx/include/ntbx.h
@@ -0,0 +1,80 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: ntbx.h,v $
+Revision 1.1  2000/02/08 15:25:20  oaumage
+- ajout du sous-repertoire `net' a la toolbox
+
+
+______________________________________________________________________________
+*/
+
+/*
+ * NTbx.h
+ * ======
+ */
+
+#ifndef NTBX_H
+#define NTBX_H
+
+/*
+ * Checks
+ * ------
+ */
+#ifndef __GNUC__
+#error The GNU C Compiler is required to build the library
+#endif __GNUC__
+
+/*
+ * Headers
+ * -------
+ */
+#ifdef PM2
+#include <marcel.h>
+#endif /* PM2 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+#include <ntbx_types.h>
+
+#ifdef NTBX_TCP
+#include <ntbx_tcp.h>
+#endif /* NTBX_TCP */
+
+#include <ntbx_interface.h>
+
+#endif /* NTBX_H */

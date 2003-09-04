4ae4c9e73d97a15f936149df633c552a8daa4b23
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:04:33 +0000

    rev 1.2 at 2000/01/16 11:50:19 GMT  Modofied to match the new pm2_init/pm2_halt interface.
    (Logical change 1.47)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@431 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/examples/X/regul.h
+++ pm2/pm2/pm2/examples/X/regul.h
@@ -36,13 +36,13 @@
 #ifndef REGUL_EST_DEF
 #define REGUL_EST_DEF
 
-void regul_init_rpc();
+void regul_rpc_init(void);
 
-void regul_init(int *les_modules, int nb_modules);
+void regul_init(void);
 
-void regul_start();
-void regul_stop();
+void regul_start(void);
+void regul_stop(void);
 
-void regul_exit();
+void regul_exit(void);
 
 #endif

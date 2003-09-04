55c9bc4aca42a29f0e4a86416850024cfa6461af
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:26:07 +0000

    rev 1.5 at 2000/03/13 09:48:40 GMT  - ajout de l'option TBX_SAFE_MALLOC - support de safe_malloc
    (Logical change 1.98)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1232 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/source/tbx_malloc.c
+++ pm2/pm2/tbx/source/tbx_malloc.c
@@ -34,6 +34,10 @@
 
 ______________________________________________________________________________
 $Log: tbx_malloc.c,v $
+Revision 1.5  2000/03/13 09:48:40  oaumage
+- ajout de l'option TBX_SAFE_MALLOC
+- support de safe_malloc
+
 Revision 1.4  2000/03/08 17:17:15  oaumage
 - utilisation de TBX_MALLOC
 
@@ -94,13 +98,227 @@ tbx_aligned_free (void  *ptr,
 
 
 /*
+ * Safe malloc
+ * -----------
+ */
+
+static char tbx_safe_malloc_magic[] =
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine "
+"MarcelMadeleine MarcelMadeleine MarcelMadeleine MarcelMadeleine"; /* 512 */
+
+typedef struct s_tbx_safe_malloc_header *p_tbx_safe_malloc_header_t;
+typedef struct s_tbx_safe_malloc_header
+{
+  p_tbx_safe_malloc_header_t next;
+  p_tbx_safe_malloc_header_t prev;
+  size_t                     size;
+  unsigned int               line;
+} tbx_safe_malloc_header_t;
+
+#define TBX_SAFE_MALLOC_MAGIC_SIZE    (sizeof(tbx_safe_malloc_magic))
+#define TBX_SAFE_MALLOC_HEADER_SIZE   (tbx_aligned(sizeof(tbx_safe_malloc_header_t), 16) + TBX_SAFE_MALLOC_MAGIC_SIZE)
+#define TBX_SAFE_MALLOC_ALIGNED_HEADER_SIZE (tbx_aligned(TBX_SAFE_MALLOC_HEADER_SIZE, 16))
+
+static p_tbx_safe_malloc_header_t list = NULL;
+static p_tbx_safe_malloc_header_t last = NULL;
+static size_t                     allocated = 0;
+static size_t                     freed     = 0;
+
+static void
+tbx_safe_malloc_mem_check()
+{
+  fprintf(stderr, "*** SafeMalloc Stats ***\n");
+  fprintf(stderr, "Allocated: %u, Freed: %u, Lost: %u\n",
+	  allocated,
+	  freed,
+	  allocated - freed);
+  
+  if (list)
+    {
+      fprintf(stderr, "SafeMalloc: Warning! All allocated memory has not been restitued :\n");
+      tbx_safe_malloc_check(tbx_safe_malloc_VERBOSE);
+    }
+}
+
+void
+tbx_safe_malloc_init()
+{
+  atexit(tbx_safe_malloc_mem_check);
+}
+
+void *
+tbx_safe_malloc(size_t    size,
+		char     *file,
+		unsigned  line)
+{
+  p_tbx_safe_malloc_header_t p =
+    malloc(TBX_SAFE_MALLOC_HEADER_SIZE +
+	   size +
+	   TBX_SAFE_MALLOC_MAGIC_SIZE +
+	   strlen(file) +
+	   1);
+  void *ptr = p;
+
+  if (!ptr)
+    return NULL;
+
+  allocated += size;
+
+  p->next = NULL;
+  p->size = size;
+  p->line = line;
+
+  memcpy(ptr + TBX_SAFE_MALLOC_ALIGNED_HEADER_SIZE,
+	 tbx_safe_malloc_magic,
+	 TBX_SAFE_MALLOC_MAGIC_SIZE);
+
+  ptr += TBX_SAFE_MALLOC_HEADER_SIZE + size;
+  memcpy(ptr,
+	 tbx_safe_malloc_magic,
+	 TBX_SAFE_MALLOC_MAGIC_SIZE);
+
+  ptr += TBX_SAFE_MALLOC_MAGIC_SIZE;
+  strcpy(ptr, file);
+
+  if(!list)
+    {
+      list = last = p;
+      p->prev = NULL;
+    }
+  else
+    {
+      last->next = p;
+      p->prev    = last;
+      last       = p;
+    }
+
+  return (char *)p + TBX_SAFE_MALLOC_HEADER_SIZE;
+}
+
+void *
+tbx_safe_calloc(size_t    nmemb,
+		size_t    size,
+		char     *file,
+		unsigned  line)
+{
+  void *p = tbx_safe_malloc(nmemb * size, file, line);
+
+  if (p)
+    memset(p, 0, nmemb * size);
+
+  return p;
+}
+
+static void
+tbx_safe_malloc_check_chunk(p_tbx_safe_malloc_header_t p)
+{
+  void *base = p;
+  void *data = base + TBX_SAFE_MALLOC_HEADER_SIZE;
+
+  if(memcmp(base + TBX_SAFE_MALLOC_ALIGNED_HEADER_SIZE,
+	    tbx_safe_malloc_magic,
+	    TBX_SAFE_MALLOC_MAGIC_SIZE))
+    fprintf(stderr,
+	    "SafeMalloc: Error: The block at %p has a corrupted header:\n",
+	    data);
+
+  if(memcmp(data + p->size,
+	    tbx_safe_malloc_magic,
+	    TBX_SAFE_MALLOC_MAGIC_SIZE))
+    fprintf(stderr,
+	    "SafeMalloc: Error: The block at %p has a corrupted trailer:\n",
+	    data);  
+}
+
+void tbx_safe_free(void *ptr, char *file, unsigned line)
+{
+  p_tbx_safe_malloc_header_t  p   =
+    ptr - TBX_SAFE_MALLOC_HEADER_SIZE;
+  void                       *base = p;
+
+  if (!p)
+    FAILURE("cannot free NULL ptr");
+
+  tbx_safe_malloc_check_chunk(p);
+
+  if(!p->next)
+    last = p->prev;
+  else
+    p->next->prev = p->prev;
+
+  if(!p->prev)
+    list = p->next;
+  else
+    p->prev->next = p->next;
+
+  freed += p->size;
+
+  memset(base + TBX_SAFE_MALLOC_ALIGNED_HEADER_SIZE,
+	 0,
+	 TBX_SAFE_MALLOC_MAGIC_SIZE);
+  
+  base += TBX_SAFE_MALLOC_HEADER_SIZE + p->size;
+  memset(base,
+	 0,
+	 TBX_SAFE_MALLOC_MAGIC_SIZE);
+
+  free(p);
+}
+
+void 
+tbx_safe_malloc_check(tbx_safe_malloc_mode_t mode)
+{
+  p_tbx_safe_malloc_header_t p;
+  
+  for(p = list; p; p = p->next)
+    {
+      void *ptr  = p;
+      void *ptrh = ptr + TBX_SAFE_MALLOC_HEADER_SIZE;
+      char *ptrf = ptrh + p->size + TBX_SAFE_MALLOC_MAGIC_SIZE;
+
+      tbx_safe_malloc_check_chunk(p);
+
+      if(mode == tbx_safe_malloc_VERBOSE) 
+	fprintf(stderr,
+		"\t[addr=%p, size=%u, malloc'ed in file %s at line %d]\n",
+		ptrh, p->size, ptrf, p->line);
+    }
+}
+
+void *
+tbx_safe_realloc(void     *ptr,
+		 size_t    size,
+		 char     *file,
+		 unsigned  line)
+{
+  void *new_ptr;
+  
+  new_ptr = tbx_safe_malloc(size, file, line);
+  
+  if (new_ptr)
+    {
+      memcpy(new_ptr, ptr, size);
+    }
+  
+  tbx_safe_free(ptr, file, line);
+
+  return new_ptr;
+}
+
+/*
  * Fast block allocation
  * ---------------------
  */
 void
 tbx_malloc_init(p_tbx_memory_t *mem,
-		size_t block_len,
-		long initial_block_number)
+		size_t          block_len,
+		long            initial_block_number)
 {
   p_tbx_memory_t temp_mem = TBX_MALLOC(sizeof(tbx_memory_t));
 
@@ -182,7 +400,8 @@ tbx_malloc(p_tbx_memory_t mem)
 }
 
 void
-tbx_free(p_tbx_memory_t mem, void *ptr)
+tbx_free(p_tbx_memory_t  mem,
+	 void           *ptr)
 {
   TBX_LOCK_SHARED(mem);
 

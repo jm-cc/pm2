e9e4d10917a461e1027f28e48fc060479cbb02f6
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 23:42:49 +0000

    rev 1.2 at 2000/03/27 12:53:58 GMT  - progression des fonctionnalites:   * support reseau   * support execution   * extension du support fichier
    (Logical change 1.109)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1361 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/swann/include/swann_run.h
+++ pm2/pm2/swann/include/swann_run.h
@@ -36,6 +36,12 @@
 
 ______________________________________________________________________________
 $Log: swann_run.h,v $
+Revision 1.2  2000/03/27 12:53:58  oaumage
+- progression des fonctionnalites:
+  * support reseau
+  * support execution
+  * extension du support fichier
+
 Revision 1.1  2000/02/17 09:29:24  oaumage
 - ajout des fichiers constitutifs de Swann
 
@@ -51,5 +57,23 @@ ______________________________________________________________________________
 #ifndef __SWANN_RUN_H
 #define __SWANN_RUN_H
 
+typedef enum
+{
+  swann_command_state_running = 0,
+  swann_command_state_finished
+} swann_command_state_t, *p_swann_command_state_t;
+
+typedef struct s_swann_command
+{
+  swann_command_state_t  command_state;
+  p_swann_file_t         file;
+  char                 **argv;
+  char                 **envp;
+  pid_t                  pid;
+  int                    return_value;
+  p_swann_file_t         file_in;
+  p_swann_file_t         file_out;
+  p_swann_file_t         file_err;
+} swann_command_t;
 
 #endif /* __SWANN_RUN_H */
--- pm2/pm2/swann/include/swann_types.h
+++ pm2/pm2/swann/include/swann_types.h
@@ -36,6 +36,12 @@
 
 ______________________________________________________________________________
 $Log: swann_types.h,v $
+Revision 1.2  2000/03/27 12:53:58  oaumage
+- progression des fonctionnalites:
+  * support reseau
+  * support execution
+  * extension du support fichier
+
 Revision 1.1  2000/02/17 09:29:22  oaumage
 - ajout des fichiers constitutifs de Swann
 
@@ -56,5 +62,8 @@ typedef enum
   swann_success =  0,
   swann_failure = -1,
 } swann_status_t, *p_swann_status_t;
+
+typedef int swann_net_client_id_t, *p_swann_net_client_id_t;
+typedef int swann_net_server_id_t, *p_swann_net_server_id_t;
 
 #endif /* __SWANN_TYPES_H */

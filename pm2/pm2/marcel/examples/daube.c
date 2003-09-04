50e2310befdc64d6b11c575c4ca869c1cd7ae6bb
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:52:00 +0000

    rev 1.3 at 2000/01/05 13:45:29 GMT  *** empty log message ***
    2003/09/04 23:51:13+02:00 ens-lyon.fr!rnamyst
    rev 1.2 at 2000/01/05 12:36:39 GMT
     *** empty log message ***
    
    (Logical change 1.30)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@286 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/examples/daube.c
+++ pm2/pm2/marcel/examples/daube.c
@@ -33,37 +33,103 @@
  software is provided ``as is'' without express or implied warranty.
 */
 
+/* ********************************************************* */
+
+#ifdef PTHREAD
+
+#include <pthread.h>
+#include <timing.h>
+
+#define marcel_mutex_t             pthread_mutex_t
+#define marcel_mutex_init          pthread_mutex_init
+#define marcel_mutex_lock          pthread_mutex_lock
+#define marcel_mutex_unlock        pthread_mutex_unlock
+
+#define marcel_cond_t              pthread_cond_t
+#define marcel_cond_init           pthread_cond_init
+#define marcel_cond_wait           pthread_cond_wait
+#define marcel_cond_signal         pthread_cond_signal
+
+#define marcel_attr_t              pthread_attr_t
+#define marcel_attr_init           pthread_attr_init
+#define marcel_attr_setdetachstate pthread_attr_setdetachstate
+#define MARCEL_CREATE_DETACHED     PTHREAD_CREATE_DETACHED
+
+#define marcel_t                   pthread_t
+#define marcel_create              pthread_create
+#define marcel_join                pthread_join
+
+#define marcel_main                main
+
+#else
+
+#define NON_BLOCKING_IO
+
 #include <marcel.h>
+
+#endif
+
+/* ********************************************************* */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
 
 typedef struct {
   int tub [2] ;
-  marcel_sem_t sem ;
+  marcel_mutex_t mutex ;
 } tube ;
 
-tube tube_global ;
+typedef struct {
+  int inf, sup, res;
+  marcel_t pid;
+  marcel_mutex_t mutex;
+  marcel_cond_t cond;
+  int completed;
+} job_t;
+
+static tube tube_1;
+
+static marcel_attr_t glob_attr;
+
+void job_init(job_t *j, int inf, int sup)
+{
+  j->inf = inf; j->sup = sup;
+  marcel_mutex_init(&j->mutex, NULL);
+  marcel_cond_init(&j->cond, NULL);
+  j->completed = 0;
+}
 
 void create_tube (tube *t)
 {
-  marcel_sem_init (&t->sem, 1) ;
+  marcel_mutex_init (&t->mutex, NULL) ;
   pipe (t->tub) ;
-}
-
-void delete_tube (tube *t)
-{
-  close (t->tub[0]) ;
-  close (t->tub[1]) ;
+#ifdef NON_BLOCKING_IO
+  fcntl(t->tub[0], F_SETFL, O_NONBLOCK);
+  fcntl(t->tub[1], F_SETFL, O_NONBLOCK);
+#endif
 }
 
 int send_message (tube *t, char *message, int len)
 {
   int n;
 
-  marcel_sem_P (&t->sem) ;
+  marcel_mutex_lock (&t->mutex) ;
+
+#ifdef NON_BLOCKING_IO
+  while(1) {
+    n = write (t->tub[1], message, len) ;
+    if(n == -1 && errno == EAGAIN)
+      marcel_yield();
+    else
+      break;
+  }
+#else
   n = write (t->tub[1], message, len) ;
-  marcel_sem_V (&t->sem) ;
+#endif
+
+  marcel_mutex_unlock(&t->mutex) ;
 
   return n ;
 }
@@ -72,59 +138,152 @@ int receive_message (tube *t, char *message, int len)
 {
   int n;
 
+#ifdef NON_BLOCKING_IO
+  while(1) {
+    n = read (t->tub[0], message, len) ;
+    if(n == -1 && errno == EAGAIN)
+      marcel_yield();
+    else
+      break;
+  }
+#else
   n = read (t->tub[0], message, len) ;
+#endif
     
   return n ;
 }
 
-any_t producteur (any_t arg)
+void send_to_server(job_t *j)
 {
-  int i;
-
-  for (i=0; i < 100 ; i++) {
-    marcel_yield();
-    send_message (&tube_global, (char *)&i, sizeof(int)) ;
-  }
-  return NULL;
+  send_message(&tube_1, (char *)&j, sizeof(job_t *)) ;
 }
 
-any_t consommateur (any_t arg)
+/* ********************************************************* */
+
+void *compute(void *arg);
+
+void *server(void * arg)
 {
-  int i, n ;
-  Tick t1, t2;
-  unsigned long temps;
+  job_t *j;
+  marcel_t pid;
 
-  GET_TICK(t1);
-  for (i=0; i < 100 ; i++) {
-    receive_message (&tube_global, (char *)&n, sizeof(int)) ; 
+  while(1) {
+    receive_message (&tube_1, (char *)&j, sizeof(job_t *));
+    if(j->inf == 0)
+      break;
+    marcel_create(&pid, &glob_attr, compute, (void *)j);
   }
-  GET_TICK(t2);
-
-  temps = timing_tick2usec(TICK_DIFF(t1, t2));
-  printf("time = %ld.%03ldms\n", temps/1000, temps%1000);
 
   return NULL;
 }
 
+void create_new_thread(job_t *j)
+{
+#if 0
+  marcel_t pid;
+  marcel_create(&pid, &glob_attr, compute, (void *)j);
+#else
+  send_to_server(j);
+#endif
+}
+
+void wait_thread(job_t *j)
+{
+  marcel_mutex_lock(&j->mutex);
+  while(!j->completed) {
+    marcel_cond_wait(&j->cond, &j->mutex);
+  }
+  marcel_mutex_unlock(&j->mutex);
+}
+
+void wake_thread(job_t *j, int res)
+{
+  j->res = res;
+  marcel_mutex_lock(&j->mutex);
+  j->completed = 1;
+  marcel_cond_signal(&j->cond);
+  marcel_mutex_unlock(&j->mutex);
+}
+
+void *compute(void *arg)
+{
+  job_t *j = (job_t *)arg;
+
+  if(j->inf == j->sup) {
+    wake_thread(j, j->inf);
+  } else {
+    job_t j1, j2;
+
+    job_init(&j1, j->inf,(j->inf+j->sup)/2);
+    create_new_thread(&j1);
+
+
+    job_init(&j2, (j->inf+j->sup)/2+1, j->sup);
+    create_new_thread(&j2);
+
+    wait_thread(&j1);
+    wait_thread(&j2);
+
+    wake_thread(j, j1.res + j2.res);
+  }
+
+  return NULL;
+}
 
 int marcel_main(int argc, char **argv)
 {
+  marcel_t serv_pid;
   marcel_attr_t attr;
+  job_t j;
+  Tick t1, t2;
+  unsigned long temps;
 
-  create_tube (&tube_global) ;
+  create_tube (&tube_1) ;
 
+#ifdef PTHREAD
+  timing_init();
+#else
   marcel_init(&argc, argv);
 
+  marcel_trace_on();
+#endif
+
   marcel_attr_init(&attr);
-  marcel_attr_setdetachstate(&attr, TRUE);
 
-  marcel_create (NULL, &attr, producteur, (void *) 0) ;
-  marcel_create (NULL, &attr, consommateur, (void *) 0) ;
+#ifdef SMP
+  marcel_attr_setschedpolicy(&attr, MARCEL_SCHED_FIXED(1));
+#endif
+  marcel_create(&serv_pid, &attr, server, NULL);
 
+  marcel_attr_init(&glob_attr);
+  marcel_attr_setdetachstate(&glob_attr, MARCEL_CREATE_DETACHED);
+#ifdef SMP
+  marcel_attr_setschedpolicy(&glob_attr, MARCEL_SCHED_FIXED(0));
+#endif
+
+  job_init(&j, 1, 50);
+
+  GET_TICK(t1);
+
+  create_new_thread(&j);
+
+  wait_thread(&j);
+
+  GET_TICK(t2);
+
+  printf("Sum from 1 to %d = %d\n", j.sup, j.res);
+  temps = timing_tick2usec(TICK_DIFF(t1, t2));
+  printf("time = %ld.%03ldms\n", temps/1000, temps%1000);
+
+  /* terminaison du serveur */
+  job_init(&j, 0, 0);
+  send_to_server(&j);
+
+  marcel_join(serv_pid, NULL);
+
+#ifndef PTHREAD
   marcel_end();
-  delete_tube (&tube_global) ;
+#endif
 
   return 0;
 }
-
-

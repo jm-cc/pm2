1a3d602208e7c6f36bc8f36a9ca9fbf97765416e
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:39:57 +0000

    rev 1.2 at 1999/12/02 17:11:53 GMT  j'ai rajoute 2 fonctions permettant: - de tester si un bitmap est vide; - de reinitialiser un bitmap a 0.
    Ces fonctionnalites etaient requises pour le protocole de coherence
    d'Hyperion, afin d'eviter de generer des communications pour envoyer
    les diffs s'il y en a pas, respectivement pour eviter de les envoyer plusieurs fois.
    
    (Logical change 1.11)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@115 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_bitmap.h
+++ pm2/pm2/dsm/include/dsm_bitmap.h
@@ -33,6 +33,7 @@
  software is provided ``as is'' without express or implied warranty.
 */
 
+
 #ifndef DSM_BITMAP_IS_DEF
 #define DSM_BITMAP_IS_DEF
 
@@ -42,12 +43,16 @@
 typedef unsigned int* dsm_bitmap_t;
 
 /* For the following function, size is in bits */
-#define dsm_bitmap_alloc(size) ((dsm_bitmap_t)tmalloc ((size + sizeof (char) - 1) / sizeof (char)))
 
-#define dsm_bitmap_free(b) free(b)
+#define dsm_bitmap_alloc(size) \
+  ((dsm_bitmap_t) tcalloc(((size) % 8) ? ((size) / 8 + 1) : ((size) / 8), 1))
+  
+#define dsm_bitmap_free(b) tfree(b)
 
 #define dsm_bitmap_mark_dirty(offset, length, bitmap) set_bits_to_1(offset, length, bitmap)
 
+#define dsm_bitmap_is_empty(b, size) bitmap_is_empty(b, size)
+
+#define dsm_bitmap_clear(b,size) clear_bitmap(b, size)
+
 #endif
-
-

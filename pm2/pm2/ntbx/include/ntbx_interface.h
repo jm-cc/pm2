bf517b7a050c1b591b613be4340b9dcecba3d573
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:44:37 +0000

    rev 1.2 at 2000/02/17 09:14:26 GMT  - ajout du support de TCP a la net toolbox - ajout de fonctions d'empaquetage de donnees numeriques
    (Logical change 1.79)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@842 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ntbx/include/ntbx_interface.h
+++ pm2/pm2/ntbx/include/ntbx_interface.h
@@ -34,6 +34,10 @@
 
 ______________________________________________________________________________
 $Log: ntbx_interface.h,v $
+Revision 1.2  2000/02/17 09:14:26  oaumage
+- ajout du support de TCP a la net toolbox
+- ajout de fonctions d'empaquetage de donnees numeriques
+
 Revision 1.1  2000/02/08 15:25:21  oaumage
 - ajout du sous-repertoire `net' a la toolbox
 
@@ -43,16 +47,54 @@ ______________________________________________________________________________
 
 /*
  * NTbx_interface.h
- * ====================
+ * ================
  */
 
 #ifndef NTBX_INTERFACE_H
 #define NTBX_INTERFACE_H
 
+/*
+ * Main interface
+ * --------------
+ */
 void
 ntbx_init(void);
 
+
+/*
+ * Pack/Unpack
+ * -----------
+ */
+void
+ntbx_pack_int(int                  data,
+	      p_ntbx_pack_buffer_t pack_buffer);
+
+int
+ntbx_unpack_int(p_ntbx_pack_buffer_t pack_buffer);
+
+void
+ntbx_pack_double(double               data,
+		 p_ntbx_pack_buffer_t pack_buffer);
+
+int
+ntbx_unpack_double(p_ntbx_pack_buffer_t pack_buffer);
+
+
+/*
+ * TCP support
+ * -----------
+ */
 #ifdef NTBX_TCP
+void
+ntbx_tcp_retry_struct_init(p_ntbx_tcp_retry_t retry);
+
+ntbx_status_t
+ntbx_tcp_retry(p_ntbx_tcp_retry_t retry);
+
+ntbx_status_t
+ntbx_tcp_timeout(p_ntbx_tcp_retry_t retry);
+
+
 void
 ntbx_tcp_address_fill(p_ntbx_tcp_address_t  address, 
 		      ntbx_tcp_port_t       port,
@@ -61,9 +103,64 @@ ntbx_tcp_address_fill(p_ntbx_tcp_address_t  address,
 void
 ntbx_tcp_socket_setup(ntbx_tcp_socket_t desc);
 
+void
+ntbx_tcp_nb_socket_setup(ntbx_tcp_socket_t desc);
+
 ntbx_tcp_socket_t
 ntbx_tcp_socket_create(p_ntbx_tcp_address_t address,
 		       ntbx_tcp_port_t      port);
+
+void
+ntbx_tcp_server_init(p_ntbx_server_t server);
+
+void
+ntbx_tcp_client_init(p_ntbx_client_t client);
+
+void
+ntbx_tcp_client_reset(p_ntbx_client_t client);
+
+void
+ntbx_tcp_server_reset(p_ntbx_server_t server);
+
+ntbx_status_t
+ntbx_tcp_client_connect(p_ntbx_client_t           client,
+			char                     *server_host_name,
+			p_ntbx_connection_data_t  server_connection_data);
+
+ntbx_status_t
+ntbx_tcp_server_accept(p_ntbx_server_t server, p_ntbx_client_t client);
+
+void
+ntbx_tcp_client_disconnect(p_ntbx_client_t client);
+
+void
+ntbx_tcp_server_disconnect(p_ntbx_server_t server);
+
+int
+ntbx_tcp_read_poll(int              nb_clients,
+		   p_ntbx_client_t *client_array);
+
+int
+ntbx_tcp_write_poll(int              nb_clients,
+		    p_ntbx_client_t *client_array);
+
+int
+ntbx_tcp_read_block(p_ntbx_client_t  client,
+		    void            *ptr,
+		    size_t           length);
+
+int
+ntbx_tcp_write_block(p_ntbx_client_t  client,
+		     void            *ptr,
+		     size_t           length);
+
+int
+ntbx_tcp_read_pack_buffer(p_ntbx_client_t      client,
+			  p_ntbx_pack_buffer_t pack_buffer);
+
+int
+ntbx_tcp_write_pack_buffer(p_ntbx_client_t      client,
+			   p_ntbx_pack_buffer_t pack_buffer);
 #endif /* NTBX_TCP */
 
 #endif /* NTBX_INTERFACE_H */

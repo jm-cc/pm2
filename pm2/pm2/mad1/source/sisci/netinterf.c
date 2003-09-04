37531f8dcb6a459a1f3c488894db5dd32d009c08
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:39:51 +0000

    rev 1.3 at 2000/02/11 13:10:09 GMT  Modified to use MAD1_ROOT instead of MADELEINE_ROOT.
    (Logical change 1.74)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@776 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad1/source/sisci/netinterf.c
+++ pm2/pm2/mad1/source/sisci/netinterf.c
@@ -886,7 +886,7 @@ static char *get_mad_root(void)
   static char buf[1024];
   char *ptr;
 
-  if((ptr = getenv("MADELEINE_ROOT")) != NULL)
+  if((ptr = getenv("MAD1_ROOT")) != NULL)
     return ptr;
   else {
     sprintf(buf, "%s/mad1", getenv("PM2_ROOT"));
@@ -1145,7 +1145,7 @@ void mad_sisci_network_init(int *argc, char **argv, int nb_proc, int *tids, int 
     dup2(STDERR_FILENO, STDOUT_FILENO);
 
     {
-      int ret = system("exit `cat ${MADELEINE_ROOT-${PM2_ROOT}/mad1}/.madconf | wc -w`");
+      int ret = system("exit `cat ${MAD1_ROOT-${PM2_ROOT}/mad1}/.madconf | wc -w`");
 
       confsize = WEXITSTATUS(ret);
     }

778bcfbfc3b9a863f6d20475865880e1a33d8479
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:26:05 +0000

    rev 1.6 at 2000/02/04 18:27:31 GMT  Significant design/interface modifications to allow Phil use DSM-PM2 in a standard way:
    - user can specify page protection independently of page
      ownership
    - included support for user data in the page table
    - included an expert-level type of receive-page-servers,
      allowing the control of page unpacking + actions
      before/after unpacking
    - included Phil's instrumentation.
    
    (Logical change 1.67)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@657 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_comm.c
+++ pm2/pm2/dsm/source/dsm_comm.c
@@ -39,15 +39,96 @@
 #include "dsm_page_manager.h"
 #include "dsm_protocol_policy.h"
 #include "dsm_rpc.h"
+#include "dsm_page_size.h"
 #include "assert.h"
-
 
 //#define DEBUG1
 //#define DEBUG_HYP
-//#define ASSERT
+#define SEND_PAGE_CHEAPER
+
+/* tie in to the Hyperion instrumentation */
+#define HYP_INSTRUMENT 1
+#ifdef HYP_INSTRUMENT
+int hyp_readPage_in_cnt = 0;
+int hyp_readPage_out_cnt = 0;
+int hyp_writePage_in_cnt = 0;
+int hyp_writePage_out_cnt = 0;
+int hyp_sendPage_in_cnt = 0;
+int hyp_sendPage_out_cnt = 0;
+int hyp_invalidate_in_cnt = 0;
+int hyp_invalidate_out_cnt = 0;
+int hyp_invalidateAck_in_cnt = 0;
+int hyp_invalidateAck_out_cnt = 0;
+int hyp_sendDiffs_in_cnt = 0;
+int hyp_sendDiffs_out_cnt = 0;
+int hyp_lrpcLock_out_cnt = 0;
+int hyp_lrpcLock_in_cnt = 0;
+int hyp_lrpcUnlock_out_cnt = 0;
+int hyp_lrpcUnlock_in_cnt = 0;
+
+void dsm_rpc_clear_instrumentation(void)
+{
+  hyp_readPage_in_cnt = 0;
+  hyp_readPage_out_cnt = 0;
+  hyp_writePage_in_cnt = 0;
+  hyp_writePage_out_cnt = 0;
+  hyp_sendPage_in_cnt = 0;
+  hyp_sendPage_out_cnt = 0;
+  hyp_invalidate_in_cnt = 0;
+  hyp_invalidate_out_cnt = 0;
+  hyp_invalidateAck_in_cnt = 0;
+  hyp_invalidateAck_out_cnt = 0;
+  hyp_sendDiffs_in_cnt = 0;
+  hyp_sendDiffs_out_cnt = 0;
+  hyp_lrpcLock_out_cnt = 0;
+  hyp_lrpcLock_in_cnt = 0;
+  hyp_lrpcUnlock_out_cnt = 0;
+  hyp_lrpcUnlock_in_cnt = 0;
+}
+
+void dsm_rpc_dump_instrumentation(void)
+{
+  tfprintf(stderr, "%d readPage_in\n",
+    hyp_readPage_in_cnt);
+  tfprintf(stderr, "%d readPage_out\n",
+    hyp_readPage_out_cnt);
+  tfprintf(stderr, "%d writePage_in\n",
+    hyp_writePage_in_cnt);
+  tfprintf(stderr, "%d writePage_out\n",
+    hyp_writePage_out_cnt);
+  tfprintf(stderr, "%d sendPage_in\n",
+    hyp_sendPage_in_cnt);
+  tfprintf(stderr, "%d sendPage_out\n",
+    hyp_sendPage_out_cnt);
+  tfprintf(stderr, "%d invalidate_in\n",
+    hyp_invalidate_in_cnt);
+  tfprintf(stderr, "%d invalidate_out\n",
+    hyp_invalidate_out_cnt);
+  tfprintf(stderr, "%d invalidateAck_in\n",
+    hyp_invalidateAck_in_cnt);
+  tfprintf(stderr, "%d invalidateAck_out\n",
+    hyp_invalidateAck_out_cnt);
+ tfprintf(stderr, "%d sendDiffs_in\n",
+    hyp_sendDiffs_in_cnt);
+  tfprintf(stderr, "%d sendDiffs_out\n",
+    hyp_sendDiffs_out_cnt);
+  tfprintf(stderr, "%d lrpcLock_out\n",
+    hyp_lrpcLock_out_cnt);
+  tfprintf(stderr, "%d lrpcLock_in\n",
+    hyp_lrpcLock_in_cnt);
+  tfprintf(stderr, "%d lrpcUnlock_out\n",
+    hyp_lrpcUnlock_out_cnt);
+  tfprintf(stderr, "%d lrpcUnlock_in\n",
+    hyp_lrpcUnlock_in_cnt);
+}
+#endif
+
 
 void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_access_t req_access)
 {
+#ifdef HYP_INSTRUMENT
+  hyp_sendPage_out_cnt++;
+#endif
 #ifdef ASSERT
   assert(dest_node != dsm_self());
 #endif
@@ -73,10 +154,12 @@ void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req
   }
 }
 
+
 void dsm_invalidate_copyset(unsigned long index, dsm_node_t new_owner)
 {
   (*dsm_get_invalidate_server(index))(index, dsm_self(), new_owner);
 }
+
 
 void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_access_t access)
 {
@@ -84,20 +167,40 @@ void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_access_t acces
   unsigned long page_size = dsm_get_page_size(index);
   dsm_node_t reply_node = dsm_self();
 
+#ifdef HYP_INSTRUMENT
+  hyp_sendPage_out_cnt++;
+#endif
+
+#ifdef DEBUG_HYP
+  tfprintf(stderr, "dsm_send_page: sending page %ld!\n", index);
+#endif
+
   pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_PAGE, NULL);
   pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
-  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size, sizeof(unsigned long));
-  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&reply_node, sizeof(dsm_node_t));
-  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&access, sizeof(dsm_access_t));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size,
+    sizeof(unsigned long));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&reply_node,
+    sizeof(dsm_node_t));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&access,
+    sizeof(dsm_access_t));
+#ifdef SEND_PAGE_CHEAPER
   pm2_pack_byte(SEND_CHEAPER, RECV_CHEAPER, (char *)addr, page_size); 
+#else
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, page_size); 
+#endif
   pm2_rawrpc_end();
 #ifdef DEBUG3
   fprintf(stderr, "Page %d sent -> %d for %s, a = %d \n", index, dest_node, (access == 1)?"read":"write", ((atomic_t *)addr)->counter);
 #endif
 }
 
+
 void dsm_send_invalidate_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_node_t new_owner)
 {
+#ifdef HYP_INSTRUMENT
+  hyp_invalidate_out_cnt++;
+#endif
+
   pm2_rawrpc_begin((int)dest_node, DSM_LRPC_INVALIDATE_REQ, NULL);
   pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
@@ -107,6 +210,10 @@ void dsm_send_invalidate_req(dsm_node_t dest_node, unsigned long index, dsm_node
 
 void dsm_send_invalidate_ack(dsm_node_t dest_node, unsigned long index)
 {
+#ifdef HYP_INSTRUMENT
+  hyp_invalidateAck_out_cnt++;
+#endif
+
   pm2_rawrpc_begin((int)dest_node, DSM_LRPC_INVALIDATE_ACK, NULL);
   pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_rawrpc_end();
@@ -117,6 +224,9 @@ static void DSM_LRPC_READ_PAGE_REQ_threaded_func(void)
 {
   dsm_node_t node;
   unsigned long index;
+#ifdef HYP_INSTRUMENT
+  hyp_readPage_in_cnt++;
+#endif
 
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&node, sizeof(dsm_node_t));
@@ -135,7 +245,10 @@ static void DSM_LRPC_WRITE_PAGE_REQ_threaded_func(void)
 {
   dsm_node_t node;
   unsigned long index;
-
+#ifdef HYP_INSTRUMENT
+  hyp_writePage_in_cnt++;
+#endif
+  
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&node, sizeof(dsm_node_t));
   pm2_rawrpc_waitdata(); 
@@ -154,26 +267,42 @@ static void DSM_LRPC_SEND_PAGE_threaded_func(void)
   dsm_node_t reply_node;
   unsigned long page_size;
   void * addr;
-  dsm_access_t access, old_access;
+  dsm_access_t access;
   unsigned long index;
-
+  
+#ifdef HYP_INSTRUMENT
+  hyp_sendPage_in_cnt++;
+#endif
+  
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size, sizeof(unsigned long));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&reply_node, sizeof(dsm_node_t));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&access, sizeof(dsm_access_t));
-
+  
   index = dsm_page_index(addr);
-  old_access = dsm_get_access(index);
-
-  //     fprintf(stderr, "lock_task: I am %p \n", marcel_self());  
-  // lock_task();
-  dsm_protect_page(addr, WRITE_ACCESS); // to enable the page to be copied
-  pm2_unpack_byte(SEND_CHEAPER, RECV_CHEAPER, (char *)addr, page_size); 
-  //unlock_task();
-  pm2_rawrpc_waitdata(); 
-  if (old_access != access)
-    dsm_protect_page(addr, old_access);
-  (*dsm_get_receive_page_server(index))(addr, access, reply_node);
+  
+  if  (dsm_get_expert_receive_page_server(index))
+    (*dsm_get_expert_receive_page_server(index))(addr, access, reply_node, page_size);
+  else
+    {
+      dsm_access_t old_access = dsm_get_access(index);
+      if (old_access != WRITE_ACCESS)
+	dsm_protect_page(addr, WRITE_ACCESS); // to enable the page to be copied
+#ifdef DEBUG4
+      fprintf(stderr, "Page %d before unpack before wait -> %d for %s, a = %d addr = %p\n", index, reply_node, (access == 1)?"read":"write", ((atomic_t *)addr)->counter, addr);
+#endif
+       pm2_unpack_byte(SEND_CHEAPER, RECV_CHEAPER, (char *)addr, page_size); 
+#ifdef DEBUG4
+       fprintf(stderr, "Page %d after unpack before wait -> %d for %s, a = %d \n", index, reply_node, (access == 1)?"read":"write", ((atomic_t *)addr)->counter);
+#endif
+       pm2_rawrpc_waitdata(); 
+#ifdef DEBUG3
+       fprintf(stderr, "Page %d received -> %d for %s, a = %d \n", index, reply_node, (access == 1)?"read":"write", ((atomic_t *)addr)->counter);
+#endif
+       if (old_access != WRITE_ACCESS)
+	 dsm_protect_page(addr, old_access);
+       (*dsm_get_receive_page_server(index))(addr, access, reply_node);
+    }
 }
 
 
@@ -187,6 +316,10 @@ static void DSM_LRPC_INVALIDATE_REQ_threaded_func(void)
 {
   dsm_node_t req_node, new_owner;
   unsigned long index;
+
+#ifdef HYP_INSTRUMENT
+  hyp_invalidate_in_cnt++;
+#endif
 
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
@@ -207,6 +340,10 @@ void DSM_LRPC_INVALIDATE_REQ_func(void)
 void DSM_LRPC_INVALIDATE_ACK_func(void)
 {
   unsigned long index;
+
+#ifdef HYP_INSTRUMENT
+  hyp_invalidateAck_in_cnt++;
+#endif
   
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long));
   pm2_rawrpc_waitdata(); 
@@ -219,13 +356,23 @@ void DSM_LRPC_INVALIDATE_ACK_func(void)
 
 /***********************  Hyperion stuff: ****************************/
 
+/* pjh: This must be synchronous! */
 void dsm_send_diffs(unsigned long index, dsm_node_t dest_node)
 {
   void *addr;
   int size;
+  pm2_completion_t c;
   
+#ifdef HYP_INSTRUMENT
+  hyp_sendDiffs_out_cnt++;
+#endif
+
+  pm2_completion_init(&c);
+
   pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_DIFFS, NULL);
-  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index, sizeof(unsigned long));
+
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index,
+    sizeof(unsigned long));
   while ((addr = dsm_get_next_modified_data(index, &size)) != NULL)
     {
       pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
@@ -233,14 +380,82 @@ void dsm_send_diffs(unsigned long index, dsm_node_t dest_node)
       pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, size);
     }
   // send the NULL value to terminate 
-  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));   
-  pm2_rawrpc_end();
-}
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
 
 
-void DSM_LRPC_SEND_DIFFS_func(void)
-{
 #ifdef DEBUG_HYP
-  tfprintf(stderr, "Received diffs(%d)\n", req.index);
+tfprintf(stderr, "dsm_send_diffs: calling pm2_pack_completion\n");
+#endif
+  pm2_pack_completion(&c);
+
+#ifdef DEBUG_HYP
+tfprintf(stderr, "dsm_send_diffs: calling pm2_rawrpc_end\n");
+#endif
+  pm2_rawrpc_end();
+
+#ifdef DEBUG_HYP
+tfprintf(stderr, "dsm_send_diffs: calling pm2_completion_wait\n");
+#endif
+  pm2_completion_wait(&c);
+
+#ifdef DEBUG_HYP
+tfprintf(stderr, "dsm_send_diffs: returning\n");
 #endif
 }
+
+
+/* pjh: must be synchronous
+ *
+ *      I still worry about the atomicity of the unpacks. Could this thread
+ *      lose hand while in the process of unpacking a field? We can't have
+ *      another thread see a partial update. 
+ *  ga: the atomicity has to be ensured at the protocol level, using dsm_lock_page
+ */
+void DSM_LRPC_SEND_DIFFS_func(void)
+{
+  unsigned long index;
+  void *addr;
+  int size;
+  pm2_completion_t c;
+
+#ifdef HYP_INSTRUMENT
+  hyp_sendDiffs_in_cnt++;
+#endif
+#ifdef DEBUG_HYP
+  tfprintf(stderr,"DSM_LRPC_SEND_DIFFS_func: entered\n");
+#endif
+
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index,
+    sizeof(unsigned long));
+#ifdef DEBUG_HYP
+  tfprintf(stderr,"DSM_LRPC_SEND_DIFFS_func: %ld\n", index);
+#endif
+  pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+  while (addr != NULL)
+  {
+      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&size, sizeof(int));
+#ifdef DEBUG_HYP
+tfprintf(stderr,"DSM_LRPC_SEND_DIFFS_func: %p %d\n", addr, size);
+#endif
+      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, size);
+      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+  }
+
+  pm2_unpack_completion(&c);
+
+  pm2_rawrpc_waitdata();
+
+#ifdef DEBUG_HYP
+  tfprintf(stderr,"DSM_LRPC_SEND_DIFFS_func: msg unpacked\n");
+#endif
+
+  /* no actual work to do, but unload the diffs which was done above */
+
+  pm2_completion_signal(&c);
+
+#ifdef DEBUG_HYP
+  tfprintf(stderr,"DSM_LRPC_SEND_DIFFS_func: returning\n");
+#endif
+}
+
+

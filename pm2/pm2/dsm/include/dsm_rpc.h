c6a468a15c889c52448ac881c9c3c068abc9ede1
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 21:39:12 +0000

    rev 1.1 at 1999/12/01 14:44:48 GMT  Rajoute DSM (version 30/11: interface protocol rajoute au niveau utilisateur).
    (Logical change 1.9)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@99 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/dsm/include/dsm_rpc.h
@@ -0,0 +1,88 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+*/
+
+#ifndef DSM_RPC_IS_DEF
+#define DSM_RPC_IS_DEF
+
+#include "sys/isomalloc_rpc.h"
+#include "dsm_const.h"
+
+
+BEGIN_LRPC_LIST
+  DSM_LRPC_READ_PAGE_REQ,//11
+  DSM_LRPC_WRITE_PAGE_REQ,//12
+  DSM_LRPC_SEND_PAGE,//13
+  DSM_LRPC_INVALIDATE_REQ,//14
+  DSM_LRPC_INVALIDATE_ACK,//15
+  DSM_LRPC_SEND_DIFFS//16
+END_LRPC_LIST
+
+/*******************************************************/
+ 
+LRPC_DECL_REQ(DSM_LRPC_READ_PAGE_REQ, unsigned long index; dsm_node_t req_node;);
+LRPC_DECL_RES(DSM_LRPC_READ_PAGE_REQ,);
+ 
+/*******************************************************/
+
+LRPC_DECL_REQ(DSM_LRPC_WRITE_PAGE_REQ, unsigned long index; dsm_node_t req_node;);
+LRPC_DECL_RES(DSM_LRPC_WRITE_PAGE_REQ,);
+ 
+/*******************************************************/
+
+LRPC_DECL_REQ(DSM_LRPC_SEND_PAGE, void *addr; unsigned long page_size; dsm_node_t reply_node; dsm_access_t access;);
+LRPC_DECL_RES(DSM_LRPC_SEND_PAGE,);
+  
+/*******************************************************/
+
+LRPC_DECL_REQ(DSM_LRPC_INVALIDATE_REQ, unsigned long index; dsm_node_t req_node; dsm_node_t new_owner;);
+LRPC_DECL_RES(DSM_LRPC_INVALIDATE_REQ,);
+  
+/*******************************************************/
+
+LRPC_DECL_REQ(DSM_LRPC_INVALIDATE_ACK, unsigned long index;);
+LRPC_DECL_RES(DSM_LRPC_INVALIDATE_ACK,);
+  
+/*******************************************************/
+
+LRPC_DECL_REQ(DSM_LRPC_SEND_DIFFS, unsigned long index;);
+LRPC_DECL_RES(DSM_LRPC_SEND_DIFFS,);
+  
+/*******************************************************/
+
+void dsm_pm2_init_rpc(void);
+
+#endif
+
+

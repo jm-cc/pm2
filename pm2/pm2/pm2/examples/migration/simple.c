ead031ed722912efe9790d9e1585656237cccb93
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:03:07 +0000

    rev 1.2 at 2000/01/14 17:07:28 GMT  Updated to match the new pm2_init()/pm2_halt() interface.
    (Logical change 1.45)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@415 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/examples/migration/simple.c
+++ pm2/pm2/pm2/examples/migration/simple.c
@@ -35,8 +35,6 @@
 
 #include <pm2.h>
 
-int *les_modules, nb_modules;
-
 #define NB	2
 static char *mess[NB] = { "Hi guys !", "Hi girls !" };
 
@@ -44,24 +42,23 @@ static marcel_sem_t sem;
 
 void thread_func(void *arg)
 {
-  int i, ind, nb;
+  int i, proc;
 
   pm2_enable_migration();
 
-  ind = 0;
-  nb = nb_modules;
+  proc = 0;
 
-  for(i=0; i<3*nb; i++) {
+  for(i=0; i<3*pm2_config_size(); i++) {
     pm2_printf("%s\n", (char *)arg);
 
     marcel_delay(100);
     if((i+1) % 3 == 0) {
 
-      ind++;
+      proc = (proc+1) % pm2_config_size();
 
-      pm2_printf("I'm now going on node [t%x]\n", les_modules[ind]);
+      pm2_printf("I'm now going on node [%d]\n", proc);
 
-      pm2_migrate_self(les_modules[ind]);
+      pm2_migrate_self(proc);
 
       pm2_printf("Here I am!\n");
     }
@@ -74,9 +71,9 @@ int pm2_main(int argc, char **argv)
 {
   int i;
 
-  pm2_init(&argc, argv, ASK_USER, &les_modules, &nb_modules);
+  pm2_init(&argc, argv);
 
-  if(pm2_self() == les_modules[0]) { /* master process */
+  if(pm2_self() == 0) { /* master process */
 
     marcel_sem_init(&sem, 0);
 
@@ -86,7 +83,7 @@ int pm2_main(int argc, char **argv)
     for(i=0; i<NB; i++)
       marcel_sem_P(&sem);
 
-    pm2_kill_modules(les_modules, nb_modules);
+    pm2_halt();
   }
 
    pm2_exit();

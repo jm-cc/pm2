5bbc8e5063846576794b37e0bd307e596a1a9c58
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-04 22:44:37 +0000

    rev 1.2 at 2000/02/17 09:14:28 GMT  - ajout du support de TCP a la net toolbox - ajout de fonctions d'empaquetage de donnees numeriques
    (Logical change 1.79)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@844 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ntbx/include/ntbx_types.h
+++ pm2/pm2/ntbx/include/ntbx_types.h
@@ -34,6 +34,10 @@
 
 ______________________________________________________________________________
 $Log: ntbx_types.h,v $
+Revision 1.2  2000/02/17 09:14:28  oaumage
+- ajout du support de TCP a la net toolbox
+- ajout de fonctions d'empaquetage de donnees numeriques
+
 Revision 1.1  2000/02/08 15:25:24  oaumage
 - ajout du sous-repertoire `net' a la toolbox
 
@@ -43,12 +47,103 @@ ______________________________________________________________________________
 
 /*
  * NTbx_types.h
- * ==========
+ * ============
  */
 
 #ifndef NTBX_TYPES_H
 #define NTBX_TYPES_H
 
-typedef int                ntbx_host_id_t,    *p_ntbx_host_id_t;
+
+/*
+ * Simple types
+ * ------------
+ */
+typedef int ntbx_host_id_t,    *p_ntbx_host_id_t;
+
+
+/*
+ * Pointers
+ * --------
+ */
+typedef struct s_ntbx_host   *p_ntbx_host_t;
+typedef struct s_ntbx_client *p_ntbx_client_t;
+typedef struct s_ntbx_server *p_ntbx_server_t;
+
+
+/*
+ * Pack buffers
+ * ------------
+ */
+#define NTBX_PACK_BUFFER_LEN     17
+#define NTBX_PACK_BUFFER_TAG_LEN  4
+typedef char                ntbx_pack_buffer_t[NTBX_PACK_BUFFER_LEN];
+typedef ntbx_pack_buffer_t *p_ntbx_pack_buffer_t;
+
+
+/*
+ * Connection data
+ * ---------------
+ */
+#define NTBX_CONNECTION_DATA_LEN 11
+typedef char ntbx_connection_data_t[NTBX_CONNECTION_DATA_LEN];
+typedef ntbx_connection_data_t *p_ntbx_connection_data_t;
+
+
+/*
+ * Status
+ * ------
+ */
+typedef enum
+{
+  ntbx_success =  0,
+  ntbx_failure = -1,
+} ntbx_status_t, *p_ntbx_status_t;
+
+
+/*
+ * Communication objects
+ * ---------------------
+ */
+
+/*...State Constant.....................*/
+typedef enum
+{
+  ntbx_client_state_uninitialized = 0,
+  ntbx_client_state_initialized,
+  ntbx_client_state_connected,
+  ntbx_client_state_data_ready,
+  ntbx_client_state_write_ready,
+  ntbx_client_state_peer_closed,
+  ntbx_client_state_shutdown,
+  ntbx_client_state_closed,
+} ntbx_client_state_t, *p_ntbx_client_state_t;
+
+typedef enum
+{
+  ntbx_server_state_uninitialized = 0,
+  ntbx_server_state_initialized,
+  ntbx_server_state_accepting,
+  ntbx_server_state_connection_ready,
+  ntbx_server_state_shutdown,
+  ntbx_server_state_closed,
+} ntbx_server_state_t, *p_ntbx_server_state_t;
+
+
+/*...Client/Server structures...........*/
+typedef struct s_ntbx_client
+{
+  ntbx_client_state_t  state;
+  void                *local_host;
+  void                *remote_host;
+  void                *specific;
+} ntbx_client_t;
+
+typedef struct s_ntbx_server
+{
+  ntbx_server_state_t     state;
+  void                   *local_host;
+  ntbx_connection_data_t  connection_data;
+  void                   *specific;
+} ntbx_server_t;
 
 #endif /* NTBX_TYPES_H */

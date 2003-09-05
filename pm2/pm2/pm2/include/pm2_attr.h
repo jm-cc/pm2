88412140b16ec636fe3746f4642962f8a0a586da
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:30:47 +0000

    rev 1.7 at 2001/02/23 15:45:44 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.415)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5533 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/pm2_attr.h
+++ pm2/pm2/pm2/include/pm2_attr.h
@@ -1,4 +1,19 @@
 
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #ifndef PM2_ATTR_EST_DEF
 #define PM2_ATTR_EST_DEF
 
@@ -9,16 +24,12 @@
 typedef unsigned pm2_channel_t;
 
 _PRIVATE_ typedef struct {
-  unsigned priority;
   int sched_policy;
   pm2_channel_t channel;
   pm2_completion_t *completion;
 } pm2_attr_t;
 
 int pm2_attr_init(pm2_attr_t *attr);
-
-int pm2_attr_setprio(pm2_attr_t *attr, unsigned prio);
-int pm2_attr_getprio(pm2_attr_t *attr, unsigned *prio);
 
 int pm2_attr_setschedpolicy(pm2_attr_t *attr, int policy);
 int pm2_attr_getschedpolicy(pm2_attr_t *attr, int *policy);

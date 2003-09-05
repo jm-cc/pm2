ef772024d09fc158b204225eed5b539a9728b00c
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:27:27 +0000

    rev 1.18 at 2002/10/26 14:14:34 GMT  added type dsm_proto_t; minor modifs
    (Logical change 1.709)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8762 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_protocol_policy.c
+++ pm2/pm2/dsm/source/dsm_protocol_policy.c
@@ -23,96 +23,116 @@
 #include "dsmlib_hbrc_mw_update_protocol.h"
 #include "hierarch_protocol.h"
 
-//#define TRACE_PROT
 
-/* Global data structures for the protocol policy module */
-
-typedef struct _dsm_protocol_t
-{
-  dsm_rf_action_t read_fault_handler;
-  dsm_wf_action_t write_fault_handler;
-  dsm_rs_action_t read_server;
-  dsm_ws_action_t write_server;
-  dsm_is_action_t invalidate_server;
-  dsm_rp_action_t receive_page_server;
-  dsm_erp_action_t expert_receive_page_server;
-  dsm_acq_action_t acquire_func;
-  dsm_rel_action_t release_func;
-  dsm_pi_action_t prot_init_func;
-  dsm_pa_action_t page_add_func;
-} dsm_protocol_t;
 
 #define MAX_DSM_PROTOCOLS 15
 
+
+/**********************************************************************/
+/* PUBLIC GLOBAL VARIABLES                                            */
+/**********************************************************************/
+
+/* pre-defined consistency protocols */
+dsm_proto_t LI_HUDAK;
+dsm_proto_t MIGRATE_THREAD;
+dsm_proto_t ERC;
+dsm_proto_t HBRC;
+dsm_proto_t HIERARCH;
+
+/* illegal protocol identifier */
+const dsm_proto_t NO_PROTOCOL = -1;
+
+/* default protocol as set by dsm_set_default_protocol() */
+const dsm_proto_t DEFAULT_PROTOCOL = -2;
+
+
+/**********************************************************************/
+/* PRIVATE DATA STRUCTURES                                            */
+/**********************************************************************/
+
+typedef struct _dsm_protocol_t
+{
+   dsm_rf_action_t read_fault_handler;
+   dsm_wf_action_t write_fault_handler;
+   dsm_rs_action_t read_server;
+   dsm_ws_action_t write_server;
+   dsm_is_action_t invalidate_server;
+   dsm_rp_action_t receive_page_server;
+   dsm_erp_action_t expert_receive_page_server;
+   dsm_acq_action_t acquire_func;
+   dsm_rel_action_t release_func;
+   dsm_pi_action_t prot_init_func;
+   dsm_pa_action_t page_add_func;
+} dsm_protocol_t;
+
+
+/**********************************************************************/
+/* PRIVATE GLOBAL VARIABLES                                           */
+/**********************************************************************/
+
 static int _nb_protocols = 0;
-static dsm_protocol_t dsm_protocol_table[MAX_DSM_PROTOCOLS]; /* the protocol table */
+ /* the protocol table */
+static dsm_protocol_t dsm_protocol_table[MAX_DSM_PROTOCOLS];
 
-int LI_HUDAK;
-int MIGRATE_THREAD;
-int ERC;
-int HBRC;
-int HIERARCH;
 
-/* Public functions */
+/**********************************************************************/
+/* PUBLIC FUNCTIONS                                                   */
+/**********************************************************************/
 
 void
 dsm_init_protocol_table (void)
 {
-  /* LI_HUDAK */
-  LI_HUDAK = dsm_create_protocol(dsmlib_rf_ask_for_read_copy, // read_fault_handler
-		      dsmlib_wf_ask_for_write_access, // write_fault_handler
-		      dsmlib_rs_send_read_copy, // read_server
-		      dsmlib_ws_send_page_for_write_access, // write_server
-		      dsmlib_is_invalidate, // invalidate_server
-		      dsmlib_rp_validate_page, // receive_page_server
-		      NULL, // expert_receive_page_server
-		      NULL, // acquire_func
-		      NULL, // release_func
-		      NULL, // prot_init_func
-		      NULL  // page_add_func
-		      );
+   /* LI_HUDAK */
+   LI_HUDAK = dsm_create_protocol(dsmlib_rf_ask_for_read_copy /* read_fault_handler */,
+                                  dsmlib_wf_ask_for_write_access /* write_fault_handler */,
+                                  dsmlib_rs_send_read_copy /* read_server */,
+                                  dsmlib_ws_send_page_for_write_access /* write_server */,
+                                  dsmlib_is_invalidate /* invalidate_server */,
+                                  dsmlib_rp_validate_page /* receive_page_server */,
+                                  NULL /* expert_receive_page_server */,
+                                  NULL /* acquire_func */,
+                                  NULL /* release_func */,
+                                  NULL /* prot_init_func */,
+                                  NULL /* page_add_func */);
 
-  /* MIGRATE_THREAD */
-  MIGRATE_THREAD = dsm_create_protocol(dsmlib_migrate_thread, // read_fault_handler
-		      dsmlib_migrate_thread, // write_fault_handler
-		      NULL,// read_server
-		      NULL,// write_server
-		      NULL, // invalidate_server
-		      NULL, // receive_page_server
-		      NULL, // expert_receive_page_server
-		      NULL, // acquire_func
-		      NULL, // release_func
-		      NULL, // prot_init_func
-		      NULL  // page_add_func
-		      );
+   /* MIGRATE_THREAD */
+   MIGRATE_THREAD = dsm_create_protocol(dsmlib_migrate_thread /* read_fault_handler */,
+                                        dsmlib_migrate_thread /* write_fault_handler */,
+                                        NULL /* read_server */,
+                                        NULL /* write_server */,
+                                        NULL /* invalidate_server */,
+                                        NULL /* receive_page_server */,
+                                        NULL /* expert_receive_page_server */,
+                                        NULL /* acquire_func */,
+                                        NULL /* release_func */,
+                                        NULL /* prot_init_func */,
+                                        NULL /* page_add_func */);
 
-  /* ERC */
-  ERC = dsm_create_protocol(dsmlib_erc_sw_inv_rfh, // read_fault_handler
-		      dsmlib_erc_sw_inv_wfh, // write_fault_handler
-		      dsmlib_erc_sw_inv_rs, // read_server
-		      dsmlib_erc_sw_inv_ws, // write_server
-		      dsmlib_erc_sw_inv_is, // invalidate_server
-		      dsmlib_erc_sw_inv_rps, // receive_page_server
-		      NULL, // expert_receive_page_server
-		      NULL, // acquire_func
-		      dsmlib_erc_release, // release_func
-		      dsmlib_erc_sw_inv_init, // prot_init_func
-		      dsmlib_erc_add_page  // page_add_func
-		      );
+   /* ERC */
+   ERC = dsm_create_protocol(dsmlib_erc_sw_inv_rfh /* read_fault_handler */,
+                             dsmlib_erc_sw_inv_wfh /* write_fault_handler */,
+                             dsmlib_erc_sw_inv_rs /* read_server */,
+                             dsmlib_erc_sw_inv_ws /* write_server */,
+                             dsmlib_erc_sw_inv_is /* invalidate_server */,
+                             dsmlib_erc_sw_inv_rps /* receive_page_server */,
+                             NULL /* expert_receive_page_server */,
+                             NULL /* acquire_func */,
+                             dsmlib_erc_release /* release_func */,
+                             dsmlib_erc_sw_inv_init /* prot_init_func */,
+                             dsmlib_erc_add_page /* page_add_func */);
 
-  /* HBRC */
-  HBRC = dsm_create_protocol(dsmlib_hbrc_mw_update_rfh, // read_fault_handler
-			     dsmlib_hbrc_mw_update_wfh, // write_fault_handler
-			     dsmlib_hbrc_mw_update_rs, // read_server
-			     dsmlib_hbrc_mw_update_ws, // write_server
-			     dsmlib_hbrc_mw_update_is, // invalidate_server
-			     dsmlib_hbrc_mw_update_rps, // receive_page_server
-			     NULL, // expert_receive_page_server
-			     NULL, // acquire_func
-			     dsmlib_hbrc_release, // release_func
-			     dsmlib_hbrc_mw_update_prot_init, // prot_init_func
-			     dsmlib_hbrc_add_page  // page_add_func
-			     );
+   /* HBRC */
+   HBRC = dsm_create_protocol(dsmlib_hbrc_mw_update_rfh /* read_fault_handler */,
+                              dsmlib_hbrc_mw_update_wfh /* write_fault_handler */,
+                              dsmlib_hbrc_mw_update_rs /* read_server */,
+                              dsmlib_hbrc_mw_update_ws /* write_server */,
+                              dsmlib_hbrc_mw_update_is /* invalidate_server */,
+                              dsmlib_hbrc_mw_update_rps /* receive_page_server */,
+                              NULL /* expert_receive_page_server */,
+                              NULL /* acquire_func */,
+                              dsmlib_hbrc_release /* release_func */,
+                              dsmlib_hbrc_mw_update_prot_init /* prot_init_func */,
+                              dsmlib_hbrc_add_page /* page_add_func */);
 
   /* Hierarchical consistency protocol: HIERARCH; to be used in
    * conjunction with the token_locks */
@@ -132,183 +152,192 @@ dsm_init_protocol_table (void)
 }
 
 
-int dsm_create_protocol(dsm_rf_action_t read_fault_handler,
-			dsm_wf_action_t write_fault_handler,
-			dsm_rs_action_t read_server,
-			dsm_ws_action_t write_server,
-			dsm_is_action_t invalidate_server,
-			dsm_rp_action_t receive_page_server,
-			dsm_erp_action_t expert_receive_page_server,
-			dsm_acq_action_t acquire_func,
-			dsm_rel_action_t release_func,
-			dsm_pi_action_t prot_init_func,
-			dsm_pa_action_t page_add_func
-			)
+dsm_proto_t
+dsm_create_protocol (dsm_rf_action_t const read_fault_handler,
+                     dsm_wf_action_t const write_fault_handler,
+                     dsm_rs_action_t const read_server,
+                     dsm_ws_action_t const write_server,
+                     dsm_is_action_t const invalidate_server,
+                     dsm_rp_action_t const receive_page_server,
+                     dsm_erp_action_t const expert_receive_page_server,
+                     dsm_acq_action_t const acquire_func,
+                     dsm_rel_action_t const release_func,
+                     dsm_pi_action_t const prot_init_func,
+                     dsm_pa_action_t const page_add_func)
 {
- dsm_protocol_table[_nb_protocols].read_fault_handler = read_fault_handler;
- dsm_protocol_table[_nb_protocols].write_fault_handler = write_fault_handler;
- dsm_protocol_table[_nb_protocols].read_server = read_server;
- dsm_protocol_table[_nb_protocols].write_server = write_server;
- dsm_protocol_table[_nb_protocols].invalidate_server = invalidate_server;
- dsm_protocol_table[_nb_protocols].receive_page_server = receive_page_server;
- dsm_protocol_table[_nb_protocols].expert_receive_page_server = expert_receive_page_server;
- dsm_protocol_table[_nb_protocols].acquire_func = acquire_func;
- dsm_protocol_table[_nb_protocols].release_func = release_func;
- dsm_protocol_table[_nb_protocols].prot_init_func = prot_init_func;
- dsm_protocol_table[_nb_protocols].page_add_func = page_add_func;
+   dsm_protocol_table[_nb_protocols].read_fault_handler = read_fault_handler;
+   dsm_protocol_table[_nb_protocols].write_fault_handler = write_fault_handler;
+   dsm_protocol_table[_nb_protocols].read_server = read_server;
+   dsm_protocol_table[_nb_protocols].write_server = write_server;
+   dsm_protocol_table[_nb_protocols].invalidate_server = invalidate_server;
+   dsm_protocol_table[_nb_protocols].receive_page_server = receive_page_server;
+   dsm_protocol_table[_nb_protocols].expert_receive_page_server = expert_receive_page_server;
+   dsm_protocol_table[_nb_protocols].acquire_func = acquire_func;
+   dsm_protocol_table[_nb_protocols].release_func = release_func;
+   dsm_protocol_table[_nb_protocols].prot_init_func = prot_init_func;
+   dsm_protocol_table[_nb_protocols].page_add_func = page_add_func;
 
- _nb_protocols++;
-
- return _nb_protocols - 1 ;
+   return (dsm_proto_t)(_nb_protocols++);
 }
 
 
-int dsm_registered_protocols()
+int
+dsm_get_registered_protocols (void)
 {
-  return _nb_protocols;
+   return _nb_protocols;
 }
 
 
-void dsm_set_read_fault_action(int key, dsm_rf_action_t action)
+void
+dsm_set_read_fault_action (const dsm_proto_t key, dsm_rf_action_t const action)
 {
-  dsm_protocol_table[key].read_fault_handler = action;
+   dsm_protocol_table[key].read_fault_handler = action;
 }
 
 
-dsm_rf_action_t dsm_get_read_fault_action(int key)
+dsm_rf_action_t
+dsm_get_read_fault_action (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].read_fault_handler;
+   return dsm_protocol_table[key].read_fault_handler;
 }
 
 
-void dsm_set_write_fault_action(int key, dsm_wf_action_t action)
+void
+dsm_set_write_fault_action (const dsm_proto_t key, dsm_wf_action_t const action)
 {
-  dsm_protocol_table[key].write_fault_handler = action;
+   dsm_protocol_table[key].write_fault_handler = action;
 }
 
 
-dsm_wf_action_t dsm_get_write_fault_action(int key)
+dsm_wf_action_t
+dsm_get_write_fault_action (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].write_fault_handler;
+   return dsm_protocol_table[key].write_fault_handler;
 }
 
 
-void dsm_set_read_server(int key, dsm_rs_action_t action)
+void
+dsm_set_read_server (const dsm_proto_t key, dsm_rs_action_t const action)
 {
-  dsm_protocol_table[key].read_server = action;
+   dsm_protocol_table[key].read_server = action;
 }
 
 
-dsm_rs_action_t dsm_get_read_server(int key)
+dsm_rs_action_t
+dsm_get_read_server (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].read_server;
+   return dsm_protocol_table[key].read_server;
 }
 
 
-void dsm_set_write_server(int key, dsm_ws_action_t action)
+void
+dsm_set_write_server (const dsm_proto_t key, dsm_ws_action_t const action)
 {
-  dsm_protocol_table[key].write_server = action;
+   dsm_protocol_table[key].write_server = action;
 }
 
 
-dsm_ws_action_t dsm_get_write_server(int key)
+dsm_ws_action_t
+dsm_get_write_server (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].write_server;
+   return dsm_protocol_table[key].write_server;
 }
 
 
-void dsm_set_invalidate_server(int key, dsm_is_action_t action)
+void
+dsm_set_invalidate_server (const dsm_proto_t key, dsm_is_action_t const action)
 {
-  dsm_protocol_table[key].invalidate_server = action;
+   dsm_protocol_table[key].invalidate_server = action;
 }
 
 
-dsm_is_action_t dsm_get_invalidate_server(int key)
+dsm_is_action_t
+dsm_get_invalidate_server (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].invalidate_server;
+   return dsm_protocol_table[key].invalidate_server;
 }
 
 
-void dsm_set_receive_page_server(int key, dsm_rp_action_t action)
+void
+dsm_set_receive_page_server (const dsm_proto_t key, dsm_rp_action_t const action)
 {
-  dsm_protocol_table[key].receive_page_server = action;
+   dsm_protocol_table[key].receive_page_server = action;
 }
 
 
-dsm_rp_action_t dsm_get_receive_page_server(int key)
+dsm_rp_action_t
+dsm_get_receive_page_server (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].receive_page_server;
+   return dsm_protocol_table[key].receive_page_server;
 }
 
 
-void dsm_set_expert_receive_page_server(int key, dsm_erp_action_t action)
+void
+dsm_set_expert_receive_page_server (const dsm_proto_t key, dsm_erp_action_t const action)
 {
-  dsm_protocol_table[key].expert_receive_page_server = action;
+   dsm_protocol_table[key].expert_receive_page_server = action;
 }
 
 
-dsm_erp_action_t dsm_get_expert_receive_page_server(int key)
+dsm_erp_action_t
+dsm_get_expert_receive_page_server (const dsm_proto_t key)
 {
-  return dsm_protocol_table[key].expert_receive_page_server;
+   return dsm_protocol_table[key].expert_receive_page_server;
 }
 
 
-void dsm_set_acquire_func(int key, dsm_acq_action_t action)
+void
+dsm_set_acquire_func (const dsm_proto_t key, dsm_acq_action_t const action)
 {
-  dsm_protocol_table[key].acquire_func = action;
+   dsm_protocol_table[key].acquire_func = action;
 }
 
 
-dsm_acq_action_t dsm_get_acquire_func(int key)
+dsm_acq_action_t
+dsm_get_acquire_func (const dsm_proto_t key)
 {
-#ifdef TRACE_PROT
-  fprintf(stderr,"Entering %s\n", __FUNCTION__);
-#endif
-  return dsm_protocol_table[key].acquire_func;
+   return dsm_protocol_table[key].acquire_func;
 }
 
 
-void dsm_set_release_func(int key, dsm_rel_action_t action)
+void
+dsm_set_release_func (const dsm_proto_t key, dsm_rel_action_t const action)
 {
-  dsm_protocol_table[key].release_func = action;
+   dsm_protocol_table[key].release_func = action;
 }
 
 
-dsm_rel_action_t dsm_get_release_func(int key)
+dsm_rel_action_t
+dsm_get_release_func (const dsm_proto_t key)
 {
-#ifdef TRACE_PROT
-  fprintf(stderr,"[%s]: Entering...\n", __FUNCTION__);
-#endif
-  return dsm_protocol_table[key].release_func;
+   return dsm_protocol_table[key].release_func;
 }
 
 
-void dsm_set_prot_init_func(int key, dsm_pi_action_t action)
+void
+dsm_set_prot_init_func (const dsm_proto_t key, dsm_pi_action_t const action)
 {
-  dsm_protocol_table[key].prot_init_func = action;
+   dsm_protocol_table[key].prot_init_func = action;
 }
 
 
-dsm_pi_action_t dsm_get_prot_init_func(int key)
+dsm_pi_action_t
+dsm_get_prot_init_func (const dsm_proto_t key)
 {
-#ifdef TRACE_PROT
-  fprintf(stderr,"[%s]: Entering...\n", __FUNCTION__);
-#endif
-  return dsm_protocol_table[key].prot_init_func;
+   return dsm_protocol_table[key].prot_init_func;
 }
 
 
-void dsm_set_page_add_func(int key, dsm_pa_action_t action)
+void
+dsm_set_page_add_func (const dsm_proto_t key, dsm_pa_action_t const action)
 {
-  dsm_protocol_table[key].page_add_func = action;
+   dsm_protocol_table[key].page_add_func = action;
 }
 
 
-dsm_pa_action_t dsm_get_page_add_func(int key)
+dsm_pa_action_t
+dsm_get_page_add_func (const dsm_proto_t key)
 {
-#ifdef TRACE_PROT
- fprintf(stderr,"[%s]: Entering...\n", __FUNCTION__);
-#endif
-  return dsm_protocol_table[key].page_add_func;
+   return dsm_protocol_table[key].page_add_func;
 }
 

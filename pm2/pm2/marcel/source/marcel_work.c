e37b01334b392f67b0ff4e7ee770a4219e0c6e2b
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:36:25 +0000

    rev 1.5 at 2001/04/09 20:04:42 GMT  Modified MA__WORK section and implemented a preliminary working 'deviate' functionality.
    (Logical change 1.496)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7055 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/source/marcel_work.c
+++ pm2/pm2/marcel/source/marcel_work.c
@@ -18,40 +18,24 @@
 
 #ifdef MA__WORK
 
-volatile int marcel_global_work=0;
-marcel_lock_t marcel_work_lock=MARCEL_LOCK_INIT_UNLOCKED;
-
-
 void do_work(marcel_t self)
 {
-  int has_done_work=0;
-#ifdef DEBUG
-  if(locked() != 1)
-    RAISE(LOCK_TASK_ERROR);
-#endif
-  LOCK_WORK(self);
   MTRACE("DO_WORK", self);
-  mdebug_work("do_work : marcel_global_work=%p, has_work=%p\n", 
-	      (void*)marcel_global_work, (void*)self->has_work);
-  while (marcel_global_work || self->has_work) {
-    if (self->has_work & MARCEL_WORK_DEVIATE) {
-      handler_func_t h=self->deviation_func;
-      any_t arg=self->deviation_arg;
-      self->deviation_func=NULL;
-      self->has_work &= ~MARCEL_WORK_DEVIATE;
-      UNLOCK_WORK(self);
-      unlock_task();
-      (*h)(arg);
-      lock_task();
-      LOCK_WORK(self);     
-    }
-    if ((marcel_global_work  || self->has_work) && !has_done_work) {
-      RAISE(PROGRAM_ERROR);
-    }
+
+  if(HAS_DEVIATE_WORK(self)) {
+    handler_func_t h = self->deviation_func;
+    any_t arg = self->deviation_arg;
+
+    self->deviation_func = NULL;
+    self->has_work &= ~MARCEL_WORK_DEVIATE;
+
+    unlock_task();
+    (*h)(arg);
+    lock_task();
+    return;
   }
 
-  UNLOCK_WORK(self);
-
+  RAISE(PROGRAM_ERROR);
 }
 
 #endif

39b3c5e1ddddb468b32b859a69ec3c8a3ac1431e
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:04:52 +0000

    rev 1.4 at 2000/04/11 09:07:11 GMT  Merged the "reorganisation" development branch.
    (Logical change 1.138)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1601 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_mutex.h
+++ pm2/pm2/marcel/include/marcel_mutex.h
@@ -34,6 +34,12 @@
 
 ______________________________________________________________________________
 $Log: marcel_mutex.h,v $
+Revision 1.4  2000/04/11 09:07:11  rnamyst
+Merged the "reorganisation" development branch.
+
+Revision 1.3.2.1  2000/03/15 15:54:46  vdanjean
+réorganisation de marcel : commit pour CVS
+
 Revision 1.3  2000/03/06 15:30:11  rnamyst
 Added the macro MARCEL_MUTEX_INITIALIZER.
 

e07229a49ef4c78192c36de2fc333ed225264917
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:31:30 +0000

    rev 1.1 at 2001/04/05 13:24:29 GMT  Management of signals and timers has moved to marcel_sig.c.
    (Logical change 1.491)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7025 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/sys/marcel_sig.h
@@ -0,0 +1,40 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef MARCEL_SIG_IS_DEF
+#define MARCEL_SIG_IS_DEF
+
+#include <signal.h>
+
+#include "marcel.h"
+
+void marcel_sig_init(void);
+
+void marcel_sig_pause(void);
+
+void marcel_sig_enable_interrupts(void);
+void marcel_sig_disable_interrupts(void);
+
+void marcel_sig_reset_timer(void);
+void marcel_sig_start_timer(void);
+void marcel_sig_stop_timer(void);
+
+unsigned long marcel_clock(void);
+void marcel_update_time(marcel_t cur);
+void marcel_settimeslice(unsigned long microsecs);
+
+#endif
+

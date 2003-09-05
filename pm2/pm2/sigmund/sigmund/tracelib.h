c849a75eed7865f97a58ceb042383dafced5643b
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:28:41 +0000

    rev 1.3 at 2001/04/03 12:56:57 GMT  *** empty log message ***
    2003/09/05 12:26:50+02:00 ens-lyon.fr!cmenier
    rev 1.2 at 2001/04/03 12:39:20 GMT
     stable ??
    
    (Logical change 1.488)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6990 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/sigmund/tracelib.h
+++ pm2/pm2/sigmund/sigmund/tracelib.h
@@ -9,7 +9,13 @@
 
 extern filter options;
 
-extern int tracelib_init(char *supertrace);
+extern char *fut_code2name(int code);
+
+extern int name2code(char *name, mode *type, int *a);
+
+extern char *fkt_code2name(int code);
+
+extern void tracelib_init(char *supertrace);
 
 extern int get_next_filtered_trace(trace *tr);
 

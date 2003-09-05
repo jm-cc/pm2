ea55a9e0997628c89c49b47bf3c6b4a3b185a55a
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.1 at 2001/06/11 16:06:51 GMT  - synchro
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7569 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/leonie/source/leo_communication.c
@@ -0,0 +1,131 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * leo_communication.c
+ * ===================
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "leonie.h"
+
+void
+leo_send_int(p_ntbx_client_t client,
+	     const int       data)
+{
+  int                status = ntbx_failure;
+  ntbx_pack_buffer_t buffer;
+
+  LOG_IN();
+  ntbx_pack_int(data, &buffer);
+  status = ntbx_btcp_write_pack_buffer(client, &buffer);
+  
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+  
+  LOG_OUT();
+}
+
+int
+leo_receive_int(p_ntbx_client_t client)
+{
+  int                status = ntbx_failure;
+  int                data   = 0;
+  ntbx_pack_buffer_t buffer;
+
+  LOG_IN();
+  status = ntbx_btcp_read_pack_buffer(client, &buffer);
+
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+
+  data = ntbx_unpack_int(&buffer);
+  LOG_OUT();
+
+  return data;
+}
+
+void
+leo_send_unsigned_int(p_ntbx_client_t    client,
+		      const unsigned int data)
+{
+  int                status = ntbx_failure;
+  ntbx_pack_buffer_t buffer;
+
+  LOG_IN();
+  ntbx_pack_unsigned_int(data, &buffer);
+  status = ntbx_btcp_write_pack_buffer(client, &buffer);
+  
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+  
+  LOG_OUT();
+}
+
+unsigned int
+leo_receive_unsigned_int(p_ntbx_client_t client)
+{
+  int                status = ntbx_failure;
+  unsigned int       data   = 0;
+  ntbx_pack_buffer_t buffer;
+
+  LOG_IN();
+  status = ntbx_btcp_read_pack_buffer(client, &buffer);
+
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+
+  data = ntbx_unpack_unsigned_int(&buffer);
+  LOG_OUT();
+
+  return data;
+}
+
+void
+leo_send_string(p_ntbx_client_t  client,
+		const char      *string)
+{
+  int status = ntbx_failure;
+
+  LOG_IN();
+  status = ntbx_btcp_write_string(client, string);
+  
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+  
+  LOG_OUT();
+}
+
+char *
+leo_receive_string(p_ntbx_client_t client)
+{
+  int   status = ntbx_failure;
+  char *result = NULL;
+
+  LOG_IN();
+  status = ntbx_btcp_read_string(client, &result);
+
+  if (status == ntbx_failure)
+    FAILURE("control link failure");
+
+  LOG_OUT();
+
+  return result;
+}
---
+++ pm2/pm2/leonie/source/leo_includes.c
@@ -0,0 +1,83 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * leo_includes.c
+ * ==============
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "leonie.h"
+
+void
+process_network_include_file(p_leo_networks_t networks,
+			     p_tbx_htable_t   include_htable)
+{
+  p_tbx_slist_t network_slist = NULL;
+
+  LOG_IN();
+  network_slist = leoparse_read_slist(include_htable, "networks");
+  if (network_slist && !tbx_slist_is_nil(network_slist))
+    {
+      tbx_slist_merge_after(networks->slist, network_slist);
+
+      do
+	{
+	  p_leoparse_object_t  object        = NULL;
+	  p_tbx_htable_t       network_entry = NULL;
+	  char                *name          = NULL;
+	  
+	  object = tbx_slist_remove_from_head(network_slist);
+	  network_entry = leoparse_get_htable(object);
+	  
+	  name = leoparse_read_id(network_entry, "name");
+	  tbx_htable_add(networks->htable, name, network_entry);
+	}
+      while(!tbx_slist_is_nil(network_slist));      
+    }
+  LOG_OUT();
+}
+
+void
+include_network_files(p_leo_networks_t networks,
+		      p_tbx_slist_t    include_files_slist)
+{
+  LOG_IN();
+  tbx_slist_ref_to_head(include_files_slist);
+      
+  do
+    {
+      p_leoparse_object_t  object         = NULL;
+      p_tbx_htable_t       include_htable = NULL;
+      char                *filename       = NULL;
+      
+      object = tbx_slist_ref_get(include_files_slist);
+      filename = leoparse_get_id(object);
+      
+      TRACE_STR("Including", filename);
+      include_htable = leoparse_parse_local_file(filename);
+      process_network_include_file(networks, include_htable);
+
+      // *** include_htable should be freed here *** //
+      include_htable = NULL;
+    }
+  while (tbx_slist_ref_forward(include_files_slist));
+  LOG_OUT();
+}
+

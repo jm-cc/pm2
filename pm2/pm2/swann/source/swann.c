33f30fb3c228acb400fc5da2f2c4e91d3db95e99
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 15:01:22 +0000

    rev 1.9 at 2002/04/05 13:53:44 GMT  - resynchronisation avec version O.A.
    (Logical change 1.639)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8349 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/swann/source/swann.c
+++ pm2/pm2/swann/source/swann.c
@@ -1,4 +1,3 @@
-
 /*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
@@ -25,353 +24,199 @@
 
 #include "swann.h"
 
-/*
- * Commands
- * --------
- */
-static void
-swann_command_open_file(p_swann_net_server_t net_server)
+static
+p_swann_t
+swann_object_init(void)
 {
-  p_swann_file_t  file;
-  char           *file_name;
-  
+  p_swann_t       swann  = NULL;
+  p_ntbx_client_t client = NULL;
+
   LOG_IN();
-  file_name = swann_net_receive_string(net_server->controler->client);
-
-  file = malloc(sizeof(swann_file_t));
-  CTRL_ALLOC(file);
-  file->state = swann_file_state_uninitialized;
+  swann = swann_init();
   
-  swann_file_init(file, file_name);
-  swann_file_open(file);
+  swann->session = swann_session_init();
 
-  swann_net_send_long(net_server->controler->client, (long)file);
-  free(file_name);
+  client = ntbx_client_cons();
+  ntbx_tcp_client_init(client);
+  swann->session->leonie_link = client;
   LOG_OUT();
+
+  return swann;
 }
 
-static void
-swann_command_create_file(p_swann_net_server_t net_server)
+static
+p_swann_settings_t
+process_command_line(int    argc,
+		     char **argv)
 {
-  p_swann_file_t  file;
-  char           *file_name;
+  p_swann_settings_t settings = NULL;
+
+  tbx_arg_iterator(argc, argv, swann_usage);
   
   LOG_IN();
-  file_name = swann_net_receive_string(net_server->controler->client);
+  if (!tbx_argit_init())
+    swann_usage();
 
-  file = malloc(sizeof(swann_file_t));
-  CTRL_ALLOC(file);
-  file->state = swann_file_state_uninitialized;
-  
-  swann_file_init(file, file_name);
-  swann_file_create(file);
+  settings = swann_settings_init();
 
-  swann_net_send_long(net_server->controler->client, (long)file);
-  free(file_name);
-  LOG_OUT();
-}
-
-static void
-swann_command_close_file(p_swann_net_server_t net_server)
-{
-  p_swann_file_t file;
-  
-  LOG_IN();
-  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
-  swann_file_close(file);
-  LOG_OUT();
-}
-
-static void
-swann_command_destroy_file(p_swann_net_server_t net_server)
-{
-  p_swann_file_t file;
-  
-  LOG_IN();
-  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
-  swann_file_destroy(file);
-  free(file);
-  LOG_OUT();
-}
-
-static void
-swann_command_read_file_block(p_swann_net_server_t net_server)
-{
-  p_swann_file_t file;
-  
-  LOG_IN();
-  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
-  LOG_PTR("File handle", file);
-  swann_file_send_block(net_server->controler, file);
-  LOG_OUT();
-}
-
-static void
-swann_command_write_file_block(p_swann_net_server_t net_server)
-{
-  p_swann_file_t file;
-  
-  LOG_IN();
-  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
-  swann_file_receive_block(net_server->controler, file);
-  LOG_OUT();
-}
-
-static void
-swann_command_exec(p_swann_net_server_t net_server)
-{
-  FAILURE("unimplemented function");
-}
-
-static void
-swann_command_swann_spawn(p_swann_net_server_t net_server)
-{
-  FAILURE("unimplemented function");
-}
-
-static void
-swann_command_mad_spawn(p_swann_net_server_t net_server)
-{
-  FAILURE("unimplemented function");
-}
-
-static void
-swann_command_send_data_block(p_swann_net_server_t net_server)
-{
-  FAILURE("unimplemented function");
-}
-
-static void
-swann_command_receive_data_block(p_swann_net_server_t net_server)
-{
-  FAILURE("unimplemented function");
-}
-
-/*
- * Command processor
- * -----------------
- */
-static void
-swann_command_processor(p_swann_net_server_t net_server)
-{
-  p_ntbx_client_t controler = net_server->controler->client;
-  tbx_bool_t      fin       = tbx_false;
-
-  LOG_IN();
-  while(!fin)
+  if (tbx_argit_more_opts())
     {
-      ntbx_pack_buffer_t    pack_buffer;
-      ntbx_command_code_t   command;
-      size_t                length;
-      swann_net_server_id_t address;
+      if (tbx_argit_opt_equals("--help"))
+	{
+	  swann_help();
+	}
       
-      while (!ntbx_tcp_read_poll(1, &controler));
-      ntbx_tcp_read_block(controler, &pack_buffer, sizeof(ntbx_pack_buffer_t));
-      address = (swann_net_server_id_t)ntbx_unpack_int(&pack_buffer);
-
-      DISP_VAL("Address", address);
-
-      while (!ntbx_tcp_read_poll(1, &controler));
-      ntbx_tcp_read_block(controler, &pack_buffer, sizeof(ntbx_pack_buffer_t));
-      length = (size_t)ntbx_unpack_int(&pack_buffer);
-
-      if (address == net_server->id)
+      do
 	{
-	  if (length != sizeof(ntbx_pack_buffer_t))
-	    FAILURE("invalid command packet size");
-	  
-	  while (!ntbx_tcp_read_poll(1, &controler));
-	  ntbx_tcp_read_block(controler,
-			      &pack_buffer,
-			      sizeof(ntbx_pack_buffer_t));
-	  command = (ntbx_command_code_t)ntbx_unpack_int(&pack_buffer);
-
-	  switch (command)
+	  if (tbx_argit_vopt_equals("--mad_leonie"))
 	    {
-	      /* synchronization */
-	    case ntbx_command_terminate:
-	      {
-		fin = tbx_true;
-		break;
-	      }
-	    case ntbx_command_completed:
-	      {
-		break;
-	      }
+	      if (!settings->leonie_server_host_name)
+		{
+		  settings->leonie_server_host_name =
+		    tbx_argit_vopt_value_cstr();
+		}
+	      else
+		swann_terminate("duplicate leonie host name definition"); 
 
-	      /* file management */
-	    case ntbx_command_open_file:
-	      {
-		swann_command_open_file(net_server);
-		break;
-	      }
-	    case ntbx_command_create_file:
-	      {
-		swann_command_create_file(net_server);
-		break;
-	      }
-	    case ntbx_command_close_file:
-	      {
-		swann_command_close_file(net_server);
-		break;
-	      }
-	    case ntbx_command_destroy_file:
-	      {
-		swann_command_destroy_file(net_server);
-		break;
-	      }
-	    case ntbx_command_read_file_block:
-	      {
-		swann_command_read_file_block(net_server);
-		break;
-	      }
-	    case ntbx_command_write_file_block:
-	      {
-		swann_command_write_file_block(net_server);
-		break;
-	      }
-	  
-	      /* remote command execution */
-	    case ntbx_command_exec:
-	      {
-		swann_command_exec(net_server);
-		FAILURE("unimplemented feature");
-		break;
-	      }
-	    case ntbx_command_swann_spawn:
-	      {
-		swann_command_swann_spawn(net_server);
-		break;
-	      }
-	    case ntbx_command_mad_spawn:
-	      {
-		swann_command_mad_spawn(net_server);
-		break;
-	      }
-	  
-	      /* data transfer */
-	    case ntbx_command_send_data_block:
-	      {
-		swann_command_send_data_block(net_server);
-		break;
-	      }
-	    case ntbx_command_receive_data_block:
-	      {
-		swann_command_receive_data_block(net_server);
-		break;
-	      }
-
-	      /* default */
-	    default:
-	      {
-		FAILURE("unknown command code");
-	      }
+	      DISP_STR("Leonie host name",
+		       settings->leonie_server_host_name);
 	    }
+	  else if (tbx_argit_vopt_equals("--mad_link"))
+	    {
+	      if (!settings->leonie_server_port)
+		{
+		  settings->leonie_server_port =
+		    tbx_argit_vopt_value_cstr();
+		}
+	      else
+		swann_terminate("duplicate leonie link port number definition"); 
+
+	      DISP_STR("Leonie link port number",
+		       settings->leonie_server_port);
+	    }
+	  else if (tbx_argit_arg_equals("-x"))
+	    {
+	      settings->xterm_mode = tbx_true;
+	      settings->log_mode   = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("--x"))
+	    {
+	      settings->xterm_mode = tbx_false;
+	      settings->gdb_mode   = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("-l"))
+	    {
+	      settings->log_mode   = tbx_true;
+
+	      settings->xterm_mode = tbx_false;
+	      settings->gdb_mode   = tbx_false;
+	      settings->pause_mode = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("--l"))
+	    {
+	      settings->log_mode = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("-p"))
+	    {
+	      settings->pause_mode = tbx_true;
+
+	      settings->gdb_mode   = tbx_false;
+	      settings->log_mode   = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("--p"))
+	    {
+	      settings->pause_mode = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("-d"))
+	    {
+	      settings->gdb_mode   = tbx_true;
+	      
+	      settings->xterm_mode = tbx_true;
+	      settings->log_mode   = tbx_false;
+	      settings->pause_mode = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("--d"))
+	    {
+	      settings->gdb_mode = tbx_false;
+	    }
+	  else if (tbx_argit_arg_equals("-smp"))
+	    {
+	      settings->smp_mode   = tbx_true;
+	    }
+	  else if (tbx_argit_arg_equals("--smp"))
+	    {
+	      settings->smp_mode = tbx_false;
+	    }
+	  else
+	    tbx_argit_invalid_arg();
+	  
 	}
-      else
-	{
-	  /* Routing/forwarding needed */
-	  FAILURE("Routing/forwarding not implemented");
-	}
+      while (tbx_argit_next_opt());
     }
-  LOG_OUT();
+  else
+    {
+      DISP("no command line options");
+    }
+
+  return settings;
 }
 
+static 
+void
+swann_leonie_link_init(p_swann_t swann)
+{
+  p_swann_settings_t     settings = NULL;
+  p_swann_session_t      session  = NULL;
+  p_ntbx_client_t        client   = NULL;
+  int                    status   = ntbx_failure;
+  ntbx_connection_data_t data;
+
+  LOG_IN();
+  settings = swann->settings;
+  session  = swann->session;
+  client   = session->leonie_link;
+
+  strcpy(data.data, settings->leonie_server_port);
+  status = ntbx_tcp_client_connect(client,
+				   settings->leonie_server_host_name,
+				   &data);
+  if (status == ntbx_failure)
+    FAILURE("could not setup the Leonie link");
+
+  DISP("Leonie link is up");  
+  swann_ntbx_send_string(client, client->local_host);
+  session->process_rank = swann_ntbx_receive_int(client);
+  DISP_VAL("process rank", session->process_rank);
+  LOG_OUT();
+}
 
 /*
  * Initialization
  * --------------
  */
 int 
-main(int   argc   __attribute__((unused)),
-     char *argv[] __attribute__((unused)))
+main(int   argc,
+     char *argv[])
 {
-  p_swann_net_server_t   net_server         = NULL;
-  int                    param_id           = 0;
-  swann_net_server_id_t  id                 = -1;
-  int                    param_master       = 0;
-  char                  *master_host_name   = NULL;
-  int                    param_cnx          = 0;
-  ntbx_connection_data_t connection_data    = {0};
+  p_swann_t swann = NULL;
 
-  /* Toolboxes initialization */
-  tbx_init();
-  ntbx_init();
+  LOG_IN();
+  common_pre_init(&argc, argv, NULL);
+  common_post_init(&argc, argv, NULL);
+  
+  DISP("== Initializing parser");
+  leoparse_init(argc, argv);
+  leoparse_purge_cmd_line(&argc, argv);
+  
+  DISP("== Initializing internal objects");
+  swann = swann_object_init();
 
-  /* Command line parsing */
-  {
-    int i;
+  DISP("== Processing command line");
+  swann->settings = process_command_line(argc, argv);
 
-    i = 1;
-    
-    while (i < argc)
-      {
-	if(!strcmp(argv[i], "-id"))
-	{
-	  if (param_id)
-	    FAILURE("id parameter already specified");
-
-	  param_id++;
-
-	  if (i == (argc - 1))
-	    FAILURE("-id option must be followed "
-		    "by the id of the process");
-
-	  id = atoi(argv[i + 1]);
-	  i++;
-	}
-      else if (!strcmp(argv[i], "-cnx"))
-	{
-	  if (param_cnx)
-	    FAILURE("cnx parameter already specified");
-	  
-	  param_cnx++;
-
-	  if (i == (argc - 1))
-	    FAILURE("-cnx must be followed "
-		    "by the master node connection parameter");
-
-	  strcpy(connection_data, argv[i + 1]);
-	  i++;
-	}
-      else if (!strcmp(argv[i], "-master"))
-	{
-	  if (param_master)
-	    FAILURE("master parameter already specified");
-
-	  param_master++;
-	  if(i == (argc - 1))
-	    FAILURE(" -master must be followed by the master host name");
-
-	  master_host_name = TBX_MALLOC(strlen(argv[i + 1]) + 1);
-	  CTRL_ALLOC(master_host_name);
-	    
-	  strcpy(master_host_name,
-		 argv[i + 1]);
-	  i++;
-	}
-      else
-	FAILURE("invalid parameter");
-	
-      i++;
-    }
-  }
-
-    {
-      char   output[1024];
-      int    f;
-
-      sprintf(output, "/tmp/%s-swannlog-%d", getenv("USER"), (int)id);
-      f = open(output, O_WRONLY|O_CREAT|O_TRUNC, 0600);
-      dup2(f, STDOUT_FILENO);
-      dup2(STDOUT_FILENO, STDERR_FILENO);
-    }
-
-  /* Net server initialization */
-  net_server = swann_net_server_init(id,
-				     master_host_name,
-				     &connection_data);
-  swann_command_processor(net_server);
+  DISP("== Initializing Leonie link");
+  swann_leonie_link_init(swann);
+  LOG_OUT();
 
   return 0;
 }

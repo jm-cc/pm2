53609ec6231cf3190fdacc924383d13fed1ac1b3
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:48:05 +0000

    rev 1.6 at 2000/07/11 19:39:43 GMT  Bug fixed: PM2_DEFAULT_LOADER is only defined if the madeleine package is not used
    (Logical change 1.279)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2995 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/config/marcel.sh
+++ pm2/pm2/marcel/config/marcel.sh
@@ -1,4 +1,9 @@
-PM2_DEFAULT_LOADER=${PM2_ROOT}/marcel/bin/marcelload
+
+case " $PM2_LIBS " in
+    *\ mad?\ *) ;;
+    *) PM2_DEFAULT_LOADER=${PM2_ROOT}/marcel/bin/marcelload ;;
+esac
+
 if [ "${PM2_ARCH}" = RS6K_ARCH ]; then
   PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -mno-powerpc"
 fi

7418af33dce48771e8381765931064d015a1fc34
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:36:25 +0000

    rev 1.5 at 2001/04/09 20:04:40 GMT  Modified MA__WORK section and implemented a preliminary working 'deviate' functionality.
    (Logical change 1.496)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7060 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_work.h
+++ pm2/pm2/marcel/include/sys/marcel_work.h
@@ -21,24 +21,13 @@
 
 /* flags indiquant le type de travail à faire */
 enum {
-  MARCEL_WORK_DEVIATE=0x1,
+  MARCEL_WORK_DEVIATE = 0x1,
 };
 
-extern volatile int marcel_global_work;
-extern marcel_lock_t marcel_work_lock;
-
-#define HAS_WORK(self) (self->has_work || marcel_global_work)
-#define LOCK_WORK(self) (marcel_lock_acquire(&marcel_work_lock))
-#define UNLOCK_WORK(self) (marcel_lock_release(&marcel_work_lock))
+#define HAS_WORK(self)          ((self)->has_work)
+#define HAS_DEVIATE_WORK(self)  ((self)->has_work & MARCEL_WORK_DEVIATE)
 
 void do_work(marcel_t self);
-
-#else /* MA__WORK */
-
-#define HAS_WORK(self) 0
-#define LOCK_WORK(self) ((void)0)
-#define UNLOCK_WORK(self) ((void)0)
-#define do_work(marcel_self) ((void)0)
 
 #endif /* MA__WORK */
 

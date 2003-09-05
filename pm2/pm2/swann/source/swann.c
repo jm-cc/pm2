9744102e0ab1eb53f8aa965479d42e89b7b6b349
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:18:09 +0000

    rev 1.4 at 2000/04/21 15:40:20 GMT  - fusion de la barnche pm2_mad2_multicluster
    (Logical change 1.160)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1774 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/swann/source/swann.c
+++ pm2/pm2/swann/source/swann.c
@@ -34,6 +34,22 @@
 
 ______________________________________________________________________________
 $Log: swann.c,v $
+Revision 1.4  2000/04/21 15:40:20  oaumage
+- fusion de la barnche pm2_mad2_multicluster
+
+Revision 1.3.2.4  2000/04/15 15:23:15  oaumage
+- progression du transfert de fichiers distants
+
+Revision 1.3.2.3  2000/04/03 13:45:25  oaumage
+- ajout de fonctions de transmission pour int, long, double et string
+- support du transfert de fichiers distants bloc a bloc
+
+Revision 1.3.2.2  2000/04/03 08:45:43  oaumage
+- pre-integration du processeur de commandes
+
+Revision 1.3.2.1  2000/03/30 15:16:36  oaumage
+- transmission du nom de la machine locale, pour debogage
+
 Revision 1.3  2000/03/27 12:54:05  oaumage
 - progression des fonctionnalites:
   * support reseau
@@ -51,13 +67,270 @@ ______________________________________________________________________________
  * swann.c
  * =======
  */ 
-
+#define DEBUG
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "swann.h"
 
+/*
+ * Commands
+ * --------
+ */
+static void
+swann_command_open_file(p_swann_net_server_t net_server)
+{
+  p_swann_file_t  file;
+  char           *file_name;
+  
+  LOG_IN();
+  file_name = swann_net_receive_string(net_server->controler->client);
+
+  file = malloc(sizeof(swann_file_t));
+  CTRL_ALLOC(file);
+  file->state = swann_file_state_uninitialized;
+  
+  swann_file_init(file, file_name);
+  swann_file_open(file);
+
+  swann_net_send_long(net_server->controler->client, (long)file);
+  free(file_name);
+  LOG_OUT();
+}
+
+static void
+swann_command_create_file(p_swann_net_server_t net_server)
+{
+  p_swann_file_t  file;
+  char           *file_name;
+  
+  LOG_IN();
+  file_name = swann_net_receive_string(net_server->controler->client);
+
+  file = malloc(sizeof(swann_file_t));
+  CTRL_ALLOC(file);
+  file->state = swann_file_state_uninitialized;
+  
+  swann_file_init(file, file_name);
+  swann_file_create(file);
+
+  swann_net_send_long(net_server->controler->client, (long)file);
+  free(file_name);
+  LOG_OUT();
+}
+
+static void
+swann_command_close_file(p_swann_net_server_t net_server)
+{
+  p_swann_file_t file;
+  
+  LOG_IN();
+  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
+  swann_file_close(file);
+  LOG_OUT();
+}
+
+static void
+swann_command_destroy_file(p_swann_net_server_t net_server)
+{
+  p_swann_file_t file;
+  
+  LOG_IN();
+  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
+  swann_file_destroy(file);
+  free(file);
+  LOG_OUT();
+}
+
+static void
+swann_command_read_file_block(p_swann_net_server_t net_server)
+{
+  p_swann_file_t file;
+  
+  LOG_IN();
+  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
+  LOG_PTR("File handle", file);
+  swann_file_send_block(net_server->controler, file);
+  LOG_OUT();
+}
+
+static void
+swann_command_write_file_block(p_swann_net_server_t net_server)
+{
+  p_swann_file_t file;
+  
+  LOG_IN();
+  file = (p_swann_file_t)swann_net_receive_long(net_server->controler->client);
+  swann_file_receive_block(net_server->controler, file);
+  LOG_OUT();
+}
+
+static void
+swann_command_exec(p_swann_net_server_t net_server)
+{
+  FAILURE("unimplemented function");
+}
+
+static void
+swann_command_swann_spawn(p_swann_net_server_t net_server)
+{
+  FAILURE("unimplemented function");
+}
+
+static void
+swann_command_mad_spawn(p_swann_net_server_t net_server)
+{
+  FAILURE("unimplemented function");
+}
+
+static void
+swann_command_send_data_block(p_swann_net_server_t net_server)
+{
+  FAILURE("unimplemented function");
+}
+
+static void
+swann_command_receive_data_block(p_swann_net_server_t net_server)
+{
+  FAILURE("unimplemented function");
+}
+
+/*
+ * Command processor
+ * -----------------
+ */
+static void
+swann_command_processor(p_swann_net_server_t net_server)
+{
+  p_ntbx_client_t controler = net_server->controler->client;
+  tbx_bool_t      fin       = tbx_false;
+
+  LOG_IN();
+  while(!fin)
+    {
+      ntbx_pack_buffer_t    pack_buffer;
+      ntbx_command_code_t   command;
+      size_t                length;
+      swann_net_server_id_t address;
+      
+      while (!ntbx_tcp_read_poll(1, &controler));
+      ntbx_tcp_read_block(controler, &pack_buffer, sizeof(ntbx_pack_buffer_t));
+      address = (swann_net_server_id_t)ntbx_unpack_int(&pack_buffer);
+
+      DISP_VAL("Address", address);
+
+      while (!ntbx_tcp_read_poll(1, &controler));
+      ntbx_tcp_read_block(controler, &pack_buffer, sizeof(ntbx_pack_buffer_t));
+      length = (size_t)ntbx_unpack_int(&pack_buffer);
+
+      if (address == net_server->id)
+	{
+	  if (length != sizeof(ntbx_pack_buffer_t))
+	    FAILURE("invalid command packet size");
+	  
+	  while (!ntbx_tcp_read_poll(1, &controler));
+	  ntbx_tcp_read_block(controler,
+			      &pack_buffer,
+			      sizeof(ntbx_pack_buffer_t));
+	  command = (ntbx_command_code_t)ntbx_unpack_int(&pack_buffer);
+
+	  switch (command)
+	    {
+	      /* synchronization */
+	    case ntbx_command_terminate:
+	      {
+		fin = tbx_true;
+		break;
+	      }
+	    case ntbx_command_completed:
+	      {
+		break;
+	      }
+
+	      /* file management */
+	    case ntbx_command_open_file:
+	      {
+		swann_command_open_file(net_server);
+		break;
+	      }
+	    case ntbx_command_create_file:
+	      {
+		swann_command_create_file(net_server);
+		break;
+	      }
+	    case ntbx_command_close_file:
+	      {
+		swann_command_close_file(net_server);
+		break;
+	      }
+	    case ntbx_command_destroy_file:
+	      {
+		swann_command_destroy_file(net_server);
+		break;
+	      }
+	    case ntbx_command_read_file_block:
+	      {
+		swann_command_read_file_block(net_server);
+		break;
+	      }
+	    case ntbx_command_write_file_block:
+	      {
+		swann_command_write_file_block(net_server);
+		break;
+	      }
+	  
+	      /* remote command execution */
+	    case ntbx_command_exec:
+	      {
+		swann_command_exec(net_server);
+		FAILURE("unimplemented feature");
+		break;
+	      }
+	    case ntbx_command_swann_spawn:
+	      {
+		swann_command_swann_spawn(net_server);
+		break;
+	      }
+	    case ntbx_command_mad_spawn:
+	      {
+		swann_command_mad_spawn(net_server);
+		break;
+	      }
+	  
+	      /* data transfer */
+	    case ntbx_command_send_data_block:
+	      {
+		swann_command_send_data_block(net_server);
+		break;
+	      }
+	    case ntbx_command_receive_data_block:
+	      {
+		swann_command_receive_data_block(net_server);
+		break;
+	      }
+
+	      /* default */
+	    default:
+	      {
+		FAILURE("unknown command code");
+	      }
+	    }
+	}
+      else
+	{
+	  /* Routing/forwarding needed */
+	  FAILURE("Routing/forwarding not implemented");
+	}
+    }
+  LOG_OUT();
+}
+
+
+/*
+ * Initialization
+ * --------------
+ */
 int 
 main(int   argc   __attribute__((unused)),
      char *argv[] __attribute__((unused)))
@@ -133,10 +406,21 @@ main(int   argc   __attribute__((unused)),
     }
   }
 
+    {
+      char   output[1024];
+      int    f;
+
+      sprintf(output, "/tmp/%s-swannlog-%d", getenv("USER"), (int)id);
+      f = open(output, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+      dup2(f, STDOUT_FILENO);
+      dup2(STDOUT_FILENO, STDERR_FILENO);
+    }
+
   /* Net server initialization */
   net_server = swann_net_server_init(id,
 				     master_host_name,
 				     &connection_data);
-  
+  swann_command_processor(net_server);
+
   return 0;
 }

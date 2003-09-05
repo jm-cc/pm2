1e043a40692c6b390726b03eedec18844cd9f3be
ens-lyon.fr!vbernard <ens-lyon.fr!vbernard@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:51 +0000

    rev 1.3 at 2000/07/18 22:32:12 GMT  *** empty log message ***
    (Logical change 1.294)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3103 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsmlib_hbrc_mw_update_protocol.c
+++ pm2/pm2/dsm/source/dsmlib_hbrc_mw_update_protocol.c
@@ -81,7 +81,7 @@ void dsmlib_hbrc_mw_update_ws(unsigned long index, dsm_node_t req_node, int arg)
   dsm_lock_page(index);
 #ifdef DSM_PROT_TRACE
   assert(dsm_get_prob_owner(index) == dsm_self());
-#endif
+#endif //DSM_PROT_TRACE
   dsm_add_to_copyset(index, req_node);
   dsm_send_page(req_node, index, WRITE_ACCESS, arg);
   dsm_unlock_page(index);
@@ -118,7 +118,7 @@ void dsmlib_hbrc_mw_update_is(unsigned long index, dsm_node_t req_node, dsm_node
       }
 #ifdef DSM_PROT_TRACE
     tfprintf(stderr, "exiting the invalidate server(%d), req node =%d\n", index, req_node);
-#endif
+#endif //DSM_PROT_TRACE
 }
 
 void dsmlib_hbrc_mw_update_rps(void *addr, dsm_access_t access, dsm_node_t reply_node, int arg)
@@ -196,11 +196,14 @@ void dsmlib_hbrc_mw_update_prot_init(unsigned long index)
 {
   int i, count = 0;
   hbrc_mw_update_list = init_be_list(10);
+#ifdef DSM_PROT_TRACE
   fprintf(stderr,"Initial  printing\n");
   fprintf_be_list(hbrc_mw_update_list);
   fprintf(stderr,"Done printing\n");
   fprintf(stderr,"Nb total pages: %lu\n", dsm_get_nb_static_pages() + dsm_get_nb_pseudo_static_pages());
-  /*  for(i = 0; i < dsm_get_nb_static_pages() + dsm_get_nb_pseudo_static_pages(); i++)
+#endif // DSM_PROT_TRACE
+
+/*  for(i = 0; i < dsm_get_nb_static_pages() + dsm_get_nb_pseudo_static_pages(); i++)
     if(dsm_get_page_protocol(i) == protocol_number){
       count++;
       if(dsm_get_prob_owner(i) == dsm_self())
@@ -222,24 +225,55 @@ void dsmlib_hrbc_send_diffs(unsigned long index, dsm_node_t dest_node)
   int i = 0;
   void *start;
   void *twin;
-  int granularity = 4;
+  int stacking_mode = 0;
+  int stack_start = 0;
+  int stack_size = 0;
+
   start = dsm_get_page_addr(index);
   twin = dsm_get_twin(index);
 #ifdef DSM_PROT_TRACE
   assert(dsm_get_twin(index) != NULL);
   fprintf(stderr,"[%s]Sending diffs for %lu from %d to %d\n",__FUNCTION__, index, dsm_self(), dest_node); 
-#endif DSM_PROT_TRACE
+#endif // DSM_PROT_TRACE
 
   pm2_completion_init(&c, NULL, NULL);
-
   pm2_rawrpc_begin((int)dest_node, DSM_LRPC_HBRC_DIFFS, NULL);
   pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index, sizeof(unsigned long));
-  for(i = 0; i < (DSM_PAGE_SIZE / 4);i++)
+  for(i = 0; i < (DSM_PAGE_SIZE / 4);i++){
     if(*(((int *)start) + i) != *(((int *)twin) + i)){
-      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&i, sizeof(int));
-      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&granularity, sizeof(int));
-      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)(((int *)start) + i), granularity);
+      if(stacking_mode){
+	stack_size+=sizeof(int);
+      }
+      else{
+	stacking_mode = 1;
+        stack_start = i;
+	stack_size = 4;
+      }
     }
+    else{
+      if(stacking_mode){
+        stacking_mode = 0;
+#ifdef DSM_PROT_TRACE
+	tfprintf(stderr, "[%s]Packing offset %d, size(in words) %d\n", sizeof(int) * stack_start, stack_size);
+#endif //DSM_PROT_TRACE
+        pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&stack_start, sizeof(int));
+        pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&stack_size, sizeof(int));
+        pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)(((int *)start) + stack_start), stack_size);
+	tfprintf(stderr, "[%s]Packed\n");
+      }
+    }
+  }
+  if(stacking_mode){
+#ifdef DSM_PROT_TRACE
+    tfprintf(stderr, "[%s]Packing offset %d, size(in words) %d\n", sizeof(int) * stack_start, stack_size);
+#endif //DSM_PROT_TRACE
+    pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&stack_start, sizeof(int));
+    pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&stack_size, sizeof(int));
+    pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)(((int *)start) + stack_start), stack_size);
+#ifdef DSM_PROT_TRACE
+    tfprintf(stderr, "[%s]Packed\n");
+#endif //DSM_PROT_TRACE
+  }
 
   i = -1;
   // send the -1 value to terminate 
@@ -266,12 +300,17 @@ void DSM_HRBC_DIFFS_threaded_func(void)
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index,
     sizeof(unsigned long));
   start = dsm_get_page_addr(index);
+  
   pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&i, sizeof(void *));
   while (i != -1)
   {
-      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&size, sizeof(int));
-      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)((int *)start + i), size);
-      pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&i, sizeof(void *));
+    pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&size, sizeof(int));
+    pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)((int *)start + i), size);
+#ifdef DSM_PROT_TRACE
+    tfprintf(stderr,"[%s] Received %d words diff at %d offset, first word is %d\n", __FUNCTION__, size, i * sizeof(int), *((int *)start + i));
+#endif //DSM_PROT_TRACE
+
+    pm2_unpack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&i, sizeof(void *));
   }
 
   pm2_unpack_completion(SEND_CHEAPER, RECV_CHEAPER,&c);

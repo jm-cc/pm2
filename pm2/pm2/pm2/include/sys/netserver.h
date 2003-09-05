26a9890ce6be506d8778f8ac593c224bb730e9cf
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:30:47 +0000

    rev 1.9 at 2001/02/23 15:45:49 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.415)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5399 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/sys/netserver.h
+++ pm2/pm2/pm2/include/sys/netserver.h
@@ -1,4 +1,19 @@
 
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #ifndef NETSERVER_EST_DEF
 #define NETSERVER_EST_DEF
 
@@ -11,9 +26,9 @@ enum {
 };
 
 #ifdef MAD2
-void netserver_start(p_mad_channel_t channel, unsigned priority);
+void netserver_start(p_mad_channel_t channel);
 #else
-void netserver_start(unsigned priority);
+void netserver_start(void);
 #endif
 
 void netserver_wait_end(void);

af79e1ae054622e5a3e95094c89abe69c7e6c497
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.2 at 2000/07/14 16:16:54 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3064 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/dsm/source/dsm_lock.c
@@ -0,0 +1,73 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+*/
+
+#include "marcel.h"
+#include "dsm_lock.h"
+
+#define DSM_LOCKS 15
+
+static struct {
+  dsm_lock_struct_t locks[DSM_LOCKS];
+  int nb;
+} _dsm_lock_table;
+
+
+void dsm_lock_init(dsm_lock_t *lock, dsm_lock_attr_t *attr)
+{
+  int i;
+
+  *lock = &_dsm_lock_table.locks[_dsm_lock_table.nb++];
+
+  if(!attr)
+    (*lock)->dsm_mutex.owner = 0;
+  else
+    (*lock)->dsm_mutex.owner = attr->dsm_mutex_attr.owner;
+
+  marcel_mutex_init(&((*lock)->dsm_mutex.mutex), NULL);
+
+  if (attr)
+    {
+      (*lock)->nb_prot = attr->nb_prot;
+      
+      for(i = 0; i < attr->nb_prot; i++)
+	((*lock)->prot)[i] = (attr->prot)[i];
+    }
+  else
+     (*lock)->nb_prot =0;
+}
+
+
+
+

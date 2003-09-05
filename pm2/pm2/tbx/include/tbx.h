2adb4946c6f9e164b29e38fbe586230947ee5751
ens-lyon.fr!denisa <ens-lyon.fr!denisa@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 18:31:55 +0000

    rev 1.17 at 2003/06/18 17:19:47 GMT  bug fix pour Darwin.
    (Logical change 1.743)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9270 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/include/tbx.h
+++ pm2/pm2/tbx/include/tbx.h
@@ -52,6 +52,11 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
+#ifndef DARWIN_SYS
+#include <malloc.h>
+#else
+#include <sys/malloc.h>
+#endif
 
 #include "tbx_timing.h"
 #include "tbx_macros.h"

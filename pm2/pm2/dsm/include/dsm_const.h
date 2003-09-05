2a044a7aaa02362333e11c90543bb9c4f85251c7
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.4 at 2000/07/14 16:16:47 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3099 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_const.h
+++ pm2/pm2/dsm/include/dsm_const.h
@@ -46,9 +46,27 @@ typedef int dsm_node_t;
 
 #define NO_NODE -1
 
-#define NO_FATHER -1
+typedef void (*dsm_rf_action_t)(unsigned long index); // read fault handler
 
-typedef int dsm_state_t;
+typedef void (*dsm_wf_action_t)(unsigned long index); // write fault handler
+
+typedef void (*dsm_rs_action_t)(unsigned long index, dsm_node_t req_node, int tag); // read server
+
+typedef void (*dsm_ws_action_t)(unsigned long index, dsm_node_t req_node, int tag); // write server
+
+typedef void (*dsm_is_action_t)(unsigned long index, dsm_node_t req_node, dsm_node_t new_owner); // invalidate server
+
+typedef void (*dsm_rp_action_t)(void *addr, dsm_access_t access, dsm_node_t reply_node, int tag); // receive page server
+
+typedef void (*dsm_erp_action_t)(void *addr, dsm_access_t access, dsm_node_t reply_node, unsigned long page_size, int tag); // expert receive page server
+
+typedef void (*dsm_acq_action_t)(); // acquire func
+
+typedef void (*dsm_rel_action_t)(); // release func
+
+typedef void (*dsm_pi_action_t)(int prot_id); // acquire func
+
+typedef void (*dsm_pa_action_t)(int index); // release func
 
 #endif
 

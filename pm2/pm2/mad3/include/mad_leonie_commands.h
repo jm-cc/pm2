c79d1f9787227a11c79b753ab26667aada64a8ce
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:46:00 +0000

    rev 1.1 at 2001/06/19 13:59:44 GMT  - suppression d'une fuite de memoire, ajout des macros LDISP, programme mad_ping version MadIII
    (Logical change 1.569)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7733 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/mad3/include/mad_leonie_commands.h
@@ -0,0 +1,13 @@
+#ifndef MAD_LEONIE_COMMANDS_H
+#define MAD_LEONIE_COMMANDS_H
+
+// Commands
+//----------///////////////////////////////////////////////////////////////
+
+typedef enum e_mad_leo_command
+{
+  mad_leo_command_end = 0,
+  mad_leo_command_print,
+} mad_leo_command_t, *p_mad_leo_command_t;
+
+#endif // MAD_LEONIE_COMMANDS_H

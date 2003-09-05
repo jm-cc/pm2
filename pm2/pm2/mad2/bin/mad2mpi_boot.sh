165cd1ca9d5fcbff7c0a643ee0b8cfea7feb9632
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:54:26 +0000

    rev 1.2 at 2000/06/07 11:24:48 GMT  - Mise a jour des scripts LAM-MPI
    (Logical change 1.222)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2623 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/bin/mad2mpi_boot.sh
+++ pm2/pm2/mad2/bin/mad2mpi_boot.sh
@@ -1,27 +1,9 @@
 #!/bin/sh
 ##########
 
-if [ ! -f ${MAD2_ROOT}/.mad2mpi_implementation ] ; then
-  echo "No MPI implementation has been selected"
-  exit 0
-fi
+echo "Calling recon"
+recon   -v ${MAD2_ROOT}/.mad2_conf
+echo Calling lamboot ...
+lamboot -v ${MAD2_ROOT}/.mad2_conf
 
-IMPL=`cat ${MAD2_ROOT}/.mad2mpi_implementation`
-echo "Booting MPI/${IMPL} ..."
-
-case "$IMPL" in
-  lam) 
-    echo "Calling recon"
-    recon   -v ${MAD2_ROOT}/.mad2_conf
-    echo Calling lamboot ...
-    lamboot -v ${MAD2_ROOT}/.mad2_conf
-    ;;
-  bip) 
-    echo "unimplemented"
-    ;;
-  *)
-    echo "invalid MPI implementation name"
-    exit 0
-    ;;
-esac
 ##########

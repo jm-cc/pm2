0d0ea8a68a10a8ebd3dfdaa46acde4e650fa98f7
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:38:57 +0000

    rev 1.4 at 2000/11/18 09:38:23 GMT  Updated mad1 implementation over LAM MPI.
    (Logical change 1.362)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3968 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad1/config/options/10mpi_lam.sh
+++ pm2/pm2/mad1/config/options/10mpi_lam.sh
@@ -1,7 +1,8 @@
 PM2_PROTOCOLS=mpi_lam
-LAM_DIR=${LAM_DIR:-${HOME}/lam}
+LAM_DIR=${LAM_DIR:-/usr/lib/lam}
 PM2_LOADER=${PM2_ROOT}/mad1/bin/mpi_lam/madload
 PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -I${LAM_DIR}/h"
-PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${LAM_DIR}/lib -lmpi -largs -ltrillium -ltstdio -lt"
+#PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${LAM_DIR}/lib -lmpi -largs -ltrillium -ltstdio -lt"
+PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${LAM_DIR}/lib -lmpi"
 PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNET_ARCH="'\"'mpi_lam'\"'
 PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNETINTERF_INIT=mad_mpi_netinterf_init"

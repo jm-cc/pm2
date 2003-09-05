eb1a7e1fb4c9308b9220272c11780aa9d679fdaa
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.1 at 2001/06/11 09:11:02 GMT  - tbx, import de MadIII
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7656 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/tbx/include/tbx_string.h
@@ -0,0 +1,35 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * Tbx_string.h : string of characters
+ * ===========------------------------
+ */
+
+#ifndef TBX_STRING_H
+#define TBX_STRING_H
+
+/*
+ * Data structures 
+ * ---------------
+ */
+typedef struct s_tbx_string
+{
+  size_t  length;
+  size_t  allocated_length;
+  char   *data;
+} tbx_string_t;
+
+#endif /* TBX_STRING_H */

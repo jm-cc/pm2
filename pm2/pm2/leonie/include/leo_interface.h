4570aa9b29e0b90c344a10c93d74cc1999625975
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.9 at 2001/06/11 14:53:21 GMT  - gestion de deconnexion avec leonie
    2003/09/05 13:56:25+02:00 ens-lyon.fr!oaumage
    rev 1.8 at 2001/06/11 07:40:09 GMT
     - leonie, import de MadIII
    
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7676 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leonie/include/leo_interface.h
+++ pm2/pm2/leonie/include/leo_interface.h
@@ -1,5 +1,4 @@
 /*
-/*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
  *
@@ -14,18 +13,129 @@
  * General Public License for more details.
  */
 
+/*
  * leo_interface.h
  * ---------------
  */
 
-#ifndef __LEO_INTERFACE_H
-#define __LEO_INTERFACE_H
-/*
-int
-leo_yy_input(char *buffer,
-	     int   max_size);
+#ifndef LEO_INTERFACE_H
+#define LEO_INTERFACE_H
 
-p_leo_cluster_definition_t
-leo_build_cluster_definition(p_leo_clu_cluster_file_t cluster_file);
-*/
-#endif /* __LEO_INTERFACE_H */
+// Objects initialisation
+//------------------------/////////////////////////////////////////////////
+
+p_leo_process_specific_t
+leo_process_specific_init(void);
+
+p_ntbx_server_t
+leo_net_server_init(void);
+
+p_tbx_htable_t
+leo_htable_init(void);
+
+p_leo_directory_t
+leo_directory_init(void);
+
+p_leo_dir_adapter_t
+leo_dir_adapter_init(void);
+
+p_leo_dir_driver_process_specific_t
+leo_dir_driver_process_specific_init(void);
+
+p_leo_dir_node_t
+leo_dir_node_init(void);
+
+p_leo_dir_driver_t
+leo_dir_driver_init(void);
+
+p_leo_dir_channel_process_specific_t
+leo_dir_channel_process_specific_init(void);
+
+p_leo_dir_channel_t
+leo_dir_channel_init(void);
+
+p_leo_dir_vchannel_process_routing_table_t
+leo_dir_vchannel_process_routing_table_init(void);
+
+p_leo_dir_vchannel_process_specific_t
+leo_dir_vchannel_process_specific_init(void);
+
+p_leo_dir_fchannel_t
+leo_dir_fchannel_init(void);
+
+p_leo_dir_vchannel_t
+leo_dir_vchannel_init(void);
+
+p_leo_networks_t
+leo_networks_init(void);
+
+p_leo_spawn_groups_t
+leo_spawn_groups_init(void);
+
+p_leo_spawn_group_t
+leo_spawn_group_init(void);
+
+p_leo_loader_t
+leo_loader_init(void);
+
+
+// Include files processing
+//--------------------------///////////////////////////////////////////////
+void
+process_network_include_file(p_leo_networks_t networks,
+			     p_tbx_htable_t   include_htable);
+
+void
+include_network_files(p_leo_networks_t networks,
+		      p_tbx_slist_t    include_files_slist);
+
+
+// Loaders
+//---------////////////////////////////////////////////////////////////////
+p_tbx_htable_t
+leo_loaders_register(void);
+
+
+// Loaders
+//---------////////////////////////////////////////////////////////////////
+void
+leo_send_int(p_ntbx_client_t client,
+	     const int       data);
+
+int
+leo_receive_int(p_ntbx_client_t client);
+
+void
+leo_send_unsigned_int(p_ntbx_client_t    client,
+		      const unsigned int data);
+
+unsigned int
+leo_receive_unsigned_int(p_ntbx_client_t client);
+
+void
+leo_send_string(p_ntbx_client_t  client,
+		const char      *string);
+
+char *
+leo_receive_string(p_ntbx_client_t client);
+
+
+
+// Usage
+//-------//////////////////////////////////////////////////////////////////
+void 
+leo_usage(void) TBX_NORETURN;
+
+void 
+leo_terminate(const char *msg) TBX_NORETURN;
+
+void 
+leo_error(const char *command) TBX_NORETURN;
+
+void
+leonie_failure_cleanup(void);
+
+void
+leonie_processes_cleanup(void);
+
+#endif /* LEO_INTERFACE_H */

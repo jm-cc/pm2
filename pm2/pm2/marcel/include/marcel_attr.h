ad47bf324285e3394cc2972b81cf2200079b4243
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 18:17:56 +0000

    rev 1.12 at 2003/04/29 18:23:17 GMT  added __THROW attribute for C++
    (Logical change 1.736)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9186 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_attr.h
+++ pm2/pm2/marcel/include/marcel_attr.h
@@ -44,17 +44,17 @@ _PRIVATE_ typedef struct __marcel_attr_s {
 #define MARCEL_CLASS_REGULAR      FALSE
 #define MARCEL_CLASS_REALTIME     TRUE
 
-int marcel_attr_init(marcel_attr_t *attr);
+int marcel_attr_init(marcel_attr_t *attr) __THROW;
 #define marcel_attr_destroy(attr_ptr)	0
 
-int marcel_attr_setstacksize(marcel_attr_t *attr, size_t stack);
-int marcel_attr_getstacksize(__const marcel_attr_t *attr, size_t *stack);
+int marcel_attr_setstacksize(marcel_attr_t *attr, size_t stack) __THROW;
+int marcel_attr_getstacksize(__const marcel_attr_t *attr, size_t *stack) __THROW;
 
-int marcel_attr_setstackaddr(marcel_attr_t *attr, void *addr);
-int marcel_attr_getstackaddr(__const marcel_attr_t *attr, void **addr);
+int marcel_attr_setstackaddr(marcel_attr_t *attr, void *addr) __THROW;
+int marcel_attr_getstackaddr(__const marcel_attr_t *attr, void **addr) __THROW;
 
-int marcel_attr_setdetachstate(marcel_attr_t *attr, boolean detached);
-int marcel_attr_getdetachstate(__const marcel_attr_t *attr, boolean *detached);
+int marcel_attr_setdetachstate(marcel_attr_t *attr, boolean detached) __THROW;
+int marcel_attr_getdetachstate(__const marcel_attr_t *attr, boolean *detached) __THROW;
 
 int marcel_attr_setuserspace(marcel_attr_t *attr, unsigned space);
 int marcel_attr_getuserspace(__const marcel_attr_t *attr, unsigned *space);
@@ -68,8 +68,8 @@ int marcel_attr_getmigrationstate(__const marcel_attr_t *attr, boolean *migratab
 int marcel_attr_setdeviationstate(marcel_attr_t *attr, boolean deviatable);
 int marcel_attr_getdeviationstate(__const marcel_attr_t *attr, boolean *deviatable);
 
-int marcel_attr_setschedpolicy(marcel_attr_t *attr, int policy);
-int marcel_attr_getschedpolicy(__const marcel_attr_t *attr, int *policy);
+int marcel_attr_setschedpolicy(marcel_attr_t *attr, int policy) __THROW;
+int marcel_attr_getschedpolicy(__const marcel_attr_t *attr, int *policy) __THROW;
 
 int marcel_attr_setrealtime(marcel_attr_t *attr, boolean realtime);
 int marcel_attr_getrealtime(__const marcel_attr_t *attr, boolean *realtime);

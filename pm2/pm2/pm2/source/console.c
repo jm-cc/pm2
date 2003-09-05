d552ab9b2c8c0093c1f2b3e5b9403742bdc542b7
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:30:47 +0000

    rev 1.6 at 2001/02/23 15:45:50 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.415)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5393 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/console.c
+++ pm2/pm2/pm2/source/console.c
@@ -1,4 +1,19 @@
 
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include "pm2.h"
 
 #include <ctype.h>
@@ -27,7 +42,8 @@ static void snap(marcel_t p)
    old_mad_pack_str(MAD_IN_HEADER, buf);
    tamp=p->number;
    old_mad_pack_long(MAD_IN_HEADER, &tamp, 1);
-   old_mad_pack_int(MAD_IN_HEADER, &p->prio, 1);
+   temp=1;
+   old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
    temp=p->state;
    old_mad_pack_int(MAD_IN_HEADER, &temp, 1);
    temp=64*1024;

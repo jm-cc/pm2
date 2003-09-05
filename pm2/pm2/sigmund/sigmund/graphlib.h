e245120fab736457fa304182bcc309de770c5070
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 11:37:35 +0000

    rev 1.2 at 2001/05/21 09:12:50 GMT  *** empty log message ***
    (Logical change 1.544)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7432 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/sigmund/graphlib.h
+++ pm2/pm2/sigmund/sigmund/graphlib.h
@@ -6,6 +6,8 @@
 typedef struct thread_on_list_st {
   int thread;
   int disactivated;
+  mode type;
+  u_64 last_up;
   struct thread_on_list_st *next;
 } * thread_on_list;
 
@@ -19,8 +21,16 @@ extern void set_lwp_last_up(int lwp, u_64 last_up);
 
 extern u_64 get_lwp_last_up(int lwp);
 
-extern void set_thread_disactived(int thread, int deactived);
+extern void set_thread_disactived(int thread, int disactivated);
 
 extern int get_thread_disactivated(int thread);
+
+extern mode get_last_type_thread(int thread);
+
+extern void set_last_type_thread(int thread, mode type);
+
+extern void set_thread_last_up(int thread, u_64 last_up);
+
+extern u_64 get_thread_last_up(int thread);
 
 #endif

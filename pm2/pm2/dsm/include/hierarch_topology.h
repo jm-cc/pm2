dd22a06d8c20c528255e1e473f04434bc524a4b0
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:19:12 +0000

    rev 1.5 at 2002/10/22 18:18:00 GMT  still working on token locks, now in hierarchical configurations
    (Logical change 1.705)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8728 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/hierarch_topology.h
+++ pm2/pm2/dsm/include/hierarch_topology.h
@@ -1,6 +1,6 @@
 
 /*
- * CVS Id: $Id: hierarch_topology.h,v 1.4 2002/10/19 15:13:30 slacour Exp $
+ * CVS Id: $Id: hierarch_topology.h,v 1.5 2002/10/22 18:18:00 slacour Exp $
  */
 
 #include "dsm_const.h"   /* dsm_node_t */
@@ -19,19 +19,27 @@ topology_initialization (void);
 extern int
 topology_finalization (void);
 
-/* get the number of clusters; this function is not protected against
- * concurrency. */
+/* get the number of clusters; this function is NOT protected against
+ * concurrency and must be called after pm2_init(). */
 extern unsigned int
 topology_get_cluster_number (void);
 
 /* get the color of a node to determine to what cluster it belongs;
- * this function is not protected against concurrency. */
+ * this function is NOT protected against concurrency and must be
+ * called after pm2_init(). */
 extern unsigned int
 topology_get_cluster_color (const dsm_node_t);
 
 /* set the colors of the processes, depending on what cluster they
  * belong to: 2 processes with the same color belong to the same
- * cluster; this function is NOT protected against concurrency. */
+ * cluster; this function is NOT protected against concurrency and
+ * must be called after pm2_init(). */
 extern int
 topology_set_cluster_colors (const unsigned int * const);
+
+/* dump on stderr all the topology information (for debugging
+ * purpose...); this function is NOT protected against concurrency and
+ * must be called after pm2_init(). */
+extern int
+topology_dump_information (void);
 

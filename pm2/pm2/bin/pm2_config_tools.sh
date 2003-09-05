efa7e95cad69542bff4d2aa1a1a89ccab6282dfc
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 14:30:10 +0000

    rev 1.1 at 2001/11/26 13:27:26 GMT  added some commodity functions (pm2_config_tools) and moved the patched libpthread into the PM2 repository.
    (Logical change 1.626)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8208 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/bin/pm2_config_tools.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Example:
+# if defined_in -O6 PM2_MARCEL_CFLAGS ; then
+#    ...
+# fi
+included_in()
+{
+    eval _l=\"`echo '$'$2`\"
+    case " $_l " in
+	*\ $1\ *) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+not_included_in()
+{
+    if included_in "$@" ; then
+	return 1
+    else
+	return 0
+    fi
+}
+
+# Example:
+# if defined_in MARCEL_SMP PM2_MARCEL_CFLAGS ; then
+#    ...
+# fi
+defined_in() # option list
+{
+    if included_in -D$1 $2 ; then
+	return 0
+    else
+	return 1
+    fi
+}
+
+not_defined_in() # option list
+{
+    if defined_in "$@" ; then
+	return 1
+    else
+	return 0
+    fi
+}
+

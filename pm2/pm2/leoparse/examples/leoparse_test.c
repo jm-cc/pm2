7a7efa54499d1e3e888b72dec16f3b56177c853e
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:13:55 +0000

    rev 1.4 at 2001/02/23 15:43:32 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.414)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5241 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leoparse/examples/leoparse_test.c
+++ pm2/pm2/leoparse/examples/leoparse_test.c
@@ -1,53 +1,18 @@
+
 /*
-                      PM2 HIGH-PERF/ISOMALLOC
-           High Performance Parallel Multithreaded Machine
-                           version 3.0
-
-     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
-       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
-
-            Laboratoire de l'Informatique du Parallelisme
-                        UMR 5668 CNRS-INRIA
-                 Ecole Normale Superieure de Lyon
-
-                      External Contributors:
-                 Yves Denneulin (LMC - Grenoble),
-                 Benoit Planquelle (LIFL - Lille)
-
-                    1998 All Rights Reserved
-
-
-                             NOTICE
-
- Permission to use, copy, modify, and distribute this software and
- its documentation for any purpose and without fee is hereby granted
- provided that the above copyright notice appear in all copies and
- that both the copyright notice and this permission notice appear in
- supporting documentation.
-
- Neither the institutions (Ecole Normale Superieure de Lyon,
- Laboratoire de L'informatique du Parallelisme, Universite des
- Sciences et Technologies de Lille, Laboratoire d'Informatique
- Fondamentale de Lille), nor the Authors make any representations
- about the suitability of this software for any purpose. This
- software is provided ``as is'' without express or implied warranty.
-
-______________________________________________________________________________
-$Log: leoparse_test.c,v $
-Revision 1.3  2000/12/19 16:57:46  oaumage
-- finalisation de leoparse
-- exemples pour leoparse
-- modification des macros de logging
-- version typesafe de certaines macros
-- finalisation des tables de hachage
-- finalisation des listes de recherche
-
-Revision 1.2  2000/11/16 13:24:03  oaumage
-- mise a jour initialisation
-
-
-______________________________________________________________________________
-*/
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
 
 /*
  * leoparse_test.c

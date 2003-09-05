e3eb2daf06592f4f45992f4946031d486dc4b171
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 13:01:38 +0000

    rev 1.10 at 2001/06/29 07:08:38 GMT  - acquittement par messages en forwarding, amelioration du transfert des flottants
    (Logical change 1.581)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7828 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ntbx/source/ntbx_pack.c
+++ pm2/pm2/ntbx/source/ntbx_pack.c
@@ -22,6 +22,7 @@
 #include "ntbx.h"
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 /*
  * Integer
@@ -32,7 +33,7 @@ ntbx_pack_int(int                  data,
 	      p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr = pack_buffer->buffer;
-  int   i;
+  int   i   = 0;
   
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -40,8 +41,7 @@ ntbx_pack_int(int                  data,
       *(char*)ptr++ = 'i';
     }
 
-  sprintf(ptr, "%*d", 
-	  NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 1, data);
+  sprintf(ptr, "%*d-", NTBX_PACK_INT_LEN, data);
   LOG_OUT();
 }
 
@@ -50,8 +50,8 @@ ntbx_unpack_int(p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr     = pack_buffer->buffer;
   char *end_ptr = NULL;
-  int   data;
-  int   i;
+  int   data    = 0;
+  int   i       = 0;
  
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -61,7 +61,7 @@ ntbx_unpack_int(p_ntbx_pack_buffer_t pack_buffer)
     }
 
   data = (int)strtol(ptr, &end_ptr, 10);
-  if (*end_ptr != '\0')
+  if (*end_ptr != '-')
     FAILURE("synchronisation error");  
   LOG_OUT();
 
@@ -77,7 +77,7 @@ ntbx_pack_long(long                 data,
 	       p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr = pack_buffer->buffer;
-  int   i;
+  int   i   = 0;
   
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -85,8 +85,7 @@ ntbx_pack_long(long                 data,
       *(char*)ptr++ = 'l';
     }
 
-  sprintf(ptr, "%*ld",
-	  NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 1, data);
+  sprintf(ptr, "%*ld-", NTBX_PACK_LONG_LEN, data);
   LOG_OUT();
 }
 
@@ -95,8 +94,8 @@ ntbx_unpack_long(p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr     = pack_buffer->buffer;
   char *end_ptr = NULL;
-  long  data;
-  int   i;
+  long  data    = 0;
+  int   i       = 0;
  
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -106,7 +105,7 @@ ntbx_unpack_long(p_ntbx_pack_buffer_t pack_buffer)
     }
 
   data = strtol(ptr, &end_ptr, 10);
-  if (*end_ptr != '\0')
+  if (*end_ptr != '-')
     FAILURE("synchronisation error");  
   LOG_OUT();
 
@@ -122,7 +121,7 @@ ntbx_pack_unsigned_int(unsigned int         data,
 		       p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr = pack_buffer->buffer;
-  int   i;
+  int   i   = 0;
   
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -130,8 +129,7 @@ ntbx_pack_unsigned_int(unsigned int         data,
       *(char*)ptr++ = 'I';
     }
 
-  sprintf(ptr, "%*u",
-	  NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 1, data);
+  sprintf(ptr, "%*u-", NTBX_PACK_UINT_LEN, data);
   LOG_OUT();
 }
 
@@ -140,8 +138,8 @@ ntbx_unpack_unsigned_int(p_ntbx_pack_buffer_t pack_buffer)
 {
   void         *ptr     = pack_buffer->buffer;
   char         *end_ptr = NULL;
-  unsigned int  data;
-  int           i;
+  unsigned int  data    = 0;
+  int           i       = 0;
  
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -151,7 +149,7 @@ ntbx_unpack_unsigned_int(p_ntbx_pack_buffer_t pack_buffer)
     }
 
   data = (unsigned int)strtoul(ptr, &end_ptr, 10);
-  if (*end_ptr != '\0')
+  if (*end_ptr != '-')
     FAILURE("synchronisation error");
   LOG_OUT();
 
@@ -167,7 +165,7 @@ ntbx_pack_unsigned_long(unsigned long        data,
 			p_ntbx_pack_buffer_t pack_buffer)
 {
   void *ptr = pack_buffer->buffer;
-  int   i;
+  int   i   = 0;
   
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -175,18 +173,17 @@ ntbx_pack_unsigned_long(unsigned long        data,
       *(char*)ptr++ = 'L';
     }
 
-  sprintf(ptr, "%*ld",
-	  NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 1, data);
+  sprintf(ptr, "%*ld-", NTBX_PACK_ULONG_LEN, data);
   LOG_OUT();
 }
 
 unsigned long
 ntbx_unpack_unsigned_long(p_ntbx_pack_buffer_t pack_buffer)
 {
-  void          *ptr = pack_buffer->buffer;
+  void          *ptr     = pack_buffer->buffer;
   char          *end_ptr = NULL;
-  unsigned long  data;
-  int            i;
+  unsigned long  data    = 0;
+  int            i       = 0;
  
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -196,7 +193,7 @@ ntbx_unpack_unsigned_long(p_ntbx_pack_buffer_t pack_buffer)
     }
 
   data = strtoul(ptr, &end_ptr, 10);
-  if (*end_ptr != '\0')
+  if (*end_ptr != '-')
     FAILURE("synchronisation error");  
   LOG_OUT();
 
@@ -211,29 +208,36 @@ void
 ntbx_pack_double(double               data,
 		 p_ntbx_pack_buffer_t pack_buffer)
 {
-  void *ptr    = pack_buffer->buffer;
-  int   status = 0;
-  int   i      = 0;
+  void   *ptr      = pack_buffer->buffer;
+  int     i        = 0;
+  int     exponent = 0;
+  double  mantissa = 0.0;
   
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
     {
       *(char*)ptr++ = 'D';
     }
-  status =
-    snprintf(ptr, NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN, "%*.*g",
-	     NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 1,
-	     NTBX_PACK_BUFFER_LEN - NTBX_PACK_BUFFER_TAG_LEN - 3, data);
+
+  mantissa = frexp(data, &exponent);
+  sprintf(ptr, "%*d-", NTBX_PACK_INT_LEN, exponent);
+
+  ptr += NTBX_PACK_INT_LEN + 1;
+
+  sprintf(ptr, "%*.*f-", NTBX_PACK_MANTISSA_LEN,
+	  NTBX_PACK_MANTISSA_LEN - 3, mantissa);
   LOG_OUT();
 }
 
-int
+double
 ntbx_unpack_double(p_ntbx_pack_buffer_t pack_buffer)
 {
-  void   *ptr = pack_buffer->buffer;
-  char   *end_ptr = NULL;
-  double  data;
-  int     i;
+  void   *ptr      = pack_buffer->buffer;
+  char   *end_ptr  = NULL;
+  int     i        = 0;
+  int     exponent = 0;
+  double  mantissa = 0.0;
+  double  data     = 0.0;
  
   LOG_IN();
   for (i = 0; i < NTBX_PACK_BUFFER_TAG_LEN; i++)
@@ -242,9 +246,17 @@ ntbx_unpack_double(p_ntbx_pack_buffer_t pack_buffer)
 	FAILURE("synchronisation error");
     }
 
-  data = strtod(ptr, &end_ptr);
-  if (*end_ptr != '\0')
-    FAILURE("synchronisation error");  
+  exponent = (int)strtol(ptr, &end_ptr, 10);
+  if (*end_ptr != '-')
+    FAILURE("synchronisation error");
+
+  end_ptr++;
+  
+  mantissa = strtod(end_ptr, &end_ptr);
+  if (*end_ptr != '-')
+    FAILURE("synchronisation error");
+
+  data = ldexp(mantissa, exponent);
   LOG_OUT();
 
   return data;

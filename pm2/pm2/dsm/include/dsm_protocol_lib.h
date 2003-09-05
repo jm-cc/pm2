8f7f9e105b872cb481ff7222e852d30e478733fd
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:08:14 +0000

    rev 1.9 at 2002/10/11 20:36:29 GMT  moved the dsm_lock/unlock functions from outside to inside the following consistency functions: read fault server, write fault server, read server, write server, invalidate server.  That was done for the 4 predefined consistency protocols; replaced a few 'unsigned long' with 'dsm_page_index_t'
    (Logical change 1.695)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8680 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_protocol_lib.h
+++ pm2/pm2/dsm/include/dsm_protocol_lib.h
@@ -19,17 +19,18 @@
 
 #include "dsm_const.h" 
 
-void dsmlib_rf_ask_for_read_copy(unsigned long index);
+void dsmlib_rf_ask_for_read_copy(dsm_page_index_t index);
 
-void dsmlib_migrate_thread(unsigned long index);
+void dsmlib_migrate_thread(dsm_page_index_t index);
 
-void dsmlib_wf_ask_for_write_access(unsigned long index);
+void dsmlib_wf_ask_for_write_access(dsm_page_index_t index);
 
-void dsmlib_rs_send_read_copy(unsigned long index, dsm_node_t req_node, int tag);
+void dsmlib_rs_send_read_copy(dsm_page_index_t index, dsm_node_t req_node, int tag);
 
-void dsmlib_ws_send_page_for_write_access(unsigned long index, dsm_node_t req_node, int tag);
+void dsmlib_ws_send_page_for_write_access(dsm_page_index_t index, dsm_node_t req_node, int tag);
 
-void dsmlib_is_invalidate(unsigned long index, dsm_node_t req_node, dsm_node_t new_owner);
+void dsmlib_is_invalidate(dsm_page_index_t index, dsm_node_t req_node, dsm_node_t new_owner);
+void dsmlib_is_invalidate_internal(dsm_page_index_t index, dsm_node_t req_node, dsm_node_t new_owner);
 
 void dsmlib_rp_validate_page(void *addr, dsm_access_t access, dsm_node_t reply_node, int tag);
 

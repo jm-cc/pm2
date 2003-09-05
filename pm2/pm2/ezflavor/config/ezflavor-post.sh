9ebca06426bd324f5539ab305527d5ffe47e3a1d
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 17:55:28 +0000

    rev 1.1 at 2003/04/14 14:44:14 GMT  ezflavor devient un module (avec option GTK2 ;-) )
    (Logical change 1.734)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9116 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/ezflavor/config/ezflavor-post.sh
@@ -0,0 +1,14 @@
+
+if defined_in GTK2 PM2_EZFLAVOR_CFLAGS ; then
+    PM2_EZFLAVOR_CFLAGS="$PM2_EZFLAVOR_CFLAGS `pkg-config --cflags gtk+-2.0`"
+    PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS `pkg-config --libs --libs gtk+-2.0`"
+else
+    PM2_EZFLAVOR_CFLAGS="$PM2_EZFLAVOR_CFLAGS `gtk-config --cflags`"
+    PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS `gtk-config --libs`"
+fi
+
+if defined_in WIN98_SYS_WINNT_SYS_WIN2K_SYS PM2_SYS ; then
+    PM2_EZFLAVOR_CFLAGS="$PM2_EZFLAVOR_CFLAGS -fnative-struct"
+    PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS -e _mainCRTStartup"
+fi    
+

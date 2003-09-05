2e83c0ee968d23a228b078e1830ac3567c2273a6
ens-lyon.fr!cperez <ens-lyon.fr!cperez@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:34:15 +0000

    Delete: mad1/config/options/10mpi-lam.sh
    }(Logical change 1.203)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2459 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad1/config/options/10mpi-lam.sh
+++
@@ -1,6 +0,0 @@
-PM2_PROTOCOLS=mpi-lam
-LAM_DIR=${LAM_DIR:-${HOME}/lam}
-PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -I${LAM_DIR}/h"
-PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${LAM_DIR}/lib -lmpi -largs -ltrillium -ltstdio -lt"
-PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNET_ARCH="'\"'mpi-lam'\"'
-PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNETINTERF_INIT=mad_mpi_netinterf_init"

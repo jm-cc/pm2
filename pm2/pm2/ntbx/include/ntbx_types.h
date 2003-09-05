0dda01611e79864b66e515542fd2419f66c2142d
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.9 at 2001/06/11 12:48:51 GMT  - corrections de compatibilite MadII/MadIII
    2003/09/05 14:03:09+02:00 ens-lyon.fr!oaumage
    rev 1.8 at 2001/06/11 09:11:59 GMT
     - ntbx, import de MadIII
    
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7679 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ntbx/include/ntbx_types.h
+++ pm2/pm2/ntbx/include/ntbx_types.h
@@ -27,17 +27,26 @@
  * Simple types
  * ------------
  */
-typedef int ntbx_host_id_t,    *p_ntbx_host_id_t;
+typedef int ntbx_node_id_t,       *p_ntbx_node_id_t;
+typedef int ntbx_process_lrank_t, *p_ntbx_process_lrank_t;
+typedef int ntbx_process_grank_t, *p_ntbx_process_grank_t;
+
+/* Begin: MadII compatibility */
+typedef int ntbx_host_id_t, *p_ntbx_host_id_t;
+/* End: MadII compatibility */
 
 
 /*
  * Pointers
  * --------
  */
-typedef struct s_ntbx_host   *p_ntbx_host_t;
-typedef struct s_ntbx_client *p_ntbx_client_t;
-typedef struct s_ntbx_server *p_ntbx_server_t;
-
+typedef struct s_ntbx_client            *p_ntbx_client_t;
+typedef struct s_ntbx_server            *p_ntbx_server_t;
+typedef struct s_ntbx_process_info      *p_ntbx_process_info_t;
+typedef struct s_ntbx_process_container *p_ntbx_process_container_t;
+typedef struct s_ntbx_process           *p_ntbx_process_t;
+typedef struct s_ntbx_topology_element  *p_ntbx_topology_element_t;
+typedef struct s_ntbx_topology_table    *p_ntbx_topology_table_t;
 
 /*
  * Pack buffers
@@ -79,7 +88,7 @@ typedef enum
  * ---------------------
  */
 
-/*...State Constant.....................*/
+/*...State Constants ...................*/
 typedef enum
 {
   ntbx_client_state_uninitialized = 0,
@@ -108,7 +117,9 @@ typedef struct s_ntbx_client
 {
   ntbx_client_state_t  state;
   char                *local_host;
+  p_tbx_slist_t        local_alias;
   char                *remote_host;
+  p_tbx_slist_t        remote_alias;
   void                *specific;
 } ntbx_client_t;
 
@@ -116,8 +127,75 @@ typedef struct s_ntbx_server
 {
   ntbx_server_state_t     state;
   char                   *local_host;
+  p_tbx_slist_t           local_alias;  
   ntbx_connection_data_t  connection_data;
   void                   *specific;
 } ntbx_server_t;
+
+/*
+ * Configuration objects
+ * ---------------------
+ */
+
+// A process container element
+typedef struct s_ntbx_process_info
+{
+  ntbx_process_lrank_t local_rank;
+  p_ntbx_process_t     process;
+  void                *specific;
+} ntbx_process_info_t;
+
+// A process container
+typedef struct s_ntbx_process_container
+{
+  ntbx_process_lrank_t   local_array_size;
+  p_ntbx_process_info_t *local_index;
+  ntbx_process_grank_t   global_array_size;
+  p_ntbx_process_info_t *global_index;
+  int                    count;
+} ntbx_process_container_t;
+
+// A routing table
+/*...Topology Constants ...................*/
+typedef enum
+{
+  ntbx_topology_empty   = 0,
+  ntbx_topology_regular,
+  ntbx_topology_full,
+  ntbx_topology_star,
+  ntbx_topology_ring,
+  ntbx_topology_grid,
+  ntbx_topology_torus,
+  ntbx_topology_hypercube,
+  ntbx_topology_function,
+} ntbx_topology_t, *p_ntbx_topology_t;
+
+typedef struct s_ntbx_topology_element
+{
+  void *specific;
+} ntbx_topology_element_t;
+
+typedef struct s_ntbx_topology_table
+{
+  p_ntbx_topology_element_t *table;
+  ntbx_process_lrank_t       size;
+} ntbx_topology_table_t;
+
+// A processus
+typedef struct s_ntbx_process
+{
+  // Global internal id
+  ntbx_process_grank_t  global_rank;
+  
+  // Objects referencing that process
+  p_tbx_htable_t        ref;
+
+  // Pid
+  pid_t                 pid;
+
+  // Module-specific data
+  void                 *specific;
+  
+} ntbx_process_t;
 
 #endif /* NTBX_TYPES_H */

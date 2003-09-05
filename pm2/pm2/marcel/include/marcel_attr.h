3297732092c73b5416662d0aa62b3d4ac100f9e6
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 17:55:28 +0000

    rev 1.11 at 2003/04/14 15:05:23 GMT  ajout libpthread
    (Logical change 1.734)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8894 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_attr.h
+++ pm2/pm2/marcel/include/marcel_attr.h
@@ -19,7 +19,8 @@
 
 #include "sys/marcel_privatedefs.h"
 
-_PRIVATE_ typedef struct {
+/* in "bits/marcel_pthreadtypes.h now"
+_PRIVATE_ typedef struct __marcel_attr_s {
   unsigned stack_size;
   char *stack_base;
   boolean detached;
@@ -32,10 +33,12 @@ _PRIVATE_ typedef struct {
   marcel_vpmask_t vpmask;
   int flags;
 } marcel_attr_t;
+*/
 
 /* detachstate */
-#define MARCEL_CREATE_JOINABLE    FALSE
+/*#define MARCEL_CREATE_JOINABLE    FALSE
 #define MARCEL_CREATE_DETACHED    TRUE
+*/
 
 /* realtime */
 #define MARCEL_CLASS_REGULAR      FALSE
@@ -45,38 +48,38 @@ int marcel_attr_init(marcel_attr_t *attr);
 #define marcel_attr_destroy(attr_ptr)	0
 
 int marcel_attr_setstacksize(marcel_attr_t *attr, size_t stack);
-int marcel_attr_getstacksize(marcel_attr_t *attr, size_t *stack);
+int marcel_attr_getstacksize(__const marcel_attr_t *attr, size_t *stack);
 
 int marcel_attr_setstackaddr(marcel_attr_t *attr, void *addr);
-int marcel_attr_getstackaddr(marcel_attr_t *attr, void **addr);
+int marcel_attr_getstackaddr(__const marcel_attr_t *attr, void **addr);
 
 int marcel_attr_setdetachstate(marcel_attr_t *attr, boolean detached);
-int marcel_attr_getdetachstate(marcel_attr_t *attr, boolean *detached);
+int marcel_attr_getdetachstate(__const marcel_attr_t *attr, boolean *detached);
 
 int marcel_attr_setuserspace(marcel_attr_t *attr, unsigned space);
-int marcel_attr_getuserspace(marcel_attr_t *attr, unsigned *space);
+int marcel_attr_getuserspace(__const marcel_attr_t *attr, unsigned *space);
 
 int marcel_attr_setactivation(marcel_attr_t *attr, boolean immediate);
-int marcel_attr_getactivation(marcel_attr_t *attr, boolean *immediate);
+int marcel_attr_getactivation(__const marcel_attr_t *attr, boolean *immediate);
 
 int marcel_attr_setmigrationstate(marcel_attr_t *attr, boolean migratable);
-int marcel_attr_getmigrationstate(marcel_attr_t *attr, boolean *migratable);
+int marcel_attr_getmigrationstate(__const marcel_attr_t *attr, boolean *migratable);
 
 int marcel_attr_setdeviationstate(marcel_attr_t *attr, boolean deviatable);
-int marcel_attr_getdeviationstate(marcel_attr_t *attr, boolean *deviatable);
+int marcel_attr_getdeviationstate(__const marcel_attr_t *attr, boolean *deviatable);
 
 int marcel_attr_setschedpolicy(marcel_attr_t *attr, int policy);
-int marcel_attr_getschedpolicy(marcel_attr_t *attr, int *policy);
+int marcel_attr_getschedpolicy(__const marcel_attr_t *attr, int *policy);
 
 int marcel_attr_setrealtime(marcel_attr_t *attr, boolean realtime);
-int marcel_attr_getrealtime(marcel_attr_t *attr, boolean *realtime);
+int marcel_attr_getrealtime(__const marcel_attr_t *attr, boolean *realtime);
 
 int marcel_attr_setvpmask(marcel_attr_t *attr, marcel_vpmask_t mask);
-int marcel_attr_getvpmask(marcel_attr_t *attr, marcel_vpmask_t *mask);
+int marcel_attr_getvpmask(__const marcel_attr_t *attr, marcel_vpmask_t *mask);
 
 // only for internal use
 int marcel_attr_setflags(marcel_attr_t *attr, int flags);
-int marcel_attr_getflags(marcel_attr_t *attr, int *flags);
+int marcel_attr_getflags(__const marcel_attr_t *attr, int *flags);
 
 _PRIVATE_ extern marcel_attr_t marcel_attr_default;
 

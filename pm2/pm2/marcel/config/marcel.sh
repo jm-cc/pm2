a2cecbb31ec22a11d7185b013aa6b6f8950fed0a
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 08:29:34 +0000

    rev 1.8 at 2001/03/01 14:33:26 GMT  Moved back the 'omit-frame-pointer' option into marcel's kernel.
    (Logical change 1.433)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6089 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/config/marcel.sh
+++ pm2/pm2/marcel/config/marcel.sh
@@ -1,14 +1,11 @@
+PM2_MARCEL_LIBNAME=marcel
+PM2_MARCEL_CFLAGS_KERNEL="$PM2_MARCEL_CFLAGS_KERNEL -fomit-frame-pointer"
 
 case " $PM2_LIBS " in
     *\ mad?\ *) ;;
     *) PM2_DEFAULT_LOADER=${PM2_ROOT}/marcel/bin/marcelload_conf_not_needed ;;
 esac
 
-if [ "${PM2_ARCH}" = RS6K_ARCH ]; then
-  PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -mno-powerpc"
-fi
-PM2_MARCEL_LIBNAME=marcel
-PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -fomit-frame-pointer"
 if [ "$PM2_SYS" = SOLARIS_SYS ]; then
   PM2_MARCEL_LIBS="$PM2_MARCEL_LIBS -lrt"
 fi

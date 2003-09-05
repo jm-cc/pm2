5020572ec59536644c5c7ab44f8dc8760968b968
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 05:53:14 +0000

    rev 1.3 at 2001/01/30 17:37:32 GMT  Ouch: just removed the old PM2-copyright header from _all_ files.
    (Logical change 1.392)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4462 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/examples/userspace.c
+++ pm2/pm2/marcel/examples/userspace.c
@@ -1,37 +1,3 @@
-/*
-                      PM2 HIGH-PERF/ISOMALLOC
-           High Performance Parallel Multithreaded Machine
-                           version 3.0
-
-     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
-       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
-
-            Laboratoire de l'Informatique du Parallelisme
-                        UMR 5668 CNRS-INRIA
-                 Ecole Normale Superieure de Lyon
-
-                      External Contributors:
-                 Yves Denneulin (LMC - Grenoble),
-                 Benoit Planquelle (LIFL - Lille)
-
-                    1998 All Rights Reserved
-
-
-                             NOTICE
-
- Permission to use, copy, modify, and distribute this software and
- its documentation for any purpose and without fee is hereby granted
- provided that the above copyright notice appear in all copies and
- that both the copyright notice and this permission notice appear in
- supporting documentation.
-
- Neither the institutions (Ecole Normale Superieure de Lyon,
- Laboratoire de L'informatique du Parallelisme, Universite des
- Sciences et Technologies de Lille, Laboratoire d'Informatique
- Fondamentale de Lille), nor the Authors make any representations
- about the suitability of this software for any purpose. This
- software is provided ``as is'' without express or implied warranty.
-*/
 
 /* userspace.c */
 

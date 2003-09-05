d9681720dfe5229e280cc67625a850ea8fe0dc74
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:36:25 +0000

    rev 1.4 at 2001/04/09 20:04:39 GMT  Modified MA__WORK section and implemented a preliminary working 'deviate' functionality.
    (Logical change 1.496)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7056 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_kthread.h
+++ pm2/pm2/marcel/include/sys/marcel_kthread.h
@@ -63,6 +63,8 @@ marcel_kthread_t marcel_kthread_self(void);
 
 void marcel_kthread_sigmask(int how, sigset_t *newmask, sigset_t *oldmask);
 
+void marcel_kthread_kill(marcel_kthread_t pid, int sig);
+
 #endif // MA__SMP
 
 #endif

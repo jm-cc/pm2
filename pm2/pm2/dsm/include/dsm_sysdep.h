1a50f2ac3658841055d777acdf87303a4bf15898
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.5 at 2000/07/14 16:16:50 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3055 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_sysdep.h
+++ pm2/pm2/dsm/include/dsm_sysdep.h
@@ -48,5 +48,9 @@ void dsm_uninstall_pagefault_handler();
 
 void dsm_setup_secondary_SIGSEGV_handler(dsm_std_handler_t handler_func);
 
+typedef void (*dsm_safe_ss_t)(char*t);
+
+void dsm_safe_ss(dsm_safe_ss_t f);
+
 #endif
 

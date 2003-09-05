7379c0205392c7d9f6089dad175dce26e91abe95
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:52:48 +0000

    rev 1.7 at 2001/06/21 10:52:24 GMT  - modification de la gestion des listes est des blocs dans leoparse
    (Logical change 1.575)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7788 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leoparse/include/leoparse_types.h
+++ pm2/pm2/leoparse/include/leoparse_types.h
@@ -21,21 +21,6 @@
 #ifndef LEOPARSE_TYPES_H
 #define LEOPARSE_TYPES_H
 
-typedef enum e_leoparse_entry_type
-{
-  leoparse_e_undefined = 0,
-  leoparse_e_slist,
-  leoparse_e_object,
-} leoparse_entry_type_t;
-
-typedef struct s_leoparse_htable_entry
-{
-  char                  *id;
-  p_leoparse_object_t    object;
-  p_tbx_slist_t          slist;
-  leoparse_entry_type_t  type;
-} leoparse_htable_entry_t;
-
 typedef struct s_leoparse_range
 {
   int begin;
@@ -50,6 +35,7 @@ typedef enum e_leoparse_object_type
   leoparse_o_htable,
   leoparse_o_integer,
   leoparse_o_range,
+  leoparse_o_slist,
 } leoparse_object_type_t;
 
 typedef struct s_leoparse_object
@@ -61,6 +47,7 @@ typedef struct s_leoparse_object
   int                     val;
   p_leoparse_range_t      range;
   p_leoparse_modifier_t   modifier;
+  p_tbx_slist_t           slist;
 } leoparse_object_t;
 
 typedef enum e_leoparse_modifier_type

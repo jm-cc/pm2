5e815a5737e8deb083e6047171cca3538512a6d8
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:41:04 +0000

    rev 1.17 at 2002/10/28 20:55:59 GMT  *** empty log message ***
    2003/09/05 18:37:47+02:00 ens-lyon.fr!slacour
    rev 1.16 at 2002/10/28 17:44:56 GMT
     resolved cycles in include header files
    
    2003/09/05 18:37:01+02:00 ens-lyon.fr!slacour
    rev 1.15 at 2002/10/28 17:00:13 GMT
     dsm protocol has type 'dsm_proto_t'
    
    (Logical change 1.712)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8800 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_const.h
+++ pm2/pm2/dsm/include/dsm_const.h
@@ -17,13 +17,12 @@
 #ifndef DSM_CONST_IS_DEF
 #define DSM_CONST_IS_DEF
 
-/* the following is useful for "token_lock_id_t" */
-#include "token_lock.h"
+typedef int dsm_proto_t;
+
+typedef unsigned long int dsm_page_index_t;
 
 typedef enum
 { NO_ACCESS, READ_ACCESS, WRITE_ACCESS, UNKNOWN_ACCESS } dsm_access_t;
-
-typedef unsigned long int dsm_page_index_t;
 
 typedef int dsm_node_t;
 
@@ -43,29 +42,6 @@ enum
    DSM_ERR_NOT_INIT,    /* utilization of an object not initialized */
    DSM_ERR_MISC,        /* miscellaneous errors */
 };
-
-
-typedef void (*dsm_rf_action_t)(dsm_page_index_t index); // read fault handler
-
-typedef void (*dsm_wf_action_t)(dsm_page_index_t index); // write fault handler
-
-typedef void (*dsm_rs_action_t)(dsm_page_index_t index, dsm_node_t req_node, int tag); // read server
-
-typedef void (*dsm_ws_action_t)(dsm_page_index_t index, dsm_node_t req_node, int tag); // write server
-
-typedef void (*dsm_is_action_t)(dsm_page_index_t index, dsm_node_t req_node, dsm_node_t new_owner); // invalidate server
-
-typedef void (*dsm_rp_action_t)(void *addr, dsm_access_t access, dsm_node_t reply_node, int tag); // receive page server
-
-typedef void (*dsm_erp_action_t)(void *addr, dsm_access_t access, dsm_node_t reply_node, unsigned long page_size, int tag); // expert receive page server
-
-typedef void (*dsm_acq_action_t)(const token_lock_id_t); // acquire func
-
-typedef void (*dsm_rel_action_t)(const token_lock_id_t); // release func
-
-typedef void (*dsm_pi_action_t)(int prot_id); // protocol init func
-
-typedef void (*dsm_pa_action_t)(dsm_page_index_t index); // page add func
 
 #endif
 

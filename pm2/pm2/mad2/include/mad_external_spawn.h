b198441239d88d29c161c7f8c671e0b1ffed0270
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 09:42:57 +0000

    rev 1.8 at 2001/03/23 10:13:17 GMT  - corrections mineures
    (Logical change 1.461)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6723 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/include/mad_external_spawn.h
+++ pm2/pm2/mad2/include/mad_external_spawn.h
@@ -47,6 +47,9 @@ mad_spawn_driver_init(p_mad_madeleine_t   madeleine,
 		      char              **argv);
 
 void
+mad_exchange_connection_info(p_mad_madeleine_t madeleine);
+
+void
 mad_exchange_configuration_info(p_mad_madeleine_t madeleine);
 
 #endif /* MAD_EXTERNAL_SPAWN_H */

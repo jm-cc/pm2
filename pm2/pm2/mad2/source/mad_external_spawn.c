2cb29b84d28843636d58b518c43ae411e9e22e31
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:34:29 +0000

    rev 1.12 at 2000/11/16 14:21:51 GMT  - correction external spawn
    2003/09/05 06:33:39+02:00 ens-lyon.fr!oaumage
    rev 1.11 at 2000/11/16 14:09:13 GMT
     - corrections diverses
    
    2003/09/05 06:33:26+02:00 ens-lyon.fr!oaumage
    rev 1.10 at 2000/11/16 14:01:50 GMT
     - correction external spawn
    
    2003/09/05 06:33:14+02:00 ens-lyon.fr!oaumage
    rev 1.9 at 2000/11/16 13:56:41 GMT
     - correction external spawn
    
    2003/09/05 06:32:25+02:00 ens-lyon.fr!oaumage
    rev 1.8 at 2000/11/16 13:24:06 GMT
     - mise a jour initialisation
    
    (Logical change 1.360)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3936 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/source/mad_external_spawn.c
+++ pm2/pm2/mad2/source/mad_external_spawn.c
@@ -34,6 +34,21 @@
 
 ______________________________________________________________________________
 $Log: mad_external_spawn.c,v $
+Revision 1.12  2000/11/16 14:21:51  oaumage
+- correction external spawn
+
+Revision 1.11  2000/11/16 14:09:13  oaumage
+- corrections diverses
+
+Revision 1.10  2000/11/16 14:01:50  oaumage
+- correction external spawn
+
+Revision 1.9  2000/11/16 13:56:41  oaumage
+- correction external spawn
+
+Revision 1.8  2000/11/16 13:24:06  oaumage
+- mise a jour initialisation
+
 Revision 1.7  2000/07/12 07:55:14  oaumage
 - Correction de la logique de localisation du fichier de configuration
 
@@ -92,12 +107,13 @@ ______________________________________________________________________________
  * Objet Madeleine
  * ---------------
  */
-static mad_madeleine_t main_madeleine;
+/* static mad_madeleine_t main_madeleine; */
 
 /*
  * Initialisation des drivers
  * --------------------------
  */
+/*
 static void 
 mad_driver_init(p_mad_madeleine_t madeleine)
 {
@@ -167,14 +183,16 @@ mad_connect_hosts(p_mad_madeleine_t   madeleine)
     }
   LOG_OUT();
 }
+*/
 
+/*
 #ifdef PM2
 p_mad_madeleine_t
 mad2_init(int                  *argc,
 	  char                **argv,
 	  char                 *configuration_file TBX_UNUSED,
 	  p_mad_adapter_set_t   adapter_set)
-#else /* PM2 */
+#else
 p_mad_madeleine_t
 mad_init(
 	 int                   *argc,
@@ -182,7 +200,7 @@ mad_init(
 	 char                  *configuration_file TBX_UNUSED,
 	 p_mad_adapter_set_t    adapter_set
 	 )
-#endif /* PM2 */
+#endif
 {
   p_mad_madeleine_t          madeleine       = &(main_madeleine);
   p_mad_configuration_t      configuration   =
@@ -195,7 +213,7 @@ mad_init(
   p_mad_adapter_t            spawn_adapter   = NULL;
 
   mad_managers_init(argc, argv);
-  LOG_IN();  /* After pm2debug_init ... */
+  LOG_IN();  
   
   madeleine->nb_channel = 0;
   TBX_INIT_SHARED(madeleine);
@@ -238,7 +256,6 @@ mad_init(
       slave  = tbx_true;
     }
 
-  /* output redirection */
   if (rank > 0)
     {
       char   output[MAX_ARG_LEN];
@@ -254,6 +271,207 @@ mad_init(
   tbx_list_init(&(madeleine->channel));
 
   LOG_OUT();
+  return madeleine;
+}
+*/
+
+void
+mad_spawn_driver_init(p_mad_madeleine_t   madeleine,
+		      int                *argc,
+		      char              **argv)
+{
+  p_mad_configuration_t    configuration   = madeleine->configuration;
+  p_mad_driver_t           spawn_driver    = NULL;
+  p_mad_driver_interface_t spawn_interface = NULL;
+  p_mad_adapter_t          spawn_adapter   = NULL;
+
+  LOG_IN();
+  spawn_driver           = &(madeleine->driver[EXTERNAL_SPAWN]);
+  spawn_interface        = &(madeleine->driver[EXTERNAL_SPAWN].interface);
+  spawn_interface->driver_init(spawn_driver);
+  
+  spawn_adapter = &(madeleine->adapter[EXTERNAL_SPAWN]);
+  
+  if (spawn_interface->adapter_init)
+    spawn_interface->adapter_init(spawn_adapter);
+
+  if (spawn_interface->external_spawn_init)
+    spawn_interface->external_spawn_init(spawn_adapter, argc, argv);
+
+  spawn_interface->configuration_init(spawn_adapter, configuration);
+  
+  if (spawn_interface->adapter_configuration_init)
+    spawn_interface->adapter_configuration_init(spawn_adapter);
+  LOG_OUT();
+}
+
+p_mad_madeleine_t
+mad_init(int                  *argc,
+	 char                **argv,
+	 char                 *configuration_file,
+	 p_mad_adapter_set_t   adapter_set)
+{
+  p_mad_madeleine_t madeleine = NULL;
+  
+  LOG_IN();
+
+  /*
+   * Toolbox services
+   * ----------------
+   *
+   * Provides:
+   * - fast malloc
+   * - safe malloc
+   * - timing
+   * - list objects
+   * - slist objects
+   * - htable objects
+   *
+   * Requires:
+   * - marcel mutexes
+   */
+  tbx_init(*argc, argv);
+
+  /*
+   * Net-Toolbox services
+   * --------------------
+   *
+   * Provides:
+   * - pack/unpack routines for hetereogenous architecture communication
+   * - safe non-blocking client-server framework over TCP
+   *
+   * Requires:
+   * - TBX services
+   */
+  ntbx_init(*argc, argv);
+  
+  /*
+   * Mad2 memory manager 
+   * -------------------
+   *
+   * Provides:
+   * - memory management for MadII internal buffer structure
+   *
+   * Requires:
+   * - TBX services
+   */
+  mad_memory_manager_init(*argc, argv);
+
+  /*
+   * Mad2 core initialization 
+   * -------------------
+   *
+   * Provides:
+   * - Mad2 core objects initialization
+   * - the `madeleine' object
+   *
+   * Requires:
+   * - NTBX services
+   * - Mad2 memory manager services
+   *
+   * Problemes:
+   * - argument "configuration file"
+   * - argument "adapter_set"
+   */
+  madeleine = mad_object_init(*argc, argv, configuration_file, adapter_set);
+
+  /*
+   * Mad2 spawn driver initialization
+   * --------------------------------
+   *
+   * Provides:
+   * - Mad2 initialization from driver info
+   *
+   * Should provide:
+   * - node rank
+   * - pm2_self
+   * - pm2_conf_size
+   *
+   * Requires:
+   * - the `madeleine' object
+   */
+  mad_spawn_driver_init(madeleine, argc, argv);
+
+  /*
+   * Mad2 command line parsing
+   * -------------------------
+   *
+   * Provides:
+   * - Mad2 initialization from command line arguments
+   *
+   * May provide:
+   * - node rank
+   * - pm2_self
+   *
+   * Requires:
+   * - the `madeleine' object
+   */
+  mad_cmd_line_init(madeleine, *argc, argv);
+
+  /*
+   * Mad2 output redirection
+   * -------------------------
+   *
+   * Provides:
+   * - Output redirection to log files 
+   *
+   * Requires:
+   * - the `madeleine' object
+   * - the node rank
+   * - high priority
+   */
+  mad_output_redirection_init(madeleine, *argc, argv);
+
+  /*
+   * Mad2 session configuration initialization
+   * -----------------------------------------
+   *
+   * Provides:
+   * - session configuration information
+   *
+   * May provide:
+   * - pm2_conf_size
+   *
+   * Requires:
+   * - the `madeleine' object
+   */
+  mad_configuration_init(madeleine, *argc, argv);
+
+  /*
+   * Mad2 network components initialization
+   * --------------------------------------
+   *
+   * Provides:
+   * - MadII network components ready to be connected
+   * - connection data
+   *
+   * Requires:
+   * - the `madeleine' object
+   * - session configuration information
+   */
+  mad_network_components_init(madeleine, *argc, argv);
+
+  /*
+   * Mad2 command line clean-up
+   * --------------------------
+   *
+   * Provides:
+   * - command line clean-up
+   *
+   * Requires:
+   * - Mad2 initialization from command line arguments
+   */
+  mad_purge_command_line(madeleine, argc, argv);
+
+  /*
+   * inutile
+   * mad_slave_spawn(madeleine, *argc, argv);
+   */
+
+  mad_connect(madeleine, *argc, argv);
+
+  LOG_OUT();
+
   return madeleine;
 }
 

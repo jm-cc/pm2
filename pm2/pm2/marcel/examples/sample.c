dff596b34edc4b7df41470691d62193e9048529f
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 09:36:37 +0000

    rev 1.12 at 2001/03/13 16:00:53 GMT  Merged the 'new-sched' branch
    (Logical change 1.449)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6677 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/examples/sample.c
+++ pm2/pm2/marcel/examples/sample.c
@@ -18,8 +18,6 @@
 
 #include "marcel.h"
 
-#define SCHED_POLICY   MARCEL_SCHED_BALANCE
-
 any_t ALL_IS_OK = (any_t)123456789L;
 
 #define NB    3
@@ -33,8 +31,7 @@ any_t writer(any_t arg)
   for(i=0;i<10;i++) {
     tfprintf(stderr, "Hi %s! (I'm %p on vp %d)\n",
 	     (char*)arg, marcel_self(), marcel_current_vp());
-    //tfprintf(stderr, "LWP(%d): Locked = %d\n", marcel_current_vp(), locked());
-    j = 19000000; while(j--);
+    j = 20000000; while(j--);
   }
 
   return ALL_IS_OK;
@@ -44,37 +41,22 @@ int marcel_main(int argc, char *argv[])
 {
   any_t status;
   marcel_t pid[NB];
-  marcel_attr_t attr;
   int i;
 
-   marcel_trace_on();
+  marcel_init(&argc, argv);
 
-   marcel_init(&argc, argv);
+  for(i=0; i<NB; i++)
+    marcel_create(&pid[i], NULL, writer, (any_t)mess[i]);
 
-   marcel_attr_init(&attr);
+  for(i=0; i<NB; i++) {
+    marcel_join(pid[i], &status);
+    if(status == ALL_IS_OK)
+      tprintf("Thread %p completed ok.\n", pid[i]);
+  }
 
-#ifdef PROFILE
-   profile_activate(FUT_ENABLE, MARCEL_PROF_MASK);
-#endif
+  marcel_end();
 
-   for(i=0; i<NB; i++) {
-     marcel_attr_setrealtime(&attr, i==1);
-     marcel_attr_setschedpolicy(&attr, SCHED_POLICY);
-     marcel_create(&pid[i], &attr, writer, (any_t)mess[i]);
-   }
-
-   for(i=0; i<NB; i++) {
-     marcel_join(pid[i], &status);
-     if(status == ALL_IS_OK)
-       tprintf("Thread %p completed ok.\n", pid[i]);
-   }
-
-#ifdef PROFILE
-   profile_stop();
-#endif
-
-   marcel_end();
-   return 0;
+  return 0;
 }
 
 

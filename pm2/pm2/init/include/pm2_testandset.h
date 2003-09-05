70034219e603872f65ce1543c5b468c098de1b00
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:51:26 +0000

    rev 1.2 at 2001/02/27 12:52:18 GMT  Merged the stable-mars branch into the trunk.
    (Logical change 1.424)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5655 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/init/include/pm2_testandset.h
@@ -0,0 +1,146 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef PM2_TESTANDSET_EST_DEF
+#define PM2_TESTANDSET_EST_DEF
+
+/* 
+ * Architecture-dependant definitions.
+ * Most are extracted from Linux kernel sources & LinuxThreads sources.
+ */
+
+#ifdef X86_ARCH
+static __inline__ int pm2_spinlock_testandset(volatile int *spinlock) __attribute__ ((unused));
+static __inline__ int pm2_spinlock_testandset(volatile int *spinlock)
+{
+  int ret;
+
+  __asm__ __volatile__(
+       "xchgl %0, %1"
+       : "=r"(ret), "=m"(*spinlock)
+       : "0"(1), "m"(*spinlock)
+       : "memory");
+
+  return ret;
+}
+
+#define pm2_spinlock_release(spinlock) (*(spinlock) = 0)
+#endif /* X86_ARCH */
+
+
+#ifdef SPARC_ARCH
+static __inline__ int pm2_spinlock_testandset(volatile int *spinlock)
+{
+  char ret = 0;
+
+  __asm__ __volatile__("ldstub [%0], %1"
+	: "=r"(spinlock), "=r"(ret)
+	: "0"(spinlock), "1" (ret) : "memory");
+
+  return (int)ret;
+}
+
+#define pm2_spinlock_release(spinlock) \
+  __asm__ __volatile__("stbar\n\tstb %1,%0" : "=m"(*(spinlock)) : "r"(0));
+#endif /* SPARC_ARCH */
+
+#ifdef ALPHA_ARCH
+static __inline__ long int pm2_spinlock_testandset(volatile int *spinlock)
+{
+  long int ret, temp;
+
+  __asm__ __volatile__(
+	"/* Inline spinlock test & set */\n"
+	"1:\t"
+	"ldl_l %0,%3\n\t"
+	"bne %0,2f\n\t"
+	"or $31,1,%1\n\t"
+	"stl_c %1,%2\n\t"
+	"beq %1,1b\n"
+	"2:\tmb\n"
+	"/* End spinlock test & set */"
+	: "=&r"(ret), "=&r"(temp), "=m"(*spinlock)
+	: "m"(*spinlock)
+        : "memory");
+
+  return ret;
+}
+
+#define pm2_spinlock_release(spinlock) \
+  __asm__ __volatile__("mb" : : : "memory"); \
+  *spinlock = 0
+#endif /* ALPHA_ARCH */
+
+#ifdef RS6K_ARCH
+#define pm2_spinlock_testandset(volatile int *spinlock) \
+  (*(spinlock) ? 1 : (*(spinlock)=1,0))
+
+#define pm2_spinlock_release(spinlock) (*(spinlock) = 0)
+#endif /* RS6K_ARCH */
+
+#ifdef MIPS_ARCH
+static __inline__ long int pm2_spinlock_testandset(volatile int *spinlock)
+{
+  long int ret, temp;
+
+  __asm__ __volatile__(
+        ".set\tmips2\n"
+        "1:\tll\t%0,0(%2)\n\t"
+        "bnez\t%0,2f\n\t"
+        ".set\tnoreorder\n\t"
+        "li\t%1,1\n\t"
+        "sc\t%1,0(%2)\n\t"
+        ".set\treorder\n\t"
+        "beqz\t%1,1b\n\t"
+        "2:\t.set\tmips0\n\t"
+        : "=&r"(ret), "=&r" (temp)
+        : "r"(spinlock)
+        : "memory");
+
+  return ret;
+}
+
+#define pm2_spinlock_release(spinlock) (*(spinlock) = 0)
+#endif /* MIPS_ARCH */
+
+#ifdef PPC_ARCH
+#define sync() __asm__ __volatile__ ("sync")
+
+static __inline__ int __compare_and_swap (long int *p, long int oldval, long int newval)
+{
+  int ret;
+
+  sync();
+  __asm__ __volatile__(
+		       "0:    lwarx %0,0,%1 ;"
+		       "      xor. %0,%3,%0;"
+		       "      bne 1f;"
+		       "      stwcx. %2,0,%1;"
+		       "      bne- 0b;"
+		       "1:    "
+	: "=&r"(ret)
+	: "r"(p), "r"(newval), "r"(oldval)
+	: "cr0", "memory");
+  sync();
+  return ret == 0;
+}
+
+#define pm2_spinlock_testandset(spinlock) __compare_and_swap(spinlock, 0, 1)
+
+#define pm2_spinlock_release(spinlock) (*(spinlock) = 0)
+#endif /* PPC_ARCH */
+
+#endif /* TESTANDSET_EST_DEF */

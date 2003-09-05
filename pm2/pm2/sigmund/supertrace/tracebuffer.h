368f5a2fd3929e011f802a4009ee7a435cc6821a
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:30:03 +0000

    rev 1.2 at 2001/04/04 08:57:38 GMT  *** empty log message ***
    (Logical change 1.489)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7012 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/supertrace/tracebuffer.h
+++ pm2/pm2/sigmund/supertrace/tracebuffer.h
@@ -42,7 +42,7 @@ typedef struct {
   trace_list last;
 } trace_buffer;
 
-void init_trace_buffer(char *futin, char *fktin);
+void init_trace_buffer(char *futin, char *fktin, int relative, int dec);
 
 int get_next_trace(trace *tr);
 

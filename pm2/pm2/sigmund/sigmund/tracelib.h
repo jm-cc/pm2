7225f70bc9c61f53dc5cf327f0aa9b18e4abc512
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:30:03 +0000

    rev 1.4 at 2001/04/04 14:31:58 GMT  *** empty log message ***
    (Logical change 1.489)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7013 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/sigmund/tracelib.h
+++ pm2/pm2/sigmund/sigmund/tracelib.h
@@ -3,6 +3,7 @@
 
 #include "filter.h"
 #include "tracebuffer.h"
+#include "fkt.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -11,7 +12,11 @@ extern filter options;
 
 extern char *fut_code2name(int code);
 
-extern int name2code(char *name, mode *type, int *a);
+extern int name2code(char *name, mode *type, int *code);
+
+extern int sys2code(char *name, int *code);
+
+extern int trap2code(char *name, int *code);
 
 extern char *fkt_code2name(int code);
 

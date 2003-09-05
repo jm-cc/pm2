33a2b8d3b227cda486e5db916444c75973a1fe37
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:57:58 +0000

    rev 1.4 at 2000/06/09 08:38:22 GMT  Examples are now user-friendly! ;-)
    (Logical change 1.229)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2647 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/examples/simple/hello.c
+++ pm2/pm2/pm2/examples/simple/hello.c
@@ -39,6 +39,13 @@ int pm2_main(int argc, char **argv)
 {
   pm2_init(&argc, argv);
 
+  if(pm2_config_size() < 2) {
+    fprintf(stderr,
+	    "This program requires at least two processes.\n"
+	    "Please rerun pm2conf.\n");
+    exit(1);
+  }
+
   printf("Hello world!\n");
 
   if(pm2_self() == pm2_config_size()-1)

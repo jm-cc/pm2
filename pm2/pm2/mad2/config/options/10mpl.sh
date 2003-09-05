dc2c80e6dd632dd4992e9a5a31ea9bdbfc461373
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:32:32 +0000

    rev 1.1 at 2000/05/29 10:11:54 GMT  - Mise-a-jour du support de la machine IBM/SP2
    (Logical change 1.201)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2441 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/mad2/config/options/10mpl.sh
@@ -0,0 +1,8 @@
+PM2_PROTOCOLS="$PM2_PROTOCOLS mpi"
+PM2_MAD2_CFLAGS="$PM2_MAD2_CFLAGS -DDRV_MPI=mad_MPI"
+PM2_MAD2_CFLAGS="$PM2_MAD2_CFLAGS -DEXTERNAL_SPAWN=DRV_MPI"
+PM2_MAD2_CFLAGS="$PM2_MAD2_CFLAGS -D_AIX -D_AIX32 -D_IBMR2 -D_POWER -I/usr/lpp/\ppe.poe/include"
+PM2_MAD2_LIBS="/usr/lpp/ppe.poe/lib/crt0.o $PM2_MAD2_LIBS -L/usr/lpp/\
+ppe.poe/lib -L/usr/lpp/ppe.poe/lib/ip -lmpi -lvtd -lc -L/opt/lib/gcc-lib/rs6000\-ibm-aix4.1.4.0/2.7.2.3 -lgcc -lc -lg -nostdlib"
+PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD2_MAD1_MAIN_PROTO=mad_MPI"
+PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD2_MAD1_MAIN_PROTO_PARAM=NULL"

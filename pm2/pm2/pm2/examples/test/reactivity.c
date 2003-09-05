3e66ebcab8983be80493ba0c46c8f83345dcaca5
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:03:26 +0000

    rev 1.2 at 2001/03/28 08:02:54 GMT  Fixed a bug in the pm2/mad2 termination protocol + introduced 'busy' threads in the reactivity example...
    (Logical change 1.468)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6850 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/examples/test/reactivity.c
+++ pm2/pm2/pm2/examples/test/reactivity.c
@@ -16,7 +16,37 @@
 
 #include "pm2.h"
 
-static unsigned SAMPLE;
+static unsigned SAMPLE, START_BUSY, STOP_BUSY;
+
+static volatile boolean stay_busy = TRUE;
+
+void busy_thread(void *arg)
+{
+  pm2_completion_t c;
+
+  pm2_unpack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
+  pm2_rawrpc_waitdata();
+
+  pm2_completion_signal(&c);
+
+  while(stay_busy) ;
+
+  fprintf(stderr, "oups! it's time to die!\n");
+}
+
+static void START_BUSY_service(void)
+{
+  pm2_thread_create(busy_thread, NULL);
+}
+
+static void STOP_BUSY_service(void)
+{
+  pm2_rawrpc_waitdata();
+
+  stay_busy = FALSE;
+
+  fprintf(stderr, "Busy threads should die now...\n");
+}
 
 void sample_thread(void *arg)
 {
@@ -25,25 +55,65 @@ void sample_thread(void *arg)
   pm2_unpack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
   pm2_rawrpc_waitdata();
 
-  pm2_printf("Hi! I'm a reactive service thread, believe me or not!\n");
-  pm2_printf("vpmask = %x, realtime = %d\n",
-	     marcel_self()->vpmask,
-	     MA_TASK_REAL_TIME(marcel_self()));
+  //pm2_printf("Hi! I'm a reactive service thread, believe me or not!\n");
 
   pm2_completion_signal(&c);
 }
 
 static void SAMPLE_service(void)
 {
-  pm2_printf("Daemon: vpmask = %x, realtime = %d\n",
-	     marcel_self()->vpmask,
-	     MA_TASK_REAL_TIME(marcel_self()));
   pm2_service_thread_create(sample_thread, NULL);
+}
+
+static void usage(char *prog)
+{
+  fprintf(stderr, "Usage: %s [ --busy <nb_threads> ]\n", prog);
+  pm2_halt();
+  pm2_exit();
+  exit(1);
+}
+
+static void spawn_busy_threads(int argc, char **argv)
+{
+  unsigned i, nb = 0;
+  pm2_completion_t c;
+
+  if(argc == 3) {
+    if(!strcmp(argv[1], "--busy")) {
+      nb = atoi(argv[2]);
+    } else {
+      usage(argv[0]);
+    }
+  } else if(argc > 1) {
+    usage(argv[0]);
+  }
+
+  pm2_completion_init(&c, NULL, NULL);
+
+  for(i=0; i<nb; i++) {
+    pm2_rawrpc_begin(1, START_BUSY, NULL);
+    pm2_pack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
+    pm2_rawrpc_end();
+  }
+
+  for(i=0; i<nb; i++) {
+    pm2_completion_wait(&c);
+  }
+
+  fprintf(stderr, "%d \"busy\" threads spawned on node 1...\n", nb);
+}
+
+static void stop_busy_threads(void)
+{
+  pm2_rawrpc_begin(1, STOP_BUSY, NULL);
+  pm2_rawrpc_end();
 }
 
 int pm2_main(int argc, char **argv)
 {
   pm2_rawrpc_register(&SAMPLE, SAMPLE_service);
+  pm2_rawrpc_register(&START_BUSY, START_BUSY_service);
+  pm2_rawrpc_register(&STOP_BUSY, STOP_BUSY_service);
 
   pm2_init(&argc, argv);
 
@@ -55,15 +125,30 @@ int pm2_main(int argc, char **argv)
   }
 
   if(pm2_self() == 0) {
+
     pm2_completion_t c;
+    tbx_tick_t t1, t2;
+    unsigned long temps;
+    int i;
 
-    pm2_completion_init(&c, NULL, NULL);
+    spawn_busy_threads(argc, argv);
 
-    pm2_rawrpc_begin(1, SAMPLE, NULL);
-    pm2_pack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
-    pm2_rawrpc_end();
+    for(i=0; i<10; i++) {
+      pm2_completion_init(&c, NULL, NULL);
 
-    pm2_completion_wait(&c);
+      TBX_GET_TICK(t1);
+      pm2_rawrpc_begin(1, SAMPLE, NULL);
+      pm2_pack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
+      pm2_rawrpc_end();
+
+      pm2_completion_wait(&c);
+      TBX_GET_TICK(t2);
+      temps = TBX_TIMING_DELAY(t1, t2);
+      printf("ping %d: round trip time = %ld.%03ldms\n",
+	     i, temps/1000, temps%1000);
+    }
+
+    stop_busy_threads();
 
     pm2_halt();
   }

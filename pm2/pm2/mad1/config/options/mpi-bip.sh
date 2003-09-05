e186cf6ff4fe6a6294450348f23c6c028bee734d
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:39:52 +0000

    rev 1.2 at 2000/04/27 19:22:04 GMT  update new makefile (minor bugs fixed)
    (Logical change 1.167)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1972 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad1/config/options/mpi-bip.sh
+++ pm2/pm2/mad1/config/options/mpi-bip.sh
@@ -1,1 +1,6 @@
+BIP_DIR=${BIP_DIR:-/usr/local/bip}
 PM2_PROTOCOLS=mpi-bip
+PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -I${BIP_DIR}/include -I${BIP_DIR}/include/mpi"
+PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${BIP_DIR}/lib -L${BIP_DIR}/lib/mpi_gnu -lmpi -lbip"
+PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNET_ARCH="'\"'mpi-bip'\"'
+PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNETINTERF_INIT=mad_mpi_netinterf_init"

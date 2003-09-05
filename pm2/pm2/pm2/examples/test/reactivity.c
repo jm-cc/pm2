fb202fcaee817e71b7fb5c6b31e007ecdd7f1b67
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:02:11 +0000

    rev 1.1 at 2001/03/27 16:39:13 GMT  Added the 'reactivity' test program.
    (Logical change 1.467)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6832 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/pm2/examples/test/reactivity.c
@@ -0,0 +1,75 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "pm2.h"
+
+static unsigned SAMPLE;
+
+void sample_thread(void *arg)
+{
+  pm2_completion_t c;
+
+  pm2_unpack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
+  pm2_rawrpc_waitdata();
+
+  pm2_printf("Hi! I'm a reactive service thread, believe me or not!\n");
+  pm2_printf("vpmask = %x, realtime = %d\n",
+	     marcel_self()->vpmask,
+	     MA_TASK_REAL_TIME(marcel_self()));
+
+  pm2_completion_signal(&c);
+}
+
+static void SAMPLE_service(void)
+{
+  pm2_printf("Daemon: vpmask = %x, realtime = %d\n",
+	     marcel_self()->vpmask,
+	     MA_TASK_REAL_TIME(marcel_self()));
+  pm2_service_thread_create(sample_thread, NULL);
+}
+
+int pm2_main(int argc, char **argv)
+{
+  pm2_rawrpc_register(&SAMPLE, SAMPLE_service);
+
+  pm2_init(&argc, argv);
+
+  if(pm2_config_size() < 2) {
+    fprintf(stderr,
+	    "This program requires at least two processes.\n"
+	    "Please rerun pm2conf.\n");
+    exit(1);
+  }
+
+  if(pm2_self() == 0) {
+    pm2_completion_t c;
+
+    pm2_completion_init(&c, NULL, NULL);
+
+    pm2_rawrpc_begin(1, SAMPLE, NULL);
+    pm2_pack_completion(SEND_CHEAPER, RECV_CHEAPER, &c);
+    pm2_rawrpc_end();
+
+    pm2_completion_wait(&c);
+
+    pm2_halt();
+  }
+
+  pm2_exit();
+
+  tfprintf(stderr, "Main is ending\n");
+  return 0;
+}

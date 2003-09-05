f19b1c1b1355df2b56b9bbd7feb301d0456195aa
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:08:14 +0000

    rev 1.1 at 2002/10/11 18:03:51 GMT  added a parameter to the consistency functions acquire and release (that's the lock identifier)
    (Logical change 1.695)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8664 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/dsm/source/hierarch_topology.c
@@ -0,0 +1,10 @@
+
+/*
+ * CVS Id: $Id: hierarch_topology.c,v 1.1 2002/10/11 18:03:51 slacour Exp $
+ */
+
+/* Description of the topology (clusters of nodes) using colors: two
+ * nodes in the same cluster have the same color */
+unsigned int cluster_colors[1] = { 0 };
+unsigned int cluster_number = 1;
+

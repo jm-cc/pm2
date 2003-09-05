4cba4d53c77b97da082ae5df53a8caa0ac792a65
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.1 at 2001/06/11 16:06:52 GMT  - synchro
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7608 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/leonie/source/leo_loaders.c
@@ -0,0 +1,313 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * leonie_loaders.c
+ * ================
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "leonie.h"
+
+/*
+ * Leonie loaders general modes
+ * ============================
+ */
+
+// #define LEO_DEBUG_MODE
+// #define LEO_MAD_DEBUG_MODE
+// #define LEO_TBX_DEBUG_MODE
+// #define LEO_TRACE_MODE
+// #define LEO_MARCEL_DEBUG_MODE
+#define LEO_XMODE
+
+
+/*
+ * Leonie loaders general modes information
+ * ========================================
+ */
+
+#ifdef LEO_DEBUG_MODE
+#warning [1;33m<<< [1;37mLeonie loaders debug mode:        [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders debug mode:        [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_DEBUG_MODE
+
+#ifdef LEO_MAD_DEBUG_MODE
+#warning [1;33m<<< [1;37mLeonie loaders MAD debug mode:    [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders MAD debug mode:    [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_MAD_DEBUG_MODE
+
+#ifdef LEO_TBX_DEBUG_MODE
+#warning [1;33m<<< [1;37mLeonie loaders TBX debug mode:    [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders TBX debug mode:    [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_TBX_DEBUG_MODE
+
+#ifdef LEO_MARCEL_DEBUG_MODE
+#warning [1;33m<<< [1;37mLeonie loaders Marcel debug mode: [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders Marcel debug mode: [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_MARCEL_DEBUG_MODE
+
+#ifdef LEO_TRACE_MODE
+#warning [1;33m<<< [1;37mLeonie loaders trace mode:        [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders trace mode:        [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_TRACE_MODE
+
+#ifdef LEO_XMODE
+#warning [1;33m<<< [1;37mLeonie loaders Xwindow mode:      [1;32mactivated [1;33m>>>[0m
+#else
+#warning [1;33m<<< [1;37mLeonie loaders Xwindow mode:      [1;31mnot activated [1;33m>>>[0m
+#endif // LEO_XMODE
+
+
+//
+// PM2 scripts
+// -----------
+///
+static const char *leo_rsh      =      NULL;
+static tbx_bool_t  auto_display = tbx_false;
+
+// ... Default loader .................................................. //
+static
+void
+leo_default_loader(char              *application_name,
+		   char              *application_flavor,
+		   p_ntbx_server_t    net_server,
+		   p_tbx_slist_t      process_slist)
+{
+  LOG_IN();
+  TRACE("default loader selected");
+
+  if (tbx_slist_is_nil(process_slist))
+    return;
+  
+  tbx_slist_ref_to_head(process_slist);
+      
+  do
+    {
+      p_ntbx_process_t      process              = NULL;
+      p_leo_dir_node_t      dir_node             = NULL;
+      char                 *host_name            = NULL;
+      p_tbx_string_t        main_command_string  = NULL;
+      p_tbx_string_t        relay_command_string = NULL;
+      p_tbx_string_t        env_command_string   = NULL;
+      p_tbx_argument_set_t  arg_set              = NULL;
+	  
+      process   = tbx_slist_ref_get(process_slist);
+      dir_node  = tbx_htable_get(process->ref, "node");
+      host_name = dir_node->name;
+
+      {
+	p_tbx_command_t   main_command = NULL;
+	p_tbx_arguments_t args         = NULL;
+	    
+	main_command = tbx_command_init_to_c_string(application_name);
+
+
+	args = main_command->arguments;	    
+
+	tbx_arguments_append_c_strings_option(args, "--mad_leonie", ' ',
+					      net_server->local_host);
+
+	tbx_arguments_append_c_strings_option(args, "--mad_link", ' ',
+					      net_server->
+					      connection_data.data);
+
+#ifdef LEO_MAD_DEBUG_MODE
+	tbx_arguments_append_c_strings_option(args, "--debug", ':',
+					      "mad3-log");
+#endif // LEO_MAD_DEBUG_MODE
+#ifdef LEO_TBX_DEBUG_MODE
+	tbx_arguments_append_c_strings_option(args, "--debug", ':',
+					      "tbx-log");
+	tbx_arguments_append_c_strings_option(args, "--debug", ':',
+					      "ntbx-log");
+#endif // LEO_TBX_DEBUG_MODE
+#ifdef LEO_TRACE_MODE
+	tbx_arguments_append_c_strings_option(args, "--debug", ':',
+					      "mad3-trace");
+#endif // LEO_TRACE_MODE
+	
+#ifdef LEO_MARCEL_DEBUG_MODE
+	tbx_arguments_append_c_strings_option(args, "--debug", ':',
+					      "marcel-log");
+#endif // LEO_MARCEL_DEBUG_MODE
+	
+	main_command_string = tbx_command_to_string(main_command);
+
+	tbx_command_free(main_command);
+	main_command = NULL;
+      }
+      
+      {
+	p_tbx_command_t              relay_command = NULL;
+	p_tbx_environment_t          env           = NULL;
+	p_tbx_arguments_t            args          = NULL;
+	p_tbx_environment_variable_t var           = NULL;
+
+	relay_command = tbx_command_init_to_c_string("leo-load");
+	env  = relay_command->environment;
+	args = relay_command->arguments;
+
+	var = tbx_environment_variable_to_variable("PATH");
+	tbx_environment_append_variable(env, var);
+	    
+	var = tbx_environment_variable_to_variable("PM2_ROOT");
+	tbx_environment_append_variable(env, var);
+	    
+	var = tbx_environment_variable_to_variable("LEO_XTERM");
+	tbx_environment_append_variable(env, var);
+	
+	if (!auto_display)
+	  {
+	    var = tbx_environment_variable_to_variable("DISPLAY");
+	    tbx_environment_append_variable(env, var);
+	  }
+
+#ifdef LEO_DEBUG_MODE
+	tbx_arguments_append_c_strings_option(args, "-d", 0, NULL);
+#endif // LEO_DEBUG_MODE
+#ifdef LEO_XMODE
+	tbx_arguments_append_c_strings_option(args, "-x", 0, NULL);
+#endif // LEO_XMODE
+
+	tbx_arguments_append_c_strings_option(args, "-f", ' ',
+					      application_flavor);
+
+	tbx_arguments_append_strings_option(args, main_command_string,
+					    0, NULL);
+	tbx_string_free(main_command_string);
+	main_command_string = NULL;
+
+	relay_command_string = tbx_command_to_string(relay_command);
+	tbx_command_free(relay_command);
+	relay_command = NULL;
+      }
+	  
+      {
+	p_tbx_command_t   env_command = NULL;
+	p_tbx_arguments_t args        = NULL;
+	    
+	env_command = tbx_command_init_to_c_string("env");
+	args = env_command->arguments;
+
+	tbx_arguments_append_strings_option(args, relay_command_string,
+					    0, NULL);
+	tbx_string_free(relay_command_string);
+	relay_command_string = NULL;
+
+	env_command_string = tbx_command_to_string(env_command);
+	tbx_command_free(env_command);
+	env_command = NULL;
+      }
+	  
+      {
+	p_tbx_command_t   rsh_command = NULL;
+	p_tbx_arguments_t args        = NULL;
+
+	LOG_STR("leo_rsh", leo_rsh);
+	rsh_command = tbx_command_init_to_c_string(leo_rsh);
+	args = rsh_command->arguments;
+
+	tbx_arguments_append_c_strings_option(args, host_name, 0, NULL);
+
+	tbx_arguments_append_strings_option(args, env_command_string,
+					    0, NULL);
+	tbx_string_free(env_command_string);
+	env_command_string = NULL;
+
+	arg_set = tbx_command_to_argument_set(rsh_command);
+	tbx_command_free(rsh_command);
+	rsh_command = NULL;
+      }
+
+      {
+	pid_t pid = -1;
+
+	pid = fork();
+
+	if (pid == -1)
+	  {
+	    leo_error("fork");
+	  }
+
+	if (!pid)
+	  {
+	    TRACE_STR("execvp command", arg_set->argv[0]);
+	    execvp(arg_set->argv[0], arg_set->argv);
+	    leo_error("execvp");
+	  }
+  
+	process->pid = pid;
+#if defined (LEO_DEBUG_MODE) || defined (LEO_XMODE)
+	// Necessary to avoid Xlib multiple connection overflow
+	sleep(2);
+#endif // LEO_DEBUG_MODE / LEO_XMODE
+      }
+    }
+  while (tbx_slist_ref_forward(process_slist));
+  LOG_OUT();
+}
+
+static
+void
+leo_default_loader_register(p_tbx_htable_t loaders)
+{
+  p_leo_loader_t loader = NULL;
+
+  LOG_IN();
+  loader = leo_loader_init();
+
+  loader->name        = strdup("default");
+  loader->loader_func = leo_default_loader;
+
+  tbx_htable_add(loaders, loader->name, loader);
+
+  if (!(leo_rsh = getenv("LEO_RSH")))
+    {
+      leo_rsh = "rsh";
+    }
+
+  if (strstr(leo_rsh, "ssh"))
+    {
+      auto_display = tbx_true;
+    }
+
+  LOG_OUT();
+}
+
+// ... Loaders registration ............................................ //
+p_tbx_htable_t
+leo_loaders_register(void)
+{
+  p_tbx_htable_t loaders = NULL;
+  
+  LOG_IN();
+  loaders = leo_htable_init();
+  
+  leo_default_loader_register(loaders);
+  LOG_OUT();
+  
+  return loaders;
+}
---
+++ pm2/pm2/leonie/source/leo_objects.c
@@ -0,0 +1,284 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * leo_objects.c
+ * =============
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "leonie.h"
+
+p_leo_process_specific_t
+leo_process_specific_init(void)
+{
+  p_leo_process_specific_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_process_specific_t));
+
+  object->client                   = ntbx_client_cons();
+  object->current_loader_priority  = leo_loader_priority_undefined;
+  LOG_OUT();
+
+  return object;
+}
+
+p_ntbx_server_t
+leo_net_server_init(void)
+{
+  p_ntbx_server_t object = NULL;
+
+  LOG_IN();
+  object = ntbx_server_cons();
+  ntbx_tcp_server_init(object);
+  LOG_OUT();
+  
+  return object;
+}
+
+p_tbx_htable_t
+leo_htable_init(void)
+{
+  p_tbx_htable_t object = NULL;
+  
+  LOG_IN();
+  object = calloc(1, sizeof(tbx_htable_t));
+  tbx_htable_init(object, 0);
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_directory_t
+leo_directory_init(void)
+{
+  p_leo_directory_t object = NULL;
+  
+  LOG_IN();
+  object = calloc(1, sizeof(leo_directory_t));
+
+  object->process_slist   = tbx_slist_nil();
+  object->node_htable     = leo_htable_init();
+  object->node_slist      = tbx_slist_nil();
+  object->driver_htable   = leo_htable_init();
+  object->driver_slist    = tbx_slist_nil();
+  object->channel_htable  = leo_htable_init();
+  object->channel_slist   = tbx_slist_nil();
+  object->fchannel_htable = leo_htable_init();
+  object->fchannel_slist  = tbx_slist_nil();
+  object->vchannel_htable = leo_htable_init();
+  object->vchannel_slist  = tbx_slist_nil();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_adapter_t
+leo_dir_adapter_init(void)
+{
+  p_leo_dir_adapter_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_adapter_t));
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_driver_process_specific_t
+leo_dir_driver_process_specific_init(void)
+{
+  p_leo_dir_driver_process_specific_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_driver_process_specific_t));
+
+  object->adapter_htable = leo_htable_init();
+  object->adapter_slist  = tbx_slist_nil();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_node_t
+leo_dir_node_init(void)
+{
+  p_leo_dir_node_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_node_t));
+
+  object->pc = ntbx_pc_cons();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_driver_t
+leo_dir_driver_init(void)
+{
+  p_leo_dir_driver_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_driver_t));
+
+  object->pc = ntbx_pc_cons();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_channel_process_specific_t
+leo_dir_channel_process_specific_init(void)
+{
+  p_leo_dir_channel_process_specific_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_channel_process_specific_t));
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_channel_t
+leo_dir_channel_init(void)
+{
+  p_leo_dir_channel_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_channel_t));
+
+  object->pc     = ntbx_pc_cons();
+  object->public = tbx_true;
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_vchannel_process_routing_table_t
+leo_dir_vchannel_process_routing_table_init(void)
+{
+  p_leo_dir_vchannel_process_routing_table_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_vchannel_process_routing_table_t));
+
+  object->destination_rank = -1;
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_vchannel_process_specific_t
+leo_dir_vchannel_process_specific_init(void)
+{
+  p_leo_dir_vchannel_process_specific_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_vchannel_process_specific_t));
+
+  object->pc = ntbx_pc_cons();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_fchannel_t
+leo_dir_fchannel_init(void)
+{
+  p_leo_dir_fchannel_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_fchannel_t));
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_dir_vchannel_t
+leo_dir_vchannel_init(void)
+{
+  p_leo_dir_vchannel_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_dir_vchannel_t));
+
+  object->dir_channel_slist  = tbx_slist_nil();
+  object->dir_fchannel_slist = tbx_slist_nil();
+  object->pc                 = ntbx_pc_cons();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_networks_t
+leo_networks_init(void)
+{
+  p_leo_networks_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_networks_t));
+
+  object->htable = leo_htable_init();
+  object->slist  = tbx_slist_nil();
+  LOG_OUT();
+
+  return object;
+}
+
+p_leo_spawn_group_t
+leo_spawn_group_init(void)
+{
+  p_leo_spawn_group_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_spawn_group_t));
+  object->process_slist = tbx_slist_nil();
+  LOG_OUT();
+  
+  return object;
+}
+  
+p_leo_spawn_groups_t
+leo_spawn_groups_init(void)
+{
+  p_leo_spawn_groups_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_spawn_groups_t));
+
+  object->htable = leo_htable_init();
+  object->slist  = tbx_slist_nil();
+  LOG_OUT();
+  
+  return object;
+}
+
+p_leo_loader_t
+leo_loader_init(void)
+{
+  p_leo_loader_t object = NULL;
+
+  LOG_IN();
+  object = calloc(1, sizeof(leo_loader_t));
+  LOG_OUT();
+  
+  return object;
+}

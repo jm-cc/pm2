3524f45964641d8c1b85bf85d1f2a219d70035ef
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 03:50:24 +0000

    rev 1.16 at 2000/10/10 13:31:11 GMT  Modified the startup function mechanism to allow functions to get an argument. Updated the pm2_sync functions: global (= node-level) barriers are ready !
    (Logical change 1.332)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3608 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_page_manager.c
+++ pm2/pm2/dsm/source/dsm_page_manager.c
@@ -364,6 +364,7 @@ static __inline__ void _dsm_page_ownership_init()
 	  low = (split * (chunk + 1)) + ((dsm_local_node_rank - split) * chunk);
 	  high = low + chunk;
 	}
+      fprintf(stderr, "low = %d, high = %d, split = %d, chunk = %d\n", low, high, split, chunk);
       if (_dsm_page_protect_mode == DSM_OWNER_WRITE_ACCESS_OTHER_NO_ACCESS || _dsm_page_protect_mode == DSM_OWNER_READ_ACCESS_OTHER_NO_ACCESS)     
 	mprotect(static_dsm_base_addr + (low * DSM_PAGE_SIZE),
 		 (high - low) * DSM_PAGE_SIZE, _dsm_get_prot(dsm_page_table[i]->access));

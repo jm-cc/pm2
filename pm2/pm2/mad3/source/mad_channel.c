0b663af9704999b86c31007f0087908d2115b7c9
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.1 at 2001/06/11 09:27:57 GMT  - import de MadIII
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7636 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/mad3/source/mad_channel.c
@@ -0,0 +1,49 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * Mad_channel.c
+ * =============
+ */
+/* #define DEBUG */
+/* #define TRACING */
+
+/*
+ * Headers
+ * -------
+ */
+#include "madeleine.h"
+
+/*
+ * Functions
+ * ---------
+ */
+
+p_mad_channel_t
+mad_get_channel(p_mad_madeleine_t  madeleine,
+		char              *name)
+{
+  p_mad_channel_t channel = NULL;
+
+  LOG_IN();
+  channel = tbx_htable_get(madeleine->channel_htable, name);
+  if (!channel)
+    FAILURE("channel not found");
+  LOG_OUT();
+  
+  return channel;
+}
+
+

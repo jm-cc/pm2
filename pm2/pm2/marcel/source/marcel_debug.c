6f504646321c5e27d708c017b2147e90184160d1
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:28:16 +0000

    rev 1.2 at 2000/05/25 00:23:53 GMT  marcel_poll with sisci and few bugs fixes
    (Logical change 1.197)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2399 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/source/marcel_debug.c
+++ pm2/pm2/marcel/source/marcel_debug.c
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: marcel_debug.c,v $
+Revision 1.2  2000/05/25 00:23:53  vdanjean
+marcel_poll with sisci and few bugs fixes
+
 Revision 1.1  2000/05/09 10:52:45  vdanjean
 pm2debug module
 
@@ -45,17 +48,32 @@ ______________________________________________________________________________
 #include "sys/marcel_debug.h"
 #define NULL ((void*)0)
 
+#ifdef PM2DEBUG
 #ifdef MARCEL_DEBUG
 debug_type_t marcel_mdebug=NEW_DEBUG_TYPE(0, "MAR: ", "mar-mdebug");
 debug_type_t marcel_trymdebug=NEW_DEBUG_TYPE(0, "MAR: ", "mar-tick");
-debug_type_t marcel_mdebug_state=NEW_DEBUG_TYPE(0, "MAR: ", "mar-state");
+debug_type_t marcel_debug_state=NEW_DEBUG_TYPE(0, "MAR: ", "mar-state");
+debug_type_t marcel_debug_work=NEW_DEBUG_TYPE(0, "MAR: ", "mar-work");
+debug_type_t marcel_debug_deviate=NEW_DEBUG_TYPE(0, "MAR: ", "mar-deviate");
+#else
+debug_type_t marcel_mdebug=NEW_DEBUG_TYPE(1, "MAR: ", "mar-mdebug");
+debug_type_t marcel_trymdebug=NEW_DEBUG_TYPE(1, "MAR: ", "mar-tick");
+debug_type_t marcel_debug_state=NEW_DEBUG_TYPE(1, "MAR: ", "mar-state");
+debug_type_t marcel_debug_work=NEW_DEBUG_TYPE(1, "MAR: ", "mar-work");
+debug_type_t marcel_debug_deviate=NEW_DEBUG_TYPE(1, "MAR: ", "mar-deviate");
 #endif
 
 #ifdef DEBUG_LOCK_TASK
 debug_type_t marcel_lock_task_debug=NEW_DEBUG_TYPE(0, "MAR: ", "mar-locktask");
+#else
+debug_type_t marcel_lock_task_debug=NEW_DEBUG_TYPE(1, "MAR: ", "mar-locktask");
 #endif
+
 #ifdef DEBUG_SCHED_LOCK
 debug_type_t marcel_sched_lock_debug=NEW_DEBUG_TYPE(0, "MAR: ", 
+						    "mar-schedlock");
+#else
+debug_type_t marcel_sched_lock_debug=NEW_DEBUG_TYPE(1, "MAR: ", 
 						    "mar-schedlock");
 #endif
 
@@ -63,33 +81,37 @@ debug_type_t marcel_sched_lock_debug=NEW_DEBUG_TYPE(0, "MAR: ",
 debug_type_t marcel_mtrace=NEW_DEBUG_TYPE(0, "MAR_TRACE: ", "mar-trace");
 debug_type_t marcel_mtrace_timer=NEW_DEBUG_TYPE(0, "MAR_TRACE: ", 
 						"mar-trace-timer");
+#else
+debug_type_t marcel_mtrace=NEW_DEBUG_TYPE(1, "MAR_TRACE: ", "mar-trace");
+debug_type_t marcel_mtrace_timer=NEW_DEBUG_TYPE(1, "MAR_TRACE: ", 
+						"mar-trace-timer");
+#endif
 #endif
 
-void marcel_debug_init(int* argc, char** argv)
+void marcel_debug_init(int* argc, char** argv, int debug_flags)
 {
 	static int called=0;
 	if (called) 
 		return;
 	called=1;
-#ifdef MARCEL_DEBUG
+#ifdef PM2DEBUG
 	pm2debug_register(&marcel_mdebug);
 	pm2debug_register(&marcel_trymdebug);
-	pm2debug_register(&marcel_mdebug_state);
+	pm2debug_register(&marcel_debug_state);
 	pm2debug_setup(&marcel_trymdebug, DEBUG_TRYONLY, 1);
-#endif
-#ifdef DEBUG_LOCK_TASK
+	pm2debug_register(&marcel_debug_work);
+	pm2debug_register(&marcel_debug_deviate);
+
 	pm2debug_register(&marcel_lock_task_debug);
 	pm2debug_setup(&marcel_lock_task_debug, DEBUG_SHOW_FILE, 1);
-#endif
-#ifdef DEBUG_SCHED_LOCK
+
 	pm2debug_register(&marcel_sched_lock_debug);
 	pm2debug_setup(&marcel_sched_lock_debug, DEBUG_SHOW_FILE, 1);
-#endif		
-#ifdef MARCEL_TRACE
+
 	pm2debug_register(&marcel_mtrace);
 	pm2debug_register(&marcel_mtrace_timer);
 #endif
-	pm2debug_init(argc, argv);
+	pm2debug_init_ext(argc, argv, debug_flags);
 }
 
 

afd26fcc573c007298c10b133c50eff235ddacf4
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:20:28 +0000

    rev 1.3 at 2000/11/10 14:17:52 GMT  - nouvelle procedure d'initialisation
    (Logical change 1.355)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3849 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/init/source/init.c
+++ pm2/pm2/init/source/init.c
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: init.c,v $
+Revision 1.3  2000/11/10 14:17:52  oaumage
+- nouvelle procedure d'initialisation
+
 Revision 1.2  2000/11/06 15:02:18  rnamyst
 pm2_init() has now a modular structure (in fact, common_init).
 
@@ -43,10 +46,29 @@ The 'common' module now (almost) behaves like a classical one : there is a libco
 ______________________________________________________________________________
 */
 
+#ifdef PM2
 #include "pm2.h"
+#endif /* PM2 */
+
+#ifdef MAD2
+#include "madeleine.h"
+#endif /* MAD2 */
+
+#ifdef TBX
+#include "tbx.h"
+#endif /* TBX */
+
+#ifdef NTBX
+#include "ntbx.h"
+#endif /* NTBX */
 
 void common_init(int *argc, char *argv[])
 {
+  /* Objet Madeleine (II) */
+#ifdef MAD2
+  p_mad_madeleine_t madeleine = NULL;
+#endif /* MAD2 */
+
 #ifdef PM2
   unsigned pm2_self, pm2_conf_size;
 #endif
@@ -63,18 +85,214 @@ void common_init(int *argc, char *argv[])
   //marcel_init(argc, argv);
 #endif
 
-#if defined(MAD1) || defined(MAD2)
+#ifdef TBX
+  /*
+   * Toolbox services
+   * ----------------
+   *
+   * Provides:
+   * - fast malloc
+   * - safe malloc
+   * - timing
+   * - list objects
+   * - slist objects
+   * - htable objects
+   *
+   * Requires:
+   * - marcel mutexes
+   */
+  tbx_init(*argc, argv);
+#endif /* TBX */
+
+#ifdef NTBX
+   /*
+   * Net-Toolbox services
+   * --------------------
+   *
+   * Provides:
+   * - pack/unpack routines for hetereogenous architecture communication
+   * - safe non-blocking client-server framework over TCP
+   *
+   * Requires:
+   * - TBX services
+   */
+  ntbx_init(*argc, argv);
+ 
+#endif /* NTBX */
+
+#ifdef MAD2
+  /*
+   * Mad2 memory manager 
+   * -------------------
+   *
+   * Provides:
+   * - memory management for MadII internal buffer structure
+   *
+   * Requires:
+   * - TBX services
+   */
+  mad_memory_manager_init(*argc, argv);
+#endif /* MAD2 */
+
+#ifdef MAD1
   mad_init_thread_related(argc, argv);
 #endif
 
-#if defined(MAD1) || defined(MAD2)
+#ifdef MAD2
+  /*
+   * Mad2 core initialization 
+   * -------------------
+   *
+   * Provides:
+   * - Mad2 core objects initialization
+   * - the `madeleine' object
+   *
+   * Requires:
+   * - NTBX services
+   * - Mad2 memory manager services
+   *
+   * Problemes:
+   * - argument "configuration file"
+   * - argument "adapter_set"
+   */
+  {
+    void *configuration_file = NULL;
+    void *adapter_set        = NULL;
+    
+    madeleine = mad_object_init(*argc, argv, configuration_file, adapter_set);
+  }
+  
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 command line parsing
+   * -------------------------
+   *
+   * Provides:
+   * - Mad2 initialization from command line arguments
+   * - node rank
+   * - pm2_self
+   *
+   * Requires:
+   * - the `madeleine' object
+   */
+  mad_cmd_line_init(madeleine, *argc, argv);
+
+#ifdef PM2
+  pm2_self = madeleine->configuration->local_host_id;
+#endif
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 output redirection
+   * -------------------------
+   *
+   * Provides:
+   * - Output redirection to log files 
+   *
+   * Requires:
+   * - the `madeleine' object
+   * - the node rank
+   * - high priority
+   */
+  mad_output_redirection_init(madeleine, *argc, argv);
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 session configuration initialization
+   * -----------------------------------------
+   *
+   * Provides:
+   * - session configuration information
+   * - pm2_conf_size
+   *
+   * Requires:
+   * - the `madeleine' object
+   */
+  mad_configuration_init(madeleine, *argc, argv);
+  
+#ifdef PM2
+  pm2_conf_size = mad->configuration->size;
+#endif
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 network components initialization
+   * --------------------------------------
+   *
+   * Provides:
+   * - MadII network components ready to be connected
+   * - connection data
+   *
+   * Requires:
+   * - the `madeleine' object
+   * - session configuration information
+   */
+  mad_network_components_init(madeleine, *argc, argv);
+#endif /* MAD2 */
+
+#ifdef MAD1
   mad_init(argc, argv);
 
 #ifdef PM2
   pm2_self = mad_who_am_i();
   pm2_conf_size = mad_config_size();
 #endif
+#endif /* MAD1 */
+
+#ifdef MAD2
+  /*
+   * Mad2 command line clean-up
+   * --------------------------
+   *
+   * Provides:
+   * - command line clean-up
+   *
+   * Requires:
+   * - Mad2 initialization from command line arguments
+   */
+  mad_purge_command_line(madeleine, argc, argv);
 #endif
+
+#ifdef PM2
+  pm2_init_purge_cmdline(argc, argv);
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 slave nodes spawn
+   * ----------------------
+   *
+   * Provides:
+   * - slave nodes
+   *
+   * Requires:
+   * - command line clean-up
+   * - MadII network components ready to be connected
+   * - connection data
+   */
+  mad_slave_spawn(madeleine, *argc, argv);
+#endif
+
+#ifdef MAD2
+  /*
+   * Mad2 network connection
+   * -----------------------
+   *
+   * Provides:
+   * - network connection
+   *
+   * Requires:
+   * - slave nodes
+   * - MadII network components ready to be connected
+   * - connection data
+   */
+  mad_connect(madeleine, *argc, argv);
+#endif /* MAD2 */
 
 #ifdef PM2
   pm2_init_open_channels(argc, argv, pm2_self, pm2_conf_size);
@@ -90,9 +308,5 @@ void common_init(int *argc, char *argv[])
 
 #ifdef PM2
   pm2_init_listen_network(argc, argv);
-#endif
-
-#ifdef PM2
-  pm2_init_purge_cmdline(argc, argv);
 #endif
 }

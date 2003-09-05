4a6891b69c0d11e2485d76b032f60f0b861eeb3f
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 11:50:09 +0000

    rev 1.3 at 2001/05/29 07:57:38 GMT  *** empty log message ***
    (Logical change 1.552)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7495 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/sigmund/graphlib.h
+++ pm2/pm2/sigmund/sigmund/graphlib.h
@@ -7,21 +7,22 @@ typedef struct thread_on_list_st {
   int thread;
   int disactivated;
   mode type;
-  u_64 last_up;
+  u_64 last_up; // ?
+  int dec;
   struct thread_on_list_st *next;
 } * thread_on_list;
 
-typedef struct lwp_on_list_st {
-  int lwp;
+typedef struct pid_on_list_st {
+  int pid;
   u_64 last_up;
-  struct lwp_on_list_st *next;
-} * lwp_on_list;
+  struct pid_on_list_st *next;
+} * pid_on_list;
 
-extern void set_lwp_last_up(int lwp, u_64 last_up);
+extern void set_pid_last_up(int pid, u_64 last_up);
 
-extern u_64 get_lwp_last_up(int lwp);
+extern u_64 get_pid_last_up(int pid);
 
-extern void set_thread_disactived(int thread, int disactivated);
+extern void set_thread_disactivated(int thread, int disactivated);
 
 extern int get_thread_disactivated(int thread);
 
@@ -32,5 +33,9 @@ extern void set_last_type_thread(int thread, mode type);
 extern void set_thread_last_up(int thread, u_64 last_up);
 
 extern u_64 get_thread_last_up(int thread);
+
+extern void add_thread_dec(int thread, int add);
+
+extern int get_thread_dec(int thread);
 
 #endif

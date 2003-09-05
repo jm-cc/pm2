e893e55897e792af7285917fd940408ff4929488
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 14:31:11 +0000

    rev 1.21 at 2001/11/27 13:09:37 GMT  Preliminary SMP version on Alpha systems
    (Logical change 1.627)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8217 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_lock.h
+++ pm2/pm2/marcel/include/marcel_lock.h
@@ -23,6 +23,7 @@
 #define MARCEL_SPIN_ITERATIONS 100
 
 #ifdef X86_ARCH
+/* begining of X86_ARCH section */
 
 #ifdef MA__LWPS
 #define LOCK_PREFIX "lock\n\t"
@@ -63,7 +64,56 @@ static __inline__ void atomic_dec(volatile atomic_t *v)
 		:"m" (0));
 }
 
-#else /* X86_ARCH */
+/* end of X86_ARCH section */
+#elif defined(ALPHA_ARCH)
+/* begining of ALPHA_ARCH section */
+
+#define __atomic_fool_gcc(x) (*(struct { int a[100]; } *)x)
+
+typedef int atomic_t;
+
+#define ATOMIC_INIT(i)	        (i)
+#define atomic_read(v)		(*(v))
+#define atomic_set(v, i)	(*(v) = (i))
+
+extern __inline__ void atomic_add(atomic_t i, volatile atomic_t * v)
+{
+        unsigned long temp;
+        __asm__ __volatile__(
+                "\n1:\t"
+                "ldl_l %0,%1\n\t"
+                "addl %0,%2,%0\n\t"
+                "stl_c %0,%1\n\t"
+                "beq %0,1b\n"
+                "2:"
+                :"=&r" (temp),
+                 "=m" (__atomic_fool_gcc(v))
+                :"Ir" (i),
+                 "m" (__atomic_fool_gcc(v)));
+}
+
+extern __inline__ void atomic_sub(atomic_t i, volatile atomic_t * v)
+{
+        unsigned long temp;
+        __asm__ __volatile__(
+                "\n1:\t"
+                "ldl_l %0,%1\n\t"
+                "subl %0,%2,%0\n\t"
+                "stl_c %0,%1\n\t"
+                "beq %0,1b\n"
+                "2:"
+                :"=&r" (temp),
+                 "=m" (__atomic_fool_gcc(v))
+                :"Ir" (i),
+                 "m" (__atomic_fool_gcc(v)));
+}
+
+#define atomic_inc(v) atomic_add(1,(v))
+#define atomic_dec(v) atomic_sub(1,(v))
+
+/* end of ALPHA_ARCH section */
+#else
+/* begining of generic (fallback) section */
 
 typedef unsigned atomic_t;
 
@@ -85,7 +135,8 @@ static __inline__ void atomic_dec(volatile atomic_t *v)
   (*v)--;
 }
 
-#endif /* X86_ARCH */
+/* end of begining (fallback) section */
+#endif
 
 /* 
  * Generic Part.

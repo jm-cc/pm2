b44b24d5545657aa09415a3b03b85de835bd73fb
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:36:44 +0000

    rev 1.9 at 2000/05/31 14:20:11 GMT  - Loader par defaut
    (Logical change 1.204)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2475 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/config/mad2.sh
+++ pm2/pm2/mad2/config/mad2.sh
@@ -2,4 +2,5 @@ PM2_MAD2_LIBNAME=mad
 if [ "${PM2_ARCH}" = RS6K_ARCH ]; then
   PM2_MAD2_CFLAGS="$PM2_MAD2_CFLAGS -mno-powerpc"
 fi
+PM2_DEFAULT_LOADER=$PM2_ROOT/mad2/bin/madload
 PM2_MAD2_MODULE_DEPEND_LIB="${PM2_MAD2_MODULE_DEPEND_LIB} tbx ntbx marcel"

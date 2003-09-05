2df5d2bb48ef9369033664223202ac7e3df4f19c
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.4 at 2000/07/14 16:16:48 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3069 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_mutex.h
+++ pm2/pm2/dsm/include/dsm_mutex.h
@@ -36,21 +36,44 @@
 #ifndef DSM_MUTEX_EST_DEF
 #define DSM_MUTEX_EST_DEF
 
+#include "marcel.h"
 #include "dsm_const.h"
 
-_PRIVATE_ typedef struct {
+typedef struct {
   dsm_node_t owner;
 } dsm_mutexattr_t;
 
-_PRIVATE_ typedef struct {
+typedef struct {
   marcel_mutex_t mutex;
   dsm_node_t owner;
 } dsm_mutex_t;
 
-int dsm_mutexattr_setowner(dsm_mutexattr_t *attr, dsm_node_t owner);
-int dsm_mutexattr_getowner(dsm_mutexattr_t *attr, dsm_node_t *owner);
 
-int dsm_mutex_init(dsm_mutex_t *mutex, dsm_mutexattr_t *attr);
+static __inline__ int dsm_mutexattr_setowner(dsm_mutexattr_t *attr, dsm_node_t owner) __attribute__ ((unused));
+static __inline__ int dsm_mutexattr_setowner(dsm_mutexattr_t *attr, dsm_node_t owner)
+{
+  attr->owner = owner;
+  return 0;
+}
+
+static __inline__ int dsm_mutexattr_getowner(dsm_mutexattr_t *attr, dsm_node_t *owner) __attribute__ ((unused));
+static __inline__ int dsm_mutexattr_getowner(dsm_mutexattr_t *attr, dsm_node_t *owner)
+{
+  *owner = attr->owner;
+  return 0;
+}
+
+static __inline__ int dsm_mutex_init(dsm_mutex_t *mutex, dsm_mutexattr_t *attr) __attribute__ ((unused));
+static __inline__ int dsm_mutex_init(dsm_mutex_t *mutex, dsm_mutexattr_t *attr)
+{
+  if(!attr)
+    mutex->owner = 0;
+  else
+    mutex->owner = attr->owner;
+  marcel_mutex_init(&mutex->mutex, NULL);
+  return 0;
+}
+
 int dsm_mutex_lock(dsm_mutex_t *mutex);
 int dsm_mutex_unlock(dsm_mutex_t *mutex);
 

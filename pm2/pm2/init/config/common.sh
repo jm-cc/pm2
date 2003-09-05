2d524f29ea5a93a36035e778a24330335ae9a67a
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:10:12 +0000

    rev 1.3 at 2000/11/05 20:48:58 GMT  The 'common' module now (almost) behaves like a classical one : there is a libcommon.a file, etc.
    (Logical change 1.349)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3760 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/init/config/common.sh
+++ pm2/pm2/init/config/common.sh
@@ -1,5 +1,8 @@
+PM2_COMMON_LIBNAME=common
 PM2_COMMON_CFLAGS="$PM2_COMMON_CFLAGS -Wall"
+PM2_COMMON_CFLAGS="$PM2_COMMON_CFLAGS -fomit-frame-pointer"
 
 if [ "${PM2_ARCH}" = RS6K_ARCH ]; then
   PM2_COMMON_CFLAGS="$PM2_COMMON_CFLAGS -mno-powerpc"
 fi
+

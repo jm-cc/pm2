9c402d67bdabf5ad24b64055b164521f8e36593e
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:27:27 +0000

    rev 1.12 at 2002/10/26 14:14:33 GMT  added type dsm_proto_t; minor modifs
    (Logical change 1.709)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8758 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_protocol_policy.h
+++ pm2/pm2/dsm/include/dsm_protocol_policy.h
@@ -14,84 +14,129 @@
  * General Public License for more details.
  */
 
-#ifndef DSM_PROT_POL_IS_DEF
-#define DSM_PROT_POL_IS_DEF
+#ifndef DSM_PROTOCOL_POLICY_H
+#define DSM_PROTOCOL_POLICY_H
 
 #include "dsm_const.h"
 
-extern int LI_HUDAK;
-extern int MIGRATE_THREAD;
-extern int ERC;
-extern int HBRC;
-extern int HIERARCH;
 
+
+/* Garbage... to get rid of */
 #define DEFAULT_DSM_PROTOCOL -1
 
-void dsm_init_protocol_table();
-
-int dsm_create_protocol( dsm_rf_action_t read_fault_handler,
-			 dsm_wf_action_t write_fault_handler,
-			 dsm_rs_action_t read_server,
-			 dsm_ws_action_t write_server,
-			 dsm_is_action_t invalidate_server,
-			 dsm_rp_action_t receive_page_server,
-			 dsm_erp_action_t expert_receive_page_server,
-			 dsm_acq_action_t acquire_func,
-			 dsm_rel_action_t release_func,
-			 dsm_pi_action_t prot_init_func,
-			 dsm_pa_action_t page_add_func);
-
-int dsm_registered_protocols();
-
-void dsm_set_read_fault_action(int key, dsm_rf_action_t action);
-
-dsm_rf_action_t dsm_get_read_fault_action(int key);
-
-void dsm_set_write_fault_action(int key, dsm_wf_action_t action);
-
-dsm_wf_action_t dsm_get_write_fault_action(int key);
-
-void dsm_set_read_server(int key, dsm_rs_action_t action);
-
-dsm_rs_action_t dsm_get_read_server(int key);
-
-void dsm_set_write_server(int key, dsm_ws_action_t action);
-
-dsm_ws_action_t dsm_get_write_server(int key);
-
-void dsm_set_invalidate_server(int key, dsm_is_action_t action);
-
-dsm_is_action_t dsm_get_invalidate_server(int key);
-
-void dsm_set_receive_page_server(int key, dsm_rp_action_t action);
-
-dsm_rp_action_t dsm_get_receive_page_server(int key);
-
-void dsm_set_expert_receive_page_server(int key, dsm_erp_action_t action);
-
-dsm_erp_action_t dsm_get_expert_receive_page_server(int key);
-
-dsm_acq_action_t dsm_get_acquire_func(int key);
-
-void dsm_set_acquire_func(int key, dsm_acq_action_t action);
-
-dsm_rel_action_t dsm_get_release_func(int key);
-
-void dsm_set_release_func(int key, dsm_rel_action_t action);
-
-void dsm_set_prot_init_func(int key, dsm_pi_action_t action);
-
-dsm_pi_action_t dsm_get_prot_init_func(int key);
-
-void dsm_set_page_add_func(int key, dsm_pa_action_t action);
-
-dsm_pa_action_t dsm_get_page_add_func(int key);
-
-#endif
 
 
+/**********************************************************************/
+/* PUBLIC TYPE DEFINITIONS                                            */
+/**********************************************************************/
+
+/* protocol identifier */
+typedef int dsm_proto_t;
 
 
+/**********************************************************************/
+/* GLOBAL VARIABLES                                                   */
+/**********************************************************************/
+
+/* pre-defined consistency protocols */
+extern dsm_proto_t LI_HUDAK;
+extern dsm_proto_t MIGRATE_THREAD;
+extern dsm_proto_t ERC;
+extern dsm_proto_t HBRC;
+extern dsm_proto_t HIERARCH;
 
 
+/* illegal protocol identifier */
+extern const dsm_proto_t NO_PROTOCOL;
+
+/* default protocol as set by dsm_set_default_protocol() */
+extern const dsm_proto_t DEFAULT_PROTOCOL;
+
+/* default protocol in case dsm_set_default_protocol() is not called */
+#define DEFAULT_DEFAULT_PROTOCOL LI_HUDACK
+
+
+/**********************************************************************/
+/* PUBLIC FUNCTIONS                                                   */
+/**********************************************************************/
+
+extern void
+dsm_init_protocol_table (void);
+
+extern dsm_proto_t
+dsm_create_protocol (dsm_rf_action_t, dsm_wf_action_t, dsm_rs_action_t,
+                     dsm_ws_action_t, dsm_is_action_t, dsm_rp_action_t,
+                     dsm_erp_action_t, dsm_acq_action_t, dsm_rel_action_t,
+                     dsm_pi_action_t, dsm_pa_action_t);
+
+extern int
+dsm_get_registered_protocols (void);
+
+extern void
+dsm_set_read_fault_action(dsm_proto_t, dsm_rf_action_t);
+
+extern dsm_rf_action_t
+dsm_get_read_fault_action(dsm_proto_t);
+
+extern void
+dsm_set_write_fault_action(dsm_proto_t, dsm_wf_action_t);
+
+extern dsm_wf_action_t
+dsm_get_write_fault_action(dsm_proto_t);
+
+extern void
+dsm_set_read_server(dsm_proto_t, dsm_rs_action_t);
+
+extern dsm_rs_action_t
+dsm_get_read_server(dsm_proto_t);
+
+extern void
+dsm_set_write_server(dsm_proto_t, dsm_ws_action_t);
+
+extern dsm_ws_action_t
+dsm_get_write_server(dsm_proto_t);
+
+extern void
+dsm_set_invalidate_server(dsm_proto_t, dsm_is_action_t);
+
+extern dsm_is_action_t
+dsm_get_invalidate_server(dsm_proto_t);
+
+extern void
+dsm_set_receive_page_server(dsm_proto_t, dsm_rp_action_t);
+
+extern dsm_rp_action_t
+dsm_get_receive_page_server(dsm_proto_t);
+
+extern void
+dsm_set_expert_receive_page_server(dsm_proto_t, dsm_erp_action_t);
+
+extern dsm_erp_action_t
+dsm_get_expert_receive_page_server(dsm_proto_t);
+
+extern void
+dsm_set_acquire_func(dsm_proto_t, dsm_acq_action_t);
+
+extern dsm_acq_action_t
+dsm_get_acquire_func(dsm_proto_t);
+
+extern void
+dsm_set_release_func(dsm_proto_t, dsm_rel_action_t);
+
+extern dsm_rel_action_t
+dsm_get_release_func(dsm_proto_t);
+
+extern void
+dsm_set_prot_init_func(dsm_proto_t, dsm_pi_action_t);
+
+extern dsm_pi_action_t
+dsm_get_prot_init_func(dsm_proto_t);
+
+extern void
+dsm_set_page_add_func(dsm_proto_t, dsm_pa_action_t);
+
+extern dsm_pa_action_t
+dsm_get_page_add_func(dsm_proto_t);
+
+#endif   /* DSM_PROTOCOL_POLICY_H */
 

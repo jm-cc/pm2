f8d2d179d08ad4895d097c2269e7cecf111d20ac
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 11:19:59 +0000

    rev 1.1 at 2001/05/14 18:31:13 GMT  Small modifs AFA Windows is concerned: marcel_for_win -> marcel_win_sys
    (Logical change 1.536)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7340 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/sys/marcel_win_sys.h
@@ -0,0 +1,46 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef MARCEL_WIN_SYS_IS_DEF
+#define MARCEL_WIN_SYS_IS_DEF
+
+void marcel_win_sys_init(int *argc, char *argv[]);
+
+#ifdef WIN_SYS
+
+#include <sys/time.h>
+
+#define	timerclear(tvp)		((tvp)->tv_sec = (tvp)->tv_usec = 0)
+#define	timercmp(a, b, CMP) 						      \
+  (((a)->tv_sec == (b)->tv_sec) ? 					      \
+   ((a)->tv_usec CMP (b)->tv_usec) : 					      \
+   ((a)->tv_sec CMP (b)->tv_sec))
+
+#include <sys/mman.h>
+
+#ifndef __WIN_MMAP_KERNEL__
+#define mmap(a, l, p, f, fd, o)    win_mmap(a, l, p, f, fd, o)
+#define munmap(a, l)               win_munmap(a, l)
+#endif
+
+extern caddr_t win_mmap(caddr_t __addr, size_t __len, int __prot, int __flags,
+		    int __fd, off_t __off);
+
+extern int win_munmap(caddr_t __addr, size_t __len);
+
+#endif
+
+#endif

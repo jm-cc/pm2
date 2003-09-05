72e9d094f1bbe4bb4c8cb96161fc72bb48f553bd
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:54:26 +0000

    rev 1.2 at 2000/06/07 11:24:49 GMT  - Mise a jour des scripts LAM-MPI
    (Logical change 1.222)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2618 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/bin/mad2mpi_debug.sh
+++ pm2/pm2/mad2/bin/mad2mpi_debug.sh
@@ -4,24 +4,7 @@
 DBG_SCRIPT = mad2_run_ddd.sh
 # DBG_SCRIPT = mad2_run_gdb.sh
 
-if [ ! -f ${MAD2_ROOT}/.mad2mpi_implementation ] ; then
-  echo "No MPI implementation has been selected"
-  exit 0
-fi
+echo "Calling mpirun..."
+mpirun N -x DISPLAY ${DBG_SCRIPT}$*
 
-IMPL=`cat ${MAD2_ROOT}/.mad2mpi_implementation`
-
-case "$IMPL" in
-  lam) 
-    echo "Calling mpirun..."
-    mpirun N -x DISPLAY ${DBG_SCRIPT}$*
-    ;;
-  bip) 
-    echo "unimplemented"
-    ;;
-  *)
-    echo "invalid MPI implementation name"
-    exit 0
-    ;;
-esac
 ##########

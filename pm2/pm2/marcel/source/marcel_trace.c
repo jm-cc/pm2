1b0ae7f06c642fb421ff85b47da932fbb83ed837
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:04:52 +0000

    rev 1.4 at 2000/04/11 09:07:38 GMT  Merged the "reorganisation" development branch.
    (Logical change 1.138)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1591 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/source/marcel_trace.c
+++ pm2/pm2/marcel/source/marcel_trace.c
@@ -34,6 +34,12 @@
 
 ______________________________________________________________________________
 $Log: marcel_trace.c,v $
+Revision 1.4  2000/04/11 09:07:38  rnamyst
+Merged the "reorganisation" development branch.
+
+Revision 1.3.2.1  2000/03/15 15:55:15  vdanjean
+réorganisation de marcel : commit pour CVS
+
 Revision 1.3  2000/02/28 10:25:12  rnamyst
 Changed #include <> into #include "".
 

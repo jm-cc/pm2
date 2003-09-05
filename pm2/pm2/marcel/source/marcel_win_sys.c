402d4a855d8f361b5d5118dc114fa8b02e321ca5
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 11:19:59 +0000

    rev 1.2 at 2001/05/14 18:48:02 GMT  Minor modifs
    2003/09/05 13:19:25+02:00 ens-lyon.fr!rnamyst
    rev 1.1 at 2001/05/14 18:31:14 GMT
     Small modifs AFA Windows is concerned: marcel_for_win -> marcel_win_sys
    
    (Logical change 1.536)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7339 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/source/marcel_win_sys.c
@@ -0,0 +1,62 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifdef WIN_SYS
+
+#define __WIN_MMAP_KERNEL__
+
+#include "sys/marcel_win_sys.h"
+#include "sys/isomalloc_archdep.h"
+
+void *ISOADDR_AREA_TOP;
+static void *ISOADDR_AREA_BOTTOM;
+
+caddr_t win_mmap(caddr_t __addr, size_t __len, int __prot, int __flags,
+		    int __fd, off_t __off)
+{
+  return __addr;
+}
+
+int win_munmap(caddr_t __addr, size_t __len)
+{
+  return 0;
+}
+
+#endif // WIN_SYS
+
+void marcel_win_sys_init(int *argc, char *argv[])
+{
+#ifdef WIN_SYS
+  ISOADDR_AREA_BOTTOM = mmap(NULL,
+	     ISOADDR_AREA_SIZE,
+	     PROT_READ | PROT_WRITE,
+	     MMAP_MASK,
+	     FILE_TO_MAP, 0);
+
+  if(ISOADDR_AREA_BOTTOM == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  ISOADDR_AREA_TOP = ISOADDR_AREA_BOTTOM + ISOADDR_AREA_SIZE;
+
+  // Ajustements pour aligner sur une frontière de SLOT_SIZE
+  ISOADDR_AREA_BOTTOM = (void *)(((unsigned long)ISOADDR_AREA_BOTTOM + SLOT_SIZE-1)
+				 & ~(SLOT_SIZE-1));
+  ISOADDR_AREA_TOP = (void *)((unsigned long)ISOADDR_AREA_TOP & ~(SLOT_SIZE-1));
+#endif
+}
+

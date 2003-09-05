d6f4728b797d2399abae5489f9b91e415a2884a5
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 03:40:16 +0000

    rev 1.3 at 2000/09/14 20:50:34 GMT  Still some changes to the FUT stuff
    2003/09/05 05:39:39+02:00 ens-lyon.fr!rnamyst
    rev 1.2 at 2000/09/14 02:08:32 GMT
     Put profile.h into common/include and added few FUT_SWITCH_TO calls
    
    (Logical change 1.319)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3531 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/profile/source/profile.c
+++ pm2/pm2/profile/source/profile.c
@@ -1,3 +1,47 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: profile.c,v $
+Revision 1.3  2000/09/14 20:50:34  rnamyst
+Still some changes to the FUT stuff
+
+Revision 1.2  2000/09/14 02:08:32  rnamyst
+Put profile.h into common/include and added few FUT_SWITCH_TO calls
+
+______________________________________________________________________________
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +55,18 @@
 
 #ifdef PM2
 #include "pm2.h"
+#endif
+
+#ifdef MARCEL
+
+#include "marcel.h"
+
+#define PROF_THREAD_ID()  (marcel_self()->number)
+
+#else // MARCEL
+
+#define PROF_THREAD_ID()  0
+
 #endif
 
 #define PROF_BUFFER_SIZE  (1024*1024)
@@ -28,7 +84,7 @@ void profile_init(void)
     strcpy(PROF_FILE, "/tmp/prof_file");
 #endif
 
-    if(fut_setup(PROF_BUFFER_SIZE, 0) < 0) {
+    if(fut_setup(PROF_BUFFER_SIZE, 0, PROF_THREAD_ID()) < 0) {
       perror("fut_setup");
       exit(EXIT_FAILURE);
     }
@@ -38,7 +94,7 @@ void profile_init(void)
 
 void profile_activate(int how, unsigned keymask)
 {
-  fut_keychange(how, keymask);
+  fut_keychange(how, keymask, PROF_THREAD_ID());
 }
 
 void profile_stop(void)

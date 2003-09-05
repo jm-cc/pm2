d74dd40a7d79ea196b52140ded1fc6bc34ef66c6
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.2 at 2000/07/14 16:17:07 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3088 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/pm2/include/pm2_sync.h
@@ -0,0 +1,57 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+*/
+
+#ifndef PM2_SYNC_EST_DEF
+#define PM2_SYNC_EST_DEF
+
+#include "marcel.h"
+
+typedef struct {
+  int *tab_sync;
+  marcel_mutex_t sync_mutex;
+} pm2_barrier_t;
+
+
+void pm2_barrier_init_rpc();
+
+void pm2_sync_init(int myself, int confsize);
+
+void pm2_barrier_init(pm2_barrier_t *bar, int size);
+
+void pm2_barrier(pm2_barrier_t *bar);
+
+#endif

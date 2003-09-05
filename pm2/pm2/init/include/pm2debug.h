0803dda3c55ca27f0fad3f23f7fc126a7d9b9728
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:45:21 +0000

    rev 1.5 at 2000/06/05 15:31:54 GMT  adaptation of debug messages
    (Logical change 1.213)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2539 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/init/include/pm2debug.h
+++ pm2/pm2/init/include/pm2debug.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: pm2debug.h,v $
+Revision 1.5  2000/06/05 15:31:54  vdanjean
+adaptation of debug messages
+
 Revision 1.4  2000/05/25 00:23:56  vdanjean
 marcel_poll with sisci and few bugs fixes
 
@@ -106,11 +109,13 @@ int pm2debug_printf(debug_type_t *type, int line, char* file,
 		     const char *format, ...);
 void pm2debug_register(debug_type_t *type);
 void pm2debug_setup(debug_type_t* type, debug_action_t action, int value);
-#else
+void pm2debug_flush();
+#else /* PM2DEBUG */
 #define pm2debug_init_ext(argc, argv, debug_flags)
 #define pm2debug_printf(type, line, file, format...)
 #define pm2debug_register(type)
 #define pm2debug_setup(type, action, value)
+#define pm2debug_flush()
 #endif
 
 #define pm2debug_init(argc, argv) \
@@ -123,7 +128,132 @@ void pm2debug_setup(debug_type_t* type, debug_action_t action, int value);
 #define debug(fmt, args...) \
    debug_printf(&debug_type_default, fmt, ##args)
 
+#endif /* PM2DEBUG */
+
+#ifdef PM2DEBUG
+#ifndef DEBUG_NAME
+#ifndef MODULE
+#define DEBUG_NAME app
+#else
+#define DEBUG_NAME MODULE
 #endif
+#endif
+
+#define CONCAT3(a,b,c) subconcat3(a,b,c)
+#define subconcat3(a,b,c) a##b##c
+#ifndef DEBUG_NAME_DISP
+#define DEBUG_NAME_DISP(DEBUG_NAME) CONCAT3(debug_, DEBUG_NAME, _disp)
+#endif
+#ifndef DEBUG_NAME_LOG
+#define DEBUG_NAME_LOG(DEBUG_NAME) CONCAT3(debug_, DEBUG_NAME, _log)
+#endif
+#ifndef DEBUG_NAME_TRACE
+#define DEBUG_NAME_TRACE(DEBUG_NAME) CONCAT3(debug_, DEBUG_NAME, _trace)
+#endif
+
+extern debug_type_t DEBUG_NAME_DISP(DEBUG_NAME);
+extern debug_type_t DEBUG_NAME_LOG(DEBUG_NAME);
+extern debug_type_t DEBUG_NAME_TRACE(DEBUG_NAME);
+
+#define DEBUG_DECLARE(DEBUG_NAME) \
+debug_type_t DEBUG_NAME_DISP(DEBUG_NAME)= \
+  NEW_DEBUG_TYPE(1, #DEBUG_NAME "-disp: ", \
+		    #DEBUG_NAME "-disp"); \
+debug_type_t DEBUG_NAME_LOG(DEBUG_NAME)= \
+  NEW_DEBUG_TYPE(0, #DEBUG_NAME "-log: ", \
+		    #DEBUG_NAME "-log"); \
+debug_type_t DEBUG_NAME_TRACE(DEBUG_NAME)= \
+  NEW_DEBUG_TYPE(0, #DEBUG_NAME "-trace: ", \
+		    #DEBUG_NAME "-trace"); 
+#define DEBUG_INIT(DEBUG_NAME) \
+  { pm2debug_register(&DEBUG_NAME_DISP(DEBUG_NAME)); \
+    pm2debug_register(&DEBUG_NAME_LOG(DEBUG_NAME)); \
+    pm2debug_register(&DEBUG_NAME_TRACE(DEBUG_NAME)); }
+
+#else /* PM2DEBUG */
+#define DEBUG_DECLARE(DEBUG_NAME)
+#endif /* PM2DEBUG */
+
+/*
+ * Display  macros  _________________________________________________
+ * ________________//////////////////////////////////////////////////
+ */
+#if defined(PM2DEBUG)
+#define DISP(str, args...)   debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   str "\n" , ## args)
+#define DISP_IN()            debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   __FUNCTION__": -->\n")
+#define DISP_OUT()           debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   __FUNCTION__": <--\n")
+#define DISP_VAL(str, val)   debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   str " = %d\n" , (int)(val))
+#define DISP_PTR(str, ptr)   debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   str " = %p\n" , (void *)(ptr))
+#define DISP_STR(str, str2)  debug_printf(&DEBUG_NAME_DISP(DEBUG_NAME), \
+					   str " : %s\n" , (char *)(str2))
+#else /* PM2DEBUG */
+#define DISP(str, args...)
+#define DISP_IN()
+#define DISP_OUT()
+#define DISP_VAL(str, val)
+#define DISP_PTR(str, ptr)
+#define DISP_STR(str, str2)
+#endif /* DEBUG */
+
+
+/*
+ * Logging macros  __________________________________________________
+ * _______________///////////////////////////////////////////////////
+ */
+#if defined(PM2DEBUG) && defined(DEBUG)
+#define LOG(str, args...)     debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+                                           str "\n" , ## args)
+#define LOG_IN()              debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+					   __FUNCTION__": -->\n")
+#define LOG_OUT()             debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+					   __FUNCTION__": <--\n")
+#define LOG_VAL(str, val)     debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+					   str " = %d\n" , (int)(val))
+#define LOG_PTR(str, ptr)     debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+					   str " = %p\n" , (void *)(ptr))
+#define LOG_STR(str, str2)    debug_printf(&DEBUG_NAME_LOG(DEBUG_NAME), \
+					   str " : %s\n" , (char *)(str2))
+#else /* PM2DEBUG */
+#define LOG(str, args...) 
+#define LOG_IN() 
+#define LOG_OUT() 
+#define LOG_VAL(str, val) 
+#define LOG_PTR(str, ptr) 
+#define LOG_STR(str, str2)
+#endif /* DEBUG */
+
+
+/*
+ * Tracing macros  __________________________________________________
+ * _______________///////////////////////////////////////////////////
+ */
+#if defined(PM2DEBUG) && defined(DEBUG)
+#define TRACE(str, args...)   debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   str "\n" , ## args)
+#define TRACE_IN()            debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   __FUNCTION__": -->\n")
+#define TRACE_OUT()           debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   __FUNCTION__": <--\n")
+#define TRACE_VAL(str, val)   debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   str " = %d\n" , (int)(val))
+#define TRACE_PTR(str, ptr)   debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   str " = %p\n" , (void *)(ptr))
+#define TRACE_STR(str, str2)  debug_printf(&DEBUG_NAME_TRACE(DEBUG_NAME), \
+					   str " : %s\n" , (char *)(str2))
+#else /* PM2DEBUG */
+#define TRACE(str, args...) 
+#define TRACE_IN()
+#define TRACE_OUT()
+#define TRACE_VAL(str, val) 
+#define TRACE_PTR(str, ptr) 
+#define TRACE_STR(str, str2) 
+#endif /* PM2DEBUG */
+
 
 
 

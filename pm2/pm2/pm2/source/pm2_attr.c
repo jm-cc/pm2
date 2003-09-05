409b280377e8131e238658c9ed92528f1f763b9c
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:30:47 +0000

    rev 1.7 at 2001/02/23 15:45:54 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.415)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5485 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/source/pm2_attr.c
+++ pm2/pm2/pm2/source/pm2_attr.c
@@ -1,8 +1,22 @@
 
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include "pm2_attr.h"
 
 pm2_attr_t pm2_attr_default = {
-  STD_PRIO,           /* priority */
   MARCEL_SCHED_OTHER, /* scheduling policy */
   0,                  /* channel number */
   NULL                /* completion ptr */
@@ -11,20 +25,6 @@ pm2_attr_t pm2_attr_default = {
 int pm2_attr_init(pm2_attr_t *attr)
 {
    *attr = pm2_attr_default;
-   return 0;
-}
-
-int pm2_attr_setprio(pm2_attr_t *attr, unsigned prio)
-{
-   if(prio == 0 || prio > MAX_PRIO)
-      RAISE(CONSTRAINT_ERROR);
-   attr->priority = prio;
-   return 0;
-}
-
-int pm2_attr_getprio(pm2_attr_t *attr, unsigned *prio)
-{
-   *prio = attr->priority;
    return 0;
 }
 

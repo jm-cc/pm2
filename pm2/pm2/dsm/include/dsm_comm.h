998eda8d7f7b496be098d77639e5dab2be515ace
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.6 at 2000/07/14 16:16:47 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3045 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_comm.h
+++ pm2/pm2/dsm/include/dsm_comm.h
@@ -40,13 +40,13 @@
 #include "dsm_page_manager.h"
 
 
-void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_access_t req_access);
+void dsm_send_page_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_access_t req_access, int tag);
 
 void dsm_invalidate_copyset(unsigned long index, dsm_node_t new_owner);
 
-void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_access_t access);
+void dsm_send_page(dsm_node_t dest_node, unsigned long index, dsm_access_t access, int tag);
 
-void dsm_send_page_with_user_data(dsm_node_t dest_node, unsigned long index, dsm_access_t access, void *user_addr, int user_length);
+void dsm_send_page_with_user_data(dsm_node_t dest_node, unsigned long index, dsm_access_t access, void *user_addr, int user_length, int tag);
 
 void dsm_send_invalidate_req(dsm_node_t dest_node, unsigned long index, dsm_node_t req_node, dsm_node_t new_owner);
 
@@ -55,6 +55,122 @@ void dsm_send_invalidate_ack(dsm_node_t dest_node, unsigned long index);
 /***********************  Hyperion stuff: ****************************/
 
 void dsm_send_diffs(unsigned long index, dsm_node_t dest_node);
+
+void dsm_send_diffs_start(unsigned long index, dsm_node_t dest_node,
+                          pm2_completion_t* c);
+void dsm_send_diffs_wait(pm2_completion_t* c);
+
+#define dsm_begin_send_multiple_diffs(dest_node)  pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_MULTIPLE_DIFFS, NULL)
+
+static __inline__ void dsm_pack_diffs(unsigned long index) __attribute__ ((unused));
+static __inline__ void dsm_pack_diffs(unsigned long index)
+{
+  void *addr;
+  int size;
+
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&index, sizeof(unsigned long)); 
+  while ((addr = dsm_get_next_modified_data(index, &size)) != NULL) 
+    { 
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *)); 
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&size, sizeof(int)); 
+      pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)addr, size); 
+    } 
+  // send the NULL value to terminate 
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *)); 
+}
+
+static __inline__  void dsm_end_send_diffs()  __attribute__ ((unused));
+static __inline__  void dsm_end_send_diffs() 
+{ 
+  pm2_completion_t c; 
+  int end = -1; 
+  
+  pm2_completion_init(&c, NULL, NULL); 
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&end, sizeof(int)); 
+  pm2_pack_completion(SEND_CHEAPER, RECV_CHEAPER,&c); 
+  pm2_rawrpc_end(); 
+#ifdef DEBUG_HYP 
+  tfprintf(stderr, "dsm_send_multiple_diffs: calling pm2_completion_wait (I am %p)\n", marcel_self()); 
+#endif 
+  pm2_completion_wait(&c); 
+}
+
+static __inline__ void dsm_begin_send_multiple_page_req(dsm_node_t dest_node, dsm_node_t req_node, dsm_access_t req_access, int tag)  __attribute__ ((unused));
+static __inline__ void dsm_begin_send_multiple_page_req(dsm_node_t dest_node, dsm_node_t req_node, dsm_access_t req_access, int tag)
+{
+ switch(req_access){
+  case READ_ACCESS: 
+    {
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_MULTIPLE_READ_PAGE_REQ, NULL);
+      break;
+    }
+  case WRITE_ACCESS: 
+    {
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_MULTIPLE_WRITE_PAGE_REQ, NULL);
+      break;
+    }
+  default:
+    RAISE(PROGRAM_ERROR);
+ }
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&req_node, sizeof(dsm_node_t));
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&tag, sizeof(int));
+}
+
+#define dsm_pack_page_req(index) pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&index, sizeof(unsigned long))
+
+static __inline__ void  dsm_end_send_multiple_page_req()  __attribute__ ((unused));
+static __inline__ void  dsm_end_send_multiple_page_req()
+{
+ int end = -1;
+
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&end, sizeof(unsigned long));
+ pm2_rawrpc_end();
+}
+
+static __inline__ void dsm_begin_send_multiple_pages(dsm_node_t dest_node, dsm_access_t access, int tag) __attribute__ ((unused));
+static __inline__ void dsm_begin_send_multiple_pages(dsm_node_t dest_node, dsm_access_t access, int tag)
+{
+  dsm_node_t reply_node = dsm_self();
+
+  switch(access){
+  case READ_ACCESS: 
+    {
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_MULTIPLE_PAGES_READ, NULL);
+      break;
+    }
+  case WRITE_ACCESS: 
+    {
+      pm2_rawrpc_begin((int)dest_node, DSM_LRPC_SEND_MULTIPLE_PAGES_WRITE, NULL);
+      break;
+    }
+  default:
+    RAISE(PROGRAM_ERROR);
+ }
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&reply_node, sizeof(dsm_node_t));
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&tag, sizeof(int));
+}
+
+static __inline__ void dsm_pack_page(unsigned long index) __attribute__ ((unused));
+static __inline__ void dsm_pack_page(unsigned long index)
+{
+  void *addr = dsm_get_page_addr(index);
+  unsigned long page_size = dsm_get_page_size(index);
+
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&addr, sizeof(void *));
+  pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char *)&page_size, sizeof(unsigned long));
+  pm2_pack_byte(SEND_CHEAPER, RECV_CHEAPER, (char *)addr, page_size); 
+}
+
+
+static __inline__ void  dsm_end_send_multiple_pages() __attribute__ ((unused));
+static __inline__ void  dsm_end_send_multiple_pages()
+{
+ void *end = NULL;
+
+ /* Send NULL to terminate */
+ pm2_pack_byte(SEND_SAFER, RECV_EXPRESS, (char*)&end, sizeof(void *));
+ pm2_rawrpc_end();
+}
 
 #endif
 

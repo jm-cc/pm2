3b3e9bfadb876bb977e70882f8e1999c2056cecc
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 17:07:46 +0000

    rev 1.10 at 2003/02/28 15:15:20 GMT  - ajout de marcel_sem_try_P
    (Logical change 1.733)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8887 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_sem.h
+++ pm2/pm2/marcel/include/marcel_sem.h
@@ -33,6 +33,7 @@ typedef struct semaphor_struct marcel_sem_t;
 
 void marcel_sem_init(marcel_sem_t *s, int initial);
 void marcel_sem_P(marcel_sem_t *s);
+int marcel_sem_try_P(marcel_sem_t *s);
 void marcel_sem_V(marcel_sem_t *s);
 void marcel_sem_timed_P(marcel_sem_t *s, unsigned long timeout);
 _PRIVATE_ void marcel_sem_VP(marcel_sem_t *s1, marcel_sem_t *s2);

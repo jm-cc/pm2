2e0f79a5bdd5627d69111982bce3b2d0309eb02d
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:48:47 +0000

    rev 1.6 at 2001/06/20 15:37:47 GMT  - retablissement des arguments par defaut
    (Logical change 1.572)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7758 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad3/examples/mad_ping.c
+++ pm2/pm2/mad3/examples/mad_ping.c
@@ -56,9 +56,9 @@ static const int param_control_receive   = 0;
 static const int param_send_mode         = mad_send_CHEAPER;
 static const int param_receive_mode      = mad_receive_CHEAPER;
 static const int param_nb_samples        = 1000;
-static const int param_min_size          = 0;
-static const int param_max_size          = 256;
-static const int param_step              = 4; /* 0 = progression log. */
+static const int param_min_size          = 1;
+static const int param_max_size          = 2*1024*1024;
+static const int param_step              = 0; /* 0 = progression log. */
 static const int param_nb_tests          = 5;
 static const int param_no_zero           = 1;
 static const int param_fill_buffer       = 1;

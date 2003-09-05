3705dbf59ff40950da281793732dfbfea52d3a19
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:40:38 +0000

    rev 1.2 at 2000/06/02 09:08:58 GMT  Bug fixed in mpi_*.sh scripts: mpi- changed into mpi_
    (Logical change 1.207)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2500 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad1/config/options/10mpi_sp2.sh
+++ pm2/pm2/mad1/config/options/10mpi_sp2.sh
@@ -1,6 +1,6 @@
-PM2_PROTOCOLS=mpi-sp2
+PM2_PROTOCOLS=mpi_sp2
 SP2_DIR=${SP2_DIR:-/usr/lpp/ppe.poe}
 PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -I${SP2_DIR}/include '-DMPI_Init(a,b)=poe_remote_main();MPI_Init(a,b)'"
 PM2_MAD1_LIBS="$PM2_MAD1_LIBS -L${SP2_DIR}/lib -L${SP2_DIR}/lib/ip -lmpi"
-PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNET_ARCH="'\"'mpi-sp2'\"'
+PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNET_ARCH="'\"'mpi_sp2'\"'
 PM2_MAD1_CFLAGS="$PM2_MAD1_CFLAGS -DNETINTERF_INIT=mad_mpi_netinterf_init"

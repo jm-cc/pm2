600ab7281bfd042f4d20125f4bac03f9e6c1fe85
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.10 at 2001/06/11 09:11:58 GMT  - ntbx, import de MadIII
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7574 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ntbx/include/ntbx_interface.h
+++ pm2/pm2/ntbx/include/ntbx_interface.h
@@ -35,6 +35,151 @@ ntbx_purge_cmd_line(int   *argc,
 		    char **argv);
 
 /*
+ * Ntbx - constructors
+ * -------------------
+ */
+p_ntbx_client_t
+ntbx_client_cons(void);
+
+p_ntbx_server_t
+ntbx_server_cons(void);
+
+p_ntbx_process_info_t
+ntbx_process_info_cons(void);
+
+p_ntbx_process_container_t
+ntbx_pc_cons(void);
+
+p_ntbx_process_t
+ntbx_process_cons(void);
+
+p_ntbx_topology_element_t
+ntbx_topology_element_cons(void);
+
+p_ntbx_topology_table_t
+ntbx_topology_table_cons(void);
+
+/*
+ * Ntbx - destructors
+ * ------------------
+ */
+void
+ntbx_client_dest(p_ntbx_client_t            object,
+		 p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_server_dest(p_ntbx_server_t            object,
+		 p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_process_info_dest(p_ntbx_process_info_t      object,
+		       p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_pc_dest(p_ntbx_process_container_t object,
+	     p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_process_dest(p_ntbx_process_t           object,
+		  p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_topology_element_dest(p_ntbx_topology_element_t  object,
+			   p_tbx_specific_dest_func_t dest_func);
+
+void
+ntbx_topology_table_dest(p_ntbx_topology_table_t    object,
+			 p_tbx_specific_dest_func_t dest_func);
+
+
+/*
+ * Ntbx - misc
+ * -----------
+ */
+void
+ntbx_misc_init(void);
+
+void
+ntbx_pc_add(p_ntbx_process_container_t  pc,
+	    p_ntbx_process_t            process,
+	    ntbx_process_lrank_t        local_rank,
+	    void                       *object,
+	    const char                 *name,
+	    void                       *specific);
+
+ntbx_process_grank_t
+ntbx_pc_local_to_global(p_ntbx_process_container_t pc,
+			ntbx_process_lrank_t       local_rank);
+
+p_ntbx_process_t
+ntbx_pc_get_local_process(p_ntbx_process_container_t pc,
+			  ntbx_process_grank_t       local_rank);
+
+p_ntbx_process_info_t
+ntbx_pc_get_local(p_ntbx_process_container_t pc,
+		  ntbx_process_lrank_t       local_rank);
+
+void *
+ntbx_pc_get_local_specific(p_ntbx_process_container_t pc,
+			   ntbx_process_lrank_t       local_rank);
+
+tbx_bool_t
+ntbx_pc_first_local_rank(p_ntbx_process_container_t pc,
+			 p_ntbx_process_lrank_t     local_rank);
+
+tbx_bool_t
+ntbx_pc_next_local_rank(p_ntbx_process_container_t pc,
+			p_ntbx_process_lrank_t     local_rank);
+
+ntbx_process_lrank_t
+ntbx_pc_global_to_local(p_ntbx_process_container_t pc,
+			ntbx_process_grank_t       global_rank);
+
+p_ntbx_process_t
+ntbx_pc_get_global_process(p_ntbx_process_container_t pc,
+			   ntbx_process_grank_t       global_rank);
+
+p_ntbx_process_info_t
+ntbx_pc_get_global(p_ntbx_process_container_t pc,
+		   ntbx_process_grank_t       global_rank);
+
+void *
+ntbx_pc_get_global_specific(p_ntbx_process_container_t pc,
+			    ntbx_process_grank_t       global_rank);
+
+tbx_bool_t
+ntbx_pc_first_global_rank(p_ntbx_process_container_t pc,
+			  p_ntbx_process_grank_t     global_rank);
+
+tbx_bool_t
+ntbx_pc_next_global_rank(p_ntbx_process_container_t pc,
+			 p_ntbx_process_grank_t     global_rank);
+
+p_ntbx_topology_table_t
+ntbx_topology_table_init(p_ntbx_process_container_t  pc,
+			 ntbx_topology_t             topology,
+			 void                       *parameter);
+
+void
+ntbx_topology_table_set(p_ntbx_topology_table_t ttable,
+			ntbx_process_lrank_t    src,
+			ntbx_process_lrank_t    dst);
+
+void
+ntbx_topology_table_clear(p_ntbx_topology_table_t ttable,
+			  ntbx_process_lrank_t    src,
+			  ntbx_process_lrank_t    dst);
+
+p_ntbx_topology_element_t
+ntbx_topology_table_get(p_ntbx_topology_table_t ttable,
+			ntbx_process_lrank_t    src,
+			ntbx_process_lrank_t    dst);
+
+char *
+ntbx_true_name(char *host_name);
+
+
+/*
  * Pack/Unpack
  * -----------
  */
@@ -51,6 +196,20 @@ ntbx_pack_long(long                 data,
 
 long
 ntbx_unpack_long(p_ntbx_pack_buffer_t pack_buffer);
+
+void
+ntbx_pack_unsigned_int(unsigned int         data,
+		       p_ntbx_pack_buffer_t pack_buffer);
+
+unsigned int
+ntbx_unpack_unsigned_int(p_ntbx_pack_buffer_t pack_buffer);
+
+void
+ntbx_pack_unsigned_long(unsigned long        data,
+			p_ntbx_pack_buffer_t pack_buffer);
+
+unsigned long
+ntbx_unpack_unsigned_long(p_ntbx_pack_buffer_t pack_buffer);
 
 void
 ntbx_pack_double(double               data,
@@ -131,8 +290,8 @@ ntbx_tcp_read_block(p_ntbx_client_t  client,
 
 int
 ntbx_tcp_write_block(p_ntbx_client_t  client,
-		     void            *ptr,
-		     size_t           length);
+		     const void      *ptr,
+		     const size_t     length);
 
 int
 ntbx_btcp_read_block(p_ntbx_client_t  client,
@@ -141,8 +300,8 @@ ntbx_btcp_read_block(p_ntbx_client_t  client,
 
 int
 ntbx_btcp_write_block(p_ntbx_client_t  client,
-		     void            *ptr,
-		     size_t           length);
+		     const void       *ptr,
+		     const size_t      length);
 
 int
 ntbx_tcp_read_pack_buffer(p_ntbx_client_t      client,
@@ -166,17 +325,17 @@ ntbx_btcp_read_string(p_ntbx_client_t   client,
 
 int
 ntbx_btcp_write_string(p_ntbx_client_t  client,
-		       char            *string);
+		       const char      *string);
 
 void
-ntbx_tcp_read(int     socket,
+ntbx_tcp_read(int     socket_fd,
 	      void   *ptr,
 	      size_t  length);
 
 void
-ntbx_tcp_write(int     socket,
-	       void   *ptr,
-	       size_t  length);
+ntbx_tcp_write(int           socket_fd,
+	       const void   *ptr,
+	       const size_t  length);
 
 #endif /* NTBX_TCP */
 

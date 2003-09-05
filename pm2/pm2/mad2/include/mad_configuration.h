312e1c4d75de9817c5bb24f72f04bc3386b78f5b
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 03:23:27 +0000

    rev 1.4 at 2000/09/05 14:01:21 GMT  - support de configurations a plusieurs executables
    (Logical change 1.303)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3395 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/include/mad_configuration.h
+++ pm2/pm2/mad2/include/mad_configuration.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: mad_configuration.h,v $
+Revision 1.4  2000/09/05 14:01:21  oaumage
+- support de configurations a plusieurs executables
+
 Revision 1.3  2000/02/08 17:47:21  oaumage
 - prise en compte des types de la net toolbox
 
@@ -60,6 +63,7 @@ typedef struct s_mad_configuration
   mad_configuration_size_t     size;
   ntbx_host_id_t               local_host_id;
   char                       **host_name; /* configuration host name list */
+  char                       **program_name; /* program name list */
 } mad_configuration_t;
 
 #endif /* MAD_CONFIGURATION_H */

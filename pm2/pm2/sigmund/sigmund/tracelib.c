f64e72ac370081200bbb595e68c766aa10554cb2
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:28:41 +0000

    rev 1.4 at 2001/04/03 12:56:56 GMT  *** empty log message ***
    2003/09/05 12:26:35+02:00 ens-lyon.fr!cmenier
    rev 1.3 at 2001/04/03 12:39:19 GMT
     stable ??
    
    2003/09/05 12:25:39+02:00 ens-lyon.fr!cmenier
    rev 1.2 at 2001/04/03 09:59:57 GMT
     cleaning
    
    (Logical change 1.488)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6987 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/sigmund/tracelib.c
+++ pm2/pm2/sigmund/sigmund/tracelib.c
@@ -1,10 +1,56 @@
 #include "tracebuffer.h"
+#include "filter.h"
 #include "tracelib.h"
+#include "temp.h"
 
-int tracelib_init(char *supertrace)
+char *fut_code2name(int code)
+{
+  int i = 0;
+  while(1) {
+    if (code_table[i].code == 0) return NULL;
+    if (code_table[i].code == code) return code_table[i].name;
+    i++;
+  }
+}
+
+int name2code(char *name, mode *type, int *a)
+{
+  int i = 0;
+  while(1) {
+    if (code_table[i].code == 0) break;
+    if (!strcmp(code_table[i].name,name)) {
+      *type = USER;
+      *a = code_table[i].code;
+      return 0;
+    }
+    i++;
+  }
+  i = 0;
+  while(1) {
+    if (fkt_code_table[i].code == 0) return -1;
+    if (!strcmp(fkt_code_table[i].name,name)) {
+      *type = KERNEL;
+      *a = fkt_code_table[i].code;
+      return 0;
+    }
+    i++;
+  }
+  return -1;
+}
+
+char *fkt_code2name(int code)
+{
+  int i = 0;
+  while(1) {
+    if (fkt_code_table[i].code == 0) return NULL;
+    if (fkt_code_table[i].code == code) return fkt_code_table[i].name;
+    i++;
+  }
+}
+
+void tracelib_init(char *supertrace)
 {
   init_trace_file(supertrace);
-  init_filter();
 }
 
 int get_next_filtered_trace(trace *tr)

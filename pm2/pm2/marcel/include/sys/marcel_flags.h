885b13a3474ede3713133fb67e66460d145c5974
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 18:08:30 +0000

    rev 1.19 at 2003/04/14 15:05:29 GMT  ajout libpthread
    (Logical change 1.735)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9159 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_flags.h
+++ pm2/pm2/marcel/include/sys/marcel_flags.h
@@ -18,48 +18,48 @@
 #define MARCEL_FLAGS_EST_DEF
 
 #ifdef PM2DEBUG
-#define MA__DEBUG
+#  define MA__DEBUG
 #endif
 
 #ifdef __ACT__
-#error use of __ACT__ depreciated. Define MARCEL_ACT instead
-#undef __ACT__
-#define MARCEL_ACT
+#  error use of __ACT__ depreciated. Define MARCEL_ACT instead
+#  undef __ACT__
+#  define MARCEL_ACT
 #endif
 
 #ifdef SMP
-#error use of SMP depreciated. Define MARCEL_SMP instead
-#undef SMP
-#define MARCEL_SMP
+#  error use of SMP depreciated. Define MARCEL_SMP instead
+#  undef SMP
+#  define MARCEL_SMP
 #endif
 
 #ifdef MARCEL_SMP
-#undef MARCEL_SMP
-#define MARCEL_SMP 1
+#  undef MARCEL_SMP
+#  define MARCEL_SMP 1
 #endif
 
 #ifdef MARCEL_ACT
-#undef MARCEL_ACT
-#define MARCEL_ACT 1
+#  undef MARCEL_ACT
+#  define MARCEL_ACT 1
 #endif
 
 #ifdef MARCEL_ACTSMP
-#undef MARCEL_ACTSMP
-#define MARCEL_ACTSMP 1
+#  undef MARCEL_ACTSMP
+#  define MARCEL_ACTSMP 1
 #endif
 
 #ifdef MARCEL_MONO
-#undef MARCEL_MONO
-#define MARCEL_MONO 1
+#  undef MARCEL_MONO
+#  define MARCEL_MONO 1
 #endif
 
 #if ((MARCEL_SMP + MARCEL_ACT + MARCEL_ACTSMP + MARCEL_MONO) == 0)
-#error No MARCEL_... defined. Choose between MONO SMP ACT and ACTSMP
+#  error No MARCEL_... defined. Choose between MONO SMP ACT and ACTSMP
 //#define MARCEL_MONO 1
 #endif
 
 #if ((MARCEL_SMP + MARCEL_ACT + MARCEL_ACTSMP + MARCEL_MONO) > 1)
-#error You must define at most one of MARCEL_SMP, MARCEL_ACT, MARCEL_ACTSMP or MARCEL_MONO
+#  error You must define at most one of MARCEL_SMP, MARCEL_ACT, MARCEL_ACTSMP or MARCEL_MONO
 #endif
 
 /* MA__LWPS : indique que l'on a plusieurs entités ordonnancées par
@@ -68,7 +68,7 @@
  *  Cela arrive en SMP ou en Activation sur multiprocesseur 
  * */
 #ifdef MA__LWPS
-#undef MA__LWPS
+#  undef MA__LWPS
 #endif
 
 /* MA__ONE_QUEUE : indique qu'il n'y a qu'une seule queue pour les threads
@@ -77,7 +77,7 @@
  * C'est le cas tout le temps sauf en SMP
  * */
 #ifdef MA__ONE_QUEUE
-#undef MA__ONE_QUEUE
+#  undef MA__ONE_QUEUE
 #endif
 
 /* MA__MULTIPLE_RUNNING : indique qu'il peut y avoir plusieurs threads
@@ -86,69 +86,74 @@
  * C'est le cas avec les Activations
  * */
 #ifdef MA__MULTIPLE_RUNNING
-#undef MA__MULTIPLE_RUNNING
+#  undef MA__MULTIPLE_RUNNING
 #endif
 
 /* MA__ACTIVATION : indique qu'on utilise les activations (mono ou
  * smp). On peut donc utiliser les constantes de act.h
  * */
 #ifdef MA__ACTIVATION
-#undef MA__ACTIVATION
+#  undef MA__ACTIVATION
 #endif
 
 /* MA__TIMER : indique qu'il faut utiliser le timer unix (signaux)
  * pour la préemption. (ne pas valider avec les activations)
  * */
 #ifdef MA__TIMER
-#undef MA__TIMER
+#  undef MA__TIMER
 #endif
 
 /* MA__WORK : utilisation des files de travail à la fin des unlock_task()
  * */
 #ifdef MA__WORK
-#undef MA__WORK
+#  undef MA__WORK
 #endif
 
 #ifdef MARCEL_MONO /* Marcel Mono */
-#define MA__MONO
-#define MA__ONE_QUEUE
-#define MA__TIMER
+#  define MA__MONO
+#  define MA__ONE_QUEUE
+#  define MA__TIMER
 #endif /* Fin Marcel Mono */
 
 #ifdef MARCEL_SMP /* Marcel SMP */
-#define MA__SMP /* Utilisation des pthreads pour les lwp */
-#define MA__LWPS
-#define MA__TIMER
-#define MA__WORK
-#ifndef SMP_MULTIPLE_QUEUES
-#define MA__ONE_QUEUE
-#define MA__MULTIPLE_RUNNING
-#endif
-
+#  define MA__SMP /* Utilisation des pthreads pour les lwp */
+#  define MA__LWPS
+#  define MA__TIMER
+#  define MA__WORK
+#  ifndef SMP_MULTIPLE_QUEUES
+#    define MA__ONE_QUEUE
+#    define MA__MULTIPLE_RUNNING
+#  endif
 #endif /* Fin Marcel SMP */
 
 #if defined(MARCEL_ACT) || defined(MARCEL_ACTSMP) /* Marcel Activation */
-#define MA__ONE_QUEUE
-#define MA__MULTIPLE_RUNNING
-#define MA__ACTIVATION
-#define ACTIVATION
-#define MA__WORK
+#  define MA__ONE_QUEUE
+#  define MA__MULTIPLE_RUNNING
+#  define MA__ACTIVATION
+#  define ACTIVATION
+#  define MA__WORK
 #endif /* Fin Marcel Activation */
 
 #ifdef MARCEL_ACT /* Marcel Activation Mono */
-#define MA__ACT
-#undef CONFIG_SMP
+#  define MA__ACT
+#  undef CONFIG_SMP
 #endif /* Fin Marcel Activation Mono */
 
 #ifdef MARCEL_ACTSMP /* Marcel Activation SMP */
-#define MA__ACTSMP
-#define CONFIG_SMP
-#define __SMP__
-#define MA__LWPS
+#  define MA__ACTSMP
+#  define CONFIG_SMP
+#  define __SMP__
+#  define MA__LWPS
 #endif /* Fin Marcel Activation SMP */
 
 #if defined(MA__LWPS) && defined(MA__TIMER)
-#define MA_PROTECT_LOCK_TASK_FROM_SIG
+#  define MA_PROTECT_LOCK_TASK_FROM_SIG
+#endif
+
+#ifdef MARCEL_POSIX
+#  define MA__POSIX_FUNCTIONS_NAMES
+#  define MA__POSIX_BEHAVIOUR
+#  define MA__PTHREAD_FUNCTIONS
 #endif
 
 #endif

fa5f50bba6384c35a1dc9c594320e271d8d560c3
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 17:55:28 +0000

    rev 1.12 at 2003/04/14 15:05:23 GMT  ajout libpthread
    (Logical change 1.734)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8904 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_mutex.h
+++ pm2/pm2/marcel/include/marcel_mutex.h
@@ -19,44 +19,62 @@
 
 #include <sys/time.h>
 
-#define MARCEL_MUTEX_INITIALIZER { 1 }
+//#define MARCEL_MUTEX_INITIALIZER { 1 }
 
-_PRIVATE_ typedef struct {
-	int dummy;
-} marcel_mutexattr_struct;
+//_PRIVATE_ typedef struct {
+//	int dummy;
+//} marcel_mutexattr_struct;
 
-_PRIVATE_ typedef marcel_sem_t marcel_mutex_t;
+//_PRIVATE_ typedef marcel_sem_t marcel_mutex_t;
 
-_PRIVATE_ typedef struct {
-	int dummy;
-} marcel_condattr_struct;
+//_PRIVATE_ typedef marcel_sem_t marcel_cond_t;
 
-_PRIVATE_ typedef marcel_sem_t marcel_cond_t;
+//typedef marcel_mutexattr_struct marcel_mutexattr_t;
 
-typedef marcel_mutexattr_struct marcel_mutexattr_t;
+//typedef marcel_condattr_struct marcel_condattr_t;
 
-typedef marcel_condattr_struct marcel_condattr_t;
+DEC_MARCEL(int, mutex_init, (marcel_mutex_t *mutex,
+			     __const marcel_mutexattr_t *attr))
+DEC_MARCEL(int, mutex_destroy, (marcel_mutex_t *mutex))
 
-int marcel_mutexattr_init(marcel_mutexattr_t *attr);
-int marcel_mutexattr_destroy(marcel_mutexattr_t *attr);
+DEC_MARCEL(int, mutex_lock, (marcel_mutex_t *mutex))
+DEC_MARCEL(int, mutex_trylock, (marcel_mutex_t *mutex))
+DEC_MARCEL(int, mutex_unlock, (marcel_mutex_t *mutex))
 
-int marcel_mutex_init(marcel_mutex_t *mutex, marcel_mutexattr_t *attr);
-int marcel_mutex_destroy(marcel_mutex_t *mutex);
+DEC_MARCEL(int, mutexattr_init, (marcel_mutexattr_t *attr))
+DEC_MARCEL(int, mutexattr_destroy, (marcel_mutexattr_t *attr))
 
-int marcel_mutex_lock(marcel_mutex_t *mutex);
-int marcel_mutex_trylock(marcel_mutex_t *mutex);
-int marcel_mutex_unlock(marcel_mutex_t *mutex);
 
-int marcel_condattr_init(marcel_condattr_t *attr);
-int marcel_condattr_destroy(marcel_condattr_t *attr);
 
-int marcel_cond_init(marcel_cond_t *cond, marcel_condattr_t *attr);
-int marcel_cond_destroy(marcel_cond_t *cond);
+DEC_POSIX(int, mutex_init, (pmarcel_mutex_t *mutex,
+			    __const pmarcel_mutexattr_t *attr))
+DEC_POSIX(int, mutex_destroy, (pmarcel_mutex_t *mutex))
 
-int marcel_cond_signal(marcel_cond_t *cond);
-int marcel_cond_broadcast(marcel_cond_t *cond);
+DEC_POSIX(int, mutex_lock, (pmarcel_mutex_t *mutex))
+DEC_POSIX(int, mutex_trylock, (pmarcel_mutex_t *mutex))
+DEC_POSIX(int, mutex_unlock, (pmarcel_mutex_t *mutex))
 
-int marcel_cond_wait(marcel_cond_t *cond, marcel_mutex_t *mutex);
-int marcel_cond_timedwait(marcel_cond_t *cond, marcel_mutex_t *mutex,
-			  const struct timespec *abstime);
+DEC_POSIX(int, mutexattr_init, (pmarcel_mutexattr_t *attr))
+DEC_POSIX(int, mutexattr_destroy, (pmarcel_mutexattr_t *attr))
+DEC_POSIX(int, mutexattr_settype, (pmarcel_mutexattr_t *attr, int kind))
+DEC_POSIX(int, mutexattr_setkind_np, (pmarcel_mutexattr_t *attr, int kind))
+DEC_POSIX(int, mutexattr_gettype, (const pmarcel_mutexattr_t *attr, int *kind))
+DEC_POSIX(int, mutexattr_getkind_np, (const pmarcel_mutexattr_t *attr, int *kind))
+DEC_POSIX(int, mutexattr_getpshared, (const pmarcel_mutexattr_t *attr,
+				      int *pshared))
+DEC_POSIX(int, mutexattr_setpshared, (pmarcel_mutexattr_t *attr, int pshared))
+
+DEC_POSIX(int, once, (pmarcel_once_t * once_control, 
+		      void (*init_routine)(void)))
+#ifdef MA__PTHREAD_FUNCTIONS
+extern void __pthread_once_fork_prepare (void);
+extern void __pthread_once_fork_parent (void);
+extern void __pthread_once_fork_child (void);
+extern void __pmarcel_once_fork_prepare (void);
+extern void __pmarcel_once_fork_parent (void);
+extern void __pmarcel_once_fork_child (void);
+extern void __flockfilelist (void);
+extern void __funlockfilelist (void);
+extern void __fresetlockfiles (void);
+#endif
 #endif

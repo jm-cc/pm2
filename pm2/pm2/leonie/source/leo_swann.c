32295e17dbb8ca248c7108f626718de82875236e
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:01:10 +0000

    rev 1.5 at 2000/06/09 12:40:35 GMT  - Progression du code
    (Logical change 1.235)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2684 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leonie/source/leo_swann.c
+++ pm2/pm2/leonie/source/leo_swann.c
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: leo_swann.c,v $
+Revision 1.5  2000/06/09 12:40:35  oaumage
+- Progression du code
+
 Revision 1.4  2000/06/09 08:45:58  oaumage
 - Progression du code
 
@@ -147,7 +150,7 @@ leo_launch_mad_module(p_leonie_t                  leonie,
   ntbx_tcp_client_init(module->net_client);
 
   sprintf(cmd,
-	  "cd%s;/pm2load %s -id %d -cnx %s -master %s &",
+	  "cd %s;./pm2load %s -id %d -cnx %s -master %s &",
 	  cluster->path,
 	  cluster->executable,
 	  module->id,

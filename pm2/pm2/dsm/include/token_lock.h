5bdec7ef3a33bc1e51ac40812d4a63f96f97d594
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:11:51 +0000

    rev 1.2 at 2002/10/14 15:42:06 GMT  started to integrate the hierarchical token locks into DSM-PM2.  basic tests passed.  needs more work though
    (Logical change 1.697)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8702 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/token_lock.h
+++ pm2/pm2/dsm/include/token_lock.h
@@ -1,6 +1,6 @@
 
 /*
- * CVS Id: $Id: token_lock.h,v 1.1 2002/10/13 12:14:02 slacour Exp $
+ * CVS Id: $Id: token_lock.h,v 1.2 2002/10/14 15:42:06 slacour Exp $
  */
 
 /* distributed management of the locks (tokens) for DSM-PM2 */
@@ -39,31 +39,25 @@ token_lock_recv_server (void);
 
 /* initialization function of the lock data structures, called by
  * dsm_pm2_init() */
-extern void
+extern int
 token_lock_initialization (void);
 
 /* clean up and free memory for the lock data structures */
-extern void
+extern int
 token_lock_finalization (void);
 
 /* initialize a new lock; the ID of the lock is returned as an output
- * argument; the return value is 0 in case of success */
+ * argument */
 extern int
 token_lock_init (token_lock_id_t * const);
 
 /* acquire a DSM lock */
-extern void
+extern int
 token_lock (const token_lock_id_t);
 
 /* release a DSM lock */
-extern void
+extern int
 token_unlock (const token_lock_id_t);
-
-/* partial release of a lock: signal the lock can be acquired in the
- * local cluster, but not by a node in a remote cluster.  This
- * function should be called by the memory consistency release function. */
-extern void
-token_partial_unlock (const token_lock_id_t);
 
 /* service function called by RPC to request a lock */
 extern void

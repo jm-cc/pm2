f8b12d7c5b01d6c678301e24eb3ea6176426519e
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 03:23:27 +0000

    rev 1.4 at 2000/09/05 13:58:54 GMT  - quelques corrections et reorganisations dans le support des types   structures
    (Logical change 1.303)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3397 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/include/tbx_pointers.h
+++ pm2/pm2/tbx/include/tbx_pointers.h
@@ -34,6 +34,10 @@
 
 ______________________________________________________________________________
 $Log: tbx_pointers.h,v $
+Revision 1.4  2000/09/05 13:58:54  oaumage
+- quelques corrections et reorganisations dans le support des types
+  structures
+
 Revision 1.3  2000/07/07 14:49:42  oaumage
 - Ajout d'un support pour les tables de hachage
 
@@ -59,7 +63,6 @@ typedef struct s_tbx_list            *p_tbx_list_t;
 typedef struct s_tbx_list_reference  *p_tbx_list_reference_t;
 typedef struct s_tbx_slist_element   *p_tbx_slist_element_t;
 typedef struct s_tbx_slist           *p_tbx_slist_t;
-typedef struct s_tbx_slist_reference *p_tbx_slist_reference_t;
 typedef struct s_tbx_memory          *p_tbx_memory_t;
 typedef struct s_tbx_htable_element  *p_tbx_htable_element_t;
 typedef struct s_tbx_htable          *p_tbx_htable_t;

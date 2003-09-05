2a2e162fed25dc2bb7d6f7924f75b1276200f958
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:54:26 +0000

    rev 1.2 at 2000/06/07 11:24:50 GMT  - Mise a jour des scripts LAM-MPI
    (Logical change 1.222)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2613 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/bin/mad2mpi_run.sh
+++ pm2/pm2/mad2/bin/mad2mpi_run.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 ##########
 
-if [ ! -f ${MAD2_ROOT}/.mad2mpi_implementation ] ; then
-  echo "No MPI implementation has been selected"
-  exit 0
-fi
+echo "Calling mpirun..."
+mpirun N $*
 
-IMPL=`cat ${MAD2_ROOT}/.mad2mpi_implementation`
-
-case "$IMPL" in
-  lam) 
-    echo "Calling mpirun..."
-    mpirun N $*
-    ;;
-  bip) 
-    echo "unimplemented"
-    ;;
-  *)
-    echo "invalid MPI implementation name"
-    exit 0
-    ;;
-esac
 ##########
--- pm2/pm2/mad2/bin/mad2mpi_wipe.sh
+++ pm2/pm2/mad2/bin/mad2mpi_wipe.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 ##########
 
-if [ ! -f ${MAD2_ROOT}/.mad2mpi_implementation ] ; then
-  echo "No MPI implementation has been selected"
-  exit 0
-fi
+echo "Calling wipe..."
+wipe -v ${MAD2_ROOT}/.mad2_conf
 
-IMPL=`cat ${MAD2_ROOT}/.mad2mpi_implementation`
-
-case "$IMPL" in
-  lam) 
-    echo "Calling wipe..."
-    wipe -v ${MAD2_ROOT}/.mad2_conf
-    ;;
-  bip) 
-    echo "unimplemented"
-    ;;
-  *)
-    echo "invalid MPI implementation name"
-    exit 0
-    ;;
-esac
 ##########

7ad7514cc0079d62076f9e808aeb529a45ac7fa4
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:22:57 +0000

    rev 1.4 at 2000/11/14 19:40:40 GMT  Added a 'Common option panel' that make it easy to (un)select the 'gdb' (for instance) option in all modules
    (Logical change 1.357)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3869 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/ezflavor/menu.h
+++ pm2/pm2/ezflavor/menu.h
@@ -2,6 +2,8 @@
 #ifndef MENU_IS_DEF
 #define MENU_IS_DEF
 
+#include <gtk/gtk.h>
+
 void menu_init(GtkWidget *vbox);
 
 void menu_update_flavor(gint load_enabled, gint reload,
@@ -11,5 +13,7 @@ void menu_update_flavor(gint load_enabled, gint reload,
 			gint check_enabled);
 
 void menu_update_module(gint select_enabled, gint deselect_enabled);
+
+void menu_update_common_opt(gint displayed);
 
 #endif

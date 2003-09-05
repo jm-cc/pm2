f27d1bf7f0c99e8c435e8eae28092fa82ffbe702
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:56:43 +0000

    rev 1.5 at 2001/01/03 11:05:25 GMT  - integration des headers du module de forwarding
    (Logical change 1.382)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4109 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/include/mad_channel.h
+++ pm2/pm2/mad2/include/mad_channel.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: mad_channel.h,v $
+Revision 1.5  2001/01/03 11:05:25  oaumage
+- integration des headers du module de forwarding
+
 Revision 1.4  2000/03/08 17:18:48  oaumage
 - support de compilation avec Marcel sans PM2
 - pre-support de packages de Threads != Marcel
@@ -61,10 +64,33 @@ typedef struct s_mad_channel
   TBX_SHARED;
   mad_channel_id_t          id ;
   p_mad_adapter_t           adapter;
+#ifdef MAD_FORWARDING
+  tbx_bool_t                is_forward;
+  struct s_mad_user_channel *user_channel;
+#endif //MAD_FORWARDING
   volatile tbx_bool_t       reception_lock;
   p_mad_connection_t        input_connection; 
   p_mad_connection_t        output_connection;
   p_mad_driver_specific_t   specific;
 } mad_channel_t;
+
+#ifdef MAD_FORWARDING
+typedef struct s_mad_user_channel
+{
+  TBX_SHARED;
+  p_mad_channel_t  *channels;
+  p_mad_channel_t  *forward_channels;
+  mad_adapter_id_t *adapter_ids;
+  ntbx_host_id_t   *gateways;
+
+  int               nb_active_channels;
+  marcel_sem_t      sem_message_ready;
+  marcel_sem_t      sem_lock_msgs;
+  marcel_sem_t      sem_msg_handled;
+  p_mad_connection_t msg_connection;
+
+  char* name;
+} mad_user_channel_t, *p_mad_user_channel_t;
+#endif //MAD_FORWARDING
 
 #endif /* MAD_CHANNEL_H */

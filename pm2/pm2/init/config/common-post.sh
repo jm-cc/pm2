74a29363d693c6b267cfdc33967d641853107d7a
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 11:45:54 +0000

    rev 1.3 at 2001/05/28 14:51:19 GMT  typo fix in warning message
    (Logical change 1.551)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7470 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/init/config/common-post.sh
+++ pm2/pm2/init/config/common-post.sh
@@ -1,8 +1,10 @@
 
 # Just for people which do not upgrade their flavors often! ;-)
-if [ "$PM2_COMMON_LINK_MODE_STATIC" != yes -a "$PM2_COMMON_LINK_MODE_DYNAMIC" != yes ] ; then
-    echo "Warning, you should update for flavor to choose STATIC or DYNAMIC linking" 1>&2
-    echo "Default set to STATIC for now" 1>&2
+if [ "$PM2_COMMON_LINK_MODE_STATIC" != yes \
+      -a "$PM2_COMMON_LINK_MODE_DYNAMIC" != yes ] ; then
+    echo "Warning, you should update your flavor to choose" \
+        "STATIC or DYNAMIC linking mode" 1>&2
+    echo "******** Default set to STATIC for now" 1>&2
     PM2_COMMON_LINK_MODE_STATIC=yes
 fi
 

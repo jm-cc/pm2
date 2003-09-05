d611f8d19b58122345a3aeb1befe8f976db22219
ens-lyon.fr!bouge <ens-lyon.fr!bouge@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:57:23 +0000

    rev 1.3 at 2001/04/19 13:28:09 GMT  *** empty log message ***
    (Logical change 1.509)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7198 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/doc/Getting_started/emph.pl
+++ pm2/pm2/doc/Getting_started/emph.pl
@@ -1,8 +1,15 @@
 
-$emph = '(?:here\s*[!]?)';
+$line_emph = '(?:[Hh]ere\s*[!]?)';
 
-$emph_pattern = '(?:\s*/\*[*\s]*' . $emph . '[*\s]*\*/\s*)';
-$substpattern = "\001verbatimemph|";
+$start_emph = '(?:[Ss]tart\s*[!]?)';
+$end_emph = '(?:[Ee]nd\s*[!]?)';
+
+$line_emph_pattern = '(?:\s*/\*[*\s]*' . $line_emph . '[*\s]*\*/\s*)';
+$start_emph_pattern = '(?:\s*/\*[*\s]*' . $start_emph . '[*\s]*\*/\s*)';
+$end_emph_pattern = '(?:\s*/\*[*\s]*' . $end_emph . '[*\s]*\*/\s*)';
+
+$start_subst_pattern = "\001";
+$end_subst_pattern = "\002";
 
 if ($#ARGV != 0) {
     die "Usage: $0 file-to-emph";
@@ -22,9 +29,29 @@ open (OUT, ">$out") ||
 die ("Cannot open |$out| for writing");
 
 while (defined ($line = <IN>)) {
-    if  ($line =~ m/^(\s*)(.*)$emph_pattern$/io) {
-	$line = "$1$substpattern$2\n";
+
+## Suppress EOL
+
+    $line =~ s/$//o;
+
+## Line emphasize: start + line + end + EOL
+
+    if  ($line =~ s/$line_emph_pattern//o) {
+	$line =~ s/\A/$start_subst_pattern/o;
+	$line =~ s/\Z/$end_subst_pattern/o;
+	$line .= "\n";
     }
+
+## Start/Stop emphasize: replace with start/stop and suppress EOF so
+## that they can stand on dedicated line without disturbing
+
+    else {
+	$line =~ s/$start_emph_pattern/$start_subst_pattern/o;
+	$line =~ s/$end_emph_pattern/$end_subst_pattern/io;
+    }
+
+## Print out line
+
     print OUT $line;
 }
 

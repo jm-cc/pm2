23e633d3950778141b02b94ae85c2495c539e6ca
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:07:16 +0000

    rev 1.11 at 2000/04/14 11:41:47 GMT  move SCHED_DATA(lwp).sched_task to lwp->idle_task
    2003/09/05 02:06:44+02:00 ens-lyon.fr!vdanjean
    rev 1.10 at 2000/04/14 08:49:03 GMT
     new kernel activation version : adaptation of marcel
    
    (Logical change 1.145)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1635 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/upcalls.h
+++ pm2/pm2/marcel/include/sys/upcalls.h
@@ -1,6 +1,8 @@
 #ifndef _UPCALLS_H
 #define _UPCALLS_H
 
+#ifdef MA__ACTIVATION
+
 #ifdef ACT_TIMER
 #ifdef CONFIG_ACT_TIMER
 #undef CONFIG_ACT_TIMER
@@ -17,10 +19,18 @@ extern volatile int act_nb_unblocked;
 void act_lock(marcel_t self);
 void act_unlock(marcel_t self);
 
-void act_goto_next_task(marcel_t pid);
+void act_goto_next_task(marcel_t pid, int from);
 //void restart_thread(task_desc *desc);
 
 void init_upcalls(int nb_act);
 //#define launch_upcalls(__nb_act_wanted)
+
+enum {
+	ACT_RESTART_FROM_SCHED,
+	ACT_RESTART_FROM_IDLE,
+	ACT_RESTART_FROM_UPCALL_NEW,
+};
+
+#endif 
 
 #endif /* _UPCALLS_H */

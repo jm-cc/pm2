ef24d3d438e9b528fdc8fb129df539aaceeb1ea0
ens-lyon.fr!bouge <ens-lyon.fr!bouge@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:52:29 +0000

    rev 1.1 at 2001/04/18 16:13:48 GMT  *** empty log message ***
    (Logical change 1.505)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7160 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/doc/Getting_started/emph.pl
@@ -0,0 +1,32 @@
+
+$emph = '(?:here\s*!*)';
+
+$emph_pattern = '(?:\s*/\*[*\s]*' . $emph . '[*\s]*\*/\s*)';
+$substpattern = "\001verbatimbf|";
+
+if ($#ARGV != 0) {
+    die "Usage: $0 file-to-emph";
+}
+
+$filename = $ARGV[0];
+
+$in = "$filename";
+$out = "$in-emph";
+
+print "Emphasizing |$in|...\n";
+
+open (IN, "<$in") || 
+die ("Cannot open |$in| for reading");
+
+open (OUT, ">$out") || 
+die ("Cannot open |$out| for writing");
+
+while (defined ($line = <IN>)) {
+    if  ($line =~ m/^(\s*)(.*)$emph_pattern$/io) {
+	$line = "$1$substpattern$2\n";
+    }
+    print OUT $line;
+}
+
+close (IN);
+close (OUT);

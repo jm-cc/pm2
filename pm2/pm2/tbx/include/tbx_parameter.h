04793d46d54b88589dadfcccab66b45eeab35f1f
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 13:10:49 +0000

    rev 1.4 at 2001/07/02 15:52:12 GMT  - corrections de bugs divers
    2003/09/05 15:10:18+02:00 ens-lyon.fr!oaumage
    rev 1.3 at 2001/07/02 15:50:23 GMT
     - corrections de bugs divers, aide de Leonie
    
    2003/09/05 15:07:43+02:00 ens-lyon.fr!oaumage
    rev 1.2 at 2001/07/02 13:09:26 GMT
     - common_exit, fonctionnement en ligne de commande, BIP/MadIII
    
    (Logical change 1.582)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7829 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/include/tbx_parameter.h
+++ pm2/pm2/tbx/include/tbx_parameter.h
@@ -25,7 +25,6 @@
  * Data structures 
  * ---------------
  */
-
 typedef struct s_tbx_argument_option
 {
   p_tbx_string_t option;
@@ -64,4 +63,145 @@ typedef struct s_tbx_command
   p_tbx_arguments_t   arguments;
 } tbx_command_t;
 
+
+/*
+ * Argument iterator 
+ * -----------------
+ *
+ * Note: this feature uses GCC nested functions
+ * ====
+ */
+
+#define __TBX_ARGIT_MORE_ARGS(ARGC, ARGV) \
+  tbx_bool_t tbx_argit_more_args(void)    \
+    {                                     \
+      return ((ARGC) != 0);               \
+    }
+
+#define __TBX_ARGIT_ARG_CSTR(ARGC, ARGV, USAGE) \
+  char *tbx_argit_arg_cstr(void)                \
+    {                                           \
+      if (!tbx_argit_more_args())               \
+        USAGE();                                \
+      return tbx_strdup(*(ARGV));               \
+    }
+
+#define __TBX_ARGIT_ARG_EQUALS(ARGC, ARGV, USAGE) \
+  tbx_bool_t tbx_argit_arg_equals(char *arg)      \
+    {                                             \
+      if (!tbx_argit_more_args())                 \
+        USAGE();                                  \
+      return !strcmp(*(ARGV), arg);               \
+    }
+
+#define __TBX_ARGIT_NEXT_ARG(ARGC, ARGV) \
+  tbx_bool_t tbx_argit_next_arg(void)    \
+    {                                    \
+      (ARGC)--;                          \
+      (ARGV)++;                          \
+      return tbx_argit_more_args();      \
+    }
+  
+#define __TBX_ARGIT_OPT_CSTR(ARGC, ARGV) \
+  char *tbx_argit_opt_cstr(void)         \
+    {                                    \
+      return tbx_argit_arg_cstr();       \
+    }
+
+#define __TBX_ARGIT_OPT_EQUALS(ARGC, ARGV)   \
+  tbx_bool_t tbx_argit_opt_equals(char *arg) \
+    {                                        \
+      return tbx_argit_arg_equals(arg);      \
+    }
+
+#define __TBX_ARGIT_VOPT_EQUALS(ARGC, ARGV, USAGE) \
+  tbx_bool_t tbx_argit_vopt_equals(char *arg)      \
+    {                                              \
+      int  _len = strlen(arg);                     \
+      char _arg[_len + 2];                         \
+      strcpy(_arg, arg);                           \
+      strcat(_arg, "=");                           \
+      if (!tbx_argit_more_args())                  \
+        USAGE();                                   \
+      return !strncmp(*(ARGV), _arg, _len + 1);    \
+    }
+
+#define __TBX_ARGIT_VOPT_VALUE_CSTR(ARGC, ARGV, USAGE) \
+  char *tbx_argit_vopt_value_cstr(void)                \
+    {                                                  \
+      char *ptr = NULL;                                \
+      ptr = strchr(*(ARGV), '=');                      \
+      if (!ptr)                                        \
+        USAGE();                                       \
+      ptr++;                                           \
+      return tbx_strdup(ptr);                          \
+    }
+
+#define __TBX_ARGIT_MORE_OPTS(ARGC, ARGV) \
+  tbx_bool_t tbx_argit_more_opts(void)    \
+    {                                     \
+      if (!tbx_argit_more_args())         \
+	return tbx_false;                 \
+                                          \
+      if ((*(ARGV))[0] != '-')            \
+	return tbx_false;                 \
+                                          \
+      if (tbx_argit_arg_equals("--"))     \
+	{                                 \
+	  tbx_argit_next_arg();           \
+	  return tbx_false;               \
+	}                                 \
+                                          \
+      return tbx_true;                    \
+    }
+
+#define __TBX_ARGIT_NEXT_OPT(ARGC, ARGV) \
+  tbx_bool_t tbx_argit_next_opt(void)    \
+    {                                    \
+      tbx_argit_next_arg();              \
+      return tbx_argit_more_opts();      \
+    }
+
+#define __TBX_ARGIT_VALUE_CSTR(ARGC, ARGV, USAGE) \
+  char *tbx_argit_value_cstr(void)                \
+    {                                             \
+      tbx_argit_next_arg();                       \
+                                                  \
+      if (argc)                                   \
+	{                                         \
+	  return tbx_argit_arg_cstr();            \
+	}                                         \
+      else                                        \
+	USAGE();                                  \
+    }
+
+#define __TBX_ARGIT_INVALID_ARG(ARGC, ARGV, USAGE)        \
+  void tbx_argit_invalid_arg(void)                        \
+    {                                                     \
+      fprintf(stderr, "invalid argument: %s\n", *(ARGV)); \
+      USAGE();                                            \
+    }
+
+#define __TBX_ARGIT_INIT(ARGC, ARGV) \
+  tbx_bool_t tbx_argit_init(void)    \
+    {                                \
+      tbx_argit_next_arg();          \
+      return tbx_argit_more_args();  \
+    }
+
+#define tbx_arg_iterator(ARGC, ARGV, USAGE)    \
+__TBX_ARGIT_MORE_ARGS(ARGC, ARGV)              \
+__TBX_ARGIT_ARG_CSTR(ARGC, ARGV, USAGE)        \
+__TBX_ARGIT_ARG_EQUALS(ARGC, ARGV, USAGE)      \
+__TBX_ARGIT_NEXT_ARG(ARGC, ARGV)               \
+__TBX_ARGIT_OPT_CSTR(ARGC, ARGV)               \
+__TBX_ARGIT_OPT_EQUALS(ARGC, ARGV)             \
+__TBX_ARGIT_VOPT_EQUALS(ARGC, ARGV, USAGE)     \
+__TBX_ARGIT_VOPT_VALUE_CSTR(ARGC, ARGV, USAGE) \
+__TBX_ARGIT_MORE_OPTS(ARGC, ARGV)              \
+__TBX_ARGIT_NEXT_OPT(ARGC, ARGV)               \
+__TBX_ARGIT_VALUE_CSTR(ARGC, ARGV, USAGE)      \
+__TBX_ARGIT_INVALID_ARG(ARGC, ARGV, USAGE)     \
+__TBX_ARGIT_INIT(ARGC, ARGV)
+ 
 #endif /* TBX_PARAMETER_H */

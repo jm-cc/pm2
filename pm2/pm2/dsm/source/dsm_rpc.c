31f1c5cc004579ba20646e085a0ed01c92110977
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:58:40 +0000

    rev 1.8 at 2000/07/14 16:16:56 GMT  Merged with branch dsm3
    (Logical change 1.293)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3086 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_rpc.c
+++ pm2/pm2/dsm/source/dsm_rpc.c
@@ -34,9 +34,6 @@
 */
 
 #include "pm2.h"
-//#include "dsm_rpc.h"
-//#include "dsm_page_manager.h"
-
 
 extern void DSM_LRPC_READ_PAGE_REQ_func(void);
 extern void DSM_LRPC_WRITE_PAGE_REQ_func(void);
@@ -46,6 +43,12 @@ extern void DSM_LRPC_INVALIDATE_ACK_func(void);
 extern void DSM_LRPC_SEND_DIFFS_func(void);
 extern void DSM_LRPC_LOCK_func(void);
 extern void DSM_LRPC_UNLOCK_func(void);
+extern void DSM_LRPC_MULTIPLE_READ_PAGE_REQ_func(void);
+extern void DSM_LRPC_MULTIPLE_WRITE_PAGE_REQ_func(void);
+extern void DSM_LRPC_SEND_MULTIPLE_PAGES_READ_func(void);
+extern void DSM_LRPC_SEND_MULTIPLE_PAGES_WRITE_func(void);
+extern void DSM_LRPC_SEND_MULTIPLE_DIFFS_func(void);
+extern void DSM_LRPC_HBRC_DIFFS_func(void);
 
 void dsm_pm2_init_rpc()
 {
@@ -57,6 +60,12 @@ void dsm_pm2_init_rpc()
   pm2_rawrpc_register(&DSM_LRPC_SEND_DIFFS, DSM_LRPC_SEND_DIFFS_func);
   pm2_rawrpc_register(&DSM_LRPC_LOCK, DSM_LRPC_LOCK_func);
   pm2_rawrpc_register(&DSM_LRPC_UNLOCK, DSM_LRPC_UNLOCK_func);
+  pm2_rawrpc_register(&DSM_LRPC_MULTIPLE_READ_PAGE_REQ, DSM_LRPC_MULTIPLE_READ_PAGE_REQ_func);
+  pm2_rawrpc_register(&DSM_LRPC_MULTIPLE_WRITE_PAGE_REQ, DSM_LRPC_MULTIPLE_WRITE_PAGE_REQ_func);
+  pm2_rawrpc_register(&DSM_LRPC_SEND_MULTIPLE_PAGES_READ, DSM_LRPC_SEND_MULTIPLE_PAGES_READ_func);
+  pm2_rawrpc_register(&DSM_LRPC_SEND_MULTIPLE_PAGES_WRITE, DSM_LRPC_SEND_MULTIPLE_PAGES_WRITE_func);
+  pm2_rawrpc_register(&DSM_LRPC_SEND_MULTIPLE_DIFFS, DSM_LRPC_SEND_MULTIPLE_DIFFS_func);
+  pm2_rawrpc_register(&DSM_LRPC_HBRC_DIFFS, DSM_LRPC_HBRC_DIFFS_func);
 }
 
 

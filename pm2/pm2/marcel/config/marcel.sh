c675781283728fedf6a941b146e5b69ed3334e88
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:55:08 +0000

    rev 1.4 at 2000/06/07 15:48:17 GMT  omit-frame-pointer is back
    (Logical change 1.224)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2629 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/config/marcel.sh
+++ pm2/pm2/marcel/config/marcel.sh
@@ -2,6 +2,7 @@ if [ "${PM2_ARCH}" = RS6K_ARCH ]; then
   PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -mno-powerpc"
 fi
 PM2_MARCEL_LIBNAME=marcel
+PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -fomit-frame-pointer"
 if [ "$PM2_SYS" = SOLARIS_SYS ]; then
   PM2_MARCEL_LIBS="$PM2_MARCEL_LIBS -lrt"
 fi

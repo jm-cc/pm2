95e4b6c8623ba0cf77f9e27532c41b28f2ef7047
ens-lyon.fr!gantoniu <ens-lyon.fr!gantoniu@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 03:51:21 +0000

    rev 1.6 at 2000/10/11 12:10:37 GMT  *** empty log message ***
    2003/09/05 05:50:46+02:00 ens-lyon.fr!gantoniu
    rev 1.5 at 2000/10/11 12:07:17 GMT
     Added thread-level barriers (pm2_thread_barrier_t) + an example
    (pm2/pm2/examples/dsm/thread_sync.c).
    
    (Logical change 1.333)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@3627 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/pm2_sync.h
+++ pm2/pm2/pm2/include/pm2_sync.h
@@ -46,14 +46,33 @@ typedef struct {
 } pm2_barrier_t;
 
 
+typedef struct {
+  marcel_sem_t mutex, wait;
+  pm2_barrier_t node_barrier;
+  int local;
+  int nb;
+} pm2_thread_barrier_t;
+
+typedef struct {
+  int local;
+} pm2_thread_barrier_attr_t;
 
 
 void pm2_barrier_init_rpc();
 
 void pm2_sync_init(int myself, int confsize);
 
+
+/* The following two primitives need to be called by PM2 in a startup function,
+to allow for proper barrier initialization. */
 void pm2_barrier_init(pm2_barrier_t *bar);
 
+void pm2_thread_barrier_init(pm2_thread_barrier_t *bar, pm2_thread_barrier_attr_t *attr);
+
+/* Node-level barrier */
 void pm2_barrier(pm2_barrier_t *bar);
+
+/*Thread-level barrier */
+void pm2_thread_barrier(pm2_thread_barrier_t *bar);
 
 #endif

4b082c6bfa962af206edc286f65f9410e8f6b93a
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 01:12:22 +0000

    rev 1.1 at 2000/05/15 13:51:05 GMT  - Reorganisation des sources de Leonie
    (Logical change 1.182)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2260 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/leonie/include/leo_swann.h
@@ -0,0 +1,57 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ suppong documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+
+______________________________________________________________________________
+$Log: leo_swann.h,v $
+Revision 1.1  2000/05/15 13:51:05  oaumage
+- Reorganisation des sources de Leonie
+
+
+______________________________________________________________________________
+*/
+
+/*
+ * Leo_swann.h
+ * ===========
+ */ 
+
+#ifndef __LEO_SWANN_H
+#define __LEO_SWANN_H
+
+p_leo_swann_module_t
+leo_launch_swann_module(p_leonie_t             leonie,
+			char                  *cluster_id,
+			p_leo_clu_host_name_t  host_description);
+
+#endif /* __LEO_SWANN_H */

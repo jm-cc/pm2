a1c267bf00bd4a9305188d03f5725f1695f392ba
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:56:43 +0000

    rev 1.4 at 2001/01/03 11:05:25 GMT  - integration des headers du module de forwarding
    (Logical change 1.382)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4105 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/include/mad_channel_interface.h
+++ pm2/pm2/mad2/include/mad_channel_interface.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: mad_channel_interface.h,v $
+Revision 1.4  2001/01/03 11:05:25  oaumage
+- integration des headers du module de forwarding
+
 Revision 1.3  1999/12/15 17:31:19  oaumage
 Ajout de la commande de logging de CVS
 
@@ -52,9 +55,15 @@ ______________________________________________________________________________
  * Functions 
  * ---------
  */
+#ifdef MAD_FORWARDING
+p_mad_user_channel_t
+mad_open_channel(p_mad_madeleine_t   madeleine,
+                 char               *name);
+#else /* MAD_FORWARDING */
 p_mad_channel_t
 mad_open_channel(p_mad_madeleine_t   madeleine,
 		 mad_adapter_id_t    adapter);
+#endif /* MAD_FORWARDING */
 
 void
 mad_close_channels(p_mad_madeleine_t madeleine);

9a3cbf47ee0b94f7a44593052862d07fedf5d461
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:41:04 +0000

    rev 1.8 at 2002/10/28 16:52:03 GMT  dsm protocol has type 'dsm_proto_t'
    (Logical change 1.712)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8793 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/source/dsm_lock.c
+++ pm2/pm2/dsm/source/dsm_lock.c
@@ -53,7 +53,8 @@ void dsm_lock_init(dsm_lock_t *lock, dsm_lock_attr_t *attr)
   LOG_OUT();
 }
 
-void dsm_lock_attr_register_protocol(dsm_lock_attr_t *attr, int prot)
+
+void dsm_lock_attr_register_protocol(dsm_lock_attr_t *attr, dsm_proto_t prot)
 {
   if (attr->nb_prot >= _MAX_PROT_PER_LOCK)
     RAISE(CONSTRAINT_ERROR);
@@ -66,3 +67,4 @@ void dsm_lock_attr_init(dsm_lock_attr_t *attr)
   dsm_mutexattr_init(&attr->dsm_mutex_attr);
   attr->nb_prot = 0;
 }
+

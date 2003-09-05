dc2f18b8705785703620160ceff61c4fed5535d3
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 13:29:21 +0000

    rev 1.10 at 2001/07/09 13:23:26 GMT  - PM2/MadIII support
    (Logical change 1.587)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7908 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/include/tbx_types.h
+++ pm2/pm2/tbx/include/tbx_types.h
@@ -1,3 +1,7 @@
+/*! \file tbx_types.h
+ *  \brief TBX simple types definition.
+ *  This file defines TBX simple, general use types.
+ */
 
 /*
  * PM2: Parallel Multithreaded Machine
@@ -22,75 +26,162 @@
 #ifndef TBX_TYPES_H
 #define TBX_TYPES_H
 
-/*
- * General purpose types
- * :::::::::::::::::::::______________________________________________________
- */
 
-/*
- * Classical boolean type
- * ----------------------
+/*! \typedef tbx_bool_t
+ *  The boolean type.
+ *  \sa p_tbx_bool_t
  */
-typedef enum
+/*! \typedef p_tbx_bool_t
+ *  A pointer to a boolean object.
+ *  \sa tbx_bool_t
+ */
+/*! \enum e_tbx_bool
+ *  A boolean 
+ *  \sa tbx_bool_t, p_tbx_bool_t
+ */
+typedef enum e_tbx_bool
 {
-  tbx_false = 0,
-  tbx_true
+  tbx_false = 0, //!< condition is false
+  tbx_true       //!< condition is true
 } tbx_bool_t, *p_tbx_bool_t;
 
-/*
- * Flag type
- * ----------------------
+
+/*! \typedef tbx_flag_t
+ *  The general purpose flag type.
+ * \sa p_tbx_flag_t
  */
-typedef enum
+/*! \typedef p_tbx_flag_t
+ *  A pointer to a flag object.
+ * \sa tbx_flag_t
+ */
+/*! \enum e_tbx_flag
+ *  A flag value.
+ * \sa tbx_flag_t, p_tbx_flag_t
+ */
+typedef enum e_tbx_flag
 {
-  tbx_flag_clear = 0,
-  tbx_flag_set   = 1
+  tbx_flag_clear = 0, //!< flag is cleared
+  tbx_flag_set   = 1  //!< flag is set
 } tbx_flag_t, *p_tbx_flag_t;
 
-/*
- * Comparison type
- * ----------------------
+
+/*! \typedef tbx_cmp_t
+ *  The comparison result type.
+ * \sa p_tbx_cmp_t
  */
-typedef enum
+/*! \typedef p_tbx_cmp_t
+ *  A pointer to a comparison result object.
+ * \sa tbx_cmp_t
+ */
+/*! \enum e_tbx_cmp
+ *  A comparison function result value.
+ * \sa tbx_cmp_t, p_tbx_cmp_t
+ */
+typedef enum e_tbx_cmp
 {
-  tbx_cmp_eq =  0,  /* == */
-  tbx_cmp_gt =  1,  /*  > */
-  tbx_cmp_lt = -1   /*  < */
+  tbx_cmp_eq =  0, //!< objects are equal
+  tbx_cmp_gt =  1, //!< object 1 is greater than object 2
+  tbx_cmp_lt = -1  //!< object 1 is lower than object 2
 } tbx_cmp_t, *p_tbx_cmp_t;
 
-/*
- * Safe malloc related types
- * :::::::::::::::::::::::::__________________________________________________
+
+/*! \typedef tbx_align_t
+ *  A safe-malloc alignment value.
+ *  \sa p_tbx_align_t
+ */
+/*! \typedef p_tbx_align_t
+ *  A pointer to a safe-malloc alignment object.
+ *  \sa tbx_align_t
  */
 typedef unsigned long tbx_align_t, *p_tbx_align_t;
 
-/*
- * List management related types 
- * :::::::::::::::::::::::::::::______________________________________________
+
+/*! \typedef tbx_list_length_t
+ *  A linked list length.
+ *  \sa p_tbx_list_length_t
  */
-typedef int tbx_list_length_t,        *p_tbx_list_length_t;
+/*! \typedef tbx_list_length_t
+ *  A pointer to a linked list length object.
+ *  \sa tbx_list_length_t
+ */
+typedef int tbx_list_length_t, *p_tbx_list_length_t;
+
+
+/*! \typedef tbx_list_mark_position_t
+ *  A linked list mark position.
+ *  \sa p_tbx_list_mark_position_t
+ */
+/*! \typedef tbx_list_mark_position_t
+ *  A pointer to a linked list mark position object.
+ *  \sa tbx_list_mark_position_t
+ */
 typedef int tbx_list_mark_position_t, *p_tbx_list_mark_position_t;
 
-/*
- * SList management related types 
- * ::::::::::::::::::::::::::::::_____________________________________________
- */
-typedef int tbx_slist_index_t,   *p_tbx_slist_index_t;
-typedef int tbx_slist_length_t,  *p_tbx_slist_length_t;
-typedef int tbx_slist_offset_t,  *p_tbx_slist_offset_t;
-typedef tbx_bool_t (*p_tbx_slist_search_func_t)(void *ref_obj, void *obj);
-typedef tbx_cmp_t  (*p_tbx_slist_cmp_func_t)(void *ref_obj, void *obj);
-typedef void *     (*p_tbx_slist_dup_func_t)(void *object);
 
-/*
- * Darray management related types 
- * :::::::::::::::::::::::::::::::____________________________________________
+/*! \typedef tbx_slist_index_t
+ *  A slist index value.
+ *  \sa p_tbx_slist_index_t
+ */
+/*! \typedef p_tbx_slist_index_t
+ *  A pointer to a slist index object.
+ *  \sa tbx_slist_index_t
+ */
+typedef int tbx_slist_index_t, *p_tbx_slist_index_t;
+
+
+/*! \typedef tbx_slist_length_t
+ *  A slist index length.
+ *  \sa p_tbx_slist_length_t
+ */
+/*! \typedef p_tbx_slist_length_t
+ *  A pointer to a slist index length object.
+ *  \sa tbx_slist_length_t
+ */
+typedef int tbx_slist_length_t, *p_tbx_slist_length_t;
+
+
+/*! \typedef tbx_slist_offset_t
+ *  A slist offset.
+ *  \sa p_tbx_slist_offset_t
+ */
+/*! \typedef p_tbx_slist_offset_t
+ *  A pointer to a slist offset object.
+ *  \sa tbx_slist_offset_t
+ */
+typedef int tbx_slist_offset_t, *p_tbx_slist_offset_t;
+
+
+/*! \typedef p_tbx_slist_search_func_t
+ *  A slist search function.
+ */
+typedef tbx_bool_t (*p_tbx_slist_search_func_t)(void *ref_obj, void *obj);
+
+
+/*! \typedef p_tbx_slist_cmp_func_t
+ *  A slist comparison function.
+ */
+typedef tbx_cmp_t (*p_tbx_slist_cmp_func_t)(void *ref_obj, void *obj);
+
+
+/*! \typedef p_tbx_slist_dup_func_t
+ *  A slist duplication function.
+ */
+typedef void * (*p_tbx_slist_dup_func_t)(void *object);
+
+
+/*! \typedef tbx_darray_index_t
+ *  A darray index value.
+ *  \sa p_tbx_darray_index_t
+ */
+/*! \typedef p_tbx_darray_index_t
+ *  A pointer to a darray index object.
+ *  \sa tbx_darray_index_t
  */
 typedef int tbx_darray_index_t, *p_tbx_darray_index_t;
 
-/*
- * Destructor function types 
- * :::::::::::::::::::::::::__________________________________________________
+
+/*! \typedef p_tbx_specific_dest_func_t
+ *  A \em specific field destructor function.
  */
 typedef void (*p_tbx_specific_dest_func_t)(void *specific);
 

f9df2170d43aa3820e7b0f651f0cb0b9981c12e4
ens-lyon.fr!cmenier <ens-lyon.fr!cmenier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 10:56:54 +0000

    rev 1.4 at 2001/04/19 12:12:33 GMT  *** empty log message ***
    (Logical change 1.508)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7186 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/sigmund/supertrace/tracebuffer.c
+++ pm2/pm2/sigmund/supertrace/tracebuffer.c
@@ -10,15 +10,7 @@
 
 #include "fkt.h"
 
-#define FUT_SETUP_CODE				0x210
-#define FUT_KEYCHANGE_CODE			0x211
-#define FUT_RESET_CODE				0x212
-#define FUT_CALIBRATE0_CODE			0x220
-#define FUT_CALIBRATE1_CODE			0x221
-#define FUT_CALIBRATE2_CODE			0x222
-#define FUT_SWITCH_TO_CODE			0x230
-#define FUT_MAIN_ENTRY_CODE			0x240
-#define FUT_MAIN_EXIT_CODE			0x340
+#include "fut_code.h"
 
 #define NB_MAX_PROC  16
 

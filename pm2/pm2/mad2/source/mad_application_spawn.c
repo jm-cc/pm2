03d06af6f9a50ac80be2a4bb95a632e445ba0ca8
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 09:51:19 +0000

    rev 1.13 at 2001/03/26 08:45:39 GMT  - mise a jour application spawn
    (Logical change 1.464)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6783 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad2/source/mad_application_spawn.c
+++ pm2/pm2/mad2/source/mad_application_spawn.c
@@ -49,77 +49,8 @@
  * Objet Madeleine
  * ---------------
  */
-static mad_madeleine_t main_madeleine;
 static p_mad_madeleine_t madeleine;
-/*
- * Initialisation des drivers
- * --------------------------
- */
-static void 
-mad_driver_init(p_mad_madeleine_t madeleine)
-{
-  mad_driver_id_t drv;
-    
-  LOG_IN();
-  for (drv = 0;
-       drv < madeleine->nb_driver;
-       drv++)
-    {
-      p_mad_driver_t driver;
-      
-      driver = &(madeleine->driver[drv]);      
-      driver->specific = NULL;
-      driver->interface.driver_init(driver);
-    }
-  LOG_OUT();
-}
 
-/*
- * Lecture du fichier de configuration
- * -----------------------------------
- */
-static void
-mad_read_conf(p_mad_configuration_t   configuration,
-	      char                   *configuration_file)
-{
-  /* configuration retrieval */
-  FILE *f;
-  int i;
-
-  LOG_IN();
-  f = fopen(configuration_file, "r");
-  if(f == NULL)
-    {
-      perror("configuration file");
-      exit(1);
-    }
-
-    {
-      char commande[MAX_ARG_STR_LEN];
-      int ret;
-      
-      sprintf(commande, "exit `cat %s | wc -w`", configuration_file);
-
-      ret = system(commande);
-
-      configuration->size = WEXITSTATUS(ret);
-    }
-
-  configuration->host_name = TBX_MALLOC(configuration->size * sizeof(char *));
-  CTRL_ALLOC(configuration->host_name);
-
-  for (i = 0;
-       i < configuration->size;
-       i++)
-    {
-      configuration->host_name[i] = TBX_MALLOC(MAX_HOSTNAME_LEN);
-      CTRL_ALLOC(configuration->host_name[i]);
-      fscanf(f, "%s", configuration->host_name[i]);
-    }
-
-  fclose(f);
-  LOG_OUT();
-}
 
 /*
  * Generation de l'URL de connexion
@@ -169,37 +100,12 @@ mad_generate_url(p_mad_madeleine_t madeleine)
 }
 
 /*
- * Premiere phase d'initialisation de Madeleine
- * --------------------------------------------
- */
-char *
-old_mad_pre_init(p_mad_adapter_set_t adapter_set)
-{
-  p_mad_madeleine_t  madeleine  = &main_madeleine;
-  int                dummy_argc = 1;
-  char              *dummy_argv[] = { NULL } ;
-
-  madeleine->nb_channel = 0;
-  TBX_INIT_SHARED(madeleine);
-  mad_managers_init(&dummy_argc, dummy_argv);
-  LOG_IN(); /* After pm2debug_init ... */
-  mad_driver_fill(madeleine);
-  mad_adapter_fill(madeleine, adapter_set);  
-
-  mad_driver_init(madeleine);
-  mad_adapter_init(madeleine);
-  LOG_OUT();
-  return mad_generate_url(madeleine);
-}
-
-/*
  * Interpretation de l'URL de connexion
  * ------------------------------------
  */
 static void
 mad_parse_url(p_mad_madeleine_t  madeleine,
-	      char              *url,
-	      p_tbx_bool_t       master)
+	      char              *url)
 {
   mad_adapter_id_t  ad = 0;
   char             *tag;
@@ -209,11 +115,9 @@ mad_parse_url(p_mad_madeleine_t  madeleine,
   LOG_IN();
   if (!url)
     {
-      *master = tbx_true;
       LOG_OUT();
       return;
     }
-  *master = tbx_false;
 
   c = strchr(url, '?');
   if (!c)
@@ -264,94 +168,11 @@ mad_parse_url(p_mad_madeleine_t  madeleine,
   LOG_OUT();
 }
 
-
-/*
- * mad_init
- * --------
- */
-p_mad_madeleine_t
-old_mad_init(
-	 ntbx_host_id_t  rank,
-	 char           *configuration_file,
-	 char           *url
-	 )
-{
-  p_mad_madeleine_t          madeleine       = &(main_madeleine);
-  p_mad_configuration_t      configuration   =
-    &(madeleine->configuration);
-  tbx_bool_t                 master          = tbx_false;
-  tbx_bool_t                 slave           = tbx_false;
-  char                       conf_file[128];
-
-  LOG_IN(); 
-  if (!configuration_file)
-    {    
-      if (getenv("PM2_CONF_FILE"))
-	{
-	  configuration_file = conf_file;
-	  sprintf(configuration_file, "%s", getenv("PM2_CONF_FILE"));
-	}
-      else
-	FAILURE("configuration file not specified");
-    }  
-
-  mad_read_conf(configuration, configuration_file);
-  mad_parse_url(madeleine, url, &master);
-  slave = !master;
-  if (master)
-    {
-      if (rank == -1)
-	{
-	  rank = 0;
-	}
-      else if (rank != 0)
-	FAILURE("invalid rank specified for master node");
-    }
-  else
-    {
-      if (rank == -1)
-	{
-	  int   i         = 1;
-	  char *host_name = NULL;
-	  
-	  host_name = TBX_MALLOC(MAX_HOSTNAME_LEN);
-	  CTRL_ALLOC(host_name);  
-
-	  SYSCALL(gethostname(host_name, MAX_HOSTNAME_LEN));
-	  
-	  while(i < configuration->size)
-	    {
-	      if (!strcmp(configuration->host_name[i],
-			  host_name))
-		break;
-	      i++;
-	    }
-
-	  if (i >= configuration->size)
-	    FAILURE("unable to determine rank from host name");
-
-	  rank = i;
-	}
-      else if (rank <= 0)
-	FAILURE("invalid rank specified for slave node");
-    }
-
-  configuration->local_host_id = rank;
-
-  mad_adapter_configuration_init(madeleine);
-  tbx_list_init(&(madeleine->channel));
-
-  LOG_OUT();
-  return madeleine;
-}
-
-
 char *
 mad_pre_init(p_mad_adapter_set_t adapter_set)
 {
-  p_mad_madeleine_t  madeleine     =    NULL;
-  int                dummy_argc    =       1;
-  char              *dummy_argv[] = { NULL } ;
+  int   dummy_argc    =       1;
+  char *dummy_argv[] = { NULL } ;
 
   tbx_init(dummy_argc, dummy_argv);
   ntbx_init(dummy_argc, dummy_argv);
@@ -370,25 +191,56 @@ mad_init(
 	 char           *url
 	 )
 {
-  int                dummy_argc    =        1;
-  char              *dummy_argv[] = { NULL };
+  int   dummy_argc   =        1;
+  char *dummy_argv[] = { NULL };
 
-  madeleine->configuration->local_host_id = rank;
+ 
+  if (configuration_file)
+    {
+      if (madeleine->settings->configuration_file)
+	{
+	  TBX_FREE(madeleine->settings->configuration_file);
+	  madeleine->settings->configuration_file = NULL;
+	}
 
-  madeleine->settings->configuration_file =
-    TBX_MALLOC(strlen(configuration_file) + 1);
-  CTRL_ALLOC(madeleine->settings->configuration_file);
-  strcpy(madeleine->settings->configuration_file, configuration_file);
+      madeleine->settings->configuration_file =
+	TBX_MALLOC(strlen(configuration_file) + 1);
+      CTRL_ALLOC(madeleine->settings->configuration_file);
+      strcpy(madeleine->settings->configuration_file, configuration_file);
+    }
 
-  madeleine->settings->url = TBX_MALLOC(strlen(url) + 1);
-  CTRL_ALLOC(madeleine->settings->url);
-  strcpy(madeleine->settings->url, url);
+  if (url)
+    {
+      madeleine->settings->url = TBX_MALLOC(strlen(url) + 1);
+      CTRL_ALLOC(madeleine->settings->url);
+      strcpy(madeleine->settings->url, url);
 
-  mad_output_redirection_init(madeleine, *dummy_argc, dummy_argv);
-  mad_configuration_init(madeleine, *dummy_argc, dummy_argv);
-  mad_parse_url(madeleine, *dummy_argc, dummy_argv);
-  mad_connect(madeleine, *dummy_argc, dummy_argv);
+      if (rank < 0)
+	{
+	  madeleine->configuration->local_host_id = 1;
+	}
+      else
+	{
+	  madeleine->configuration->local_host_id = rank;
+	}
+    }
+  else
+    {
+      if (rank < 0)
+	{
+	  madeleine->configuration->local_host_id = 0;
+	}
+      else
+	{
+	  madeleine->configuration->local_host_id = rank;
+	}
+    }
+  mad_output_redirection_init(madeleine, dummy_argc, dummy_argv);
+  mad_configuration_init(madeleine, dummy_argc, dummy_argv);
+  mad_parse_url(madeleine, url);
+  mad_connect(madeleine, dummy_argc, dummy_argv);
   
+  return madeleine;
 }
 
 

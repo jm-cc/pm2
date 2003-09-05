ebaf58225ad711cde8700ca8e1cb5443a5f9d8d3
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.6 at 2001/06/11 07:50:23 GMT  - leonie, import de MadIII
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7618 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leoparse/include/leoparse_types.h
+++ pm2/pm2/leoparse/include/leoparse_types.h
@@ -1,5 +1,4 @@
 /*
-/*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
  *
@@ -14,12 +13,13 @@
  * General Public License for more details.
  */
 
+/*
  * leoparse_types.h
  * ----------------
  */
 
-#ifndef __LEOPARSE_TYPES_H
-#define __LEOPARSE_TYPES_H
+#ifndef LEOPARSE_TYPES_H
+#define LEOPARSE_TYPES_H
 
 typedef enum e_leoparse_entry_type
 {
@@ -36,12 +36,20 @@ typedef struct s_leoparse_htable_entry
   leoparse_entry_type_t  type;
 } leoparse_htable_entry_t;
 
+typedef struct s_leoparse_range
+{
+  int begin;
+  int end;
+} leoparse_range_t;
+
 typedef enum e_leoparse_object_type
 {
   leoparse_o_undefined = 0,
   leoparse_o_id,
   leoparse_o_string,
   leoparse_o_htable,
+  leoparse_o_integer,
+  leoparse_o_range,
 } leoparse_object_type_t;
 
 typedef struct s_leoparse_object
@@ -50,6 +58,23 @@ typedef struct s_leoparse_object
   p_tbx_htable_t          htable;
   char                   *string;
   char                   *id;
+  int                     val;
+  p_leoparse_range_t      range;
+  p_leoparse_modifier_t   modifier;
 } leoparse_object_t;
 
-#endif /* __LEOPARSE_TYPES_H */
+typedef enum e_leoparse_modifier_type
+{
+  leoparse_m_undefined = 0,
+  leoparse_m_sbracket,
+  leoparse_m_parenthesis,
+} leoparse_modifier_type_t;
+
+typedef struct s_leoparse_modifier
+{
+  leoparse_modifier_type_t type;
+  p_tbx_slist_t            sbracket;
+  p_tbx_slist_t            parenthesis;
+} leoparse_modifier_t;
+
+#endif /* LEOPARSE_TYPES_H */

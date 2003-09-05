0cb1d26955bfaf73036e04e4101759cf88e0e3bd
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.2 at 2001/06/11 16:32:50 GMT  - signaux
    2003/09/05 14:25:39+02:00 ens-lyon.fr!oaumage
    rev 1.1 at 2001/06/11 16:06:52 GMT
     - synchro
    
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7627 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/leonie/source/leo_status.c
@@ -0,0 +1,85 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * leo_status.c
+ * ============
+ */ 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "leonie.h"
+
+/*
+ * Static variables
+ * ================
+ */
+
+/*
+ * Usage
+ * -----
+ */
+static const char *usage_str = "leonie <net_conf_file> <app_conf_file>";
+
+void
+leo_usage(void)
+{
+  fprintf(stderr, "Usage: %s\n", usage_str);
+  exit(EXIT_FAILURE);
+}
+
+void
+leonie_failure_cleanup(void)
+{
+  pid_t pid = 0;
+  
+  LOG_IN();
+  pid = getpid();
+  kill(-pid, SIGTERM);
+  LOG_OUT();
+}
+
+void
+leo_terminate(const char *msg)
+{
+  fprintf(stderr, "error: %s\n", msg);
+  leonie_failure_cleanup();
+  exit(EXIT_FAILURE);
+}
+
+void
+leo_error(const char *command) 
+{
+  perror(command);
+  leonie_failure_cleanup();
+  exit(EXIT_FAILURE);
+}
+
+void
+leonie_processes_cleanup(void)
+{
+  pid_t pid = 0;
+  
+  LOG_IN();
+  pid = getpid();
+  kill(-pid, SIGHUP);
+  LOG_OUT();
+}
+

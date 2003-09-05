a5a0c7acb6ff620de72ed598d0dafb1b198f2685
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:54:45 +0000

    rev 1.3 at 2000/12/19 16:57:46 GMT  - finalisation de leoparse - exemples pour leoparse - modification des macros de logging - version typesafe de certaines macros - finalisation des tables de hachage - finalisation des listes de recherche
    (Logical change 1.379)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4093 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leoparse/examples/leoparse_test.c
+++ pm2/pm2/leoparse/examples/leoparse_test.c
@@ -34,6 +34,14 @@
 
 ______________________________________________________________________________
 $Log: leoparse_test.c,v $
+Revision 1.3  2000/12/19 16:57:46  oaumage
+- finalisation de leoparse
+- exemples pour leoparse
+- modification des macros de logging
+- version typesafe de certaines macros
+- finalisation des tables de hachage
+- finalisation des listes de recherche
+
 Revision 1.2  2000/11/16 13:24:03  oaumage
 - mise a jour initialisation
 
@@ -57,12 +65,69 @@ usage(void)
   exit(1);
 }
 
+void
+process_emails(p_tbx_htable_t emails)
+{
+  char *s = NULL;
+
+  s = leoparse_read_string(emails, "main");
+  DISP_STR("first email", s);
+
+  s = leoparse_read_string(emails, "second");
+  DISP_STR("second email", s);
+}
+
+void
+process_data(p_tbx_htable_t employer)
+{
+  char *s = NULL;
+
+  s = leoparse_read_id(employer, "first_name");
+  DISP_STR("first name", s);
+
+  s = leoparse_read_id(employer, "last_name");
+  DISP_STR("last name", s);
+
+  s = leoparse_read_string(employer, "birthdate");
+  DISP_STR("birthdate", s);
+
+  process_emails(leoparse_read_htable(employer, "emails"));
+}
+
+void
+process(p_tbx_htable_t file)
+{
+  p_tbx_slist_t  data = NULL;
+  char          *s    = NULL;
+
+  data = leoparse_read_slist(file, "data");
+
+  s = leoparse_read_id(file, "name");
+  DISP_STR("name", s);
+  
+  if (!tbx_slist_is_nil(data))
+    {
+      tbx_slist_ref_to_head(data);
+
+      do
+	{
+	  p_leoparse_object_t object = NULL;
+	    
+	  object = tbx_slist_ref_get(data);
+	  process_data(leoparse_get_htable(object));
+	}
+      while (tbx_slist_ref_forward(data));
+    }
+}
+
 int
 main(int    argc,
      char **argv)
 {
   p_tbx_htable_t result = NULL;
 
+  common_init(&argc, argv);
+  
   tbx_init(argc, argv);
   leoparse_init(argc, argv);
 
@@ -75,7 +140,9 @@ main(int    argc,
 
   result = leoparse_parse_local_file(*argv);
   printf("Parsing succeeded\n");
-  
+
+  process(leoparse_read_htable(result, "file"));
+    
   return 0;
 }
 

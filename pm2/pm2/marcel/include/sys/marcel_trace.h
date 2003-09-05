dbf58a18e7bc24fd6885ec75e02a0c167c286b58
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 00:04:52 +0000

    rev 1.4 at 2000/04/11 09:07:18 GMT  Merged the "reorganisation" development branch.
    (Logical change 1.138)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@1597 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_trace.h
+++ pm2/pm2/marcel/include/sys/marcel_trace.h
@@ -34,6 +34,28 @@
 
 ______________________________________________________________________________
 $Log: marcel_trace.h,v $
+Revision 1.4  2000/04/11 09:07:18  rnamyst
+Merged the "reorganisation" development branch.
+
+Revision 1.3.2.6  2000/03/31 18:38:38  vdanjean
+Activation mono OK
+
+Revision 1.3.2.5  2000/03/31 11:20:29  vdanjean
+debug ACT
+
+Revision 1.3.2.4  2000/03/31 09:39:47  vdanjean
+debug ACT
+
+Revision 1.3.2.3  2000/03/30 16:57:28  rnamyst
+Introduced TOP_STACK_FREE_AREA...
+
+Revision 1.3.2.2  2000/03/29 14:24:39  rnamyst
+Added the marcel_stdio.h that provides the marcel_printf functions.
+=> MTRACE now uses marcel_fprintf.
+
+Revision 1.3.2.1  2000/03/15 15:54:55  vdanjean
+réorganisation de marcel : commit pour CVS
+
 Revision 1.3  2000/01/31 15:56:50  oaumage
 - ajout du Log CVS
 
@@ -46,19 +68,23 @@ ______________________________________________________________________________
 
 #ifdef MAR_TRACE
 
+#include "marcel_stdio.h"
+
 #define MTRACE(msg, pid) \
   (__mar_trace ? \
-    (void)fprintf(stderr, \
-            "[P%02d][%-11s:%3ld (pid=%p)." \
+    (void)marcel_fprintf(stderr, \
+            "[P%02d][%-11s:%3ld (pid=%p:%X)." \
             " %3d A,%3d S,%3d B,%3d F /%3d T]\n", \
             ((pid)->lwp ? (pid)->lwp->number : -1), \
-            msg, (pid)->number, (pid), \
+            msg, (pid)->number, (pid), (pid)->special_flags, \
             marcel_activethreads(), \
             marcel_sleepingthreads(), \
             marcel_blockedthreads(), \
             marcel_frozenthreads(), \
             marcel_nbthreads() + 1) \
    : (void)0)
+
+
 
 #else
 

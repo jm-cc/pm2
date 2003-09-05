562456d8600e07db585f6477a9c174df3dadeac1
ens-lyon.fr!slacour <ens-lyon.fr!slacour@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 16:13:00 +0000

    rev 1.3 at 2002/10/16 15:53:44 GMT  working on token locks
    (Logical change 1.698)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8707 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/hierarch_topology.h
+++ pm2/pm2/dsm/include/hierarch_topology.h
@@ -1,9 +1,12 @@
 
 /*
- * CVS Id: $Id: hierarch_topology.h,v 1.2 2002/10/14 15:42:05 slacour Exp $
+ * CVS Id: $Id: hierarch_topology.h,v 1.3 2002/10/16 15:53:44 slacour Exp $
  */
 
 #include "dsm_const.h"   /* dsm_node_t */
+
+
+const unsigned int NO_COLOR;
 
 
 /* initialization function of the topology, called from dsm_pm2_init() */
@@ -21,4 +24,10 @@ topology_get_cluster_number (void);
 /* get the color of a node to determine to what cluster it belongs */
 extern unsigned int
 topology_get_cluster_color (const dsm_node_t);
+
+/* set the colors of the processes, depending on what cluster they
+ * belong to: 2 processes with the same color belong to the same
+ * cluster */
+extern int
+topology_set_cluster_colors (const unsigned int * const);
 

04de9de1bd7102c84ce1f80be757666e28368d4c
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 05:53:14 +0000

    rev 1.8 at 2001/01/30 17:35:44 GMT  Ouch: just removed the old PM2-copyright header from _all_ files.
    (Logical change 1.392)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4327 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/dsm/include/dsm_comm.h
+++ pm2/pm2/dsm/include/dsm_comm.h
@@ -1,37 +1,3 @@
-/*
-                      PM2 HIGH-PERF/ISOMALLOC
-           High Performance Parallel Multithreaded Machine
-                           version 2.0
-
-             Gabriel Antoniu, Luc Bouge, Christian Perez,
-                Jean-Francois Mehaut, Raymond Namyst
-
-            Laboratoire de l'Informatique du Parallelisme
-                        UMR 8512 CNRS-INRIA
-                 Ecole Normale Superieure de Lyon
-
-                      External Contributors:
-                 Yves Denneulin (LMC - Grenoble),
-                 Benoit Planquelle (LIFL - Lille)
-
-                    1998 All Rights Reserved
-
-
-                             NOTICE
-
- Permission to use, copy, modify, and distribute this software and
- its documentation for any purpose and without fee is hereby granted
- provided that the above copyright notice appear in all copies and
- that both the copyright notice and this permission notice appear in
- supporting documentation.
-
- Neither the institutions (Ecole Normale Superieure de Lyon,
- Laboratoire de L'informatique du Parallelisme, Universite des
- Sciences et Technologies de Lille, Laboratoire d'Informatique
- Fondamentale de Lille), nor the Authors make any representations
- about the suitability of this software for any purpose. This
- software is provided ``as is'' without express or implied warranty.
-*/
 
 #ifndef DSM_COMM_IS_DEF
 #define DSM_COMM_IS_DEF

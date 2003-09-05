20a3f3c066bc3d2aa4977be83e2ebea0c0db8734
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 13:29:21 +0000

    rev 1.4 at 2001/07/09 13:23:45 GMT  - PM2/MadIII support
    (Logical change 1.587)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7932 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/tbx/source/tbx_string_management.c
+++ pm2/pm2/tbx/source/tbx_string_management.c
@@ -1,3 +1,10 @@
+/*! \file tbx_string_management.c
+ *  \brief TBX string object management routines.
+ *
+ *  This file contains the TBX string object management functions.
+ * 
+ */
+
 /*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)

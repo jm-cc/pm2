1e9d53aef4ab2c183159f9b59b3841a103f110f0
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 02:14:01 +0000

    rev 1.1 at 2000/06/15 08:52:09 GMT  Added the argv test program
    (Logical change 1.247)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@2793 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/pm2/examples/simple/argv.c
@@ -0,0 +1,53 @@
+/*
+                      PM2 HIGH-PERF/ISOMALLOC
+           High Performance Parallel Multithreaded Machine
+                           version 3.0
+
+     Gabriel Antoniu, Olivier Aumage, Luc Bouge, Vincent Danjean,
+       Christian Perez, Jean-Francois Mehaut, Raymond Namyst
+
+            Laboratoire de l'Informatique du Parallelisme
+                        UMR 5668 CNRS-INRIA
+                 Ecole Normale Superieure de Lyon
+
+                      External Contributors:
+                 Yves Denneulin (LMC - Grenoble),
+                 Benoit Planquelle (LIFL - Lille)
+
+                    1998 All Rights Reserved
+
+
+                             NOTICE
+
+ Permission to use, copy, modify, and distribute this software and
+ its documentation for any purpose and without fee is hereby granted
+ provided that the above copyright notice appear in all copies and
+ that both the copyright notice and this permission notice appear in
+ supporting documentation.
+
+ Neither the institutions (Ecole Normale Superieure de Lyon,
+ Laboratoire de L'informatique du Parallelisme, Universite des
+ Sciences et Technologies de Lille, Laboratoire d'Informatique
+ Fondamentale de Lille), nor the Authors make any representations
+ about the suitability of this software for any purpose. This
+ software is provided ``as is'' without express or implied warranty.
+*/
+
+#include "pm2.h"
+
+int pm2_main(int argc, char **argv)
+{
+  int i;
+
+  pm2_init(&argc, argv);
+
+  printf("argc = %d\n", argc);
+  for(i=0; i<argc; i++)
+    printf("argv[%d] = <%s>\n", i, argv[i]);
+
+  if(pm2_self() == 0)
+    pm2_halt();
+
+  pm2_exit();
+  return 0;
+}

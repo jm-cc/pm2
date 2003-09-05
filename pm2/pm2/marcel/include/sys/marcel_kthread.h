08fd515dd98bef6df42aceaddbbbdf37476de7db
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 09:46:41 +0000

    rev 1.2 at 2001/03/23 22:15:42 GMT  The option 'DO_NOT_USE_POSIX_THREADS' is OK under Linux.
    2003/09/05 11:45:56+02:00 ens-lyon.fr!rnamyst
    rev 1.1 at 2001/03/23 17:11:20 GMT
     Added the option 'DO_NOT_USE_POSIX_THREADS'. It is not yet usable. Also re-organized few options under marcel/config + extended the pm2-flavor script to source pre & post scripts.
    
    (Logical change 1.462)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@6749 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/sys/marcel_kthread.h
@@ -0,0 +1,67 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef MARCEL_KERNEL_THREADS_IS_DEF
+#define MARCEL_KERNEL_THREADS_IS_DEF
+
+#include "sys/marcel_flags.h"
+
+#ifdef MA__SMP
+
+typedef void * (*marcel_kthread_func_t)(void *arg);
+
+#ifdef MARCEL_DONT_USE_POSIX_THREADS
+
+#ifdef SOLARIS_SYS
+
+#include <thread.h>
+typedef thread_t marcel_kthread_t;
+
+#elif LINUX_SYS
+
+#include <sys/types.h>
+#include <unistd.h>
+typedef pid_t marcel_kthread_t;
+
+#else
+
+#error CANNOT AVOID USING PTHREADS ON THIS ARCHITECTURE. SORRY.
+
+#endif
+
+#else // MARCEL_DONT_USE_POSIX_THREADS
+
+#include <pthread.h>
+
+typedef pthread_t marcel_kthread_t;
+
+#endif
+
+void marcel_kthread_create(marcel_kthread_t *pid,
+			   marcel_kthread_func_t func, void *arg);
+
+void marcel_kthread_join(marcel_kthread_t pid);
+
+void marcel_kthread_exit(void *retval);
+
+marcel_kthread_t marcel_kthread_self(void);
+
+void marcel_kthread_sigmask(int how, sigset_t *newmask, sigset_t *oldmask);
+
+#endif // MA__SMP
+
+#endif
+
---
+++ pm2/pm2/marcel/source/marcel_kthread.c
@@ -0,0 +1,118 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "sys/marcel_kthread.h"
+
+#ifdef MA__SMP
+
+#ifdef MARCEL_DONT_USE_POSIX_THREADS
+
+#ifdef LINUX_SYS
+
+#include <sched.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define __STACK_SIZE  (1024 * 1024)
+
+// WARNING: stack is never freed. That's not a problem since kernel
+// threads only terminate at the end of the program, but...
+void marcel_kthread_create(marcel_kthread_t *pid,
+			   marcel_kthread_func_t func, void *arg)
+{
+  void *stack;
+
+  //stack = mmap(NULL, __STACK_SIZE, PROT_READ | PROT_WRITE,
+  //	       MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS | MAP_GROWSDOWN,
+  //	       -1, 0);
+  stack = malloc(__STACK_SIZE);
+
+  *pid = clone((int (*)(void *))func, (stack + __STACK_SIZE - 64),
+	       CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
+	       SIGCHLD, arg);
+}
+
+void marcel_kthread_join(marcel_kthread_t pid)
+{
+  waitpid(pid, NULL, 0);
+}
+
+void marcel_kthread_exit(void *retval)
+{
+  _exit((int)retval);
+}
+
+marcel_kthread_t marcel_kthread_self(void)
+{
+  return getpid();
+}
+
+void marcel_kthread_sigmask(int how, sigset_t *newmask, sigset_t *oldmask)
+{
+  sigprocmask(how, newmask, oldmask);
+}
+
+#else
+
+#error NOT YET IMPLEMENTED
+
+#endif
+
+#else
+
+#include <errno.h>
+
+void marcel_kthread_create(marcel_kthread_t *pid,
+			   marcel_kthread_func_t func, void *arg)
+{
+  pthread_attr_t attr;
+
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+  pthread_create(pid, &attr, func, arg);
+}
+
+void marcel_kthread_join(marcel_kthread_t pid)
+{
+  int cc;
+
+  do {
+    cc = pthread_join(pid, NULL);
+  } while(cc == -1 && errno == EINTR);
+}
+
+void marcel_kthread_exit(void *retval)
+{
+  pthread_exit(retval);
+}
+
+marcel_kthread_t marcel_kthread_self(void)
+{
+  return pthread_self();
+}
+
+void marcel_kthread_sigmask(int how, sigset_t *newmask, sigset_t *oldmask)
+{
+  pthread_sigmask(how, newmask, oldmask);
+}
+
+#endif
+
+#endif // MA__SMP

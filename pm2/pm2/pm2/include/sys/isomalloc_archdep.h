57a96ffcd4b4f10a4eb985dc7819c0315509a256
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 04:59:19 +0000

    rev 1.6 at 2001/01/03 14:12:27 GMT  Added support for Win2K. For now, only Marcel is available under Cygwin...
    (Logical change 1.383)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@4119 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/pm2/include/sys/isomalloc_archdep.h
+++ pm2/pm2/pm2/include/sys/isomalloc_archdep.h
@@ -34,6 +34,9 @@
 
 ______________________________________________________________________________
 $Log: isomalloc_archdep.h,v $
+Revision 1.6  2001/01/03 14:12:27  rnamyst
+Added support for Win2K. For now, only Marcel is available under Cygwin...
+
 Revision 1.5  2000/09/22 09:17:29  gantoniu
 Added double mapping.
 
@@ -74,22 +77,27 @@ ______________________________________________________________________________
 #ifndef ISOMALLOC_ARCHDEP_IS_DEF
 #define ISOMALLOC_ARCHDEP_IS_DEF
 
-#define ISOADDR_PAGES (128*1024)
-#define DYN_DSM_AREA_SIZE (ISOADDR_PAGES * 4096)
+#define ISOADDR_PAGES                 (128*1024)
 #define THREAD_SLOT_SIZE              0x10000 /* 64 Ko */
+#define SLOT_AREA_TOP                 (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+
+#ifdef DSM
+#define DYN_DSM_AREA_SIZE             (ISOADDR_PAGES * 4096)
+#else
+#define DYN_DSM_AREA_SIZE             0
+#endif
 
 #if defined(SOLARIS_SYS) && defined(SPARC_ARCH)
+
 extern int __zero_fd;
-#define ISOADDR_AREA_TOP          0xe0000000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0xe0000000
 #define IS_ON_MAIN_STACK(sp)   ((sp) > ISOADDR_AREA_TOP)
 #define FILE_TO_MAP            __zero_fd
 #define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED)
 
 #elif defined(LINUX_SYS) && defined(X86_ARCH)
 
-#define ISOADDR_AREA_TOP          0x40000000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0x40000000
 #define MAIN_STACK_BOT         0xa0000000
 #define IS_ON_MAIN_STACK(sp)   ((sp) > MAIN_STACK_BOT)
 #define FILE_TO_MAP            -1
@@ -97,24 +105,22 @@ extern int __zero_fd;
 
 #elif defined(LINUX_SYS) && defined(PPC_ARCH)
 
-#define ISOADDR_AREA_TOP          0x30000000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0x30000000
 #define FILE_TO_MAP            -1
 #define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS)
 
 #elif defined(SOLARIS_SYS) && defined(X86_ARCH)
+
 extern int __zero_fd;
 extern void marcel_init(int *argc, char **argv);
-#define ISOADDR_AREA_TOP          0xafff0000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0xafff0000
 #define IS_ON_MAIN_STACK(sp)   ((sp) < (unsigned long)marcel_init)
 #define FILE_TO_MAP            __zero_fd
 #define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED)
 
 #elif defined(FREEBSD_SYS) && defined(X86_ARCH)
 
-#define ISOADDR_AREA_TOP          0x40000000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0x40000000
 #define MAIN_STACK_BOT         0xa0000000
 #define IS_ON_MAIN_STACK(sp)   ((sp) > MAIN_STACK_BOT)
 #define FILE_TO_MAP            __zero_fd
@@ -122,18 +128,31 @@ extern void marcel_init(int *argc, char **argv);
 
 #elif defined(AIX_SYS) && defined(RS6K_ARCH)
 
-#define ISOADDR_AREA_TOP          0xcfff0000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0xcfff0000
 #define FILE_TO_MAP            -1
 #define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS)
 
 #elif defined(IRIX_SYS) && defined(MIPS_ARCH)
+
 extern int __zero_fd;
-#define ISOADDR_AREA_TOP          0x40000000
-#define SLOT_AREA_TOP  (ISOADDR_AREA_TOP - DYN_DSM_AREA_SIZE)
+#define ISOADDR_AREA_TOP       0x40000000
 #define FILE_TO_MAP            __zero_fd
 #define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED)
 #define IS_ON_MAIN_STACK(sp)   ((sp) > SLOT_AREA_TOP)
+
+#elif defined(WIN_SYS) || defined(X86_ARCH)
+
+extern void *ISOADDR_AREA_TOP;
+#define MAIN_STACK_BOT         (ISOADDR_AREA_TOP)
+#define ISOADDR_AREA_SIZE      (512 * SLOT_SIZE)
+#define FILE_TO_MAP            -1
+#define IS_ON_MAIN_STACK(sp)   ((sp) > MAIN_STACK_BOT)
+#define MMAP_MASK              (MAP_PRIVATE | MAP_ANONYMOUS)
+
+//#define ISOADDR_AREA_TOP       0x40000000
+//#define FILE_TO_MAP            -1
+//#define IS_ON_MAIN_STACK(sp)   ((sp) > MAIN_STACK_BOT)
+//#define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS)
 
 #else
 

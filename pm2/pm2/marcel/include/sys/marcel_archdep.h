f978a7d1420fee376bea5bd57928c5c9896263f7
ens-lyon.fr!vdanjean <ens-lyon.fr!vdanjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 18:24:48 +0000

    rev 1.12 at 2003/05/26 19:56:57 GMT  ia64 support (marcel/example/sample works)
    (Logical change 1.739)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9239 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_archdep.h
+++ pm2/pm2/marcel/include/sys/marcel_archdep.h
@@ -70,75 +70,85 @@ static __inline__ void SCHED_YIELD(void)
 
 /* Solaris sparc */
 #if defined(SOLARIS_SYS) && defined(SPARC_ARCH)
-#include <sys/stack.h>
-#define TOP_STACK_FREE_AREA     (WINDOWSIZE+128)
-#define SP_FIELD(buf)           ((buf)[1])
+#  define STACK_INFO
+#  include <sys/stack.h>
+#  define TOP_STACK_FREE_AREA     (WINDOWSIZE+128)
+#  define SP_FIELD(buf)           ((buf)[1])
 #endif
 
 /* Alpha Linux */
 #if defined(ALPHA_ARCH) && defined(LINUX_SYS)
-#define TOP_STACK_FREE_AREA     128
-#define SP_FIELD(buf)           ((buf)->__jmpbuf[JB_SP])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     128
+#  define SP_FIELD(buf)           ((buf)->__jmpbuf[JB_SP])
 #endif
 
 /* Alpha Tru64 */
 #if defined(ALPHA_ARCH) && defined(OSF_SYS)
-#define TOP_STACK_FREE_AREA     128
-#define JB_REGS         4               /* registers */
-#define JB_SP           (JB_REGS+30)    /* stack pointer */
-#define SP_FIELD(buf)           ((buf)[JB_SP])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     128
+#  define JB_REGS         4               /* registers */
+#  define JB_SP           (JB_REGS+30)    /* stack pointer */
+#  define SP_FIELD(buf)           ((buf)[JB_SP])
 #endif
 
 /* Cray T3E */
 #if defined(UNICOS_SYS) && defined(ALPHA_ARCH)
-#define TOP_STACK_FREE_AREA	256
-#define SP_FIELD(buf)           ((buf)[2])
-#define FP_FIELD(buf)           ((buf)[3])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA	256
+#  define SP_FIELD(buf)           ((buf)[2])
+#  define FP_FIELD(buf)           ((buf)[3])
 #endif
 
 /* Any Intel x86 system */
 #if defined(X86_ARCH)
+#  define STACK_INFO
 #  define TOP_STACK_FREE_AREA     64
 #  define SP_FIELD(buf)           ((buf)[MARCEL_JB_SP])
-/* If MEMORY_BARRIER isn't defined in pt-machine.h, assume the
-   architecture doesn't need a memory barrier instruction (e.g. Intel
-   x86).  Still we need the compiler to respect the barrier and emit
-   all outstanding operations which modify memory.  Some architectures
-   distinguish between full, read and write barriers.  */
+#endif
 
-#  ifndef MEMORY_BARRIER
-#    define MEMORY_BARRIER() asm ("" : : : "memory")
-#  endif
-#  ifndef READ_MEMORY_BARRIER
-#    define READ_MEMORY_BARRIER() MEMORY_BARRIER()
-#  endif
-#  ifndef WRITE_MEMORY_BARRIER
-#    define WRITE_MEMORY_BARRIER() MEMORY_BARRIER()
-#  endif
+/* Itanium */
+#if defined(IA64_ARCH)
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     64
+#  define JB_SP                   12
+#  define SP_FIELD(buf)           ((buf).uc_mcontext.sc_gr[JB_SP])
+#  define BSP_FIELD(buf)          ((buf).uc_mcontext.sc_ar_bsp)
 #endif
 
 /* IBM SP2 */
 #if defined(AIX_SYS) && defined(RS6K_ARCH)
-#define TOP_STACK_FREE_AREA     256
-#define SP_FIELD(buf)           ((buf)[3])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     256
+#  define SP_FIELD(buf)           ((buf)[3])
 #endif
 
 /* Linux PPC */
 #if defined(LINUX_SYS) && defined(PPC_ARCH)
-#define TOP_STACK_FREE_AREA     256
-#define SP_FIELD(buf)           ((buf)->__jmpbuf[JB_GPR1])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     256
+#  define SP_FIELD(buf)           ((buf)->__jmpbuf[JB_GPR1])
 #endif
 
 /* SGI */
 #if defined(IRIX_SYS) && defined(MIPS_ARCH)
-#define TOP_STACK_FREE_AREA     128
-#define SP_FIELD(buf)           ((buf)[JB_SP])
+#  define STACK_INFO
+#  define TOP_STACK_FREE_AREA     128
+#  define SP_FIELD(buf)           ((buf)[JB_SP])
 #endif
 
+
+/* ******************* Sparc spécific ******************* */
+#if defined(SPARC_ARCH)
+extern void call_ST_FLUSH_WINDOWS(void);
+#else
+#  define call_ST_FLUSH_WINDOWS()  (void)0
+#endif
 
 /* ******************* Sparc ******************* */
 
 #if defined(SPARC_ARCH)
+#  define SET_GET_SP_DEFINED
 static __inline__ long get_sp()
 {
   register long sp;
@@ -146,29 +156,27 @@ static __inline__ long get_sp()
   __asm__ __volatile__("mov %%sp, %0" : "=r" (sp));
   return sp;
 }
-#define set_sp(val) \
+#  define set_sp(val) \
     __asm__ __volatile__("mov %0, %%sp\n\t" \
                          : : "r" (val) : "memory")
-extern void call_ST_FLUSH_WINDOWS(void);
-#else
-#define call_ST_FLUSH_WINDOWS()  (void)0
 #endif
 
 
 /* ******************* Intel ******************* */
 
 #if defined(X86_ARCH)
+#  define SET_GET_SP_DEFINED
 
-#ifdef MARCEL_SELF_IN_REG
+#  ifdef MARCEL_SELF_IN_REG
 //register marcel_t __marcel_self_in_reg asm ("%%gs");
-#define SET_MARCEL_SELF_FROM_SP(sp) \
-  __asm__ __volatile__("movl %0, %%gs" \
+#    define SET_MARCEL_SELF_FROM_SP(sp) \
+       __asm__ __volatile__("movl %0, %%gs" \
                          : : "m" ( \
-  ((((sp) & ~(SLOT_SIZE-1)) + SLOT_SIZE) - MAL(sizeof(task_desc)))\
-  ) : "memory" )
-#else
-#define SET_MARCEL_SELF_FROM_SP(val) (void)(0)
-#endif
+       ((((sp) & ~(SLOT_SIZE-1)) + SLOT_SIZE) - MAL(sizeof(task_desc)))\
+        ) : "memory" )
+#  else
+#    define SET_MARCEL_SELF_FROM_SP(val) (void)(0)
+#  endif
 
 static __inline__ long get_gs(void)
 {
@@ -185,7 +193,7 @@ static __inline__ long get_sp(void)
   __asm__ __volatile__("movl %%esp, %0" : "=r" (sp));
   return sp;
 }
-#define set_sp(val) \
+#  define set_sp(val) \
   do { \
     SET_MARCEL_SELF_FROM_SP(val); \
     __asm__ __volatile__("movl %0, %%esp" \
@@ -194,9 +202,81 @@ static __inline__ long get_sp(void)
 #endif
 
 
+/* ******************* Intel IA64 ******************* */
+
+#if defined(IA64_ARCH)
+#  define SET_GET_SP_DEFINED
+
+#  define SET_MARCEL_SELF_FROM_SP(val) (void)(0)
+static __inline__ long get_sp(void)
+{
+  register long sp;
+
+  __asm__ __volatile__(
+		  ";; \n\t" \
+		  "mov %0 = sp ;; \n\t"
+		  ";; \n\t" \
+		  : "=r" (sp));
+
+  return sp;
+}
+static __inline__ long get_bsp(void)
+{
+  register long bsp;
+
+  __asm__ __volatile__(
+		  ";; \n\t" \
+		  "flushrs \n\t" \
+		  "mov %0 = ar.bsp ;; \n\t"
+		  ";; \n\t" \
+		  : "=r" (bsp));
+
+  return bsp;
+}
+#  define set_sp(val) \
+  do { \
+    SET_MARCEL_SELF_FROM_SP(val); \
+    __asm__ __volatile__( \
+		    ";; \n\t" \
+		    "mov sp = %0 \n\t" \
+		    ";; \n\t" \
+                       : : "r" (val) : "memory", "sp" ); \
+  } while (0)
+#define rTMP "%0"
+#define rRSC "%1"
+#  define set_sp_bsp(val, bsp) \
+  do { \
+    SET_MARCEL_SELF_FROM_SP(val); \
+    __asm__ __volatile__( \
+		    ";; \n\t" \
+		    "flushrs \n\t" \
+		    ";; \n\t" \
+		    "mov.m "rRSC" = ar.rsc \n\t" \
+		    ";; \n\t" \
+		    "dep "rTMP" = 0, "rRSC", 16, 14 \n\t" \
+		    ";; \n\t" \
+		    "and "rTMP" = ~0x3, "rTMP" \n\t" \
+		    ";; \n\t" \
+		    "mov.m ar.rsc = "rTMP" \n\t" \
+		    ";; \n\t" \
+		    "loadrs \n\t" \
+		    ";; \n\t" \
+		    "mov.m ar.bspstore = %3 \n\t" \
+		    ";; \n\t" \
+		    "mov.m ar.rsc = "rRSC" \n\t" \
+		    ";; \n\t" \
+		    "mov sp = %2 \n\t" \
+		    ";; \n\t" \
+                       : : "r" (0), "r"(0), "r" (val), "r" (bsp) : "memory", "sp" ); \
+  } while (0)
+#endif
+
+
 /* ******************* Mips ******************* */
 
 #if defined(MIPS_ARCH)
+#  define SET_GET_SP_DEFINED
+
 static __inline__ long get_sp()
 {
   register long sp;
@@ -204,7 +284,7 @@ static __inline__ long get_sp()
   __asm__ __volatile__("move %0, $sp" : "=r" (sp));
   return sp;
 }
-#define set_sp(val) \
+#  define set_sp(val) \
   __asm__ __volatile__("move $sp, %0" \
                        : : "r" (val) : "memory" )
 #endif
@@ -213,6 +293,8 @@ static __inline__ long get_sp()
 /* ******************* Alpha ******************* */
 
 #if defined(ALPHA_ARCH)
+#  define SET_GET_SP_DEFINED
+
 static __inline__ long get_sp(void)
 {
   register long sp;
@@ -220,7 +302,7 @@ static __inline__ long get_sp(void)
   __asm__ __volatile__("addq $sp, $31, %0" : "=r" (sp));
   return sp;
 }
-#define set_sp(val) \
+#  define set_sp(val) \
   __asm__ __volatile__("addq %0, $31, $sp" \
                        : : "r" (val) : "memory" )
 #endif
@@ -229,9 +311,38 @@ static __inline__ long get_sp(void)
 /* *********** PowerPC, RS6000 *********** */
 
 #if defined(PPC_ARCH) || defined(RS6K_ARCH)
+#  define SET_GET_SP_DEFINED
+
 extern void set_sp(long);
 extern long get_sp(void);
 #endif
 
+/* If MEMORY_BARRIER isn't defined in pt-machine.h, assume the
+   architecture doesn't need a memory barrier instruction (e.g. Intel
+   x86).  Still we need the compiler to respect the barrier and emit
+   all outstanding operations which modify memory.  Some architectures
+   distinguish between full, read and write barriers.  */
+
+#  ifndef MEMORY_BARRIER
+#    define MEMORY_BARRIER() asm ("" : : : "memory")
+#  endif
+#  ifndef READ_MEMORY_BARRIER
+#    define READ_MEMORY_BARRIER() MEMORY_BARRIER()
+#  endif
+#  ifndef WRITE_MEMORY_BARRIER
+#    define WRITE_MEMORY_BARRIER() MEMORY_BARRIER()
+#  endif
+
+#ifndef SET_GET_SP_DEFINED
+#  error set_sp and get_sp not defined for the current ARCH
+#else
+#  undef SET_GET_SP_DEFINED
+#endif
+
+#ifndef STACK_INFO
+#  error TOP_STACK_FREE_AREA and SP_FIELD not defined for the current ARCH
+#else
+#  undef STACK_INFO
+#endif
 
 #endif

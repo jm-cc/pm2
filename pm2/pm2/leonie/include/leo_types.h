e3518d5ec75a9373ae7b299da5aeaa7f2c49f1cd
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 12:27:43 +0000

    rev 1.11 at 2001/06/11 15:27:00 GMT  - MadIII, suppression de tags eventuels
    2003/09/05 13:56:59+02:00 ens-lyon.fr!oaumage
    rev 1.10 at 2001/06/11 07:40:11 GMT
     - leonie, import de MadIII
    
    (Logical change 1.562)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@7632 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leonie/include/leo_types.h
+++ pm2/pm2/leonie/include/leo_types.h
@@ -1,5 +1,4 @@
 /*
-/*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
  *
@@ -14,34 +13,146 @@
  * General Public License for more details.
  */
 
+/*
  * leo_types.h
- * ----------
+ * -----------
  */
 
-#ifndef __LEO_TYPES_H
-#define __LEO_TYPES_H
+#ifndef LEO_TYPES_H
+#define LEO_TYPES_H
 
-typedef void              *leo_file_handle_t;
-typedef leo_file_handle_t *p_file_handle_t;
-
-typedef void undefined_t, *p_undefined_t;
-
-typedef struct s_leo_application_cluster
+// Directory
+//-----------//////////////////////////////////////////////////////////////
+typedef enum e_leo_loader_priority
 {
-  char          *id;
-  char          *executable;
-  char          *path;
-  p_tbx_slist_t  host_list;
-  p_tbx_slist_t  channel_list;
-} leo_application_cluster_t, *p_leo_application_cluster_t;
+  leo_loader_priority_undefined = -1,
+  leo_loader_priority_low    = 0,
+  leo_loader_priority_medium = 1,
+  leo_loader_priority_high   = 2,
+  leo_loader_priority_count
+} leo_loader_priority_t;
 
-typedef struct s_leo_cluster_definition
+typedef struct s_leo_process_specific
 {
-  char                *id;
-  p_tbx_slist_t        host_model_list;
-  p_tbx_slist_t        host_list;
-  p_leo_mad_module_t   mad_module;
-  p_leo_swann_module_t swann_module;
-} leo_cluster_definition_t, *p_leo_cluster_definition_t;
+  p_ntbx_client_t       client;
+  char                  *current_loader_name;
+  char                  *current_spawn_group_name;
+  leo_loader_priority_t  current_loader_priority;
+} leo_process_specific_t;
 
-#endif /* __LEO_TYPES_H */
+typedef struct s_leo_dir_node
+{
+  char                       *name;
+  p_ntbx_process_container_t  pc;
+} leo_dir_node_t;
+
+typedef struct s_leo_dir_adapter
+{
+  char         *name;
+  char         *selector;
+  char         *parameter;
+  unsigned int  mtu;
+} leo_dir_adapter_t;
+
+typedef struct s_leo_dir_driver_process_specific
+{
+  p_tbx_htable_t adapter_htable;
+  p_tbx_slist_t  adapter_slist;
+} leo_dir_driver_process_specific_t;
+
+typedef struct s_leo_dir_driver
+{
+  char                       *name;
+  p_ntbx_process_container_t  pc;
+} leo_dir_driver_t;
+
+typedef struct s_leo_dir_channel_process_specific
+{
+  char *adapter_name;
+} leo_dir_channel_process_specific_t;
+
+typedef struct s_leo_dir_channel
+{
+  char                       *name;
+  p_ntbx_process_container_t  pc;
+  p_leo_dir_driver_t          driver;
+  tbx_bool_t                  public;
+  p_ntbx_topology_table_t     ttable;
+} leo_dir_channel_t;
+
+typedef struct s_leo_dir_vchannel_process_routing_table
+{
+  char                 *channel_name;
+  ntbx_process_grank_t  destination_rank;
+} leo_dir_vchannel_process_routing_table_t;
+
+typedef struct s_leo_dir_vchannel_process_specific
+{
+  p_ntbx_process_container_t  pc;
+} leo_dir_vchannel_process_specific_t;
+
+typedef struct s_leo_dir_fchannel
+{
+  char *name;
+  char *channel_name;
+} leo_dir_fchannel_t;
+
+typedef struct s_leo_dir_vchannel
+{
+  char                       *name;
+  p_tbx_slist_t               dir_channel_slist;
+  p_tbx_slist_t               dir_fchannel_slist;
+  p_ntbx_process_container_t  pc;
+} leo_dir_vchannel_t;
+
+typedef struct s_leo_directory
+{
+  p_tbx_slist_t  process_slist;
+
+  p_tbx_htable_t node_htable;
+  p_tbx_slist_t  node_slist;
+
+  p_tbx_htable_t driver_htable;
+  p_tbx_slist_t  driver_slist;
+
+  p_tbx_htable_t channel_htable;  
+  p_tbx_slist_t  channel_slist;
+
+  p_tbx_htable_t fchannel_htable;  
+  p_tbx_slist_t  fchannel_slist;
+
+  p_tbx_htable_t vchannel_htable;  
+  p_tbx_slist_t  vchannel_slist;
+} leo_directory_t;
+
+// Networks
+//----------///////////////////////////////////////////////////////////////
+typedef struct s_leo_networks
+{
+  p_tbx_htable_t htable;
+  p_tbx_slist_t  slist;
+} leo_networks_t;
+
+// Processes groups 
+//------------------///////////////////////////////////////////////////////
+typedef struct s_leo_spawn_group
+{
+  char          *loader_name;
+  p_tbx_slist_t  process_slist;
+} leo_spawn_group_t;
+
+typedef struct s_leo_spawn_groups
+{
+  p_tbx_htable_t htable; // indexed by network name
+  p_tbx_slist_t  slist;
+} leo_spawn_groups_t;
+
+// Loaders
+//---------////////////////////////////////////////////////////////////////
+typedef struct s_leo_loader
+{
+  char                *name;
+  p_leo_loader_func_t  loader_func;
+} leo_loader_t;
+
+#endif /* LEO_TYPES_H */

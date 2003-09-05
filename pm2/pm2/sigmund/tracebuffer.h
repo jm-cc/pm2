0b336d58aeca0dd11e72ef7f825bbcff18a02f42
ens-lyon.fr!rnamyst <ens-lyon.fr!rnamyst@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 07:30:47 +0000

    rev 1.2 at 2001/02/23 15:46:04 GMT  Merge between marcel_lifting2 and main trunk.
    (Logical change 1.415)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@5416 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/sigmund/tracebuffer.h
@@ -0,0 +1,26 @@
+#ifndef TRACEBUFFER
+#define TRACEBUFFER
+
+#include "sigmund.h"
+
+#define TRACE_BUFFER_SIZE 1000
+#define EMPTY_LIST (trace_list) NULL
+
+typedef struct trace_item_st * trace_list;
+
+struct trace_item_st {
+  trace tr;
+  trace_list next;
+  trace_list prev;
+};
+
+typedef struct {
+  trace_list first;
+  trace_list last;
+} trace_buffer;
+
+int init_trace_buffer(char *futin, char *fktin);
+
+int get_next_trace(trace *tr);
+
+#endif

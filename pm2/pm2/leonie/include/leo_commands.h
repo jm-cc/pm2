451f00f868f2170d698fd12adef041e2db3bdc21
ens-lyon.fr!oaumage <ens-lyon.fr!oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-09-05 13:55:39 +0000

    rev 1.2 at 2001/07/20 10:57:28 GMT  - mad_leonie_barrier
    (Logical change 1.601)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@8069 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leonie/include/leo_commands.h
+++ pm2/pm2/leonie/include/leo_commands.h
@@ -8,6 +8,8 @@ typedef enum e_leo_command
 {
   leo_command_end = 0,
   leo_command_print,
+  leo_command_barrier,
+  leo_command_barrier_passed,
 } leo_command_t, *p_leo_command_t;
 
 #endif // LEO_COMMANDS_H

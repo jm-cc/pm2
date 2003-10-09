1d22564a967543bac32555c419934d62f42c0270
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-10-09 15:14:45 +0000

    Meilleurs gestion des piles marcel Possibilit� de v�rif d�passement d'allocation
    (Logical change 1.760)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9357 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/source/marcel_alloc.c
+++ pm2/pm2/marcel/source/marcel_alloc.c
@@ -29,74 +29,145 @@ static marcel_lock_t alloc_lock = MARCEL_LOCK_INIT;
 static int __zero_fd;
 #endif
 
-static struct {
-   unsigned last;
-   void *stacks[MAX_STACK_CACHE];
-} stack_cache = { 0, };
+struct cache_head {
+	struct cache_head *prev;
+	unsigned nb_max_cached;
+	unsigned nb_cached;
+	void* stacks[];
+};
+
+static struct cache_head *stack_cache_mapped = NULL;
+static struct cache_head *stack_cache_unmapped = NULL;
+
+static unsigned stack_in_use=0;
 
 void marcel_slot_init(void)
 {
 #if defined(SOLARIS_SYS) || defined(IRIX_SYS) || defined(FREEBSD_SYS) || defined(DARWIN_SYS)
   __zero_fd = open("/dev/zero", O_RDWR);
 #endif
-  stack_cache.last = 0;
+  stack_cache_mapped = NULL;
+  stack_cache_unmapped = NULL;
 
   next_slot = (void *)SLOT_AREA_TOP;
 }
 
 extern volatile unsigned long threads_created_in_cache;
 
+inline static void* slot_cache_get(struct cache_head **head, int *main_slot)
+{
+	struct cache_head *cache=*head;
+	register void *ptr;
+
+	if (!cache) {
+		return NULL;
+	}
+	if (cache->nb_cached) {
+		ptr=cache->stacks[--cache->nb_cached];
+	} else {
+		if (main_slot) {
+			*main_slot=1;
+		}
+		ptr=cache;
+		*head=cache->prev;
+	}
+	return ptr;
+
+}
+
+inline static int slot_cache_put(void* ptr, struct cache_head *cache)
+{
+	if (!cache || cache->nb_cached >= cache->nb_max_cached) {
+		return 0;
+	}
+	cache->stacks[cache->nb_cached++]=ptr;
+	return 1;
+}
+
+inline static void slot_cache_init(struct cache_head *cache, 
+				   struct cache_head *old)
+{
+	static int max_cached=
+		(THREAD_SLOT_SIZE - sizeof(struct cache_head))/sizeof(void*);
+	cache->prev=old;
+	cache->nb_max_cached=max_cached;
+	cache->nb_cached=0;
+}
+
 void *marcel_slot_alloc(void)
 {
-  register void *ptr;
+	register void *ptr;
+	int main_slot=0;
 
-  marcel_lock_acquire(&alloc_lock);
+	marcel_lock_acquire(&alloc_lock);
 
-  if(stack_cache.last) {
+	if(NULL != (ptr=slot_cache_get(&stack_cache_mapped, NULL))) {
+		threads_created_in_cache++;
+	} else {
+		if(NULL == (ptr=slot_cache_get(&stack_cache_unmapped, &main_slot))) {
+			next_slot -= THREAD_SLOT_SIZE;
+#ifdef SLOT_AREA_BOTTOM
+			if ((unsigned long)next_slot < SLOT_AREA_BOTTOM) {
+				RAISE(STORAGE_ERROR);
+			}
+#endif
+			ptr=next_slot;
+		}
+		if (main_slot) {
+			threads_created_in_cache++;
+		} else {
+			ptr = mmap(next_slot,
+				   THREAD_SLOT_SIZE,
+				   PROT_READ | PROT_WRITE | PROT_EXEC,
+				   MMAP_MASK,
+				   FILE_TO_MAP, 0);
 
-    ptr = stack_cache.stacks[--stack_cache.last];
+			if(ptr == MAP_FAILED) {
+				perror("mmap");
+				RAISE(CONSTRAINT_ERROR);
+			}
+		}
+	}
+	stack_in_use++;
+	marcel_lock_release(&alloc_lock);
 
-    threads_created_in_cache++;
-
-  } else {
-
-    next_slot -= THREAD_SLOT_SIZE;
-
-    ptr = mmap(next_slot,
-	       THREAD_SLOT_SIZE,
-	       PROT_READ | PROT_WRITE | PROT_EXEC,
-	       MMAP_MASK,
-	       FILE_TO_MAP, 0);
-
-    if(ptr == MAP_FAILED) {
-      perror("mmap");
-      RAISE(CONSTRAINT_ERROR);
-    }
-  }
-
-  marcel_lock_release(&alloc_lock);
-
-  return ptr;
+	return ptr;
 }
 
 void marcel_slot_free(void *addr)
 {
-  marcel_lock_acquire(&alloc_lock);
+	marcel_lock_acquire(&alloc_lock);
 
-  if(stack_cache.last < MAX_STACK_CACHE) /* Si le cache n'est pas plein */
-    stack_cache.stacks[stack_cache.last++] = addr;
-  else
-    if(munmap(addr, THREAD_SLOT_SIZE) == -1)
-      RAISE(CONSTRAINT_ERROR);
-
-  marcel_lock_release(&alloc_lock);
+	if(!stack_cache_mapped) {
+		slot_cache_init(addr, NULL);
+		stack_cache_mapped=addr;
+	} else if(!slot_cache_put(addr, stack_cache_mapped)) {
+		if(slot_cache_put(addr, stack_cache_unmapped)) {
+			if(munmap(addr, THREAD_SLOT_SIZE) == -1)
+				RAISE(CONSTRAINT_ERROR);
+		} else {
+			slot_cache_init(addr, stack_cache_unmapped);
+			stack_cache_unmapped=addr;
+		}
+	}
+	stack_in_use--;
+	marcel_lock_release(&alloc_lock);
 }
 
 void marcel_slot_exit(void)
 {
-  while(stack_cache.last) {
-    stack_cache.last--;
-    if(munmap(stack_cache.stacks[stack_cache.last], THREAD_SLOT_SIZE) == -1)
-      RAISE(CONSTRAINT_ERROR);
-  }
+	register void *ptr;
+        int main_slot=0;
+	while((ptr=slot_cache_get(&stack_cache_mapped,NULL))!=NULL) {
+		if(munmap(ptr, THREAD_SLOT_SIZE) == -1)
+			RAISE(CONSTRAINT_ERROR);
+	}
+	while((ptr=slot_cache_get(&stack_cache_mapped,&main_slot))!=NULL) {
+		if (main_slot) {
+			if(munmap(ptr, THREAD_SLOT_SIZE) == -1)
+				RAISE(CONSTRAINT_ERROR);
+			main_slot=0;
+		}
+	}
+	
 }

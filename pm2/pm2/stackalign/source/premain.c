4202f3ef51dcccc34dee772a5be4fbf7d3ea12ae
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2003-10-23 11:39:37 +0000

    d�finition de __unbounded si non d�j� d�fini
    (Logical change 1.765)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9371 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/stackalign/source/premain.c
+++ pm2/pm2/stackalign/source/premain.c
@@ -10,14 +10,21 @@ int __attribute__ ((weak)) marcel_pthread_initialize();
 //{
 //  printf("toto\n");
 //}
+
+/****************************************************************
+ * Les variables globales
+ */
+#ifndef __unbounded
+#define __unbounded
+#endif
 static int (*global_main) (int, char **, char **);
-#if 1
 static int global_argc;
 static char *__unbounded *__unbounded global_ubp_av;
 static void (*global_init) (void);
 static void (*global_fini) (void);
 static void (*global_rtld_fini) (void);
 static void *__unbounded global_stack_end;
+
 #define BP_SYM(sym) sym
 typedef int BP_SYM (__libc_start_main_t) (int (*main) (int, char **, char **),
 					  int argc,
@@ -31,44 +38,61 @@ static __libc_start_main_t *global_libc_start_main __attribute__ ((noreturn));
 
 extern __libc_start_main_t local__libc_start_main;
 extern __libc_start_main_t __libc_start_main;
+/****************************************************************
+ * launch_libc_start_main :
+ * appelle la vraie fonction __libc_start_main
+ * en utilisant les variables globales.
+ */
 inline static void __attribute__ ((noreturn)) launch_libc_start_main() {
-  char* err;
-  global_libc_start_main=dlsym(RTLD_NEXT, "__libc_start_main");
-  err=dlerror();
-  //global_libc_start_main=__libc_start_main;
+	char* err;
+	global_libc_start_main=dlsym(RTLD_NEXT, "__libc_start_main");
+	err=dlerror();
+	//global_libc_start_main=__libc_start_main;
 #ifdef PM2DEBUG
-  printf("have old start_main\n");
+	printf("have old start_main\n");
 #endif
-  if (err || !global_libc_start_main) {
-    printf("__libc_start_main at %p error: %s\n", global_libc_start_main, err);
-    exit(1);
-  }
+	if (err || !global_libc_start_main) {
+		printf("__libc_start_main at %p error: %s\n", global_libc_start_main, err);
+		exit(1);
+	}
 
 #ifdef PM2DEBUG
-  printf("old = %p, our = %p \n", global_libc_start_main, local__libc_start_main);
+	printf("old = %p, our = %p \n", global_libc_start_main, local__libc_start_main);
 #endif
 
-  global_libc_start_main(global_main,
-			 global_argc,
-			 global_ubp_av,
-			 global_init,
-			 global_fini,
-			 global_rtld_fini,
-			 global_stack_end);
+	global_libc_start_main(global_main,
+			       global_argc,
+			       global_ubp_av,
+			       global_init,
+			       global_fini,
+			       global_rtld_fini,
+			       global_stack_end);
 }
 
+/****************************************************************
+ * marcel_main : appelle la vraie fonction __libc_start_main
+ */
 int marcel_main(int argc, char** argv)
 {
 #ifdef PM2DEBUG
-  printf("on the good stack\n");
+	printf("on the good stack\n");
 #endif
-  if(marcel_pthread_initialize) {
-    marcel_pthread_initialize(&argc, argv);
-  }
-  launch_libc_start_main();
+	if(marcel_pthread_initialize) {
+		marcel_pthread_initialize(&argc, argv);
+	}
+	launch_libc_start_main();
 }
 
 
+/****************************************************************
+ * notre fonction __libc_start_main
+ * 1) remplit les variables globales
+ * 2) Si marcel présent (go_marcel_main non nul)
+ *      appelle go_marcel_main
+ *        (qui change de pile, appelle marcel_main qui appelle __libc_start_main)
+ *    Sinon 
+ *      appelle la vraie fonction __libc_start_main
+ */
 extern __libc_start_main_t __libc_start_main __attribute__ ((noreturn));
 int BP_SYM (__libc_start_main) (int (*main) (int, char **, char **),
 				      int argc,
@@ -79,119 +103,34 @@ int BP_SYM (__libc_start_main) (int (*main) (int, char **, char **),
 				      void *__unbounded stack_end)
 {
 #ifdef PM2DEBUG
-  printf("changing main\n");
+	printf("changing main\n");
 #endif
-  global_main=main;
-  global_argc=argc;
-  global_ubp_av=ubp_av;
-  global_init=init;
-  global_fini=fini;
-  global_rtld_fini=rtld_fini;
-  global_stack_end=stack_end;
-
-  //main=jump_to_go_marcel_main;
-  if (go_marcel_main) {
+	global_main=main;
+	global_argc=argc;
+	global_ubp_av=ubp_av;
+	global_init=init;
+	global_fini=fini;
+	global_rtld_fini=rtld_fini;
+	global_stack_end=stack_end;
+	
+	//main=jump_to_go_marcel_main;
+	if (go_marcel_main) {
 #ifdef PM2DEBUG
-    printf("running go_marcel_main at %p\n", go_marcel_main);
+		printf("running go_marcel_main at %p\n", go_marcel_main);
 #endif
-    go_marcel_main(argc, ubp_av);
+		go_marcel_main(argc, ubp_av);
 #ifdef PM2DEBUG
-    printf("__libc_start_main error: comming back from marcel\n");
+		printf("__libc_start_main error: comming back from marcel\n");
 #endif
-    exit(1);    
-  } else {
+		exit(1);    
+	} else {
 #ifdef PM2DEBUG
-    printf("starting direct. go_marcel_main=%p, %p\n", go_marcel_main);
+		printf("starting direct. go_marcel_main=%p, %p\n", go_marcel_main);
 #endif
-    launch_libc_start_main();
-  }
+		launch_libc_start_main();
+	}
 }
 
 typeof(__libc_start_main) local__libc_start_main 
 	__attribute__((alias("__libc_start_main")));
 
-#else
-static char **global_env;
-extern int jump_to_go_marcel_main (int argc, char **argv, char**env);
-
-int marcel_main(int argc, char** argv)
-{
-  static int (*fp) (int *argc, char** argv);
-  char*err;
-  printf("on the good stack\n");
-  //printf("before marcel_main\n");
-  //if (!fp) fp=dlsym(RTLD_DEFAULT, "marcel_pthread_initialize");
-  //err=dlerror();
-  //printf("pthread_initialize at %p error: %s\n", fp, err);
-  //if (fp) {
-  //  (*fp)(&argc, argv);
-  //}
-  if(marcel_pthread_initialize) {
-    marcel_pthread_initialize(&argc, argv);
-  }
-  return (*global_main)(argc, argv, global_env);
-}
-
-int jump_to_go_marcel_main (int argc, char **argv, char **env)
-{
-  static int (*fp) (int, char**);
-  int result;
-  char*err;
-  global_env=env;
-  //printf("before go_marcel_main\n");
-  //if (!fp) fp=dlsym(RTLD_DEFAULT, "go_marcel_main");
-  //err=dlerror();
-  //printf("go_marcel_main at %p error: %s\n", fp, err);
-  if (go_marcel_main) {
-    result=go_marcel_main(argc, argv);
-  } else {
-    result=(*global_main)(argc, argv, env);
-  }
-  //printf("result of go_marcel_main: %i\n", result);  
-  return result;
-}
-
-void* change_main(void* ret, int (*main) (int, char **, char **))
-{
-  static void* fp;
-  char* err;
-  printf("changing main\n");
-  global_main=main;
-  main=jump_to_go_marcel_main;
-  if (!fp) fp=dlsym(RTLD_NEXT, "__libc_start_main");
-  err=dlerror();
-  if (err || !fp) {
-    printf("__libc_start_main at %p error: %s\n", fp, err);
-    exit(1);
-  }  
-
-  return fp;
-}
-
-extern int __libc_start_main (int main)
-     asm ("__libc_start_main");
-
-__asm__ (".align 16 \n\
-.globl __libc_start_main \n\
-        .type    __libc_start_main,@function \n\
-__libc_start_main: \n\
-        call change_main \n\
-        jmp  *%eax\n\
-.Lmye: \n\
-        .size    __libc_start_main,.Lmye-__libc_start_main \n\
-") ;
-
-
-int sched_yield2(void)
-{
-	static int (* fp)(void);
-	static int (* fp2)(void);
-	if (!fp) {
-		fp=dlsym(RTLD_DEFAULT, "marcel_yield");
-		if (!fp) {
-			fp=dlsym(RTLD_NEXT, "sched_yield");
-		}
-	}
-	return (*fp)();
-}
-#endif

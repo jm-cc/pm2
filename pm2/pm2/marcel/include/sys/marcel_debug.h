2e9643b276d8865d5fe4926b6c07d0f173348d3e
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    Marcel mono avec timer
    2004/02/20 05:45:33+01:00 labri.fr!Vincent.Danjean
    new sched
    
    2003/11/02 15:52:39+01:00 labri.fr!Vincent.Danjean
    __locked dans marcel_task_t au lieu de lwp_t
    
    2003/10/09 17:18:45+02:00 labri.fr!Vincent.Danjean
    R�organisation de Marcel
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9884 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/sys/marcel_debug.h
+++ pm2/pm2/marcel/include/sys/marcel_debug.h
@@ -14,16 +14,11 @@
  * General Public License for more details.
  */
 
-#ifndef MARCEL_DEBUG_EST_DEF
-#define MARCEL_DEBUG_EST_DEF
-
+#section types
 #include "tbx_debug.h"
 #include "sys/marcel_flags.h"
 
-
-
-#undef mdebug
-
+#section variables
 #ifdef PM2DEBUG
 extern debug_type_t marcel_mdebug;
 extern debug_type_t marcel_trymdebug;
@@ -39,79 +34,91 @@ extern debug_type_t marcel_sched_lock_debug;
 
 extern debug_type_t marcel_mtrace;
 extern debug_type_t marcel_mtrace_timer;
+#endif
 
-#define mdebug(fmt, args...) \
-    debug_printf(&marcel_mdebug, fmt, ##args)
-#define try_mdebug(fmt, args...) \
-    debug_printf(&marcel_trymdebug, fmt, ##args)
-#define mdebug_state(fmt, args...) \
-    debug_printf(&marcel_debug_state, fmt, ##args)
-#define mdebug_work(fmt, args...) \
-    debug_printf(&marcel_debug_work, fmt, ##args)
-#define mdebug_deviate(fmt, args...) \
-    debug_printf(&marcel_debug_deviate, fmt, ##args)
-#define mdebug_upcall(fmt, args...) \
-    debug_printf(&marcel_debug_upcall, fmt, ##args)
-#define mdebug_sched_q(fmt, args...) \
-    debug_printf(&marcel_mdebug_sched_q, fmt, ##args)
-
+#section macros
+#ifdef PM2DEBUG
+#  define mdebug(fmt, args...) \
+    debug_printf(&marcel_mdebug, fmt , ##args)
+#  define try_mdebug(fmt, args...) \
+    debug_printf(&marcel_trymdebug, fmt , ##args)
+#  define mdebug_state(fmt, args...) \
+    debug_printf(&marcel_debug_state, fmt , ##args)
+#  define mdebug_work(fmt, args...) \
+    debug_printf(&marcel_debug_work, fmt , ##args)
+#  define mdebug_deviate(fmt, args...) \
+    debug_printf(&marcel_debug_deviate, fmt , ##args)
+#  define mdebug_upcall(fmt, args...) \
+    debug_printf(&marcel_debug_upcall, fmt , ##args)
+#  define mdebug_sched_q(fmt, args...) \
+    debug_printf(&marcel_mdebug_sched_q, fmt , ##args)
 #else // PM2DEBUG
-
-#define mdebug(fmt, args...)     (void)0
-#define try_mdebug(fmt, args...)     (void)0
-#define mdebug_state(fmt, args...)     (void)0
-#define mdebug_work(fmt, args...)     (void)0
-#define mdebug_deviate(fmt, args...)     (void)0
-#define mdebug_upcall(fmt, args...)     (void)0
-#define mdebug_sched_q(fmt, args...)     (void)0
+#  define mdebug(fmt, args...)     (void)0
+#  define try_mdebug(fmt, args...)     (void)0
+#  define mdebug_state(fmt, args...)     (void)0
+#  define mdebug_work(fmt, args...)     (void)0
+#  define mdebug_deviate(fmt, args...)     (void)0
+#  define mdebug_upcall(fmt, args...)     (void)0
+#  define mdebug_sched_q(fmt, args...)     (void)0
 #endif // PM2DEBUG
 
+#define MA_BUG() RAISE(PROGRAM_ERROR)
+#define MA_BUG_ON(cond) \
+  do { \
+	if (cond) { \
+		RAISE(PROGRAM_ERROR); \
+	} \
+  } while (0);
+
+#define MA_WARN_ON(cond) \
+  do { \
+	if (cond) { \
+		mdebug("%s:%l:Warning on '" #cond "'", __FILE__, __LINE__); \
+	} \
+  } while (0);
+
 #ifdef DEBUG_LOCK_TASK
-#define lock_task_debug(fmt, args...) debug_printf(&marcel_lock_task_debug, \
+#  define lock_task_debug(fmt, args...) debug_printf(&marcel_lock_task_debug, \
         fmt, ##args)
 #endif
 #ifdef DEBUG_SCHED_LOCK
-#define sched_lock_debug(fmt, args...) debug_printf(&marcel_sched_lock_debug, \
+#  define sched_lock_debug(fmt, args...) debug_printf(&marcel_sched_lock_debug, \
         fmt, ##args)
 #endif
 
-#ifdef MARCEL_TRACE
-
-#define MTRACE(msg, pid) \
+#ifndef MARCEL_TRACE
+#  define MTRACE(msg, pid) (void)0
+#  define MTRACE_TIMER(msg, pid) (void)0
+#  define marcel_trace_on() (void)0
+#  define marcel_trace_off() (void)0
+#else
+#  define MTRACE(msg, pid) \
     (msg[0] ? debug_printf(&marcel_mtrace, \
-            "[%-11s:%3ld (pid=%p:%X)." \
-            " %3d A,%3d S,%3d B,%3d F /%3d T]\n", \
-            msg, (pid)->number, (pid), (pid)->special_flags, \
+            "[%-14s:%3ld (pid=%p:%2lX)." \
+            " [%1d], %3d A,%3d S,%3d B,%3d F /%3d T]\n", \
+            msg, (pid)->number, (pid), (pid)->flags, \
+            pid->preempt_count, \
             marcel_activethreads(), \
             marcel_sleepingthreads(), \
             marcel_blockedthreads(), \
             marcel_frozenthreads(), \
             marcel_nbthreads() + 1) : (void)0)
-#define MTRACE_TIMER(msg, pid) \
+#  define MTRACE_TIMER(msg, pid) \
     debug_printf(&marcel_mtrace_timer, \
-            "[%-11s:%3ld (pid=%p:%X)." \
+            "[%-14s:%3ld (pid=%p:%2lX)." \
             " %3d A,%3d S,%3d B,%3d F /%3d T]\n", \
-            msg, (pid)->number, (pid), (pid)->special_flags, \
+            msg, (pid)->number, (pid), (pid)->flags, \
             marcel_activethreads(), \
             marcel_sleepingthreads(), \
             marcel_blockedthreads(), \
             marcel_frozenthreads(), \
             marcel_nbthreads() + 1)
-#define marcel_trace_on() pm2debug_setup(&marcel_mtrace, PM2DEBUG_SHOW, 1)
-#define marcel_trace_off() pm2debug_setup(&marcel_mtrace, PM2DEBUG_SHOW, 0)
-
-#else
-
-#define MTRACE(msg, pid) (void)0
-#define MTRACE_TIMER(msg, pid) (void)0
-#define marcel_trace_on() (void)0
-#define marcel_trace_off() (void)0
-
+#  define marcel_trace_on() pm2debug_setup(&marcel_mtrace, PM2DEBUG_SHOW, 1)
+#  define marcel_trace_off() pm2debug_setup(&marcel_mtrace, PM2DEBUG_SHOW, 0)
 #endif
 
+#section marcel_functions
 void marcel_debug_init(int* argc, char** argv, int debug_flags);
-
-#endif
 
 
 

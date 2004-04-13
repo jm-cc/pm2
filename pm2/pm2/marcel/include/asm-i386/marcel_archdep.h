fa9d3ad7d308027a0fc7fe28fe065ba0b8f9a44a
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    new sched
    2004/02/11 15:10:06+01:00 labri.fr!Vincent.Danjean
    bk cp sys/marcel_archdep.h asm-i386/marcel_archdep.h
    
    2003/09/07 20:09:41+02:00 labri.fr!Vincent.Danjean
    fix dans set_sp pour gcc r�cent
    
    2003/09/05 20:30:06+02:00 ens-lyon.fr!denisa
    rev 1.13 at 2003/06/18 17:21:55 GMT
     *** empty log message ***
    
    2003/09/05 20:24:21+02:00 ens-lyon.fr!vdanjean
    rev 1.12 at 2003/05/26 19:56:57 GMT
     ia64 support (marcel/example/sample works)
    
    2003/09/05 19:55:53+02:00 ens-lyon.fr!vdanjean
    rev 1.11 at 2003/04/14 15:05:29 GMT
     ajout libpthread
    
    2003/09/05 16:46:46+02:00 ens-lyon.fr!rnamyst
    rev 1.10 at 2002/03/22 09:54:19 GMT
     Modified several files to correctly match the Tru64/Alpha architecture.
    
    2003/09/05 16:20:15+02:00 ens-lyon.fr!rnamyst
    rev 1.9 at 2001/11/15 17:53:28 GMT
     maj marcel mono sur Alpha/Linux
    
    2003/09/05 13:18:54+02:00 ens-lyon.fr!rnamyst
    rev 1.8 at 2001/05/14 18:31:12 GMT
     Small modifs AFA Windows is concerned: marcel_for_win -> marcel_win_sys
    
    2003/09/05 13:16:02+02:00 ens-lyon.fr!rnamyst
    rev 1.7 at 2001/05/10 16:41:58 GMT
     Bug fixed on Solaris: sys/stack.h is now included by marcel_archdep.h
    
    2003/09/05 12:34:57+02:00 ens-lyon.fr!cperez
    rev 1.6 at 2001/04/09 17:40:21 GMT
     Convert some functions from f() to f(void)
    
    2003/09/05 12:31:03+02:00 ens-lyon.fr!rnamyst
    rev 1.5 at 2001/04/05 13:24:29 GMT
     Management of signals and timers has moved to marcel_sig.c.
    
    2003/09/05 11:35:00+02:00 ens-lyon.fr!rnamyst
    rev 1.4 at 2001/03/13 16:00:55 GMT
     Merged the 'new-sched' branch
    
    2003/09/05 11:19:17+02:00 ens-lyon.fr!rnamyst
    rev 1.2.2.1 at 2001/03/05 22:00:15 GMT
     The 'smp-one-queue' chantier does begin\!
    
    2003/09/05 11:02:46+02:00 ens-lyon.fr!rnamyst
    rev 1.3 at 2001/03/02 16:30:38 GMT
     Merged the stable-mars branch (again\!)
    
    2003/09/05 10:15:25+02:00 ens-lyon.fr!rnamyst
    rev 1.1.2.2 at 2001/02/28 11:56:37 GMT
     Modified the copyrights in all file (to reference the author file) + added the dsm/test directory.
    
    2003/09/05 09:49:24+02:00 ens-lyon.fr!rnamyst
    rev 1.2 at 2001/02/27 12:52:25 GMT
     Merged the stable-mars branch into the trunk.
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9836 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/asm-i386/marcel_archdep.h
@@ -0,0 +1,79 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef ARCHDEP_EST_DEF
+#define ARCHDEP_EST_DEF
+
+#include "sys/marcel_flags.h"
+#include "sys/marcel_win_sys.h"
+
+#define TOP_STACK_FREE_AREA     64
+#define SP_FIELD(buf)           ((buf)[MARCEL_JB_SP])
+
+#define call_ST_FLUSH_WINDOWS()  ((void)0)
+
+#ifdef MARCEL_SELF_IN_REG
+//register marcel_t __marcel_self_in_reg asm ("%%gs");
+#  define SET_MARCEL_SELF_FROM_SP(sp) \
+       __asm__ __volatile__("movl %0, %%gs" \
+                         : : "m" ( \
+       ((((sp) & ~(SLOT_SIZE-1)) + SLOT_SIZE) - MAL(sizeof(task_desc)))\
+        ) : "memory" )
+#else
+#    define SET_MARCEL_SELF_FROM_SP(val) (void)(0)
+#endif
+
+static __inline__ long get_gs(void)
+{
+  register long gs;
+
+    __asm__ __volatile__("movl %%gs, %0" : "=r" (gs));
+    return gs;
+}
+
+static __inline__ long get_sp(void)
+{
+  register long sp;
+
+  __asm__ __volatile__("movl %%esp, %0" : "=r" (sp));
+  return sp;
+}
+#define set_sp(val) \
+  do { \
+    typeof(val) value=(val); \
+    SET_MARCEL_SELF_FROM_SP(value); \
+    __asm__ __volatile__("movl %0, %%esp" \
+                       : : "m" (value) : "memory" ); \
+  } while (0)
+
+
+/* If MEMORY_BARRIER isn't defined in pt-machine.h, assume the
+   architecture doesn't need a memory barrier instruction (e.g. Intel
+   x86).  Still we need the compiler to respect the barrier and emit
+   all outstanding operations which modify memory.  Some architectures
+   distinguish between full, read and write barriers.  */
+
+#ifndef MEMORY_BARRIER
+#  define MEMORY_BARRIER() asm ("" : : : "memory")
+#endif
+#ifndef READ_MEMORY_BARRIER
+#  define READ_MEMORY_BARRIER() MEMORY_BARRIER()
+#endif
+#ifndef WRITE_MEMORY_BARRIER
+#  define WRITE_MEMORY_BARRIER() MEMORY_BARRIER()
+#endif
+
+#endif

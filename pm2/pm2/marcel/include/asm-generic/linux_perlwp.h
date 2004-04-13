f1d1c3228beb075364f1b111bf5090ab7b44d9a4
loria.fr!samuel.thibault <loria.fr!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    correction per_lwp: pour l'instant, allocation de la zone locale dans lwp_init(), et on se souvient de l'offset par rapport � la zone d'initialisation.
    2004/04/05 16:54:30+02:00 labri.fr!Vincent.Danjean
    update fichiers noyaux
    
    2004/03/09 17:23:12+01:00 labri.fr!Vincent.Danjean
    Rename: marcel/include/asm-generic/linux/marcel_perlwp.h -> marcel/include/asm-generic/linux_perlwp.h
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9907 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/asm-generic/linux_perlwp.h
@@ -0,0 +1,74 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section common
+/*
+ * Similar to:
+ * include/asm-generic/percpu.h
+ */
+
+#section marcel_macros
+#define __MA_PERLWP_SECTION ".ma.per.lwp"
+
+#define __MA_GENERIC_PER_LWP
+#ifdef MA__LWPS
+
+//extern int __ma_per_lwp_data asm(__MA_PERLWP_SECTION);
+//__attribute__ ((visibility ("protected")));
+
+/* Separate out the type, so (int[3], foo) works. */
+/*#define MA_DEFINE_PER_LWP_SUBSEC(subsec, type, name) \
+    __attribute__((__section__(__MA_PERLWP_SECTION ".offset" \
+                                     subsec ",\"\",@progbits \n#"))) \
+       __typeof__(type) ma_per_lwp__##name; \
+    __attribute__((__section__(__MA_PERLWP_SECTION ".main" subsec))) \
+       __typeof__(type) ma_per_lwp_main__##name
+*/
+#define MA_DEFINE_PER_LWP_SUBSEC(subsec, type, name) \
+    __attribute__((__section__(__MA_PERLWP_SECTION ".main" subsec))) \
+       __typeof__(type) ma_per_lwp__##name
+
+extern int __ma_main_lwp_start;
+/* var is in discarded region: offset to particular copy we want */
+extern void *__per_lwp_data;
+#define ma_per_lwp(var, lwp) (*TBX_RELOC_HIDE(&ma_per_lwp__##var, \
+   (((ma_lwp_t)lwp)->per_lwp_offset)))
+#define __ma_get_lwp_var(var) ma_per_lwp(var, LWP_SELF)
+
+///* Défini dans le script du linker */
+//extern int __ma_per_lwp_end;
+//#define __ma_per_lwp_size ((size_t)&__ma_per_lwp_end-(size_t)&__ma_per_lwp_start)
+
+#else /* ! MA__LWPS */
+
+#define MA_DEFINE_PER_LWP_SUBSEC(subsec, type, name) \
+    __attribute__((__section__(__MA_PERLWP_SECTION ".main" subsec))) \
+       __typeof__(type) ma_per_lwp__##name
+
+#define ma_per_lwp(var, lwp)		(*((void)lwp, &ma_per_lwp__##var))
+#define __ma_get_lwp_var(var)		ma_per_lwp__##var
+
+#endif	/* MA__LWPS */
+
+/* Définis dans le script du linker */
+extern int __ma_main_lwp_start;
+extern int __ma_main_lwp_end;
+#define __ma_per_lwp_size ((size_t)((unsigned long)&__ma_main_lwp_end \
+                                    -(unsigned long)&__ma_main_lwp_start))
+
+#define MA_DEFINE_PER_LWP(type, name) MA_DEFINE_PER_LWP_SUBSEC("", type, name)
+#define MA_DECLARE_PER_LWP(type, name) extern __typeof__(type) ma_per_lwp__##name
+

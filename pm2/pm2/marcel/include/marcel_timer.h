400a08c395e01d35e466082d0edea8a14a337210
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    d�finition de MA_JIFFIES_PER_TIMER_TICK
    2004/03/01 12:24:24+01:00 labri.fr!Vincent.Danjean
    suite de l'adaptation de marcel
    
    2004/02/28 16:39:54+01:00 labri.fr!Vincent.Danjean
    Rename: marcel/include/sys/marcel_sig.h -> marcel/include/marcel_timer.h
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9828 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/marcel_timer.h
@@ -0,0 +1,50 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section macros
+#define MA_JIFFIES_PER_TIMER_TICK 1
+
+#section functions
+void marcel_settimeslice(unsigned long microsecs);
+unsigned long marcel_clock(void);
+
+#section marcel_functions
+void marcel_sig_exit(void);
+void marcel_sig_pause(void);
+void marcel_sig_enable_interrupts(void);
+void marcel_sig_disable_interrupts(void);
+
+#section marcel_functions
+static __inline__ void disable_preemption(void);
+static __inline__ void enable_preemption(void);
+static __inline__ unsigned int preemption_enabled(void);
+#section marcel_variables
+extern ma_atomic_t __preemption_disabled;
+#section marcel_inline
+static __inline__ void disable_preemption(void)
+{
+	ma_atomic_inc(&__preemption_disabled);
+}
+
+static __inline__ void enable_preemption(void)
+{
+	ma_atomic_dec(&__preemption_disabled);
+}
+
+static __inline__ unsigned int preemption_enabled(void)
+{
+	return ma_atomic_read(&__preemption_disabled) == 0;
+}

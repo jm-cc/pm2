2b98e2601d44e428390faaef623c9a777e9db8c2
loria.fr!samuel.thibault <loria.fr!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    initializer C99
    2004/04/06 15:41:00+02:00 loria.fr!samuel.thibault
    gcc >=3.2 n'aime pas qu'on caste les initialiseurs constants. Dans le code d'une fonction, c'est cependant obligatoire...
    
    2004/03/11 10:15:15+01:00 labri.fr!Vincent.Danjean
    Marcel mono avec timer
    
    2004/02/20 05:45:35+01:00 labri.fr!Vincent.Danjean
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9896 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/marcel_fastlock.h
@@ -0,0 +1,32 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section macros
+#define MA_FASTLOCK_UNLOCKED {.__status=0, .__spinlock=MA_SPIN_LOCK_UNLOCKED}
+
+#section types
+typedef int __marcel_atomic_lock_t;
+
+#section structures
+#depend "linux_spinlock.h[marcel_structures]"
+/* Fast locks (not abstract because mutexes and conditions aren't abstract). */
+struct _marcel_fastlock
+{
+  long int __status;   /* "Free" or "taken" or head of waiting list */
+  ma_spinlock_t __spinlock;  /* Used by compare_and_swap emulation. Also,
+			  adaptive SMP lock stores spin count here. */
+};
+

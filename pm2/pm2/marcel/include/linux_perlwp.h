7fa4db53a2c8ce239dfedb6c814a69a2548c499c
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    Marcel mono avec timer
    2004/03/09 17:21:10+01:00 labri.fr!Vincent.Danjean
    Rename: marcel/include/linux/marcel_perlwp.h -> marcel/include/linux_perlwp.h
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9830 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/linux_perlwp.h
@@ -0,0 +1,34 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section common
+/*
+ * Similar to:
+ * include/linux/percpu.h
+ */
+#depend "asm/linux_perlwp.h[]"
+
+#section marcel_macros
+/* Enough to cover all DEFINE_PER_LWPs in marcel. */
+#ifndef MA_PERLWP_ENOUGH_ROOM
+#define MA_PERLWP_ENOUGH_ROOM 32768
+#endif
+
+/* Must be an lvalue. */
+#define ma_get_lwp_var(var) (*({ ma_preempt_disable(); &__ma_get_lwp_var(var); }))
+#define ma_put_lwp_var(var) ma_preempt_enable()
+
+

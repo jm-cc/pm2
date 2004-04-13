c6920675ef2a230b0184c06d49a8fdbccd187086
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    merge avec la branche principale
    2004/04/06 16:32:08+02:00 loria.fr!samuel.thibault
    initializer C99
    
    2004/04/06 15:41:01+02:00 loria.fr!samuel.thibault
    Ne pas utiliser __LOCK_INITIALIZER, qui est aussi d�fini dans /usr/include/bits/initspin.h, ce qui fait des clashes quand on inclut /usr/include/pthread.h
    
    2004/02/20 05:45:33+01:00 labri.fr!Vincent.Danjean
    new sched
    
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9854 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_rwlock.h
+++ pm2/pm2/marcel/include/marcel_rwlock.h
@@ -1,4 +1,57 @@
-#ifdef __USE_UNIX98
+
+#section macros
+
+//#ifdef __USE_UNIX98
+# define MARCEL_RWLOCK_INITIALIZER \
+  { .__rw_lock=MA_SPIN_LOCK_UNLOCKED, .__rw_readers=0, .__rw_writer=NULL,     \
+    .__rw_read_waiting=NULL, .__rw_write_waiting=NULL,			      \
+    .__rw_kind=MARCEL_RWLOCK_DEFAULT_NP, .__rw_pshared=MARCEL_PROCESS_PRIVATE }
+//#endif
+//#ifdef __USE_GNU
+# define MARCEL_RWLOCK_WRITER_NONRECURSIVE_INITIALIZER_NP \
+  { .__rw_lock=MA_SPIN_LOCK_UNLOCKED, .__rw_readers=0, .__rw_writer=NULL,     \
+    .__rw_read_waiting=NULL, .__rw_write_waiting=NULL,			      \
+    .__rw_kind=MARCEL_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP, 		      \
+    .__rw_pshared=MARCEL_PROCESS_PRIVATE }
+//#endif
+
+//#ifdef __USE_UNIX98
+enum
+{
+  MARCEL_RWLOCK_PREFER_READER_NP,
+  MARCEL_RWLOCK_PREFER_WRITER_NP,
+  MARCEL_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP,
+  MARCEL_RWLOCK_DEFAULT_NP = MARCEL_RWLOCK_PREFER_WRITER_NP
+};
+//#endif	/* Unix98 */
+
+#section structures
+#depend "marcel_threads.h[types]"
+
+/* Read-write locks.  */
+typedef struct _marcel_rwlock_t
+{
+  struct _marcel_fastlock __rw_lock; /* Lock to guarantee mutual exclusion */
+  int __rw_readers;                   /* Number of readers */
+  p_marcel_task_t __rw_writer;         /* Identity of writer, or NULL if none */
+  p_marcel_task_t __rw_read_waiting;   /* Threads waiting for reading */
+  p_marcel_task_t __rw_write_waiting;  /* Threads waiting for writing */
+  int __rw_kind;                      /* Reader/Writer preference selection */
+  int __rw_pshared;                   /* Shared between processes or not */
+} marcel_rwlock_t;
+
+
+/* Attribute for read-write locks.  */
+typedef struct
+{
+  int __lockkind;
+  int __pshared;
+} marcel_rwlockattr_t;
+
+#section functions
+#depend "marcel_alias.h[macros]"
+#depend "sched.h"
+//#ifdef __USE_UNIX98
 
 DEC_MARCEL_POSIX(int,
 		 rwlock_init, (marcel_rwlock_t *rwlock,
@@ -8,9 +61,11 @@ DEC_MARCEL_POSIX(int,
 
 DEC_MARCEL_POSIX(int,
 		 rwlock_rdlock, (marcel_rwlock_t *rwlock) __THROW)
+//#ifdef __USE_XOPEN2K
 DEC_MARCEL_POSIX(int,
 		 rwlock_timedrdlock, (marcel_rwlock_t *rwlock,
 				      const struct timespec *abstime) __THROW)
+//#endif
 DEC_MARCEL_POSIX(int,
 		 rwlock_tryrdlock, (marcel_rwlock_t *rwlock) __THROW)
 DEC_MARCEL_POSIX(int,
@@ -34,4 +89,4 @@ DEC_MARCEL_POSIX(int,
 		 (const marcel_rwlockattr_t *attr, int *pref) __THROW)
 DEC_MARCEL_POSIX(int,
 		 rwlockattr_setkind_np, (marcel_rwlockattr_t *attr, int pref) __THROW)
-#endif
+//#endif

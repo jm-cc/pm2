93c20bbfe5d01c669b25c8cb4817e6064a7d99ea
loria.fr!samuel.thibault <loria.fr!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-13 14:15:46 +0000

    marcel_archdep.h a chang� de place
    (Logical change 1.840)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9844 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/examples/clone.c
+++ pm2/pm2/marcel/examples/clone.c
@@ -16,7 +16,7 @@
 
 
 #include "clone.h"
-#include "sys/marcel_archdep.h"
+#include "asm/marcel_archdep.h"
 
 marcel_key_t _clone_key, _slave_key;
 
--- pm2/pm2/pm2/source/bitmap.c
+++ pm2/pm2/pm2/source/bitmap.c
@@ -19,7 +19,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
-#include "sys/marcel_archdep.h"
+#include "asm/marcel_archdep.h"
 #include "sys/bitmap.h"
 #include "marcel.h"
 

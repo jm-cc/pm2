bb5a604a4813af42435760e32612932114c1b00e
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-26 14:51:25 +0000

    - configuration des canaux virtuels - construction des tables de routage
    (Logical change 1.862)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9991 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leo_vchannel.py
+++ pm2/pm2/leony/leo_vchannel.py
@@ -1,19 +1,228 @@
+import sys
+import sets
+
 import leo_pp
 
 def vchannels_process(s):
+
+    # on initialise la table des canaux virtuels de la session
     s.vchannel_dict = {}
 
+    # on initialise la table des canaux de forwarding
+    s.fchannel_dict = {}
+
+    # s'il n'y a pas de canaux virtuels dans le fichier de configuration
     if not s.appnetdata.has_key('vchannels'):
+
+        # on ne va pas plus loin
         return
 
+    # sinon, on récupère la liste des canaux virtuels configurés
     vchannel_list = s.appnetdata['vchannels']
+
+    # on normalize la liste si l'utilisateur à fait une simplification
+    # dans le fichier de configuration (suppression des parenthèses
+    # pour une liste à un unique élément)
     if type(vchannel_list) is not list:
         vchannel_list = [vchannel_list]
 
+    # on traite chaque canal virtuel en séquence
     for vchannel in vchannel_list:
-        channel_list = vchannel['channels']
-        for channel_num, channel_name in enumerate(channel_list):
-            channel_list[channel_num] = s.channel_dict[channel_name]
 
+        # liste de noms, telle que dans le fichier de configuration
+        old_channel_list	= vchannel['channels']
+
+        # liste de canaux réguliers et de forwarding
+        channel_list	= []
+        fchannel_list	= []
+
+        ps_dict = {}
+        
+        rt   = {} # routing table
+
+        # ensemble des rangs globaux des processus du canal virtuel
+        g_set           = sets.ImmutableSet()
+
+        # parcours des noms de canaux et initialisation du canal virtuel
+        for channel_name in old_channel_list:
+
+            # récupération du canal régulier
+            channel	= s.channel_dict[channel_name]
+
+            # changement du status du canal
+            channel['public']	= False
+
+            # initialisation du canal de forwarding
+            fchannel	= {}
+
+            # nom du fcanal
+            fchannel['name']	= 'f_' + channel_name
+
+            # canal maître du fcanal
+            fchannel['channel']	= channel
+
+            # ajout aux listes de canaux/fcanaux
+            channel_list.append(channel)
+            fchannel_list.append(fchannel)
+
+            # ajout du canal de forwarding dans la table de la session
+            # (note: le canal régulier est déja répertorié au niveau de
+            #  la session)
+            s.fchannel_dict[fchannel['name']] = fchannel
+
+            # récupération de la liste des processus du canal régulier
+            ps_l	= channel['processes']
+
+            # construction du dictionnaire des processus du canal
+            for ps in ps_l:
+                if not ps_dict.has_key(ps.global_rank):
+                    ps_dict[ps.global_rank] = ps
+
+            # construction de l'ensemble des rangs globaux au canal régulier
+            channel_g_set = sets.ImmutableSet([ps.global_rank for ps in ps_l])
+
+            # remplissage de la rt avec les connexions du canal régulier
+            for g_src in channel_g_set:
+                for g_dst in channel_g_set:
+                    
+                    # pas de connexion 'boucle' dans les canaux réguliers
+                    if g_src == g_dst:
+                        continue
+
+                    # la table de routage est indexée par
+                    # un tuple (rang global source, rang global destination)
+                    key	= (g_src, g_dst)
+                    print key
+
+                    # s'il n'y a pas déja une route entre src et dst
+                    if not rt.has_key(key):
+                        
+                        # on rajoute la connexion du canal régulier comme
+                        # route directe
+                        #
+                        # organisation du quartet:
+                        # - canal pour la connexion
+                        # - fcanal associé
+                        # - rang global de la destination
+                        # - booleen indiquant si la route est directe ou non
+                        rt[key] = (channel, fchannel, g_dst, True)
+
+            print channel_name, channel_g_set
+            
+            # rajout des rangs globaux du canal régulier aux rangs
+            # globaux du canal virtuel par union des deux ensembles
+            g_set = g_set.union(channel_g_set)
+            print g_set
+
+        # affectation des listes de canaux/fcanaux au canal virtuel
+        vchannel['channels']	= channel_list
+        vchannel['fchannels']	= fchannel_list
+        vchannel['g_set']	= g_set
+        vchannel['processes']   = ps_dict.values()
+
+        # boucle de generation de la table de routage du canal virtuel
+        while True:
+
+            # a chaque iteration, on utilise les infos de rt pour construire
+            # la table new_rt
+            new_rt			= {}
+
+            # 'vrai' des qu'on a detecté un chemin non résolu à l'itération
+            # courante
+            need_additional_pass	= False
+
+            # 'vrai' des qu'on a ajouté au moins une route durant l'itération
+            # courante
+            converging			= False
+
+            # boucle sur toutes les connexions virtuelles
+            for g_src in g_set:
+                for g_dst in g_set:
+
+                    # construction de la clé de la connexion virtuelle
+                    # considérée
+                    key	= (g_src, g_dst)
+                    print key
+
+                    # si une route existe pour cette connexion virtuelle
+                    if rt.has_key(key):
+
+                        # on copie cette route dans la nouvelle table
+                        new_rt[key] = rt[key]
+                        # et on passe à la connexion virtuelle suivante
+                        continue
+
+                    # sinon, on essaie de créer une route indirecte
+                    # qu'on commence par initialiser à None (~ NULL)
+                    path = None
+
+                    # et on cherche une passerelle
+                    for g_med in g_set:
+                        # on considère un passage par med
+                        
+                        key_src = (g_src, g_med)
+                        # si on ne sait pas aller à med depuis src
+                        if not rt.has_key(key_src):
+                            # on poursuit la recherche d'une passerelle
+                            continue
+                        
+                        key_dst = (g_med, g_dst)
+                        # si on ne sait pas aller de med à dst
+                        if not rt.has_key(key_dst):
+                            # on poursuit la recherche d'une passerelle
+                            continue
+
+                        # si on arrive ici, c'est qu'on a trouvé un med
+                        # accessible depuis src et capable d'atteindre dst
+                        print 'new path:', g_src, g_dst, ' by', g_med
+
+                        # le premier saut de la route indirecte est le même
+                        # que src->med, mais avec le flag 'route directe' à
+                        # faux
+                        (_ch, _fch, _g_rank, _last) = rt[key_src]
+                        path = (_ch, _fch, _g_rank, False)
+                        break
+
+                    # on fait le point sur la recherche d'une route indirecte
+
+                    # si on n'a pas trouvé de chemin
+                    if path == None:
+
+                        # alors, on a besoin d'une passe
+                        # supplémentaire (ou la convergence est
+                        # impossible, mais on ne le sais pas encore)
+                        need_additional_pass = True
+
+                    # si au contraire on a trouvé un chemin indirect
+                    else:
+
+                        # on met ce chemin dans la nouvelle table
+                        new_rt[key] = path
+
+                        # on progressé, et on le fait savoir
+                        converging = True
+
+            # on fait le point sur l'itération globale
+
+            # si on a besoin d'une nouvelle passe alors qu'on n'a pas
+            # progressé à celle-ci,
+            if need_additional_pass and not converging:
+
+                # on ne vas pas plus loin: le canal virtuel a des
+                # composantes non connexes
+                print 'routing table generation is not converging'
+                sys.exit(1)
+
+            # la nouvelle table devient la table de référence
+            rt = new_rt
+
+            # si on n'a plus de chemins non résolus, on cesse d'itérer
+            if not need_additional_pass:
+                break
+
+        # on sauve la table dans le canal virtuel
+        vchannel['rt'] = rt
+
+        # on sauve le canal virtuel dans la table des canaux virtuels
         s.vchannel_dict[vchannel['name']] = vchannel
 

0e578f0522791c01e2b82aa3173664a4bfd9124c
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-26 14:51:25 +0000

    - appels des fonctions d'activation des canaux - lancement de la boucle de traitement des commandes des processus clients - ajout d'une temporisation (temporaire...) en attendant le code de nettoyage de session
    (Logical change 1.862)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9987 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leony.py
+++ pm2/pm2/leony/leony.py
@@ -5,6 +5,10 @@ import leo_cfg
 import leo_comm
 import leo_spawn
 import leo_dir_send
+import leo_dir_activate
+import leo_loop
+
+import time
 
 class Session:
     pass
@@ -15,4 +19,12 @@ leo_cfg.appcfg_process(s)
 leo_comm.server_init(s)
 leo_spawn.session_spawn(s)
 leo_dir_send.send_dir(s)
+leo_dir_activate.driver_init(s)
+leo_dir_activate.channels_init(s)
+leo_dir_activate.fchannels_init(s)
+leo_dir_activate.vchannels_init(s)
+leo_dir_activate.xchannels_init(s)
+leo_loop.loop(s)
+
+time.sleep(10)
 # end

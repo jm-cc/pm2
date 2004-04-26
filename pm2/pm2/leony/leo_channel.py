7b4586e6b911969d101d7a4186612fe8182aca70
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-04-26 14:51:25 +0000

    - support partiel des adaptateurs r�seau multiples et des param�tres de connexion de niveau 'adapter' - liste des processus de la session - flag public/priv� dans les canaux physiques - modification de l'allocation des rangs globaux
    (Logical change 1.862)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@9992 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leo_channel.py
+++ pm2/pm2/leony/leo_channel.py
@@ -7,9 +7,12 @@ def channel_process(s, channel):
     """Channel settings processing."""
     (ps_l, loader_ps_l, driver_ps_l, host_l) = leo_pp.channel_lists_get(s, channel)
 
+    dev = s.net_dict[channel['net']]['dev']
+
     for host in host_l:
         (hostname, id_range_l) = host
         node_ps_dict	= leo_pp.node_process_dict_get(s, hostname)
+
         for id_range in id_range_l:
             for id in id_range:
                 if node_ps_dict.has_key(id):
@@ -17,21 +20,38 @@ def channel_process(s, channel):
                 else:
                     ps			= Process()
                     ps.location		= (hostname, id)
+
+                    adapter	= {}
+                    adapter['name']	= 'default'
+
+                    adapter_dict 	= {}
+                    adapter_dict[adapter['name']] = adapter
+                    
+                    ps.driver_dict      = {}
+                    ps.driver_dict[dev] = adapter_dict
+                    
+                    ps.global_rank	= s.next_global_rank
+                    s.next_global_rank	= s.next_global_rank + 1
+                    
                     node_ps_dict[id]	= ps
+                    s.process_list.append(ps)
                     loader_ps_l.append(ps)
                     driver_ps_l.append(ps)
 
                 ps_l.append(ps)
 
     channel['processes']		= ps_l
+    channel['public']			= True
     s.channel_dict[channel['name']]	= channel
 
 def channels_process(s):
     """Channel list processing."""
+    s.process_list	= []
     s.channel_dict	= {}
     s.process_dict	= {}
     s.driver_dict	= {}
     s.loader_dict	= {}
+    s.next_global_rank	= 0
 
     channel_l = leo_pp.channel_list_get(s)
 

318eda3f149a700af483c31439912836905f07ed
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-05-11 13:33:35 +0000

    - boucle pour sessions multiples
    2004/04/28 16:25:09+02:00 labri.fr!olivier.aumage
    - suppression des logs
    - affichage des messages LDISP
    
    (Logical change 1.888)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10079 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leo_loop.py
+++ pm2/pm2/leony/leo_loop.py
@@ -1,57 +1,115 @@
 import leo_comm
+import leo_session
+
+import logging
+import os
+import select
+import string
+import sys
+import time
+
+logger	= logging.getLogger()
+prompt_string = 'leony$ '
 
 _LEO_COMMAND_END		= 0
 _LEO_COMMAND_PRINT		= 1
 _LEO_COMMAND_BARRIER		= 2
 _LEO_COMMAND_BARRIER_PASSED	= 3
 
-def loop(s):
-    ps_dict	= dict([(ps.client, ps) for ps in s.process_list])
-    initial_ps_nb	= len(ps_dict)
-    barrier_dict	= None
+def prompt():
+    sys.stdout.write(prompt_string)
+    sys.stdout.flush()
+
+def poll(s):
+    ps_dict	= s.active_process_dict
     
-    while len(ps_dict) > 0:
-        active_client_list	= leo_comm.client_select(ps_dict.keys())
+    active_client_list	= leo_comm.client_select(ps_dict.keys())
+    if len(active_client_list) == 0:
+        return
 
-        for active_client in active_client_list:
+    print
 
-            command = leo_comm.receive_int(active_client)
-            print 'command', command, 'from', ps.location
+    for active_client in active_client_list:
 
-            if command == _LEO_COMMAND_END:
-                if not barrier_dict == None:
-                    print 'inconsistent end of session request: a barrier is ongoing'
-                    
-                del ps_dict[active_client]
+        command = leo_comm.receive_int(active_client)
+        print s.name, 'client command:', command
+
+        if command == _LEO_COMMAND_END:
+            if not s.barrier_dict == None:
+                print
+                print '- inconsistent end of session request: a barrier is ongoing'
                 
-            elif command == _LEO_COMMAND_PRINT:
-                if not barrier_dict == None:
-                    print 'inconsistent print request: a barrier is ongoing'
+            del ps_dict[active_client]
+            
+        elif command == _LEO_COMMAND_PRINT:
+            if not s.barrier_dict == None:
+                print '- inconsistent print request: a barrier is ongoing'
 
-                string = leo_comm.receive_string(active_client)
-                
-            elif command == _LEO_COMMAND_BARRIER:
-                if barrier_dict == None:
-                    barrier_dict = {active_client: active_client}
-                    if len(ps_dict) < initial_ps_nb:
-                        print 'inconsistent barrier request: some processes have already leaved the session'
-                else:
-                    if not barrier_dict.has_key(active_client):
-                        barrier_dict[active_client] = active_client
-                    else:
-                        print 'inconsistent barrier request: duplicate barrier command'
-
-                    if len(barrier_dict) == initial_ps_nb:
-                        # barrier passed...
-                        for client in ps_dict.keys():
-                            leo_comm.send_int(client, _LEO_COMMAND_BARRIER_PASSED)
-                        barrier_dict = None
-                
-            elif command == _LEO_COMMAND_BARRIER_PASSED:
-                print 'invalid client command', command
-                
+            string = leo_comm.receive_string(active_client)
+            print string
+            
+        elif command == _LEO_COMMAND_BARRIER:
+            if s.barrier_dict == None:
+                s.barrier_dict = {active_client: active_client}
+                if len(ps_dict) < s.size:
+                    print 'inconsistent barrier request: some processes have already leaved the session'
             else:
-                print 'invalid command', command
+                if not s.barrier_dict.has_key(active_client):
+                    s.barrier_dict[active_client] = active_client
+                else:
+                    print 'inconsistent barrier request: duplicate barrier command'
 
-    print 'the end...'
+                if len(s.barrier_dict) == s.size:
+                    # barrier passed...
+                    for client in ps_dict.keys():
+                        leo_comm.send_int(client, _LEO_COMMAND_BARRIER_PASSED)
+                    s.barrier_dict = None
+            
+        elif command == _LEO_COMMAND_BARRIER_PASSED:
+            print 'invalid client command', command
+            
+        else:
+            print 'invalid command', command
 
+        prompt()
+
+def loop(leo):
+    prompt()
+
+    while True:
+        # logger.info('<loop>')
+        for session_name in leo.session_dict.keys():
+            session = leo.session_dict[session_name]
+            poll(session)
+            if len(session.active_process_dict) == 0:
+                logger.info("session '"+session.name+"' completed")
+                prompt()
+                session.exit()
+                del leo.session_dict[session_name]
+
+        fd_list = [ 0 ]
+        (ilist, olist, elist) = select.select(fd_list, [], [], 0.1)
+        if len(ilist) == 1:
+            command = sys.stdin.readline()
+            command_tokens	= string.split(command)
+            if len(command_tokens) == 0:
+                prompt()
+                continue
+            
+            command_word	= command_tokens.pop(0)
+
+            if command_word == 'quit':
+                print 'quit'
+                break
+            elif command_word == 'add':
+                name	= command_tokens.pop(0)
+                print 'add', name
+                s	= leo_session.Session(leo, name, command_tokens)
+                s.init()
+            else:
+                print 'invalid command'
+
+            prompt()
+
+        # if len(leo.session_dict) == 0:
+        #    break

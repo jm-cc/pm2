52757d0ef1109b55f86b041a80bf26d402ea743a
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-06-15 17:50:50 +0000

    - conversion des VXchannels en classes/objets
    2004/05/10 13:12:30+02:00 labri.fr!olivier.aumage
    - g�n�ralisation de l'utilisation de classes � la place des dictionnaires
    
    (Logical change 1.979)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10401 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leo_vchannel.py
+++ pm2/pm2/leony/leo_vchannel.py
@@ -8,140 +8,98 @@ import leo_pp
 
 logger = logging.getLogger('vchannels')
 
-def vchannels_process(s, mux_p):
+class Fchannel:
 
-    if mux_p:
-        cfg_key = 'xchannels'
-        vxchannel_dict = s.xchannel_dict
-    else:
-        cfg_key = 'vchannels'
-        fchannel_dict   = s.fchannel_dict
-        vxchannel_dict	= s.vchannel_dict
-
-    # s'il n'y a pas de canaux virtuels dans le fichier de configuration
-    if not s.appnetdata.has_key(cfg_key):
-
-        # on ne va pas plus loin
-        return
-
-    # sinon, on récupère la liste des canaux virtuels configurés
-    vxchannel_list = s.appnetdata[cfg_key]
-
-    # on normalize la liste si l'utilisateur à fait une simplification
-    # dans le fichier de configuration (suppression des parenthèses
-    # pour une liste à un unique élément)
-    if type(vxchannel_list) is not list:
-        vxchannel_list = [vxchannel_list]
-
-    # on traite chaque canal virtuel en séquence
-    for vxchannel in vxchannel_list:
+    def __init__(self, channel):
+        self.channel	= channel
+        self.name	= 'f_' + channel.name
+        self.session	= channel.session
+        self.session.fchannel_dict[self.name] = self
         
-        logger.info('processing vxchannel %s', vxchannel['name'])
-        
+
+# end
+
+
+class VXchannel:
+
+    def __init__(self, session, vxchannel_cfg):
+        self.name	= vxchannel_cfg['name']
+        logger.debug('new vxchannel %s', self.name)
+
+        self.session		= session
+        self.channel_list	= []
+        self.ps_dict		= {}
+        self.rt			= {}
+        self.g_set		= sets.ImmutableSet()
+
         # liste de noms, telle que dans le fichier de configuration
-        old_channel_list	= vxchannel['channels']
-
-        # liste de canaux réguliers et de forwarding
-        channel_list	= []
-
-        if not mux_p:
-            fchannel_list	= []
-
-        ps_dict = {}
-        
-        rt   = {} # routing table
-
-        # ensemble des rangs globaux des processus du canal virtuel
-        g_set           = sets.ImmutableSet()
+        old_channel_list	= vxchannel_cfg['channels']
 
         # parcours des noms de canaux et initialisation du canal virtuel
         for channel_name in old_channel_list:
 
             # récupération du canal régulier
-            channel	= s.channel_dict[channel_name]
+            channel	= self.session.channel_dict[channel_name]
 
             # changement du status du canal
-            channel['public']	= False
+            channel.public	= False
 
             # ajout à la liste de canaux
-            channel_list.append(channel)
+            self.channel_list.append(channel)
 
-            if mux_p:
-                fchannel	= None
-            else:
-                # initialisation du canal de forwarding
-                fchannel	= {}
+            self.call_integrate_channel(channel)
 
-                # nom du fcanal
-                fchannel['name']	= 'f_' + channel_name
+        self.compute_rt()
 
-                # canal maître du fcanal
-                fchannel['channel']	= channel
+    def integrate_channel(self, channel, fchannel):
+        
+        # construction du dictionnaire des processus du canal
+        for ps in channel.processes:
+            if not self.ps_dict.has_key(ps.global_rank):
+                self.ps_dict[ps.global_rank] = ps
 
-                # ajout à la liste de fcanaux
-                fchannel_list.append(fchannel)
+        # construction de l'ensemble des rangs globaux au canal régulier
+        channel_g_set = sets.ImmutableSet([ps.global_rank for ps in channel.processes])
 
-                # ajout du canal de forwarding dans la table de la session
-                # (note: le canal régulier est déja répertorié au niveau de
-                #  la session)
-                fchannel_dict[fchannel['name']] = fchannel
+        # remplissage de la rt avec les connexions du canal régulier
+        for g_src in channel_g_set:
+            for g_dst in channel_g_set:
+                
+                # pas de connexion 'boucle' dans les canaux réguliers
+                if g_src == g_dst:
+                    continue
 
-            # récupération de la liste des processus du canal régulier
-            ps_l	= channel['processes']
+                # la table de routage est indexée par
+                # un tuple (rang global source, rang global destination)
+                key	= (g_src, g_dst)
+                logger.debug('direct route %s', str(key))
 
-            # construction du dictionnaire des processus du canal
-            for ps in ps_l:
-                if not ps_dict.has_key(ps.global_rank):
-                    ps_dict[ps.global_rank] = ps
-
-            # construction de l'ensemble des rangs globaux au canal régulier
-            channel_g_set = sets.ImmutableSet([ps.global_rank for ps in ps_l])
-
-            # remplissage de la rt avec les connexions du canal régulier
-            for g_src in channel_g_set:
-                for g_dst in channel_g_set:
+                # s'il n'y a pas déja une route entre src et dst
+                if not self.rt.has_key(key):
                     
-                    # pas de connexion 'boucle' dans les canaux réguliers
-                    if g_src == g_dst:
-                        continue
+                    # on rajoute la connexion du canal régulier comme
+                    # route directe
+                    #
+                    # organisation du quartet:
+                    # - canal pour la connexion
+                    # - fcanal associé
+                    # - rang global de la destination
+                    # - booleen indiquant si la route est directe ou non
+                    self.rt[key] = (channel, fchannel, g_dst, True)
 
-                    # la table de routage est indexée par
-                    # un tuple (rang global source, rang global destination)
-                    key	= (g_src, g_dst)
-                    logger.info('direct route %s', str(key))
+        logger.debug('channel %s: globals %s', str(channel.name), str(channel_g_set))
+        
+        # rajout des rangs globaux du canal régulier aux rangs
+        # globaux du canal virtuel par union des deux ensembles
+        self.g_set = self.g_set.union(channel_g_set)
+        logger.debug('vchannel: globals %s', str(self.g_set))
 
-                    # s'il n'y a pas déja une route entre src et dst
-                    if not rt.has_key(key):
-                        
-                        # on rajoute la connexion du canal régulier comme
-                        # route directe
-                        #
-                        # organisation du quartet:
-                        # - canal pour la connexion
-                        # - fcanal associé
-                        # - rang global de la destination
-                        # - booleen indiquant si la route est directe ou non
-                        rt[key] = (channel, fchannel, g_dst, True)
-
-            logger.info('channel %s: globals %s', str(channel_name), str(channel_g_set))
-            
-            # rajout des rangs globaux du canal régulier aux rangs
-            # globaux du canal virtuel par union des deux ensembles
-            g_set = g_set.union(channel_g_set)
-            logger.info('vchannel: globals %s', str(g_set))
-
-        # affectation des listes de canaux/fcanaux au canal virtuel
-        vxchannel['channels']	= channel_list
-        if not mux_p:
-            vxchannel['fchannels']	= fchannel_list
-            
-        vxchannel['g_set']	= g_set
-        vxchannel['processes']   = ps_dict.values()
-
+    def compute_rt(self):
+        
         # boucle de generation de la table de routage du canal virtuel
         while True:
 
-            # a chaque iteration, on utilise les infos de rt pour construire
+            # a chaque iteration, on utilise les debugs de rt pour construire
             # la table new_rt
             new_rt			= {}
 
@@ -154,19 +112,19 @@ def vchannels_process(s, mux_p):
             converging			= False
 
             # boucle sur toutes les connexions virtuelles
-            for g_src in g_set:
-                for g_dst in g_set:
+            for g_src in self.g_set:
+                for g_dst in self.g_set:
 
                     # construction de la clé de la connexion virtuelle
                     # considérée
                     key	= (g_src, g_dst)
-                    logger.info('routing %s', str(key))
+                    logger.debug('routing %s', str(key))
 
                     # si une route existe pour cette connexion virtuelle
-                    if rt.has_key(key):
+                    if self.rt.has_key(key):
 
                         # on copie cette route dans la nouvelle table
-                        new_rt[key] = rt[key]
+                        new_rt[key] = self.rt[key]
                         # et on passe à la connexion virtuelle suivante
                         continue
 
@@ -175,29 +133,29 @@ def vchannels_process(s, mux_p):
                     path = None
 
                     # et on cherche une passerelle
-                    for g_med in g_set:
+                    for g_med in self.g_set:
                         # on considère un passage par med
                         
                         key_src = (g_src, g_med)
                         # si on ne sait pas aller à med depuis src
-                        if not rt.has_key(key_src):
+                        if not self.rt.has_key(key_src):
                             # on poursuit la recherche d'une passerelle
                             continue
                         
                         key_dst = (g_med, g_dst)
                         # si on ne sait pas aller de med à dst
-                        if not rt.has_key(key_dst):
+                        if not self.rt.has_key(key_dst):
                             # on poursuit la recherche d'une passerelle
                             continue
 
                         # si on arrive ici, c'est qu'on a trouvé un med
                         # accessible depuis src et capable d'atteindre dst
-                        logger.info('new path: %s, %s by %s', str(g_src), str(g_dst), str(g_med))
+                        logger.debug('new path: %s, %s by %s', str(g_src), str(g_dst), str(g_med))
 
                         # le premier saut de la route indirecte est le même
                         # que src->med, mais avec le flag 'route directe' à
                         # faux
-                        (_ch, _fch, _g_rank, _last) = rt[key_src]
+                        (_ch, _fch, _g_rank, _last) = self.rt[key_src]
                         path = (_ch, _fch, _g_rank, False)
                         break
 
@@ -232,17 +190,72 @@ def vchannels_process(s, mux_p):
                 sys.exit(1)
 
             # la nouvelle table devient la table de référence
-            rt = new_rt
+            self.rt = new_rt
 
             # si on n'a plus de chemins non résolus, on cesse d'itérer
             if not need_additional_pass:
                 break
 
-        # on sauve la table dans le canal virtuel
-        vxchannel['rt'] = rt
+# end
 
-        # on sauve le canal virtuel dans la table des canaux virtuels
-        vxchannel_dict[vxchannel['name']] = vxchannel
+class Vchannel(VXchannel):
+
+    def __init__(self, session, vxchannel_cfg):
+        self.fchannel_list	= []
+        VXchannel.__init__(self, session, vxchannel_cfg)
+
+    def call_integrate_channel(self, channel):
+        fchannel	= Fchannel(channel)
+
+        # ajout à la liste de fcanaux
+        self.fchannel_list.append(fchannel)
+        self.integrate_channel(channel, fchannel)
+        self.session.vchannel_dict[self.name] = self
+
+#end
+
+class Xchannel(VXchannel):
+
+    def __init__(self, session, vxchannel_cfg):
+        VXchannel.__init__(self, session, vxchannel_cfg)
+        if vxchannel_cfg.has_key('sub_channels'):
+            self.sub_channel_list = leo_pp.list_normalize(vxchannel_cfg['sub_channels'])
+        else:
+            self.sub_channel_list = []
+
+        self.session.xchannel_dict[self.name] = self
+
+    def call_integrate_channel(self, channel):
+        self.integrate_channel(channel, None)
+#end
+
+def vchannels_process(s, mux_p):
+
+    if mux_p:
+        cfg_key = 'xchannels'
+        vxchannel_dict = s.xchannel_dict
+    else:
+        cfg_key = 'vchannels'
+        fchannel_dict   = s.fchannel_dict
+        vxchannel_dict	= s.vchannel_dict
+
+    # s'il n'y a pas de canaux virtuels dans le fichier de configuration
+    if not s.appnetdata.has_key(cfg_key):
+
+        # on ne va pas plus loin
+        return
+
+    # sinon, on récupère la liste des canaux virtuels configurés
+    vxchannel_list = leo_pp.list_normalize(s.appnetdata[cfg_key])
+
+    # on traite chaque canal virtuel en séquence
+    for vxchannel_cfg in vxchannel_list:
+
+        if mux_p:
+            vxchannel = Xchannel(s, vxchannel_cfg)
+        else:
+            vxchannel = Vchannel(s, vxchannel_cfg)        
+        
 
 
     

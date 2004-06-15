c739441d296e43c457458fc701e1f341712ab16d
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-06-15 17:50:50 +0000

    - correction d'une faute de frappe
    2004/05/12 10:07:45+02:00 labri.fr!olivier.aumage
    - logs suppl�mentaires
    
    2004/05/11 15:02:48+02:00 labri.fr!olivier.aumage
    - modification de l'utilisation du dictionnaire active_process_dict chaque processus est maintenant � la fois la cl� et la valeur
    
    2004/05/10 13:12:30+02:00 labri.fr!olivier.aumage
    - d�placement de code vers les classes
    
    (Logical change 1.979)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10402 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/leony/leo_session.py
+++ pm2/pm2/leony/leo_session.py
@@ -5,6 +5,10 @@ import leo_dir_deactivate
 import leo_dir_send
 import leo_spawn
 
+import logging
+
+logger	= logging.getLogger()
+
 class Session:
 
     def __init__(self, leo, name, args):
@@ -22,30 +26,75 @@ class Session:
         leo.session_dict[name] = self
         leo_args.cmdline_parse(self, args)
 
+    def node_process_dict_get(self, host_name):
+        if not self.node_dict.has_key(host_name):
+            self.node_dict[host_name] = {}
+
+        return self.node_dict[host_name]
+
+    def loader_process_list_get(self, net_name):
+        net	= self.net_dict[net_name]
+        loader_name	= net.loader_name
+
+        if not self.loader_dict.has_key(loader_name):
+            self.loader_dict[loader_name] = []
+
+        return self.loader_dict[loader_name]
+
+    def net_name_to_dev_name(self, net_name):
+        net		= self.net_dict[net_name]
+        return net.dev_name
+
+    def driver_process_list_get(self, net_name):
+        net	= self.net_dict[net_name]
+        driver	= net.get_driver()
+        return driver.processes
+
     def init(self):
+        logger.info('0% - parsing configuration file')
         leo_cfg.appcfg_process(self)
+        logger.info('10% - spawning session')
         leo_spawn.session_spawn(self)
+        logger.info('20% - sending directory')
         leo_dir_send.send_dir(self)
+        logger.info('30% - initializing drivers')
         leo_dir_activate.driver_init(self)
+        logger.info('40% - initializing channels')
         leo_dir_activate.channels_init(self)
+        logger.info('50% - initializing forwarding channels')
         leo_dir_activate.fchannels_init(self)
+        logger.info('60% - initializing virtual channels')
         leo_dir_activate.vchannels_init(self)
+        logger.info('70% - initializing multiplexing channels')
         leo_dir_activate.xchannels_init(self)
         
-        self.active_process_dict	= dict([(ps.client, ps) for ps in self.process_list])
-        self.size	= len(self.active_process_dict)
+        logger.info('80% - building active processes container')
+        self.active_process_dict	= dict([(ps, ps) for ps in self.process_list])
+        logger.info('90% - initializing remaining internal state structures')
+        self.size	= len(self.process_list)
         self.barrier_dict	= None
+        logger.info('100% - session ready')
 
 
     def exit(self):
+        logger.info('0% - synchronizing processes')
         leo_dir_deactivate.sync(self)
+        logger.info('10% - disconnecting virtual channels')
         leo_dir_deactivate.vchannels_disconnect(self)
+        logger.info('20% - disconnecting multiplexing channels')
         leo_dir_deactivate.xchannels_disconnect(self)
+        logger.info('30% - cleaning virtual channels')
         leo_dir_deactivate.vchannels_exit(self)
+        logger.info('40% - cleaning multiplexing channels')
         leo_dir_deactivate.xchannels_exit(self)
+        logger.info('50% - cleaning forwarding channels')
         leo_dir_deactivate.fchannels_exit(self)
+        logger.info('60% - cleaning channels')
         leo_dir_deactivate.channels_exit(self)
+        logger.info('70% - cleaning drivers')
         leo_dir_deactivate.drivers_exit(self)
+        logger.info('80% - synchronizing processes')
         leo_dir_deactivate.sync_exit(self)
+        logger.info('100% - session removed')
 
 #_____________

37a6114b50cd480abcfdba60e5e63dc90644b0e5
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-06-24 18:27:09 +0000

    Rajout d'un commentaire
    (Logical change 1.992)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10464 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/appli/config/options/00debug.sh
+++ pm2/pm2/appli/config/options/00debug.sh
@@ -1,2 +1,3 @@
+# auto generated from generic/config/options/*
 PM2_APPLI_CFLAGS_KERNEL="$PM2_APPLI_CFLAGS_KERNEL -DPM2DEBUG"
 
--- pm2/pm2/appli/config/options/00opt.sh
+++ pm2/pm2/appli/config/options/00opt.sh
@@ -1,1 +1,2 @@
+# auto generated from generic/config/options/*
 PM2_APPLI_CFLAGS_KERNEL="$PM2_APPLI_CFLAGS_KERNEL -O6"

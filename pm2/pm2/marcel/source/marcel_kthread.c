8a1cd5d03db28fd71516bce67c4f32edcf3f545f
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-10-05 12:41:23 +0000

    correction affichage d'un size_t: %zu
    (Logical change 1.1029)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10643 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/source/marcel_kthread.c
+++ pm2/pm2/marcel/source/marcel_kthread.c
@@ -131,7 +131,7 @@ void marcel_kthread_create(marcel_kthread_t *pid, void *sp,
 	if ((err=pthread_attr_setstack (&attr, stack_base, stack_size))) {
 		char s[256];
 		strerror_r(err,s,256);
-		fprintf(stderr, "Error: pthread_attr_setstack(%p, %p, %lx):"
+		fprintf(stderr, "Error: pthread_attr_setstack(%p, %p, %zu):"
 			" (%d)%s\n", &attr, stack_base, stack_size, err, s);
 		fprintf(stderr, "PTHREAD_STACK_MIN: %x\n", PTHREAD_STACK_MIN);
 		abort();

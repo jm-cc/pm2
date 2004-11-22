5f959c8a88565f9b0e621d13595d908f8acf3f0b
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2004-11-22 00:02:46 +0000

    (Logical change 1.1074)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10758 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/marcel_topology.h
@@ -0,0 +1,37 @@
+
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2004 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section marcel_types
+#ifdef MA__LWPS
+typedef unsigned long ma_cpu_set_t;
+#else
+typedef unsigned ma_cpu_set_t;
+#endif
+
+#section marcel_macros
+#define MA_CPU_ZERO(cpusetp)		(*(cpusetp)=0)
+#ifdef MA__LWPS
+#define MA_CPU_FILL(cpusetp)		(*(cpusetp)=~0UL)
+#define MA_CPU_SET(cpu, cpusetp)	(*(cpusetp)|=1UL<<(cpu))
+#define MA_CPU_CLR(cpu, cpusetp)	(*(cpusetp)&=~(1UL<<(cpu)))
+#define MA_CPU_ISSET(cpu, cpusetp)	((*(cpusetp)&(1UL<<(cpu))) != 0)
+#else
+#define MA_CPU_FILL(cpusetp)		(*(cpusetp)=1)
+#define MA_CPU_SET(cpu, cpusetp)	MA_CPU_FILL(cpusetp)
+#define MA_CPU_CLR(cpu, cpusetp)	MA_CPU_ZERO(cpusetp)
+#define MA_CPU_ISSET(cpu, cpusetp)	(*(cpusetp))
+#endif

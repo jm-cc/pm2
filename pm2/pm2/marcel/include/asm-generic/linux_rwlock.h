44a377517354918f7761a86039fcd8b26aae41e0
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-01-17 09:32:24 +0000

    impl�mentation
    (Logical change 1.1103)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10850 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/asm-generic/linux_rwlock.h
+++ pm2/pm2/marcel/include/asm-generic/linux_rwlock.h
@@ -15,6 +15,7 @@
  */
 
 #section common
+#depend "asm-generic/linux_atomic.h[]"
 /*
  * Similar to:
  * include/asm-i386/rwlock.h
@@ -36,65 +37,63 @@
 
 #section marcel_macros
 #define MA_RW_LOCK_BIAS		 0x01000000
-#define MA_RW_LOCK_BIAS_STR	"0x01000000"
 
-#define __ma_build_read_lock_ptr(rw, helper)   \
-	asm volatile(MA_LOCK_PREFIX "subl $1,(%0)\n\t" \
-		     "js 2f\n" \
-		     "1:\n" \
-		     MA_LOCK_SECTION_START("") \
-		     "2:\tcall " helper "\n\t" \
-		     "jmp 1b\n" \
-		     MA_LOCK_SECTION_END \
-		     ::"a" (rw) : "memory")
+#section marcel_types
+/*
+ * Read-write spinlocks, allowing multiple readers
+ * but only one writer.
+ *
+ * NOTE! it is quite common to have readers in interrupts
+ * but no interrupt writers. For those circumstances we
+ * can "mix" irq-safe locks - any writer needs to get a
+ * irq-safe write-lock, but readers can get non-irqsafe
+ * read-locks.
+ */
+typedef ma_atomic_t ma_rwlock_t;
 
-#define __ma_build_read_lock_const(rw, helper)   \
-	asm volatile(MA_LOCK_PREFIX "subl $1,%0\n\t" \
-		     "js 2f\n" \
-		     "1:\n" \
-		     MA_LOCK_SECTION_START("") \
-		     "2:\tpushl %%eax\n\t" \
-		     "leal %0,%%eax\n\t" \
-		     "call " helper "\n\t" \
-		     "popl %%eax\n\t" \
-		     "jmp 1b\n" \
-		     MA_LOCK_SECTION_END \
-		     :"=m" (*(volatile int *)rw) : : "memory")
+#section marcel_macros
+#define MA_RW_LOCK_UNLOCKED MA_ATOMIC_INIT(MA_RW_LOCK_BIAS);
 
-#define __ma_build_read_lock(rw, helper)	do { \
-						if (__builtin_constant_p(rw)) \
-							__ma_build_read_lock_const(rw, helper); \
-						else \
-							__ma_build_read_lock_ptr(rw, helper); \
-					} while (0)
+#define ma_rwlock_init(x)	do { ma_atomic_set(x, MA_RW_LOCK_UNLOCKED); } while(0)
 
-#define __ma_build_write_lock_ptr(rw, helper) \
-	asm volatile(MA_LOCK_PREFIX "subl $" MA_RW_LOCK_BIAS_STR ",(%0)\n\t" \
-		     "jnz 2f\n" \
-		     "1:\n" \
-		     MA_LOCK_SECTION_START("") \
-		     "2:\tcall " helper "\n\t" \
-		     "jmp 1b\n" \
-		     MA_LOCK_SECTION_END \
-		     ::"a" (rw) : "memory")
+#define ma_rwlock_is_locked(x) (ma_atomic_read(x) != MA_RW_LOCK_BIAS)
 
-#define __ma_build_write_lock_const(rw, helper) \
-	asm volatile(MA_LOCK_PREFIX "subl $" MA_RW_LOCK_BIAS_STR ",%0\n\t" \
-		     "jnz 2f\n" \
-		     "1:\n" \
-		     MA_LOCK_SECTION_START("") \
-		     "2:\tpushl %%eax\n\t" \
-		     "leal %0,%%eax\n\t" \
-		     "call " helper "\n\t" \
-		     "popl %%eax\n\t" \
-		     "jmp 1b\n" \
-		     MA_LOCK_SECTION_END \
-		     :"=m" (*(volatile int *)rw) : : "memory")
+#section marcel_inline
+/*
+ * On x86, we implement read-write locks as a 32-bit counter
+ * with the high bit (sign) being the "contended" bit.
+ *
+ * The inline assembly is non-obvious. Think about it.
+ *
+ * Changed to use the same technique as rw semaphores.  See
+ * semaphore.h for details.  -ben
+ */
+/* the spinlock helpers are in arch/i386/kernel/semaphore.c */
 
-#define __ma_build_write_lock(rw, helper)	do { \
-						if (__builtin_constant_p(rw)) \
-							__ma_build_write_lock_const(rw, helper); \
-						else \
-							__ma_build_write_lock_ptr(rw, helper); \
-					} while (0)
+extern void __ma_read_lock_failed(ma_rwlock_t *rw);
+static inline void _ma_raw_read_lock(ma_rwlock_t *rw)
+{
+	if (ma_atomic_add_negative(-1,rw))
+		__ma_read_lock_failed(rw);
+}
+
+extern void __ma_write_lock_failed(ma_rwlock_t *rw);
+static inline void _ma_raw_write_lock(ma_rwlock_t *rw)
+{
+	if (!ma_atomic_sub_and_test(MA_RW_LOCK_BIAS,rw))
+		__ma_write_lock_failed(rw);
+}
+
+#section marcel_macros
+#define _ma_raw_read_unlock(rw) ma_atomic_inc(rw)
+#define _ma_raw_write_unlock(rw) ma_atomic_add(MA_RW_LOCK_BIAIS,rw);
+
+#section marcel_inline
+static inline int _ma_raw_write_trylock(ma_rwlock_t *rw)
+{
+	if (ma_atomic_sub_and_test(MA_RW_LOCK_BIAS,rw))
+		return 1;
+	ma_atomic_add(MA_RW_LOCK_BIAS,rw);
+	return 0;
+}
 
--- pm2/pm2/marcel/include/asm-generic/linux_spinlock.h
+++ pm2/pm2/marcel/include/asm-generic/linux_spinlock.h
@@ -19,25 +19,23 @@
  * Similar to:
  * include/asm-i386/spinlock.h
  */
-#depend "asm/linux_rwlock.h[]"
-
-#error "to write..."
-
-#ifdef MA__LWPS
+#depend "asm-generic/linux_rwlock.h[]"
 
 #section types
 /*
  * Your basic SMP spinlocks, allowing only a single CPU anywhere
  */
 
-typedef struct {
-	volatile unsigned int lock;
-} ma_spinlock_t;
+#ifdef MA__LWPS
+typedef volatile unsigned ma_spinlock_t;
+#endif
 
 #section macros
-#define MA_SPIN_LOCK_UNLOCKED { 1 }
+#ifdef MA__LWPS
+#define MA_SPIN_LOCK_UNLOCKED 0
 
 #define ma_spin_lock_init(x)	do { *(x) = (ma_spinlock_t) MA_SPIN_LOCK_UNLOCKED; } while(0)
+#endif
 
 #section marcel_macros
 /*
@@ -47,133 +45,13 @@ typedef struct {
  * We make no fairness assumptions. They have a cost.
  */
 
-#define ma_spin_is_locked(x)	(*(volatile signed char *)(&(x)->lock) <= 0)
+#ifdef MA__LWPS
+
+#define ma_spin_is_locked(x)	(*(x) == 1)
 #define ma_spin_unlock_wait(x)	do { ma_barrier(); } while(ma_spin_is_locked(x))
 
-#define ma_spin_lock_string \
-	"\n1:\t" \
-	"lock ; decb %0\n\t" \
-	"js 2f\n" \
-	MA_LOCK_SECTION_START("") \
-	"2:\t" \
-	"rep;nop\n\t" \
-	"cmpb $0,%0\n\t" \
-	"jle 2b\n\t" \
-	"jmp 1b\n" \
-	MA_LOCK_SECTION_END
+#define _ma_raw_spin_unlock(x) pm2_spinlock_release(x)
+#define _ma_raw_spin_trylock(x) (!pm2_spinlock_testandset(x))
+#define _ma_raw_spin_lock(x) do { } while (!(_ma_raw_spin_trylock(x)))
 
-#section marcel_inline
-/*
- * This works. Despite all the confusion.
- * (except on PPro SMP or if we are using OOSTORE)
- * (PPro errata 66, 92)
- */
- 
-#if !defined(CONFIG_X86_OOSTORE) && !defined(CONFIG_X86_PPRO_FENCE)
-
-#define ma_spin_unlock_string \
-	"movb $1,%0" \
-		:"=m" (lock->lock) : : "memory"
-
-
-static inline void _ma_raw_spin_unlock(ma_spinlock_t *lock)
-{
-	__asm__ __volatile__(
-		ma_spin_unlock_string
-	);
-}
-
-#else
-
-#define ma_spin_unlock_string \
-	"xchgb %b0, %1" \
-		:"=q" (oldval), "=m" (lock->lock) \
-		:"0" (oldval) : "memory"
-
-static inline void _ma_raw_spin_unlock(ma_spinlock_t *lock)
-{
-	char oldval = 1;
-	__asm__ __volatile__(
-		ma_spin_unlock_string
-	);
-}
-
-#endif
-
-static inline int _ma_raw_spin_trylock(ma_spinlock_t *lock)
-{
-	char oldval;
-	__asm__ __volatile__(
-		"xchgb %b0,%1"
-		:"=q" (oldval), "=m" (lock->lock)
-		:"0" (0) : "memory");
-	return oldval > 0;
-}
-
-static inline void _ma_raw_spin_lock(ma_spinlock_t *lock)
-{
-	__asm__ __volatile__(
-		ma_spin_lock_string
-		:"=m" (lock->lock) : : "memory");
-}
-
-#section marcel_types
-/*
- * Read-write spinlocks, allowing multiple readers
- * but only one writer.
- *
- * NOTE! it is quite common to have readers in interrupts
- * but no interrupt writers. For those circumstances we
- * can "mix" irq-safe locks - any writer needs to get a
- * irq-safe write-lock, but readers can get non-irqsafe
- * read-locks.
- */
-typedef struct {
-	volatile unsigned int lock;
-} ma_rwlock_t;
-
-#section marcel_macros
-#define MA_RW_LOCK_UNLOCKED (ma_rwlock_t) { MA_RW_LOCK_BIAS }
-
-#define ma_rwlock_init(x)	do { *(x) = MA_RW_LOCK_UNLOCKED; } while(0)
-
-#define ma_rwlock_is_locked(x) ((x)->lock != MA_RW_LOCK_BIAS)
-
-#section marcel_inline
-/*
- * On x86, we implement read-write locks as a 32-bit counter
- * with the high bit (sign) being the "contended" bit.
- *
- * The inline assembly is non-obvious. Think about it.
- *
- * Changed to use the same technique as rw semaphores.  See
- * semaphore.h for details.  -ben
- */
-/* the spinlock helpers are in arch/i386/kernel/semaphore.c */
-
-static inline void _ma_raw_read_lock(ma_rwlock_t *rw)
-{
-	__ma_build_read_lock(rw, "__read_lock_failed");
-}
-
-static inline void _ma_raw_write_lock(ma_rwlock_t *rw)
-{
-	__ma_build_write_lock(rw, "__write_lock_failed");
-}
-
-#section marcel_macros
-#define _ma_raw_read_unlock(rw)		asm volatile("lock ; incl %0" :"=m" ((rw)->lock) : : "memory")
-#define _ma_raw_write_unlock(rw)	asm volatile("lock ; addl $" MA_RW_LOCK_BIAS_STR ",%0":"=m" ((rw)->lock) : : "memory")
-
-#section marcel_inline
-static inline int _ma_raw_write_trylock(ma_rwlock_t *lock)
-{
-	ma_atomic_t *count = (ma_atomic_t *)lock;
-	if (ma_atomic_sub_and_test(MA_RW_LOCK_BIAS, count))
-		return 1;
-	ma_atomic_add(MA_RW_LOCK_BIAS, count);
-	return 0;
-}
-
-#section common
 #endif /* MA__LWPS */

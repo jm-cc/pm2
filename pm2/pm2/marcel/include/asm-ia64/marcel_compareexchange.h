fe37954eece47971857b402faed61b9d7c66fed3
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-01-18 18:02:26 +0000

    typo...
    (Logical change 1.1109)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@10870 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/asm-ia64/marcel_compareexchange.h
+++ pm2/pm2/marcel/include/asm-ia64/marcel_compareexchange.h
@@ -15,6 +15,6 @@
  */
 
 #section common
-#depend "asm/linux_intrisincs.h[]"
+#depend "asm/linux_intrisics.h[]"
 #section marcel_macros
 #define pm2_compareexchange(p,o,n,s) ma_ia64_cmpxchg(acq,(p),(o),(n),(s))

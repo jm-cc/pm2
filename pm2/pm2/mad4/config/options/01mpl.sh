0a9040e01e7f399d24eb57a0c0b97b15a00a9adc
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-02-08 18:08:45 +0000

    - s/mad3/mad4/g
    2005/01/06 11:18:21+01:00 labri.fr!olivier.aumage
    
    (Logical change 1.1129)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11089 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad4/config/options/01mpl.sh
+++ pm2/pm2/mad4/config/options/01mpl.sh
@@ -1,6 +1,5 @@
 PM2_PROTOCOLS="$PM2_PROTOCOLS mpi"
 PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DDRV_MPI=mad_MPI"
-PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DEXTERNAL_SPAWN=DRV_MPI"
 PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -D_AIX -D_AIX32 -D_IBMR2 -D_POWER -I/usr/lpp/\ppe.poe/include"
 PM2_MAD4_LIBS="/usr/lpp/ppe.poe/lib/crt0.o $PM2_MAD4_LIBS -L/usr/lpp/\
 ppe.poe/lib -L/usr/lpp/ppe.poe/lib/ip -lmpi -lvtd -lc -L/opt/lib/gcc-lib/rs6000\-ibm-aix4.1.4.0/2.7.2.3 -lgcc -lc -lg -nostdlib"

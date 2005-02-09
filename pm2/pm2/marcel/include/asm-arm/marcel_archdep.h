27cc68753bb6c09e98627c2dce64d81a1f82a550
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-02-09 13:12:55 +0000

    bk cp marcel_archdep.h ../asm-arm/marcel_archdep.h
    2005/01/14 14:55:16+01:00 ens-lyon.org!samuel.thibault
    
    (Logical change 1.1135)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11206 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/asm-arm/marcel_archdep.h
@@ -0,0 +1,44 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef ARCHDEP_EST_DEF
+#define ARCHDEP_EST_DEF
+
+#include "sys/marcel_flags.h"
+#include "sys/marcel_win_sys.h"
+
+#error "to write !"
+
+#define TOP_STACK_FREE_AREA     
+#define SP_FIELD(buf)           
+#define BSP_FIELD(buf)          
+#define PC_FIELD(buf)           
+
+#define call_ST_FLUSH_WINDOWS()  
+
+#define SET_MARCEL_SELF_FROM_SP(sp)
+
+static __inline__ long get_gs(void)
+{
+}
+
+static __inline__ long get_sp(void)
+{
+}
+
+#define set_sp(val)
+
+#endif

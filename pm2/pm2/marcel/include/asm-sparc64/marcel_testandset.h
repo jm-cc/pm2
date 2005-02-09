0da84b017fbe2fea2fd243ec84e9a5fe4fae074a
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-02-09 13:12:55 +0000

    ajout sparc64
    2005/02/09 14:00:36+01:00 ens-lyon.org!samuel.thibault
    bk cp marcel_testandset.h ../asm-sparc64/marcel_testandset.h
    
    2005/02/08 10:09:45+01:00 ens-lyon.org!samuel.thibault
    ajout� une version "pthread_mutex_lock()" pour l'impl�mentation des spinlocks
    
    2005/01/18 18:52:09+01:00 ens-lyon.org!samuel.thibault
    correction compilation
    
    2005/01/14 14:55:16+01:00 ens-lyon.org!samuel.thibault
    
    (Logical change 1.1135)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11188 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/include/asm-sparc64/marcel_testandset.h
@@ -0,0 +1,37 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section macros
+#define MA_HAVE_TESTANDSET 1
+
+#section marcel_functions
+static __inline__ unsigned pm2_spinlock_testandset(volatile unsigned *spinlock);
+#section marcel_inline
+static __inline__ unsigned pm2_spinlock_testandset(volatile unsigned *spinlock)
+{
+  char ret = 0;
+
+  __asm__ __volatile__("ldstub [%0], %1"
+	: "=r"(spinlock), "=r"(ret)
+	: "0"(spinlock), "1" (ret) : "memory");
+
+  return (unsigned)ret;
+}
+
+#section marcel_macros
+#define pm2_spinlock_release(spinlock) \
+  __asm__ __volatile__("stbar\n\tstb %1,%0" : "=m"(*(spinlock)) : "r"(0));
+

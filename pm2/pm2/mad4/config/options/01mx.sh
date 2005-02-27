2224b23b820d849240a8eb828a0a798a8dab31fc
labri.fr!elisabeth.brunet <labri.fr!elisabeth.brunet@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-02-27 18:57:34 +0000

    - options de compilation pour Mad4/MX
    2005/02/16 16:35:28+01:00 labri.fr!olivier.aumage
    
    (Logical change 1.1170)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11297 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/mad4/config/options/01mx.sh
@@ -0,0 +1,16 @@
+MX_DIR=${MX_DIR:-~/mx}
+PM2_PROTOCOLS="$PM2_PROTOCOLS mx"
+PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${MX_DIR}/include"
+PM2_MAD4_LIBS="$PM2_MAD4_LIBS -L${MX_DIR}/lib"
+# PM2_MAD4_LIBS="$PM2_MAD4_LIBS -lmyriexpress"
+PM2_MAD4_LIBS="$PM2_MAD4_LIBS ${MX_DIR}/lib/libmyriexpress.a"
+PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DDRV_MX=mad_MX"
+PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD4_MAD1_MAIN_PROTO=mad_MX"
+PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD4_MAD1_MAIN_PROTO_PARAM=NULL"
+
+
+# mx_open_endpoint calls pthread_create, we need to link libpthread
+# (or disable the use of pthread in MX)
+
+PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -D_REENTRANT"
+PM2_MAD4_LIBS="$PM2_MAD4_LIBS -lpthread"

011f3ab9de00470df3f5a0bd9be2785cd3a07b83
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-15 15:41:59 +0000

    utiliser un objet de link plut��t qu'un script de link non portable
    (Logical change 1.1215)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11431 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/config/marcel.sh
+++ pm2/pm2/marcel/config/marcel.sh
@@ -1,6 +1,8 @@
 PM2_MARCEL_CFLAGS_KERNEL="$PM2_MARCEL_CFLAGS_KERNEL -fomit-frame-pointer"
-PM2_MARCEL_LIBS_SCRIPTS="$PM2_MARCEL_LIBS_SCRIPTS ${PM2_ROOT}/marcel/marcel.lds"
+#PM2_MARCEL_LIBS_SCRIPTS="$PM2_MARCEL_LIBS_SCRIPTS ${PM2_ROOT}/marcel/marcel.lds"
 PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -I${PM2_ROOT}/marcel/autogen-include"
+
+PM2_MARCEL_STAMP_FILES="_marcel_link.o"
 
 case " $PM2_LIBS " in
     *\ mad?\ *) ;;

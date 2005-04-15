83be1ec93df32343f4f64c6c1d0a4357376ff625
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-15 15:41:59 +0000

    (Logical change 1.1215)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11437 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/source/_marcel_link.c
@@ -0,0 +1,110 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * This file is used as linker script, needs be included first while linking.
+ */
+#include <sys/types.h>
+#include "tbx_macros.h"
+#include "tbx_compiler.h"
+
+TBX_SECTION(".ma.main.lwp") TBX_ALIGN(4096)	int __ma_main_lwp_start[0]={};
+TBX_SECTION(".ma.per.lwp.main")			int __ma_per_lwp_main[0]={};
+TBX_SECTION(".ma.main.lwp.end")			int __ma_main_lwp_end[0]={};
+
+TBX_SECTION(".ma.debug.start") TBX_ALIGN(4096)	int __ma_debug_start[0]={};
+TBX_SECTION(".ma.debug.var")			int __ma_debug_var[0]={};
+TBX_SECTION(".ma.debug.end")			int __ma_debug_end[0]={};
+
+TBX_SECTION(".ma.debug.size") TBX_ALIGN(4096)	int __ma_debug_size[0]={};
+TBX_SECTION(".ma.debug.size.0")			int __ma_debug_size_0[0]={};
+TBX_SECTION(".ma.debug.size.1")			int __ma_debug_size_1[0]={};
+
+TBX_SECTION(".ma.init.start") TBX_ALIGN(4096)	int __ma_init_start[0]={};
+TBX_SECTION(".ma.init.ind.0")			int __ma_init_ind_0[0]={};
+TBX_SECTION(".ma.init.ind.1")			int __ma_init_ind_1[0]={};
+TBX_SECTION(".ma.init.ind.2")			int __ma_init_ind_2[0]={};
+TBX_SECTION(".ma.init.ind.3")			int __ma_init_ind_3[0]={};
+TBX_SECTION(".ma.init.ind.4")			int __ma_init_ind_4[0]={};
+TBX_SECTION(".ma.init.ind.5")			int __ma_init_ind_5[0]={};
+TBX_SECTION(".ma.init.inf.0")			int __ma_init_inf_0[0]={};
+TBX_SECTION(".ma.init.inf.0.0")			int __ma_init_inf_0_0[0]={};
+TBX_SECTION(".ma.init.inf.0.1")			int __ma_init_inf_0_1[0]={};
+TBX_SECTION(".ma.init.inf.0.2")			int __ma_init_inf_0_2[0]={};
+TBX_SECTION(".ma.init.inf.0.3")			int __ma_init_inf_0_3[0]={};
+TBX_SECTION(".ma.init.inf.0.4")			int __ma_init_inf_0_4[0]={};
+TBX_SECTION(".ma.init.inf.0.5")			int __ma_init_inf_0_5[0]={};
+TBX_SECTION(".ma.init.inf.0.6")			int __ma_init_inf_0_6[0]={};
+TBX_SECTION(".ma.init.inf.0.7")			int __ma_init_inf_0_7[0]={};
+TBX_SECTION(".ma.init.inf.0.8")			int __ma_init_inf_0_8[0]={};
+TBX_SECTION(".ma.init.inf.0.9")			int __ma_init_inf_0_9[0]={};
+TBX_SECTION(".ma.init.inf.1")			int __ma_init_inf_1[0]={};
+TBX_SECTION(".ma.init.inf.1.0")			int __ma_init_inf_1_0[0]={};
+TBX_SECTION(".ma.init.inf.1.1")			int __ma_init_inf_1_1[0]={};
+TBX_SECTION(".ma.init.inf.1.2")			int __ma_init_inf_1_2[0]={};
+TBX_SECTION(".ma.init.inf.1.3")			int __ma_init_inf_1_3[0]={};
+TBX_SECTION(".ma.init.inf.1.4")			int __ma_init_inf_1_4[0]={};
+TBX_SECTION(".ma.init.inf.1.5")			int __ma_init_inf_1_5[0]={};
+TBX_SECTION(".ma.init.inf.1.6")			int __ma_init_inf_1_6[0]={};
+TBX_SECTION(".ma.init.inf.1.7")			int __ma_init_inf_1_7[0]={};
+TBX_SECTION(".ma.init.inf.1.8")			int __ma_init_inf_1_8[0]={};
+TBX_SECTION(".ma.init.inf.1.9")			int __ma_init_inf_1_9[0]={};
+TBX_SECTION(".ma.init.inf.2")			int __ma_init_inf_2[0]={};
+TBX_SECTION(".ma.init.inf.2.0")			int __ma_init_inf_2_0[0]={};
+TBX_SECTION(".ma.init.inf.2.1")			int __ma_init_inf_2_1[0]={};
+TBX_SECTION(".ma.init.inf.2.2")			int __ma_init_inf_2_2[0]={};
+TBX_SECTION(".ma.init.inf.2.3")			int __ma_init_inf_2_3[0]={};
+TBX_SECTION(".ma.init.inf.2.4")			int __ma_init_inf_2_4[0]={};
+TBX_SECTION(".ma.init.inf.2.5")			int __ma_init_inf_2_5[0]={};
+TBX_SECTION(".ma.init.inf.2.6")			int __ma_init_inf_2_6[0]={};
+TBX_SECTION(".ma.init.inf.2.7")			int __ma_init_inf_2_7[0]={};
+TBX_SECTION(".ma.init.inf.2.8")			int __ma_init_inf_2_8[0]={};
+TBX_SECTION(".ma.init.inf.2.9")			int __ma_init_inf_2_9[0]={};
+TBX_SECTION(".ma.init.inf.3")			int __ma_init_inf_3[0]={};
+TBX_SECTION(".ma.init.inf.3.0")			int __ma_init_inf_3_0[0]={};
+TBX_SECTION(".ma.init.inf.3.1")			int __ma_init_inf_3_1[0]={};
+TBX_SECTION(".ma.init.inf.3.2")			int __ma_init_inf_3_2[0]={};
+TBX_SECTION(".ma.init.inf.3.3")			int __ma_init_inf_3_3[0]={};
+TBX_SECTION(".ma.init.inf.3.4")			int __ma_init_inf_3_4[0]={};
+TBX_SECTION(".ma.init.inf.3.5")			int __ma_init_inf_3_5[0]={};
+TBX_SECTION(".ma.init.inf.3.6")			int __ma_init_inf_3_6[0]={};
+TBX_SECTION(".ma.init.inf.3.7")			int __ma_init_inf_3_7[0]={};
+TBX_SECTION(".ma.init.inf.3.8")			int __ma_init_inf_3_8[0]={};
+TBX_SECTION(".ma.init.inf.3.9")			int __ma_init_inf_3_9[0]={};
+TBX_SECTION(".ma.init.inf.4")			int __ma_init_inf_4[0]={};
+TBX_SECTION(".ma.init.inf.4.0")			int __ma_init_inf_4_0[0]={};
+TBX_SECTION(".ma.init.inf.4.1")			int __ma_init_inf_4_1[0]={};
+TBX_SECTION(".ma.init.inf.4.2")			int __ma_init_inf_4_2[0]={};
+TBX_SECTION(".ma.init.inf.4.3")			int __ma_init_inf_4_3[0]={};
+TBX_SECTION(".ma.init.inf.4.4")			int __ma_init_inf_4_4[0]={};
+TBX_SECTION(".ma.init.inf.4.5")			int __ma_init_inf_4_5[0]={};
+TBX_SECTION(".ma.init.inf.4.6")			int __ma_init_inf_4_6[0]={};
+TBX_SECTION(".ma.init.inf.4.7")			int __ma_init_inf_4_7[0]={};
+TBX_SECTION(".ma.init.inf.4.8")			int __ma_init_inf_4_8[0]={};
+TBX_SECTION(".ma.init.inf.4.9")			int __ma_init_inf_4_9[0]={};
+TBX_SECTION(".ma.init.inf.5")			int __ma_init_inf_5[0]={};
+TBX_SECTION(".ma.init.inf.5.0")			int __ma_init_inf_5_0[0]={};
+TBX_SECTION(".ma.init.inf.5.1")			int __ma_init_inf_5_1[0]={};
+TBX_SECTION(".ma.init.inf.5.2")			int __ma_init_inf_5_2[0]={};
+TBX_SECTION(".ma.init.inf.5.3")			int __ma_init_inf_5_3[0]={};
+TBX_SECTION(".ma.init.inf.5.4")			int __ma_init_inf_5_4[0]={};
+TBX_SECTION(".ma.init.inf.5.5")			int __ma_init_inf_5_5[0]={};
+TBX_SECTION(".ma.init.inf.5.6")			int __ma_init_inf_5_6[0]={};
+TBX_SECTION(".ma.init.inf.5.7")			int __ma_init_inf_5_7[0]={};
+TBX_SECTION(".ma.init.inf.5.8")			int __ma_init_inf_5_8[0]={};
+TBX_SECTION(".ma.init.inf.5.9")			int __ma_init_inf_5_9[0]={};
+TBX_SECTION(".ma.init.end")			int __ma_init_end[0]={};
+

3ca921acfe61ee04fbd89a210a6057ba0c551def
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-19 14:14:15 +0000

    correction initialisation per_lwp
    2004/11/22 14:11:10+01:00 ens-lyon.org!samuel.thibault
    correction re-r�veil d'une bulle qu'on vient de fermer: le faire dans finish_task_switch, apr�s d�verouillage de tout le monde
    
    2004/11/22 12:12:55+01:00 ens-lyon.org!samuel.thibault
    correction time_slice des bulles: proportionnel au parall�lisme des runqueues
    
    2004/11/15 17:19:12+01:00 ens-lyon.org!samuel.thibault
    correction mono
    
    2004/11/15 10:48:28+01:00 ens-lyon.org!samuel.thibault
    r�organisation infos topologie
    
    2004/11/09 17:49:10+01:00 ens-lyon.org!samuel.thibault
    r�organisation bulle: marcel_bubble_t contient un marcel_bubble_entity_t, comme marcel_task_t
    refermeture des bulles
    
    2004/09/29 12:34:40+02:00 ens-lyon.org!samuel.thibault
    ajout d'une bulle "root" qui englobe tout le monde (bien plus simple au niveau du code)
    
    2004/09/28 17:44:09+02:00 ens-lyon.org!samuel.thibault
    hum, ce test �tait un 1 au final...
    
    2004/09/28 16:56:54+02:00 ens-lyon.org!samuel.thibault
    attention, les taches ne doivent pas etre r�veill�es: elles sont d�j� ordonnanc�es quand on les lance.
    
    2004/09/28 16:08:23+02:00 ens-lyon.org!samuel.thibault
    se souvenir d'o� la bulle a explos�
    
    2004/09/28 15:42:25+02:00 ens-lyon.org!samuel.thibault
    correction verrouillage
    
    2004/09/28 15:11:59+02:00 ens-lyon.org!samuel.thibault
    correction verrouillage
    
    2004/06/18 11:56:02+02:00 loria.fr!samuel.thibault
    Rename: marcel/source/scheduler-marcel/bubble_sched.c -> marcel/source/scheduler-marcel/marcel_bubble_sched.c
    
    (Logical change 1.1225)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11472 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/source/scheduler-marcel/marcel_bubble_sched.c
@@ -0,0 +1,157 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+
+#ifdef MA__LWPS
+#define SCHED_LEVEL_INIT .sched_level = MARCEL_LEVEL_DEFAULT,
+#else
+#define SCHED_LEVEL_INIT
+#endif
+
+#define MARCEL_BUBBLE_INITIALIZER(b) { \
+	.heldentities = LIST_HEAD_INIT((b).heldentities), \
+	.nbrunning = 0, \
+	.lock = MA_SPIN_LOCK_UNLOCKED, \
+	.status = MA_BUBBLE_CLOSED, \
+	.sched = { \
+		.type = MARCEL_BUBBLE_ENTITY, \
+		.run_list = LIST_HEAD_INIT((b).sched.run_list), \
+		.init_rq = NULL, .cur_rq = NULL, \
+		.array = NULL, \
+		.prio = MA_BATCH_PRIO, \
+		.timestamp = 0, .last_ran = 0, \
+		.time_slice = MA_ATOMIC_INIT(0), \
+		SCHED_LEVEL_INIT \
+	}, \
+}
+
+marcel_bubble_t marcel_root_bubble = MARCEL_BUBBLE_INITIALIZER(marcel_root_bubble);
+
+MA_DEFINE_PER_LWP(marcel_bubble_t *, bubble_towake, NULL);
+
+int marcel_bubble_init(marcel_bubble_t *bubble) {
+	*bubble = (marcel_bubble_t) MARCEL_BUBBLE_INITIALIZER(*bubble);
+	return 0;
+}
+
+int marcel_entity_setschedlevel(marcel_bubble_entity_t *entity, int level) {
+#ifdef MA__LWPS
+	if (level>marcel_topo_nblevels-1)
+		level=marcel_topo_nblevels-1;
+	entity->sched_level = level;
+#endif
+	return 0;
+}
+int marcel_entity_getschedlevel(__const marcel_bubble_entity_t *entity, int *level) {
+#ifdef MA__LWPS
+	*level = entity->sched_level;
+#else
+	*level = 0;
+#endif
+	return 0;
+}
+
+int marcel_bubble_setprio(marcel_bubble_t *bubble, int prio) {
+	bubble->sched.prio = prio;
+	return 0;
+}
+
+int marcel_bubble_getprio(__const marcel_bubble_t *bubble, int *prio) {
+	*prio = bubble->sched.prio;
+	return 0;
+}
+
+int __marcel_bubble_insertentity(marcel_bubble_t *bubble, marcel_bubble_entity_t *entity) {
+	ma_runqueue_t *rq;
+	LOG_IN();
+	bubble_sched_debug("inserting %p in bubble %p\n",entity,bubble);
+	list_add(&entity->entity_list, &bubble->heldentities);
+	entity->holdingbubble=bubble;
+	if (bubble->status == MA_BUBBLE_OPENED
+			&& entity->type==MARCEL_BUBBLE_ENTITY) {
+		/* containing bubble already has exploded ! wake up this one */
+		rq=entity->init_rq=bubble->sched.init_rq;
+		ma_spin_lock_softirq(&rq->lock);
+		activate_entity(entity, rq);
+		bubble->nbrunning++;
+		ma_spin_unlock_softirq(&rq->lock);
+	}
+	LOG_OUT();
+	return 0;
+}
+
+int marcel_bubble_insertentity(marcel_bubble_t *bubble, marcel_bubble_entity_t *entity) {
+	ma_spin_lock_softirq(&bubble->lock);
+	__marcel_bubble_insertentity(bubble, entity);
+	ma_spin_unlock_softirq(&bubble->lock);
+	return 0;
+}
+
+void marcel_wake_up_bubble(marcel_bubble_t *bubble) {
+	LOG_IN();
+	bubble_sched_debug("waking up bubble %p\n",bubble);
+	if (!bubble->sched.init_rq)
+		bubble->sched.init_rq = &ma_main_runqueue;
+	ma_spin_lock_softirq(&bubble->sched.init_rq->lock);
+	activate_entity(&bubble->sched, bubble->sched.init_rq);
+	ma_spin_unlock_softirq(&bubble->sched.init_rq->lock);
+	LOG_OUT();
+}
+
+static void __marcel_close_bubble(marcel_bubble_t *bubble) {
+	marcel_bubble_entity_t *e;
+	marcel_task_t *t;
+	marcel_bubble_t *b;
+	ma_runqueue_t *rq;
+
+	bubble_sched_debug("bubble %p closing\n", bubble);
+	bubble->status = MA_BUBBLE_CLOSING;
+	list_for_each_entry(e, &bubble->heldentities, entity_list) {
+		if (e->type == MARCEL_BUBBLE_ENTITY) {
+			b = tbx_container_of(e, marcel_bubble_t, sched);
+			ma_spin_lock(&b->lock);
+			__marcel_close_bubble(b);
+			ma_spin_unlock(&b->lock);
+		} else {
+			t=tbx_container_of(e, marcel_task_t, sched.internal);
+			rq = task_rq_lock(t);
+			if (e->array) { /* not running */
+				bubble_sched_debug("deactivating task %s(%p) from %s\n", t->name, t, rq->name);
+				deactivate_task(t,rq);
+				bubble->nbrunning--;
+			}
+			task_rq_unlock(rq);
+		}
+	}
+}
+
+void marcel_close_bubble(marcel_bubble_t *bubble) {
+	int wake_bubble;
+	LOG_IN();
+
+	ma_spin_lock_softirq(&bubble->lock);
+	__marcel_close_bubble(bubble);
+	if ((wake_bubble = (!bubble->nbrunning))) {
+		bubble_sched_debug("last already off, bubble %p closed\n", bubble);
+		bubble->status = MA_BUBBLE_CLOSED;
+	}
+	ma_spin_unlock_softirq(&bubble->lock);
+
+	if (wake_bubble)
+		marcel_wake_up_bubble(bubble);
+	LOG_OUT();
+}

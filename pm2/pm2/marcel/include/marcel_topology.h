f1dc80e54714ad9d635f71271f8e7d88bb5a26f2
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-19 14:14:15 +0000

    option pour g�rer correctement l'idleness: si quelqu'un bosse sur l'autre cpu logique, on lui laisse le processeur !
    2004/11/26 16:37:55+01:00 ens-lyon.org!samuel.thibault
    correction: si un niveau n'a qu'une arit� de 1, ce n'est pas la peine de faire de runqueue...
    
    2004/11/26 16:10:04+01:00 ens-lyon.org!samuel.thibault
    SCCS merged
    
    2004/11/26 16:00:45+01:00 ens-lyon.org!samuel.thibault
    Auto merged
    
    2004/11/26 16:00:41+01:00 ens-lyon.org!samuel.thibault
    cpuset de type ma_cpu_set
    
    2004/11/26 15:07:19+01:00 ens-lyon.org!samuel.thibault
    ajout die
    
    2004/11/22 01:56:34+01:00 ens-lyon.org!samuel.thibault
    Auto merged
    
    2004/11/22 01:39:35+01:00 ens-lyon.org!samuel.thibault
    Auto merged
    
    2004/11/22 01:21:21+01:00 ens-lyon.org!samuel.thibault
    SCCS merged
    
    2004/11/22 01:08:35+01:00 ens-lyon.org!samuel.thibault
    warning trop verbeux
    
    2004/11/22 01:06:44+01:00 ens-lyon.org!samuel.thibault
    merge topology de bubble
    
    (Logical change 1.1225)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11502 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/include/marcel_topology.h
+++ pm2/pm2/marcel/include/marcel_topology.h
@@ -1,5 +1,4 @@
 
-
 /*
  * PM2: Parallel Multithreaded Machine
  * Copyright (C) 2004 "the PM2 team" (see AUTHORS file)
@@ -14,6 +13,22 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  */
+
+#section types
+enum marcel_topo_level_t {
+	MARCEL_LEVEL_MACHINE,
+#ifndef MA__LWPS
+#define MARCEL_LEVEL_LAST MARCEL_LEVEL_MACHINE
+#else
+#ifdef MA__NUMA
+	MARCEL_LEVEL_NODE,
+	MARCEL_LEVEL_DIE,
+	MARCEL_LEVEL_CORE,
+#endif
+	MARCEL_LEVEL_PROC,
+#define MARCEL_LEVEL_LAST MARCEL_LEVEL_PROC
+#endif
+};
 
 #section types
 #ifdef MA__LWPS
@@ -39,4 +54,87 @@ typedef unsigned ma_cpu_set_t;
 #define MA_CPU_CLR(cpu, cpusetp)	MA_CPU_ZERO(cpusetp)
 #define MA_CPU_ISSET(cpu, cpusetp)	(*(cpusetp))
 #define MA_CPU_WEIGHT(cpusetp)		(*(cpusetp))
+#endif
+
+#section structures
+// XXX TODO #warning il ne faudrait pas dépendre d un scheduler particulier
+#depend "scheduler-marcel/linux_runqueues.h[marcel_types]"
+#depend "[types]"
+#ifdef MARCEL_SMT_IDLE
+#depend "asm/linux_atomic.h[marcel_types]"
+#endif
+struct marcel_topo_level {
+	enum marcel_topo_level_t type;
+	unsigned number;
+
+	ma_cpu_set_t cpuset;
+
+	unsigned arity;
+	struct marcel_topo_level **sons;
+
+#ifdef MARCEL_SMT_IDLE
+	ma_atomic_t nbidle;
+#endif
+
+	ma_topo_level_schedinfo *sched;
+};
+
+#section variables
+extern unsigned marcel_topo_nblevels;
+extern struct marcel_topo_level marcel_machine_level[];
+extern struct marcel_topo_level *marcel_topo_levels[MARCEL_LEVEL_LAST+1];
+
+#section marcel_variables
+#ifdef MARCEL_SMT_IDLE
+MA_DECLARE_PER_LWP(struct marcel_topo_level *, core_level);
+#endif
+
+#section functions
+#depend "[variables]"
+static inline unsigned marcel_topo_arity(unsigned level) {
+	return marcel_topo_levels[level][0].arity;
+}
+
+#section marcel_functions
+#ifdef MARCEL_SMT_IDLE
+// return whether one should sleep (because other siblings are working)
+static inline void ma_topology_lwp_idle_start(ma_lwp_t lwp);
+static inline  int ma_topology_lwp_idle_core(ma_lwp_t lwp);
+static inline void ma_topology_lwp_idle_end(ma_lwp_t lwp);
+#else
+#define ma_topology_lwp_idle_start(lwp) (void)0
+#define ma_topology_lwp_idle_core(lwp) 1
+#define ma_topology_lwp_idle_end(lwp) (void)0
+#endif
+
+#section marcel_inline
+#ifdef MARCEL_SMT_IDLE
+static inline void ma_topology_lwp_idle_start(ma_lwp_t lwp) {
+	struct marcel_topo_level *level;
+	if ((level = ma_per_lwp(core_level,lwp)))
+		ma_atomic_inc(&level->nbidle);
+}
+
+static inline  int ma_topology_lwp_idle_core(ma_lwp_t lwp) {
+	struct marcel_topo_level *level;
+	if ((level = ma_per_lwp(core_level,lwp)))
+		return (ma_atomic_read(&level->nbidle) == level->arity);
+	return 1;
+}
+
+static inline void ma_topology_lwp_idle_end(ma_lwp_t lwp) {
+	struct marcel_topo_level *level;
+	if ((level = ma_per_lwp(core_level,lwp)))
+		ma_atomic_dec(&level->nbidle);
+}
+#endif
+
+#section marcel_macros
+#ifdef MA__LWPS
+#define MARCEL_NBMAXCPUS	(sizeof(unsigned long)*8)
+#ifdef MA__NUMA
+#define MARCEL_NBMAXCORES	16
+#define MARCEL_NBMAXDIES	8
+#define MARCEL_NBMAXNODES	8
+#endif
 #endif

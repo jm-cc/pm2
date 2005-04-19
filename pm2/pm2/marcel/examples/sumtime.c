201ce01f6123269d554934b678ae52f76d962a55
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-19 14:14:15 +0000

    il faut que le premier thread de comm h�rite de la bulle root
    2004/09/29 12:34:35+02:00 ens-lyon.org!samuel.thibault
    les bulles de sumtime sont bien plus jolies ainsi
    
    2004/06/14 16:01:49+02:00 loria.fr!samuel.thibault
    utiliser la macro ma_lwp_rq, qui marche en mono, elle
    
    2004/06/14 12:07:33+02:00 loria.fr!samuel.thibault
    open_level -> sched_level
    
    2004/06/02 11:49:41+02:00 loria.fr!samuel.thibault
    on utilise des entiers pour les niveaux
    
    2004/06/02 11:41:29+02:00 loria.fr!samuel.thibault
    correction topologie: les applications donnent plutot un num�ro, � nous de l'interpr�ter
    
    2004/05/28 10:12:09+02:00 loria.fr!samuel.thibault
    ajout de quoi comparer avec le cas confin� � un processeur
    
    2004/05/26 10:54:18+02:00 loria.fr!samuel.thibault
    correction niveaux d'ouverture
    
    2004/05/25 18:25:25+02:00 loria.fr!samuel.thibault
    correction utilisation des bulles
    
    2004/05/19 16:41:08+02:00 loria.fr!samuel.thibault
    d�ploiement de bulles, avec des threads de comm. On suppose avoir un nombre puissance de deux de processeurs
    
    (Logical change 1.1225)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11494 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/marcel/examples/sumtime.c
+++ pm2/pm2/marcel/examples/sumtime.c
@@ -18,18 +18,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CPU0ONLY
+//#define dprintf(fmt,args...) fprintf(stderr,fmt,##args)
+#define dprintf(fmt,args...) (void)0
+
+#define MAX_BUBBLE_LEVEL 6
+
 typedef struct {
   int inf, sup, res;
   marcel_sem_t sem;
+  int level;
 } job;
 
-marcel_attr_t attr;
+marcel_attr_t attr,commattr;
 
-static __inline__ void job_init(job *j, int inf, int sup)
+static __inline__ void job_init(job *j, int inf, int sup, int level)
 {
   j->inf = inf;
   j->sup = sup;
   marcel_sem_init(&j->sem, 0);
+  j->level = level;
 }
 
 any_t sum(any_t arg)
@@ -37,20 +45,38 @@ any_t sum(any_t arg)
   job *j = (job *)arg;
   job j1, j2;
 
+  dprintf("sum(%p,%p,%p) started\n",j,&j1,&j2);
+
   if(j->inf == j->sup) {
     j->res = j->inf;
     marcel_sem_V(&j->sem);
     return NULL;
   }
 
-  job_init(&j1, j->inf, (j->inf+j->sup)/2);
-  job_init(&j2, (j->inf+j->sup)/2+1, j->sup);
+  job_init(&j1, j->inf, (j->inf+j->sup)/2, j->level + 1);
+  job_init(&j2, (j->inf+j->sup)/2+1, j->sup, j->level + 1);
 
-  marcel_create(NULL, &attr, sum, (any_t)&j1);
-  marcel_create(NULL, &attr, sum, (any_t)&j2);
-
-  marcel_sem_P(&j1.sem);
-  marcel_sem_P(&j2.sem);
+  if (j->level<MAX_BUBBLE_LEVEL) {
+    marcel_bubble_t b1,b2;
+    marcel_t p1,p2;
+    marcel_bubble_init(&b1);
+    marcel_bubble_init(&b2);
+    marcel_bubble_setschedlevel(&b1,j->level);
+    marcel_bubble_setschedlevel(&b2,j->level);
+    marcel_create_dontsched(&p1, &commattr, sum, (any_t)&j1);
+    marcel_create_dontsched(&p2, &commattr, sum, (any_t)&j2);
+    marcel_bubble_inserttask(&b1, p1);
+    marcel_bubble_inserttask(&b2, p2);
+    marcel_bubble_insertbubble(marcel_bubble_holding_task(marcel_self()),&b1);
+    marcel_bubble_insertbubble(marcel_bubble_holding_task(marcel_self()),&b2);
+    marcel_sem_P(&j1.sem);
+    marcel_sem_P(&j2.sem);
+  } else {
+    marcel_create(NULL, &attr, sum, (any_t)&j1);
+    marcel_create(NULL, &attr, sum, (any_t)&j2);
+    marcel_sem_P(&j1.sem);
+    marcel_sem_P(&j2.sem);
+  }
 
   j->res = j1.res+j2.res;
   marcel_sem_V(&j->sem);
@@ -59,10 +85,24 @@ any_t sum(any_t arg)
 
 tbx_tick_t t1, t2;
 
+void compute(job j) {
+  int nbvps;
+  unsigned long temps;
+
+  nbvps = marcel_nbvps();
+  TBX_GET_TICK(t1);
+  marcel_create(NULL, &attr, sum, (any_t)&j);
+  marcel_sem_P(&j.sem);
+
+  TBX_GET_TICK(t2);
+  printf("Sum from 1 to %d = %d\n", j.sup, j.res);
+  temps = TBX_TIMING_DELAY(t1, t2);
+  printf("time = %ld.%03ldms\n", temps/1000, temps%1000);
+}
+
 int marcel_main(int argc, char **argv)
 {
   job j;
-  unsigned long temps;
 
   marcel_trace_on();
   marcel_init(&argc, argv);
@@ -70,22 +110,28 @@ int marcel_main(int argc, char **argv)
   marcel_attr_init(&attr);
   marcel_attr_setdetachstate(&attr, TRUE);
   marcel_attr_setschedpolicy(&attr, MARCEL_SCHED_AFFINITY);
+  marcel_attr_setstayinbubble(&attr, TRUE);
+#ifdef CPU0ONLY
+  marcel_attr_setvpmask(&attr, MARCEL_VPMASK_ALL_BUT_VP(0));
+#endif
+
+  marcel_attr_init(&commattr);
+  marcel_attr_setdetachstate(&commattr, TRUE);
+  marcel_attr_setschedpolicy(&commattr, MARCEL_SCHED_AFFINITY);
+#ifdef CPU0ONLY
+  marcel_attr_setvpmask(&commattr, MARCEL_VPMASK_ALL_BUT_VP(0));
+#endif
 
   marcel_sem_init(&j.sem, 0);
   j.inf = 1;
+  j.level = 0;
   if(argc > 1) {
     // Execution unique en utilisant l'argument en ligne de commande
 #ifdef PROFILE
    profile_activate(FUT_ENABLE, MARCEL_PROF_MASK, 0);
 #endif
     j.sup = atoi(argv[1]);
-    TBX_GET_TICK(t1);
-    marcel_create(NULL, &attr, sum, (any_t)&j);
-    marcel_sem_P(&j.sem);
-    TBX_GET_TICK(t2);
-    printf("Sum from 1 to %d = %d\n", j.sup, j.res);
-    temps = TBX_TIMING_DELAY(t1, t2);
-    printf("time = %ld.%03ldms\n", temps/1000, temps%1000);
+    compute(j);
 #ifdef PROFILE
    profile_stop();
 #endif
@@ -94,15 +140,9 @@ int marcel_main(int argc, char **argv)
     LOOP(bcle)
       printf("Enter a rather small integer (0 to quit) : ");
       scanf("%d", &j.sup);
-      if(j.sup == 0)
+      if(j.sup <= 0)
 	EXIT_LOOP(bcle);
-      TBX_GET_TICK(t1);
-      marcel_create(NULL, &attr, sum, (any_t)&j);
-      marcel_sem_P(&j.sem);
-      TBX_GET_TICK(t2);
-      printf("Sum from 1 to %d = %d\n", j.sup, j.res);
-      temps = TBX_TIMING_DELAY(t1, t2);
-      printf("time = %ld.%03ldms\n", temps/1000, temps%1000);
+      compute(j);
     END_LOOP(bcle)
   }
 

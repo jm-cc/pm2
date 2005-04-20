babaeb203bd85090df5f15477b077785cec381f5
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-20 15:55:06 +0000

    mise � jour fxt
    2005/04/20 17:50:17+02:00 ens-lyon.org!samuel.thibault
    Rename: profile/include/fkt-tools.h -> profile/include/pm2_fxt-tools.h
    
    (Logical change 1.1226)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11527 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/profile/include/pm2_fxt-tools.h
@@ -0,0 +1,12 @@
+#ifndef __PM2_FXT_TOOLS_H__
+#define __PM2_FXT_TOOLS_H__
+#include <fxt.h>
+#undef DEFAULT_TRACE_FILE
+#ifndef FUT
+#define DEFAULT_TRACE_FILE "trace_file"
+#else
+#define DEFAULT_TRACE_FILE "fut_trace_file"
+#endif
+extern void dumptime(time_t *the_time, clock_t *the_jiffies);
+extern fxt_t fut;
+#endif /* __PM2_FXT_TOOLS_H__ */

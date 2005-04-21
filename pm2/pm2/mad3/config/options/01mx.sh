6c5983316f95169f2b5a7a3d8907e2ecc17e4d7a
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-04-21 11:21:33 +0000

    - pas d'ajout de -lpthread si MX est install� sans support Pthread
    (Logical change 1.1231)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11546 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/pm2/mad3/config/options/01mx.sh
+++ pm2/pm2/mad3/config/options/01mx.sh
@@ -9,8 +9,8 @@ PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD3_MAD1_MAIN_PROTO=mad_MX"
 PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD3_MAD1_MAIN_PROTO_PARAM=NULL"
 
 
-# mx_open_endpoint calls pthread_create, we need to link libpthread
-# (or disable the use of pthread in MX)
-
-PM2_MAD3_CFLAGS="$PM2_MAD3_CFLAGS -D_REENTRANT"
-PM2_MAD3_LIBS="$PM2_MAD3_LIBS -lpthread"
+## mx_open_endpoint calls pthread_create, we need to link libpthread
+## (or disable the use of pthread in MX)
+#
+#PM2_MAD3_CFLAGS="$PM2_MAD3_CFLAGS -D_REENTRANT"
+#PM2_MAD3_LIBS="$PM2_MAD3_LIBS -lpthread"

4ec159aa52a9a6bc788c1413165ac48a98af488c
labri.fr!olivier.aumage <labri.fr!olivier.aumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-05-30 14:19:21 +0000

    (Logical change 1.1290)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11745 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/tbx/include/tbx_darray_inline.h
@@ -0,0 +1,306 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * tbx_darray_inline.h
+ * :::::::::::::::::::::::////////////////////////////////////////////////////
+ */
+
+
+/*
+ * Macros
+ * ___________________________________________________________________________
+ */
+#define TBX_DARRAY_MIN_SIZE 16
+
+/*
+ *  Functions
+ * ___________________________________________________________________________
+ */
+
+
+static
+__inline__
+p_tbx_darray_t
+tbx_darray_init(void)
+{
+  p_tbx_darray_t darray = NULL;
+
+  LOG_IN();
+  darray = TBX_CALLOC(1, sizeof(tbx_darray_t));
+  CTRL_ALLOC(darray);
+
+  TBX_INIT_SHARED(darray);
+  darray->length           =    0;
+  darray->allocated_length =    0;
+  darray->data             = NULL;
+  LOG_OUT();
+
+  return darray;
+}
+
+static
+__inline__
+void
+tbx_darray_free(p_tbx_darray_t darray)
+{
+  LOG_IN();
+  tbx_darray_reset(darray);
+  TBX_FREE(darray);
+  LOG_OUT();
+}
+
+static
+__inline__
+size_t
+tbx_darray_length(p_tbx_darray_t darray)
+{
+  size_t length = 0;
+
+  LOG_IN();
+  length = darray->length;
+  LOG_OUT();
+
+  return length;
+}
+
+static
+__inline__
+void
+tbx_darray_reset(p_tbx_darray_t darray)
+{
+  LOG_IN();
+  if (darray->allocated_length)
+    {
+      TBX_FREE(darray->data);
+    }
+
+  darray->length           =    0;
+  darray->allocated_length =    0;
+  darray->data             = NULL;
+  LOG_OUT();
+}
+
+static
+void
+__tbx_darray_grow(p_tbx_darray_t     darray,
+		  tbx_darray_index_t idx)
+{
+  LOG_IN();
+  idx++;
+
+  if (darray->allocated_length)
+    {
+      if (idx > darray->allocated_length)
+	{
+	  darray->allocated_length =
+	    tbx_max(2 * (darray->allocated_length + 1), idx);
+
+	  darray->data =
+	    TBX_REALLOC(darray->data,
+			darray->allocated_length * sizeof(void *));
+	  CTRL_ALLOC(darray->data);
+	}
+    }
+  else
+    {
+      darray->allocated_length =  tbx_max(idx, TBX_DARRAY_MIN_SIZE);
+
+      darray->data = TBX_CALLOC(darray->allocated_length, sizeof(void *));
+      CTRL_ALLOC(darray->data);
+    }
+  LOG_OUT();
+}
+
+static
+__inline__
+void *
+tbx_darray_get(p_tbx_darray_t     darray,
+	       tbx_darray_index_t idx)
+{
+  void *result = NULL;
+
+  LOG_IN();
+  if (idx < 0)
+    {
+      idx = darray->length - idx;
+    }
+
+  if (idx < 0)
+    FAILURE("out-of-bounds index");
+
+  if (idx < darray->length)
+    {
+      result = darray->data[idx];
+    }
+  LOG_OUT();
+
+  return result;
+}
+
+static
+__inline__
+void
+tbx_darray_set(p_tbx_darray_t      darray,
+	       tbx_darray_index_t  idx,
+	       void               *object)
+{
+  LOG_IN();
+  if (idx < 0)
+    {
+      idx = darray->length - idx;
+    }
+
+  if (idx < 0 || idx >= darray->length)
+    FAILURE("out-of-bounds index");
+
+  darray->data[idx] = object;
+  LOG_OUT();
+}
+
+static
+__inline__
+void
+tbx_darray_expand_and_set(p_tbx_darray_t      darray,
+			  tbx_darray_index_t  idx,
+			  void               *object)
+{
+  LOG_IN();
+  if (idx < 0)
+    FAILURE("invalid index");
+
+  if (idx >= darray->length)
+    {
+      if (idx >= darray->allocated_length)
+	{
+	  __tbx_darray_grow(darray, idx);
+	}
+
+      memset(darray->data + darray->length, 0,
+	     sizeof(void *) * ((idx + 1) - darray->length));
+
+      darray->length = idx + 1;
+    }
+
+  darray->data[idx] = object;
+  LOG_OUT();
+}
+
+static
+__inline__
+void *
+tbx_darray_expand_and_get(p_tbx_darray_t     darray,
+			  tbx_darray_index_t idx)
+{
+  void *object = NULL;
+
+  LOG_IN();
+  if (idx < 0)
+    FAILURE("invalid index");
+
+  if (idx >= darray->length)
+    {
+      if (idx >= darray->allocated_length)
+	{
+	  __tbx_darray_grow(darray, idx);
+	}
+
+      memset(darray->data + darray->length, 0,
+	     sizeof(void *) * ((idx + 1) - darray->length));
+
+      darray->length = idx + 1;
+    }
+
+  object = darray->data[idx];
+  LOG_OUT();
+
+  return object;
+}
+
+static
+__inline__
+void
+tbx_darray_append(p_tbx_darray_t  darray,
+		  void           *object)
+{
+  LOG_IN();
+  if (darray->length == darray->allocated_length)
+    {
+      __tbx_darray_grow(darray, darray->length);
+    }
+  darray->data[darray->length] = object;
+  darray->length++;
+  LOG_OUT();
+}
+
+static
+__inline__
+void *
+tbx_darray_first_idx(p_tbx_darray_t       darray,
+		     p_tbx_darray_index_t idx)
+{
+  void *result = NULL;
+
+  LOG_IN();
+  if (darray->length)
+    {
+      *idx = 0;
+
+      do
+	{
+	  if (darray->data[*idx])
+	    {
+	      result = darray->data[*idx];
+	      break;
+	    }
+
+	  (*idx)++;
+	}
+      while (*idx < darray->length);
+    }
+
+  LOG_OUT();
+
+  return result;
+}
+
+static
+__inline__
+void *
+tbx_darray_next_idx(p_tbx_darray_t       darray,
+		    p_tbx_darray_index_t idx)
+{
+  void *result = NULL;
+
+  LOG_IN();
+  if (darray->length)
+    {
+      (*idx)++;
+
+      while (*idx < darray->length)
+	{
+	  if (darray->data[*idx])
+	    {
+	      result = darray->data[*idx];
+	      break;
+	    }
+
+	  (*idx)++;
+	}
+    }
+  LOG_OUT();
+
+  return result;
+}
---
+++ pm2/pm2/tbx/include/tbx_list_inline.h
@@ -0,0 +1,395 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * tbx_list_inline.h
+ * -----------------
+ */
+extern p_tbx_memory_t tbx_list_manager_memory;
+
+static
+__inline__
+void
+tbx_list_init(p_tbx_list_t list)
+{
+  list->length     = 0;
+  list->read_only = tbx_false;
+  list->mark_next = tbx_true; /* mark is always set
+				 on the first element */
+}
+
+static
+__inline__
+void
+tbx_destroy_list(p_tbx_list_t list)
+{
+  if (list->length)
+    {
+      p_tbx_list_element_t list_element ;
+
+      list_element = list->first ;
+
+      while (list_element != list->last)
+	{
+	  p_tbx_list_element_t temp_list_element = list_element->next;
+
+	  tbx_free(tbx_list_manager_memory, list_element);
+	  list_element = temp_list_element;
+	}
+
+      tbx_free(tbx_list_manager_memory, list->last);
+
+      list->length = 0 ;
+    }
+}
+
+static
+__inline__
+void
+tbx_foreach_destroy_list(p_tbx_list_t                list,
+			 p_tbx_list_foreach_func_t   func)
+{
+  if (list->length)
+    {
+      p_tbx_list_element_t list_element ;
+
+      list_element = list->first ;
+
+      while (list_element != list->last)
+	{
+	  p_tbx_list_element_t temp_list_element = list_element->next;
+
+	  func(list_element->object);
+	  tbx_free(tbx_list_manager_memory, list_element);
+	  list_element = temp_list_element;
+	}
+
+      func(list_element->object);
+      tbx_free(tbx_list_manager_memory, list->last);
+
+      list->length = 0 ;
+    }
+}
+
+/*
+ * Put/Get
+ * -------
+ */
+static
+__inline__
+void
+tbx_append_list(p_tbx_list_t   list,
+		void          *object)
+{
+  p_tbx_list_element_t list_element ;
+
+  if (list->read_only)
+    FAILURE("attempted to modify a read only list");
+
+  list_element = tbx_malloc(tbx_list_manager_memory);
+
+  list_element->object = object;
+  list_element->next = NULL ;
+
+  if (list->length)
+    {
+      list_element->previous = list->last ;
+      list_element->previous->next = list_element ;
+    }
+  else
+    {
+      /* This is the first list element */
+
+      list->first = list_element ;
+      list_element->previous = NULL ;
+    }
+
+  list->last = list_element ;
+  list->length++;
+
+  if (list->mark_next)
+    {
+      list->mark = list_element ;
+      list->mark_position = list->length - 1;
+      list->mark_next = tbx_false ;
+    }
+}
+
+static
+__inline__
+void *
+tbx_get_list_object(p_tbx_list_t list)
+{
+  if (list->length)
+    {
+      return list->last->object;
+    }
+  else
+    FAILURE("empty list");
+}
+
+
+/*
+ * list mark control function
+ * --------------------------
+ */
+
+static
+__inline__
+void
+tbx_mark_list(p_tbx_list_t list)
+{
+  /* start a new sub list -> the next element
+     added to the list will be the first element
+     of the new sub list */
+  list->mark_next = tbx_true;
+}
+
+static
+__inline__
+tbx_bool_t
+tbx_empty_list(p_tbx_list_t list)
+{
+  if (list->length)
+    {
+      return tbx_false ;
+    }
+  else
+    {
+      return tbx_true ;
+    }
+}
+
+/*
+ * sub-list extraction
+ * -------------------
+ */
+
+static
+__inline__
+void
+tbx_duplicate_list(p_tbx_list_t   source,
+		   p_tbx_list_t   destination)
+{
+  *destination = *source ;
+  destination->mark          = destination->first ;
+  destination->mark_position = 0;
+  destination->read_only     = tbx_true;
+}
+
+static
+__inline__
+void
+tbx_extract_sub_list(p_tbx_list_t   source,
+		     p_tbx_list_t   destination)
+{
+  if (source->length)
+    {
+      destination->first         = source->mark ;
+      destination->last          = source->last ;
+      destination->length        = source->length - source->mark_position;
+      destination->mark          = destination->first ;
+      destination->mark_position = 0;
+      destination->read_only     = tbx_true;
+    }
+  else
+    FAILURE("empty list");
+}
+
+/*
+ * list reference functions
+ * ------------------------
+ */
+
+static
+__inline__
+void
+tbx_list_reference_init(p_tbx_list_reference_t   ref,
+			p_tbx_list_t             list)
+{
+  ref->list = list;
+
+  if (list->length)
+    {
+      ref->reference = ref->list->first;
+      ref->after_end = tbx_false;
+    }
+  else
+    {
+      ref->reference = NULL;
+      ref->after_end = tbx_true;
+    }
+}
+
+static
+__inline__
+void *
+tbx_get_list_reference_object(p_tbx_list_reference_t ref)
+{
+  if (!ref->reference)
+    {
+      if (ref->list->length)
+	{
+	  ref->reference = ref->list->first;
+	  ref->after_end = tbx_false;
+	}
+    }
+
+  if (ref->list->length)
+    {
+      if (ref->reference)
+	{
+	  if (ref->after_end)
+	    {
+	      if (ref->reference != ref->list->last)
+		{
+		  ref->reference = ref->reference->next;
+		  ref->after_end = tbx_false;
+		  return ref->reference->object;
+		}
+	      else
+		{
+		  /* reference passed the end of the list */
+		  return NULL;
+		}
+	    }
+	  else
+	    {
+	      return ref->reference->object;
+	    }
+	}
+      else
+	{
+	  /* the referenced list was empty when
+	     the reference was initialized */
+
+	  ref->reference = ref->list->first;
+	  ref->after_end = tbx_false;
+
+	  return ref->reference->object;
+	}
+    }
+  else
+    FAILURE("empty list");
+}
+
+/*
+ * Cursor control function
+ * -----------------------
+ */
+static
+__inline__
+tbx_bool_t
+tbx_forward_list_reference(p_tbx_list_reference_t ref)
+{
+  if (!ref->reference)
+    {
+      if (ref->list->length)
+	{
+	  ref->reference = ref->list->first;
+	  ref->after_end = tbx_false;
+	}
+    }
+
+  if (ref->list->length)
+    {
+      if (ref->after_end)
+	{
+	  if (ref->reference != ref->list->last)
+	    {
+	      ref->reference = ref->reference->next;
+	      ref->after_end = tbx_false;
+	      return tbx_true ;
+	    }
+	  else
+	    {
+	      return tbx_false;
+	    }
+	}
+      else
+	{
+	  if (ref->reference != ref->list->last)
+	    {
+	      ref->reference = ref->reference->next ;
+	      return tbx_true ;
+	    }
+	  else
+	    {
+	      ref->after_end = tbx_true ;
+
+	      return tbx_false ;
+	    }
+	}
+    }
+  else
+    FAILURE("empty list");
+}
+
+static
+__inline__
+void
+tbx_reset_list_reference(p_tbx_list_reference_t ref)
+{
+  if (ref->list->length)
+    {
+      ref->reference = ref->list->first;
+      ref->after_end = tbx_false ;
+    }
+  else
+    {
+      ref->reference = NULL;
+      ref->after_end = tbx_true;
+    }
+}
+
+static
+__inline__
+tbx_bool_t
+tbx_reference_after_end_of_list(p_tbx_list_reference_t ref)
+{
+  LOG_IN();
+  if (!ref->reference)
+    {
+      if (ref->list->length)
+	{
+	  ref->reference = ref->list->first;
+	  ref->after_end = tbx_false;
+	}
+    }
+
+  if (ref->list->length)
+    {
+      if (ref->after_end)
+	{
+	  if (ref->reference != ref->list->last)
+	    {
+	      ref->reference = ref->reference->next;
+	      ref->after_end = tbx_false;
+	    }
+	  LOG_OUT();
+	  return ref->after_end;
+	}
+      else
+	{
+	  LOG_OUT();
+	  return tbx_false;
+	}
+    }
+  else
+    {
+      LOG_OUT();
+      return tbx_true;
+    }
+}

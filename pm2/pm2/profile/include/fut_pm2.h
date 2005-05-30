58c4c2044f56d13368e676118517775c1f28190f
labri.fr!Vincent.Danjean <labri.fr!Vincent.Danjean@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-05-30 16:13:52 +0000

    Auto merged
    2005/05/17 18:11:56+02:00 labri.fr!Vincent.Danjean
    PROF_NEW_LWP doit enregistrer des infos utiles...
    
    2005/05/17 17:02:37+02:00 ens-lyon.org!samuel.thibault
    correction compilation: il ne serait pas inclu au preprocessing sinon
    
    2005/05/15 03:06:37+02:00 labri.fr!Vincent.Danjean
    Remise � plat des macros de trace
    
    2005/05/14 20:36:51+02:00 labri.fr!Vincent.Danjean
    bk cp pm2_profile.h ../../profile/include/fut_pm2.h
    
    2005/05/12 16:02:09+02:00 ens-lyon.org!samuel.thibault
    suite merge
    
    2005/05/12 15:59:53+02:00 ens-lyon.org!samuel.thibault
    c'�tait parti dans le merge...
    
    2005/05/12 15:48:48+02:00 ens-lyon.org!samuel.thibault
    merge
    
    2005/05/12 15:43:30+02:00 ens-lyon.org!samuel.thibault
    ajout macro PROF_ALWAYS_PROBE()
    
    2005/04/27 17:10:53+02:00 ens-lyon.org!samuel.thibault
    correction compilation + �limination enregistrement de marcel_self()->number, qui est effectu� par fut_header
    
    2005/04/26 15:14:34+02:00 ens-lyon.org!samuel.thibault
    merge
    
    2005/04/26 15:12:53+02:00 ens-lyon.org!samuel.thibault
    ajout PROF_EVENT1/PROF_EVENT2
    
    2005/04/21 17:00:55+02:00 ens-lyon.org!samuel.thibault
    correction marcel numa
    
    2004/06/24 14:51:09+02:00 loria.fr!samuel.thibault
    ajout� l'�v�nement set_thread_name pour avoir les noms des threads
    
    2004/06/23 01:11:56+02:00 labri.fr!Vincent.Danjean
    Profiling revu
    
    2004/03/09 11:33:56+01:00 loria.fr!samuel.thibault
    remis marcel_self()->number dans les probes
    
    2004/03/04 11:10:09+01:00 loria.fr!samuel.thibault
    �limin� le probl�me de la concat�nation de __FUNCTION__: on utilise simplement __FUNCTION__, ce qui permet de r�cup�rer au moins l'adresse de la fonction, qu'on peut traiter exactement comme l'auto-profiling (-finstrument-functions)
    aussi ajout� le prototype de fkt_new_lwp, qui n'est plus un appel syst�me
    
    2004/02/19 15:08:37+01:00 loria.fr!samuel.thibault
    gcc ne distinguait pas les extern int foo asm("blabla_exit_code") et extern int foo asm("blabla_entry_code") meme s'ils sont dans des blocs diff�rents. Solution: ajouter le num�ro de line � foo
    
    2003/09/05 20:26:50+02:00 ens-lyon.fr!vdanjean
    rev 1.10 at 2003/06/11 10:48:21 GMT
     previous commit was an error
    
    2003/09/05 20:26:29+02:00 ens-lyon.fr!vdanjean
    rev 1.9 at 2003/06/11 10:46:03 GMT
     add 'lock' prefix for LWPS versions for X86 pm2_spinlock_testandset
    
    2003/09/05 13:53:43+02:00 ens-lyon.fr!cmenier
    rev 1.8 at 2001/05/31 13:26:24 GMT
     For profiling: change fut_new_lwp to fkt_mew_lwp
    
    2003/09/05 13:25:14+02:00 ens-lyon.fr!rnamyst
    rev 1.7 at 2001/05/18 14:19:49 GMT
     I've modified the Makefiles to insert a pre-support for 'user application profiling'. It is not yet complete.
    
    2003/09/05 13:13:36+02:00 ens-lyon.fr!rnamyst
    rev 1.6 at 2001/05/10 16:18:50 GMT
     Integrated FKT in PM2 + modified FUT to include the current thread number in SMP mode
    
    2003/09/05 13:06:50+02:00 ens-lyon.fr!rnamyst
    rev 1.5 at 2001/05/02 19:47:55 GMT
     Added profile events THREAD_BIRTH & THREAD_DEATH
    
    2003/09/05 13:05:14+02:00 ens-lyon.fr!rnamyst
    rev 1.4 at 2001/05/01 18:50:11 GMT
     New profile features: added 'NEW_LWP' event and modified SWITCH_TO. Note that THR_LONGJMP was modified for this purpose...
    
    2003/09/05 10:37:21+02:00 ens-lyon.fr!rnamyst
    rev 1.3 at 2001/03/02 16:28:20 GMT
     Merged the stable-mars branch (again\!)
    
    2003/09/05 09:57:02+02:00 ens-lyon.fr!rnamyst
    rev 1.1.2.2 at 2001/02/28 11:55:03 GMT
     Modified the copyrights in all file (to reference the author file) + added the dsm/test directory.
    
    2003/09/05 09:47:41+02:00 ens-lyon.fr!rnamyst
    rev 1.2 at 2001/02/27 12:52:18 GMT
     Merged the stable-mars branch into the trunk.
    
    (Logical change 1.1291)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11755 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/profile/include/fut_pm2.h
@@ -0,0 +1,213 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef FUT_PM2_IS_DEF
+#define FUT_PM2_IS_DEF
+
+#define MARCEL_PROF_MASK  0x01
+#define MAD_PROF_MASK     0x02
+#define PM2_PROF_MASK     0x04
+#define DSM_PROF_MASK     0x08
+#define TBX_PROF_MASK     0x10
+#define NTBX_PROF_MASK    0x20
+#define USER_APP_MASK     0x40
+
+#if defined(MARCEL_KERNEL)
+#define PROFILE_KEYMASK MARCEL_PROF_MASK
+#elif defined(MAD2_KERNEL) || defined(MAD1_KERNEL)
+#define PROFILE_KEYMASK MAD_PROF_MASK
+#elif defined(PM2_KERNEL)
+#define PROFILE_KEYMASK PM2_PROF_MASK
+#elif defined(DSM_KERNEL)
+#define PROFILE_KEYMASK DSM_PROF_MASK
+#elif defined(TBX_KERNEL)
+#define PROFILE_KEYMASK TBX_PROF_MASK
+#elif defined(NTBX_KERNEL)
+#define PROFILE_KEYMASK NTBX_PROF_MASK
+#else
+#define PROFILE_KEYMASK USER_APP_MASK
+#endif
+
+#if !defined(MODULE) && defined(COMMON_PROFILE)
+// Application utilisateur
+#define DO_PROFILE
+#endif
+
+#define CONFIG_FUT
+#include "fxt/fxt.h"
+#include "fxt/fut.h"
+
+#define PROF_START                                                  \
+ do {                                                               \
+   if (__pm2_profile_active) {
+#define PROF_END                                                    \
+   }                                                                \
+ } while(0)
+
+#ifdef DO_PROFILE
+
+#define PROF_PROBE(keymask, code, ...)                    \
+  FUT_PROBE(keymask, code , ##__VA_ARGS__)
+
+#define PROF_ALWAYS_PROBE(code, ...)                      \
+ PROF_START                                               \
+      FUT_DO_PROBE(code, ##__VA_ARGS__);                  \
+ PROF_END
+
+#define GEN_PREPROC(name) _GEN_PREPROC(name,__LINE__)
+#define _GEN_PREPROC(name,line) __GEN_PREPROC(name,line)
+#define GEN_PREPROC1(name,arg1) _GEN_PREPROC1(name,__LINE__,arg1)
+#define _GEN_PREPROC1(name,line,arg1) __GEN_PREPROC1(name,line,arg1)
+#define GEN_PREPROC2(name,arg1,arg2) _GEN_PREPROC2(name,__LINE__,arg1,arg2)
+#define _GEN_PREPROC2(name,line,arg1,arg2) __GEN_PREPROC2(name,line,arg1,arg2)
+
+#ifdef PREPROC
+
+#define __GEN_PREPROC(name,line)                          \
+  do {                                                    \
+    extern int foo##line asm ("this_is_the_fut_" name "_code"); \
+    foo##line = 1;                                              \
+  } while(0)
+#define __GEN_PREPROC1(name,line,arg1) __GEN_PREPROC(name,line)
+#define __GEN_PREPROC2(name,line,arg1,arg2) __GEN_PREPROC(name,line)
+
+#else // ifndef PREPROC
+
+#define __GEN_PREPROC(name,line)                          \
+  do {                                                    \
+    extern unsigned __code##line asm("fut_" name "_code");\
+    FUT_PROBE0(PROFILE_KEYMASK, __code##line);            \
+  } while(0)
+
+#define __GEN_PREPROC1(name,line,arg1)                    \
+  do {                                                    \
+    extern unsigned __code##line asm("fut_" name "_code");\
+    FUT_PROBE1(PROFILE_KEYMASK, __code##line, arg1);      \
+  } while(0)
+
+#define __GEN_PREPROC2(name,line,arg1,arg2)               \
+  do {                                                    \
+    extern unsigned __code##line asm("fut_" name "_code");\
+    FUT_PROBE2(PROFILE_KEYMASK, __code##line, arg1,arg2); \
+  } while(0)
+
+#endif // PREPROC
+
+#if 0
+/* __FUNCTION__ can't be catenated with strings litterals, since it may be a
+ * variable */
+#define PROF_IN()            GEN_PREPROC(__FUNCTION__ "_entry")
+#define PROF_OUT()           GEN_PREPROC(__FUNCTION__ "_exit")
+#else
+#define PROF_IN()					\
+  do {							\
+    extern void fun(void) asm(__FUNCTION__);		\
+    /*__cyg_profile_func_enter(fun,NULL);	*/	\
+    FUT_PROBE1(PROFILE_KEYMASK, FUT_GCC_INSTRUMENT_ENTRY_CODE, fun);	\
+  } while(0)
+#define PROF_OUT()					\
+  do {							\
+    extern void fun(void) asm(__FUNCTION__);		\
+    /*__cyg_profile_func_exit(fun,NULL);	*/	\
+    FUT_PROBE1(PROFILE_KEYMASK, FUT_GCC_INSTRUMENT_EXIT_CODE, fun);	\
+  } while(0)
+#endif
+
+#define PROF_IN_EXT(name)             GEN_PREPROC(#name "_entry")
+#define PROF_OUT_EXT(name)            GEN_PREPROC(#name "_exit")
+#define PROF_EVENT(name)              GEN_PREPROC(#name "_single")
+#define PROF_EVENT1(name, arg1)       GEN_PREPROC1(#name "_single1", arg1)
+#define PROF_EVENT2(name, arg1, arg2) GEN_PREPROC2(#name "_single2", arg1, arg2)
+
+#else // ifndef DO_PROFILE
+
+#define PROF_PROBE(keymask, code, ...)        (void)0
+#define PROF_ALWAYS_PROBE(code, ...)          (void)0
+
+#define PROF_IN()                             (void)0
+#define PROF_OUT()                            (void)0
+
+#define PROF_IN_EXT(name)                     (void)0
+#define PROF_OUT_EXT(name)                    (void)0
+#define PROF_EVENT(name)                      (void)0
+#define PROF_EVENT1(name, arg1)               (void)0
+#define PROF_EVENT2(name, arg1, arg2)         (void)0
+
+#endif // DO_PROFILE
+
+// In the following calls to fut_header, the keymask is ignored in
+// PROF_SWITCH_TO() because this trace-instruction should _always_ be
+// active...
+
+// Must be called each time a context switch is performed between two
+// user-level threads. The parameters are the values of the 'number'
+// fields of the task_desc structure.
+#define PROF_SWITCH_TO(thr1, thr2)                                  \
+ PROF_START                                                         \
+      FUT_DO_PROBE2(FUT_SWITCH_TO_CODE, MA_PROFILE_TID(thr2), thr2->number); \
+ PROF_END
+
+// Must be called when a new kernel threads (SMP or activation flavors
+// only) is about to execute the very first thread (usually the
+// idle_task).
+
+#ifdef USE_FKT
+extern int fkt_new_lwp(unsigned int thread_num, unsigned int lwp_logical_num);
+#else /* USE_FKT */
+#  define fkt_new_lwp(thread_num, lwp_logical_num) (void)0
+#endif /* USE_FKT */
+
+#define PROF_NEW_LWP(num, thr)                                      \
+ PROF_START                                                         \
+      FUT_DO_PROBE2(FUT_NEW_LWP_CODE, num, thr);                    \
+      fkt_new_lwp(MA_PROFILE_TID(thr), num);                        \
+ PROF_END
+
+
+#define PROF_THREAD_BIRTH(thr)                                      \
+ PROF_START                                                         \
+  FUT_DO_PROBE1(FUT_THREAD_BIRTH_CODE, MA_PROFILE_TID(thr));        \
+ PROF_END
+
+#define PROF_THREAD_DEATH(thr)                                      \
+ PROF_START                                                         \
+  FUT_DO_PROBE1(FUT_THREAD_DEATH_CODE, MA_PROFILE_TID(thr));        \
+ PROF_END
+
+#define PROF_SET_THREAD_NAME()                                      \
+ PROF_START                                                         \
+      unsigned long *__name = (unsigned long *) &MARCEL_SELF->name; \
+      FUT_DO_PROBE4(FUT_SET_THREAD_NAME_CODE,                       \
+                 __name[0],                                         \
+                 __name[1],                                         \
+                 __name[2],                                         \
+                 __name[3]);                                        \
+ PROF_END
+
+
+void profile_init(void);
+
+void profile_set_tracefile(char *fmt, ...);
+
+void profile_activate(int how, unsigned user_keymask, unsigned kernel_keymask);
+
+void profile_stop(void);
+
+void profile_exit(void);
+
+extern volatile unsigned __pm2_profile_active;
+
+#endif

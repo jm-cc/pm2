d5eee2d27766e03afb9bce449a1159c827b2170e
ens-lyon.org!samuel.thibault <ens-lyon.org!samuel.thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-05-30 12:24:43 +0000

    �bench des conditions
    2005/05/30 14:07:32+02:00 ens-lyon.org!samuel.thibault
    bk cp contsw.c cond.c
    
    2004/12/06 12:19:12+01:00 ens-lyon.org!samuel.thibault
    ajout du test de yield sans changement de contexte (pour tester ma_schedule simple)
    
    2003/09/05 19:52:37+02:00 ens-lyon.fr!vdanjean
    rev 1.9 at 2003/04/14 15:05:21 GMT
     ajout libpthread
    
    2003/09/05 16:26:45+02:00 ens-lyon.fr!rnamyst
    rev 1.8 at 2001/11/22 13:26:42 GMT
     extended the contws test program to bench setjmp/longjmp
    
    2003/09/05 13:50:45+02:00 ens-lyon.fr!rnamyst
    rev 1.7 at 2001/05/30 13:59:24 GMT
     Minor modifications.
    
    2003/09/05 10:59:18+02:00 ens-lyon.fr!rnamyst
    rev 1.6 at 2001/03/02 16:30:21 GMT
     Merged the stable-mars branch (again\!)
    
    2003/09/05 10:12:50+02:00 ens-lyon.fr!rnamyst
    rev 1.5.2.1 at 2001/02/28 11:56:23 GMT
     Modified the copyrights in all file (to reference the author file) + added the dsm/test directory.
    
    2003/09/05 09:08:37+02:00 ens-lyon.fr!rnamyst
    rev 1.5 at 2001/02/23 15:44:36 GMT
     Merge between marcel_lifting2 and main trunk.
    
    2003/09/05 08:33:08+02:00 ens-lyon.fr!rnamyst
    rev 1.4.2.1 at 2001/02/23 12:18:00 GMT
     PM2 is now GPL\!
    
    2003/09/05 07:35:25+02:00 ens-lyon.fr!rnamyst
    rev 1.4 at 2001/01/30 17:37:24 GMT
     Ouch: just removed the old PM2-copyright header from _all_ files.
    
    2003/09/05 06:24:33+02:00 ens-lyon.fr!rnamyst
    rev 1.3 at 2000/11/15 21:32:16 GMT
     Removed 'timing' and 'safe_malloc' : all modules now use the toolbox for timing & safe malloc
    
    2003/09/05 01:29:52+02:00 ens-lyon.fr!vdanjean
    rev 1.2.2.1 at 2000/03/15 15:53:56 GMT
     r�organisation de marcel : commit pour CVS
    
    2003/09/05 00:45:21+02:00 ens-lyon.fr!rnamyst
    rev 1.2 at 2000/02/28 10:22:44 GMT
     Changed the Makefile and pruned the old (and obsolete) files...
    
    2003/09/04 23:34:15+02:00 ens-lyon.fr!oaumage
    rev 1.1.1.1 at 1999/11/23 13:40:37 GMT
     Importation des sources sous CVS
    rev 1.1 at 1999/11/23 13:40:37 GMT
     Initial revision
    
    (Logical change 1.1288)
    
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@11734 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ pm2/pm2/marcel/examples/cond.c
@@ -0,0 +1,86 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+#include <stdio.h>
+
+volatile int a=0;
+marcel_cond_t cond;
+marcel_mutex_t mutex;
+marcel_sem_t sem;
+
+any_t f(any_t arg)
+{
+  register int n = (int)arg;
+  tbx_tick_t t1, t2;
+
+  marcel_mutex_lock(&mutex);
+  marcel_sem_V(&sem);
+  TBX_GET_TICK(t1);
+  while(--n) {
+    marcel_cond_wait(&cond,&mutex);
+    marcel_cond_signal(&cond);
+  }
+  TBX_GET_TICK(t2);
+  marcel_mutex_unlock(&mutex);
+
+  printf("cond'time =  %fus\n", TBX_TIMING_DELAY(t1, t2));
+  return NULL;
+}
+
+void bench_cond(unsigned nb)
+{
+  marcel_t pid;
+  any_t status;
+  register int n;
+
+  if(!nb)
+    return;
+
+  n = nb >> 1;
+  n++;
+
+  marcel_sem_init(&sem, 0);
+  marcel_create(&pid, NULL, f, (any_t)n);
+
+  marcel_sem_P(&sem);
+  marcel_mutex_lock(&mutex);
+  while(--n) {
+    marcel_cond_signal(&cond);
+    marcel_cond_wait(&cond,&mutex);
+  }
+  marcel_mutex_unlock(&mutex);
+
+  marcel_join(pid, &status);
+}
+
+int marcel_main(int argc, char *argv[])
+{ 
+  int essais = 3;
+
+  marcel_init(&argc, argv);
+
+  if(argc != 2) {
+    fprintf(stderr, "Usage: %s <nb>\n", argv[0]);
+    exit(1);
+  }
+
+  while(essais--) {
+    bench_cond(atoi(argv[1]));
+  }
+
+  return 0;
+}

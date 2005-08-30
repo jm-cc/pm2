f6984834a17624bc594ffed9766fa680ed4013bb
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-08-30 13:49:39 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12068 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/marcel_cond.h
+++ marcel/include/marcel_cond.h
@@ -14,8 +14,6 @@
  * General Public License for more details.
  */
 
-#section common
-
 #depend "linux_spinlock.h[macros]"
 
 #section macros
@@ -77,4 +75,3 @@ DEC_POSIX(int, cond_wait, (pmarcel_cond_t *cond,
 			   pmarcel_mutex_t *mutex));
 DEC_POSIX(int, cond_timedwait, (pmarcel_cond_t *cond, pmarcel_mutex_t *mutex,
 				const struct timespec *abstime));
-#section common

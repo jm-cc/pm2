354370f9756a559aeef23bc3cd55f92818676f28
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-09-08 15:23:14 +0000

    Sur les machines MacOS, main et marcel_main doivent etre prefixees par le caractere '_'
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12138 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- tbx/include/tbx_macros.h
+++ tbx/include/tbx_macros.h
@@ -258,15 +258,21 @@
 #define tbx_flag_is_set(f)   (!!*(f))
 #define tbx_flag_is_clear(f)   (!*(f))
 
+#if defined(__GNUC__) && ( defined(__APPLE_CPP__) || defined(__APPLE_CC__) || defined(__MACOS_CLASSIC__) )
+#define DEFINE_TBX_MAIN(name) _ ## name
+#else
+#define DEFINE_TBX_MAIN(name) name
+#endif
+
 /*
  * Threads specific macros  _________________________________________
  * ________________________//////////////////////////////////////////
  */
 #if defined (MARCEL)       /*# Thread sync : Marcel mode */
 #  if defined(STANDARD_MAIN) || defined(MARCEL_MAIN_AS_FUNC)
-#    define tbx_main main
+#    define tbx_main DEFINE_TBX_MAIN(main)
 #  else  /* STANDARD_MAIN */
-#    define tbx_main marcel_main
+#    define tbx_main DEFINE_TBX_MAIN(marcel_main)
 #  endif /* STANDARD_MAIN */
 #  define TBX_CRITICAL_SECTION(name)              marcel_mutex_t __tbx_section_ ## name = MARCEL_MUTEX_INITIALIZER
 #  define TBX_CRITICAL_SECTION_ENTER(name)        marcel_mutex_lock(&(__tbx_section_ ## name))
@@ -282,7 +288,7 @@
 #  define TBX_UNLOCK()           unlock_task()
 #  define TBX_YIELD()            marcel_yield()
 #elif defined (_REENTRANT) /*# Thread sync : Pthread mode */
-#  define tbx_main main
+#  define tbx_main DEFINE_TBX_MAIN(main)
 #  define TBX_CRITICAL_SECTION(name)              pthread_mutex_t __tbx_section_ ## name = PTHREAD_MUTEX_INITIALIZER
 #  define TBX_CRITICAL_SECTION_ENTER(name)        pthread_mutex_lock(&(__tbx_section_ ## name))
 #  define TBX_CRITICAL_SECTION_TRY_ENTERING(name) pthread_mutex_trylock(&(__tbx_section_ ## name))
@@ -301,7 +307,7 @@
 #    define TBX_YIELD()           pthread_yield()
 #  endif                        /*# pthread yield : end */
 #else                         /*# Threads sync : no thread mode */
-#  define tbx_main main
+#  define tbx_main DEFINE_TBX_MAIN(main)
 
 #  define TBX_CRITICAL_SECTION(name) const int __tbx_section_ ## name = 0
 #  define TBX_CRITICAL_SECTION_ENTER(name)

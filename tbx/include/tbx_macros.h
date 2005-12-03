0884479d914fe59a19d7695ee5b34c45ad6311ff
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-12-03 00:44:07 +0000

    r768@implementation:  samy | 2005-12-03 01:01:03 +0100  cygwin n'a pas backtrace
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12415 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- tbx/include/tbx_macros.h
+++ tbx/include/tbx_macros.h
@@ -39,7 +39,7 @@
 /* TBX_BACKTRACE_ON_FAILURE: controls usage of the backtracing features
  * provided by the GNU C library
  */
-#if !defined(DARWIN_SYS)
+#if !defined(DARWIN_SYS) && !defined(WIN_SYS)
 #  define TBX_BACKTRACE_ON_FAILURE
 #  define TBX_BACKTRACE_DEPTH 15
 #else

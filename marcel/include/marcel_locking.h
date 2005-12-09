2025f53c5310e7db2667e717f39a4e916b324e29
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2005-12-09 22:04:06 +0000

    r1118@implementation:  samy | 2005-12-09 18:53:50 +0100  ah pardon, en fait lock_task/unlock_task faisait partie de l'api  marcel
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12481 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/marcel_locking.h
+++ marcel/include/marcel_locking.h
@@ -32,6 +32,15 @@ typedef ma_spinlock_t marcel_lock_t;
 #define marcel_lock_release(l)    ma_spin_unlock(l)
 #define marcel_lock_locked(l)     ma_spin_is_locked(l)
 
+#section marcel_macros
+/****************************************************************
+ * Pour la compatibilité avec lock_task et assimilé
+ */
+
+#define lock_task() ma_preempt_disable()
+#define unlock_task() ma_preempt_enable()
+#define locked() ma_preempt_count()
+
 #section functions
 #ifdef MA__LIBPTHREAD
 #define marcel_extlib_protect()

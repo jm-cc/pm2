b3b5dffd030cb860ae65cdd52bd95f7a5bee874a
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-02-19 18:02:58 +0000

    r2941@implementation:  samy | 2006-02-14 12:10:21 +0100  # dans le format imprime 0x
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12757 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_top.c
+++ marcel/source/marcel_top.c
@@ -78,7 +78,7 @@ static void printtask(marcel_task_t *t) {
 	}
 	utime = ma_atomic_read(&t->top_utime);
 	cpu = djiffies?(utime*1000UL)/djiffies:0;
-	top_printf("0x%*p %*s %2d %3lu.%1lu %c %2d %10s %10s %10s\r\n", (int) (2*sizeof(void*)), t,
+	top_printf("%#*p %*s %2d %3lu.%1lu %c %2d %10s %10s %10s\r\n", (int) (2*sizeof(void*)), t,
         	MARCEL_MAXNAMESIZE, t->name,
 		t->sched.internal.prio, cpu/10UL, cpu%10UL,
 		state, GET_LWP_NUMBER(t),

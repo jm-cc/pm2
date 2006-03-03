13ae54f5301dd58997e993201834d48edfdc45d6
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-03-03 15:52:12 +0000

    texte sur marcel
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12788 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ doc/Getting_started/Progs/marcel-hello.c
@@ -0,0 +1,54 @@
+#include "marcel.h"/* Here! */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int inf, sup, res;
+  marcel_sem_t sem;/* Here! */
+} job;
+
+static void job_init(job *j, int inf, int sup) {
+  j->inf = inf;
+  j->sup = sup;
+  marcel_sem_init(&j->sem, 0);/* Here! */
+}
+
+any_t sum(any_t arg) {
+  job *j = (job *)arg;
+  job j1, j2;
+
+  if(j->inf == j->sup) {
+    j->res = j->inf;
+    marcel_sem_V(&j->sem);/* Here! */
+    return NULL;
+  }
+
+  job_init(&j1, j->inf, (j->inf+j->sup)/2 );
+  job_init(&j2, (j->inf+j->sup)/2+1, j->sup);
+
+  marcel_create(NULL, NULL, sum, (any_t)&j1);/* Here! */
+  marcel_create(NULL, NULL, sum, (any_t)&j2);/* Here! */
+  marcel_sem_P(&j1.sem);/* Here! */
+  marcel_sem_P(&j2.sem);/* Here! */
+
+  j->res = j1.res+j2.res;
+  marcel_sem_V(&j->sem);/* Here! */
+  return NULL;
+}
+
+int marcel_main(int argc, char **argv) {/* Here! */
+  job j;
+
+  marcel_init(&argc, argv);/* Here! */
+
+  marcel_sem_init(&j.sem, 0);/* Here! */
+  j.inf = 1;
+  j.sup = 1000;
+
+  marcel_create(NULL, NULL, sum, (any_t)&j);/* Here! */
+  marcel_sem_P(&j.sem);/* Here! */
+  printf("Sum from 1 to %d = %d\n", j.sup, j.res);
+
+  marcel_end();/* Here! */
+  return 0;
+}

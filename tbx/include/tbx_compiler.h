6f761fc52a5b78daf45db1a1f7d8df77c870484c
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-04-05 09:18:19 +0000

    r3756@implementation:  samy | 2006-04-05 11:17:12 +0200  ajout TBX_HIDDEN
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12939 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- tbx/include/tbx_compiler.h
+++ tbx/include/tbx_compiler.h
@@ -160,6 +160,7 @@ void __memory_barrier(void);
 #define TBX_VISIBILITY_POP
 #endif
 #define TBX_EXTERN TBX_VISIBILITY("default")
+#define TBX_HIDDEN TBX_VISIBILITY("hidden")
 #define TBX_PROTECTED TBX_VISIBILITY("protected")
 #define TBX_INTERNAL TBX_VISIBILITY("internal")
 

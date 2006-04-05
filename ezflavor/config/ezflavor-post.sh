dc3a386059102ee01f23fe05ec0e72d55f71e1df
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-04-05 10:09:40 +0000

    r3768@implementation:  samy | 2006-04-05 12:09:57 +0200  bien séparer les -L et les -l pour pm2-configure --kernel --libs-only-[lL]
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12942 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- ezflavor/config/ezflavor-post.sh
+++ ezflavor/config/ezflavor-post.sh
@@ -1,7 +1,8 @@
 
 if defined_in GTK2 PM2_EZFLAVOR_CFLAGS ; then
     PM2_EZFLAVOR_CFLAGS="$PM2_EZFLAVOR_CFLAGS `pkg-config --cflags gtk+-2.0`"
-    PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS `pkg-config --libs --libs gtk+-2.0`"
+    PM2_EZFLAVOR_LD_PATH="$PM2_EZFLAVOR_LD_PATH `pkg-config --libs-only-L gtk+-2.0`"
+    PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS `pkg-config --libs-only-l gtk+-2.0`"
 else
     PM2_EZFLAVOR_CFLAGS="$PM2_EZFLAVOR_CFLAGS `gtk-config --cflags`"
     PM2_EZFLAVOR_LIBS="$PM2_EZFLAVOR_LIBS `gtk-config --libs`"

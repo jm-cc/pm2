1345d0976de3bb83660a78787be62e7bf102d84a
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-04-10 16:39:02 +0000

    r3905@implementation:  samy | 2006-04-10 18:15:59 +0200  numa sur osf
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12979 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_sysdep.c
+++ marcel/source/marcel_sysdep.c
@@ -18,6 +18,10 @@
 #include "marcel.h"
 #include <errno.h>
 
+/*
+ * How to bind a thread on a given processor
+ */
+
 #if defined(MA__SMP) && defined(MA__BIND_LWP_ON_PROCESSOR)
 #ifdef LINUX_SYS
 #include <sched.h>
@@ -102,16 +106,21 @@ void ma_bind_on_processor(unsigned target) {
 #elif defined(AIX_SYS)
 	bindprocessor(BINDTHREAD, thread_self(), target);
 #else
+	/* TODO: GNU_SYS, FREEBSD_SYS, DARWIN_SYS, IRIX_SYS */
 #warning "don't know how to bind on processors on this system, please disable smp_bind_proc in flavor"
 #endif
 }
 #endif
 
+/*
+ * How to get the number of processors
+ */
+
 #ifdef MA__LWPS
 unsigned ma_nbprocessors(void) {
-#if defined(SOLARIS_SYS) || defined(LINUX_SYS) || defined(OSF_SYS) || defined(WIN_SYS) || defined(AIX_SYS)
+#if defined(_SC_NPROCESSORS_CONF)
 	return sysconf(_SC_NPROCESSORS_CONF);
-#elif defined(IRIX_SYS)
+#elif defined(_SC_NPROC_CONF) || defined(IRIX_SYS)
 	return sysconf(_SC_NPROC_CONF);
 #else
 #warning No known way to discover number of available processors on this system
@@ -121,6 +130,10 @@ unsigned ma_nbprocessors(void) {
 #endif
 }
 #endif
+
+/*
+ * How to allocate memory on specific nodes
+ */
 
 #ifdef MA__NUMA
 #ifdef LINUX_SYS
@@ -161,7 +174,26 @@ void ma_migrate_mem(void *ptr, size_t size, int node) {
 	if (mbind(ptr, size, MPOL_BIND, &mask, sizeof(mask), MPOL_MF_MOVE_ALL) == -1 && errno == EPERM)
 		mbind(ptr, size, MPOL_BIND, &mask, sizeof(mask), MPOL_MF_MOVE);
 }
+#elif defined(OSF_SYS)
+#include <numa.h>
+void *ma_malloc_node(size_t size, int node, char *file, unsigned line) {
+	/* TODO: utiliser acreate/amalloc plutôt ? */
+	memalloc_attr_t mattr;
+	memset(&mattr, 0, sizeof(mattr));
+	mattr.mattr_policy = MPOL_DIRECTED;
+	mattr.mattr_rad = RAD_NONE;
+	radsetcreate(&mattr.mattr_radset);
+	rademptyset(mattr,mattr_radset);
+	radaddset(mattr.mattr_radset,node);
+	return nmmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0, &mattr);
+}
+void ma_free_node(void *ptr, size_t size, int node, char * __restrict file, unsigned line) {
+	munmap(ptr, size);
+}
+void ma_migrate_mem(void *ptr, size_t size, int node) {
+}
 #else
+	/* TODO: SOLARIS_SYS, AIX_SYS, WIN_SYS, GNU_SYS, FREEBSD_SYS, DARWIN_SYS, IRIX_SYS */
 #warning "don't know how to allocate memory on specific nodes, please disable numa in flavor"
 #endif
 #endif

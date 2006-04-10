bcfc13b835fdcd4cdf92fe081773943c3b779039
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-04-10 14:32:48 +0000

    r3899@implementation:  samy | 2006-04-10 16:32:48 +0200  isolé le code système-dépendant pour binder les threads noyaux
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@12976 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_lwp.c
+++ marcel/source/marcel_lwp.c
@@ -14,42 +14,7 @@
  * General Public License for more details.
  */
 
-#define _GNU_SOURCE /* pour sched_setaffinity sous linux */
 #include "marcel.h"
-#ifdef MA__SMP
-#ifdef LINUX_SYS
-#include <sched.h>
-#ifndef CPU_SET
-	/* libc doesn't have support for sched_setaffinity,
-	 * build system call ourselves: */
-#include <linux/unistd.h>
-#ifndef __NR_sched_setaffinity
-	/* not even syscall number... */
-#if defined(X86_ARCH)
-#define __NR_sched_setaffinity 241
-#elif defined(IA64_ARCH)
-#define __NR_sched_setaffinity 1231
-#define __ia64_syscall(arg1,arg2,arg3,arg4,arg5,nr) syscall(nr,arg1,arg2,arg3,arg4,arg5)
-#else
-#error "don't know the syscall number for sched_setaffinity on this architecture"
-#endif
-	/* declare system call */
-_syscall3(int, sched_setaffinity, pid_t, pid, unsigned int, lg,
-		unsigned long *, mask);
-#endif
-#endif
-#endif
-#ifdef WIN_SYS
-#include <windows.h>
-#endif
-#ifdef OSF_SYS
-#include <radset.h>
-#include <numa.h>
-#endif
-#ifdef AIX_SYS
-#include <sys/processor.h>
-#endif
-#endif
 
 #ifdef MA__LWPS
 static MA_DEFINE_NOTIFIER_CHAIN(lwp_chain, "LWP");
@@ -346,76 +311,18 @@ static void lwp_init(ma_lwp_t lwp)
 	LOG_OUT();
 }
 
-#ifdef MA__SMP
-
-inline static void bind_on_processor(marcel_lwp_t *lwp)
-{
-#if defined(MA__BIND_LWP_ON_PROCESSORS)
-	unsigned long target = ma_cpu_of_lwp_num(LWP_NUMBER(lwp));
-#if defined(SOLARIS_SYS)
-	if(processor_bind(P_LWPID, P_MYID,
-			  (processorid_t)(target),
-			  NULL) != 0) {
-		perror("processor_bind");
-		exit(1);
-	}
-#elif defined(LINUX_SYS)
-
-#ifndef CPU_SET
-	/* no libc support, use direct system call */
-	unsigned long mask = 1UL<<target;
-
-	if (sched_setaffinity(0,sizeof(mask),&mask)<0) {
-		perror("sched_setaffinity");
-		exit(1);
-	}
-#else
-	cpu_set_t mask;
-
-	CPU_ZERO(&mask);
-	CPU_SET(target, &mask);
-#ifdef HAVE_OLD_SCHED_SETAFFINITY
-	if(sched_setaffinity(0,&mask)<0) {
-#else /* HAVE_OLD_SCHED_SETAFFINITY */
-	if(sched_setaffinity(0,sizeof(mask),&mask)<0) {
-#endif /* HAVE_OLD_SCHED_SETAFFINITY */
-		perror("sched_setaffinity");
-		exit(1);
-	}
-#endif
-#elif defined(WIN_SYS)
-	DWORD mask = 1UL<<target;
-	SetThreadAffinityMask(GetCurrentThread(), mask);
-#elif defined(OSF_SYS)
-	radset_t radset;
-	radsetcreate(&radset);
-	rademptyset(radset);
-	radaddset(radset, target);
-	if (pthread_rad_bind(pthread_self(), radset, RAD_INSIST)) {
-		perror("pthread_rad_attach");
-		exit(1);
-	}
-	radsetdestroy(&radset);
-#elif defined(AIX_SYS)
-	bindprocessor(BINDTHREAD, thread_self(), target);
-#else
-#warning "don't know how to bind on processors on this system, please disable smp_bind_proc in flavor"
-#endif
-	mdebug("LWP %u bound to processor %lu\n",
-			LWP_NUMBER(lwp), target);
-#endif
-}
-
-#endif /* MA__SMP */
-
 static int lwp_start(ma_lwp_t lwp)
 {
 	LOG_IN();
 	PROF_NEW_LWP(LWP_NUMBER(lwp), ma_per_lwp(run_task,lwp));
 
-#ifdef MA__SMP
-	if (LWP_NUMBER(lwp)<get_nb_lwps())
-		bind_on_processor(lwp);
+#if defined(MA__SMP) && defined(MA__BIND_LWP_ON_PROCESSORS)
+	if (LWP_NUMBER(lwp)<get_nb_lwps()) {
+		unsigned long target = ma_cpu_of_lwp_num(LWP_NUMBER(lwp));
+		ma_bind_on_processor(target);
+		mdebug("LWP %u bound to processor %lu\n",
+				LWP_NUMBER(lwp), target);
+	}
 #endif
 	LOG_OUT();
 	return 0;

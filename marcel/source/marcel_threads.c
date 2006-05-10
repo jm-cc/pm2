0f1e7ec7400745f4e2d3930878429d14bd4ecaba
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-05-10 07:06:03 +0000

    r4597@implementation:  samy | 2006-05-10 09:05:20 +0200  toujours donner les numéros des threads, puisqu'on annonce toujours leur création
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13123 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_threads.c
+++ marcel/source/marcel_threads.c
@@ -244,7 +244,7 @@ marcel_create_internal(marcel_t * __restrict pid,
 		static ma_atomic_t norun_pid = MA_ATOMIC_INIT(0);
 		new_task->number = ma_atomic_dec_return(&norun_pid);
 	}
-	PROF_EVENT2(set_thread_number,new_task,new_task->number);
+	PROF_EVENT2_ALWAYS(set_thread_number,new_task,new_task->number);
 	MTRACE("Creation", new_task);
 	
 	//PROF_IN_EXT(newborn_thread);

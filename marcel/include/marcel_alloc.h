318b6bc60b54d1280c38fdc7fc28dac993d67868
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-05-19 15:16:38 +0000

    r4865@implementation:  samy | 2006-05-19 17:04:01 +0200  ne pas exposer l'utilisation de l'allocateur
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13174 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/marcel_alloc.h
+++ marcel/include/marcel_alloc.h
@@ -24,6 +24,8 @@ extern ma_allocator_t *marcel_mapped_slot_allocator, *marcel_unmapped_slot_alloc
 TBX_FMALLOC void *marcel_slot_alloc(void);
 void marcel_slot_free(void *addr);
 void marcel_slot_exit(void);
+
+#section marcel_macros
 #define marcel_slot_alloc() ma_obj_alloc(marcel_mapped_slot_allocator)
 #define marcel_slot_free(addr) ma_obj_free(marcel_mapped_slot_allocator, addr)
 

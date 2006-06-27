fdc642600e0b0bce3c407cec450e2689b4bf98f5
ebrunet <ebrunet@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-06-27 13:08:03 +0000

    modification interface
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13345 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/schedulers/sched_opt/src/nm_so_stack.c
@@ -0,0 +1,55 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <assert.h>
+#include <tbx.h>
+
+struct nm_so_stack{
+    int nb_entries;
+    void **obj;
+    int next_to_add;
+};
+
+struct nm_so_stack *
+nm_so_stack_create(int nb_entries){
+    struct nm_so_stack * stack
+        = TBX_MALLOC(sizeof(struct nm_so_stack));
+    stack->nb_entries = nb_entries;
+    stack->obj = TBX_MALLOC(nb_entries * sizeof(void *));
+    stack->next_to_add = 0;
+    return stack;
+}
+
+void
+nm_so_stack_push(struct nm_so_stack *stack, void *obj){
+    assert(stack->next_to_add < stack->nb_entries);
+    stack->obj[stack->next_to_add] = obj;
+    stack->next_to_add++;
+}
+
+void *
+nm_so_stack_pop(struct nm_so_stack *stack){
+    assert(stack->next_to_add > 0);
+    void * obj = stack->obj[stack->next_to_add - 1];
+    stack->next_to_add--;
+    return obj;
+}
+
+int
+nm_so_stack_length(struct nm_so_stack *stack){
+    return stack->next_to_add;
+}
+

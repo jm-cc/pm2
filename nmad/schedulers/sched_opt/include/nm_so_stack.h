45d6b08b09c89055d205c9897ed673544f0e3f0a
ebrunet <ebrunet@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-06-27 13:08:28 +0000

    modif interface
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13346 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/schedulers/sched_opt/include/nm_so_stack.h
@@ -0,0 +1,33 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef NM_SO_STACK_H
+#define NM_SO_STACK_H
+
+typedef struct nm_so_stack * nm_so_stack_t;
+
+struct nm_so_stack *
+nm_so_stack_create(int nb_entries);
+
+void
+nm_so_stack_push(struct nm_so_stack *stack, void *obj);
+
+void *
+nm_so_stack_pop(struct nm_so_stack *stack);
+
+int
+nm_so_stack_length(struct nm_so_stack *stack);
+
+#endif

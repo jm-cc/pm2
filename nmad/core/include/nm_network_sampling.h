e652d1bbb88d21a0eb639a8980bd67b7d1847306
ebrunet <ebrunet@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-06-28 15:33:40 +0000

    ajout de la licence
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13348 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/core/include/nm_network_sampling.h
+++ nmad/core/include/nm_network_sampling.h
@@ -1,3 +1,21 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef NM_NETWORK_SAMPLING_H
+#define NM_NETWORK_SAMPLING_H
+
 int
 nm_ns_network_sampling(struct nm_drv *driver,
                        uint8_t gate_id,
@@ -6,3 +24,5 @@ nm_ns_network_sampling(struct nm_drv *driver,
 double
 nm_ns_evaluate(struct nm_drv *driver,
                int length);
+
+#endif

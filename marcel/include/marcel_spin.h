4188073ba8d7066bdc0c1eb95e0419506bd0a1ff
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-07-04 08:27:31 +0000

    r5134@implementation:  samy | 2006-07-03 19:03:39 +0200  fichier oublié
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13385 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/include/marcel_spin.h
@@ -0,0 +1,27 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2006 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#section types
+#define marcel_spinlock_t ma_spinlock_t
+#define pmarcel_spinlock_t marcel_spinlock_t
+#define pthread_spinlock_t pmarcel_spinlock_t
+
+#section functions
+DEC_MARCEL_POSIX(int, spin_init, (marcel_spinlock_t *lock, int pshared) __THROW);
+DEC_MARCEL_POSIX(int, spin_destroy, (marcel_spinlock_t *lock) __THROW);
+DEC_MARCEL_POSIX(int, spin_lock, (marcel_spinlock_t *lock) __THROW);
+DEC_MARCEL_POSIX(int, spin_trylock, (marcel_spinlock_t *lock) __THROW);
+DEC_MARCEL_POSIX(int, spin_unlock, (marcel_spinlock_t *lock) __THROW);

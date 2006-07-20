48bfd4b2d606d57723369b306378dde265bcbc0a
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-07-20 13:34:31 +0000

    r5269@implementation:  samy | 2006-07-20 15:33:15 +0200  Darwin a la merveilleuse idée de ne définir MAP_ANON que quand _POSIX_C_SOURCE n'est pas défini....
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13537 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/sys/isomalloc_archdep.h
+++ marcel/include/sys/isomalloc_archdep.h
@@ -113,7 +113,7 @@ extern int __zero_fd;
 #    define MAIN_STACK_BOT         0x90000000
 #    define IS_ON_MAIN_STACK(sp)   ((sp) > MAIN_STACK_BOT)
 #    define FILE_TO_MAP            -1
-#    define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED | MAP_ANON)
+#    define MMAP_MASK              (MAP_PRIVATE | MAP_FIXED | 0x1000)
 #elif defined(AIX_SYS)
 #    if defined(RS6K_ARCH) || defined(PPC_ARCH)
 #      define ISOADDR_AREA_TOP       0xcfff0000

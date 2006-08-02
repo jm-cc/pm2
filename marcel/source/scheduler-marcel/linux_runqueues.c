ed4d294e9ea285961b8798885e1ae19f53ef1821
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-08-02 13:40:02 +0000

    r5369@implementation:  samy | 2006-08-02 15:39:29 +0200   r5365@implementation (orig r879):  sthibault | 2006-08-02 14:27:47 +0200   initialiser nr_active à 0 ! (soumis à Linux aussi)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13617 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/scheduler-marcel/linux_runqueues.c
+++ marcel/source/scheduler-marcel/linux_runqueues.c
@@ -44,6 +44,7 @@ void init_rq(ma_runqueue_t *rq, char *name, enum ma_rq_type type)
 
 	for (j = 0; j < 2; j++) {
 		array = rq->arrays + j;
+		array->nr_active = 0;
 		memset(&array->bitmap, 0, sizeof(array->bitmap));
 		for (k = 0; k < MA_MAX_PRIO; k++)
 			INIT_LIST_HEAD(ma_array_queue(array, k));

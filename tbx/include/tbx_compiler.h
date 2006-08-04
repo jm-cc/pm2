bce052089081eea6564bf556bfd0d8198cc53e70
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-08-04 15:14:37 +0000

    r5454@implementation:  samy | 2006-08-04 17:14:28 +0200  les attributs ne sont pas supportés par le format Mach-O
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13665 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- tbx/include/tbx_compiler.h
+++ tbx/include/tbx_compiler.h
@@ -97,7 +97,7 @@ void __memory_barrier(void);
 #  define __tbx_deprecated__		__attribute__((deprecated))
 #  define __tbx_attribute_used__	__attribute__((__used__))
 #  define __tbx_attribute_pure__	__attribute__((pure))
-#ifndef AIX_SYS
+#if !defined(AIX_SYS) && !defined(DARWIN_SYS)
 #  define TBX_VISIBILITY(vis) __attribute__ ((__visibility__(vis)))
 #  define TBX_VISIBILITY_PUSH_DEFAULT _Pragma("GCC visibility push(default)")
 #  define TBX_VISIBILITY_PUSH_INTERNAL _Pragma("GCC visibility push(internal)")

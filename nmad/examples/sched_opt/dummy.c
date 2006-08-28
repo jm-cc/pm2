f812c4f7a54aafc2c1a2f6cc548e20e2df7c3d2a
gforgeron <gforgeron@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-08-28 15:36:44 +0000

    The sched_opt directory now contains examples to be used ONLY with the sched_opt optimizer.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13768 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sched_opt/dummy.c
@@ -0,0 +1,165 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/uio.h>
+
+/* Choose a scheduler
+ */
+#include <tbx.h>
+#include <nm_public.h>
+#include <nm_so_public.h>
+#include "nm_so_pkt_wrap.h"
+
+
+/* Choose a driver
+ */
+#if defined CONFIG_MX
+#  include <nm_mx_public.h>
+#elif defined CONFIG_GM
+#  include <nm_gm_public.h>
+#elif defined CONFIG_QSNET
+#  include <nm_qsnet_public.h>
+#else
+#  include <nm_tcp_public.h>
+#endif
+
+#define TAG0 128
+#define TAG1 129
+
+static int data_handler(struct nm_so_pkt_wrap *p_so_pw, 
+			void *ptr, uint32_t len,
+			uint8_t proto_id, uint8_t seq,
+			void *arg)
+{
+  printf("Data header : data = %p [%s], len = %u, tag = %d, seq = %u\n",
+	 ptr, ptr, len, proto_id, seq);
+
+  return NM_SO_HEADER_MARK_UNREAD;
+}
+
+static int rdv_handler(struct nm_so_pkt_wrap *p_so_pw, 
+		       uint8_t tag_id, uint8_t seq,
+		       void *arg)
+{
+  printf("Rdv header : tag = %d, seq = %u\n", tag_id, seq);
+
+  return NM_SO_HEADER_MARK_UNREAD;
+}
+
+int
+main(int	  argc,
+     char	**argv)
+{
+  struct nm_core *p_core = NULL;
+  int err;
+  tbx_tick_t t1, t2;
+  int k;
+
+  err = nm_core_init(&argc, argv, &p_core, nm_so_load);
+  if (err != NM_ESUCCESS) {
+    printf("nm_core_init returned err = %d\n", err);
+    goto out;
+  }
+
+  for(k = 0; k < 5; k++)
+  {
+    struct nm_so_pkt_wrap *p_so_pw;
+    NM_SO_ALIGN_TYPE buf[NM_SO_PREALLOC_BUF_LEN / sizeof(NM_SO_ALIGN_TYPE)];
+    struct iovec *vec;
+    void *ptr;
+    uint32_t len;
+    int i;
+
+    TBX_GET_TICK(t1);
+
+    err = nm_so_pw_alloc_and_fill_with_data(TAG0, 0,
+					    "abc", 4,
+					    NM_SO_DATA_USE_COPY,
+					    &p_so_pw);
+    if(err != NM_ESUCCESS) {
+      printf("nm_so_pw_alloc failed: err = %d\n", err);
+      goto out;
+    }
+   
+    nm_so_pw_add_data(p_so_pw, TAG1, 0, "d", 2, NM_SO_DATA_FORCE_CONTIGUOUS);
+
+    nm_so_pw_add_data(p_so_pw, TAG0, 1, "efgh", 5, 0);
+
+    nm_so_pw_add_rdv(p_so_pw, TAG0, 2);
+
+    nm_so_pw_finalize(p_so_pw);
+
+    TBX_GET_TICK(t2);
+
+    printf("Total length = %lld\n", p_so_pw->pw.length);
+    for(i=0; i<p_so_pw->pw.v_nb; i++)
+      printf("iovec[%d] contains %u bytes (@ = %p)\n",
+	     i, p_so_pw->pw.v[i].iov_len, p_so_pw->pw.v[i].iov_base);
+
+    printf("Iteration sur le wrapper d'emission :\n");
+    nm_so_pw_iterate_over_headers(p_so_pw,
+				  data_handler,
+				  rdv_handler,
+				  NULL,
+				  NULL);
+
+    /* Simulation d'un envoi + réception dans un tampon 'unexpected' */
+    vec = p_so_pw->pw.v;
+    ptr = buf;
+    for(i=0; i<p_so_pw->pw.v_nb; i++) {
+      memcpy(ptr, vec->iov_base, vec->iov_len);
+      ptr += vec->iov_len;
+      vec++;
+    }
+
+    len = ptr - (void *)buf;
+    printf("taille recopiée = %d\n", len);
+
+
+    nm_so_pw_free(p_so_pw);
+
+    err = nm_so_pw_alloc_and_fill_with_data(0, 0,
+                                            buf, NM_SO_PREALLOC_BUF_LEN,
+			                    NM_SO_DATA_DONT_USE_HEADER,
+                                            &p_so_pw);
+    if(err != NM_ESUCCESS) {
+      printf("nm_so_pw_alloc failed: err = %d\n", err);
+      goto out;
+    }
+
+    printf("Iteration sur le wrapper de reception :\n");
+    nm_so_pw_iterate_over_headers(p_so_pw,
+				  data_handler,
+				  rdv_handler,
+				  NULL,
+				  NULL);
+
+    nm_so_pw_free(p_so_pw);
+
+  }
+
+
+  printf("time = %lf\n", TBX_TIMING_DELAY(t1, t2));
+
+ out:
+        return err;
+}
+
---
+++ nmad/examples/sched_opt/hello.c
@@ -0,0 +1,167 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <nm_public.h>
+#include <nm_so_public.h>
+
+#include <nm_so_interface.h>
+
+#if defined CONFIG_MX
+#  include <nm_mx_public.h>
+#elif defined CONFIG_GM
+#  include <nm_gm_public.h>
+#elif defined CONFIG_QSNET
+#  include <nm_qsnet_public.h>
+#elif defined CONFIG_SISCI
+#  include <nm_sisci_public.h>
+#else
+#  include <nm_tcp_public.h>
+#endif
+
+const char *msg	= "hello, world";
+
+static
+void
+usage(void) {
+        fprintf(stderr, "usage: hello [[-h <remote_hostname>] <remote url>]\n");
+        exit(EXIT_FAILURE);
+}
+
+int
+main(int	  argc,
+     char	**argv) {
+        struct nm_core		*p_core		= NULL;
+        char			*r_url		= NULL;
+        char			*l_url		= NULL;
+        uint8_t			 drv_id		=    0;
+        uint8_t			 gate_id	=    0;
+        char			*buf		= NULL;
+        char			*hostname	= "localhost";
+        uint64_t		 len;
+	struct nm_so_cnx        *cnx            = NULL;
+        int err;
+
+        err = nm_core_init(&argc, argv, &p_core, nm_so_load);
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_init returned err = %d\n", err);
+                goto out;
+        }
+
+        argc--;
+        argv++;
+
+        if (argc) {
+                /* client */
+                while (argc) {
+                        if (!strcmp(*argv, "-h")) {
+                                argc--;
+                                argv++;
+
+                                if (!argc)
+                                        usage();
+
+                                hostname = *argv;
+                        } else {
+                                r_url	= *argv;
+                        }
+
+                        argc--;
+                        argv++;
+                }
+
+                printf("running as client using remote url: %s[%s]\n", hostname, r_url);
+        } else {
+                /* no args: server */
+                printf("running as server\n");
+        }
+
+#if defined CONFIG_MX
+        err = nm_core_driver_init(p_core, nm_mx_load, &drv_id, &l_url);
+#elif defined CONFIG_GM
+        err = nm_core_driver_init(p_core, nm_gm_load, &drv_id, &l_url);
+#elif defined CONFIG_QSNET
+        err = nm_core_driver_init(p_core, nm_qsnet_load, &drv_id, &l_url);
+#elif defined CONFIG_SISCI
+	err = nm_core_driver_init(p_core, nm_sisci_load, &drv_id, &l_url);
+#else
+        err = nm_core_driver_init(p_core, nm_tcp_load, &drv_id, &l_url);
+#endif
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_driver_init returned err = %d\n", err);
+                goto out;
+        }
+
+        err = nm_core_gate_init(p_core, &gate_id);
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_gate_init returned err = %d\n", err);
+                goto out;
+        }
+
+        len = 1+strlen(msg);
+        buf = malloc((size_t)len);
+
+        if (!r_url) {
+                /* server
+                 */
+                printf("local url: [%s]\n", l_url);
+
+                err = nm_core_gate_accept(p_core, gate_id, drv_id, NULL, NULL);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_accept returned err = %d\n", err);
+                        goto out;
+                }
+
+                memset(buf, 0, len);
+
+		nm_so_begin_unpacking(p_core, gate_id, 0, &cnx);
+
+		nm_so_unpack(cnx, buf, len);
+
+		nm_so_end_unpacking(p_core, cnx);
+
+        } else {
+                /* client
+                 */
+                err = nm_core_gate_connect(p_core, gate_id, drv_id,
+                                           hostname, r_url);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_connect returned err = %d\n", err);
+                        goto out;
+                }
+
+		printf("Client !\n");
+                strcpy(buf, msg);
+
+		nm_so_begin_packing(p_core, gate_id, 0, &cnx);
+
+		nm_so_pack(cnx, buf, len);
+
+		nm_so_end_packing(p_core, cnx);
+        }
+
+        if (!r_url) {
+                printf("buffer contents: %s\n", buf);
+        }
+
+ out:
+        return 0;
+}

3ef286c90dccc9c9375017f4b86d034ccd46f6e2
frezier <frezier@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-09-21 09:54:28 +0000

    Pas de -lpthread pour MX
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@13972 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mad3/config/options/01mx.sh
+++ mad3/config/options/01mx.sh
@@ -12,5 +12,5 @@ PM2_PM2_CFLAGS="$PM2_PM2_CFLAGS -DMAD3_MAD1_MAIN_PROTO_PARAM=NULL"
 ## mx_open_endpoint calls pthread_create, we need to link libpthread
 ## (or disable the use of pthread in MX)
 #
-PM2_MAD3_CFLAGS="$PM2_MAD3_CFLAGS -D_REENTRANT"
-PM2_MAD3_LIBS="$PM2_MAD3_LIBS -lpthread"
+#PM2_MAD3_CFLAGS="$PM2_MAD3_CFLAGS -D_REENTRANT"
+#PM2_MAD3_LIBS="$PM2_MAD3_LIBS -lpthread"

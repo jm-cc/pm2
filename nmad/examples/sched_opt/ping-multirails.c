91001854553e72d3e97c5af9fe23957bd78cb418
gforgeron <gforgeron@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-10-09 19:12:57 +0000

    Preliminar multi-rail support
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14062 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sched_opt/ping-multirails.c
@@ -0,0 +1,235 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <tbx.h>
+
+#include <nm_public.h>
+#include <nm_so_public.h>
+
+#include <nm_so_pack_interface.h>
+
+#if defined CONFIG_MX && defined CONFIG_QSNET
+#  include <nm_mx_public.h>
+#  include <nm_qsnet_public.h>
+
+#define NB_PACKS 8
+#define MIN     (4 * NB_PACKS)
+#define MAX     (1024 * 1024)
+#define LOOPS   1000
+
+static __inline__
+uint32_t _next(uint32_t len)
+{
+        if(!len)
+                return 4;
+        else if(len < 32)
+                return len + 4;
+        else if(len < 1024)
+                return len + 32;
+        else
+                return len << 1;
+}
+
+static __inline__
+unsigned long subpart_for_mx(unsigned long len)
+{
+  if(len > 32 * 1024) {
+    return nm_so_aligned((len * 100 / 473) - 7000);
+  } else
+    return len / 2;
+}
+
+static
+void
+usage(void) {
+        fprintf(stderr, "usage: hello [[-h <remote_hostname>] <remote url> <remote url2>]\n");
+        exit(EXIT_FAILURE);
+}
+
+int
+main(int	  argc,
+     char	**argv) {
+        struct nm_core		*p_core		= NULL;
+        char			*r_url1		= NULL;
+        char			*r_url2		= NULL;
+        char			*l_url1		= NULL;
+        char			*l_url2		= NULL;
+        uint8_t			 drv1_id	=    0;
+        uint8_t			 drv2_id	=    0;
+        uint8_t			 gate_id	=    0;
+        char			*buf		= NULL;
+        char			*hostname	= "localhost";
+        uint32_t		 len;
+	struct nm_so_cnx        *cnx            = NULL;
+        int err;
+
+        err = nm_core_init(&argc, argv, &p_core, nm_so_load);
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_init returned err = %d\n", err);
+                goto out;
+        }
+
+	err = nm_so_pack_interface_init();
+	if(err != NM_ESUCCESS) {
+                printf("nm_so_pack_interface_init return err = %d\n", err);
+                goto out;
+	}
+
+        argc--;
+        argv++;
+
+        if (argc) {
+                /* client */
+                while (argc) {
+                        if (!strcmp(*argv, "-h")) {
+                                argc--;
+                                argv++;
+
+                                if (!argc)
+                                        usage();
+
+                                hostname = *argv;
+                        } else {
+                                r_url1	= r_url2;
+                                r_url2	= *argv;
+                        }
+
+                        argc--;
+                        argv++;
+                }
+
+                printf("running as client using remote url: %s [%s] [%s]\n", hostname, r_url1, r_url2);
+        } else {
+                /* no args: server */
+                printf("running as server\n");
+        }
+
+        err = nm_core_driver_init(p_core, nm_mx_load, &drv1_id, &l_url1);
+        printf("local url1: [%s]\n", l_url1);
+        err = nm_core_driver_init(p_core, nm_qsnet_load, &drv2_id, &l_url2);
+        printf("local url2: [%s]\n", l_url2);
+
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_driver_init returned err = %d\n", err);
+                goto out;
+        }
+
+        err = nm_core_gate_init(p_core, &gate_id);
+        if (err != NM_ESUCCESS) {
+                printf("nm_core_gate_init returned err = %d\n", err);
+                goto out;
+        }
+
+        buf = malloc(MAX);
+	memset(buf, 0, MAX);
+
+        if (!r_url1 && !r_url2) {
+                int k;
+                /* server
+                 */
+
+                err = nm_core_gate_accept(p_core, gate_id, drv1_id, NULL, NULL);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_accept(drv1) returned err = %d\n", err);
+                        goto out;
+                }
+
+                err = nm_core_gate_accept(p_core, gate_id, drv2_id, NULL, NULL);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_accept(drv2) returned err = %d\n", err);
+                        goto out;
+                }
+
+		for(len = MIN; len <= MAX; len = _next(len)) {
+		  unsigned long n, chunk = len / NB_PACKS;
+
+		  for(k = 0; k < LOOPS; k++) {
+		    nm_so_begin_unpacking(p_core, gate_id, 0, &cnx);
+		    for(n = 0; n < NB_PACKS; n++)
+		      nm_so_unpack(cnx, buf + n * chunk, chunk);
+		    nm_so_end_unpacking(p_core, cnx);
+
+		    nm_so_begin_packing(p_core, gate_id, 0, &cnx);
+		    for(n = 0; n < NB_PACKS; n++)
+		      nm_so_pack(cnx, buf + n * chunk, chunk);
+		    nm_so_end_packing(p_core, cnx);
+		  }
+		}
+
+        } else {
+                tbx_tick_t t1, t2;
+                int k;
+
+                if (!r_url1)
+                        usage();
+          
+
+                /* client
+                 */
+                err = nm_core_gate_connect(p_core, gate_id, drv1_id,
+                                           hostname, r_url1);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_connect(drv1) returned err = %d\n", err);
+                        goto out;
+                }
+
+                err = nm_core_gate_connect(p_core, gate_id, drv2_id,
+                                           hostname, r_url2);
+                if (err != NM_ESUCCESS) {
+                        printf("nm_core_gate_connect(drv2) returned err = %d\n", err);
+                        goto out;
+                }
+
+		for(len = MIN; len <= MAX; len = _next(len)) {
+		  unsigned long n, chunk = len / NB_PACKS;
+
+                        TBX_GET_TICK(t1);
+
+                        for(k = 0; k < LOOPS; k++) {
+			  nm_so_begin_packing(p_core, gate_id, 0, &cnx);
+			  for(n = 0; n < NB_PACKS; n++)
+			    nm_so_pack(cnx, buf + n * chunk, chunk);
+			  nm_so_end_packing(p_core, cnx);
+
+			  nm_so_begin_unpacking(p_core, gate_id, 0, &cnx);
+			  for(n = 0; n < NB_PACKS; n++)
+			    nm_so_unpack(cnx, buf + n * chunk, chunk);
+			  nm_so_end_unpacking(p_core, cnx);
+                        }
+
+                        TBX_GET_TICK(t2);
+
+                        printf("%d\t%lf\n", len, TBX_TIMING_DELAY(t1, t2)/(2*LOOPS));
+		}
+        }
+
+ out:
+        return 0;
+}
+
+#else
+int main() {
+        printf("This program requires Myrinet/MX and Quadrics networks subsystems\n");
+
+        return 0;
+}
+#endif

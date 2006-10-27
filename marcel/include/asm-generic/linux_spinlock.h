82fea4846155c30f17dc744c711eaade0d6b8c6e
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-10-27 16:37:42 +0000

    r6141@implementation:  samy | 2006-10-27 18:37:48 +0200  utiliser pthread_spin_* si disponibles
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14140 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/asm-generic/linux_spinlock.h
+++ marcel/include/asm-generic/linux_spinlock.h
@@ -15,67 +15,62 @@
  */
 
 #section common
-/*
- * Similar to:
- * include/asm-i386/spinlock.h
- */
+#include <unistd.h>
+#if defined(MA__LWPS) || !defined(MA_HAVE_COMPAREEXCHANGE)
+#depend "asm/marcel_testandset.h[macros]"
+#ifdef MA_HAVE_TESTANDSET
+
+/* Test and set implementation */
 
 #section types
-/*
- * Your basic SMP spinlocks, allowing only a single CPU anywhere
- */
-
-#if defined(MA__LWPS) || !defined(MA_HAVE_COMPAREEXCHANGE)
-#depend "asm/marcel_testandset.h[macros]"
-#ifdef MA_HAVE_TESTANDSET
 typedef volatile unsigned ma_spinlock_t;
-#else
-#ifdef MARCEL_DONT_USE_POSIX_THREADS
-#warning "Can't avoid using pthreads with no native locking primitive"
-#warning "Please define pm2_compareexchange in marcel/include/asm-yourarch/marcel_compareexchange.h"
-#error "or define pm2_spinlock_testandset in marcel/include/asm-yourarch/marcel_testandset.h"
-#endif
-#include <pthread.h>
-typedef pthread_mutex_t ma_spinlock_t;
-#endif
-#endif
-
 #section macros
-#if defined(MA__LWPS) || !defined(MA_HAVE_COMPAREEXCHANGE)
-#depend "asm/marcel_testandset.h[macros]"
-#ifdef MA_HAVE_TESTANDSET
 #define MA_SPIN_LOCK_UNLOCKED 0
-
 #define ma_spin_lock_init(x)	do { *(x) = (ma_spinlock_t) MA_SPIN_LOCK_UNLOCKED; } while(0)
-#else
-#include <pthread.h>
-#define MA_SPIN_LOCK_UNLOCKED PTHREAD_MUTEX_INITIALIZER
-#define ma_spin_lock_init(x) pthread_mutex_init(x,NULL);
-#endif
-#endif
-
 #section marcel_macros
-/*
- * Simple spin lock operations.  There are two variants, one clears IRQ's
- * on the local processor, one does not.
- *
- * We make no fairness assumptions. They have a cost.
- */
-
-#if defined(MA__LWPS) || !defined(MA_HAVE_COMPAREEXCHANGE)
-
-#depend "asm/marcel_testandset.h[macros]"
-#ifdef MA_HAVE_TESTANDSET
 #define ma_spin_is_locked(x)	(!!*(x))
 #define ma_spin_unlock_wait(x)	do { ma_barrier(); } while(ma_spin_is_locked(x))
 
 #define _ma_raw_spin_unlock(x) pm2_spinlock_release(x)
 #define _ma_raw_spin_trylock(x) (!pm2_spinlock_testandset(x))
 #define _ma_raw_spin_lock(x) do { } while (!(_ma_raw_spin_trylock(x)))
+
+#section common
+#elif _POSIX_SPIN_LOCKS > 0
+
+/* Posix spinlock implementation */
+
+#section types
+typedef pthread_spinlock_t ma_spinlock_t;
+#section macros
+#define MA_SPIN_LOCK_UNLOCKED 0 /* hoping this is ok */
+#define ma_spin_lock_init(x)	pthread_spin_init(x, 0)
+#section marcel_macros
+#define ma_spin_is_locked(x)	(pthread_spin_trylock(x) ? 1 : pthread_spin_unlock(x), 0) /* yes, a bit silly... */
+#define ma_spin_unlock_wait(x)	do { ma_barrier(); } while(ma_spin_is_locked(x))
+
+#define _ma_raw_spin_unlock(x)  pthread_spin_unlock(x)
+#define _ma_raw_spin_trylock(x) (!pthread_spin_trylock(x))
+#define _ma_raw_spin_lock(x)    pthread_spin_lock(x)
+
+#section common
 #else
+
+/* No spin implementation, use posix mutexes */
+
 #ifdef MARCEL_DONT_USE_POSIX_THREADS
-#error "Can't avoid using Posix Threads library if neither cmpxchg nor testandset are implemented !"
+#warning "Can't avoid using pthreads with no native locking primitive"
+#warning "Please define pm2_compareexchange in marcel/include/asm-yourarch/marcel_compareexchange.h"
+#error "or define pm2_spinlock_testandset in marcel/include/asm-yourarch/marcel_testandset.h"
 #endif
+#section types
+#include <pthread.h>
+typedef pthread_mutex_t ma_spinlock_t;
+#section macros
+#include <pthread.h>
+#define MA_SPIN_LOCK_UNLOCKED PTHREAD_MUTEX_INITIALIZER
+#define ma_spin_lock_init(x) pthread_mutex_init(x,NULL);
+#section marcel_macros
 #define ma_spin_is_locked(x)	({ int ___ret; \
 				pthread_mutex_t *___px = (x); \
 				if (!(___ret = pthread_mutex_trylock(___px))) \
@@ -89,7 +84,8 @@ typedef pthread_mutex_t ma_spinlock_t;
 #define _ma_raw_spin_lock(x) do { if (pthread_mutex_lock((x))) \
 				abort() } \
 				while (0)
-#endif
 
-#endif /* MA__LWPS */
+#section common
+#endif /* type of LOCK */
+#endif /* LWPS & !HAVE_COMPAREEXCHANGE*/
 #section marcel_inline

2f2b57a8314355f8231cdfca99aaffcd983dcdcc
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-21 19:26:14 +0000

    r6557@implementation:  samy | 2006-11-21 20:00:46 +0100  correction génération des bulles
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14298 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubblegum/source/geneC.c
+++ profile/bubblegum/source/geneC.c
@@ -2,32 +2,30 @@
 
 FILE * fw;
 static int bid;
-static int faux_bid;
 static int tid;
 
 /* Fonction de parcours de bulle
    si mode est placé à GENERE, écriture dans le fichier généré
    sinon VISITE our simple parcours */
 
-int parcourir_bulle(Element* bulle, int where, int mode) 
+int parcourir_bulle(Element* bulle, int mybid, int mode) 
 {
    int i;
    Element * element_i;
    TypeElement type;
 
-   if (where == 0)
+   if (mybid == 0)
    {
 	  if (mode == 1)
       {
-         fwprintf(fw,L"   //création de la bulle %d :\n",bid);
-	 fprintf(fw,"   marcel_bubble_init(&b%d);\n",bid);
-	 //fprintf(fw,"   marcel_bubble_setprio(&b%d,%d);\n\n",bid,GetPrioriteBulle(bulle));
+         fwprintf(fw,L"   //création de la bulle %d :\n",mybid);
+	 fprintf(fw,"   marcel_bubble_init(&b%d);\n",mybid);
+	 //fprintf(fw,"   marcel_bubble_setprio(&b%d,%d);\n\n",mybid,GetPrioriteBulle(bulle));
       }
 	  if (mode == 2)
       {
          fprintf(fw,"marcel_bubble_t b0;\n");
       }
-	  faux_bid = 0;
    }
    int taillebulle = GetNbElement(bulle);
    for (i = 1; i <= taillebulle; i++)
@@ -36,18 +34,19 @@ int parcourir_bulle(Element* bulle, int where, int mode)
 	  type = GetTypeElement(element_i);
 	  if (type == BULLE)
       {
-         bid ++;
+   	int hisbid = ++bid;
          if(mode == 1)
          {
-            fwprintf(fw,L"   //création de la bulle %d :\n",bid);
-            fprintf(fw,"   marcel_bubble_init(&b%d);\n",bid);
-            fprintf(fw,"   marcel_bubble_insertbubble(&b%d, &b%d);\n",where,bid);
-            //fprintf(fw,"   marcel_bubble_setprio(&b%d,%d);\n\n",bid,GetPrioriteBulle(element_i));
+            fwprintf(fw,L"   //création de la bulle %d :\n",hisbid);
+            fprintf(fw,"   marcel_bubble_init(&b%d);\n",hisbid);
+            fprintf(fw,"   marcel_bubble_insertbubble(&b%d, &b%d);\n",mybid,hisbid);
+            //fprintf(fw,"   marcel_bubble_setprio(&b%d,%d);\n\n",hisbid,GetPrioriteBulle(element_i));
          }
          if(mode == 2)
          {
-            fprintf(fw,"marcel_bubble_t b%d;\n",bid);
+            fprintf(fw,"marcel_bubble_t b%d;\n",hisbid);
          }
+         parcourir_bulle(element_i,hisbid,mode);
       }  
 	  if (type == THREAD)
       {
@@ -57,28 +56,18 @@ int parcourir_bulle(Element* bulle, int where, int mode)
             fprintf(fw,"   {\n");
             fprintf(fw,"      marcel_attr_t attr;\n");
             fprintf(fw,"      marcel_attr_init(&attr);\n");
-            fprintf(fw,"      marcel_attr_setinitbubble(&attr, &b%d);\n",where);
+            fprintf(fw,"      marcel_attr_setinitbubble(&attr, &b%d);\n",mybid);
             fprintf(fw,"      marcel_attr_setid(&attr,%d);\n",GetId(element_i));
             fprintf(fw,"      marcel_attr_setprio(&attr,%d);\n",GetPrioriteThread(element_i));		  
             fprintf(fw,"      marcel_attr_setname(&attr,\"%s\");\n",GetNom(element_i));
-            fprintf(fw,"      marcel_create(&t%d, &attr, f, (any_t)%d);\n",GetId(element_i),GetId(element_i)*100+GetCharge(element_i));
+            fprintf(fw,"      marcel_create(&t%d, &attr, f, (any_t)%d);\n",tid,GetId(element_i)*100+GetCharge(element_i));
             fprintf(fw,"   }\n\n");
          }
          if(mode == 2)
          {
-            fprintf(fw,"marcel_t t%d;\n",GetId(element_i)); 
+            fprintf(fw,"marcel_t t%d;\n",tid); 
          }
          tid ++;
-      }
-   }
-   for(i = 1; i <= taillebulle; i++)
-   {
-	  element_i = GetElement(bulle,i);
-	  type = GetTypeElement(element_i);
-	  if (BULLE == GetTypeElement(element_i))
-      {
-         faux_bid ++;
-         parcourir_bulle(element_i,faux_bid,mode);
       }
    }
    return 0;
@@ -94,8 +83,7 @@ int gen_fichier_C(Element * bullemere)
 	  return -1;
    }
    bid = 0;
-   faux_bid = 0;
-   tid = 0;
+   tid = 1;
    fw = fopen(GENEC_NAME ".c","w");
    if (fw == NULL)
    {
@@ -114,6 +102,7 @@ int gen_fichier_C(Element * bullemere)
    fprintf(fw,"   int sum = 0;\n");
    fprintf(fw,"   marcel_printf(\"some work %%d in %%d\\n\",load,id);\n");
    fprintf(fw,"   for (n=0;n<load*10000000;n++) sum+=n;\n");
+   fprintf(fw,"   marcel_printf(\"%%d done\\n\",id);\n");
    fprintf(fw,"   return (void*)sum;\n}\n\n");
 
    parcourir_bulle(bullemere,0,2);
@@ -132,8 +121,8 @@ int gen_fichier_C(Element * bullemere)
    fprintf(fw,"   marcel_bubble_spread(&b0, marcel_topo_level(0,0));\n");
   
    fprintf(fw,"\n   marcel_printf(\"ok\\n\");\n");
-   //   fprintf(fw,"\n   profile_stop();\n");
-   fprintf(fw,"   marcel_end();\n\n");
+   fprintf(fw,"\n   profile_stop();\n");
+   //fprintf(fw,"   marcel_end();\n\n");
    fprintf(fw,"   return 0;\n}\n");
 
    fclose(fw);

e49697198b2715cf618d0c33ca1274222855e6a5
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-21 13:50:22 +0000

    testing non blocking send
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14282 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/star.c
@@ -0,0 +1,59 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  int numtasks, rank;
+  int tag = 1;
+  int counter, nbpings, message;
+  MPI_Status stat;
+
+  // Initialise MPI
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+  printf("Rank %d Size %d\n", rank, numtasks);
+
+  if (rank == 0) {
+    int child;
+    MPI_Request *out_requests;
+    MPI_Request *in_requests;
+    float buffer[2], r_buffer[2];
+
+    buffer[0] = 12.45;
+    buffer[1] = 3.14;
+
+    out_requests = malloc(numtasks * sizeof(MPI_Request));
+    in_requests = malloc(numtasks * sizeof(MPI_Request));
+
+    for(child=1 ; child<numtasks ; child++) {
+      MPI_Isend(buffer, 2, MPI_FLOAT, child, tag, MPI_COMM_WORLD, &out_requests[child-1]);
+      fprintf(stderr, "ISending to child %d completed\n", child);
+    }
+
+    for(child=1 ; child<numtasks ; child++) {
+      fprintf(stderr, "Waiting for sending to child %d completed\n", child);
+      MPI_Wait(&out_requests[child-1], NULL);
+    }
+
+    for(child=1 ; child<numtasks ; child++) {
+      MPI_Recv(r_buffer, 2, MPI_FLOAT, child, tag, MPI_COMM_WORLD, &stat);
+      if (r_buffer[0] != buffer[0] && r_buffer[1] != buffer[1]) {
+	fprintf(stderr, "Expected [%f,%f] - Received [%f,%f]\n", buffer[0], buffer[1], r_buffer[0], r_buffer[1]);
+      }
+      else {
+	fprintf(stdout, "Message from child %d [%f,%f]\n", child, r_buffer[0], r_buffer[1]);
+      }
+    }
+  }
+  else {
+    int father = 0;
+    float r_buffer[2];
+    MPI_Recv(r_buffer, 2, MPI_FLOAT, father, tag, MPI_COMM_WORLD, &stat);
+    MPI_Send(r_buffer, 2, MPI_FLOAT, father, tag, MPI_COMM_WORLD);
+  }
+  MPI_Finalize();
+  exit(0);
+}

299ffd48e3a6eab64649e211e0158ed06e7c16a4
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-22 10:52:44 +0000

    test MPI_Allreduce
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14304 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/mpi/global.c
+++ nmad/examples/mpi/global.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv) {
   float buffer[2];
   int reduce[2];
   int global_sum[2];
+  int global_product[2];
 
   // Initialise MPI
   MPI_Init(&argc,&argv);
@@ -24,7 +25,15 @@ int main(int argc, char **argv) {
   reduce[0] = rank*10;
   reduce[1] = 15+rank;
   MPI_Reduce(reduce, global_sum, 2, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-  fprintf(stdout, "[%d] Global sum [%d,%d]\n", rank, global_sum[0], global_sum[1]);
+
+  if (rank == 0) {
+    fprintf(stdout, "[%d] Global sum [%d,%d]\n", rank, global_sum[0], global_sum[1]);
+  }
+
+  reduce[0] = (rank+1)*2;
+  reduce[1] = rank+1;
+  MPI_Allreduce(reduce, global_product, 2, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
+  fprintf(stdout, "[%d] Global product [%d,%d]\n", rank, global_product[0], global_product[1]);
 
   MPI_Finalize();
   exit(0);

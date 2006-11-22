95db313533a584e94241429372f1a5fc7df0d5c7
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-22 12:46:19 +0000

    - gprof profile option
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14306 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ generic/config/options/00gprof.sh
@@ -0,0 +1,13 @@
+# auto generated from generic/config/options/*
+if [ "${PM2_ARCH}" = IA64_ARCH -o "${PM2_ARCH}" = X86_64_ARCH ]; then
+  PM2_@LIB@_CFLAGS_KERNEL="$PM2_@LIB@_CFLAGS_KERNEL -pg -fno-omit-frame-pointer"
+else
+  PM2_@LIB@_CFLAGS_KERNEL="$PM2_@LIB@_CFLAGS_KERNEL -pg -fno-omit-frame-pointer"
+fi
+if [ "$PM2_SYS" = LINUX_SYS -o "$PM2_SYS" = GNU_SYS ]; then
+  if [ "$PM2_@LIB@_BUILD_DYNAMIC" = yes ]; then
+    PM2_@LIB@_EARLY_LDFLAGS_KERNEL="$PM2_@LIB@_EARLY_LDFLAGS_KERNEL -rdynamic"
+  else
+    PM2_@LIB@_EARLY_LDFLAGS="$PM2_@LIB@_EARLY_LDFLAGS -rdynamic"
+  fi
+fi

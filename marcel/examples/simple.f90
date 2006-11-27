6330c76d0795b8b5dcecb37d59b8218bc6c63945
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-27 16:31:51 +0000

    r6622@implementation:  samy | 2006-11-27 17:01:50 +0100  support de base Fortran (create/yield/join)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14343 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/simple.f90
@@ -0,0 +1,53 @@
+! 
+! PM2: Parallel Multithreaded Machine
+! Copyright (C) 2006 "the PM2 team" (see AUTHORS file)
+! 
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or (at
+! your option) any later version.
+! 
+! This program is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! General Public License for more details.
+! 
+
+program launch
+
+  external marcel_create
+  external marcel_join
+  external marcel_self
+  external marcel_yield
+  external writer
+
+  integer, parameter :: nb = 3
+  integer i
+  integer (kind=8) :: pid(nb)
+
+  call marcel_init()
+
+  do i = 1 , nb
+     call marcel_create(pid(i), writer, i) ! a tester sur d'autres architectures et compilateurs
+  end do
+
+  call marcel_yield()
+
+   do i = 1 , nb
+      call marcel_join (pid(i))
+   end do
+
+  call marcel_end()
+  
+end program launch
+
+
+subroutine writer(arg)
+
+  integer arg
+  integer (kind=8) :: self
+
+  call marcel_self(self)
+  write (*,*) "marcel says I'm", self, "and my arg say I'm thread number", arg
+  
+end subroutine writer
---
+++ marcel/source/marcel_fortran.c
@@ -0,0 +1,87 @@
+
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2006 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "marcel.h"
+
+
+extern int  iargc_();
+extern void getarg_(int*,char*,int);
+
+
+void marcel_init_(){
+  int argc = 0;
+  int argsize = 1024;
+  char **argv = NULL;
+  int i = 0;
+  int j = 0;
+
+  /* Recover the args with the Fortran routines iargc_ and getarg_ */
+  argc = iargc_() + 1;
+    
+  argv = (char **) malloc (argc * sizeof (char *));
+
+  for(i = 0; i < argc; i++){
+
+    argv[i] = (char *) malloc (argsize * sizeof (char));
+    getarg_ (&i, argv[i], argsize);
+    argv[i][argsize] = 0;
+
+    /* Trim trailing blanks */
+    j = argsize - 1;
+    while(j!= 0 && argv[i][j] == ' '){
+
+      argv[i][j] = 0;
+      j--;
+    }
+
+  }
+  
+  marcel_init (&argc, argv);
+
+  for(i = 0; i < argc; i++){
+    free (argv[i]);
+  }
+
+  free(argv);
+}
+
+
+void marcel_end_ (){
+  marcel_end();
+}
+
+
+void marcel_self_(marcel_t *self){
+  *self = marcel_self();
+}
+
+// a tester sur d'autres architectures et compilateurs
+void marcel_create_(marcel_t *pid, void* (*func)(void*), void *arg){
+  marcel_create (pid, NULL,  func, arg);
+}
+
+
+void marcel_join_(marcel_t *pid){
+  marcel_join(*pid, NULL);
+}
+
+
+void marcel_yield_(){
+  marcel_yield();
+}

7a33a84ab37a157bf0f3c2244f34cbf2a120c6b3
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-28 16:12:52 +0000

    adding MPI_Testany()
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14360 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/protocols/proto_mpi/include/mpi_pt2pt.h
+++ nmad/protocols/proto_mpi/include/mpi_pt2pt.h
@@ -59,6 +59,12 @@ int MPI_Test(MPI_Request *request,
              int *flag,
              MPI_Status *status);
 
+int MPI_Testany(int count,
+                MPI_Request *array_of_requests,
+                int *index,
+                int *flag,
+                MPI_Status *status);
+
 int MPI_Iprobe(int source,
                int tag,
                MPI_Comm comm,

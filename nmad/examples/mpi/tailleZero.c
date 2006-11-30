79da929c11185e24d144dda4ec8a78ed32fd0028
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-11-30 16:56:11 +0000

    test message of size 0
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14384 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/tailleZero.c
@@ -0,0 +1,29 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int numtasks, rank, tag=1;
+  int counter,  message;
+  MPI_Status stat;
+
+  // Initialise MPI
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+  printf("Rank %d Size %d\n", rank, numtasks);
+
+  if (rank == 0) {
+    counter=1;
+    MPI_Send(&counter, 0, MPI_INT, 1, tag, MPI_COMM_WORLD);
+    MPI_Recv(&message, 0, MPI_INT, 1, tag, MPI_COMM_WORLD, &stat);
+    fprintf(stderr, "Received %d\n", message);
+  }
+  else {
+    MPI_Recv(&message, 0, MPI_INT, 0, tag, MPI_COMM_WORLD, &stat);
+    MPI_Send(&message, 0, MPI_INT, 0, tag, MPI_COMM_WORLD);
+  }
+  MPI_Finalize();
+  exit(0);
+}

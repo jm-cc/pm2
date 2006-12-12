3ea4250fe41a9a6fdb44dfcdc44c2a01a32c8d4b
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2006-12-12 16:15:02 +0000

    update the program to be similar to indexType_hetero_size
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14459 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/mpi/benchmarks/mpi_multi_ping_hetero.c
+++ nmad/examples/mpi/benchmarks/mpi_multi_ping_hetero.c
@@ -20,59 +20,30 @@
 #include <unistd.h>
 #include "mpi.h"
 
-//#define MIN       (MIN_PACKS * 4)
-//#define MAX       (8 * 1024 * 1024)
-
 #define NB_LINES 10
 #define SMALL 64
 #define LARGE (256 * 1024)
 
-//#define MIN_PACKS 1
-//#define MAX_PACKS 4//MAX
-
 #define NB_TESTS     100
 
+int main(int argc, char **argv) {
+  MPI_Request s_request[NB_LINES * 2];
+  MPI_Request r_request[NB_LINES * 2];
 
-MPI_Request *s_request[NB_LINES * 2];
-MPI_Request *r_request[NB_LINES * 2];
+  int	comm_rank	= -1;
+  int	comm_size	= -1;
+  char	host_name[1024]	= "";
+  int   param_log_only_master   = 0;
 
-static int	comm_rank	= -1;
-static int	comm_size	= -1;
-static char	host_name[1024]	= "";
-static const int param_log_only_master   = 0;
+  int    log;
+  int    ping_side;
+  int    rank_dst;
+  int    i, j, k, test;
+  double t1, t2, sum;
 
-static __inline__
-uint32_t _next(uint32_t len)
-{
-  if(!len)
-    return 4;
-  //else if(len < 32)
-  //  return len + 4;
-  //else if(len < 1024)
-  //  return len + 32;
-  else
-    return len << 1;
-}
-
-static
-void
-usage(void) {
-  fprintf(stderr, "usage: hello [[-h <remote_hostname>] <remote url>]\n");
-  exit(EXIT_FAILURE);
-}
-
-int
-main(int	  argc,
-     char	**argv) {
-  int log;
-  int ping_side;
-  int rank_dst;
-  char	*recv_buf		= NULL;
-  char	*small_buf		= NULL;
-  char	*large_buf		= NULL;
-  int len, i, j, test;
-
-
+  char	buffer[(SMALL+LARGE) * NB_LINES];
+  char	data[(SMALL+LARGE) * NB_LINES];
+  char	data2[(SMALL+LARGE) * NB_LINES];
 
   /* ------------------- Initialisation ------------------ */
   MPI_Init(&argc,&argv);
@@ -93,182 +64,82 @@ main(int	  argc,
 
   if (comm_rank == 0 && log) {
     fprintf(stderr, "The configuration size is %d\n", comm_size);
-    fprintf(stderr, " src | dst |     size     | nb_blocks |     latency     |     10^6 B/s   |   MB/s    |\n");
+    fprintf(stderr, "src  | dest  | type	     | size    | blocks | time");
   }
 
   if (comm_size & 1) {
     if (log)
       fprintf(stderr, "This program requires an even configuration size, aborting...\n");
-
-    goto out;
+    MPI_Finalize();
+    exit(0);
   }
 
   ping_side	= !(comm_rank & 1);
   rank_dst	= ping_side?(comm_rank | 1):(comm_rank & ~1);
 
-  if (ping_side) {
-    fprintf(stderr, "(%d): ping with %d\n", comm_rank, rank_dst);
-  } else {
-    if (log)
-      fprintf(stderr, "(%d): pong with %d\n", comm_rank, rank_dst);
-  }
-
-
-  for(i = 0; i < NB_LINES * 2; i++){
-    s_request[i] = malloc(sizeof(MPI_Request));
-    r_request[i] = malloc(sizeof(MPI_Request));
-  }
-
-
   /* ------------------------------------------------- */
   /* ------------------- Multi-ping ------------------ */
 
-  recv_buf = malloc((SMALL+LARGE) * NB_LINES);
-  small_buf = malloc(SMALL);
-  large_buf = malloc(LARGE);
-
-  memset(recv_buf, 0, (SMALL+LARGE) * NB_LINES);
-  memset(small_buf, 'a', SMALL);
-  memset(large_buf, 'b', LARGE);
-
-  if (!ping_side) {
-    /* server */
-
-    for(i = 0; i < NB_LINES; i++){
-
-      for(test=0 ; test<NB_TESTS ; test++) {
-        MPI_Barrier(MPI_COMM_WORLD);
-
-
-
+  for(i = 0; i < NB_LINES; i++) {
+    sum = 0;
+    for(test=0 ; test<NB_TESTS ; test++) {
+      if (!ping_side) {
+        /* server */
         for(j = 0; j <= i; j++){
-          MPI_Irecv(recv_buf + j * (SMALL + LARGE),  SMALL,
-                    MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    r_request[j*2]);
-
-          //printf("r_request[%d] = %p\n", j*2, r_request[j*2]);
-
-          MPI_Irecv(recv_buf + j *(SMALL + LARGE) + SMALL,
-                    LARGE, MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    r_request[j*2 + 1]);
-          //printf("r_request[%d] = %p\n", j*2 + 1, r_request[j*2 + 1]);
+          MPI_Irecv(buffer + j * (SMALL + LARGE),  SMALL, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(r_request[j*2]));
+          MPI_Irecv(buffer + j *(SMALL + LARGE) + SMALL, LARGE, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(r_request[j*2 + 1]));
         }
-
-        //printf("Dernière request : %d --> %d\n", j-1, (j-1)*2 +1);
-
-
-        //printf("-->les I-réception ok\n");
-
         for(j = 0; j < (i+1) * 2; j++){
-          //printf("MPI_WAIT[%d]\n", j);
-          MPI_Wait(r_request[j], NULL);
-          //printf("-->fin du wait\n");
+          MPI_Wait(&(r_request[j]), NULL);
         }
-
-        //printf("-->les wait des I-réception ok\n");
-
         for(j = 0; j <= i; j++){
-          MPI_Isend(small_buf, SMALL, MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    s_request[j*2]);
-          MPI_Isend(large_buf,  LARGE, MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    s_request[j*2 + 1]);
+          MPI_Isend(buffer + j * (SMALL+LARGE), SMALL, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(s_request[j*2]));
+          MPI_Isend(buffer + j *(SMALL+LARGE) + SMALL,  LARGE, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(s_request[j*2 + 1]));
         }
-        //printf("Dernière request : %d --> %d\n", j-1, (j-1)*2 +1);
-        //printf("-->les I-send ok\n");
-
         for(j = 0; j < (i+1) * 2; j++){
-          //printf("MPI_WAIT[%d]\n", j);
-          MPI_Wait(s_request[j], NULL);
-          //printf("-->fin du wait\n");
+          MPI_Wait(&(s_request[j]), NULL);
         }
-
-        //printf("-->les wait des I-send ok\n");
-
       }
-    }
-
-
-    } else {
-      /* client */
-
-    double t1, t2, sum, lat, bw_million_byte, bw_mbyte;
-
-    for(i = 0; i < NB_LINES; i++){
-      sum = 0;
-
-      for(test=0 ; test<NB_TESTS ; test++) {
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
+      else {
+        for(k=0 ; k<(SMALL+LARGE) * i ; k++) {
+          data[k] = 'a'+(i%26);
+        }
+        /* client */
         t1 = MPI_Wtime();
-
         for(j = 0; j <= i; j++){
-          MPI_Isend(small_buf, SMALL, MPI_CHAR,
-                   rank_dst, 0, MPI_COMM_WORLD,
-                    s_request[j*2]);
-          MPI_Isend(large_buf,  LARGE, MPI_CHAR,
-                   rank_dst, 0, MPI_COMM_WORLD,
-                    s_request[j*2 + 1]);
+          MPI_Isend(data + j * (SMALL+LARGE), SMALL, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(s_request[j*2]));
+          MPI_Isend(data + j * (SMALL+LARGE) + SMALL,  LARGE, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(s_request[j*2 + 1]));
         }
-        //printf("Dernière request : %d --> %d\n", j-1, (j-1)*2 +1);
-        //printf("-->les I-send ok\n");
-
         for(j = 0; j < (i+1) * 2; j++){
-          //printf("MPI_WAIT[%d]\n", j);
-          MPI_Wait(s_request[j], NULL);
-          //printf("-->fin du wait\n");
+          MPI_Wait(&(s_request[j]), NULL);
         }
-
-
-        //printf("-->les wait des I-send ok\n");
-
-
         for(j = 0; j <= i; j++){
-          MPI_Irecv(recv_buf + j * (SMALL + LARGE),  SMALL,
-                    MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    r_request[j*2]);
-          //printf("r_request[%d] = %p\n", j*2, r_request[j*2]);
-          MPI_Irecv(recv_buf + j *(SMALL + LARGE) + SMALL,
-                    LARGE, MPI_CHAR,
-                    rank_dst, 0, MPI_COMM_WORLD,
-                    r_request[j*2 + 1]);
-          //printf("r_request[%d] = %p\n", j*2 + 1, r_request[j*2 + 1]);
+          MPI_Irecv(data2 + j * (SMALL + LARGE),  SMALL, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(r_request[j*2]));
+          MPI_Irecv(data2 + j *(SMALL + LARGE) + SMALL, LARGE, MPI_CHAR,
+                    rank_dst, 0, MPI_COMM_WORLD, &(r_request[j*2 + 1]));
         }
-        //printf("Dernière request : %d --> %d\n", j-1, (j-1)*2 +1);
-        //printf("-->les I-réception ok\n");
-
-        for(j = 0; j < (i+1) * 2; j++){
-          //printf("MPI_WAIT[%d] = %p\n", j, r_request[j]);
-          MPI_Wait(r_request[j], NULL);
-          //printf("-->fin du wait\n");
+        for(j = 0; j < (i+1) * 2; j++) {
+          MPI_Wait(&(r_request[j]), NULL);
         }
-
-
-        //printf("-->les wait des I-recv ok\n");
-
-
         t2 = MPI_Wtime();
         sum += (t2 - t1) * 1e6;
       }
-
-
+    }
+    if (ping_side) {
       int len = (i+1) * (SMALL + LARGE);
       int nb_blocks = (i+1) * 2;
 
-      lat	      = sum / (2 * NB_TESTS);
-      bw_million_byte = len * (NB_TESTS / (sum / 2));
-      bw_mbyte        = bw_million_byte / 1.048576;
-
-      printf("%5d %5d %12d %9d  %12.3f\t %8.3f \t %8.3f\n",
-             comm_rank, rank_dst, len, nb_blocks,
-             lat, bw_million_byte, bw_mbyte);
+      sum /= 2;
+      printf("%d\t%d\t%s\t%d\t%d\t%3.2f\n", comm_rank, rank_dst, "multi_isend", len, nb_blocks, sum/NB_TESTS);
     }
   }
- out:
-  return 0;
+  MPI_Finalize();
+  exit(0);
 }

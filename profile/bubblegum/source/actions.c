1e733075707e7257b18c7c50b3c6559ba04fed97
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-01-16 15:10:37 +0000

    r6980@implementation:  samy | 2007-01-16 16:10:09 +0100   r6978@implementation (orig r1013):  sthibault | 2007-01-16 16:06:06 +0100   montrer ce qui se passe en bas
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14563 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubblegum/source/actions.c
+++ profile/bubblegum/source/actions.c
@@ -345,7 +345,7 @@ void ExecuterFlash(GtkWidget *widget, gpointer data)
    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), 0.2);
    gtk_main_iteration_do(FALSE);
    char command[1024];
-   snprintf(command,sizeof(command),"( make "GENEC_NAME" ; pm2load "GENEC_NAME" --marcel-nvp 4 --marcel-maxarity 2 ; bubbles -d /tmp/prof_file_user_%s ; realplay autobulles.swf ) &",getenv("USER"));
+   snprintf(command,sizeof(command),"( make "GENEC_NAME" ; pm2load "GENEC_NAME" --marcel-nvp 4 --marcel-maxarity 2 ; bubbles -x 1024 -y 800 -d /tmp/prof_file_user_%s ; realplay autobulles.swf ) &",getenv("USER"));
    system(command);
    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), 1);
    gtk_main_iteration_do(FALSE);

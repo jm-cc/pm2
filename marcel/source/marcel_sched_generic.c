e93c9370db573c955e4df2009b79e5c5100efa34
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-01-18 14:27:04 +0000

    r7035@implementation:  samy | 2007-01-18 15:17:44 +0100   r7032@implementation (orig r97):  thibault | 2007-01-18 15:15:02 +0100   inutile de désactiver les signaux lorsque l'on n'utilise pas pause dans idle
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14589 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_sched_generic.c
+++ marcel/source/marcel_sched_generic.c
@@ -439,25 +439,26 @@ static any_t TBX_NORETURN idle_poll_func(any_t hlwp)
 		}
 
 #endif
-		marcel_sig_disable_interrupts();
-		ma_clear_thread_flag(TIF_POLLING_NRFLAG);
-		/* make sure people see that we won't poll it afterwards */
-		ma_smp_mb__after_clear_bit();
-
-		if (ma_need_resched()) {
-			marcel_sig_enable_interrupts();
-			continue;
-		}
 #ifdef MARCEL_IDLE_PAUSE
 		if (dopoll)
 			marcel_sig_nanosleep();
-		else
+		else {
+			marcel_sig_disable_interrupts();
+			ma_clear_thread_flag(TIF_POLLING_NRFLAG);
+			/* make sure people see that we won't poll it afterwards */
+			ma_smp_mb__after_clear_bit();
+
+			if (ma_need_resched()) {
+				marcel_sig_enable_interrupts();
+				continue;
+			}
 			marcel_sig_pause();
+			/* let wakers now that we will shortly poll need_resched and
+			 * thus they don't need to send a kill */
+			ma_set_thread_flag(TIF_POLLING_NRFLAG);
+			marcel_sig_enable_interrupts();
+		}
 #endif
-		/* let wakers now that we will shortly poll need_resched and
-		 * thus they don't need to send a kill */
-		ma_set_thread_flag(TIF_POLLING_NRFLAG);
-		marcel_sig_enable_interrupts();
 	}
 }
 #ifndef MA__ACT

c32e238e95563b82ed4616ed300e8aa3983d5d66
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-01-24 16:01:42 +0000

    - missing commit
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14664 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mad4/config/mad4.sh
+++ mad4/config/mad4.sh
@@ -4,8 +4,8 @@ PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${PM2_ROOT}/nmad/core/include"
 PM2_MAD4_MODULE_DEPEND_LIB="${PM2_MAD4_MODULE_DEPEND_LIB} tbx ntbx marcel"
 
 # tcp is included by default
-PM2_PROTOCOLS="$PM2_PROTOCOLS tcp"
-PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${PM2_ROOT}/nmad/drivers/tcp/include"
-PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DDRV_TCP=mad_TCP"
+PM2_PROTOCOLS="$PM2_PROTOCOLS tcpdg"
+PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${PM2_ROOT}/nmad/drivers/tcp_dgram/include"
+PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DDRV_TCPDG=mad_TCPDG"
 
 PM2_DEFAULT_LOADER=${PM2_ROOT}/mad4/bin/mad4load_conf_not_needed

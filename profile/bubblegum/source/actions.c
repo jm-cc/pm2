66a55bf73c2a9cc457e7cce96f7539f0badea8a6
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-01-25 15:09:39 +0000

    r7164@implementation:  samy | 2007-01-25 16:09:37 +0100  il faut attendre la commande pour pouvoir rapporter son état
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14681 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubblegum/source/actions.c
+++ profile/bubblegum/source/actions.c
@@ -347,13 +347,13 @@ void ExecuterFlash(GtkWidget *widget, gpointer data)
    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), 0.2);
    gtk_main_iteration_do(FALSE);
    char command[1024];
-   snprintf(command,sizeof(command),"( make "GENEC_NAME" && pm2load "GENEC_NAME" --marcel-nvp 4 --marcel-maxarity 2 && bubbles -x 1024 -y 800 -d /tmp/prof_file_user_%s && "
+   snprintf(command,sizeof(command),"make "GENEC_NAME" && pm2load "GENEC_NAME" --marcel-nvp 4 --marcel-maxarity 2 && bubbles -x 1024 -y 800 -d /tmp/prof_file_user_%s && "
 #ifdef DARWIN_SYS
 	    "open"
 #else
 	    "realplay"
 #endif
-	    " autobulles.swf ) &",getenv("USER"));
+	    " autobulles.swf &",getenv("USER"));
    int ret = system(command);
    if (!WIFEXITED(ret)) {
    	if (WIFSIGNALED(ret)) {
@@ -361,7 +361,7 @@ void ExecuterFlash(GtkWidget *widget, gpointer data)
 		if (WCOREDUMP(ret))
 			printf("core dumped\n");
 	} else
-		printf("return status %d\n");
+		printf("return status %d\n", ret);
    }
    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_bar), 1);
    gtk_main_iteration_do(FALSE);

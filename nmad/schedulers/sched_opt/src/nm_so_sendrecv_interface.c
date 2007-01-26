97184fa3f832d99dba4cc43ae229bc776cc47624
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-01-26 09:28:00 +0000

    no longer calls tbx_container_of
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@14696 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/schedulers/sched_opt/src/nm_so_sendrecv_interface.c
+++ nmad/schedulers/sched_opt/src/nm_so_sendrecv_interface.c
@@ -221,10 +221,11 @@ nm_so_sr_irecv(struct nm_so_interface *p_so_interface,
 
   if(gate_id == -1) {
 
-//    while (any_src[tag].nb_requests != 0) {
-//      NMAD_SO_TRACE("[%s] Irecv not completed for ANY_SRC tag=%d\n", __TBX_FUNCTION__, tag);
-//      nm_so_sr_rwait(p_so_interface, (intptr_t) &any_src[tag].status);
-//    }
+    while (any_src[tag].nb_requests != 0) {
+      NMAD_SO_TRACE("[%s] Irecv not completed for ANY_SRC tag=%d nb_requests=%d\n", __TBX_FUNCTION__, tag, any_src[tag].nb_requests);
+      nm_so_sr_rwait(p_so_interface, (intptr_t) &any_src[tag].status);
+      any_src[tag].nb_requests --;
+    }
 
     any_src[tag].status &= ~NM_SO_STATUS_RECV_COMPLETED;
     any_src[tag].nb_requests ++;
@@ -303,21 +304,14 @@ nm_so_sr_rwait(struct nm_so_interface *p_so_interface,
   struct nm_core *p_core = p_so_interface->p_core;
   volatile uint8_t *p_request = (uint8_t *)request;
 
-  /* We cast the status into a any_src_status structure. That does not
-     fail even if the request is not a any_src request, the
-     nb_requests field will then be equal to 0. I am not sure though it
-     is correct to call tbx_container_of when status does not belong
-     to a any_src_status structure!
-  */
-//  struct any_src_status *p_status = tbx_container_of(request, struct any_src_status, status);
-//  if (p_status->nb_requests != 0) {
-//    NMAD_SO_TRACE("[%s] waiting for ANY_SRC request = %d\n", __TBX_FUNCTION__, p_status->nb_requests);
-//    p_status->nb_requests --;
-//    if (p_status->nb_requests != 0) {
-//      NMAD_SO_TRACE("[%s] The request has already been completed\n", __TBX_FUNCTION__);
-//      return NM_ESUCCESS;
-//    }
-//  }
+#ifdef NMAD_SO_DEBUG
+  if (*p_request & NM_SO_STATUS_RECV_COMPLETED) {
+    NMAD_SO_TRACE("[%s] request %p completed\n", __TBX_FUNCTION__, p_request);
+  }
+  else {
+    NMAD_SO_TRACE("[%s] request %p not completed\n", __TBX_FUNCTION__, p_request);
+  }
+#endif
 
   while(!(*p_request & NM_SO_STATUS_RECV_COMPLETED))
     nm_schedule(p_core);

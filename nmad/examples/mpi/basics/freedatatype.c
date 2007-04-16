8ef2733d2878f48bbf49a3f805b5f477090a2130
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-04-16 10:27:12 +0000

    new test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@15777 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/basics/freedatatype.c
@@ -0,0 +1,38 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int numtasks, rank;
+  MPI_Datatype mytype;
+
+  // Initialise MPI
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+  MPI_Type_contiguous(2, MPI_INT, &mytype);
+  MPI_Type_commit(&mytype);
+
+  if (rank == 0) {
+    int buffer[4] = {1, 2, 3, 4};
+
+    fprintf(stdout, "sending data\n");
+    MPI_Send(buffer, 2, mytype, 1, 10, MPI_COMM_WORLD);
+    MPI_Type_free(&mytype);
+  }
+  else if (rank == 1) {
+    MPI_Request request;
+    MPI_Status status;
+    int buffer[4];
+
+    fprintf(stdout, "receiving data\n");
+    MPI_Irecv(buffer, 2, mytype, 0, 10, MPI_COMM_WORLD, &request);
+    MPI_Type_free(&mytype);
+    MPI_Wait(&request, &status);
+    printf("Received data [%d, %d, %d, %d]\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+  }
+
+  MPI_Finalize();
+  exit(0);
+}

9e6e459edb91a0834f6bb9302b4574b578d6c1c7
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-05-03 12:29:40 +0000

    r7986@implementation:  samy | 2007-04-02 15:41:20 +0200  correction adresse passée à mbind: elle doit être alignée
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16041 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_sysdep.c
+++ marcel/source/marcel_sysdep.c
@@ -224,11 +224,10 @@ void ma_migrate_mem(void *ptr, size_t size, int node) {
 #if defined(MPOL_BIND) && defined(MPOL_MF_MOVE) && defined(MPOL_MF_MOVE_ALL)
 	unsigned long mask = 1<<node;
 	uintptr_t addr = ((uintptr_t)ptr) & ~(getpagesize()-1);
-	size += ((uintptr_t)ptr) - addr;
 	if (node < 0 || ma_numa_not_available)
 		return;
-	if (mbind(ptr, size, MPOL_BIND, &mask, sizeof(mask)*CHAR_BIT, MPOL_MF_MOVE_ALL) == -1 && errno == EPERM)
-		mbind(ptr, size, MPOL_BIND, &mask, sizeof(mask)*CHAR_BIT, MPOL_MF_MOVE);
+	if (mbind((void*)addr, size, MPOL_BIND, &mask, sizeof(mask)*CHAR_BIT, MPOL_MF_MOVE_ALL) == -1 && errno == EPERM)
+		mbind((void*)addr, size, MPOL_BIND, &mask, sizeof(mask)*CHAR_BIT, MPOL_MF_MOVE);
 }
 #elif defined(OSF_SYS)
 #define HAS_NUMA

b914cfe835d54190fe4471cebf7f740399c6e288
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-05-03 12:29:51 +0000

    r7989@implementation:  samy | 2007-04-03 16:52:39 +0200  des fois que per_item_load soit plus petit que 30, on ne veut vraiment pas descendre les tâches sans poid
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16044 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/scheduler-marcel/marcel_bubble_spread.c
+++ marcel/source/scheduler-marcel/marcel_bubble_spread.c
@@ -143,7 +143,7 @@ static void __marcel_bubble_spread(marcel_entity_t *e[], int ne, struct marcel_t
 		debug("entity %p(%ld)\n",e[i],ma_entity_load(e[i]));
 		/* when entities' load is very small, just leave them here */
 		/* TODO: tune */
-		if (ma_entity_load(e[i]) <= per_item_load/30) {
+		if (entity_load(e[i]) <= per_item_load/30) {
 			int state;
 			ma_runqueue_t *rq;
 			debug("small(%lx), leave it here\n", entity_load(e[i]));

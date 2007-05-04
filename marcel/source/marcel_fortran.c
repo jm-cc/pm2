640b83e938bdb230a7f5e51419ec13cc8923793d
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-05-04 16:58:07 +0000

    manage arguments using tbx routine
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16112 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_fortran.c
+++ marcel/source/marcel_fortran.c
@@ -21,40 +21,11 @@
 
 
 #ifdef MARCEL_FORTRAN
-extern int  iargc_();
-extern void getarg_(int*,char*,int);
-
-
 void marcel_init_(){
-  int argc = 0;
-  char *argv[] = {NULL};
-#if 0
-  int argsize = 1024;
-  char **argv = NULL;
-  int i = 0;
-  int j = 0;
+  int i, argc;
+  char **argv;
 
-  /* Recover the args with the Fortran routines iargc_ and getarg_ */
-  argc = iargc_() + 1;
-    
-  argv = (char **) malloc (argc * sizeof (char *));
-
-  for(i = 0; i < argc; i++){
-
-    argv[i] = (char *) malloc (argsize * sizeof (char));
-    getarg_ (&i, argv[i], argsize);
-    argv[i][argsize-1] = 0;
-
-    /* Trim trailing blanks */
-    j = argsize - 2;
-    while(j!= 0 && argv[i][j] == ' '){
-
-      argv[i][j] = 0;
-      j--;
-    }
-
-  }
-  
+  tbx_fortran_init(&argc, &argv);  
   marcel_init (&argc, argv);
 
   for(i = 0; i < argc; i++){
@@ -62,7 +33,6 @@ void marcel_init_(){
   }
 
   free(argv);
-#endif
 }
 
 
@@ -95,16 +65,19 @@ void marcel_set_load_(int *load) {
   *marcel_stats_get(marcel_self(), marcel_stats_load_offset) = *load;
 }
 
+/* Utile pour debug */
+void marcel_rien_() {
+}
+
+#ifdef MA__BUBBLES
 /* TODO: rajouter la bulle qu'on soumet en parametre */
 void marcel_bubble_submit_() {
   current_sched->submit(&marcel_root_bubble.sched);
 }
 
-/* Utile pour debug */
-void marcel_rien_() {
-}
-
 void marcel_spread_() {
   marcel_bubble_spread(&marcel_root_bubble, marcel_machine_level);
 }
+#endif /* MA__BUBBLES */
+
 #endif

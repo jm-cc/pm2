324144de6dc935e9531ef8613e4998685b088381
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-05-04 12:25:48 +0000

    allow to link C applications when using intel fortran compiler
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16104 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/protocols/proto_mpi/scripts/arg_ifort.c
@@ -0,0 +1,33 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * arg.c
+ * =====
+ */
+
+/**
+ * These functions must be defined if the flavor has been set with the
+ * Intel Fortran compiler. When compiling with mpif77 or mpif90, the
+ * functions will be defined in the libraries of the Intel Fortran
+ * compiler, when compiling with mpicc, they must be defined
+ * otherwise.
+ */
+ 
+int iargc_() {
+  return 0;
+}
+void getarg_(int* x, char* y, int z) {
+}

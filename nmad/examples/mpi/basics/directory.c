9478f7d4b271fab7461b3f653f82bd6abb82f804
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-05-10 09:35:09 +0000

    test directory
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16145 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/basics/directory.c
@@ -0,0 +1,23 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  int rank;
+  size_t size = 256;
+  char *cwd;
+
+  // Initialise MPI
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  cwd = malloc(size+1);
+  getcwd(cwd, size);
+
+  printf("[%d] Current directory %s\n", rank, cwd);
+
+  MPI_Finalize();
+  exit(0);
+}
+

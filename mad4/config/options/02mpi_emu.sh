e971bc9563f0582afde1cd6153a1a70898bfcf6c
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-06-08 07:45:44 +0000

    add include for f90
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16442 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mad4/config/options/02mpi_emu.sh
+++ mad4/config/options/02mpi_emu.sh
@@ -1,6 +1,10 @@
 PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -DCONFIG_PROTO_MPI"
 PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${PM2_ROOT}/nmad/protocols/proto_mpi/include"
 
+if [ "$PM2_COMMON_FORTRAN_TARGET" != "none" ] ; then
+    PM2_MAD4_CFLAGS="$PM2_MAD4_CFLAGS -I${PM2_ROOT}/nmad/protocols/proto_mpi/include/f90base"
+fi
+
 PM2_NMAD_PROTO="$PM2_NMAD_PROTO proto_mpi"
 
 PM2_NMAD_LAUNCHER="leonie"

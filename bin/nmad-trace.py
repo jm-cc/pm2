1aa398f53e181edd7c2735186982b23d25c3cd6c
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-06-15 20:25:39 +0000

    - small framework for bi-directional trace replay (requires python >= 2.5)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16531 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ bin/nmad-trace.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python2.5
+class NM_Event:
+    def __init__(self, list):
+	self.previous_length	= int(list.pop(0), 16)
+        self.num	= int(list.pop(0))
+        self.pid	= int(list.pop(0))
+        self.time	= int(list.pop(0))
+        self.tid	= int(list.pop(0))
+        self.code	= int(list.pop(0))
+        self.nb_args	= int(list.pop(0))
+        self.args	= list
+
+    def __repr__(self):
+        return '%08x: %016x - %02x[%04x]: %04x' % (self.num, self.time, self.pid, self.tid, self.code)
+
+# end NM_Event
+
+class NM_Event_file:
+    def __init__(self, filename):
+        self.filename	= filename
+        self.file	= open(self.filename, "rb")
+
+        self.current_event		= None
+	self.current_event_start	= self.file.tell()
+	self.eof			= False
+	
+    def read_event(self):
+        buf	= self.file.readline()
+	if buf == '':
+		return None
+
+        buf	= buf.rstrip('\n')
+        list	= buf.split(',')
+	event	= NM_Event(list)
+
+	return event
+    	
+    def previous_event(self):
+    	if self.current_event == None:
+		return None
+
+	if self.eof:
+		self.eof	= False
+		return self.current_event
+
+	if self.current_event_start == 0:
+		self.file.seek(0)
+		self.current_event	= None
+		return None
+		
+	self.file.seek(self.current_event_start - self.current_event.previous_length)
+	
+	start	= self.file.tell()
+	event	= self.read_event()	
+
+	self.current_event_start	= start
+        self.current_event		= event
+
+        return event
+
+    def next_event(self):
+    	if self.eof:
+		return None
+
+	start	= self.file.tell()
+	event	= self.read_event()	
+
+	if event == None:
+		self.eof	= True
+	else:	
+		self.current_event_start	= start
+       		self.current_event		= event
+
+        return event
+
+
+# end NM_Event_file
+
+# -- main --  example of use
+ev_file	= NM_Event_file('dummy.raw')
+
+while True:
+	event	= ev_file.next_event()
+	if event == None:
+		break
+
+	print event
+
+print '--------------------'
+
+while True:
+	event	= ev_file.previous_event()
+	if event == None:
+		break
+
+	print event
+
+print '--------------------'
+
+print ev_file.next_event()
+
+#
+

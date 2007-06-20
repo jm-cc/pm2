e137be259f5f8aa417682932180fcc5cd369dbdb
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-06-20 16:18:48 +0000

    r377@implementation:  samy | 2007-06-20 18:18:46 +0200   r375@implementation (orig r10):  sthibaul | 2007-06-20 18:39:55 +0200   ne pas définir de trop grosses piles lorsque l'on doit fournir de la TLS sur x86_64 car l'on est alors restreint à 32bits
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16582 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/sys/isomalloc_archdep.h
+++ marcel/include/sys/isomalloc_archdep.h
@@ -26,8 +26,8 @@
  * as much as PTHREAD_STACK_MIN */
 /* Pas de typage pour ASM_THREAD_SLOT_SIZE car la constante est utilisée
    dans un source assembleur */
-#if defined(X86_64_ARCH) || defined(IA64_ARCH) || defined(ALPHA_ARCH) || defined(PPC64_ARCH)
-  #define ASM_THREAD_SLOT_SIZE          (0x400000) /* 4 MB */
+#if (defined(X86_64_ARCH) && !defined(MA__PROVIDE_TLS)) || defined(IA64_ARCH) || defined(ALPHA_ARCH) || defined(PPC64_ARCH)
+  #define ASM_THREAD_SLOT_SIZE          (0x2000000) /* 32 MB */
 #else
   #ifdef MA__LIBPTHREAD
     #define ASM_THREAD_SLOT_SIZE          (0x100000) /* 1 MB */

6858bfadd4bee6e89560009f2c621f67b76c6238
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-07-10 13:16:20 +0000

    persistent requests
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@16856 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/protocols/proto_mpi/include/mpi_persistent.h
@@ -0,0 +1,45 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/*
+ * mpi_persistent.h
+ * ================
+ */
+
+#ifndef MPI_PERSISTENT_H
+#define MPI_PERSISTENT_H
+
+int MPI_Send_init(void* buf,
+                  int count,
+                  MPI_Datatype datatype,
+                  int dest,
+                  int tag,
+                  MPI_Comm comm,
+                  MPI_Request *request);
+
+int MPI_Recv_init(void* buf,
+                  int count,
+                  MPI_Datatype datatype,
+                  int source,
+                  int tag,
+                  MPI_Comm comm,
+                  MPI_Request *request);
+
+int MPI_Start(MPI_Request *request);
+
+int MPI_Startall(int count,
+                 MPI_Request *array_of_requests);
+
+#endif /* MPI_PERSISTENT_H */

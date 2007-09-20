c30ec689496650c717d8ac3f8862d70018f1a3f0
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-09-20 12:34:34 +0000

    - avoid compiling errors on force_raise example when marcel exceptions are not enabled
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@17503 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/force_raise.c
+++ marcel/examples/force_raise.c
@@ -16,6 +16,7 @@
 
 #include "marcel.h"
 
+#ifdef MARCEL_EXCEPTIONS_ENABLED
 marcel_exception_t USER_ERROR = "USER_ERROR";
 
 void deviation(void *arg)
@@ -57,4 +58,12 @@ int marcel_main(int argc, char *argv[])
 
    return 0;
 }
+#else
+#  warning Marcel exceptions must be enabled for this program
+int marcel_main(int argc, char *argv[])
+{
+  fprintf(stderr, "'marcel exceptions' feature disabled in the flavor\n");
+  return 0;
+}
+#endif
 

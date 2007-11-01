dc53aec158990c6282904c6f4c9e8e81529384d6
bgoglin <bgoglin@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-11-01 19:15:28 +0000

    add support for -R <rail> in the monorail helper so that we can dynamically choose the network there too
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@17790 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sched_opt/helper.h
+++ nmad/examples/sched_opt/helper.h
@@ -103,23 +103,27 @@ int nmad_exit(void) {
 #else /* ! CONFIG_PROTO_MAD3 */
 
 static void usage(void) {
-  fprintf(stderr, "usage: <prog> [<remote url>]\n");
+  fprintf(stderr, "usage: <prog> [-R <rail>] [<remote url>]\n");
+  fprintf(stderr, "  The rail may be:\n");
+#if defined CONFIG_MX
+  fprintf(stderr, "    mx   to use any MX board\n");
+  fprintf(stderr, "    mx:N        the Nth MX board\n");
+#endif
+#if defined CONFIG_QSNET
+  fprintf(stderr, "    qsnet       the QsNet board\n");
+#endif
+#if defined CONFIG_IBVERBS
+  fprintf(stderr, "    ib          any InfiniBand device\n");
+  fprintf(stderr, "    ib:N        the Nth InfiniBand device\n");
+#endif
+#if defined CONFIG_GM
+  fprintf(stderr, "    gm          any GM board\n");
+#endif
+  fprintf(stderr, "    tcp         any TCP connection\n");
   exit(EXIT_FAILURE);
 }
 
 typedef int (*nm_driver_load)(struct nm_drv_ops*);
-
-#if defined CONFIG_IBVERBS
-static const nm_driver_load p_driver_load = &nm_ibverbs_load;
-#elif defined CONFIG_MX
-static const nm_driver_load p_driver_load = &nm_mx_load;
-#elif defined CONFIG_GM
-static const nm_driver_load p_driver_load = &nm_gm_load;
-#elif defined CONFIG_QSNET
-static const nm_driver_load p_driver_load = &nm_qsnet_load;
-#else
-static const nm_driver_load p_driver_load = &nm_tcpdg_load;
-#endif
 
 static struct nm_core		*p_core		= NULL;
 static struct nm_so_interface	*sr_if	= NULL;
@@ -151,10 +155,131 @@ nm_gate_id_t get_gate_out_id(int dest) {
   exit(EXIT_FAILURE);
 }
 
+/* get default drivers if no rails were given on the command line
+ */
+static void
+get_default_driver_load(nm_driver_load *load)
+{
+#if defined CONFIG_MX
+  printf("Using MX\n");
+  *load = &nm_mx_load;
+#elif defined CONFIG_QSNET
+  printf("Using QsNet\n");
+  *load = &nm_qsnet_load;
+#elif defined CONFIG_IBVERBS
+  printf("Using IBVerbs\n");
+  *load = &nm_ibverbs_load;
+#elif defined CONFIG_GM
+  printf("Using GM\n");
+  *load = &nm_gm_load;
+#else
+  printf("Using TCPDG\n");
+  *load = &nm_tcpdg_load;
+#endif
+}
+
+/* look for a driver string in a rail and check whether the board id is forced
+ */
+static int
+lookup_rail_driver_and_board_id(const char *rail, const char *driver,
+				struct nm_driver_query_param *param)
+{
+  int len = strlen(driver);
+
+  if (!strcmp(rail, driver))
+    return 1;
+
+  /* look for driver:N */
+  if (strncmp(rail, driver, len))
+    return 0;
+  if (rail[len] != ':')
+    return 0;
+
+  param->key = NM_DRIVER_QUERY_BY_INDEX;
+  param->value.index = atoi(rail+len+1);
+  return 1;
+}
+
+/* handle one rail from the railstring
+ */
+static int
+handle_one_rail(char *token, int index,
+		nm_driver_load *load,
+		struct nm_driver_query_param *param)
+{
+  int board_id;
+
+#if defined CONFIG_MX
+  if (lookup_rail_driver_and_board_id(token, "mx", param)) {
+    if (param->key == NM_DRIVER_QUERY_BY_INDEX)
+      printf("Using MX board #%d for rail #%d\n",
+	     param->value.index, index);
+    else
+      printf("Using any MX board for rail #%d\n",
+	     index);
+    *load = &nm_mx_load;
+  } else
+#endif
+#ifdef CONFIG_QSNET
+  if (!strcmp("qsnet", token)) {
+    printf("Using QsNet for rail #%d\n", index);
+    *load = &nm_qsnet_load;
+  } else
+#endif
+#if defined CONFIG_IBVERBS
+  if (lookup_rail_driver_and_board_id(token, "ib", param)
+      || lookup_rail_driver_and_board_id(token, "ibv", param)
+      || lookup_rail_driver_and_board_id(token, "ibverbs", param)) {
+    if (param->key == NM_DRIVER_QUERY_BY_INDEX)
+      printf("Using IB device #%d for rail #%d\n",
+	     param->value.index, index);
+    else
+      printf("Using any IB device for rail #%d\n",
+	     index);
+    *load = &nm_ibverbs_load;
+  } else
+#endif
+#if defined CONFIG_GM
+  if (!strcmp("gm", token)) {
+    printf("Using GM for rail #%d\n", index);
+    *load = &nm_gm_load;
+  } else
+#endif
+  if (!strcmp("tcp", token) || !strcmp("tcpdg", token)) {
+    printf("Using TCPDG for rail #%d\n", index);
+    *load = &nm_tcpdg_load;
+  } else {
+    fprintf(stderr, "Unrecognized rail \"%s\"\n", token);
+    return -1;
+  }
+
+  return 0;
+}
+
+/* get drivers from the railstring from the command line
+ */
+static void
+get_railstring_driver_load(nm_driver_load *load,
+			   struct nm_driver_query_param *param,
+			   char * railstring)
+{
+  int err;
+
+  err = handle_one_rail(railstring, 0,
+			load, param);
+  if (err < 0)
+    usage();
+}
+
 void
 init(int	 *argc,
      char	**argv) {
   int i, j, err;
+  char *railstring = NULL;
+  nm_driver_load driver_load = NULL;
+  struct nm_driver_query_param params[1];
+
+  params[1].key = NM_DRIVER_QUERY_BY_NOTHING;
 
   err = nm_core_init(argc, argv, &p_core, nm_so_load);
   if (err != NM_ESUCCESS) {
@@ -176,15 +301,26 @@ init(int	 *argc,
 
   i=1;
   while (i<*argc) {
+    /* handle -R for rails */
+    if (!strcmp(argv[i], "-R")) {
+      if (*argc <= i+1)
+	usage();
+      railstring = argv[i+1];
+      i += 2;
+      continue;
+    }
+    /* other options have to be passed after rails and url */
     if (!strncmp(argv[i], "-", 1)) {
       break;
     }
+    /* handle urls */
     else {
       r_url	= argv[i];
       i++;
     }
   }
 
+  /* update command line before returning to the program */
   if (r_url) {
     printf("running as client using remote url: %s\n", r_url);
     i--;
@@ -196,7 +332,15 @@ init(int	 *argc,
     printf("running as server\n");
   }
 
-  err = nm_core_driver_load_init(p_core, p_driver_load, &drv_id, &l_url);
+  if (railstring) {
+    /* parse railstring to get drivers */
+    get_railstring_driver_load(&driver_load, params, railstring);
+  } else {
+    /* use default drivers */
+    get_default_driver_load(&driver_load);
+  }
+
+  err = nm_core_driver_load_init_with_params(p_core, driver_load, params, 1, &drv_id, &l_url);
   if (err != NM_ESUCCESS) {
     printf("nm_core_driver_load_init returned err = %d\n", err);
     goto out_err;

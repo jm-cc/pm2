21d2e06cbdeea508837da276686d14455c8da60a
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-11-12 13:09:48 +0000

    indent properly
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@17863 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/core/include/nm_drivers.h
+++ nmad/core/include/nm_drivers.h
@@ -19,44 +19,44 @@
 
 #if defined CONFIG_IBVERBS
 #  include <nm_ibverbs_public.h>
-#define CONFIG_IBVERBS_ENABLED 1
+#  define CONFIG_IBVERBS_ENABLED 1
 #else
-#define CONFIG_IBVERBS_ENABLED 0
+#  define CONFIG_IBVERBS_ENABLED 0
 #endif
 
 #if defined CONFIG_MX
 #  include <nm_mx_public.h>
-#define CONFIG_MX_ENABLED 1
+#  define CONFIG_MX_ENABLED 1
 #else
-#define CONFIG_MX_ENABLED 0
+#  define CONFIG_MX_ENABLED 0
 #endif
 
 #if defined CONFIG_GM
 #  include <nm_gm_public.h>
-#define CONFIG_GM_ENABLED 1
+#  define CONFIG_GM_ENABLED 1
 #else
-#define CONFIG_GM_ENABLED 0
+#  define CONFIG_GM_ENABLED 0
 #endif
 
 #if defined CONFIG_QSNET
 #  include <nm_qsnet_public.h>
-#define CONFIG_QSNET_ENABLED 1
+#  define CONFIG_QSNET_ENABLED 1
 #else
-#define CONFIG_QSNET_ENABLED 0
+#  define CONFIG_QSNET_ENABLED 0
 #endif
 
 #if defined CONFIG_SISCI
 #  include <nm_sisci_public.h>
-#define CONFIG_SISCI_ENABLED 1
+#  define CONFIG_SISCI_ENABLED 1
 #else
-#define CONFIG_SISCI_ENABLED 0
+#  define CONFIG_SISCI_ENABLED 0
 #endif
 
 #if defined CONFIG_TCP
 #  include <nm_tcpdg_public.h>
-#define CONFIG_TCP_ENABLED 1
+#  define CONFIG_TCP_ENABLED 1
 #else
-#define CONFIG_TCP_ENABLED 0
+#  define CONFIG_TCP_ENABLED 0
 #endif
 
 #define NR_ENABLED_DRIVERS (CONFIG_IBVERBS_ENABLED+CONFIG_MX_ENABLED+CONFIG_GM_ENABLED+CONFIG_QSNET_ENABLED+CONFIG_SISCI_ENABLED+CONFIG_TCP_ENABLED)

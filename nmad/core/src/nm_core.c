d09826ea4d89e22310de76734cd192856ea0ce02
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-11-13 10:50:20 +0000

    calls common_exit to clear up memory
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@17871 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/core/src/nm_core.c
+++ nmad/core/src/nm_core.c
@@ -896,6 +896,8 @@ nm_core_exit           (struct nm_core		*p_core) {
   tbx_malloc_clean(nm_core_iov1_mem);
   tbx_malloc_clean(nm_core_iov2_mem);
 
+  common_exit(NULL);
+
   return NM_ESUCCESS;
 }
 

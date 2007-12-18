63881e2121fa7ea8942f5eda80064682968dfaf9
cromo <cromo@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2007-12-18 13:38:14 +0000

    Bench de temps de creation d'un arbre de seed
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18006 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/createtime_rec.c
@@ -0,0 +1,105 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2006 "the PM2 team" (pm2-dev@listes.ens-lyon.fr)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU * General Public License for more details.
+ */
+
+/* Programme d'essai pour evaluer le cout de creation de threads */
+
+#include "marcel.h"
+#include <stdio.h>
+#include <sys/time.h>
+
+static tbx_tick_t t1, t2;
+marcel_attr_t attr;
+marcel_cond_t c;        
+marcel_mutex_t mutex;
+unsigned max_threads = 0;
+
+
+any_t f(any_t arg)
+{
+        any_t n;
+        n = arg;
+
+        if(n != 0)
+                {
+                        marcel_create(NULL, &attr, f, (void *) (intptr_t) (n-1));
+                        marcel_create(NULL, &attr, f, (void *) (intptr_t) (n-1));
+                }
+
+        else
+                {
+                        marcel_mutex_lock(&mutex);
+                        max_threads++;
+                        marcel_cond_signal(&c);
+                        marcel_mutex_unlock(&mutex);                      
+                }
+
+        return NULL;
+}
+
+any_t worker(any_t arg) 
+{
+        unsigned nb_feuilles;
+
+        marcel_mutex_init(&mutex, NULL);
+        marcel_cond_init(&c, NULL);
+        
+        marcel_attr_init(&attr);
+        marcel_attr_setdetachstate(&attr, tbx_true);
+        marcel_attr_setseed(&attr, 1);
+        marcel_attr_setprio(&attr, MA_BATCH_PRIO);
+        
+        nb_feuilles = 1 << (long)arg;
+
+        TBX_GET_TICK(t1);
+
+        marcel_create(NULL, &attr, f, (any_t) arg);
+
+        while(max_threads != nb_feuilles)
+                {
+                        marcel_mutex_lock(&mutex);
+                        marcel_cond_wait(&c, &mutex);
+                        marcel_mutex_unlock(&mutex);                         
+                }
+
+        TBX_GET_TICK(t2);
+
+        marcel_printf("seed create =  %fus (%fus/seed)\n", TBX_TIMING_DELAY(t1, t2), TBX_TIMING_DELAY(t1,t2)/nb_feuilles);
+
+        
+        return NULL;
+}                                                                                               
+
+
+int marcel_main(int argc, char *argv[])
+{
+        marcel_t t;
+        unsigned profondeur;
+        
+        marcel_init(&argc, argv);
+        marcel_attr_init(&attr);
+        profondeur = atoi(argv[1]);
+
+        if(argc <= 1) 
+                {
+                        printf("Usage: %s <profondeur>\n", argv[0]);
+                }
+
+        /* on cree un thread main qui va creer un arbre de seed */
+        marcel_create(&t, &attr, worker, (void *) (intptr_t) profondeur);
+        marcel_join(t, NULL);
+
+        marcel_end();
+        return 0;
+}

653093130e03cd4cec4cac513f377ecdfddf3ae1
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-04-10 09:12:35 +0000

    add option -x to ssh when X is not required
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18377 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- leonie/source/leo_loaders.c
+++ leonie/source/leo_loaders.c
@@ -241,6 +241,11 @@ leo_default_loader(p_leo_settings_t settings,
 	  {
 	    // tbx_arguments_append_cstring(args, "-f");
 	    tbx_arguments_append_cstring(args, "-n");
+
+	    if (!(settings->xterm_mode))
+	      {
+		tbx_arguments_append_cstring(args, "-x");
+	      }
 	  }
 
 	tbx_arguments_append_cstring(args, host_name);

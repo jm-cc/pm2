57bcd00297e97321edcaab4aae9b52f844ce1bf7
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-04-16 07:42:42 +0000

    new tests using the schedOpt utility functions
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18415 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sched_opt/sr_hello2.c
@@ -0,0 +1,47 @@
+#include <nm_so_util.h>
+#include <nm_so_sendrecv_interface.h>
+
+const char *msg	= "hello, world!";
+
+int main(int argc, char	**argv) {
+  struct nm_so_interface *sr_if = NULL;
+  nm_so_request s_request, r_request;
+  nm_gate_id_t gate_id;
+
+  uint32_t len = 1 + strlen(msg);
+  int rank;
+  char *buf = NULL;
+
+  nm_so_init(&argc, argv);
+  nm_so_get_rank(&rank);
+  nm_so_get_sr_if(&sr_if);
+
+  buf = malloc(len*sizeof(char));
+  memset(buf, 0, len);
+
+  if (rank == 0) {
+    nm_so_sr_irecv(sr_if, NM_SO_ANY_SRC, 0, buf, len, &r_request);
+    nm_so_sr_rwait(sr_if, r_request);
+
+    nm_so_get_gate_out_id(1, &gate_id);
+    nm_so_sr_isend(sr_if, gate_id, 0, buf, len, &s_request);
+    nm_so_sr_swait(sr_if, s_request);
+  }
+  else if (rank == 1) {
+    strcpy(buf, msg);
+
+    nm_so_get_gate_out_id(0, &gate_id);
+    nm_so_sr_isend(sr_if, gate_id, 0, buf, len, &s_request);
+    nm_so_sr_swait(sr_if, s_request);
+
+    nm_so_get_gate_in_id(0, &gate_id);
+    nm_so_sr_irecv(sr_if, gate_id, 0, buf, len, &r_request);
+    nm_so_sr_rwait(sr_if, r_request);
+
+    printf("buffer contents: <%s>\n", buf);
+  }
+
+  free(buf);
+  nm_so_exit();
+  exit(0);
+}

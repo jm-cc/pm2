d072bdb5226a5f3f0fa83baa3d9e554efa240eea
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-04-16 09:34:29 +0000

    use the padico launcher instead of calling directly the mad3 functions
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18418 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/schedulers/sched_opt/src/nm_so_util.c
+++ nmad/schedulers/sched_opt/src/nm_so_util.c
@@ -1,84 +1,91 @@
-#include <string.h>
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
 
-#ifdef CONFIG_PROTO_MAD3
-#include <pm2_common.h>
-#include <madeleine.h>
-#include <nm_mad3_private.h>
-#endif
-
-#include <tbx.h>
-#include <nm_public.h>
-#include <nm_so_public.h>
-#include <nm_so_debug.h>
-#include <nm_so_sendrecv_interface.h>
-#include <nm_so_pack_interface.h>
+#include <nm_so_util.h>
 #include <Padico/Puk.h>
 
 #ifdef CONFIG_PROTO_MAD3
 
-static p_mad_madeleine_t       madeleine	= NULL;
-static p_mad_session_t         session          = NULL;
+#include <nm_mad3_public.h>
+#include <nm_launcher.h>
+
+static puk_instance_t launcher_instance = NULL;
+static struct puk_receptacle_NewMad_Launcher_s r;
+static nm_gate_id_t *gate_ids;
 
 int nm_so_get_rank(int *rank) {
-  *rank = session->process_rank;
+  *rank = (*r.driver->get_rank)(r._status);
   return NM_ESUCCESS;
 }
 
 int nm_so_get_size(int *size) {
-  *size = tbx_slist_get_length(madeleine->dir->process_slist);
+  *size  = (*r.driver->get_size)(r._status);
   return NM_ESUCCESS;
 }
 
 int nm_so_get_sr_if(struct nm_so_interface **sr_if) {
-  *sr_if = mad_nmad_get_sr_interface();
+  *sr_if  = (*r.driver->get_so_sr_if)(r._status);
   return NM_ESUCCESS;
 }
 
 int nm_so_get_pack_if(nm_so_pack_interface *pack_if) {
-  struct nm_so_interface *sr_if = mad_nmad_get_sr_interface();
-  *pack_if = (nm_so_pack_interface)sr_if;
+  *pack_if = (*r.driver->get_so_pack_if)(r._status);
   return NM_ESUCCESS;
 }
 
 int nm_so_get_gate_out_id(int dest, nm_gate_id_t *gate_id) {
-  p_mad_channel_t channel = tbx_htable_get(madeleine->channel_htable, "pm2");
-  p_mad_connection_t connection = tbx_darray_get(channel->out_connection_darray, dest);
-  p_mad_nmad_connection_specific_t cs = connection->specific;
-  *gate_id = cs->gate_id;
+  *gate_id = gate_ids[dest];
   return NM_ESUCCESS;
 }
 
 int nm_so_get_gate_in_id(int dest, nm_gate_id_t *gate_id) {
-  p_mad_channel_t channel = tbx_htable_get(madeleine->channel_htable, "pm2");
-  p_mad_connection_t connection = tbx_darray_get(channel->in_connection_darray, dest);
-  p_mad_nmad_connection_specific_t cs = connection->specific;
-  *gate_id = cs->gate_id;
+  *gate_id = gate_ids[dest];
   return NM_ESUCCESS;
 }
 
-void nm_so_init(int *argc, char	**argv) {
-  struct nm_core         *p_core     = NULL;
+int nm_so_init(int *argc, char	**argv) {
+  /*
+   * Lazy Puk initialization (it may already have been initialized in PadicoTM)
+   */
+  if(!padico_puk_initialized()) {
+    padico_puk_init(*argc, &*argv);
+  }
+
+  nm_mad3_launcher_init();
 
   /*
-   * Initialization of various libraries.
-   * Reference to the Madeleine object.
+   * NewMad initialization is performed by component 'NewMad_Launcher'
+   * (from proto_mad3 or from PadicoTM)
    */
-  madeleine    = mad_init(argc, argv);
+  puk_adapter_t launcher = puk_adapter_resolve("NewMad_Launcher");
+  launcher_instance = puk_adapter_instanciate(launcher);
+  puk_instance_indirect_NewMad_Launcher(launcher_instance, NULL, &r);
+  (*r.driver->init)(r._status, argc, &*argv, "NewMadeleine", &nm_so_load);
 
-  /*
-   * Reference to the session information object
-   */
-  session      = madeleine->session;
+  {
+    int size = (*r.driver->get_size)(r._status);
+    gate_ids = malloc(size * sizeof(nm_gate_id_t));
+    (*r.driver->get_gate_ids)(r._status, &gate_ids[0]);
+  }
 
-  /*
-   * Reference to the NewMadeleine core object
-   */
-  p_core = mad_nmad_get_core();
-
+  return NM_ESUCCESS;
 }
 
 int nm_so_exit(void) {
-  mad_exit(madeleine);
+  free(gate_ids);
+  puk_instance_destroy(launcher_instance);
   return NM_ESUCCESS;
 }
 

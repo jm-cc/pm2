8128b563c7402c221db7210cca2627fa8dd071d0
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-06-11 14:52:12 +0000

    Marcel: Augment `barrier-native' test to use thread seeds.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18699 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/barrier-native.c
+++ marcel/examples/barrier-native.c
@@ -14,7 +14,8 @@
  */
 
 
-/* Test Marcel's native barrier API with all supported barrier modes.  */
+/* Test Marcel's native barrier API with all supported barrier modes and both
+   with and without thread seeds.  */
 
 #include <marcel.h>
 
@@ -42,76 +43,92 @@ main (int argc, char *argv[])
       MA_BARRIER_SLEEP_MODE, MA_BARRIER_YIELD_MODE
     };
 
-  static const marcel_attr_t tattr = MARCEL_ATTR_INITIALIZER;
-
-  unsigned mode, thread;
+  unsigned mode, thread, yesno;
 
   marcel_init (&argc, argv);
 
-  for (mode = 0;
-       mode < sizeof (barrier_modes) / sizeof (barrier_modes[1]);
-       mode++)
+  for (yesno = 0; yesno < 2; yesno++)
     {
-      int err;
-      marcel_t threads[THREADS];
-      marcel_barrier_t barrier;
-      marcel_barrierattr_t battr;
-      unsigned seen_barrier_serial_thread = 0;
-
-      marcel_barrierattr_init (&battr);
-      marcel_barrierattr_setmode (&battr, barrier_modes[mode]);
-
-      marcel_barrier_init (&barrier, &battr, THREADS);
-
-      for (thread = 0; thread < THREADS; thread++)
+      for (mode = 0;
+	   mode < sizeof (barrier_modes) / sizeof (barrier_modes[1]);
+	   mode++)
 	{
-	  err = marcel_create (&threads[thread], &tattr,
-			       start_thread, &barrier);
-	  if (err != 0)
+	  int err;
+	  marcel_t threads[THREADS];
+	  marcel_attr_t tattr;
+	  marcel_barrier_t barrier;
+	  marcel_barrierattr_t battr;
+	  unsigned seen_barrier_serial_thread = 0;
+
+	  marcel_barrierattr_init (&battr);
+	  marcel_barrierattr_setmode (&battr, barrier_modes[mode]);
+
+	  marcel_barrier_init (&barrier, &battr, THREADS);
+
+	  marcel_attr_init (&tattr);
+	  marcel_attr_setseed (&tattr, yesno);
+
+	  /* Use the default priority, which is the same as that of the main
+	     thread.  */
+	  marcel_attr_setprio (&tattr, MA_DEF_PRIO);
+
+	  for (thread = 0; thread < THREADS; thread++)
 	    {
-	      printf ("failed to create thread %u: error %i\n",
-		      thread, err);
+	      err = marcel_create (&threads[thread], &tattr,
+				   start_thread, &barrier);
+	      if (err != 0)
+		{
+		  printf ("failed to create thread %u: error %i\n",
+			  thread, err);
+		  return 1;
+		}
+	    }
+
+	  for (thread = 0; thread < THREADS; thread++)
+	    {
+	      void *ret;
+
+	      err = marcel_join (threads[thread], &ret);
+	      if (err != 0)
+		{
+		  printf ("failed to join thread %u: error %i\n",
+			  thread, err);
+		  return 1;
+		}
+
+	      if (ret == (void *) MARCEL_BARRIER_SERIAL_THREAD)
+		seen_barrier_serial_thread++;
+	    }
+
+	  if (seen_barrier_serial_thread != 1)
+	    {
+	      printf ("didn't get `MARCEL_BARRIER_SERIAL_THREAD' "
+		      "once as expected: %u\n",
+		      seen_barrier_serial_thread);
 	      return 1;
 	    }
-	}
 
-      for (thread = 0; thread < THREADS; thread++)
-	{
-	  void *ret;
-
-	  err = marcel_join (threads[thread], &ret);
+	  err = marcel_attr_destroy (&tattr);
 	  if (err != 0)
 	    {
-	      printf ("failed to join thread %u: error %i\n",
-		      thread, err);
+	      printf ("failed to destroy thread attributes\n");
 	      return 1;
 	    }
 
-	  if (ret == (void *) MARCEL_BARRIER_SERIAL_THREAD)
-	    seen_barrier_serial_thread++;
-	}
+	  err = marcel_barrier_destroy (&barrier);
+	  if (err != 0)
+	    {
+	      printf ("failed to destroy barrier with mode %i\n",
+		      (int) barrier_modes[mode]);
+	      return 1;
+	    }
 
-      if (seen_barrier_serial_thread != 1)
-	{
-	  printf ("didn't get `MARCEL_BARRIER_SERIAL_THREAD' "
-		  "once as expected: %u\n",
-		  seen_barrier_serial_thread);
-	  return 1;
-	}
-
-      err = marcel_barrier_destroy (&barrier);
-      if (err != 0)
-	{
-	  printf ("failed to destroy barrier with mode %i\n",
-		  (int) barrier_modes[mode]);
-	  return 1;
-	}
-
-      err = marcel_barrierattr_destroy (&battr);
-      if (err != 0)
-	{
-	  printf ("failed to destroy barrier attribute\n");
-	  return 1;
+	  err = marcel_barrierattr_destroy (&battr);
+	  if (err != 0)
+	    {
+	      printf ("failed to destroy barrier attribute\n");
+	      return 1;
+	    }
 	}
     }
 

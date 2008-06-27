4bca95790e23dae915db7a206225aaba5d115333
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-06-27 15:27:10 +0000

    Marcel: Add TLS test.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18878 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/tls.c
@@ -0,0 +1,94 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/* Test thread-local storage (TLS) support for Marcel threads.  */
+
+#include <marcel.h>
+
+#ifndef MA__PROVIDE_TLS
+# error "TLS support is disabled."
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/* Number of threads.  */
+#define THREADS 17
+
+/* Thread-local storage.  */
+static __thread struct { int i; char buf[64]; } tls_data;
+
+
+static void *
+thread_main (void *arg)
+{
+  /* Initialize our TLS.  */
+  tls_data.i = (int) arg;
+
+  /* Just leave enough time for others to override TLS_DATA (provided TLS is
+     broken).  */
+  sleep (random () % 3);
+
+  /* Return the last value seen.  */
+  return (void *) tls_data.i;
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  int err;
+  unsigned i;
+  marcel_t threads[THREADS];
+
+  marcel_init (&argc, argv);
+
+  tls_data.i = -1;
+
+  for (i = 0; i < THREADS; i++)
+    {
+      err = marcel_create (&threads[i], NULL,
+			   thread_main, (void *) i);
+      if (err)
+	return 1;
+    }
+
+  for (i = 0; i < THREADS; i++)
+    {
+      void *ret;
+
+      err = marcel_join (threads[i], &ret);
+      if (err)
+	return 2;
+
+      if (i != (int) ret)
+	{
+	  printf ("got %i instead of %i\n", (int) ret, i);
+	  return 3;
+	}
+    }
+
+  if (tls_data.i != -1)
+    {
+      printf ("main thread got %i instead of -1\n", tls_data.i);
+      return 4;
+    }
+
+  marcel_end ();
+
+  return 0;
+}

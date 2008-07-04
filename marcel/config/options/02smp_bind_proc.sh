15fc94eed9eb35bc368133e7b88f5033b1ce9c7a
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-04 12:11:02 +0000

    - remove the smp_bind_proc option (always on in multipro mode now)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@18948 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/config/options/02smp_bind_proc.sh
+++
@@ -1,2 +0,0 @@
-PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -DMARCEL_BIND_LWP_ON_PROCESSORS"
-grep -q -e '^extern int sched_setaffinity ([^,]*,[^,]*)' /usr/include/sched.h && PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -DHAVE_OLD_SCHED_SETAFFINITY"

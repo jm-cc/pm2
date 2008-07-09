de96e18f8f6e879e401e7ea89d8df869a33c8453
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-09 13:58:33 +0000

    first version of the thread_memory_xxx functionalities as well as a test-case
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19049 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/nouma/simple/thread_memory.c
+++ marcel/examples/nouma/simple/thread_memory.c
@@ -1,22 +1,142 @@
 #include "marcel.h"
+#include "marcel_topology.h"
+
+#include <errno.h>
+#include <numaif.h>
+
+#define PAGES 4
+
+void print_pagenodes(int id, void **pageaddrs, int nbpages);
 
 typedef struct thread_memory_s {
   void *address;
   size_t size;
-  int node;
-  marcel_t *thread;
+  marcel_t thread;
+  int thread_id;
+
+  int *nodes;
+  unsigned long pagesize;
+  void **pageaddrs;
+  int nbpages;
 } thread_memory_t;
 
-int thread_memory_attach(void *address, size_t size, int node, thread_memory_t* thread_memory) {
+int thread_memory_attach(void *address, size_t size, thread_memory_t* thread_memory) {
+  int i;
+
+  thread_memory->address = address;
+  thread_memory->size = size;
+  thread_memory->thread = marcel_self();
+  thread_memory->thread_id = thread_memory->thread->id;
+
+  // Count the number of pages
+  thread_memory->pagesize = getpagesize();
+  thread_memory->nbpages = size / thread_memory->pagesize;
+  if (thread_memory->nbpages * thread_memory->pagesize != size) thread_memory->nbpages++;
+
+  // Set the page addresses
+  thread_memory->pageaddrs = malloc(thread_memory->nbpages * sizeof(void *));
+  for(i=0; i<thread_memory->nbpages ; i++)
+    thread_memory->pageaddrs[i] = address + i*thread_memory->pagesize;
+
+  // fill in the nodes
+  print_pagenodes(thread_memory->thread_id,
+                  thread_memory->pageaddrs, thread_memory->nbpages);
+  return 0;
 }
 
 int thread_memory_move(thread_memory_t* thread_memory, int node) {
+  int i, err;
+  int *status, *nodes;
+
+  status = malloc(thread_memory->nbpages * sizeof(int));
+  nodes = malloc(thread_memory->nbpages * sizeof(int));
+  for(i=0; i<thread_memory->nbpages ; i++) nodes[i] = node;
+
+  err = move_pages(0, thread_memory->nbpages, thread_memory->pageaddrs,
+                   nodes, status, MPOL_MF_MOVE);
+  if (err < 0) {
+    if (errno == ENOENT) {
+      printf("[%d] warning. cannot move pages which have not been allocated\n",
+             thread_memory->thread_id);
+    }
+    else {
+      perror("move_pages");
+      exit(-1);
+    }
+  }
+
+  for(i=0; i<PAGES; i++) {
+    if (status[i] == -ENOENT)
+      printf("[%d] page #%d is not allocated\n", thread_memory->thread_id, i);
+    else
+      printf("[%d] page #%d is on node #%d\n", thread_memory->thread_id, i, status[i]);
+  }
+
 }
 
 int thread_memory_detach(thread_memory_t* thread_memory) {
 }
 
-int main(int argc, char * argv[]) {
+void print_pagenodes(int id, void **pageaddrs, int nbpages) {
+  int status[nbpages];
+  int i;
+  int err;
+
+  err = move_pages(0, nbpages, pageaddrs, NULL, status, 0);
+  if (err < 0) {
+    perror("move_pages");
+    exit(-1);
+  }
+
+  for(i=0; i<nbpages; i++) {
+    if (status[i] == -ENOENT)
+      marcel_printf("[%d]  page #%d is not allocated\n", id, i);
+    else
+      marcel_printf("[%d]  page #%d is on node #%d\n", id, i, status[i]);
+  }
+}
+
+any_t memory(any_t arg) {
+  char *buffer;
+  int nodes[PAGES];
+  thread_memory_t thread_memory;
+
+  marcel_fprintf(stderr,"[%d] launched on VP #%u\n", marcel_self()->id,
+                 marcel_current_vp());
+
+  buffer = malloc(PAGES * getpagesize() * sizeof(char));
+  memset(buffer, 0, PAGES * getpagesize() * sizeof(char));
+
+  marcel_fprintf(stderr,"[%d] allocating buffer %p of size %u with pagesize %u\n",
+                 marcel_self()->id, buffer, PAGES * getpagesize() * sizeof(char),
+                 getpagesize());
+
+  thread_memory_attach(buffer, PAGES * getpagesize() * sizeof(char),
+                       &thread_memory);
+  thread_memory_move(&thread_memory, 0);
+}
+
+int marcel_main(int argc, char * argv[]) {
+  marcel_t threads[2];
+  marcel_attr_t attr;
+
   marcel_init(&argc,argv);
+
+  marcel_attr_init(&attr);
+  // Start the 1st thread on the first VP 
+  //  marcel_print_level(&marcel_topo_vp_level[0], stderr, 1, 1, " ", "#", ":", "");
+  marcel_attr_setid(&attr, 0);
+  marcel_attr_settopo_level(&attr, &marcel_topo_vp_level[0]);
+  marcel_create(&threads[0], &attr, memory, (any_t) (intptr_t) 0);
+
+  // Start the 2nd thread on the last VP 
+  //  marcel_print_level(&marcel_topo_vp_level[marcel_nbvps()-1], stderr, 1, 1, " ", "#", ":", "");
+  marcel_attr_setid(&attr, 1);
+  marcel_attr_settopo_level(&attr, &marcel_topo_vp_level[marcel_nbvps()-1]);
+  marcel_create(&threads[1], &attr, memory, (any_t) (intptr_t) 1);
+
+  // Wait for the threads to complete
+  marcel_join(threads[0], NULL);
+  marcel_join(threads[1], NULL);
 }
 

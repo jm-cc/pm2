4cafe4a0a9d4742662724aade066232a9fbe94fc
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-09 07:58:10 +0000

    adapt code to new numa interface
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19043 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/core/src/nm_core.c
+++ nmad/core/src/nm_core.c
@@ -597,10 +597,10 @@ nm_core_driver_load_init_some_with_params(struct nm_core *p_core,
 
 #ifdef PM2_NUIOA
 	if (nuioa && preferred_node != PM2_NUIOA_ANY_NODE && preferred_node != PM2_NUIOA_CONFLICTING_NODES) {
-		nodemask_t mask;
-		nodemask_zero(&mask);
+		struct bitmask mask;
+		numa_bitmask_clearall(&mask);
 		DISP("binding to nuioa node %d", preferred_node);
-		nodemask_set(&mask, preferred_node);
+		numa_bitmask_setbit(&mask, preferred_node);
 		numa_bind(&mask);
 	}
 #endif /* PM2_NUIOA */

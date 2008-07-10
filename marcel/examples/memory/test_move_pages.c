087a602b29897dbc32e8158e92dd2bee4a3c1ab2
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-10 08:00:19 +0000

    add pm2 copyright
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19056 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/test_move_pages.c
+++ marcel/examples/memory/test_move_pages.c
@@ -1,3 +1,18 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include "marcel.h"
 
 #include <sys/mman.h>

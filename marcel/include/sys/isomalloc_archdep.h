340ef314e933034607dc569d11cc935af20cf582
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-17 15:40:11 +0000

    Marcel: Increase the default value of `THREAD_SLOT_SIZE'.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19101 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/sys/isomalloc_archdep.h
+++ marcel/include/sys/isomalloc_archdep.h
@@ -41,7 +41,7 @@
       #ifdef MA__LIBPTHREAD
         #define ASM_THREAD_SLOT_SIZE          (0x100000) /* 1 MB */
       #else
-        #define ASM_THREAD_SLOT_SIZE          (0x10000) /* 64 KB */
+        #define ASM_THREAD_SLOT_SIZE          (0x100000) /* 1 MB */
       #endif
     #endif
   #endif

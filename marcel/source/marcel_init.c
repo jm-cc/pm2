8f522376be8daeb30563f0e1de76d74a2812f96e
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-07-17 10:54:59 +0000

    grrr non-breakable space
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19093 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_init.c
+++ marcel/source/marcel_init.c
@@ -535,7 +535,7 @@ int main(int argc, char *argv[])
 			fprintf(stderr,"The current stack resource limit is too small (%ld) for the chosen marcel stack size (%ld).  Please increase it (ulimit -s, and maximum is %ld) or decrease THREAD_SLOT_SIZE in marcel/include/sys/isomalloc_archdep.h\n", (long)rlim.rlim_cur, (long)THREAD_SLOT_SIZE, (long) rlim.rlim_max);
 			abort();
 		}
-		if (THREAD_SLOT_SIZE < sizeof(marcel_t)) {
+		if (THREAD_SLOT_SIZE < sizeof(marcel_t)) {
 			fprintf(stderr,"THREAD_SLOT_SIZE is too small (%ld) for marcel_t (%ld) to fit in.  Please increase it in marcel/include/sys/isomalloc_archdep.h\n", THREAD_SLOT_SIZE, sizeof(marcel_t));
 			abort();
 		}

3be4b5214571a53113945c157d314854588f1328
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-08-22 12:45:50 +0000

    doxygen
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19201 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/marcel_memory.h
+++ marcel/include/marcel_memory.h
@@ -26,52 +26,52 @@
 
 /** Type of a tree node */
 typedef struct marcel_memory_data_s {
-  /** Start address of the memory area */
+  /** \brief Start address of the memory area */
   void *startaddress;
-  /** End address of the memory area */
+  /** \brief End address of the memory area */
   void *endaddress;
-  /** Size of the memory area */
+  /** \brief Size of the memory area */
   size_t size;
 
-  /** Page addresses of the memory area */
+  /** \brief Page addresses of the memory area */
   void **pageaddrs;
-  /** Number of pages holding the memory area */
+  /** \brief Number of pages holding the memory area */
   int nbpages;
-  /** Nodes where the memory area is located */
+  /** \brief Nodes where the memory area is located */
   int *nodes;
 } marcel_memory_data_t;
 
 /** Sorted-binary tree of allocated memory areas */
 typedef struct marcel_memory_tree_s {
-  /** Left child of the tree */
+  /** \brief Left child of the tree */
   struct marcel_memory_tree_s *leftchild;
-  /** Right child of the tree */
+  /** \brief Right child of the tree */
   struct marcel_memory_tree_s *rightchild;
-  /** Node of the tree */
+  /** \brief Node of the tree */
   marcel_memory_data_t *data;
 } marcel_memory_tree_t;
 
 /** Represent a pre-allocated space (start address + number of pages) */
 typedef struct marcel_memory_space_s {
-  /** Start address of the memory space */
+  /** \brief Start address of the memory space */
   void *start;
-  /** Number of pages of the memory space */
+  /** \brief Number of pages of the memory space */
   int nbpages;
-  /** Next pre-allocated space */
+  /** \brief Next pre-allocated space */
   struct marcel_memory_space_s *next;
 } marcel_memory_space_t;
 
 /** Memory manager */
 typedef struct marcel_memory_manager_s {
-  /** Tree containing all the allocated memory areas */
+  /** \brief Tree containing all the allocated memory areas */
   marcel_memory_tree_t *root;
-  /** List of pre-allocated memory areas */
+  /** \brief List of pre-allocated memory areas */
   marcel_memory_space_t **heaps;
-  /** Lock to manipulate the data */
+  /** \brief Lock to manipulate the data */
   marcel_spinlock_t lock;
-  /** System page size */
+  /** \brief System page size */
   int pagesize;
-  /** Number of initially pre-allocated pages */
+  /** \brief Number of initially pre-allocated pages */
   int initialpreallocatedpages;
 } marcel_memory_manager_t;
 

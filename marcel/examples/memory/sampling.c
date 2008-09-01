b5f56ca379df224238151ef46a1e49bbbbe8a639
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-09-01 13:16:07 +0000

    application to sample the cost of memory migration
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19244 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/memory/sampling.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <numaif.h>
+#include <sys/mman.h>
+#include <asm/unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define LOOPS 1000
+
+static void check_location(void **pageaddrs, int pages, int node) {
+  int *pagenodes;
+  int i;
+  int err;
+
+  pagenodes = malloc(pages * sizeof(int));
+  err = move_pages(0, pages, pageaddrs, NULL, pagenodes, 0);
+  if (err < 0) {
+    perror("move_pages (check_location)");
+    exit(-1);
+  }
+
+  for(i=0; i<pages; i++) {
+    if (pagenodes[i] != node) {
+      printf("  page #%d is not located on node #%d\n", node);
+      exit(-1);
+    }
+  }
+  free(pagenodes);
+}
+
+static void migrate(void **pageaddrs, int pages, int *nodes, int *status) {
+  int err;
+
+  //printf("binding on numa node #%d\n", nodes[0]);
+
+  err = move_pages(0, pages, pageaddrs, nodes, status, MPOL_MF_MOVE);
+
+  if (err < 0) {
+    perror("move_pages (set_bind)");
+    exit(-1);
+  }
+}
+
+static void sampling(unsigned long source, unsigned long dest, int pages, unsigned long maxnode, unsigned long pagesize) {
+  void *buffer;
+  void **pageaddrs;
+  unsigned long nodemask;
+  int i, err;
+  int *sources, *dests, *status;
+  struct timeval tv1, tv2;
+  unsigned long us, ns;
+
+  // Allocate the pages
+  buffer = mmap(NULL, pages * pagesize, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+  if (err < 0) {
+    perror("mmap");
+    exit(-1);
+  }
+
+  // Set the page addresses
+  pageaddrs = malloc(pages * sizeof(void *));
+  for(i=0; i<pages; i++)
+    pageaddrs[i] = buffer + i*pagesize;
+
+  // Set the memory policy on node source
+  nodemask = (1<<source);
+  err = set_mempolicy(MPOL_BIND, &nodemask, maxnode+1);
+  if (err < 0) {
+    perror("set_mempolicy");
+    exit(-1);
+  }
+
+  // Fill in the whole memory
+  memset(buffer, 0, pages*pagesize);
+
+  // Check the location of the pages
+  check_location(pageaddrs, pages, source);
+
+  // Move all the pages on node dest
+  status = malloc(pages * sizeof(int));
+  sources = malloc(pages * sizeof(int));
+  dests = malloc(pages * sizeof(int));
+  for(i=0; i<pages ; i++) dests[i] = dest;
+  for(i=0; i<pages ; i++) sources[i] = source;
+
+  gettimeofday(&tv1, NULL);
+  for(i=0 ; i<LOOPS ; i++) {
+    migrate(pageaddrs, pages, dests, status);
+    migrate(pageaddrs, pages, sources, status);
+  }
+  migrate(pageaddrs, pages, dests, status);
+  gettimeofday(&tv2, NULL);
+
+  // Check the location of the pages
+  for(i=0; i<pages; i++) {
+    if (status[i] != dest) {
+      printf("  page #%d has not been moved on node #%d\n", dest);
+      exit(-1);
+    }
+  }
+
+  free(pageaddrs);
+  free(status);
+  free(sources);
+  free(dests);
+
+  us = (tv2.tv_sec - tv1.tv_sec) * 1000000 + (tv2.tv_usec - tv1.tv_usec);
+  ns = us * 1000;
+  ns /= LOOPS;
+  printf("%d\t%d\t%d\t%ld\n", source, dest, pages, ns);
+}
+
+int main(int argc, char **argv) {
+  unsigned long pagesize;
+  unsigned long maxnode;
+  unsigned long source, dest;
+
+  pagesize = getpagesize();
+  maxnode = numa_max_node()+1;
+
+  printf("Source\tDest\tNb_pages\tMigration_Time\n");
+
+  for(source=0; source<maxnode ; source++) {
+    for(dest=0; dest<maxnode ; dest++) {
+
+      if (source == dest) continue;
+
+      sampling(source, dest, 10, maxnode, pagesize);
+      sampling(source, dest, 50, maxnode, pagesize);
+      sampling(source, dest, 100, maxnode, pagesize);
+      sampling(source, dest, 500, maxnode, pagesize);
+      sampling(source, dest, 1000, maxnode, pagesize);
+      sampling(source, dest, 5000, maxnode, pagesize);
+
+    }
+  }
+}

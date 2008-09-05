jm-cc/pm2

commit d1cfb33483095ac33241a0dd454228b70adc8c91

Add the marcel_vpset_isincluded (super_set, sub_set) that checks whether sub_set is included in super_set.
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19329 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/include/marcel_topology.h
@@ -278,6 +278,21 @@ static __tbx_inline__ int marcel_vpset_isset(const marcel_vpset_t * set,
 #endif
 }
 
+#section functions
+/** \brief Test whether set \e sub_set is part of set \e super_set */
+static __tbx_inline__ int marcel_vpset_isincluded (const marcel_vpset_t *super_set,
+						   const marcel_vpset_t *sub_set);
+#section inline
+static __tbx_inline__ int marcel_vpset_isincluded (const marcel_vpset_t *super_set,
+						   const marcel_vpset_t *sub_set);
+{
+#ifdef MA__LWPS
+        return (*super_set == (*super_set | *sub_set));
+#else
+	return *super_set;
+#endif
+}
+
 #section marcel_functions
 /** \brief Compute the number of VPs in VP mask */
 static __tbx_inline__ int marcel_vpset_weight(const marcel_vpset_t * vpset);

commit 59403e13ab969cca0807e05657e2fdb22c8571cf

Fix compilation problem introduced in r19329.
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19330 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/include/marcel_topology.h
@@ -284,7 +284,7 @@ static __tbx_inline__ int marcel_vpset_isincluded (const marcel_vpset_t *super_s
 						   const marcel_vpset_t *sub_set);
 #section inline
 static __tbx_inline__ int marcel_vpset_isincluded (const marcel_vpset_t *super_set,
-						   const marcel_vpset_t *sub_set);
+						   const marcel_vpset_t *sub_set)
 {
 #ifdef MA__LWPS
         return (*super_set == (*super_set | *sub_set));
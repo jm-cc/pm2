88d8fef472f955f2adb903083d0c58fab20867e9
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-09-08 08:37:19 +0000

    by default, analyze the sampling for the current machine
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19343 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/sampling/analyzeSampling.pl
+++ marcel/sampling/analyzeSampling.pl
@@ -60,7 +60,8 @@ sub help {
     print "            [-plot] [-nodumb]\n";
     print "  Performs a linear regression for the cost of the memory migration on hostname\n";
     print "  with:\n";
-    print "      -file        force the filename for the sampling results (by default get the result file of the machine hostname from the PM2 directory)\n";
+    print "      <hostname>   specifies the name of the machine for the sampling results (by default, uses the result of the command 'uname -m')\n";
+    print "      -file        specifies the filename for the sampling results (by default get the result file of the specified machine from the PM2 directory)\n";
     print "      -plot        displays the plot for the sampling result (by default does not display the plot)\n";
     print "      -nodumb      displays the plot in graphical mode (by default in dumb mode)\n";
     exit;
@@ -144,6 +145,10 @@ if ($filename eq "") {
             $pathname = $ENV{HOME};
         }
         $pathname .= "/.pm2/marcel";
+    }
+    if ($hostname eq "") {
+        use Sys::Hostname;
+        $hostname = hostname();
     }
     $filename = "$pathname/sampling_$hostname.txt";
 }

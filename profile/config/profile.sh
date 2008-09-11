c67b59de241ee28354c5bf4d9ea29aeecbfa0a30
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-09-11 12:55:46 +0000

    profile: Fix broken syntax in `config/profile.sh'.
    ... or "learn to use the shell".
    
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19404 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/config/profile.sh
+++ profile/config/profile.sh
@@ -6,6 +6,7 @@ then
     PM2_PROFILE_CFLAGS="$PM2_PROFILE_CFLAGS -I$FXT_PREFIX/include"
     PM2_PROFILE_LIBS="$PM2_PROFILE_LIBS -L$FXT_PREFIX/lib"
 elif pkg-config libming
+then
     PM2_PROFILE_CFLAGS="$PM2_PROFILE_CFLAGS $(pkg-config libming --cflags)"
     PM2_PROFILE_LIBS="$PM2_PROFILE_LIBS $(pkg-config libming --libs-only-L)"
 fi

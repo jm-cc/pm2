17b806e5a9fb871037277b7226599f3675771e88
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-09-17 12:55:06 +0000

    run sampling for memory access
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19476 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/sampling/runSamplingForMemoryAccess.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+flavor="sampling_mami"
+x=$(pm2-flavor get --flavor=$flavor 2>/dev/null)
+if [ -z "$x" ] ; then
+    # create the flavor
+    eval pm2-flavor set --flavor=\"$flavor\" \
+        --ext=\"\" \
+        --modules=\"init marcel tbx\" \
+        --common=\"fortran_target_none\" \
+        --marcel=\"numa marcel_main bubble_sched_null smp_smt_idle enable_mami enable_stats\" \
+        --all=\"build_static opt\"
+fi
+
+make -C $PM2_ROOT/marcel FLAVOR=$flavor -j 
+make -C $PM2_ROOT/marcel/examples/memory FLAVOR=$flavor sampling_for_memory_access
+prog=$(pm2-which -f $flavor sampling_for_memory_access)
+
+$prog

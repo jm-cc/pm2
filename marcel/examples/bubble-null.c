7b634f0b412cb617e7d9fc9fea958705b73f449a
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-09-24 07:19:06 +0000

    Marcel: Add test case for the `null' bubble scheduler.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19526 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/bubble-null.c
@@ -0,0 +1,68 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include "bubble-testing.h"
+
+
+int
+main (int argc, char *argv[])
+{
+  /* A simple topology: 2 nodes, each of which has 2 CPUs, each of which has 4
+     cores.  */
+  static const char topology_description[] = "2 2 4";
+
+	/* A bubble hierarchy.  */
+  static const unsigned bubble_hierarchy_description[] =
+    { 8, 2, 0 };
+
+
+  /* The expected result, i.e., the scheduling entity distribution that we
+     expect from the `null' scheduler: the root bubble is on the top level
+     and others are left unassigned.  */
+
+#define BUBBLE MA_BUBBLE_ENTITY
+#define THREAD MA_THREAD_ENTITY
+
+  static const tree_t result_core =
+    { .children_count = 0,
+      .entity_count = 0 };
+
+  static const tree_t result_cpu =
+    { .children_count = 4,
+      .children = { &result_core, &result_core,
+                    &result_core, &result_core },
+      .entity_count = 0 };
+
+  static const tree_t result_node =
+    { .children_count =  2,
+      .children = { &result_cpu, &result_cpu },
+      .entity_count = 0 };
+
+  static const tree_t result_root = /* root of the expected result */
+    { .children_count = 2,
+      .children = { &result_node, &result_node },
+      .entity_count = 1,
+      .entities = { BUBBLE } };
+
+#undef THREAD
+#undef BUBBLE
+
+	return test_marcel_bubble_scheduler (argc, argv,
+																			 &marcel_bubble_null_sched,
+																			 topology_description,
+																			 bubble_hierarchy_description,
+																			 &result_root);
+}

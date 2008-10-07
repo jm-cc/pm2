5067560ec89550364cc0adfed023445f0dee10a1
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-10-07 14:41:14 +0000

    MAMI, check_migration: define marcel_main instead of main
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19650 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/check_migration.c
+++ marcel/examples/memory/check_migration.c
@@ -22,7 +22,7 @@
 #include <stdlib.h>
 #include <numaif.h>
 
-int main(int argc, char **argv) {
+int marcel_main(int argc, char **argv) {
   int nbpages=4;
   void *buffer;
   void *pageaddrs[nbpages];

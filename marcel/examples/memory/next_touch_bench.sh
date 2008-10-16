5756974e08648db45bb742e63995d7f51817a10c
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-10-16 14:10:03 +0000

    MAMI: Call the bench several times
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19812 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/next_touch_bench.sh
+++ marcel/examples/memory/next_touch_bench.sh
@@ -1,18 +1,26 @@
 rm -f next_touch_bench.dat
 for p in 1 10 50 ; do
-    pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    for i in $(seq 1 1 10) ; do
+        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    done
 done
 
 for p in $(seq 100 100 1000) ; do
-    pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    for i in $(seq 1 1 10) ; do
+        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    done
 done
 
-for p in $(seq 1000 1000 5000) ; do
-    pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+for p in $(seq 2000 1000 5000) ; do
+    for i in $(seq 1 1 10) ; do
+        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    done
 done
 
 (
 cat <<EOF 
+set logscale x 2
+set logscale y 2
 plot "next_touch_bench.dat" using 3:4 with linespoints
 pause -1
 EOF

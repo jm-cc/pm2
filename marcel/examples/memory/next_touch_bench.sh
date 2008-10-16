de1df13979e5617d93bdcb5cc86e3df1130c3a71
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-10-16 14:10:05 +0000

    MAMI: Improve script
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@19813 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/next_touch_bench.sh
+++ marcel/examples/memory/next_touch_bench.sh
@@ -1,28 +1,32 @@
-rm -f next_touch_bench.dat
-for p in 1 10 50 ; do
+bench() {
+    pages=$1
     for i in $(seq 1 1 10) ; do
-        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+        pm2-load next_touch_bench -src 0 -dest 1 -pages $pages >> /tmp/next_touch_bench_$pages.dat
     done
-done
+    mean=$(awk 'BEGIN {sum=0} {sum+=$4} END {print sum/10}' /tmp/next_touch_bench_$pages.dat)
+    echo "0	1	$pages	$mean"
+}
 
-for p in $(seq 100 100 1000) ; do
-    for i in $(seq 1 1 10) ; do
-        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+rm -f /tmp/next_touch_bench.dat
+(
+    bench 1
+    for p in $(seq 10 10 90) ; do
+        bench $p
     done
-done
-
-for p in $(seq 2000 1000 5000) ; do
-    for i in $(seq 1 1 10) ; do
-        pm2-load next_touch_bench -src 0 -dest 1 -pages $p >> next_touch_bench.dat
+    for p in $(seq 100 100 1000) ; do
+        bench $p
     done
-done
+    for p in $(seq 2000 1000 5000) ; do
+        bench $p
+    done
+) > /tmp/next_touch_bench.dat
 
 (
 cat <<EOF 
 set logscale x 2
 set logscale y 2
-plot "next_touch_bench.dat" using 3:4 with linespoints
+plot "/tmp/next_touch_bench.dat" using 3:4 with linespoints
 pause -1
 EOF
-) > next_touch_bench.gnp
-gnuplot next_touch_bench.gnp
+) > /tmp/next_touch_bench.gnp
+gnuplot /tmp/next_touch_bench.gnp

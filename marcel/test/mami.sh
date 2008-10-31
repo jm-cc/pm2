6ee0ae290ec3b38cb02e562d838ff1eb09a70acc
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-10-31 08:42:33 +0000

    MAMI: Common file for all mami tests
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20004 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/test/mami.sh
@@ -0,0 +1,29 @@
+#-*-sh-*-
+# PM2: Parallel Multithreaded Machine
+# Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# This file is meant to be `source'd by the various
+# `test_mami*.level*' files.
+
+flavor="test_marcel_mami"
+appdir="${PM2_ROOT}/marcel/examples/memory"
+check_all_lines=1
+
+create_test_flavor() {
+# Creation de la flavor
+    eval ${PM2_ROOT}/bin/pm2-flavor set --flavor=\"$flavor\" \
+	--ext=\"\" \
+	--modules=\"marcel tbx init\" \
+	--marcel=\"numa marcel_main bubble_sched_null smp_smt_idle enable_mami enable_numa_memory enable_stats\" \
+	--all=\"opt\" --all=\"gdb\" --all=\"build_static\" $_output_redirect
+}

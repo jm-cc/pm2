4e6f44220bdb0f0c2d3b5e859cb08e61f44e0134
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-10-31 10:33:34 +0000

    MAMI: needs option enable_numa_memory
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20011 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/sampling/runSamplingForMemoryMigration.sh
+++ marcel/sampling/runSamplingForMemoryMigration.sh
@@ -8,11 +8,11 @@ if [ -z "$x" ] ; then
         --ext=\"\" \
         --modules=\"init marcel tbx\" \
         --common=\"fortran_target_none\" \
-        --marcel=\"numa marcel_main bubble_sched_null smp_smt_idle enable_mami enable_stats\" \
+        --marcel=\"numa marcel_main bubble_sched_null smp_smt_idle enable_mami enable_numa_memory enable_stats\" \
         --all=\"build_static opt\"
 fi
 
-make -C $PM2_ROOT/marcel FLAVOR=$flavor -j 
+make -C $PM2_ROOT/marcel FLAVOR=$flavor -j
 make -C $PM2_ROOT/marcel/examples/memory FLAVOR=$flavor sampling_for_memory_migration
 prog=$(pm2-which -f $flavor sampling_for_memory_migration)
 

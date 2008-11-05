7f67d5669534dfc78af8da5b16fba0dd61e2a3e0
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-05 17:13:00 +0000

    MAMI: Test if the numa level topology is available
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20090 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/fragment.c
+++ marcel/examples/memory/fragment.c
@@ -27,20 +27,25 @@ int marcel_main(int argc, char * argv[]) {
   marcel_memory_init(&memory_manager);
 
   marcel_memory_membind(&memory_manager, MARCEL_MEMORY_MEMBIND_POLICY_HUGE_PAGES, 0);
-  bigsize =  marcel_topo_node_level[0].huge_page_free * memory_manager.hugepagesize;
-
-  ptr = marcel_memory_malloc(&memory_manager, bigsize/2);
-  ptr2 = marcel_memory_malloc(&memory_manager, bigsize/2);
-  if (ptr && ptr2) {
-    marcel_printf("Success\n");
-    marcel_memory_free(&memory_manager, ptr);
-    marcel_memory_free(&memory_manager, ptr2);
-    ptr2 = marcel_memory_malloc(&memory_manager, bigsize);
-    if (!ptr2) perror("marcel_memory_malloc");
-    marcel_memory_free(&memory_manager, ptr2);
+  if (!marcel_topo_node_level) {
+    marcel_printf("NUMA node topology not accessible.\n");
   }
   else {
-    printf("Failure. Could not allocated with huge pages.\n");
+    bigsize =  marcel_topo_node_level[0].huge_page_free * memory_manager.hugepagesize;
+
+    ptr = marcel_memory_malloc(&memory_manager, bigsize/2);
+    ptr2 = marcel_memory_malloc(&memory_manager, bigsize/2);
+    if (ptr && ptr2) {
+      marcel_printf("Success\n");
+      marcel_memory_free(&memory_manager, ptr);
+      marcel_memory_free(&memory_manager, ptr2);
+      ptr2 = marcel_memory_malloc(&memory_manager, bigsize);
+      if (!ptr2) perror("marcel_memory_malloc");
+      marcel_memory_free(&memory_manager, ptr2);
+    }
+    else {
+      printf("Failure. Could not allocated with huge pages.\n");
+    }
   }
 
   marcel_memory_exit(&memory_manager);
--- marcel/examples/memory/hugepages.c
+++ marcel/examples/memory/hugepages.c
@@ -26,35 +26,40 @@ int marcel_main(int argc, char * argv[]) {
   marcel_init(&argc,argv);
   marcel_memory_init(&memory_manager);
 
-  if (marcel_memory_huge_pages_available(&memory_manager)) {
-    marcel_memory_membind(&memory_manager, MARCEL_MEMORY_MEMBIND_POLICY_HUGE_PAGES, 0);
-
-    bigsize =  marcel_topo_node_level[0].huge_page_free * memory_manager.hugepagesize;
-    ptr = marcel_memory_malloc(&memory_manager, bigsize+1);
-    perror("marcel_memory_malloc");
-
-    ptr = marcel_memory_malloc(&memory_manager, bigsize/2);
-    if (!ptr) {
-      perror("marcel_memory_malloc");
-    }
-    else {
-      ptr[10] = 10;
-      marcel_printf("ptr[10]=%d\n", ptr[10]);
-
-      ptr2 =  marcel_memory_malloc(&memory_manager, bigsize/2);
-      if (!ptr2) {
-        perror("marcel_memory_malloc");
-      }
-      else {
-        ptr2[20] = 20;
-        marcel_printf("ptr2[20]=%d\n", ptr2[20]);
-        marcel_memory_free(&memory_manager, ptr2);
-      }
-      marcel_memory_free(&memory_manager, ptr);
-    }
+  if (!(marcel_memory_huge_pages_available(&memory_manager))) {
+    marcel_printf("Huge pages are not available on the system.\n");
   }
   else {
-    marcel_printf("Huge pages are not available on the system.\n");
+    marcel_memory_membind(&memory_manager, MARCEL_MEMORY_MEMBIND_POLICY_HUGE_PAGES, 0);
+
+    if (!marcel_topo_node_level) {
+      marcel_printf("NUMA node topology not accessible.\n");
+    }
+    else {
+      bigsize =  marcel_topo_node_level[0].huge_page_free * memory_manager.hugepagesize;
+      ptr = marcel_memory_malloc(&memory_manager, bigsize+1);
+      perror("marcel_memory_malloc");
+
+      ptr = marcel_memory_malloc(&memory_manager, bigsize/2);
+      if (!ptr) {
+	perror("marcel_memory_malloc");
+      }
+      else {
+	ptr[10] = 10;
+	marcel_printf("ptr[10]=%d\n", ptr[10]);
+
+	ptr2 =  marcel_memory_malloc(&memory_manager, bigsize/2);
+	if (!ptr2) {
+	  perror("marcel_memory_malloc");
+	}
+	else {
+	  ptr2[20] = 20;
+	  marcel_printf("ptr2[20]=%d\n", ptr2[20]);
+	  marcel_memory_free(&memory_manager, ptr2);
+	}
+	marcel_memory_free(&memory_manager, ptr);
+      }
+    }
   }
 
   marcel_memory_exit(&memory_manager);

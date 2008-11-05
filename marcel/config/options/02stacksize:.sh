a895fcbadf6afc22e67f6473ec4b7485dbf5fac4
fbroquedis <fbroquedis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-05 17:17:08 +0000

    applications also need to know the configured stack size for proper marcel_self inlining
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20091 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/config/options/02stacksize:.sh
+++ marcel/config/options/02stacksize:.sh
@@ -1,4 +1,4 @@
 # Set the thread stack size, in KiB.
 
 stack_size_in_bytes="$(expr $1 \* 1024)"
-PM2_MARCEL_CFLAGS_KERNEL="$PM2_MARCEL_CFLAGS_KERNEL -DMARCEL_THREAD_SLOT_SIZE=$stack_size_in_bytes"
+PM2_MARCEL_CFLAGS="$PM2_MARCEL_CFLAGS -DMARCEL_THREAD_SLOT_SIZE=$stack_size_in_bytes"

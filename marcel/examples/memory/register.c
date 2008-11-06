8c5783c2e3a551c33a2590bd85bba4820ce03ece
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-06 10:38:50 +0000

    MAMI: New test for marcel_memory_register and marcel_memory_unregister
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20100 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/memory/register.c
@@ -0,0 +1,63 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+
+#if defined(MARCEL_MAMI_ENABLED)
+
+marcel_memory_manager_t memory_manager;
+
+int marcel_main(int argc, char * argv[]) {
+  int err;
+  void *ptr;
+  marcel_t self;
+
+  marcel_init(&argc,argv);
+  marcel_memory_init(&memory_manager);
+
+  ptr = malloc(1000);
+  err = marcel_memory_register(&memory_manager, ptr, 1000, 0);
+  if (err < 0) {
+    perror("marcel_memory_register");
+  }
+
+  self = marcel_self();
+  err = marcel_memory_attach(&memory_manager, ptr, &self);
+  if (err < 0) {
+    perror("marcel_memory_attach");
+  }
+  err = marcel_memory_unattach(&memory_manager, ptr, &self);
+  if (err < 0) {
+    perror("marcel_memory_unattach");
+  }
+
+  err = marcel_memory_unregister(&memory_manager, ptr);
+  if (err < 0) {
+    perror("marcel_memory_unregister");
+  }
+  free(ptr);
+
+  marcel_memory_exit(&memory_manager);
+
+  // Finish marcel
+  marcel_end();
+  return 0;
+}
+
+#else
+int marcel_main(int argc, char * argv[]) {
+  fprintf(stderr, "This application needs MAMI to be enabled\n");
+}
+#endif

b6d290b94dd5c6c46e55c5fb655f7086797b2970
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-13 14:35:18 +0000

    MAMI: New test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20154 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/memory/attach_split.c
@@ -0,0 +1,54 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+
+#if defined(MARCEL_MAMI_ENABLED)
+
+marcel_memory_manager_t memory_manager;
+
+int marcel_main(int argc, char * argv[]) {
+  int err;
+  void *ptr=NULL;
+  marcel_t self;
+
+  marcel_init(&argc,argv);
+  marcel_memory_init(&memory_manager);
+
+  ptr = marcel_memory_malloc(&memory_manager, 10*getpagesize());
+  self = marcel_self();
+
+  err = marcel_memory_task_attach(&memory_manager, ptr, 10, &self);
+  if (err < 0) perror("marcel_memory_task_attach");
+
+  err = marcel_memory_task_attach(&memory_manager, ptr, 2*getpagesize(), &self);
+  if (err < 0) perror("marcel_memory_task_attach");
+
+  err = marcel_memory_task_unattach(&memory_manager, ptr, &self);
+  if (err < 0) perror("marcel_memory_task_unattach");
+
+  marcel_memory_free(&memory_manager, ptr);
+  marcel_memory_exit(&memory_manager);
+
+  // Finish marcel
+  marcel_end();
+  return 0;
+}
+
+#else
+int marcel_main(int argc, char * argv[]) {
+  fprintf(stderr, "This application needs MAMI to be enabled\n");
+}
+#endif

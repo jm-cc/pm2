ef7b914947f2280a9cf8dae40176ea4f395b4fdf
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-20 14:05:10 +0000

    MaMI: New testcase
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20245 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/memory/unclean.c
@@ -0,0 +1,39 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+
+#if defined(MARCEL_MAMI_ENABLED)
+
+int marcel_main(int argc, char * argv[]) {
+  void *ptr;
+  marcel_memory_manager_t memory_manager;
+
+  marcel_init(&argc,argv);
+  marcel_memory_init(&memory_manager);
+
+  ptr = marcel_memory_malloc(&memory_manager, 100);
+  marcel_memory_exit(&memory_manager);
+
+  // Finish marcel
+  marcel_end();
+  return 0;
+}
+
+#else
+int marcel_main(int argc, char * argv[]) {
+  fprintf(stderr, "This application needs MAMI to be enabled\n");
+}
+#endif

472cc199c7a8bc81752cc398f56d908e9acadf5b
fbroquedis <fbroquedis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-21 12:59:38 +0000

    The 'shake' function has to be called on a bubble, to determine the set of entities we're distributing from.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20259 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/scheduler-marcel/marcel_bubble_sched_interface.h
+++ marcel/include/scheduler-marcel/marcel_bubble_sched_interface.h
@@ -27,8 +27,8 @@ typedef int (*ma_bubble_sched_exit)(void);
 /* Submission of a set of entities to be scheduled */
 typedef int (*ma_bubble_sched_submit)(marcel_entity_t *);
 
-/* Called to force a new distribution of entities. */
-typedef int (*ma_bubble_sched_shake)(marcel_entity_t *);
+/* Called to force a new distribution from a specific bubble. */
+typedef int (*ma_bubble_sched_shake)(marcel_bubble_t *);
 
 /* Called when a vp is idle.  Preemption and bottom halves are already disabled.  */
 typedef int (*ma_bubble_sched_vp_is_idle)(unsigned);

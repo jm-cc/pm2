bd05d9c2a282afd71cdd147ea53e5c8260a52f55
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-11-24 16:01:50 +0000

    Marcel memaware scheduler: new test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20290 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/bubble-memaware.c
@@ -0,0 +1,104 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "bubble-testing.h"
+
+static void print_hierarchy(const struct marcel_topo_level *level) {
+  marcel_entity_t *e;
+  unsigned entities, child;
+
+  entities = 0;
+  for_each_entity_scheduled_on_runqueue (e, &level->rq) {
+    if (e->type == MA_THREAD_ENTITY) {
+      marcel_t thread;
+
+      thread = ma_task_entity (e);
+      if (MA_TASK_NOT_COUNTED_IN_RUNNING (thread))
+        /* THREAD is an internal thread (e.g., `ksoftirqd') so we
+           should not take it into account here.  */
+        continue;
+    }
+    entities++;
+  }
+  level_log (stdout, level, "%u entit%s, %u child%s", entities, entities>1?"ies":"y", level->arity, level->arity?"ren":"");
+  for (child = 0; child < level->arity; child++) {
+    print_hierarchy(level->children[child]);
+  }
+}
+
+int main (int argc, char *argv[]) {
+  int ret, matches_p, i;
+  char **new_argv;
+  marcel_bubble_t *root_bubble;
+  ma_atomic_t thread_exit_signal;
+
+  /* A simple topology: 2 nodes, each of which has 2 CPUs, each of which has 4
+     cores.  */
+  static const char topology_description[] = "2 2 4";
+
+  /* A flat bubble hierarchy.  */
+  static const unsigned bubble_hierarchy_description[] =
+    { 16, 0 };
+
+
+//  return test_marcel_bubble_scheduler (argc, argv,
+//                                       &marcel_bubble_memaware_sched,
+//                                       topology_description,
+//                                       bubble_hierarchy_description,
+//                                       &result_root);
+
+  /* Pass the topology description to Marcel.  Yes, it looks hackish to
+     communicate with the library via command-line arguments.  */
+  new_argv = alloca ((argc + 2) * sizeof (*new_argv));
+  new_argv[0] = argv[0];
+  new_argv[1] = (char *) "--marcel-synthetic-topology";
+  new_argv[2] = (char *) topology_description;
+  memcpy (&new_argv[3], &argv[1], argc * sizeof (*argv));
+  argc += 2;
+
+  /* Initialize Marcel.  It should pick use the "fake" topology described in
+		 TOPOLOGY_DESCRIPTION.  */
+  marcel_init(&argc, new_argv);
+  marcel_ensure_abi_compatibility(MARCEL_HEADER_HASH);
+
+  print_topology (&marcel_machine_level[0], stdout, 0);
+
+  /* Before creating any threads, initialize the variable that they'll
+     poll.  */
+  ma_atomic_init (&thread_exit_signal, 0);
+
+  /* Create a bubble hierarchy.  */
+  root_bubble = make_simple_bubble_hierarchy (bubble_hierarchy_description,
+                                              &thread_exit_signal);
+
+  marcel_bubble_change_sched(&marcel_bubble_memaware_sched);
+
+  /* Submit the generated bubble hierarchy to the scheduler.  */
+  marcel_bubble_sched_begin ();
+
+  /* Print the hierarchy */
+
+  marcel_thread_preemption_disable ();
+  ma_bubble_lock_all (&marcel_root_bubble, marcel_machine_level);
+  print_hierarchy(marcel_machine_level);
+  ma_bubble_unlock_all (&marcel_root_bubble, marcel_machine_level);
+  marcel_thread_preemption_enable ();
+
+  /* Tell threads to leave.  */
+  ma_atomic_inc (&thread_exit_signal);
+
+  free_bubble_hierarchy ();
+  marcel_end ();
+}

a31fcbd01c6aa8d20740bd7aad5f7d8e21967530
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-12-01 10:35:04 +0000

    MaMI Testing: attempt to use readv to prefault hugepages. Does not work, no idea why...
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20354 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/testing/hugepages-test.c
+++ marcel/examples/memory/testing/hugepages-test.c
@@ -90,20 +90,11 @@ unsigned long gethugepagesize(void) {
 typedef struct huge_memory_s huge_memory_t;
 
 struct huge_memory_s {
-  void *buffer;
-  char *filename;
+  int *buffer;
   int file;
+  void **pageaddrs;
+  int nbpages;
 };
-
-char *getfilename(int node) {
-  char *filename;
-  pid_t pid;
-
-  filename = malloc(1024 * sizeof(char));
-  pid = getpid();
-  sprintf(filename, "/hugetlbfs/mami_pid_%d_node_%d", pid, node);
-  return filename;
-}
 
 void free_with_huge_pages(huge_memory_t *memory) {
   int err;
@@ -113,26 +104,42 @@ void free_with_huge_pages(huge_memory_t *memory) {
     perror("close");
     exit(-1);
   }
-  err = unlink(memory->filename);
-  if (err < 0) {
-    perror("unlink");
+}
+
+int unlinked_fd() {
+  const char *path;
+  char name[1024];
+  int fd;
+
+  path = "/hugetlbfs";
+  name[sizeof(name)-1] = '\0';
+
+  strcpy(name, path);
+  strncat(name, "/libhugetlbfs.tmp.XXXXXX", sizeof(name)-1);
+  /* FIXME: deal with overflows */
+
+  fd = mkstemp64(name);
+  if (fd < 0) {
+    perror("mkstemp");
     exit(-1);
   }
+  unlink(name);
+  return fd;
 }
 
 huge_memory_t *malloc_with_huge_pages(size_t size, int node) {
   huge_memory_t *memory;
-  int err;
+  int i, err;
   unsigned long maxnode;
   unsigned long nodemask;
+  struct iovec *iov;
 
   memory = malloc(sizeof(huge_memory_t));
   maxnode = numa_max_node();
-  memory->filename = getfilename(node);
   // Set the buffers on the nodes
   nodemask = (1<<node);
 
-  memory->file = open(memory->filename, O_CREAT|O_RDWR, S_IRWXU);
+  memory->file = unlinked_fd();
   if (memory->file == -1) {
     perror("open");
     exit(-1);
@@ -151,63 +158,91 @@ huge_memory_t *malloc_with_huge_pages(size_t size, int node) {
   }
   printf("Memory policy set\n");
 
+  memory->nbpages = size / gethugepagesize();
+  if (memory->nbpages * gethugepagesize() != size) memory->nbpages++;
+  memory->pageaddrs = malloc(memory->nbpages * sizeof(void *));
+  for(i=0; i<memory->nbpages ; i++) memory->pageaddrs[i] = memory->buffer + i*gethugepagesize();
+
+  printf("Nombre de pages: %d\n", memory->nbpages);
+
+  iov = malloc(memory->nbpages * sizeof(struct iovec));
+  for(i = 0; i<memory->nbpages; i++) {
+    iov[i].iov_base = memory->pageaddrs[i];
+    iov[i].iov_len = 1;
+  }
+
+  {
+    struct stat st;
+    fstat(memory->file, &st);
+    printf("Size %ld\n", st.st_size);
+  }
+
+#if 0
+  err = readv(memory->file, iov, memory->nbpages);
+  if (err < 0) {
+    perror("readv");
+    exit(-1);
+  }
+  if (err != memory->nbpages) {
+    printf("Failed to reserve %ld hugepages (%d != %d)\n", memory->nbpages, err, memory->nbpages);
+    exit(-1);
+  }
+#else
   memset(memory->buffer, 0, size);
-  printf("Memory memset\n");
+#endif
+  printf("Memory prefault\n");
   return memory;
 }
 
 int marcel_main(int argc, char **argv) {
   int i, err;
   huge_memory_t *memory;
-  void **pageaddrs;
-  int nbpages;
-  int dest, node, realnode;
+  int nbpages, dest, node, realnode;
   size_t size;
 
   marcel_init(&argc, argv);
 
-  if (argc < 2) {
-    marcel_printf("Error. Syntax: hugepages-test <node> <dest>\n");
+  if (argc < 3) {
+    marcel_printf("Error. Syntax: hugepages-test <nbpages> <node> <dest>\n");
     marcel_end();
     exit(1);
   }
 
-  node = atoi(argv[1]);
-  dest = atoi(argv[2]);
+  nbpages = atoi(argv[1]);
+  node = atoi(argv[2]);
+  dest = atoi(argv[3]);
 
   for(i=0 ; i<marcel_nbnodes ; i++) {
     printf("Hugepages on node #%d = %d\n", i, marcel_topo_node_level[i].huge_page_free);
   }
 
-  size = 5 * gethugepagesize();
+  size = nbpages * gethugepagesize();
   memory = malloc_with_huge_pages(size, node);
 
-  // Set the page addresses
-  nbpages = size / gethugepagesize();
-  if (nbpages * gethugepagesize() != size) nbpages++;
-  pageaddrs = malloc(nbpages * sizeof(void *));
-  for(i=0; i<nbpages ; i++) pageaddrs[i] = memory->buffer + i*gethugepagesize();
-
-  realnode = check_pages_location(pageaddrs, nbpages, node);
+  realnode = check_pages_location(memory->pageaddrs, memory->nbpages, node);
   if (realnode == node) {
     printf("All pages are located on node #%d\n", node);
   }
   else {
     node = realnode;
-    realnode = check_pages_location(pageaddrs, nbpages, node);
+    realnode = check_pages_location(memory->pageaddrs, memory->nbpages, node);
     if (realnode == node) {
       printf("All pages are located on node #%d\n", node);
+    }
+    else {
+      printf("Not all the pages are located on node #%d\n", node);
     }
   }
 
   {
-    int *dests = malloc(nbpages * sizeof(int));
-    int *statuses = malloc(nbpages * sizeof(int));
-    int i;
-    for(i=0 ; i<nbpages ; i++) dests[i] = dest;
-    my_move_pages(pageaddrs, nbpages, dests, statuses);
-    print_pages(pageaddrs, nbpages);
+    int *dests = malloc(memory->nbpages * sizeof(int));
+    int *statuses = malloc(memory->nbpages * sizeof(int));
+    for(i=0 ; i<memory->nbpages ; i++) dests[i] = dest;
+    my_move_pages(memory->pageaddrs, memory->nbpages, dests, statuses);
+    //    print_pages(memory->pageaddrs, memory->nbpages);
   }
+
+  for(i=0 ; i<size/sizeof(int) ; i++) memory->buffer[i] = 42;
 
   free_with_huge_pages(memory);
 }

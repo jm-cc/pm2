c7e49452f873fcd9cab8d95be33869c5c4788136
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-12-09 14:25:08 +0000

    adaptation de 3-hello à de vieilles évolutions de nmad...
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20390 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sendrecv/3-hello.c
+++ nmad/examples/sendrecv/3-hello.c
@@ -32,171 +32,174 @@ int main(int argc, char **argv) {
 	exit(0);
 }
 #else
-int
-main(int	  argc,
-     char	**argv) {
-        struct nm_core		*p_core		= NULL;
-        char			*r_url1		= NULL;
-        char			*r_url2		= NULL;
-        char			*l_url		= NULL;
-        nm_drv_id_t		 drv_id		=    0;
-        nm_gate_t		 gate1   	=    0;
-        nm_gate_t		 gate2   	=    0;
-        char			*s_buf		= NULL;
-        char			*r_buf		= NULL;
-        uint64_t		 len;
-        int err;
-
-        err = nm_core_init(&argc, argv, &p_core);
-        if (err != NM_ESUCCESS) {
-                printf("nm_core_init returned err = %d\n", err);
-                goto out;
-        }
-
-        argc--;
-        argv++;
-
-        if (argc) {
-          /* client */
-
-            r_url1	= *argv;
-            argc--;
-            argv++;
-
-
-            if(argc){
-              r_url2	= *argv;
-              argc--;
-              argv++;
-              printf("running as client using remote url: %s %s\n", r_url1, r_url2);
-            } else {
-              printf("running as client using remote url: %s\n", r_url1);
-            }
-
-        } else {
-          /* no args: server */
-          printf("running as server\n");
-        }
-
-#if defined CONFIG_MX
-        err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "mx"), &drv_id, &l_url);
+int main(int argc, char	**argv)
+{
+  nm_core_t		p_core		= NULL;
+  char			*r_url1		= NULL;
+  char			*r_url2		= NULL;
+  char			*l_url		= NULL;
+  nm_drv_id_t		 drv_id		=    0;
+  nm_gate_t		 gate1   	=    0;
+  nm_gate_t		 gate2   	=    0;
+  int err;
+  
+  err = nm_core_init(&argc, argv, &p_core);
+  if (err != NM_ESUCCESS) {
+    printf("nm_core_init returned err = %d\n", err);
+    goto out;
+  }
+  
+  argc--;
+  argv++;
+  
+  if (argc) {
+    /* client */
+    
+    r_url1	= *argv;
+    argc--;
+    argv++;
+    
+    
+    if(argc){
+      r_url2	= *argv;
+      argc--;
+      argv++;
+      printf("running as client using remote url: %s %s\n", r_url1, r_url2);
+    } else {
+      printf("running as client using remote url: %s\n", r_url1);
+    }
+    
+  } else {
+    /* no args: server */
+    printf("running as server\n");
+  }
+  
+#if defined CONFIG_IBVERBS
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "ibverbs"), &drv_id, &l_url);
+#elif defined CONFIG_MX
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "mx"), &drv_id, &l_url);
 #elif defined CONFIG_GM
-        err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "gm"), &drv_id, &l_url);
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "gm"), &drv_id, &l_url);
 #elif defined CONFIG_QSNET
-        err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "qsnet"), &drv_id, &l_url);
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "qsnet"), &drv_id, &l_url);
 #elif defined CONFIG_SISCI
-	err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "sisci"), &drv_id, &l_url);
-#elif defined CONFIG_IBVERBS
-	err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "ibverbs"), &drv_id, &l_url);
-
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "sisci"), &drv_id, &l_url);
 #elif defined CONFIG_TCP
-        err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "tcp"), &drv_id, &l_url);
+  err = nm_core_driver_load_init(p_core, nm_core_component_load("driver", "tcp"), &drv_id, &l_url);
 #endif
-        if (err != NM_ESUCCESS) {
-                printf("nm_core_driver_load_init returned err = %d\n", err);
-                goto out;
-        }
+  if (err != NM_ESUCCESS) {
+    printf("nm_core_driver_load_init returned err = %d\n", err);
+    goto out;
+  }
 
+  err = nm_sr_init(p_core);
+  if (err != NM_ESUCCESS) {
+    printf("nm_sr_init returned err = %d\n", err);
+    goto out;
+  }  
 
-        // ouverture de deux gates
-        err = nm_core_gate_init(p_core, &gate1);
-        if (err != NM_ESUCCESS) {
-                printf("nm_core_gate_init returned err = %d\n", err);
-                goto out;
-        }
+  err = nm_core_gate_init(p_core, &gate1);
+  if (err != NM_ESUCCESS) {
+    printf("nm_core_gate_init returned err = %d\n", err);
+    goto out;
+  }
+  
+  err = nm_core_gate_init(p_core, &gate2);
+  if (err != NM_ESUCCESS) {
+    printf("nm_core_gate_init returned err = %d\n", err);
+    goto out;
+  }
+  
+  printf("local url: [%s]\n", l_url);
 
-        err = nm_core_gate_init(p_core, &gate2);
-        if (err != NM_ESUCCESS) {
-                printf("nm_core_gate_init returned err = %d\n", err);
-                goto out;
-        }
+  if(r_url1)
+    {
+      fprintf(stderr, "connecting gate1 to url: %s\n", r_url1);
+      err = nm_core_gate_connect(p_core, gate1, drv_id, r_url1);
+      if (err != NM_ESUCCESS) 
+	{
+	  printf("nm_core_gate_connect returned err = %d\n", err);
+	  goto out;
+	}
+    }
+  else
+    {
+      fprintf(stderr, "accepting connections on gate1\n");
+      err = nm_core_gate_accept(p_core, gate1, drv_id, NULL);
+      if (err != NM_ESUCCESS) 
+	{
+	  printf("nm_core_gate_accept returned err = %d\n", err);
+	  goto out;
+	}
+    }
+  fprintf(stderr, "gate1 is connected.\n");
+  if(r_url2)
+    {
+      fprintf(stderr, "connecting gate2 to url: %s\n", r_url2);
+      err = nm_core_gate_connect(p_core, gate2, drv_id, r_url2);
+      if (err != NM_ESUCCESS) 
+	{
+	  printf("nm_core_gate_connect returned err = %d\n", err);
+	  goto out;
+	}
+    }
+  else
+    {
+      fprintf(stderr, "accepting connections on gate2\n");
+      err = nm_core_gate_accept(p_core, gate2, drv_id, NULL);
+      if (err != NM_ESUCCESS) 
+	{
+	  printf("nm_core_gate_accept returned err = %d\n", err);
+	  goto out;
+	}
+    }
+  fprintf(stderr, "gate2 is connected.\n");
 
-        printf("local url: [%s]\n", l_url);
-
-        if(!r_url1 && !r_url2){
-          // accept sur les 2 gates
-          err = nm_core_gate_accept(p_core, gate1, drv_id, NULL);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_accept returned err = %d\n", err);
-            goto out;
-          }
-
-          err = nm_core_gate_accept(p_core, gate2, drv_id, NULL);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_accept returned err = %d\n", err);
-            goto out;
-          }
-
-        } else if(!r_url2){
-          // je fais un connect sur r_url1 dans la gate n°1
-          err = nm_core_gate_connect(p_core, gate1, drv_id, r_url1);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_connect returned err = %d\n", err);
-            goto out;
-          }
-
-          // je fais un accept pour r_url2 dans la gate n°2
-          err = nm_core_gate_accept(p_core, gate2, drv_id, NULL);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_accept returned err = %d\n", err);
-            goto out;
-          }
-        } else {
-          // connect sur les 2 gates
-          err = nm_core_gate_connect(p_core, gate1, drv_id, r_url1);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_connect returned err = %d\n", err);
-            goto out;
-          }
-
-          err = nm_core_gate_connect(p_core, gate2, drv_id, r_url2);
-          if (err != NM_ESUCCESS) {
-            printf("nm_core_gate_connect returned err = %d\n", err);
-            goto out;
-          }
-        }
-
-        // Initialisation des buffers d'E/S
-        len = 1+strlen(msg);
-        s_buf = malloc((size_t)len);
-        r_buf = malloc((size_t)len);
-        strcpy(s_buf, msg);
-        memset(r_buf, 0, len);
-
-        if(!r_url1 && !r_url2){
-          nm_sr_request_t request;
-
-          // ping vers 1
-          nm_sr_isend(p_core, gate1, 0, s_buf, len, &request);
-          nm_sr_swait(p_core, &request);
-
-          // pong de 2
-          nm_sr_irecv(p_core, NM_ANY_GATE, 0, r_buf, len, &request);
-          nm_sr_rwait(p_core, &request);
-
-        } else {
-          nm_sr_request_t request;
-          nm_gate_t gate = NM_ANY_GATE;
-
-          //pong de 0
-          nm_sr_irecv(p_core, NM_ANY_GATE, 0, r_buf, len, &request);
-          nm_sr_rwait(p_core, &request);
-
-          nm_sr_recv_source(p_core, &request, &gate);
-
-          if(gate == gate1){
-             nm_sr_isend(p_core, gate2, 0, s_buf, len, &request);
-             nm_sr_swait(p_core, &request);
-          } else {
-            nm_sr_isend(p_core, gate1, 0, s_buf, len, &request);
-            nm_sr_swait(p_core, &request);
-          }
-        }
-
-        printf("buffer contents: %s\n", r_buf);
-
+  size_t len = 1 + strlen(msg);
+  char*s_buf = malloc(len);
+  char*r_buf = malloc(len);
+  strcpy(s_buf, msg);
+  memset(r_buf, 0, len);
+  
+  if(!r_url1 && !r_url2)
+    {
+      nm_sr_request_t request;
+      
+      fprintf(stderr, "sending message on gate1...\n");
+      err = nm_sr_isend(p_core, gate1, 0, s_buf, len, &request);
+      nm_sr_swait(p_core, &request);
+    
+      nm_sr_irecv(p_core, NM_ANY_GATE, 0, r_buf, len, &request);
+      nm_sr_rwait(p_core, &request);
+    }
+  else 
+    {
+      nm_sr_request_t request;
+      nm_gate_t gate = NM_ANY_GATE;
+      
+      nm_sr_irecv(p_core, NM_ANY_GATE, 0, r_buf, len, &request);
+      nm_sr_rwait(p_core, &request);
+      
+      nm_sr_recv_source(p_core, &request, &gate);
+    
+      if(gate == gate1)
+	{
+	  fprintf(stderr, "got message from gate1- sending message on gate2...\n");
+	  nm_sr_isend(p_core, gate2, 0, s_buf, len, &request);
+	  nm_sr_swait(p_core, &request);
+	} 
+      else 
+	{
+	  fprintf(stderr, "got message from gate2- sending message on gate1...\n");
+	  nm_sr_isend(p_core, gate1, 0, s_buf, len, &request);
+	  nm_sr_swait(p_core, &request);
+	}
+    }
+  
+  printf("buffer contents: %s\n", r_buf);
+  
  out:
-        return 0;
+  return 0;
 }
 
 // sur le noeud 1 : pm2-load 3-hello

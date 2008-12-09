6c8bf20e111eadff6ab9e407d154449b63e0827a
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2008-12-09 14:35:07 +0000

    MaMI: Testcase which calls twice migrate_on_next_touch
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20391 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ marcel/examples/memory/next_touch_multiple.c
@@ -0,0 +1,79 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "marcel.h"
+
+#if defined(MARCEL_MAMI_ENABLED)
+
+marcel_memory_manager_t memory_manager;
+
+int *b;
+
+any_t reader(any_t arg) {
+  int node;
+
+  marcel_memory_locate(&memory_manager, b, 0, &node);
+  marcel_printf("Address is located on node %d\n", node);
+
+  b[1] = 42;
+
+  marcel_memory_locate(&memory_manager, b, 0, &node);
+  marcel_printf("Address is located on node %d\n", node);
+
+  return 0;
+}
+
+int marcel_main(int argc, char * argv[]) {
+  marcel_t thread;
+  marcel_attr_t attr;
+
+  marcel_init(&argc,argv);
+  marcel_memory_init(&memory_manager);
+  marcel_attr_init(&attr);
+
+  if (marcel_nbnodes < 3) {
+    marcel_printf("This application needs at least two NUMA nodes.\n");
+  }
+  else {
+    b = marcel_memory_malloc(&memory_manager, 3*memory_manager.normalpagesize, MARCEL_MEMORY_MEMBIND_POLICY_SPECIFIC_NODE, 0);
+
+    marcel_memory_migrate_on_next_touch(&memory_manager, b);
+
+    // Start the thread on the numa node #1
+    marcel_attr_settopo_level(&attr, &marcel_topo_node_level[1]);
+    marcel_create(&thread, &attr, reader, NULL);
+    marcel_join(thread, NULL);
+
+    marcel_memory_migrate_on_next_touch(&memory_manager, b);
+
+    // Start the thread on the numa node #2
+    marcel_attr_settopo_level(&attr, &marcel_topo_node_level[2]);
+    marcel_create(&thread, &attr, reader, NULL);
+    marcel_join(thread, NULL);
+
+    marcel_memory_free(&memory_manager, b);
+  }
+
+  // Finish marcel
+  marcel_memory_exit(&memory_manager);
+  marcel_end();
+  return 0;
+}
+
+#else
+int marcel_main(int argc, char * argv[]) {
+  fprintf(stderr, "This application needs MAMI to be enabled\n");
+}
+#endif

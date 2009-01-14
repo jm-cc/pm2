130c01938e52dfc6a6df41717f7797c96c65ac13
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-01-14 09:48:36 +0000

    Marcel: Topology fixlet.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20589 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/source/marcel_topology.c
+++ marcel/source/marcel_topology.c
@@ -1381,7 +1381,7 @@ static void topo_discover(void) {
 #  endif
 	struct marcel_topo_level *level;
 
-#if(defined(HAVE_OPENAT) && defined(MA__LWPS))
+#ifdef HAVE_OPENAT
 	if (fsys_root_fd < 0) {
 		/* Get a file descriptor to the file system root.  */
 		if (ma_topology_set_fsys_root("/")) {

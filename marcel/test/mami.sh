a9ff591371e38ad4d0db82d8be78e90759b891a9
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-01-22 15:07:53 +0000

    MaMI Testcases: do not run the test on a non-NUMA machine
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20724 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/test/mami.sh
+++ marcel/test/mami.sh
@@ -1,12 +1,12 @@
 #-*-sh-*-
 # PM2: Parallel Multithreaded Machine
 # Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or (at
 # your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -14,6 +14,11 @@
 
 # This file is meant to be `source'd by the various
 # `test_mami*.level*' files.
+
+if [ ! -d /sys/devices/system/node/node0 ] ; then
+    echo "The machine does not seem to be NUMA-aware"
+    continue
+fi
 
 flavor="test_marcel_mami"
 appdir="${PM2_ROOT}/marcel/examples/memory"

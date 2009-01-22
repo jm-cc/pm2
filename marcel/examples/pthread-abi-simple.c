d3391f971e9119b7b6e1e5c42b2057092df93670
lcourtes <lcourtes@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-01-22 15:48:37 +0000

    Marcel: pthread-abi-simple: Show $LD_PRELOAD upon failure.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20725 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/pthread-abi-simple.c
+++ marcel/examples/pthread-abi-simple.c
@@ -1,6 +1,6 @@
 /*
  * PM2: Parallel Multithreaded Machine
- * Copyright (C) 2008 "the PM2 team" (see AUTHORS file)
+ * Copyright (C) 2008, 2009 "the PM2 team" (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -60,7 +60,8 @@ assert_running_marcel (void)
 	return;
     }
 
-  fprintf (stderr, "error: not using Marcel's libpthread\n");
+  fprintf (stderr, "error: not using Marcel's libpthread (LD_PRELOAD=\"%s\")\n",
+	   getenv ("LD_PRELOAD"));
   abort ();
 }
 

b030a5def70e0adc576d538cb1453aeaed09529a
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-01-23 09:46:31 +0000

    pm2 scripts: fix copyright
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20752 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- bin/nmad-trace.py
+++ bin/nmad-trace.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python2.5
+
+# PM2: Parallel Multithreaded Machine
+# Copyright (C) 2007 "the PM2 team" (see AUTHORS file)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+
 class NM_Event:
     def __init__(self, list):
 	self.previous_length	= int(list.pop(0), 16)

fa69cfc832ce02444c13c85d4a9d878a9b40fe64
fbroquedis <fbroquedis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-02-02 10:48:18 +0000

    Keep cleaning the geneC.c file (mainly get rid of French stuff).
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20844 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubblegum/include/geneC.h
+++ profile/bubblegum/include/geneC.h
@@ -7,5 +7,4 @@
 #define GENEC_NAME "to_pm2"
 #define GENEC_MAKEFILE "Makefile_bubblegum"
 
-int parcourir_bulle(Element*, int);
-int gen_fichier_C(const char *file, Element *root);
+int gen_fichier_C (const char *file, Element *root_bubble);
--- profile/bubblegum/source/geneC.c
+++ profile/bubblegum/source/geneC.c
@@ -1,55 +1,56 @@
+#include <assert.h>
+
 #include "geneC.h"
 #include "interfaceGauche.h"
 
 FILE * fw;
 
-/* Fonction de parcours de bulle
-   mode DECLARE ne fait que déclarer les variables de manière globale,
-   mode GENERE produit le code */
-
-int parcourir_bulle(Element* bulle, int mybid) 
+/* Browse bubble _from_bubble_ and generates the code involved in the
+   creation of the corresponding threads and bubbles tree. */
+static void generate_bubble_tree (Element* from_bubble, int mybid) 
 {
   int id;
   Element * element_i;
   ListeElement *liste;
   TypeElement type;
 
-  for (liste = FirstListeElement(bulle); liste; liste = NextListeElement(liste))
+  for (liste = FirstListeElement (from_bubble); liste; liste = NextListeElement (liste))
     {
       element_i = liste->element;
-      type = GetTypeElement(element_i);
-      id = GetId(element_i);
+      type = GetTypeElement (element_i);
+      id = GetId (element_i);
       if (type == BULLE)
 	{
-	  fprintf(fw,"marcel_bubble_t b%d;\n",id);
-	  fwprintf(fw,L"   //création de la bulle %d :\n",id);
-	  fprintf(fw,"   marcel_bubble_init(&b%d);\n",id);
-	  fprintf(fw,"   marcel_bubble_setid(&b%d, %d);\n",id,id);
-	  if (mybid>0)
-	    fprintf(fw,"   marcel_bubble_insertbubble(&b%d, &b%d);\n",mybid,id);
+	  fprintf (fw, "marcel_bubble_t b%d;\n", id);
+	  fprintf (fw, "   /* Creating bubble %d */\n", id);
+	  fprintf (fw, "   marcel_bubble_init (&b%d);\n", id);
+	  fprintf (fw, "   marcel_bubble_setid (&b%d, %d);\n", id, id);
+	  
+	  if (mybid > 0)
+	    fprintf (fw, "   marcel_bubble_insertbubble (&b%d, &b%d);\n", mybid, id);
 	  else
-	    fprintf(fw,"   marcel_bubble_insertbubble(&marcel_root_bubble, &b%d);\n",id);
-	  //fprintf(fw,"   marcel_bubble_setprio(&b%d,%d);\n\n",id,GetPrioriteBulle(element_i));
-	  parcourir_bulle(element_i,id);
+	    fprintf (fw, "   marcel_bubble_insertbubble (&marcel_root_bubble, &b%d);\n", id);
+	  generate_bubble_tree (element_i, id);
 	}  
       if (type == THREAD)
 	{
-	  fprintf(fw,"marcel_t t%d;\n",id); 
-	  fwprintf(fw,L"   //création du thread id = %d :\n",id);
-	  fprintf(fw,"   {\n");
-	  fprintf(fw,"      marcel_attr_t attr;\n");
-	  fprintf(fw,"      marcel_attr_init(&attr);\n");
-	  if (mybid)
-	    fprintf(fw,"      marcel_attr_setinitbubble(&attr, &b%d);\n",mybid);
-	  fprintf(fw,"      marcel_attr_setid(&attr,%d);\n",id);
-	  //fprintf(fw,"      marcel_attr_setprio(&attr,%d);\n",GetPrioriteThread(element_i));		  
-	  fprintf(fw,"      marcel_attr_setname(&attr,\"%s\");\n",GetNom(element_i));
-	  fprintf(fw,"      marcel_create(&t%d, &attr, f, (any_t)(intptr_t)%d);\n",id,id*MAX_CHARGE+GetCharge(element_i));
-	  fprintf(fw,"      *marcel_stats_get(t%d, load) = %d;\n",id,GetCharge(element_i));
-	  fprintf(fw,"   }\n\n");
+	  fprintf (fw, "marcel_t t%d;\n", id); 
+	  fprintf (fw, "   /* Creating thread %d */\n", id);
+	  fprintf (fw, "   {\n");
+	  fprintf (fw, "      marcel_attr_t attr;\n");
+	  fprintf (fw, "      marcel_attr_init (&attr);\n");
+
+	  if (mybid > 0)
+	    fprintf (fw, "      marcel_attr_setinitbubble (&attr, &b%d);\n", mybid);
+
+	  fprintf (fw, "      marcel_attr_setid (&attr,%d);\n", id);
+	  fprintf (fw, "      marcel_attr_setprio (&attr,%d);\n", GetPrioriteThread (element_i));		  
+	  fprintf (fw, "      marcel_attr_setname (&attr,\"%s\");\n", GetNom (element_i));
+	  fprintf (fw, "      marcel_create (&t%d, &attr, f, (any_t)(intptr_t)%d);\n", id, id * MAX_CHARGE + GetCharge (element_i));
+	  fprintf (fw, "      *marcel_stats_get (t%d, load) = %d;\n", id, GetCharge (element_i));
+	  fprintf (fw, "   }\n\n");
 	}
     }
-  return 0;
 }
 
 static void add_headers (FILE *fd)
@@ -84,7 +85,7 @@ static void add_main_function (FILE *fd, Element *root_bubble)
   fprintf (fw, "#endif\n\n");
   fprintf (fw, "   marcel_printf (\"started\\n\");\n\n");
   
-  parcourir_bulle (root_bubble, 0);
+  generate_bubble_tree (root_bubble, 0);
   
   fprintf (fw, "   marcel_start_playing ();\n");
   fprintf (fw, "   marcel_bubble_sched_begin ();\n");
@@ -94,54 +95,52 @@ static void add_main_function (FILE *fd, Element *root_bubble)
   fprintf (fw, "   return 0;\n}\n");
 }
 
-/* Fonction principale de génération du fichier .C */
-int gen_fichier_C(const char * fichier, Element * bullemere)
+/* Main function for generating the C example program. */
+int gen_fichier_C (const char * file, Element * root_bubble)
 {
-  if (GetTypeElement(bullemere) != BULLE)
-    {
-      wprintf(L"Le fichier entré en paramètre n'est pas une BULLE\n"); 
-      return -1;
-    }
-  fw = fopen(fichier, "w");
+  assert (GetTypeElement (root_bubble) == BULLE);
+
+  fw = fopen (file, "w");
   if (fw == NULL)
     {
-      wprintf(L"Erreur lors de l'ouverture du fichier en écriture\n"); 
+      printf ("Error while loading file '%s'.\n", file); 
       return -1;
     }
 
-  wprintf(L"**** Début de la génération du fichier '%s' ****\n", fichier);
+  printf ("**** Generating the '%s' file ****\n", file);
    
   add_headers (fw);
   add_static_functions (fw);
-  add_main_function (fw, bullemere);
+  add_main_function (fw, root_bubble);
 
   fclose(fw);
   
-  wprintf(L"**** Fichier '%s' généré ****\n\n", fichier);
+  printf ("**** File '%s' successfully generated ****\n\n", file);
 
-
-  fw = fopen(GENEC_MAKEFILE,"w");
+  fw = fopen (GENEC_MAKEFILE, "w");
   if (fw == NULL)
     {
-      wprintf(L"Erreur lors de l'ouverture du fichier en écriture\n"); 
+      printf ("Error while loading file '%s'.\n", GENEC_MAKEFILE); 
       return -1;
     }
 
-  wprintf(L"**** Démarrage de la génération du fichier Makefile ****\n");
+  printf ("**** Generating Makefile ****\n");
 
-  fprintf(fw,"CC\t=\t$(shell pm2-config --cc)\n");
-  fprintf(fw,"CFLAGS\t=\t$(shell pm2-config --cflags)\n");
-  fprintf(fw,"LIBS\t=\t$(shell pm2-config --libs)\n\n");
-  fprintf(fw,"modules\t\t:=\t$(shell pm2-config --modules)\n");
-  fprintf(fw,"stamplibs\t:=\t$(shell for i in ${modules} ; do pm2-config --stampfile $$i ; done)\n\n");
+  fprintf (fw, "CC\t=\t$(shell pm2-config --cc)\n");
+  fprintf (fw, "CFLAGS\t=\t$(shell pm2-config --cflags)\n");
+  fprintf (fw, "LIBS\t=\t$(shell pm2-config --libs)\n\n");
+  fprintf (fw, "modules\t\t:=\t$(shell pm2-config --modules)\n");
+  fprintf (fw, "stamplibs\t:=\t$(shell for i in ${modules} ; do pm2-config --stampfile $$i ; done)\n\n");
 
-  fprintf(fw,"all: %s\n", GENEC_NAME);
-  fprintf(fw,"%s: %s.o ${stamplibs}\n", GENEC_NAME, GENEC_NAME);
-  fprintf(fw,"\t$(CC) $< $(LIBS) -o $@\n");
-  fprintf(fw,"%s.o: %s.c\n", GENEC_NAME, GENEC_NAME);
-  fprintf(fw,"\t$(CC) $(CFLAGS) -c $< -o $@\n");
+  fprintf (fw, "all: %s\n", GENEC_NAME);
+  fprintf (fw, "%s: %s.o ${stamplibs}\n", GENEC_NAME, GENEC_NAME);
+  fprintf (fw, "\t$(CC) $< $(LIBS) -o $@\n");
+  fprintf (fw, "%s.o: %s.c\n", GENEC_NAME, GENEC_NAME);
+  fprintf (fw, "\t$(CC) $(CFLAGS) -c $< -o $@\n");
 
-  fclose(fw);
+  printf ("**** Makefile successfully generated ****\n");
+
+  fclose (fw);
 
   return 0;
 }

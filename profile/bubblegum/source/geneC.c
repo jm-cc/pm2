f7d51ab4ab81e6776b244ca7716f1cda5a38734c
fbroquedis <fbroquedis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-02-02 10:51:44 +0000

    * geneC.c cleanup: Move the Makefile generation into a static function.
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20845 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubblegum/source/geneC.c
+++ profile/bubblegum/source/geneC.c
@@ -95,6 +95,25 @@ static void add_main_function (FILE *fd, Element *root_bubble)
   fprintf (fw, "   return 0;\n}\n");
 }
 
+static void generate_makefile (FILE *fd)
+{
+  printf ("**** Generating Makefile ****\n");
+
+  fprintf (fw, "CC\t=\t$(shell pm2-config --cc)\n");
+  fprintf (fw, "CFLAGS\t=\t$(shell pm2-config --cflags)\n");
+  fprintf (fw, "LIBS\t=\t$(shell pm2-config --libs)\n\n");
+  fprintf (fw, "modules\t\t:=\t$(shell pm2-config --modules)\n");
+  fprintf (fw, "stamplibs\t:=\t$(shell for i in ${modules} ; do pm2-config --stampfile $$i ; done)\n\n");
+
+  fprintf (fw, "all: %s\n", GENEC_NAME);
+  fprintf (fw, "%s: %s.o ${stamplibs}\n", GENEC_NAME, GENEC_NAME);
+  fprintf (fw, "\t$(CC) $< $(LIBS) -o $@\n");
+  fprintf (fw, "%s.o: %s.c\n", GENEC_NAME, GENEC_NAME);
+  fprintf (fw, "\t$(CC) $(CFLAGS) -c $< -o $@\n");
+
+  printf ("**** Makefile successfully generated ****\n");
+}
+
 /* Main function for generating the C example program. */
 int gen_fichier_C (const char * file, Element * root_bubble)
 {
@@ -123,22 +142,8 @@ int gen_fichier_C (const char * file, Element * root_bubble)
       printf ("Error while loading file '%s'.\n", GENEC_MAKEFILE); 
       return -1;
     }
-
-  printf ("**** Generating Makefile ****\n");
-
-  fprintf (fw, "CC\t=\t$(shell pm2-config --cc)\n");
-  fprintf (fw, "CFLAGS\t=\t$(shell pm2-config --cflags)\n");
-  fprintf (fw, "LIBS\t=\t$(shell pm2-config --libs)\n\n");
-  fprintf (fw, "modules\t\t:=\t$(shell pm2-config --modules)\n");
-  fprintf (fw, "stamplibs\t:=\t$(shell for i in ${modules} ; do pm2-config --stampfile $$i ; done)\n\n");
-
-  fprintf (fw, "all: %s\n", GENEC_NAME);
-  fprintf (fw, "%s: %s.o ${stamplibs}\n", GENEC_NAME, GENEC_NAME);
-  fprintf (fw, "\t$(CC) $< $(LIBS) -o $@\n");
-  fprintf (fw, "%s.o: %s.c\n", GENEC_NAME, GENEC_NAME);
-  fprintf (fw, "\t$(CC) $(CFLAGS) -c $< -o $@\n");
-
-  printf ("**** Makefile successfully generated ****\n");
+  
+  generate_makefile (fw);
 
   fclose (fw);
 

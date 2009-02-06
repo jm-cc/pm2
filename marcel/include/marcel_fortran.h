f67996ed5d42e20618f74de07449fee61cfd9d27
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-02-06 09:58:25 +0000

    Marcel: Declare function prototypes as extern
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@20890 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/include/marcel_fortran.h
+++ marcel/include/marcel_fortran.h
@@ -18,26 +18,36 @@
 
 #section functions
 
+extern
 void marcel_init_(void);
 
+extern
 void marcel_end_(void);
 
+extern
 void marcel_self_(marcel_t *self);
 
+extern
 void marcel_create_(marcel_t *pid, void* (*func)(void*), void *arg);
 
+extern
 void marcel_join_(marcel_t *pid);
 
+extern
 void marcel_yield_(void);
 
+extern
 void marcel_set_load_(int *load);
 
+extern
 void marcel_rien_(void);
 
 #ifdef MA__BUBBLES
 
+extern
 void marcel_bubble_submit_(void);
 
+extern
 void marcel_spread_(void);
 
 #endif /* MA__BUBBLES */

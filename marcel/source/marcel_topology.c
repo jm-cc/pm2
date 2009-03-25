jm-cc/pm2

commit a543a1724086427083fb4509a0f00b13f83d3eb5

Marcel topology: correct marcel_nbnodes if needed (cpuset might disable some of the nodes)
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21316 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/source/marcel_topology.c
@@ -2092,6 +2092,19 @@ static void topo_discover(void) {
 	for (l=0; l<marcel_topo_nblevels; l++)
 		for (i=0; !marcel_vpset_iszero(&marcel_topo_levels[l][i].vpset); i++)
 			marcel_topo_levels[l][i].level = l;
+
+        {
+                int node, marcel_nbnodes_aux = 0;
+                for(node=0 ; node<marcel_nbnodes ; node++)
+                        if (!marcel_vpset_iszero(&marcel_topo_node_level[node].cpuset)) {
+                                marcel_nbnodes_aux ++;
+                                mdebug_topology("Node %d is enabled\n", node);
+                        }
+                if (marcel_nbnodes_aux != marcel_nbnodes) {
+                        mdebug_topology("Fixing number of nodes from %d to %d\n", marcel_nbnodes, marcel_nbnodes_aux);
+                        marcel_nbnodes = marcel_nbnodes_aux;
+                }
+        }
 }
 
 void ma_topo_exit(void) {

commit 9e7a521790d35bbc017c54e062a4455038b60593

Marcel topology: fix bug introduced by r21316
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21321 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/source/marcel_topology.c
@@ -2093,7 +2093,8 @@ static void topo_discover(void) {
 		for (i=0; !marcel_vpset_iszero(&marcel_topo_levels[l][i].vpset); i++)
 			marcel_topo_levels[l][i].level = l;
 
-        {
+#  ifdef MA__NUMA
+        if (marcel_topo_node_level) {
                 int node, marcel_nbnodes_aux = 0;
                 for(node=0 ; node<marcel_nbnodes ; node++)
                         if (!marcel_vpset_iszero(&marcel_topo_node_level[node].cpuset)) {
@@ -2105,6 +2106,7 @@ static void topo_discover(void) {
                         marcel_nbnodes = marcel_nbnodes_aux;
                 }
         }
+#  endif
 }
 
 void ma_topo_exit(void) {
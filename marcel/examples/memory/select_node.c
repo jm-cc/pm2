1d5c31b4a4bfb1db05501f6f5b2874da576573eb
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-03-27 14:21:36 +0000

    MaMI: Example select_node: fix size to allocate
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21361 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/memory/select_node.c
+++ marcel/examples/memory/select_node.c
@@ -27,7 +27,7 @@ int marcel_main(int argc, char * argv[]) {
   marcel_memory_init(&memory_manager);
 
   marcel_memory_select_node(&memory_manager, MARCEL_MEMORY_LEAST_LOADED_NODE, &anode);
-  size = marcel_topo_node_level[anode].memory_kB[MARCEL_TOPO_LEVEL_MEMORY_NODE]/1024*2;
+  size = marcel_topo_node_level[anode].memory_kB[MARCEL_TOPO_LEVEL_MEMORY_NODE]*1024/2;
   ptr = marcel_memory_malloc(&memory_manager, size, MARCEL_MEMORY_MEMBIND_POLICY_SPECIFIC_NODE, anode);
 
   marcel_memory_select_node(&memory_manager, MARCEL_MEMORY_LEAST_LOADED_NODE, &bnode);

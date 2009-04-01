54bea2aa3d894874fc9a5c32cbad6d4b5a6adebf
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-04-01 12:31:43 +0000

    bench en plusieurs passes, plus reproductible
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21398 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sendrecv/sr_bench.c
+++ nmad/examples/sendrecv/sr_bench.c
@@ -27,9 +27,10 @@
 #define MULT_DEFAULT    2
 #define INCR_DEFAULT    0
 #define WARMUPS_DEFAULT 100
-#define LOOPS_DEFAULT   5000
+#define LOOPS_DEFAULT   1000
+#define PASSES_DEFAULT  50
 
-#define MAX_DATA        (100ULL * 1024ULL * 1024ULL) /* 100 MB */
+#define MAX_DATA        (32ULL * 1024ULL * 1024ULL) /* 32 MB */
 
 static __inline__
 uint32_t _next(uint32_t len, uint32_t multiplier, uint32_t increment)
@@ -76,128 +77,133 @@ static void clear_buffer(char *buffer, int len) {
   memset(buffer, 0, len);
 }
 
-int
-main(int	  argc,
-     char	**argv) {
-        char		*buf		= NULL;
-        uint32_t	 len;
-        uint32_t	 start_len      = MIN_DEFAULT;
-        uint32_t	 end_len        = MAX_DEFAULT;
-        uint32_t         multiplier     = MULT_DEFAULT;
-        uint32_t         increment      = INCR_DEFAULT;
-        int              iterations     = LOOPS_DEFAULT;
-        int              warmups        = WARMUPS_DEFAULT;
-        int              i;
-
-        init(&argc, argv);
-
-        if (argc > 1 && !strcmp(argv[1], "--help")) {
-          usage_ping();
-          nmad_exit();
-          exit(0);
-        }
-
-        for(i=1 ; i<argc ; i+=2) {
-          if (!strcmp(argv[i], "-S")) {
-            start_len = atoi(argv[i+1]);
-          }
-          else if (!strcmp(argv[i], "-E")) {
-            end_len = atoi(argv[i+1]);
-          }
-          else if (!strcmp(argv[i], "-I")) {
-            increment = atoi(argv[i+1]);
-          }
-          else if (!strcmp(argv[i], "-M")) {
-            multiplier = atoi(argv[i+1]);
-          }
-          else if (!strcmp(argv[i], "-N")) {
-            iterations = atoi(argv[i+1]);
-          }
-          else if (!strcmp(argv[i], "-W")) {
-            warmups = atoi(argv[i+1]);
-          }
-          else {
-            fprintf(stderr, "Illegal argument %s\n", argv[i]);
-            usage_ping();
-            nmad_exit();
-            exit(0);
-          }
-        }
-
-        buf = malloc(end_len);
-
-        if (is_server) {
+int main(int argc, char	**argv)
+{
+  uint32_t	 start_len      = MIN_DEFAULT;
+  uint32_t	 end_len        = MAX_DEFAULT;
+  uint32_t         multiplier     = MULT_DEFAULT;
+  uint32_t         increment      = INCR_DEFAULT;
+  int              iterations     = LOOPS_DEFAULT;
+  int              warmups        = WARMUPS_DEFAULT;
+  int              i;
+  
+  init(&argc, argv);
+  
+  if (argc > 1 && !strcmp(argv[1], "--help")) {
+    usage_ping();
+    nmad_exit();
+    exit(0);
+  }
+  
+  for(i=1 ; i<argc ; i+=2) {
+    if (!strcmp(argv[i], "-S")) {
+      start_len = atoi(argv[i+1]);
+    }
+    else if (!strcmp(argv[i], "-E")) {
+      end_len = atoi(argv[i+1]);
+    }
+    else if (!strcmp(argv[i], "-I")) {
+      increment = atoi(argv[i+1]);
+    }
+    else if (!strcmp(argv[i], "-M")) {
+      multiplier = atoi(argv[i+1]);
+    }
+    else if (!strcmp(argv[i], "-N")) {
+      iterations = atoi(argv[i+1]);
+    }
+    else if (!strcmp(argv[i], "-W")) {
+      warmups = atoi(argv[i+1]);
+    }
+    else {
+      fprintf(stderr, "Illegal argument %s\n", argv[i]);
+      usage_ping();
+      nmad_exit();
+      exit(0);
+    }
+  }
+  
+  char* buf = malloc(end_len);
+  if(is_server)
+    {
+      /* server
+       */
+      uint32_t	 len;
+      for(len = start_len; len <= end_len; len = _next(len, multiplier, increment))
+	{
 	  int k;
-                /* server
-                 */
-		for(len = start_len; len <= end_len; len = _next(len, multiplier, increment)) {
-		  clear_buffer(buf, len);
-		  iterations = _iterations(iterations, len);
-		  for(k = 0; k < iterations + warmups; k++) {
-		    nm_sr_request_t request;
-
-		    nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_rwait(p_core, &request);
-
-		    nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_swait(p_core, &request);
-		  }
-		}
-
-        } else {
-	  tbx_tick_t t1, t2;
-          double sum, lat, bw_million_byte, bw_mbyte;
+	  clear_buffer(buf, len);
+	  iterations = _iterations(iterations, len);
+	  for(k = 0; k < iterations * PASSES_DEFAULT + warmups; k++)
+	    {
+	      nm_sr_request_t request;
+	      
+	      nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
+	      nm_sr_rwait(p_core, &request);
+	      
+	      nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
+	      nm_sr_swait(p_core, &request);
+	    }
+	}
+    }
+  else 
+    {
+      /* client
+       */
+      tbx_tick_t t1, t2;
+      printf("# size |  latency     |   10^6 B/s   |   MB/s    |\n");
+      uint32_t	 len;
+      for(len = start_len; len <= end_len; len = _next(len, multiplier, increment))
+	{
 	  int k;
-                /* client
-                 */
-                printf("# size |  latency     |   10^6 B/s   |   MB/s    |\n");
-
-		for(len = start_len; len <= end_len; len = _next(len, multiplier, increment)) {
-
-		  iterations = _iterations(iterations, len);
-		  fill_buffer(buf, len);
-
-		  for(k = 0; k < warmups; k++) {
-		    nm_sr_request_t request;
-
-		    nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_swait(p_core, &request);
-
-                    nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_rwait(p_core, &request);
-		  }
-
-		  TBX_GET_TICK(t1);
-
-		  for(k = 0; k < iterations; k++) {
-		    nm_sr_request_t request;
-
-		    nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_swait(p_core, &request);
-
-		    nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
-		    nm_sr_rwait(p_core, &request);
-		  }
-
-		  TBX_GET_TICK(t2);
-
-                   sum = TBX_TIMING_DELAY(t1, t2);
-
-                  lat	      = sum / (2 * iterations);
-                  bw_million_byte = len * (iterations / (sum / 2));
-                  bw_mbyte        = bw_million_byte / 1.048576;
-
+	  iterations = _iterations(iterations, len);
+	  fill_buffer(buf, len);
+	  
+	  for(k = 0; k < warmups; k++)
+	    {
+	      nm_sr_request_t request;
+	      
+	      nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
+	      nm_sr_swait(p_core, &request);
+	      
+	      nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
+	      nm_sr_rwait(p_core, &request);
+	    }
+	  
+	  double best = -1;
+	  int p;
+	  for(p = 0; p < PASSES_DEFAULT; p++)
+	    {
+	      TBX_GET_TICK(t1);
+	      for(k = 0; k < iterations; k++)
+		{
+		  nm_sr_request_t request;
+		  
+		  nm_sr_isend(p_core, gate_id, 0, buf, len, &request);
+		  nm_sr_swait(p_core, &request);
+		  
+		  nm_sr_irecv(p_core, gate_id, 0, buf, len, &request);
+		  nm_sr_rwait(p_core, &request);
+		}	  
+	      TBX_GET_TICK(t2);	  
+	      double sum = TBX_TIMING_DELAY(t1, t2);
+	      if(best < 0 || sum < best)
+		best = sum;
+	    }
+	  double lat	         = best / (2 * iterations);
+	  double bw_million_byte = len * (iterations / (best / 2));
+	  double bw_mbyte        = bw_million_byte / 1.048576;
+	  
 #ifdef MARCEL
-		  printf("%d\t%lf\t%8.3f\t%8.3f\tmarcel\n",
-                         len, lat, bw_million_byte, bw_mbyte);
+	  printf("%d\t%lf\t%8.3f\t%8.3f\tmarcel\n",
+		 len, lat, bw_million_byte, bw_mbyte);
 #else
-		  printf("%d\t%lf\t%8.3f\t%8.3f\n",
-                         len, lat, bw_million_byte, bw_mbyte);
+	  printf("%d\t%lf\t%8.3f\t%8.3f\n",
+		 len, lat, bw_million_byte, bw_mbyte);
 #endif
-		}
-        }
-
-	free(buf);
-        nmad_exit();
-        exit(0);
+	}
+    }
+  
+  free(buf);
+  nmad_exit();
+  exit(0);
 }

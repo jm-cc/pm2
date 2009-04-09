jm-cc/pm2

commit 0a9ead6a4c3b74b0948fbc9efc40fb5376674537

Marcel: libpthread: Rewrite rwlocks based on Glibc 2.7 code.
The definition of `lpt_rwlock_t' is updated to Glibc 2.7 and adapted to
Marcel (in an binary-compatible way).

git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21458 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/include/marcel_rwlock.h
@@ -50,8 +50,9 @@ typedef struct _marcel_rwlock_t
 } marcel_rwlock_t, pmarcel_rwlock_t;
 
 
-/* Attribute for read-write locks.  */
-typedef struct
+/* Attribute for read-write locks (from glibc's
+ * `sysdeps/unix/sysv/linux/internaltypes.h').  */
+typedef struct marcel_rwlockattr
 {
   int __lockkind;
   int __pshared;

marcel/include/asm-i386/nptl_types.h
@@ -110,16 +110,19 @@ typedef union
 {
   struct
   {
-    int __lock;
+    long int __lock;
     unsigned int __nr_readers;
-    unsigned int __readers_wakeup;
-    unsigned int __writer_wakeup;
+    struct _lpt_fastlock __readers_wakeup;
+    struct _lpt_fastlock __writer_wakeup;
     unsigned int __nr_readers_queued;
     unsigned int __nr_writers_queued;
     /* FLAGS must stay at this position in the structure to maintain
        binary compatibility.  */
-    unsigned int __flags;
-    int __writer;
+    unsigned char __flags;
+    unsigned char __shared;
+    unsigned char __pad1;
+    unsigned char __pad2;
+    marcel_t __writer;
   } __data;
   char __size[__SIZEOF_LPT_RWLOCK_T];
   long int __align;

marcel/include/asm-ia64/nptl_types.h
@@ -115,21 +115,21 @@ typedef union
 {
   struct
   {
-    int __lock;
+    long int __lock;
     unsigned int __nr_readers;
-    unsigned int __readers_wakeup;
-    unsigned int __writer_wakeup;
+    struct _lpt_fastlock __readers_wakeup;
+    struct _lpt_fastlock __writer_wakeup;
     unsigned int __nr_readers_queued;
     unsigned int __nr_writers_queued;
-    int __writer;
-    int __pad1;
+    marcel_t __writer;
+    int __shared;
+    unsigned long int __pad1;
     unsigned long int __pad2;
-    unsigned long int __pad3;
     /* FLAGS must stay at this position in the structure to maintain
        binary compatibility.  */
     unsigned int __flags;
   } __data;
-  char __size[__SIZEOF_LPT_RWLOCK_T];
+  char __size[__SIZEOF_PTHREAD_RWLOCK_T];
   long int __align;
 } lpt_rwlock_t;
 

marcel/include/asm-new/nptl_types.h
@@ -110,16 +110,16 @@ typedef union
 {
   struct
   {
-    int __lock;
+    long int __lock;
     unsigned int __nr_readers;
-    unsigned int __readers_wakeup;
-    unsigned int __writer_wakeup;
+    struct _lpt_fastlock __readers_wakeup;
+    struct _lpt_fastlock __writer_wakeup;
     unsigned int __nr_readers_queued;
     unsigned int __nr_writers_queued;
     /* FLAGS must stay at this position in the structure to maintain
        binary compatibility.  */
     unsigned int __flags;
-    int __writer;
+    marcel_t __writer;
   } __data;
   char __size[__SIZEOF_LPT_RWLOCK_T];
   long int __align;

marcel/include/asm-x86_64/nptl_types.h
@@ -110,19 +110,19 @@ typedef union
 {
   struct
   {
-    int __lock;
+    long int __lock;
     unsigned int __nr_readers;
-    unsigned int __readers_wakeup;
-    unsigned int __writer_wakeup;
+    struct _lpt_fastlock __readers_wakeup;
+    struct _lpt_fastlock __writer_wakeup;
     unsigned int __nr_readers_queued;
     unsigned int __nr_writers_queued;
-    int __writer;
-    int __pad1;
-    unsigned long int __pad2;
-    unsigned long int __pad3;
     /* FLAGS must stay at this position in the structure to maintain
        binary compatibility.  */
-    unsigned int __flags;
+    unsigned char __flags;
+    unsigned char __shared;
+    unsigned char __pad1;
+    unsigned char __pad2;
+    marcel_t __writer;
   } __data;
   char __size[__SIZEOF_LPT_RWLOCK_T];
   long int __align;

marcel/include/marcel_timer.h
@@ -81,3 +81,13 @@ static __tbx_inline__ unsigned int preemption_enabled(void)
 
 #section marcel_macros
 #define MA_LWP_RESCHED(lwp) marcel_kthread_kill((lwp)->pid, MARCEL_RESCHED_SIGNAL)
+
+/** \brief Return the number of microseconds corresponding to \param ts, a
+ * `struct timespec' pointer.  */
+#define MA_TIMESPEC_TO_USEC(_ts)								\
+  ((_ts)->tv_sec * 1e6 + (_ts)->tv_nsec / 1e3)
+
+/** \brief Return the number of jiffies corresponding to \param ts, a pointer
+ * to a `struct timespec' denoting a time interval.  */
+#define MA_TIMESPEC_TO_JIFFIES(_ts)							\
+  (JIFFIES_FROM_US (MA_TIMESPEC_TO_USEC (_ts)))

commit 6af4037eb641ac30418776a53bd4096a9cc95eb1

Marcel: Make pthread rwlocks available in Marcel's native API.
This fixes a regression introduced in r21458 ("Marcel: libpthread:
Rewrite rwlocks based on Glibc 2.7 code.").

git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21460 ee107e0b-c2fa-0310-842a-ec2723fb34f4

marcel/include/marcel_rwlock.h
@@ -16,15 +16,16 @@
 
 #section macros
 
-# define MARCEL_RWLOCK_INITIALIZER \
-  { .__rw_lock=MA_MARCEL_FASTLOCK_UNLOCKED, .__rw_readers=0, .__rw_writer=NULL,     \
-    .__rw_read_waiting=NULL, .__rw_write_waiting=NULL,			      \
-    .__rw_kind=MARCEL_RWLOCK_DEFAULT_NP, .__rw_pshared=MARCEL_PROCESS_PRIVATE }
-# define MARCEL_RWLOCK_WRITER_NONRECURSIVE_INITIALIZER_NP \
-  { .__rw_lock=MA_MARCEL_FASTLOCK_UNLOCKED, .__rw_readers=0, .__rw_writer=NULL,     \
-    .__rw_read_waiting=NULL, .__rw_write_waiting=NULL,			      \
-    .__rw_kind=MARCEL_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP, 		      \
-    .__rw_pshared=MARCEL_PROCESS_PRIVATE }
+/** \brief Static initializer for `marcel_rwlock_t' objects.  */
+#define MARCEL_RWLOCK_INITIALIZER				\
+  { { 0, 0,							\
+      MA_LPT_FASTLOCK_UNLOCKED, MA_LPT_FASTLOCK_UNLOCKED,	\
+      0, 0, 0, 0, 0, 0, 0 } }
+
+/* FIXME: `MARCEL_RWLOCK_WRITER_NONRECURSIVE_INITIALIZER_NP' not provided
+ * yet.  Being architecture-dependent, it's not a good fit for this header
+ * (see Glibc's `nptl/sysdep/pthread/pthread.h').  */
+/* #undef MARCEL_RWLOCK_WRITER_NONRECURSIVE_INITIALIZER_NP */
 
 enum
 {
@@ -38,17 +39,7 @@ enum
 #depend "marcel_threads.h[types]"
 
 /* Read-write locks.  */
-typedef struct _marcel_rwlock_t
-{
-  struct _marcel_fastlock __rw_lock; /* Lock to guarantee mutual exclusion */
-  int __rw_readers;                   /* Number of readers */
-  p_marcel_task_t __rw_writer;         /* Identity of writer, or NULL if none */
-  p_marcel_task_t __rw_read_waiting;   /* Threads waiting for reading */
-  p_marcel_task_t __rw_write_waiting;  /* Threads waiting for writing */
-  int __rw_kind;                      /* Reader/Writer preference selection */
-  int __rw_pshared;                   /* Shared between processes or not */
-} marcel_rwlock_t, pmarcel_rwlock_t;
-
+typedef lpt_rwlock_t marcel_rwlock_t, pmarcel_rwlock_t;
 
 /* Attribute for read-write locks (from glibc's
  * `sysdeps/unix/sysv/linux/internaltypes.h').  */

marcel/source/pthread_rwlock.c
@@ -51,56 +51,43 @@ static const struct marcel_rwlockattr default_attr =
   };
 
 
-int
-lpt_rwlockattr_init (attr)
-     lpt_rwlockattr_t *attr;
+DEF_MARCEL_POSIX (int, rwlockattr_init, (marcel_rwlockattr_t *attr), (attr),
 {
-  struct marcel_rwlockattr *iattr;
-
-  iattr = (struct marcel_rwlockattr *) attr;
-
-  iattr->__lockkind = MARCEL_RWLOCK_DEFAULT_NP;
-  iattr->__pshared = MARCEL_PROCESS_PRIVATE;
+  attr->__lockkind = MARCEL_RWLOCK_DEFAULT_NP;
+  attr->__pshared = MARCEL_PROCESS_PRIVATE;
 
   return 0;
-}
+})
 
-int
-lpt_rwlockattr_destroy (attr)
-     lpt_rwlockattr_t *attr;
+DEF_MARCEL_POSIX (int, rwlockattr_destroy, (marcel_rwlockattr_t *attr), (attr),
 {
   /* Nothing to do.  For now.  */
 
   return 0;
-}
+})
 
-int
-lpt_rwlockattr_getkind_np (attr, pref)
-     const lpt_rwlockattr_t *attr;
-     int *pref;
+DEF_MARCEL_POSIX (int, rwlockattr_getkind_np,
+		  (const marcel_rwlockattr_t *attr, int *pref),
+		  (attr, pref),
 {
   *pref = ((const struct marcel_rwlockattr *) attr)->__lockkind;
 
   return 0;
-}
+})
 
-int
-lpt_rwlockattr_getpshared (attr, pshared)
-     const lpt_rwlockattr_t *attr;
-     int *pshared;
+DEF_MARCEL_POSIX (int, rwlockattr_getpshared,
+		  (const marcel_rwlockattr_t *attr, int *pshared),
+		  (attr, pshared),
 {
   *pshared = ((const struct marcel_rwlockattr *) attr)->__pshared;
 
   return 0;
-}
+})
 
-int
-lpt_rwlockattr_setkind_np (attr, pref)
-     lpt_rwlockattr_t *attr;
-     int pref;
+DEF_MARCEL_POSIX (int, rwlockattr_setkind_np,
+		  (marcel_rwlockattr_t *attr, int pref),
+		  (attr, pref),
 {
-  struct marcel_rwlockattr *iattr;
-
   /* FIXME: Check whether the MARCEL_RWLOCK* constants are equal to NPTL's
      PTHREAD_RWLOCK* constants.  */
 
@@ -109,30 +96,25 @@ lpt_rwlockattr_setkind_np (attr, pref)
       && __builtin_expect  (pref != MARCEL_RWLOCK_PREFER_WRITER_NP, 0))
     return EINVAL;
 
-  iattr = (struct marcel_rwlockattr *) attr;
-
-  iattr->__lockkind = pref;
+  attr->__lockkind = pref;
 
   return 0;
-}
+})
 
-int
-lpt_rwlockattr_setpshared (attr, pshared)
-     lpt_rwlockattr_t *attr;
-     int pshared;
+DEF_MARCEL_POSIX (int, rwlockattr_setpshared,
+		  (marcel_rwlockattr_t *attr, int pshared),
+		  (attr, pshared),
 {
-  struct marcel_rwlockattr *iattr;
-
   if (pshared != MARCEL_PROCESS_SHARED
       && __builtin_expect (pshared != MARCEL_PROCESS_PRIVATE, 0))
     return EINVAL;
 
-  iattr = (struct marcel_rwlockattr *) attr;
+  attr = (struct marcel_rwlockattr *) attr;
 
-  iattr->__pshared = pshared;
+  attr->__pshared = pshared;
 
   return 0;
-}
+})
 
 
 /* RW locks.  */
@@ -142,14 +124,13 @@ lpt_rwlockattr_setpshared (attr, pshared)
   ((rwlock)->__data.__flags == 0)
 
 
-int
-lpt_rwlock_init (rwlock, attr)
-     lpt_rwlock_t *rwlock;
-     const lpt_rwlockattr_t *attr;
+DEF_MARCEL_POSIX (int, rwlock_init,
+		  (lpt_rwlock_t *rwlock, const marcel_rwlockattr_t *attr),
+		  (rwlock, attr),
 {
   const struct marcel_rwlockattr *iattr;
 
-  iattr = ((const struct marcel_rwlockattr *) attr) ?: &default_attr;
+  iattr = attr ?: &default_attr;
 
   rwlock->__data.__lock = 0;
   rwlock->__data.__nr_readers = 0;
@@ -195,20 +176,16 @@ lpt_rwlock_init (rwlock, attr)
   rwlock->__data.__pad2 = 0;
 
   return 0;
-}
+})
 
-int
-lpt_rwlock_destroy (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_destroy, (lpt_rwlock_t *rwlock), (rwlock),
 {
   /* Nothing to be done.  For now.  */
   return 0;
-}
+})
 
 /* Acquire read lock for RWLOCK.  */
-int
-lpt_rwlock_rdlock (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_rdlock, (lpt_rwlock_t *rwlock), (rwlock),
 {
   int result = 0;
 
@@ -268,13 +245,12 @@ lpt_rwlock_rdlock (rwlock)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
 /* Try to acquire read lock for RWLOCK or return after specfied time.  */
-int
-lpt_rwlock_timedrdlock (rwlock, abstime)
-     lpt_rwlock_t *rwlock;
-     const struct timespec *abstime;
+DEF_MARCEL_POSIX (int, rwlock_timedrdlock,
+		  (lpt_rwlock_t *rwlock, const struct timespec *abstime),
+		  (rwlock, abstime),
 {
   int result = 0;
 
@@ -377,13 +353,12 @@ lpt_rwlock_timedrdlock (rwlock, abstime)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
 /* Try to acquire write lock for RWLOCK or return after specfied time.	*/
-int
-lpt_rwlock_timedwrlock (rwlock, abstime)
-     lpt_rwlock_t *rwlock;
-     const struct timespec *abstime;
+DEF_MARCEL_POSIX (int, rwlock_timedwrlock,
+		  (lpt_rwlock_t *rwlock, const struct timespec *abstime),
+		  (rwlock, abstime),
 {
   int result = 0;
 
@@ -476,11 +451,9 @@ lpt_rwlock_timedwrlock (rwlock, abstime)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
-int
-lpt_rwlock_tryrdlock (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_tryrdlock, (lpt_rwlock_t *rwlock), (rwlock),
 {
   int result = EBUSY;
 
@@ -502,11 +475,9 @@ lpt_rwlock_tryrdlock (rwlock)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
-int
-lpt_rwlock_trywrlock (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_trywrlock, (lpt_rwlock_t *rwlock), (rwlock),
 {
   int result = EBUSY;
 
@@ -521,12 +492,10 @@ lpt_rwlock_trywrlock (rwlock)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
 /* Unlock RWLOCK.  */
-int
-lpt_rwlock_unlock (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_unlock, (lpt_rwlock_t *rwlock), (rwlock),
 {
   lpt_lock_acquire (&rwlock->__data.__lock);
   if (rwlock->__data.__writer)
@@ -552,13 +521,11 @@ lpt_rwlock_unlock (rwlock)
     }
   lpt_lock_release (&rwlock->__data.__lock);
   return 0;
-}
+})
 
 
 /* Acquire write lock for RWLOCK.  */
-int
-lpt_rwlock_wrlock (rwlock)
-     lpt_rwlock_t *rwlock;
+DEF_MARCEL_POSIX (int, rwlock_wrlock, (lpt_rwlock_t *rwlock),  (rwlock),
 {
   int result = 0;
 
@@ -610,29 +577,8 @@ lpt_rwlock_wrlock (rwlock)
   lpt_lock_release (&rwlock->__data.__lock);
 
   return result;
-}
+})
 
-
-/* Aliases.  */
-
-#ifdef MA__LIBPTHREAD
-strong_alias (lpt_rwlockattr_init, pthread_rwlockattr_init)
-strong_alias (lpt_rwlockattr_destroy, pthread_rwlockattr_destroy)
-strong_alias (lpt_rwlockattr_getkind_np, pthread_rwlockattr_getkind_np)
-strong_alias (lpt_rwlockattr_getpshared, pthread_rwlockattr_getpshared)
-strong_alias (lpt_rwlockattr_setkind_np, pthread_rwlockattr_setkind_np)
-strong_alias (lpt_rwlockattr_setpshared, pthread_rwlockattr_setpshared)
-
-strong_alias (lpt_rwlock_init, pthread_rwlock_init)
-strong_alias (lpt_rwlock_destroy, pthread_rwlock_destroy)
-strong_alias (lpt_rwlock_rdlock, pthread_rwlock_rdlock)
-strong_alias (lpt_rwlock_timedrdlock, pthread_rwlock_timedrdlock)
-strong_alias (lpt_rwlock_timedwrlock, pthread_rwlock_timedwrlock)
-strong_alias (lpt_rwlock_tryrdlock, pthread_rwlock_tryrdlock)
-strong_alias (lpt_rwlock_trywrlock, pthread_rwlock_trywrlock)
-strong_alias (lpt_rwlock_unlock, pthread_rwlock_unlock)
-strong_alias (lpt_rwlock_wrlock, pthread_rwlock_wrlock)
-#endif
 
 /*
    Local Variables:
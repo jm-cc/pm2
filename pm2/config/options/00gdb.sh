59bd95c1a403233ef9f753bb9084c92ab562de4b
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-04-20 13:15:33 +0000

    option gdb: propagate changes from r21006 to all modules
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21489 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pm2/config/options/00gdb.sh
+++ pm2/config/options/00gdb.sh
@@ -1,8 +1,12 @@
 # auto generated from generic/config/options/*
+
+# As of GCC 4.2, `-gdwarf-2 -g3' is the recommended way to get macro
+# expansion debugging information from GCC (info "(gdb) Compilation").
+
 if [ "${PM2_ARCH}" = IA64_ARCH -o "${PM2_ARCH}" = X86_64_ARCH ]; then
-  PM2_PM2_CFLAGS_KERNEL="$PM2_PM2_CFLAGS_KERNEL -ggdb2 -fno-omit-frame-pointer"
+  PM2_PM2_CFLAGS_KERNEL="$PM2_PM2_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
 else
-  PM2_PM2_CFLAGS_KERNEL="$PM2_PM2_CFLAGS_KERNEL -ggdb2 -fno-omit-frame-pointer"
+  PM2_PM2_CFLAGS_KERNEL="$PM2_PM2_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
 fi
 if [ "$PM2_SYS" = LINUX_SYS -o "$PM2_SYS" = GNU_SYS ]; then
   if [ "$PM2_PM2_BUILD_DYNAMIC" = yes ]; then

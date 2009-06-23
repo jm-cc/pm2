33329ac3fe7174376f757654e19dfbd6f79dc8a0
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-06-23 12:33:30 +0000

    update nmad's copy of generic options
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21961 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/config/options/00gdb.sh
+++ nmad/config/options/00gdb.sh
@@ -1,3 +1,17 @@
 # auto generated from generic/config/options/*
-PM2_NMAD_CFLAGS_KERNEL="$PM2_NMAD_CFLAGS_KERNEL -g -Wall"
-PM2_NMAD_CFLAGS="$PM2_NMAD_CFLAGS -g -Wall"
+
+# As of GCC 4.2, `-gdwarf-2 -g3' is the recommended way to get macro
+# expansion debugging information from GCC (info "(gdb) Compilation").
+
+if [ "${PM2_ARCH}" = IA64_ARCH -o "${PM2_ARCH}" = X86_64_ARCH ]; then
+  PM2_NMAD_CFLAGS_KERNEL="$PM2_NMAD_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
+else
+  PM2_NMAD_CFLAGS_KERNEL="$PM2_NMAD_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
+fi
+if [ "$PM2_SYS" = LINUX_SYS -o "$PM2_SYS" = GNU_SYS ]; then
+  if [ "$PM2_NMAD_BUILD_DYNAMIC" = yes ]; then
+    PM2_NMAD_EARLY_LDFLAGS_KERNEL="$PM2_NMAD_EARLY_LDFLAGS_KERNEL -rdynamic"
+  else
+    PM2_NMAD_EARLY_LDFLAGS="$PM2_NMAD_EARLY_LDFLAGS -rdynamic"
+  fi
+fi

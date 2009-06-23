61ccd165cc7dcef3dc091b51bd9848df48362bd6
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-06-23 12:37:35 +0000

    also define NDEBUG to keep nmad coherent with other modules
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@21964 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- generic/config/options/00opt.sh
+++ generic/config/options/00opt.sh
@@ -1,2 +1,2 @@
 # auto generated from generic/config/options/*
-PM2_@LIB@_CFLAGS_KERNEL="$PM2_@LIB@_CFLAGS_KERNEL -O6 -DPM2_OPT"
+PM2_@LIB@_CFLAGS_KERNEL="$PM2_@LIB@_CFLAGS_KERNEL -O6 -DPM2_OPT -DNDEBUG"

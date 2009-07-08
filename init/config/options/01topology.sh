f40e07f8a40d2806540cf4dc57695cdeea551c3c
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-07-08 13:21:01 +0000

    Init topology option: add whitespace after the macro name
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22153 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- init/config/options/01topology.sh
+++ init/config/options/01topology.sh
@@ -1,2 +1,2 @@
-PM2_INIT_CFLAGS="$PM2_INIT_CFLAGS -DPM2_TOPOLOGY"`pkg-config --cflags topology`
+PM2_INIT_CFLAGS="$PM2_INIT_CFLAGS -DPM2_TOPOLOGY "`pkg-config --cflags topology`
 PM2_INIT_LIBS="$PM2_INIT_LIBS "`pkg-config --libs --static topology`

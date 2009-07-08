8c6e64450bde597c1be84194cd50a80ca780fb82
nfurmento <nfurmento@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-07-08 14:32:48 +0000

    MaMI Applications: typo
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22163 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- memory/examples/mami/appli/stream.sh
+++ memory/examples/mami/appli/stream.sh
@@ -1,3 +1,3 @@
-!dd=$(date "+%Y_%m_%d_%H:%M")
+dd=$(date "+%Y_%m_%d_%H:%M")
 for e in $(seq 0 9) ; do EXTRAPAGES=$e pm2-load stream > result_${dd}_0$e; done
 for e in $(seq 10 20) ; do EXTRAPAGES=$e pm2-load stream > result_${dd}_$e; done

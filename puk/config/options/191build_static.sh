c89359f852833666a711863ee4f278283ab7d1d9
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-08-05 13:29:04 +0000

    ajout d'options de link static/dynamic/both pour Puk
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22296 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ puk/config/options/191build_static.sh
@@ -0,0 +1,3 @@
+# auto generated from generic/config/options/*
+PM2_PUK_BUILD_STATIC=yes
+PM2_COMMON_PM2_STLIBS="$PM2_COMMON_PM2_STLIBS PUK"
---
+++ puk/config/options/192build_dynamic.sh
@@ -0,0 +1,3 @@
+# auto generated from generic/config/options/*
+PM2_PUK_BUILD_DYNAMIC=yes
+PM2_COMMON_PM2_SHLIBS="$PM2_COMMON_PM2_SHLIBS PUK"

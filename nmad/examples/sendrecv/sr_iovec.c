6d39ef87f08c84d33ebc6d6c5aadad365e354170
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-08-20 14:55:42 +0000

    ajout du test sr_iovec dans la suite de tests l1
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22361 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sendrecv/sr_iovec.c
+++ nmad/examples/sendrecv/sr_iovec.c
@@ -1,74 +1,106 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #include <err.h>
 #include <stdio.h>
-#include <nm_public.h>
-#include <nm_launcher.h>
-#include <nm_sendrecv_interface.h>
 
-static int rank, peer;
-static nm_core_t p_core = NULL;
-static nm_gate_t gate_id = NULL;
+#include "helper.h"
 
+/* symmetrical iovec, n x 4 bytes, shuffled entries
+ * force client -> server as unexpected, server -> client as expected
+ */
+static void nm_test_iovec_symmetrical_regular(nm_gate_t src, int iov_count)
+{
+  int i;
+  int sbuf[iov_count];
+  int rbuf[iov_count];
+  struct iovec riov[iov_count], siov[iov_count];
+  nm_sr_request_t sreq, rreq;
 
-#define IOV_COUNT 10
+  for(i = 0; i < iov_count; i++)
+    {
+      sbuf[i] = iov_count - i - 1;
+      siov[i].iov_base = &sbuf[i];
+      siov[i].iov_len = sizeof(int);
+      
+      rbuf[i] = -1;
+      riov[i].iov_base = &rbuf[iov_count - i - 1];
+      riov[i].iov_len = sizeof(int);
+    }
+
+  if(is_server)
+    {
+      sleep(1);
+      nm_sr_isend_iov(p_core, gate_id, 42, siov, iov_count, &sreq);
+      nm_sr_swait(p_core, &sreq);
+      nm_sr_irecv_iov(p_core, src, 42, riov, iov_count, &rreq);
+    }
+  else
+    {
+      nm_sr_irecv_iov(p_core, src, 42, riov, iov_count, &rreq);
+      nm_sr_isend_iov(p_core, gate_id, 42, siov, iov_count, &sreq);
+      nm_sr_swait(p_core, &sreq);
+    }
+  nm_sr_rwait(p_core, &rreq);
+  for(i = 0; i < iov_count; ++i)
+    {
+      if(rbuf[i] != i)
+	{
+	  printf("data corruption detected- index %d value %d\n", i, rbuf[i]);
+	  abort();
+	}
+    }
+}
 
 int main(int argc, char ** argv)
 {
-	int i;
-	int sbuf[IOV_COUNT];
-	int rbuf[IOV_COUNT];
-	struct iovec riov[IOV_COUNT], siov[IOV_COUNT];
-	nm_sr_request_t sreq, rreq;
+  
+  init(&argc, argv);
+  
+  printf("iovec test, 1x 4 bytes\n");
+  nm_test_iovec_symmetrical_regular(gate_id, 1);
+  printf("  ok.\n");
 
+  printf("iovec test, 1x 4 bytes, anysrc\n");
+  nm_test_iovec_symmetrical_regular(NM_ANY_GATE, 1);
+  printf("  ok.\n");
+  
+  printf("iovec test, 4x 4 bytes\n");
+  nm_test_iovec_symmetrical_regular(gate_id, 4);
+  printf("  ok.\n");
 
-	nm_launcher_init(&argc, argv);
-	nm_launcher_get_core(&p_core);
-	nm_launcher_get_rank(&rank);
-	peer = 1 - rank;
-	nm_launcher_get_gate(peer, &gate_id);
-	
-	
-	for(i=0; i < IOV_COUNT; ++i){
-		sbuf[i] = IOV_COUNT - i - 1;
-		siov[i].iov_base = &sbuf[i];
-		siov[i].iov_len = sizeof(int);
+  printf("iovec test, 4x 4 bytes, anysrc\n");
+  nm_test_iovec_symmetrical_regular(NM_ANY_GATE, 4);
+  printf("  ok.\n");
 
-		rbuf[i] = -1;
-		riov[i].iov_base = &rbuf[IOV_COUNT - i - 1];
-		riov[i].iov_len = sizeof(int);
-	}
+  printf("iovec test, 32x 4 bytes\n");
+  nm_test_iovec_symmetrical_regular(gate_id, 32);
+  printf("  ok.\n");
 
-	if(rank == 0){
-	        sleep(1);
-		if(nm_sr_isend_iov(p_core, gate_id, 42, 
-				   siov, IOV_COUNT, 
-				   &sreq) != NM_ESUCCESS)
-			errx(1, "NM Isend failed \n");
-		while(nm_sr_stest(p_core, &sreq) != NM_ESUCCESS);
-		if(nm_sr_irecv_iov(p_core, gate_id, 42, 
-				   riov, IOV_COUNT, 
-				   &rreq) != NM_ESUCCESS)
-			errx(1, "NM Irecv failed \n");
-		
+  printf("iovec test, 32x 4 bytes, anysrc\n");
+  nm_test_iovec_symmetrical_regular(NM_ANY_GATE, 32);
+  printf("  ok.\n");
 
-	}else{
-		if(nm_sr_isend_iov(p_core, gate_id, 42, 
-				   siov, IOV_COUNT, 
-				   &sreq) != NM_ESUCCESS)
-			errx(1, "NM Isend failed \n");
-		if(nm_sr_irecv_iov(p_core, gate_id, 42, 
-				   riov, IOV_COUNT, 
-				   &rreq) != NM_ESUCCESS)
-			errx(1, "NM Irecv failed \n");
-		while(nm_sr_stest(p_core, &sreq) != NM_ESUCCESS);
-	}
+  printf("iovec test, 1024x 4 bytes\n");
+  nm_test_iovec_symmetrical_regular(gate_id, 1024);
+  printf("  ok.\n");
 
-	nm_sr_rwait(p_core, &rreq);
-	printf("rank = %d\n", rank);
-	for(i=0; i < IOV_COUNT; ++i){
-	  printf("[%i] %d\n", i, rbuf[i]);
-		if(rbuf[i] != i)
-			printf("BUG !!!! index %d value %d\n", i, rbuf[i]);
-	}
+  printf("iovec test, 1024x 4 bytes, anysrc\n");
+  nm_test_iovec_symmetrical_regular(NM_ANY_GATE, 1024);
+  printf("  ok.\n");
 
-	return 0;
+  nmad_exit();
+  return 0;
 }

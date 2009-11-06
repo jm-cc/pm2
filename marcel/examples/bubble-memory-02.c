70f1cace4488b7feb525521c1d2e535e93bf37c5
fbroquedis <fbroquedis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-11-06 10:13:22 +0000

    Adapt the bubble-memory-02 example the same way. (Follow up to commit 22562)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22563 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/bubble-memory-02.c
+++ marcel/examples/bubble-memory-02.c
@@ -30,30 +30,23 @@
 #define NB_BUBBLES 4
 #define THREADS_PER_BUBBLE 4
 
-static unsigned int node_levels[4], expected_result[4];
-static marcel_mutex_t write_lock;
+static unsigned int expected_result[4];
 
 static void *
-thread_entry_point (void *arg) {
+thread_entry_point (void *arg)
+{
   ma_atomic_t *start;
-  unsigned long current_vp = *(unsigned long *) ma_task_stats_get (marcel_self (), ma_stats_last_vp_offset);
-  unsigned int current_node;
-
-  current_node = current_vp / 4;
 
   start = (ma_atomic_t *) arg;
   while (!ma_atomic_read (start));
-
-  marcel_mutex_lock (&write_lock);
-  node_levels[current_node] += 1;
-  marcel_mutex_unlock (&write_lock);
 
   return NULL;
 }
 
 int
-main (int argc, char *argv[]) {
-  int ret;
+main (int argc, char *argv[])
+{
+  int ret = 0;
   unsigned int i;
   char **new_argv;
   marcel_bubble_sched_t *scheduler;
@@ -61,6 +54,9 @@ main (int argc, char *argv[]) {
 
   /* A quad-socket quad-core computer ("aka kwak" (tm)) */
   static const char topology_description[] = "4 4 1 1 1";
+
+  /* We expect each team to be schedule on a different node. */
+  expected_result[0] = expected_result[1] = expected_result[2] = expected_result[3] = 1;
 
   /* Pass the topology description to Marcel.  Yes, it looks hackish to
      communicate with the library via command-line arguments.  */
@@ -72,7 +68,6 @@ main (int argc, char *argv[]) {
   argc += 2;
 
   marcel_init (&argc, new_argv);
-  marcel_mutex_init (&write_lock, NULL);
 
   /* Make sure we're currently testing the memory scheduler. */
   scheduler =
@@ -97,27 +92,41 @@ main (int argc, char *argv[]) {
 
   ((long *) ma_task_stats_get (marcel_self (), ma_stats_memnode_offset))[0] = 1024;
 
-  for (team = 0; team < NB_BUBBLES; team++) {
-    marcel_bubble_init (bubbles + team);
-    marcel_bubble_insertbubble (&marcel_root_bubble, bubbles + team);
-    if (team == 0) {
-      marcel_bubble_inserttask (bubbles + team, marcel_self ());
-    }
-    marcel_attr_setnaturalbubble (&attr, bubbles + team);
+  for (team = 0; team < NB_BUBBLES; team++)
+    {
+      marcel_bubble_init (bubbles + team);
+      marcel_bubble_insertbubble (&marcel_root_bubble, bubbles + team);
+      if (team == 0)
+	marcel_bubble_inserttask (bubbles + team, marcel_self ());
 
-    for (i = team * THREADS_PER_BUBBLE; i < (team + 1) * THREADS_PER_BUBBLE; i++) {
-      /* Note: We can't use `dontsched' since THREAD would not appear
-	 on the runqueue.  */
-      if ((team == 0) && (i == 0)) {
-	continue; /* The main thread has already been created. */
-      }
-      marcel_create (threads + i, &attr, thread_entry_point, &start_signal);
-      ((long *) ma_task_stats_get (threads[i], ma_stats_memnode_offset))[0] = 1024;
+      marcel_attr_setnaturalbubble (&attr, bubbles + team);
+
+      for (i = team * THREADS_PER_BUBBLE; i < (team + 1) * THREADS_PER_BUBBLE; i++)
+	{
+	  /* Note: We can't use `dontsched' since THREAD would not appear
+	     on the runqueue.  */
+	  if ((team == 0) && (i == 0))
+	    continue; /* The main thread has already been created. */
+
+	  marcel_create (threads + i, &attr, thread_entry_point, &start_signal);
+	  ((long *) ma_task_stats_get (threads[i], ma_stats_memnode_offset))[0] = 1024;
+	}
     }
-  }
 
   /* Threads have been created, let's distribute them. */
   marcel_bubble_sched_begin ();
+
+  /* Check the resulting distribution. */
+  for (i = 0; i < 4; i++) 
+    {
+      unsigned count = ma_count_entities_on_rq (&marcel_topo_level (1, i)->rq);
+      if (count != expected_result[i]) 
+	{
+	  printf ("** node %i: expected: %i threads\n", i, expected_result[i]); 
+	  printf ("**          observed: %i threads\n", count);
+	  ret = 1;
+	}
+    }
 
   ma_atomic_inc (&start_signal);
 
@@ -125,29 +134,22 @@ main (int argc, char *argv[]) {
   thread_entry_point (&start_signal);
 
   /* Wait for other threads to end. */
-  for (team = 0; team < NB_BUBBLES; team++) {
-    for (i = team * THREADS_PER_BUBBLE; i < (team + 1) * THREADS_PER_BUBBLE; i++) {
-      if ((team == 0) && (i == 0)) {
-	continue; /* Avoid the main thread */
-      }
-      marcel_join (threads[i], NULL);
+  for (team = 0; team < NB_BUBBLES; team++)
+    {
+      for (i = team * THREADS_PER_BUBBLE; i < (team + 1) * THREADS_PER_BUBBLE; i++)
+	{
+	  if ((team == 0) && (i == 0))
+	    continue; /* Avoid the main thread */
+
+	  marcel_join (threads[i], NULL);
+	}
     }
-  }
 
-  /* We expect each team to be schedule on a different node. */
-  expected_result[0] = expected_result[1] = expected_result[2] = expected_result[3] = 4;
+  if (!ret)
+    printf ("PASS: scheduling entities were distributed as expected\n");
+  else
+    printf ("FAIL: scheduling entities were NOT distributed as expected\n");
 
-  for (i = 0; i < 4; i++) {
-    if (node_levels[i] != expected_result[i]) {
-      printf ("FAILED: Bad distribution.\n");
-      ret = 1;
-      goto end;
-    }
-  }
-  printf ("PASS: scheduling entities were distributed as expected\n");
-  ret = 0;
-
- end:
   marcel_attr_destroy (&attr);
   marcel_end ();
 
@@ -156,7 +158,7 @@ main (int argc, char *argv[]) {
 
 #else /* MM_MAMI_ENABLED */
 #  warning MaMI must be enabled for this program
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
   fprintf(stderr, "'MaMI' disabled in the flavor\n");
 

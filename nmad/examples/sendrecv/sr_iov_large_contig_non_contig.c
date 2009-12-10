084196cb0ce6ebe47c24e30051375b2983713983
ftrahay <ftrahay@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2009-12-10 12:57:05 +0000

    Add a program to test the large contig/non-contig messages case
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22722 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sendrecv/sr_iov_large_contig_non_contig.c
@@ -0,0 +1,138 @@
+#include <err.h>
+#include <stdio.h>
+#include <nm_public.h>
+#include <nm_launcher.h>
+#include <nm_sendrecv_interface.h>
+#include <sys/time.h>
+
+static int rank, peer;
+static nm_session_t p_session = NULL;
+static nm_gate_t gate_id = NULL;
+
+#define SIZE (1024*1024)
+#define NB_IOV 8
+
+void fill_buffer(char* buffer, int len, int offset)
+{
+  int i;
+  for( i=0; i<len; i++) {
+    buffer[i] = 'a' + ((i + offset)%26);
+  }
+}
+
+int check_buffer(char*buffer, int len, int offset)
+{
+  int ret = 0;
+  int i;
+  for( i=0; i<len; i++) {
+    if(buffer[i] != 'a' + ((i + offset)%26)) {
+      printf("Corrupted data at byte #%d (offset %d): car is %c (%d), expected: %c (%d)\n",
+	     i, offset, buffer[i], buffer[i], 'a' + ((i + offset)%26), 'a' + ((i + offset)%26));
+      ret = 1;
+    }
+  }
+  return ret;
+}
+
+int main(int argc, char ** argv)
+{
+  int i;
+  nm_sr_request_t sreq, rreq;
+  
+  nm_launcher_init(&argc, argv);
+  nm_launcher_get_session(&p_session);
+  nm_launcher_get_rank(&rank);
+  peer = 1 - rank;
+  nm_launcher_get_gate(peer, &gate_id);
+    
+  if(rank == 0) {
+    /* server: send/recv non_contiguous messages */
+
+    struct iovec* iov = malloc(NB_IOV * sizeof(struct iovec));
+	  
+    void** buf = malloc(NB_IOV * sizeof(void*));
+    for(i=0; i < NB_IOV; ++i){
+      buf[i]=malloc(SIZE);
+      memset(buf[i], 7, SIZE);
+
+      iov[i].iov_base = buf[i];
+      iov[i].iov_len = SIZE;
+    }
+
+#ifdef DEBUG
+    fprintf(stderr, "### Irecv %d * %d bytes ###\n", NB_IOV, SIZE);
+#endif
+    if(nm_sr_irecv_iov(p_session, gate_id, 42, 
+		       iov, NB_IOV, 
+		       &rreq) != NM_ESUCCESS) {
+      fprintf(stderr, "NM Irecv failed \n");
+      abort();
+    }
+    nm_sr_rwait(p_session, &rreq);
+
+    int cumulated_len = 0;
+    for( i=0; i<NB_IOV; i++) {
+      if(check_buffer(iov[i].iov_base, iov[i].iov_len, cumulated_len))
+	fprintf(stderr, "IOV #%d is corrupted\n", i);
+      cumulated_len += iov[i].iov_len;
+    }
+
+#ifdef DEBUG
+    fprintf(stderr, "### Isend %d * %d bytes ###\n", NB_IOV, SIZE);
+#endif
+    if(nm_sr_isend_iov(p_session, gate_id, 42, 
+		       iov, NB_IOV, 
+		       &sreq) != NM_ESUCCESS) {
+      fprintf(stderr, "NM Isend failed \n");
+      abort();
+    }
+    nm_sr_swait(p_session, &sreq);
+	  
+  } else {
+    /* client: send/recv contiguous messages */
+
+    void *sbuf = malloc(NB_IOV * SIZE);
+    void *rbuf = malloc(NB_IOV * SIZE);
+
+    fill_buffer(sbuf, NB_IOV * SIZE, 0);
+    memset(rbuf, 3, NB_IOV * SIZE);
+#ifdef DEBUG
+    fprintf(stderr, "### Isend %d bytes ###\n", NB_IOV * SIZE);
+#endif
+    if(nm_sr_isend(p_session, gate_id, 42, 
+		   sbuf, NB_IOV * SIZE, 
+		   &sreq) != NM_ESUCCESS) {
+      fprintf(stderr, "NM Isend failed \n");
+      abort();
+    }
+    nm_sr_swait(p_session, &sreq);
+
+#ifdef DEBUG
+    fprintf(stderr, "### Irecv %d bytes ###\n", NB_IOV * SIZE);
+#endif
+    if(nm_sr_irecv(p_session, gate_id, 42, 
+		   rbuf, NB_IOV * SIZE, 
+		   &rreq) != NM_ESUCCESS) {
+      fprintf(stderr, "NM Irecv failed \n");
+      abort();
+    }
+    nm_sr_rwait(p_session, &rreq);
+
+    if(check_buffer(rbuf, NB_IOV * SIZE, 0))
+      fprintf(stderr, "contiguous message is corrupted\n");
+
+  }
+  
+
+  printf("### End of the test ###\n");
+  return 0;
+}
+
+/*
+ * Local variables:
+ *  c-indent-level: 2
+ *  c-basic-offset: 2
+ * End:
+ *
+ * vim: ts=8 sts=2 sw=2 expandtab
+ */

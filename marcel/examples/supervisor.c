030ce142161a14f55edb542daafc81e2740ed98e
oaumage <oaumage@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2010-01-12 17:37:06 +0000

    - simplify test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22797 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/examples/supervisor.c
+++ marcel/examples/supervisor.c
@@ -17,11 +17,17 @@ f (void *_arg TBX_UNUSED) {
 int
 main (int argc, char ** argv) {
 	marcel_t t;
+	marcel_attr_t attr;
+	marcel_vpset_t vpset = MARCEL_VPSET_ZERO;
 	marcel_fprintf (stderr, "supervisor test\n");
 	marcel_init (&argc, argv);
 	marcel_supervisor_init ();
 	marcel_fprintf (stderr, "creating user thread\n");
-	marcel_create (&t, NULL, f, NULL);
+	marcel_attr_init (&attr);
+	marcel_vpset_set (&vpset, 2);
+	marcel_vpset_set (&vpset, 3);
+	marcel_attr_setvpset (&attr, vpset);
+	marcel_create (&t, &attr, f, NULL);
 	marcel_join (t, NULL);
 	marcel_end ();
 	return 0;

jm-cc/pm2

commit 02a529a792d0a07b09b5e5bfde46d00af2b81b49

-- strategies are written with tactics (finally!)    . only strat_default use tactics, others strategies to be done -- compute is_last_chunk flag automatically
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22773 ee107e0b-c2fa-0310-842a-ec2723fb34f4

nmad/strategies/strat_default/nm_so_strat_default.c
@@ -109,31 +109,18 @@ static void strat_default_destroy(void*status)
  *  @return The NM status.
  */
 static int strat_default_pack_ctrl(void*_status,
-                                   struct nm_gate *p_gate,
+                                   struct nm_gate*p_gate,
 				   const union nm_so_generic_ctrl_header *p_ctrl)
 {
-  struct nm_pkt_wrap *p_so_pw = NULL;
   struct nm_so_strat_default*status = _status;
-  int err;
-
-  /* Simply form a new packet wrapper */
-  err = nm_so_pw_alloc_and_fill_with_control(p_ctrl, &p_so_pw);
-  if(err != NM_ESUCCESS)
-    goto out;
-
-  /* Add the control packet to the out_list */
-  tbx_fast_list_add_tail(&p_so_pw->link,
-                &status->out_list);
-
- out:
-  return err;
+  nm_tactic_pack_ctrl(p_ctrl, &status->out_list);
+  return NM_ESUCCESS;
 }
 
 static int strat_default_todo(void* _status, struct nm_gate*p_gate)
 {
   struct nm_so_strat_default*status = _status;
-  struct tbx_fast_list_head *out_list = &(status->out_list);
-  return !(tbx_fast_list_empty(out_list));
+  return !(tbx_fast_list_empty(&status->out_list));
 }
 
 /** Handle a new packet submitted by the user code.
@@ -148,33 +135,22 @@ static int strat_default_todo(void* _status, struct nm_gate*p_gate)
  */
 static int strat_default_pack(void*_status, struct nm_pack_s*p_pack)
 {
-  struct nm_pkt_wrap *p_pw;
   struct nm_so_strat_default*status = _status;
-  const int len = p_pack->len;
 
   if(p_pack->status & NM_PACK_TYPE_CONTIGUOUS)
     {
+      const void*data = p_pack->data;
+      const int len = p_pack->len;
       if(len <= status->nm_so_max_small)
 	{
-	  /* Small packet */
-	  int flags = NM_PW_GLOBAL_HEADER;
 	  if(len <= status->nm_so_copy_on_send_threshold)
-	    flags |= NM_SO_DATA_USE_COPY;
-	  /* Simply form a new packet wrapper and add it to the out_list */
-	  nm_so_pw_alloc_and_fill_with_data(p_pack, p_pack->data, len, 0, tbx_true, flags, &p_pw);
-	  tbx_fast_list_add_tail(&p_pw->link, &status->out_list);
+	    nm_tactic_pack_small_copy(p_pack, data, len, 0, &status->out_list);
+	  else
+	    nm_tactic_pack_small_ref(p_pack, data, len, 0, &status->out_list);
 	}
       else
 	{
-	  /* ** Send large packets through rdv */
-	  /* First allocate a packet wrapper */
-	  nm_so_pw_alloc_and_fill_with_data(p_pack, p_pack->data, len, 0, tbx_true, NM_PW_NOHEADER, &p_pw);
-	  /* Then place it into the list of large pending sends. */
-	  tbx_fast_list_add_tail(&p_pw->link, &p_pack->p_gate->pending_large_send);
-	  /* Finally, generate a RdV request */
-	  union nm_so_generic_ctrl_header ctrl;
-	  nm_so_init_rdv(&ctrl, p_pack, p_pack->len, 0, NM_PROTO_FLAG_LASTCHUNK);
-	  strat_default_pack_ctrl(status, p_pack->p_gate, &ctrl);
+	  nm_tactic_pack_rdv(p_pack, data, len, 0);
 	}
     }
   else if(p_pack->status & NM_PACK_TYPE_IOV)
@@ -182,37 +158,22 @@ static int strat_default_pack(void*_status, struct nm_pack_s*p_pack)
       struct iovec*iov = p_pack->data;
       uint32_t offset = 0;
       int i;
-      for(i = 0; offset < len; i++)
+      for(i = 0; offset < p_pack->len; i++)
 	{
-	  const tbx_bool_t is_last_chunk = (offset + iov[i].iov_len >= len);
-	  const uint8_t flags = is_last_chunk ? NM_PROTO_FLAG_LASTCHUNK : 0;
-	  if(iov[i].iov_len <= status->nm_so_max_small)
+	  const char*data = iov[i].iov_base;
+	  const int len = iov[i].iov_len;
+	  if(len <= status->nm_so_max_small)
 	    {
-	      /* Small packet */
-	      int flags = NM_PW_GLOBAL_HEADER;
-	      if(iov[i].iov_len <= status->nm_so_copy_on_send_threshold)
-		flags |= NM_SO_DATA_USE_COPY;
-	      /* Simply form a new packet wrapper and add it to the out_list */
-	      nm_so_pw_alloc_and_fill_with_data(p_pack, iov[i].iov_base, iov[i].iov_len,
-						offset, is_last_chunk, flags, &p_pw);
-	      tbx_fast_list_add_tail(&p_pw->link, &status->out_list);
+	      if(len <= status->nm_so_copy_on_send_threshold)
+		nm_tactic_pack_small_copy(p_pack, data, len, 0, &status->out_list);
+	      else
+		nm_tactic_pack_small_ref(p_pack, data, len, 0, &status->out_list);
 	    }
 	  else
 	    {
-	      /* Large packets can not be sent immediately : we have to issue a
-		 RdV request. */
-	      /* First allocate a packet wrapper */
-	      nm_so_pw_alloc_and_fill_with_data(p_pack, iov[i].iov_base, iov[i].iov_len,
-						offset, is_last_chunk,
-						NM_PW_NOHEADER, &p_pw);
-	      /* Then place it into the appropriate list of large pending "sends". */
-	      tbx_fast_list_add_tail(&p_pw->link, &p_pack->p_gate->pending_large_send);
-	      /* Finally, generate a RdV request */
-	      union nm_so_generic_ctrl_header ctrl;
-	      nm_so_init_rdv(&ctrl, p_pack, iov[i].iov_len, offset, flags);
-	      strat_default_pack_ctrl(_status, p_pack->p_gate, &ctrl);
+	      nm_tactic_pack_rdv(p_pack, data, len, 0);
 	    }
-	  offset += iov[i].iov_len;
+	  offset += len;
 	}
     }
   else

nmad/core/include/nm_core_inline.h
@@ -179,7 +179,7 @@ static inline void nm_so_pw_alloc_and_fill_with_data(struct nm_pack_s*p_pack, co
 {
   struct nm_pkt_wrap *p_pw;
   nm_so_pw_alloc(flags, &p_pw);
-  nm_so_pw_add_data(p_pw, p_pack, ptr, chunk_len, chunk_offset, is_last_chunk, flags);
+  nm_so_pw_add_data(p_pw, p_pack, ptr, chunk_len, chunk_offset, flags);
   p_pw->chunk_offset = chunk_offset;
   *pp_pw = p_pw;
 }
@@ -233,6 +233,7 @@ static inline void nm_pw_add_contrib(struct nm_pkt_wrap*p_pw, struct nm_pack_s*p
       p_contrib->p_pack = p_pack;
       p_pw->n_contribs++;
     }
+  p_pack->scheduled += len;
 }
 
 

nmad/core/include/nm_core_interface.h
@@ -177,8 +177,9 @@ struct nm_pack_s
 {
   nm_status_t status;
   void*data; /**< actually, char*, struct iovec*, or DLOOP_Segment* depending on pack type (see status) */
-  int len;   /**< cumulated data length */
-  int done;
+  int len;       /**< cumulated data length */
+  int scheduled; /**< cumulated length of data scheduled for sending */
+  int done;      /**< cumulated length of data sent so far */
   nm_gate_t p_gate;
   nm_core_tag_t tag;
   nm_seq_t seq;
@@ -192,6 +193,7 @@ static inline void nm_core_pack_data(nm_core_t p_core, struct nm_pack_s*p_pack,
   p_pack->data   = (void*)data;
   p_pack->len    = len;
   p_pack->done   = 0;
+  p_pack->scheduled = 0;
 }
 
 void nm_core_pack_iov(nm_core_t p_core, struct nm_pack_s*p_pack, const struct iovec*iov, int num_entries);

nmad/core/include/nm_pkt_wrap.h
@@ -191,7 +191,7 @@ int nm_so_pw_split_data(struct nm_pkt_wrap *p_pw,
 			uint32_t offset);
 
 void nm_so_pw_add_data(struct nm_pkt_wrap *p_pw, struct nm_pack_s*p_pack,
-		       const void *data, uint32_t len, uint32_t offset, uint8_t is_last_chunk, int flags);
+		       const void *data, uint32_t len, uint32_t offset, int flags);
 
 int nm_so_pw_add_datatype(struct nm_pkt_wrap *p_pw, struct nm_pack_s*p_pack,
 			  uint32_t len, const struct DLOOP_Segment *segp);

nmad/core/include/nm_private.h
@@ -99,6 +99,7 @@ typedef uint16_t nm_drv_id_t;
 #include "nm_lock.h"
 #include "nm_core_inline.h"
 #include "nm_lock_inline.h"
+#include "nm_tactics.h"
 
 #include "nm_sampling.h"
 

nmad/core/include/nm_tactics.h
@@ -1,0 +1,70 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef NM_TACTICS_H
+#define NM_TACTICS_H
+
+
+/** Pack a generic control header as a new packet wrapper on track #0.
+ */
+static inline void nm_tactic_pack_ctrl(const union nm_so_generic_ctrl_header*p_ctrl,
+				       struct tbx_fast_list_head*out_list)
+{
+  struct nm_pkt_wrap*p_pw = NULL;
+  nm_so_pw_alloc(NM_PW_GLOBAL_HEADER, &p_pw);
+  nm_so_pw_add_control(p_pw, p_ctrl);
+  tbx_fast_list_add_tail(&p_pw->link, out_list);
+}
+
+/** Pack small data into a new packet wrapper on track #0, by copy.
+ */
+static inline void nm_tactic_pack_small_copy(struct nm_pack_s*p_pack, const char*data, int len, int offset,
+					     struct tbx_fast_list_head*out_list)
+{
+  struct nm_pkt_wrap *p_pw = NULL;
+  nm_so_pw_alloc(NM_PW_GLOBAL_HEADER, &p_pw);
+  nm_so_pw_add_data(p_pw, p_pack, data, len, offset, NM_PW_GLOBAL_HEADER | NM_SO_DATA_USE_COPY);
+  tbx_fast_list_add_tail(&p_pw->link, out_list);
+}
+
+/** Pack small data into a new packet wrapper on track #0, by iovec.
+ */
+static inline void nm_tactic_pack_small_ref(struct nm_pack_s*p_pack, const char*data, int len, int offset,
+					    struct tbx_fast_list_head*out_list)
+{
+  struct nm_pkt_wrap *p_pw = NULL;
+  nm_so_pw_alloc(NM_PW_GLOBAL_HEADER, &p_pw);
+  nm_so_pw_add_data(p_pw, p_pack, data, len, offset, NM_PW_GLOBAL_HEADER);
+  tbx_fast_list_add_tail(&p_pw->link, out_list);
+}
+
+/** Pack large data into a new packet wrapper stored as pending large,
+ * and pack a rdv for this data.
+ */
+static inline void nm_tactic_pack_rdv(struct nm_pack_s*p_pack, const char*data, int len, int offset)
+{
+  struct nm_pkt_wrap *p_pw = NULL;
+  nm_so_pw_alloc(NM_PW_NOHEADER, &p_pw);
+  nm_so_pw_add_data(p_pw, p_pack, data, len, offset, NM_PW_NOHEADER);
+  tbx_fast_list_add_tail(&p_pw->link, &p_pack->p_gate->pending_large_send);
+  union nm_so_generic_ctrl_header ctrl;
+  nm_so_init_rdv(&ctrl, p_pack, p_pack->len, 0, NM_PROTO_FLAG_LASTCHUNK);
+  struct puk_receptacle_NewMad_Strategy_s*strategy = &p_pack->p_gate->strategy_receptacle;
+  (*strategy->driver->pack_ctrl)(strategy->_status, p_pack->p_gate, &ctrl);
+}
+
+
+#endif /* NM_TACTICS_H */
+

nmad/core/src/nm_schedule_out.c
@@ -26,6 +26,7 @@ void nm_core_pack_iov(nm_core_t p_core, struct nm_pack_s*p_pack, const struct io
   p_pack->data   = (void*)iov;
   p_pack->len    = nm_so_iov_len(iov, num_entries);
   p_pack->done   = 0;
+  p_pack->scheduled = 0;
 }
 
 void nm_core_pack_datatype(nm_core_t p_core, struct nm_pack_s*p_pack, const struct DLOOP_Segment *segp)
@@ -34,6 +35,7 @@ void nm_core_pack_datatype(nm_core_t p_core, struct nm_pack_s*p_pack, const stru
   p_pack->data   = (void*)segp;
   p_pack->len    = nm_so_datatype_size(segp);
   p_pack->done   = 0;
+  p_pack->scheduled = 0;
 }
 
 int nm_core_pack_send(struct nm_core*p_core, struct nm_pack_s*p_pack, nm_core_tag_t tag, nm_gate_t p_gate,

nmad/core/src/nm_so_pkt_wrap.c
@@ -320,17 +320,23 @@ void nm_so_pw_add_data(struct nm_pkt_wrap *p_pw,
 		       struct nm_pack_s*p_pack,
 		       const void *data, uint32_t len,
 		       uint32_t offset,
-		       uint8_t is_last_chunk,
 		       int flags)
 {
   const nm_core_tag_t tag = p_pack->tag;
   const nm_seq_t seq = p_pack->seq;
   nm_proto_t proto_flags = 0;
   assert(!p_pw->p_unpack);
-  if(is_last_chunk)
-    proto_flags |= NM_PROTO_FLAG_LASTCHUNK;
+
+  /* add the contrib ref to the pw */
+  nm_pw_add_contrib(p_pw, p_pack, len);
+  if(p_pack->scheduled == p_pack->len)
+    {
+      proto_flags |= NM_PROTO_FLAG_LASTCHUNK;
+    }
   if(p_pack->status & NM_PACK_SYNCHRONOUS)
-    proto_flags |= NM_PROTO_FLAG_ACKREQ;
+    {
+      proto_flags |= NM_PROTO_FLAG_ACKREQ;
+    }
 
   if(p_pw->flags & NM_PW_GLOBAL_HEADER) /* ** Data with a global header in v[0] */
     {
@@ -355,8 +361,6 @@ void nm_so_pw_add_data(struct nm_pkt_wrap *p_pw,
       /* ** Add raw data to pw, without header */
       nm_so_pw_add_raw(p_pw, data, len, 0);
     }
-  /* add the contrib ref to the pw */
-  nm_pw_add_contrib(p_pw, p_pack, len);
 }
 
 // function dedicated to the datatypes which do not require a rendezvous

commit fd243e37bf42df790a24acc95eb48910dc05fa0b

fix bug in iovec introduced in commit r22773
git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@22788 ee107e0b-c2fa-0310-842a-ec2723fb34f4

nmad/strategies/strat_default/nm_so_strat_default.c
@@ -165,9 +165,9 @@ static int strat_default_pack(void*_status, struct nm_pack_s*p_pack)
 	  if(len <= status->nm_so_max_small)
 	    {
 	      if(len <= status->nm_so_copy_on_send_threshold)
-		nm_tactic_pack_small_copy(p_pack, data, len, 0, &status->out_list);
+		nm_tactic_pack_small_copy(p_pack, data, len, offset, &status->out_list);
 	      else
-		nm_tactic_pack_small_ref(p_pack, data, len, 0, &status->out_list);
+		nm_tactic_pack_small_ref(p_pack, data, len, offset, &status->out_list);
 	    }
 	  else
 	    {
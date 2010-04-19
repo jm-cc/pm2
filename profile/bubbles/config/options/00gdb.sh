555aff1825427b8c7e82a410bbf6bafb8a63fe24
thibault <thibault@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2010-04-19 19:56:25 +0000

    Add -D@LIB@_GDB for enabling stub variables
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@23316 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- profile/bubbles/config/options/00gdb.sh
+++ profile/bubbles/config/options/00gdb.sh
@@ -3,11 +3,8 @@
 # As of GCC 4.2, `-gdwarf-2 -g3' is the recommended way to get macro
 # expansion debugging information from GCC (info "(gdb) Compilation").
 
-if [ "${PM2_ARCH}" = IA64_ARCH -o "${PM2_ARCH}" = X86_64_ARCH ]; then
-  PM2_BUBBLES_CFLAGS_KERNEL="$PM2_BUBBLES_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
-else
-  PM2_BUBBLES_CFLAGS_KERNEL="$PM2_BUBBLES_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer"
-fi
+PM2_BUBBLES_CFLAGS_KERNEL="$PM2_BUBBLES_CFLAGS_KERNEL -gdwarf-2 -g3 -fno-omit-frame-pointer -DBUBBLES_GDB"
+
 if [ "$PM2_SYS" = LINUX_SYS -o "$PM2_SYS" = GNU_SYS ]; then
   if [ "$PM2_BUBBLES_BUILD_DYNAMIC" = yes ]; then
     PM2_BUBBLES_EARLY_LDFLAGS_KERNEL="$PM2_BUBBLES_EARLY_LDFLAGS_KERNEL -rdynamic"

571a3303458458bbb21878b6befcf6019eafa0bc
ftrahay <ftrahay@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2010-05-18 14:48:41 +0000

    Typo
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@23464 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/interfaces/sendrecv/src/nm_sendrecv_interface.c
+++ nmad/interfaces/sendrecv/src/nm_sendrecv_interface.c
@@ -285,7 +285,7 @@ int nm_sr_rwait(nm_session_t p_session, nm_sr_request_t *p_request)
   nm_sr_flush(p_core);
 #ifdef NMAD_DEBUG
   if(!nm_sr_status_test(&p_request->status, NM_SR_STATUS_RECV_POSTED))
-    TBX_FAILUREF("nm_sr_rwait- req=%p no send posted!\n", p_request);
+    TBX_FAILUREF("nm_sr_rwait- req=%p no recv posted!\n", p_request);
 #endif /* NMAD_DEBUG */
 
   NM_SO_SR_TRACE("request %p completion = %d\n", p_request,

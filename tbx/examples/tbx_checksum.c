c5245b153489a0e0172d2f573d7d3aa09446e052
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2010-10-29 08:16:27 +0000

    add benchmark for checksums
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@24070 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ tbx/examples/tbx_checksum.c
@@ -0,0 +1,67 @@
+/*
+ * PM2: Parallel Multithreaded Machine
+ * Copyright (C) 2010 "the PM2 team" (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tbx.h>
+
+static const struct tbx_checksum_s
+{
+  const char*name;
+  uint32_t (*func)(const void*, size_t);
+} checksums[] =
+  {
+    { .name = "plain32",       .func = &tbx_checksum_plain32 },
+    { .name = "block64",       .func = &tbx_checksum_block64 },
+    { .name = "Adler32",       .func = &tbx_checksum_adler32 },
+    { .name = "Fletcher64",    .func = &tbx_checksum_fletcher64 },
+    { .name = "Jenkins one-at-a-time", .func = &tbx_checksum_jenkins },
+    { .name = "FNV1a",         .func = &tbx_checksum_fnv1a },
+    { .name = "Knuth",         .func = &tbx_checksum_knuth },
+    { .name = "MurmurHash2a",  .func = &tbx_checksum_murmurhash2a },
+    { .name = "MurmurHash64a", .func = &tbx_checksum_murmurhash64a },
+    { .name = "Paul Hsieh SuperFast", .func = &tbx_checksum_hsieh },
+#ifdef __SSE4_2__
+    { .name = "SSE4.2 CRC32",  .func = &tbx_checksum_crc32 },
+#endif
+    { .name = NULL, .func = NULL }
+  };
+
+int main(int argc, char *argv[])
+{
+  tbx_init(&argc, &argv);
+
+  const int max_size = 128 * 1024 * 1024;
+  char*buffer = malloc(max_size);
+  int i;
+  for(i = 0; i < max_size; i++)
+    {
+      buffer[i] = rand();
+    }
+  for(i = 0; checksums[i].name; i++)
+    {
+      struct timespec t1, t2;
+      printf("# %s\n", checksums[i].name);
+      clock_gettime(CLOCK_MONOTONIC, &t1);
+      int checksum = (*checksums[i].func)(buffer, max_size);
+      clock_gettime(CLOCK_MONOTONIC, &t2);
+      double time_usec = (t2.tv_sec - t1.tv_sec) * 1000000.0 + (t2.tv_nsec - t1.tv_nsec) / 1000.0;
+      printf("  checksum = %x; time = %f usec.; %f MB/s\n", (unsigned)checksum, time_usec, max_size/time_usec);
+    }
+
+  tbx_exit();
+  return 0;
+}

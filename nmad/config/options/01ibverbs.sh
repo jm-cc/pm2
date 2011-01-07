d5bf8f857642492ab8d5350c9236dbd315a33cb4
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2011-01-07 10:42:59 +0000

    do not add IBHOME in include path if it is /usr
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@24311 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/config/options/01ibverbs.sh
+++ nmad/config/options/01ibverbs.sh
@@ -18,7 +18,9 @@ PM2_PROTOCOLS="nmad-ibverbs $PM2_PROTOCOLS"
 PM2_NMAD_DRIVERS="ibverbs $PM2_NMAD_DRIVERS"
 PM2_NMAD_CFLAGS="$PM2_NMAD_CFLAGS -march=native"
 PM2_NMAD_CFLAGS="$PM2_NMAD_CFLAGS -DCONFIG_IBVERBS"
-PM2_NMAD_CFLAGS="$PM2_NMAD_CFLAGS -I${IBHOME}/include"
-PM2_NMAD_LD_PATH="$PM2_NMAD_LD_PATH -L${IBLIBPATH} -Wl,-rpath,${IBLIBPATH}"
+if [ "${IBHOME}" != "x/usr" ]; then
+    PM2_NMAD_CFLAGS="$PM2_NMAD_CFLAGS -I${IBHOME}/include"
+    PM2_NMAD_LD_PATH="$PM2_NMAD_LD_PATH -L${IBLIBPATH} -Wl,-rpath,${IBLIBPATH}"
+fi
 PM2_NMAD_LIBS="$PM2_NMAD_LIBS -libverbs"
 PM2_NMAD_COMPONENTS="$PM2_NMAD_COMPONENTS NewMad_ibverbs_bycopy NewMad_ibverbs_rcache NewMad_ibverbs_adaptrdma NewMad_ibverbs_regrdma NewMad_ibverbs_auto NewMad_ibverbs_lr2"

8f025ef29b29a94735b41e74f176bdf1fed7051e
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2011-02-09 09:28:12 +0000

    move leonie to the attic
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@24598 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- leonie/config/options/00debug.sh
+++
@@ -1,3 +0,0 @@
-# auto generated from generic/config/options/*
-PM2_LEONIE_CFLAGS_KERNEL="$PM2_LEONIE_CFLAGS_KERNEL -DPM2DEBUG -DLEONIE_DEBUG"
-
--- leonie/config/options/00noinline.sh
+++
@@ -1,3 +0,0 @@
-# auto generated from generic/config/options/*
-PM2_LEONIE_CFLAGS_KERNEL="$PM2_LEONIE_CFLAGS_KERNEL -fno-inline -DPM2_NOINLINE"
-

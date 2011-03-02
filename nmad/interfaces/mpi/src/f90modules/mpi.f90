f3c2d22caf1ed0884c562fb34484e937cb2a3d57
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2011-03-02 13:00:10 +0000

    add support for Fortran 90 MPI module
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@24723 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/interfaces/mpi/src/f90modules/mpi.f90
@@ -0,0 +1,8 @@
+        MODULE MPI
+!       This module was created by the script CreateChoiceSrc
+        USE MPI_CONSTANTS,                                               &
+     &      BASE_MPI_WTIME => MPI_WTIME, BASE_MPI_WTICK => MPI_WTICK
+!
+        USE MPI1
+
+        END MODULE MPI

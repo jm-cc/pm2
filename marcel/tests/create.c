92c14307dd6d16c6162ed1cd468932932ce4a0a7
ymartin <ymartin@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2011-09-12 11:56:58 +0000

    * remove fomit-frame-pointer * fprofile-arcs and -pg are compiler options
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@25283 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- marcel/tests/create.c
+++ marcel/tests/create.c
@@ -1,7 +1,6 @@
-
 /*
  * PM2: Parallel Multithreaded Machine
- * Copyright (C) 2001 "the PM2 team" (see AUTHORS file)
+ * Copyright (C) 2010, 2011 "the PM2 team" (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,65 +14,24 @@
  */
 
 #include "marcel.h"
-#include <stdio.h>
-#include <sys/time.h>
 
-
-tbx_tick_t t1, t2;
-
-
-static
-any_t f(any_t arg)
+static void *do_nothing(void *arg)
 {
-	marcel_sem_V((marcel_sem_t *) arg);
-#ifdef VERBOSE_TESTS
-	marcel_fprintf(stderr, "Hi! I'm on LWP %d\n", marcel_current_vp());
-#endif
-	return NULL;
+	return arg;
 }
 
-static
-any_t main_thread(void *arg)
+int main(int argc, char *argv[])
 {
-	marcel_attr_t attr;
-	marcel_sem_t sem;
-	long nb;
-	int essais = 3;
+	int i;
+	pmarcel_t t;
 
-	marcel_detach(marcel_self());
-	marcel_attr_init(&attr);
-	marcel_attr_setdetachstate(&attr, tbx_true);
-#ifdef MA__LWPS
-	marcel_attr_setvpset(&attr, MARCEL_VPSET_VP(0));
-#endif
-
-	marcel_sem_init(&sem, 0);
-
-	while (essais--) {
-		nb = (long) arg;
-
-		while (nb--) {
-			TBX_GET_TICK(t1);
-			marcel_create(NULL, &attr, f, (any_t) & sem);
-			marcel_sem_P(&sem);
-			TBX_GET_TICK(t2);
-		}
-		marcel_printf("one thread: time =  %fus\n", TBX_TIMING_DELAY(t1, t2));
-	}
-
-	return NULL;
-}
-
-int marcel_main(int argc, char *argv[])
-{
-	unsigned long nb = 100000;
-	marcel_attr_t attr;
-
+	i = 0;
 	marcel_init(&argc, argv);
-	marcel_attr_init(&attr);
-	marcel_attr_setvpset(&attr, MARCEL_VPSET_VP(0));
-	marcel_create(NULL, &attr, main_thread, (void *)nb);
+	while (i < 800) {
+		pmarcel_create(&t, NULL, do_nothing, NULL);
+		pmarcel_join(t, NULL);
+		//printf("%d\n", i);
+		i ++;
+	}
 	marcel_end();
-
-	return 0;
 }

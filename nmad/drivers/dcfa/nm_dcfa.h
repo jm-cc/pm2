e30b0fcbfdd8c9eecdd6ff17115b3c1dbc30bac4
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2013-05-28 06:29:01 +0000

    add preliminary dcfa driver for MIC
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@25618 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/drivers/dcfa/nm_dcfa.h
@@ -0,0 +1,297 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2010-2013 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/* -*- Mode: C; tab-width: 2; c-basic-offset: 2 -*- */
+
+#ifndef NM_DCFA_H
+#define NM_DCFA_H
+
+#include <nm_private.h>
+#include <dcfa.h>
+
+/* *** global IB parameters ******************************** */
+
+#define NM_DCFA_PORT         1
+#define NM_DCFA_TX_DEPTH     4
+#define NM_DCFA_RX_DEPTH     2
+#define NM_DCFA_RDMA_DEPTH   4
+#define NM_DCFA_MAX_SG_SQ    1
+#define NM_DCFA_MAX_SG_RQ    1
+#define NM_DCFA_MAX_INLINE   16
+#define NM_DCFA_MTU          IBV_MTU_1024
+
+/** timeout to receive connection check after sending ACK and check (in msec.) */
+#define NM_DCFA_TIMEOUT_CHECK 20
+
+uint32_t nm_dcfa_checksum(const char*data, nm_len_t len);
+uint32_t nm_dcfa_memcpy_and_checksum(void*_dest, const void*_src, nm_len_t len);
+int nm_dcfa_checksum_enabled(void);
+
+extern int nm_dcfa_alignment;
+extern int nm_dcfa_memalign;
+
+/** list of WRIDs used in the driver. */
+enum {
+  _NM_DCFA_WRID_NONE = 0,
+  NM_DCFA_WRID_ACK,    /**< ACK for bycopy */
+  NM_DCFA_WRID_RDMA,   /**< data for bycopy */
+  NM_DCFA_WRID_RDV,    /**< rdv for rcache */
+  NM_DCFA_WRID_DATA,   /**< data for rcache */
+  NM_DCFA_WRID_HEADER, /**< headers for rcache */
+  NM_DCFA_WRID_PACKET, /**< packet for adaptrdma */
+  NM_DCFA_WRID_RECV,
+  NM_DCFA_WRID_SEND,
+  NM_DCFA_WRID_READ,
+  NM_DCFA_WRID_SYNC,   /**< synchronize connection establishment */
+  _NM_DCFA_WRID_MAX
+};
+
+/** an RDMA segment. Each method usually use one segment per connection.
+ */
+struct nm_dcfa_segment 
+{
+  uint64_t raddr;
+  uint32_t rkey;
+};
+
+/** the address for a connection (one per track+gate).
+ */
+struct nm_dcfa_cnx_addr 
+{
+  uint16_t lid;
+  uint32_t qpn;
+  uint32_t psn;
+  struct nm_dcfa_segment segment;
+};
+
+
+/** Global state of the HCA */
+struct nm_dcfa_hca_s
+{
+  struct ibv_device*ib_dev;   /**< IB device */
+  struct ibv_context*context; /**< global IB context */
+  struct ibv_pd*pd;           /**< global IB protection domain */
+  uint16_t lid;               /**< local IB LID */
+  struct
+  {
+    int max_qp;               /**< maximum number of QP */
+    int max_qp_wr;            /**< maximum number of WR per QP */
+    int max_cq;               /**< maximum number of CQ */
+    int max_cqe;              /**< maximum number of entries per CQ */
+    int max_mr;               /**< maximum number of MR */
+    uint64_t max_mr_size;     /**< maximum size for a MR */
+    uint64_t page_size_cap;   /**< maximum page size for device */
+    uint64_t max_msg_size;    /**< maximum message size */
+    int data_rate;
+  } ib_caps;
+};
+
+
+/** an IB connection for a given trk/gate pair */
+struct nm_dcfa_cnx
+{
+  struct nm_dcfa_cnx_addr local_addr;
+  struct nm_dcfa_cnx_addr remote_addr;
+  struct ibv_qp*qp;       /**< QP for this connection */
+  struct ibv_cq*of_cq;    /**< CQ for outgoing packets */
+  struct ibv_cq*if_cq;    /**< CQ for incoming packets */
+  int max_inline;         /**< max size of data for IBV inline RDMA */
+  struct
+  {
+    int total;
+    int wrids[_NM_DCFA_WRID_MAX];
+  } pending;              /**< count of pending packets */
+};
+
+
+/* ********************************************************* */
+
+struct nm_dcfa_hca_s*nm_dcfa_hca_resolve(int index);
+#ifdef PM2_NUIOA
+int nm_dcfa_hca_get_numa_node(struct nm_dcfa_hca_s*p_hca);
+#endif /* PM2_NUIOA */
+void nm_dcfa_hca_get_profile(int index, struct nm_drv_profile_s*p_profile);
+
+struct nm_dcfa_cnx*nm_dcfa_cnx_new(struct nm_dcfa_hca_s*p_hca);
+
+void nm_dcfa_cnx_sync(struct nm_dcfa_cnx*p_ibverbs_cnx);
+
+void nm_dcfa_cnx_connect(struct nm_dcfa_cnx*p_ibverbs_cnx);
+
+
+/* ** RDMA toolbox ***************************************** */
+
+
+/** messages for Work Completion status  */
+static const char*const nm_dcfa_status_strings[] =
+  {
+    [IBV_WC_SUCCESS]            = "success",
+    [IBV_WC_LOC_LEN_ERR]        = "local length error",
+    [IBV_WC_LOC_QP_OP_ERR]      = "local QP op error",
+    [IBV_WC_LOC_EEC_OP_ERR]     = "local EEC op error",
+    [IBV_WC_LOC_PROT_ERR]       = "local protection error",
+    [IBV_WC_WR_FLUSH_ERR]       = "write flush error",
+    [IBV_WC_MW_BIND_ERR]        = "MW bind error",
+    [IBV_WC_BAD_RESP_ERR]       = "bad response error",
+    [IBV_WC_LOC_ACCESS_ERR]     = "local access error",
+    [IBV_WC_REM_INV_REQ_ERR]    = "remote invalid request error",
+    [IBV_WC_REM_ACCESS_ERR]     = "remote access error",
+    [IBV_WC_REM_OP_ERR]         = "remote op error",
+    [IBV_WC_RETRY_EXC_ERR]      = "retry exceded error",
+    [IBV_WC_RNR_RETRY_EXC_ERR]  = "RNR retry exceded error",
+    [IBV_WC_LOC_RDD_VIOL_ERR]   = "local RDD violation error",
+    [IBV_WC_REM_INV_RD_REQ_ERR] = "remote invalid read request error",
+    [IBV_WC_REM_ABORT_ERR]      = "remote abort error",
+    [IBV_WC_INV_EECN_ERR]       = "invalid EECN error",
+    [IBV_WC_INV_EEC_STATE_ERR]  = "invalid EEC state error",
+    [IBV_WC_FATAL_ERR]          = "fatal error",
+    [IBV_WC_RESP_TIMEOUT_ERR]   = "response timeout error",
+    [IBV_WC_GENERAL_ERR]        = "general error"
+  };
+
+#if 0
+static inline int nm_dcfa_do_rdma(struct nm_dcfa_cnx*__restrict__ p_ibverbs_cnx,
+				     const void*__restrict__ buf, int size, uint64_t raddr,
+				     int opcode, int flags, uint32_t lkey, uint32_t rkey, uint64_t wrid)
+{
+  const int offset = buf - (void*)&ib_globals.buffer;
+  struct ibv_sge list = {
+    .mic_addr = (uintptr_t)buf,
+    .addr     =  (uintptr_t)ib_globals.mr->host_addr + offset,
+    .length   = size,
+    .lkey     = lkey
+  };
+  struct ibv_send_wr wr = {
+    .wr_id      = wrid,
+    .sg_list[0] = list,
+    .num_sge    = 1,
+    .opcode     = opcode,
+    .send_flags = flags,
+    .next       = NULL,
+    .imm_data   = 0,
+    .wr.rdma =
+    {
+      .remote_addr = raddr,
+      .rkey        = rkey
+    }
+  };
+  int rc = ibv_post_send(p_ibverbs_cnx->qp, &wr);
+  assert(wrid < _NM_DCFA_WRID_MAX);
+  p_ibverbs_cnx->pending.wrids[wrid]++;
+  p_ibverbs_cnx->pending.total++;
+  if(rc) 
+    {
+      fprintf(stderr, "nmad: FATAL- ibverbs: post RDMA write failed (rc=%d).\n", rc);
+      abort();
+    }
+  return NM_ESUCCESS;
+}
+
+#endif /* 0 */
+
+static inline int nm_dcfa_rdma_send(struct nm_dcfa_cnx*p_ibverbs_cnx, int size,
+				       const void*__restrict__ ptr,
+				       const void*__restrict__ _raddr,
+				       const void*__restrict__ _lbase,
+				       const struct nm_dcfa_segment*seg,
+				       const struct ibv_mr*__restrict__ mr,
+				       int wrid)
+{
+  const uintptr_t _rbase = seg->raddr;
+  const uint64_t raddr   = (uint64_t)(((uintptr_t)_raddr - (uintptr_t)_lbase) + _rbase);
+  struct ibv_sge list = {
+    .mic_addr = (uintptr_t)ptr,
+    .addr = (uintptr_t)mr->host_addr + ((void*)seg - _lbase),
+    .length = size,
+    .lkey   = mr->lkey
+  };
+  struct ibv_send_wr wr = {
+    .wr_id      = wrid,
+    .sg_list[0] = list,
+    .num_sge    = 1,
+    .opcode     = IBV_WR_RDMA_WRITE,
+    .send_flags = (size < p_ibverbs_cnx->max_inline) ? (IBV_SEND_INLINE | IBV_SEND_SIGNALED) : IBV_SEND_SIGNALED,
+    .wr.rdma =
+    {
+      .remote_addr = raddr,
+      .rkey        = seg->rkey
+    }
+  };
+  int rc = ibv_post_send(p_ibverbs_cnx->qp, &wr);
+  assert(wrid < _NM_DCFA_WRID_MAX);
+  p_ibverbs_cnx->pending.wrids[wrid]++;
+  p_ibverbs_cnx->pending.total++;
+  if(rc) 
+    {
+      fprintf(stderr, "nmad: FATAL- ibverbs: post RDMA send failed.\n");
+      abort();
+    }
+  return NM_ESUCCESS;
+}
+
+static inline int nm_dcfa_rdma_poll(struct nm_dcfa_cnx*__restrict__ p_ibverbs_cnx)
+{
+  struct ibv_wc wc;
+  int ne = 0, done = 0;
+  do {
+    ne = ibv_poll_cq(p_ibverbs_cnx->of_cq, 1, &wc);
+    if(ne > 0 && wc.status == IBV_WC_SUCCESS)
+      {
+	assert(wc.wr_id < _NM_DCFA_WRID_MAX);
+	p_ibverbs_cnx->pending.wrids[wc.wr_id]--;
+	p_ibverbs_cnx->pending.total--;
+	done += ne;
+      }
+    else if(ne > 0)
+      {
+	fprintf(stderr, "nmad: FATAL- ibverbs: WC send failed- status=%d (%s)\n", 
+		wc.status, nm_dcfa_status_strings[wc.status]);
+	abort();
+      }
+    else if(ne < 0)
+      {
+	fprintf(stderr, "nmad: FATAL- ibverbs: WC polling failed.\n");
+	abort();
+      }
+  } while(ne > 0);
+  return (done > 0) ? NM_ESUCCESS : -NM_EAGAIN;
+}
+
+static inline void nm_dcfa_send_flush(struct nm_dcfa_cnx*__restrict__ p_ibverbs_cnx, uint64_t wrid)
+{
+  while(p_ibverbs_cnx->pending.wrids[wrid])
+    {
+      nm_dcfa_rdma_poll(p_ibverbs_cnx);
+    }
+}
+
+static inline void nm_dcfa_send_flushn_total(struct nm_dcfa_cnx*__restrict__ p_ibverbs_cnx, int n)
+{
+  while(p_ibverbs_cnx->pending.total > n) {
+    nm_dcfa_rdma_poll(p_ibverbs_cnx);
+  }
+}
+
+static inline void nm_dcfa_send_flushn(struct nm_dcfa_cnx*__restrict__ p_ibverbs_cnx, int wrid, int n)
+{
+  while(p_ibverbs_cnx->pending.wrids[wrid] > n)
+    {
+      nm_dcfa_rdma_poll(p_ibverbs_cnx);
+    }
+}
+
+
+#endif /* NM_DCFA_H */
+
---
+++ nmad/drivers/dcfa/nm_dcfa_bycopy.c
@@ -0,0 +1,425 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2013 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+/* -*- Mode: C; tab-width: 2; c-basic-offset: 2 -*- */
+
+#include <dcfa.h>
+#include <nm_connector.h>
+#include <errno.h>
+
+#include <Padico/Module.h>
+
+#include "nm_dcfa.h"
+
+/* *** method: 'bycopy' ************************************ */
+
+#define NM_DCFA_BYCOPY_BLOCKSIZE 8192
+#define NM_DCFA_BYCOPY_RBUF_NUM  8
+
+#define NM_DCFA_BYCOPY_BUFSIZE     (NM_DCFA_BYCOPY_BLOCKSIZE - sizeof(struct nm_dcfa_bycopy_header))
+#define NM_DCFA_BYCOPY_DATA_SIZE (nm_dcfa_checksum_enabled() ? NM_DCFA_BYCOPY_BUFSIZE : NM_DCFA_BYCOPY_BUFSIZE + sizeof(uint32_t))
+#define NM_DCFA_BYCOPY_SBUF_NUM    2
+#define NM_DCFA_BYCOPY_CREDITS_THR ((NM_DCFA_BYCOPY_RBUF_NUM / 2) + 1)
+
+#define NM_DCFA_BYCOPY_STATUS_EMPTY   0x00  /**< no message in buffer */
+#define NM_DCFA_BYCOPY_STATUS_DATA    0x01  /**< data in buffer (sent by copy) */
+#define NM_DCFA_BYCOPY_STATUS_LAST    0x02  /**< last data block */
+#define NM_DCFA_BYCOPY_STATUS_CREDITS 0x04  /**< message contains credits */
+
+struct nm_dcfa_bycopy_header 
+{
+  uint32_t checksum;       /**< data checksum, if enabled; contains data otherwise */
+  uint16_t offset;         /**< data offset (packet_size = BUFSIZE - offset) */
+  uint8_t  ack;            /**< credits acknowledged */
+  volatile uint8_t status; /**< binary mask- describes the content of the message */
+}  __attribute__((packed));
+
+/** An "on the wire" packet for 'bycopy' minidriver */
+struct nm_dcfa_bycopy_packet 
+{
+  char data[NM_DCFA_BYCOPY_BUFSIZE];
+  struct nm_dcfa_bycopy_header header;
+} __attribute__((packed));
+
+/** Connection state for tracks sending by copy
+ */
+struct nm_dcfa_bycopy
+{
+  struct {
+    struct nm_dcfa_bycopy_packet sbuf[NM_DCFA_BYCOPY_SBUF_NUM];
+    struct nm_dcfa_bycopy_packet rbuf[NM_DCFA_BYCOPY_RBUF_NUM];
+    volatile uint16_t rack, sack;     /**< credits acknowledgement (recv, send) */
+    volatile uint64_t rready, sready; /**< peer is ready (recv, send) */
+  } buffer;
+  
+  struct {
+    uint32_t next_out;  /**< next sequence number for outgoing packet */
+    uint32_t credits;   /**< remaining credits for sending */
+    uint32_t next_in;   /**< cell number of next expected packet */
+    uint32_t to_ack;    /**< credits not acked yet by the receiver */
+  } window;
+  
+  struct {
+    int done;           /**< size of data received so far */
+    int msg_size;       /**< size of whole message */
+    void*buf_posted;    /**< buffer posted for receive */
+    int size_posted;    /**< length of above buffer */
+  } recv;
+  
+  struct {
+    const struct iovec*v;
+    int n;              /**< size of above iovec */
+    int current_packet; /**< current buffer for sending */
+    int msg_size;       /**< total size of message to send */
+    int done;           /**< total amount of data sent */
+    int v_done;         /**< size of current iovec segment already sent */
+    int v_current;      /**< current iovec segment beeing sent */
+  } send;
+  
+  struct nm_dcfa_segment seg; /**< remote segment */
+  struct ibv_mr*mr;           /**< global MR (used for 'buffer') */
+  struct nm_dcfa_cnx*cnx;
+  puk_context_t context;
+};
+
+static void nm_dcfa_bycopy_getprops(int index, struct nm_minidriver_properties_s*props);
+static void nm_dcfa_bycopy_init(puk_context_t context, const void**drv_url, size_t*url_size);
+static void nm_dcfa_bycopy_connect(void*_status, const void*remote_url, size_t url_size);
+static void nm_dcfa_bycopy_send_post(void*_status, const struct iovec*v, int n);
+static int  nm_dcfa_bycopy_send_poll(void*_status);
+static void nm_dcfa_bycopy_recv_init(void*_status,  struct iovec*v, int n);
+static int  nm_dcfa_bycopy_poll_one(void*_status);
+static int  nm_dcfa_bycopy_cancel_recv(void*_status);
+
+static const struct nm_minidriver_iface_s nm_dcfa_bycopy_minidriver =
+  {
+    .getprops    = &nm_dcfa_bycopy_getprops,
+    .init        = &nm_dcfa_bycopy_init,
+    .connect     = &nm_dcfa_bycopy_connect,
+    .send_post   = &nm_dcfa_bycopy_send_post,
+    .send_poll   = &nm_dcfa_bycopy_send_poll,
+    .recv_init   = &nm_dcfa_bycopy_recv_init,
+    .poll_one    = &nm_dcfa_bycopy_poll_one,
+    .cancel_recv = &nm_dcfa_bycopy_cancel_recv
+  };
+
+static void*nm_dcfa_bycopy_instanciate(puk_instance_t instance, puk_context_t context);
+static void nm_dcfa_bycopy_destroy(void*);
+
+static const struct puk_adapter_driver_s nm_dcfa_bycopy_adapter =
+  {
+    .instanciate = &nm_dcfa_bycopy_instanciate,
+    .destroy = &nm_dcfa_bycopy_destroy
+  };
+
+PADICO_MODULE_COMPONENT(NewMad_dcfa_bycopy,
+  puk_component_declare("NewMad_dcfa_bycopy",
+			puk_component_provides("PadicoAdapter", "adapter", &nm_dcfa_bycopy_adapter),
+			puk_component_provides("NewMad_minidriver", "minidriver", &nm_dcfa_bycopy_minidriver)));
+
+
+static void* nm_dcfa_bycopy_instanciate(puk_instance_t instance, puk_context_t context)
+{
+  /* check parameters consistency */
+  assert(sizeof(struct nm_dcfa_bycopy_packet) % 1024 == 0);
+  assert(NM_DCFA_BYCOPY_CREDITS_THR > NM_DCFA_BYCOPY_RBUF_NUM / 2);
+  /* init */
+  struct nm_dcfa_bycopy*bycopy = NULL;
+  if(nm_dcfa_memalign > 0)
+    {
+      posix_memalign((void**)&bycopy, nm_dcfa_memalign, sizeof(struct nm_dcfa_bycopy));
+    }
+  else
+    {
+      bycopy = TBX_MALLOC(sizeof(struct nm_dcfa_bycopy));
+    }
+  memset(&bycopy->buffer, 0, sizeof(bycopy->buffer));
+  bycopy->window.next_out = 1;
+  bycopy->window.next_in  = 1;
+  bycopy->window.credits  = NM_DCFA_BYCOPY_RBUF_NUM;
+  bycopy->window.to_ack   = 0;
+  bycopy->mr              = NULL;
+  bycopy->recv.buf_posted = NULL;
+  bycopy->send.v          = NULL;
+  bycopy->context         = context;
+  return bycopy;
+}
+
+static void nm_dcfa_bycopy_destroy(void*_status)
+{
+  /* TODO */
+}
+
+
+static void nm_dcfa_bycopy_getprops(int index, struct nm_minidriver_properties_s*props)
+{
+  nm_dcfa_hca_get_profile(index, &props->profile);
+}
+
+static void nm_dcfa_bycopy_init(puk_context_t context, const void**drv_url, size_t*url_size)
+{
+  const char*url = NULL;
+  nm_connector_create(sizeof(struct nm_dcfa_cnx_addr), &url);
+  puk_context_putattr(context, "local_url", url);
+  *drv_url = url;
+  *url_size = strlen(url);
+}
+
+
+static void nm_dcfa_bycopy_connect(void*_status, const void*remote_url, size_t url_size)
+{
+  struct nm_dcfa_bycopy*bycopy = _status;
+  const char*s_index = puk_context_getattr(bycopy->context, "index");
+  const int index= atoi(s_index);
+  struct nm_dcfa_hca_s*p_hca = nm_dcfa_hca_resolve(index);
+  struct nm_dcfa_cnx*p_dcfa_cnx = nm_dcfa_cnx_new(p_hca);
+  bycopy->cnx = p_dcfa_cnx;
+
+  /* register Memory Region */
+  bycopy->mr = ibv_reg_mr(p_hca->pd, &bycopy->buffer, sizeof(bycopy->buffer),
+			  IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_LOCAL_WRITE);
+  if(bycopy->mr == NULL)
+    {
+      const int err = errno;
+      TBX_FAILUREF("Infiniband: bycopy cannot register MR (errno = %d; %s).\n",
+		   err, strerror(err));
+    }
+
+  struct nm_dcfa_segment*seg = &p_dcfa_cnx->local_addr.segment;
+  seg->raddr = (uintptr_t)&bycopy->buffer;
+  seg->rkey  = bycopy->mr->rkey;
+  /* ** exchange addresses */
+  const char*local_url = puk_context_getattr(bycopy->context, "local_url");
+  int rc = nm_connector_exchange(local_url, remote_url,
+				 &p_dcfa_cnx->local_addr, &p_dcfa_cnx->remote_addr);
+  if(rc)
+    {
+      fprintf(stderr, "nmad: FATAL- dcfa: timeout in address exchange.\n");
+    }
+  bycopy->seg = p_dcfa_cnx->remote_addr.segment;
+  bycopy->buffer.rready = 0;
+  nm_dcfa_cnx_connect(p_dcfa_cnx);
+  /*
+  bycopy->buffer.sready = 1;
+  while(!bycopy->buffer.rready)
+    {
+      nm_dcfa_sync_send((void*)&bycopy->buffer.sready, (void*)&bycopy->buffer.rready, sizeof(bycopy->buffer.sready), p_dcfa_cnx);
+    }
+  */
+}
+
+
+
+/* ** bycopy I/O ******************************************* */
+
+static void nm_dcfa_bycopy_send_post(void*_status, const struct iovec*v, int n)
+{
+  struct nm_dcfa_bycopy*bycopy = _status;
+  bycopy->send.v              = v;
+  bycopy->send.n              = n;
+  bycopy->send.current_packet = 0;
+  bycopy->send.msg_size       = 0;
+  bycopy->send.done           = 0;
+  bycopy->send.v_done         = 0;
+  bycopy->send.v_current      = 0;
+  int i;
+  for(i = 0; i < bycopy->send.n; i++)
+    {
+      bycopy->send.msg_size += bycopy->send.v[i].iov_len;
+    }
+}
+
+static int nm_dcfa_bycopy_send_poll(void*_status)
+{
+  struct nm_dcfa_bycopy*__restrict__ bycopy = _status;
+  while(bycopy->send.done < bycopy->send.msg_size)
+    { 
+      /* 1- check credits */
+      const int rack = bycopy->buffer.rack;
+      if(rack) {
+	bycopy->window.credits += rack;
+	bycopy->buffer.rack = 0;
+      }
+      if(bycopy->window.credits <= 1)
+	{
+	  int j;
+	  for(j = 0; j < NM_DCFA_BYCOPY_RBUF_NUM; j++)
+	    {
+	      if(bycopy->buffer.rbuf[j].header.status)
+		{
+		  bycopy->window.credits += bycopy->buffer.rbuf[j].header.ack;
+		  bycopy->buffer.rbuf[j].header.ack = 0;
+		  bycopy->buffer.rbuf[j].header.status &= ~NM_DCFA_BYCOPY_STATUS_CREDITS;
+		}
+	    }
+	}
+      if(bycopy->window.credits <= 1) 
+	{
+	  goto wouldblock;
+	}
+      
+      /* 2- check window availability */
+      nm_dcfa_rdma_poll(bycopy->cnx);
+      if(bycopy->cnx->pending.wrids[NM_DCFA_WRID_RDMA] >= NM_DCFA_BYCOPY_SBUF_NUM)
+	{
+	  goto wouldblock;
+	}
+      
+      /* 3- prepare and send packet */
+      struct nm_dcfa_bycopy_packet*__restrict__ packet = &bycopy->buffer.sbuf[bycopy->send.current_packet];
+      const int remaining = bycopy->send.msg_size - bycopy->send.done;
+      const int offset = (remaining > NM_DCFA_BYCOPY_DATA_SIZE) ? 0 : (NM_DCFA_BYCOPY_DATA_SIZE - remaining);
+      int available   = NM_DCFA_BYCOPY_DATA_SIZE - offset;
+      int packet_size = 0;
+      while((available > 0) &&
+	    (bycopy->send.v_current < bycopy->send.n))
+	{
+	  const int v_remaining = bycopy->send.v[bycopy->send.v_current].iov_len - bycopy->send.v_done;
+	  const int fragment_size = (v_remaining > available) ? available : v_remaining;
+	  memcpy(&packet->data[offset + packet_size],
+		 bycopy->send.v[bycopy->send.v_current].iov_base + bycopy->send.v_done, fragment_size);
+	  packet_size += fragment_size;
+	  available   -= fragment_size;
+	  bycopy->send.v_done += fragment_size;
+	  if(bycopy->send.v_done >= bycopy->send.v[bycopy->send.v_current].iov_len) 
+	    {
+	      bycopy->send.v_current++;
+	      bycopy->send.v_done = 0;
+	    }
+	}
+      assert(NM_DCFA_BYCOPY_DATA_SIZE - offset == packet_size);
+      
+      packet->header.offset = offset;
+      packet->header.ack    = bycopy->window.to_ack;
+      packet->header.status = NM_DCFA_BYCOPY_STATUS_DATA;
+      if(nm_dcfa_checksum_enabled())
+	packet->header.checksum = nm_dcfa_checksum(&packet->data[offset], packet_size);
+      bycopy->window.to_ack = 0;
+      bycopy->send.done += packet_size;
+      if(bycopy->send.done >= bycopy->send.msg_size)
+	packet->header.status |= NM_DCFA_BYCOPY_STATUS_LAST;
+      int padding = 0;
+      const int size = sizeof(struct nm_dcfa_bycopy_packet) - offset;
+      if(nm_dcfa_alignment > 0 && size >= 2048)
+	padding = offset % nm_dcfa_alignment; 
+      nm_dcfa_rdma_send(bycopy->cnx,
+			   sizeof(struct nm_dcfa_bycopy_packet) - offset + padding,
+			   &packet->data[offset - padding],
+			   &bycopy->buffer.rbuf[bycopy->window.next_out].data[offset - padding],
+			   &bycopy->buffer,
+			   &bycopy->seg,
+			   bycopy->mr,
+			   NM_DCFA_WRID_RDMA);
+      bycopy->send.current_packet = (bycopy->send.current_packet + 1) % NM_DCFA_BYCOPY_SBUF_NUM;
+      bycopy->window.next_out     = (bycopy->window.next_out     + 1) % NM_DCFA_BYCOPY_RBUF_NUM;
+      bycopy->window.credits--;
+    }
+  nm_dcfa_rdma_poll(bycopy->cnx);
+  if(bycopy->cnx->pending.wrids[NM_DCFA_WRID_RDMA]) 
+    {
+      goto wouldblock;
+    }
+  return NM_ESUCCESS;
+ wouldblock:
+  return -NM_EAGAIN;
+}
+
+static void nm_dcfa_bycopy_recv_init(void*_status, struct iovec*v, int n)
+{
+  struct nm_dcfa_bycopy*bycopy = _status;
+  assert(bycopy->recv.buf_posted == NULL);
+  bycopy->recv.done        = 0;
+  bycopy->recv.msg_size    = -1;
+  bycopy->recv.buf_posted  = v->iov_base;
+  bycopy->recv.size_posted = v->iov_len;
+}
+
+static int nm_dcfa_bycopy_poll_one(void*_status)
+{
+  int err = -NM_EUNKNOWN;
+  int complete = 0;
+  struct nm_dcfa_bycopy*__restrict__ bycopy = _status;
+  struct nm_dcfa_bycopy_packet*__restrict__ packet = &bycopy->buffer.rbuf[bycopy->window.next_in];
+  while( ( (bycopy->recv.msg_size == -1) || (!complete) ) &&
+	 (packet->header.status != 0) ) 
+    {
+      if(bycopy->recv.msg_size == -1) 
+	{
+	  assert((packet->header.status & NM_DCFA_BYCOPY_STATUS_DATA) != 0);
+	  bycopy->recv.msg_size = 0;
+	}
+      complete = (packet->header.status & NM_DCFA_BYCOPY_STATUS_LAST);
+      const int offset = packet->header.offset;
+      const int packet_size = NM_DCFA_BYCOPY_DATA_SIZE - offset;
+      if(nm_dcfa_checksum_enabled())
+	{
+	  const uint32_t checksum = nm_dcfa_checksum(&packet->data[offset], packet_size);
+	  if(checksum != packet->header.checksum)
+	    {
+	      fprintf(stderr, "nmad: FATAL- dcfa: checksum failed- received = %x; expected = %x.\n",
+		      (unsigned)packet->header.checksum, (unsigned)checksum);
+	      abort();
+	    }
+	}
+      memcpy(bycopy->recv.buf_posted + bycopy->recv.done, &packet->data[offset], packet_size);
+      bycopy->recv.msg_size += packet_size;
+      bycopy->recv.done += packet_size;
+      bycopy->window.credits += packet->header.ack;
+      packet->header.ack = 0;
+      packet->header.status = 0;
+      bycopy->window.to_ack++;
+      if(bycopy->window.to_ack > NM_DCFA_BYCOPY_CREDITS_THR) 
+	{
+	  bycopy->buffer.sack = bycopy->window.to_ack;
+	  nm_dcfa_rdma_send(bycopy->cnx,
+			    sizeof(uint16_t),
+			    (void*)&bycopy->buffer.sack,
+			    (void*)&bycopy->buffer.rack,
+			    &bycopy->buffer,
+			    &bycopy->seg,
+			    bycopy->mr,
+			    NM_DCFA_WRID_ACK);
+	  bycopy->window.to_ack = 0;
+	}
+      nm_dcfa_rdma_poll(bycopy->cnx);
+      bycopy->window.next_in = (bycopy->window.next_in + 1) % NM_DCFA_BYCOPY_RBUF_NUM;
+      packet = &bycopy->buffer.rbuf[bycopy->window.next_in];
+    }
+  nm_dcfa_rdma_poll(bycopy->cnx);
+  if((bycopy->recv.msg_size > 0) && complete)
+    {
+      bycopy->recv.buf_posted = NULL;
+      nm_dcfa_send_flush(bycopy->cnx, NM_DCFA_WRID_ACK);
+      err = NM_ESUCCESS;
+    }
+  else 
+    {
+      err = -NM_EAGAIN;
+    }
+  return err;
+}
+
+static int nm_dcfa_bycopy_cancel_recv(void*_status)
+{
+  int err = -NM_EAGAIN;
+  struct nm_dcfa_bycopy*__restrict__ bycopy = _status;
+  if(bycopy->recv.done == 0)
+    {
+      bycopy->recv.buf_posted = NULL;
+      bycopy->recv.size_posted = 0;
+      err = NM_ESUCCESS;
+    }
+  return err;
+}

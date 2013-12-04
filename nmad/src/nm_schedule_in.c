015772ab821f59ec2847347ce2789297fc07fe51
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2013-12-04 08:59:30 +0000

    apply poll min period to all drivers (with and without poll any capability)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@25701 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/src/nm_schedule_in.c
+++ nmad/src/nm_schedule_in.c
@@ -35,29 +35,29 @@ __inline__ int nm_pw_poll_recv(struct nm_pkt_wrap*p_pw)
 	    p_pw->p_gate,
 	    p_pw->p_drv,
 	    p_pw->trk_id);
+  if(p_pw->p_drv->driver->capabilities.min_period > 0)
+    {
+      struct timespec t;
+#ifdef CLOCK_MONOTONIC_RAW
+      const clockid_t clock = CLOCK_MONOTONIC_RAW;
+#else
+      const clockid_t clock = CLOCK_MONOTONIC;
+#endif
+      clock_gettime(clock, &t);
+      if(t.tv_sec < next_poll.tv_sec ||
+	 (t.tv_sec == next_poll.tv_sec && t.tv_nsec < next_poll.tv_nsec))
+	return -NM_EAGAIN;
+      t.tv_nsec += 100 * 1000;
+      if(t.tv_nsec > 1000000000)
+	{
+	  t.tv_nsec -= 1000000000;
+	  t.tv_sec += 1;
+	}
+      next_poll = t;
+    }
   if(p_pw->p_gate)
     {
       struct puk_receptacle_NewMad_Driver_s*r = &p_pw->p_gdrv->receptacle;
-      if(r->driver->capabilities.min_period > 0)
-	{
-	  struct timespec t;
-#ifdef CLOCK_MONOTONIC_RAW
-	  const clockid_t clock = CLOCK_MONOTONIC_RAW;
-#else
-	  const clockid_t clock = CLOCK_MONOTONIC;
-#endif
-	  clock_gettime(clock, &t);
-	  if(t.tv_sec < next_poll.tv_sec ||
-	     (t.tv_sec == next_poll.tv_sec && t.tv_nsec < next_poll.tv_nsec))
-	    return -NM_EAGAIN;
-	  t.tv_nsec += 100 * 1000;
-	  if(t.tv_nsec > 1000000000)
-            {
-              t.tv_nsec -= 1000000000;
-              t.tv_sec += 1;
-            }
-	  next_poll = t;
-	}
       err = r->driver->poll_recv_iov(r->_status, p_pw);
     }
   else

96e1a13118c61e20b03af59f1c6fb86398f08188
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-06-26 09:54:15 +0000

    nmad:   -- add benchmark for data properties computation
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26150 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sendrecv/nm_bench_dataprop.c
@@ -0,0 +1,37 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include "nm_flypack.h"
+#include "nm_bench_generic.h"
+#include <nm_sendrecv_interface.h>
+
+static void nm_bench_dataprop(void*buf, nm_len_t len)
+{
+  struct nm_data_s data;
+  nm_data_flypack_set(&data, (struct flypack_data_s){ .buf = buf, .len = len });
+  nm_len_t newlen;
+  int blocks, is_contig;
+  nm_data_properties_compute(&data, &newlen, &blocks, &is_contig);
+}
+
+const struct nm_bench_s nm_bench =
+  {
+    .name = "local compute data properties for on-the-fly pack",
+    .server = &nm_bench_dataprop,
+    .client = &nm_bench_dataprop,
+    .init   = NULL
+  };
+

b1287a2c676152aac9de148c4b7e9fa69d68c163
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-07-06 14:34:37 +0000

    madmpi:   -- add sparse vector datatype benchmark
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26165 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/benchmarks/mpi_bench_bidir.c
@@ -0,0 +1,34 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+
+static void mpi_bench_bidir(void*buf, size_t len)
+{
+  MPI_Request sreq = MPI_REQUEST_NULL, rreq = MPI_REQUEST_NULL;
+  MPI_Irecv(buf, len, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &rreq);
+  MPI_Isend(buf, len, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &sreq);
+  MPI_Wait(&sreq, MPI_STATUS_IGNORE);
+  MPI_Wait(&rreq, MPI_STATUS_IGNORE);
+}
+
+const struct mpi_bench_s mpi_bench =
+  {
+    .name = "MPI bidir",
+    .server = &mpi_bench_bidir,
+    .client = &mpi_bench_bidir
+  };
+
---
+++ nmad/examples/mpi/benchmarks/mpi_bench_sparse_vector.c
@@ -0,0 +1,48 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+static void*sparse_buf = NULL;
+static MPI_Datatype dtype = MPI_DATATYPE_NULL;
+
+static const int blocksize = 4;
+
+static void mpi_bench_sv_init(void*buf, size_t len)
+{
+  sparse_buf = realloc(sparse_buf, len * 2);
+  int rc = MPI_Type_vector(len / blocksize, blocksize, 2 * blocksize, MPI_CHAR, &dtype);
+}
+
+static void mpi_bench_sv_server(void*buf, size_t len)
+{
+  MPI_Recv(sparse_buf, 1, dtype, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Send(sparse_buf, 1, dtype, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+}
+
+static void mpi_bench_sv_client(void*buf, size_t len)
+{
+  MPI_Send(sparse_buf, 1, dtype, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+  MPI_Recv(sparse_buf, 1, dtype, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+}
+
+const struct mpi_bench_s mpi_bench =
+  {
+    .name   = "MPI sparse vector",
+    .init   = &mpi_bench_sv_init,
+    .server = &mpi_bench_sv_server,
+    .client = &mpi_bench_sv_client
+  };
+

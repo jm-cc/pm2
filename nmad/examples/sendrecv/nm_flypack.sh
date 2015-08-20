4eb8ab2fb81f883d267c0795a7e27b2b2d9bf6ff
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-08-20 14:43:51 +0000

    nmad:   -- tune benchmark parameters
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26183 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sendrecv/nm_flypack.sh
+++ nmad/examples/sendrecv/nm_flypack.sh
@@ -5,14 +5,14 @@ hosts=william0,william1
 
 benches="dataprop flypack_local flypack manpack"
 
-s=2
+s=1
 smax=1024
 
 while [ ${s} -le ${smax} ]; do
     echo "# ## ${s}"
     for b in ${benches}; do
 	echo "# ## start: ${b} [ ${s} ]"
-	padico-launch -n 2 -q -nodelist ${hosts} -DFLYPACK_CHUNK_SIZE=${s} nm_bench_${b} | tee out-flypack/out-${b}-${s}.dat
+	padico-launch -n 2 -q -nodelist ${hosts} -DFLYPACK_CHUNK_SIZE=${s} nm_bench_${b} -N 100 | tee out-flypack/out-${b}-${s}.dat
 	rc=$?
 	echo "# ## rc = ${rc}"
     done

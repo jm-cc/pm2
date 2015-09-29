d014df990a5b48bfc9d3d23d72e007e58b591b5e
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-09-29 09:23:54 +0000

    madmpi:   -- convert old benchmarks (multi, multi_hetero) to new modular bench framework
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26227 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/mpi/benchmarks/mpi_bench_multi.c
@@ -0,0 +1,51 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+#define NB_PACKS 4
+
+static int chunk = 0;
+
+static void mpi_bench_multi_init(void*buf, size_t len)
+{
+  chunk = len / NB_PACKS;
+}
+
+static void mpi_bench_multi_server(void*buf, size_t len)
+{
+  int i;
+  for(i = 0; i < NB_PACKS; i++)
+    MPI_Recv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  for(i = 0; i < NB_PACKS; i++)
+    MPI_Send(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+}
+
+static void mpi_bench_multi_client(void*buf, size_t len)
+{
+  int i;
+  for(i = 0; i < NB_PACKS; i++)
+    MPI_Send(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+  for(i = 0; i < NB_PACKS; i++)
+    MPI_Recv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+}
+
+const struct mpi_bench_s mpi_bench =
+  {
+    .name = "MPI multi ping",
+    .server = &mpi_bench_multi_server,
+    .client = &mpi_bench_multi_client
+  };
+
---
+++ nmad/examples/mpi/benchmarks/mpi_bench_multi_hetero.c
@@ -0,0 +1,50 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+#define SMALL 16
+
+static int small = 0, large = 0;
+
+static void mpi_bench_multi_hetero_init(void*buf, size_t len)
+{
+  small = (len > SMALL) ? SMALL : len;
+  large = len - small;
+}
+
+static void mpi_bench_multi_hetero_server(void*buf, size_t len)
+{
+  MPI_Recv(buf, small, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Recv(buf + small, large, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Send(buf, small, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+  MPI_Send(buf + small, large, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+}
+
+static void mpi_bench_multi_hetero_client(void*buf, size_t len)
+{
+  MPI_Send(buf, small, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+  MPI_Send(buf + small, large, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
+  MPI_Recv(buf, small, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Recv(buf + small, large, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+}
+
+const struct mpi_bench_s mpi_bench =
+  {
+    .name = "MPI heterogeneous multi ping",
+    .server = &mpi_bench_multi_hetero_server,
+    .client = &mpi_bench_multi_hetero_client
+  };
+

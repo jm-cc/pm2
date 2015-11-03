870d225d4a4c6b58807a8e64ec4afc79b3150dcb
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-11-03 16:31:27 +0000

    mpibench:   -- add a frontend for threaded tests
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26309 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ mpibenchmark/mpi_bench_thread.c
@@ -0,0 +1,52 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include "mpi_bench_generic.h"
+
+
+#define MIN_DEFAULT     0
+#define MAX_DEFAULT     (128 * 1024 * 1024)
+#define MULT_DEFAULT    1.5
+#define INCR_DEFAULT    0
+#define LOOPS_DEFAULT   50
+#define LOOPS_REFERENCE 1000
+
+const extern struct mpi_bench_s mpi_bench_sendrecv;
+const extern struct mpi_bench_s mpi_bench_thread_1toN_rr;
+const extern struct mpi_bench_s mpi_bench_thread_1toN_single;
+
+int main(int argc, char	**argv)
+{
+  struct mpi_bench_param_s params =
+    {
+      .start_len   = MIN_DEFAULT,
+      .end_len     = MAX_DEFAULT,
+      .multiplier  = MULT_DEFAULT,
+      .increment   = INCR_DEFAULT,
+      .iterations  = LOOPS_DEFAULT
+    };
+
+  mpi_bench_init(argc, argv, 1);
+  params.iterations = LOOPS_REFERENCE;
+  mpi_bench_run(&mpi_bench_sendrecv, &params);
+  params.iterations = LOOPS_DEFAULT;
+  mpi_bench_run(&mpi_bench_thread_1toN_rr, &params);
+  mpi_bench_run(&mpi_bench_thread_1toN_single, &params);
+  mpi_bench_finalize();
+  exit(0);
+}
+

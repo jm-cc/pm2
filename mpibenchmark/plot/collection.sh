25e1054e8b79671238053adc804b5c1c465702dc
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-11-09 16:12:06 +0000

    mpibench:   -- fix default colors on png graph
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26315 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mpibenchmark/plot/collection.sh
+++ mpibenchmark/plot/collection.sh
@@ -22,6 +22,14 @@ set term pngcairo noenhanced color  fontscale 1 size 1024,768
 set key top left
 set logscale x 2
 set style data linespoints
+
+ set style line 1 lc rgb "red"
+ set style line 2 lc rgb "blue"
+ set style line 3 lc rgb "magenta"
+ set style line 4 lc rgb "cyan"
+ set style line 5 lc rgb "forest-green"
+ set style increment user
+
 set output "mpi_bench_sendrecv.png"
 plot [512:] for [ file in "${sendrecv_dat}" ] file using 1:3 title file
 

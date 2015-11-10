1f15f53c3493a8ad89f68f3b46ac26c702057db4
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-11-10 12:53:59 +0000

    mpibench:   -- add correct label to graphs
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26316 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mpibenchmark/plot/collection.sh
+++ mpibenchmark/plot/collection.sh
@@ -12,6 +12,7 @@ echo "# generating collection.png..."
 gm convert $( for d in ${dirs}; do echo ${d}.d/all.png; done ) -append collection.png
 
 sendrecv_dat="$( ( for d in ${dirs}; do echo ${d}.d/mpi_bench_sendrecv.dat; done ) | tr '\n' ' ' )"
+noncontig_dat="$( ( for d in ${dirs}; do echo ${d}.d/mpi_bench_noncontig.dat; done ) | tr '\n' ' ' )"
 
 echo "# sendrecv = ${sendrecv_dat}"
 
@@ -23,14 +24,21 @@ set key top left
 set logscale x 2
 set style data linespoints
 
- set style line 1 lc rgb "red"
- set style line 2 lc rgb "blue"
- set style line 3 lc rgb "magenta"
- set style line 4 lc rgb "cyan"
- set style line 5 lc rgb "forest-green"
- set style increment user
+set xtics ( "1 KB" 1024, "8 KB" 8*1024, "64 KB" 64*1024, "512 KB" 512*1024, "4 MB" 4*1024*1024, "32 MB" 32*1024*1024 )
+set xlabel "Message size (bytes)"
+set ylabel "Bandwidth (MB/s)"
+
+set style line 1 lc rgb "red"
+set style line 2 lc rgb "blue"
+set style line 3 lc rgb "magenta"
+set style line 4 lc rgb "cyan"
+set style line 5 lc rgb "forest-green"
+set style increment user
 
 set output "mpi_bench_sendrecv.png"
 plot [512:] for [ file in "${sendrecv_dat}" ] file using 1:3 title file
+
+set output "mpi_bench_noncontig.png"
+plot [512:] for [ file in "${noncontig_dat}" ] file using 1:3 title file
 
 EOF

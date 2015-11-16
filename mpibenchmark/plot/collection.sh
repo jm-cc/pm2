79e6cd390fec969e39bc03e68fab3d2f2c1b5a21
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-11-16 16:52:13 +0000

    mpibench:   -- flag to set output directory whean tracing collection graph   -- error checking
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26321 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mpibenchmark/plot/collection.sh
+++ mpibenchmark/plot/collection.sh
@@ -1,15 +1,41 @@
 #! /bin/bash
 
-dirs="$@"
+dirs=
+outdir=
+while [ "x${1}" != "x" ]; do
+    case "${1}" in
+	-o)
+	    outdir="$2"
+	    shift 2
+	    ;;
+	-*)
+	    echo "# invalid parameters $1"
+	    exit 1
+	    ;;
+	*)
+	    d="$1"
+	    if [ ! -d "${d}.d" ]; then
+		echo "# cannot find ${d}.d/"
+		exit 1
+	    fi
+	    dirs="${dirs} ${d}"
+	    shift
+	    ;;
+    esac
+done
 
-if [ "x${dirs}" = "x" ]; then
-    dirs="$(echo *.dat)"
+if [ "x${outdir}" = "x" ]; then
+    echo "# no outdir given."
+    exit 1
 fi
 
 echo "# dirs = ${dirs}"
+echo "# outdir = ${outdir}"
+echo
+mkdir -p ${outdir}
 
 echo "# generating collection.png..."
-gm convert $( for d in ${dirs}; do echo ${d}.d/all.png; done ) -append collection.png
+gm convert $( for d in ${dirs}; do echo ${d}.d/all.png; done ) -append ${outdir}/collection.png
 
 sendrecv_dat="$( ( for d in ${dirs}; do echo ${d}.d/mpi_bench_sendrecv.dat; done ) | tr '\n' ' ' )"
 noncontig_dat="$( ( for d in ${dirs}; do echo ${d}.d/mpi_bench_noncontig.dat; done ) | tr '\n' ' ' )"
@@ -35,10 +61,10 @@ set style line 4 lc rgb "cyan"
 set style line 5 lc rgb "forest-green"
 set style increment user
 
-set output "mpi_bench_sendrecv.png"
+set output "${outdir}/mpi_bench_sendrecv.png"
 plot [512:] for [ file in "${sendrecv_dat}" ] file using 1:3 title file
 
-set output "mpi_bench_noncontig.png"
+set output "${outdir}/mpi_bench_noncontig.png"
 plot [512:] for [ file in "${noncontig_dat}" ] file using 1:3 title file
 
 EOF

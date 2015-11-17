1e75133573d4a2c6d5d7c5427fd70f778ad5ad88
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2015-11-17 10:20:40 +0000

    mpibench:   -- add a frontend for all base benchmarks
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26324 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ mpibenchmark/mpi_bench_base.c
@@ -0,0 +1,57 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include "mpi_bench_generic.h"
+
+
+#define MIN_DEFAULT     0
+#define MAX_DEFAULT     (128 * 1024 * 1024)
+#define MULT_DEFAULT    1.5
+#define INCR_DEFAULT    0
+#define LOOPS_DEFAULT   50
+#define PARAM_DEFAULT   -1
+
+const extern struct mpi_bench_s mpi_bench_sendrecv;
+const extern struct mpi_bench_s mpi_bench_bidir;
+const extern struct mpi_bench_s mpi_bench_send_overhead;
+const extern struct mpi_bench_s mpi_bench_multi;
+const extern struct mpi_bench_s mpi_bench_multi_hetero;
+const extern struct mpi_bench_s mpi_bench_noncontig;
+
+int main(int argc, char	**argv)
+{
+  struct mpi_bench_param_s params =
+    {
+      .start_len   = MIN_DEFAULT,
+      .end_len     = MAX_DEFAULT,
+      .multiplier  = MULT_DEFAULT,
+      .increment   = INCR_DEFAULT,
+      .iterations  = LOOPS_DEFAULT,
+      .param       = PARAM_DEFAULT
+    };
+
+  mpi_bench_init(argc, argv, 0);
+  mpi_bench_run(&mpi_bench_sendrecv, &params);
+  mpi_bench_run(&mpi_bench_bidir, &params);
+  mpi_bench_run(&mpi_bench_send_overhead, &params);
+  mpi_bench_run(&mpi_bench_multi, &params);
+  mpi_bench_run(&mpi_bench_multi_hetero, &params);
+  mpi_bench_run(&mpi_bench_noncontig, &params);
+  mpi_bench_finalize();
+  exit(0);
+}
+

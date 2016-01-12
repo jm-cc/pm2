21a1dd637001b4bf2ca074b3ddb1f234220cbde0
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-01-12 14:34:07 +0000

    mpibenchmark   -- fix copyright   -- add AUTHORS and NEWS
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26375 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mpibenchmark/mpi_bench_thread_NtoN.c
+++ mpibenchmark/mpi_bench_thread_NtoN.c
@@ -1,6 +1,6 @@
 /*
  * NewMadeleine
- * Copyright (C) 2014-2015 (see AUTHORS file)
+ * Copyright (C) 2014-2016 (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

94115e9ede4b870dbecb5e828c2f05a4bdce47c5
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-03-07 10:35:30 +0000

    nmad:   -- narrow scope of variables in example to ease debug
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26432 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/sendrecv/nm_bench_success.c
+++ nmad/examples/sendrecv/nm_bench_success.c
@@ -1,6 +1,6 @@
 /*
  * NewMadeleine
- * Copyright (C) 2015 (see AUTHORS file)
+ * Copyright (C) 2015-2016 (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,42 +20,56 @@ static const nm_tag_t data_tag = 0x01;
 
 static void sr_bench_success_server(void*buf, nm_len_t len)
 {
-  nm_sr_request_t sreq, rreq;
   nm_sr_request_t*p_request = NULL;
-  nm_sr_recv_init(nm_bench_common.p_session, &rreq);
-  nm_sr_recv_unpack_contiguous(nm_bench_common.p_session, &rreq, buf, len);
-  nm_sr_request_set_completion_queue(nm_bench_common.p_session, &rreq);
-  nm_sr_recv_irecv(nm_bench_common.p_session, &rreq, nm_bench_common.p_gate, data_tag, NM_TAG_MASK_FULL);
-  while(nm_sr_recv_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
-    {
-    };
-  nm_sr_send_init(nm_bench_common.p_session, &sreq);
-  nm_sr_send_pack_contiguous(nm_bench_common.p_session, &sreq, buf, len);
-  nm_sr_request_set_completion_queue(nm_bench_common.p_session, &sreq);
-  nm_sr_send_isend(nm_bench_common.p_session, &sreq, nm_bench_common.p_gate, data_tag);
-  while(nm_sr_send_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
-    {
-    };
+  {
+    nm_sr_request_t rreq;
+    nm_sr_recv_init(nm_bench_common.p_session, &rreq);
+    nm_sr_recv_unpack_contiguous(nm_bench_common.p_session, &rreq, buf, len);
+    nm_sr_request_set_completion_queue(nm_bench_common.p_session, &rreq);
+    nm_sr_recv_irecv(nm_bench_common.p_session, &rreq, nm_bench_common.p_gate, data_tag, NM_TAG_MASK_FULL);
+    p_request = NULL;
+    while(nm_sr_recv_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
+      {
+      };
+  }
+  {
+    nm_sr_request_t sreq;
+    nm_sr_send_init(nm_bench_common.p_session, &sreq);
+    nm_sr_send_pack_contiguous(nm_bench_common.p_session, &sreq, buf, len);
+    nm_sr_request_set_completion_queue(nm_bench_common.p_session, &sreq);
+    nm_sr_send_isend(nm_bench_common.p_session, &sreq, nm_bench_common.p_gate, data_tag);
+    p_request = NULL;
+    while(nm_sr_send_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
+      {
+      };
+  }
 }
 
 static void sr_bench_success_client(void*buf, nm_len_t len)
 {
-  nm_sr_request_t sreq, rreq;
   nm_sr_request_t*p_request = NULL;
-  nm_sr_send_init(nm_bench_common.p_session, &sreq);
-  nm_sr_send_pack_contiguous(nm_bench_common.p_session, &sreq, buf, len);
-  nm_sr_request_set_completion_queue(nm_bench_common.p_session, &sreq);
-  nm_sr_send_isend(nm_bench_common.p_session, &sreq, nm_bench_common.p_gate, data_tag);
-  while(nm_sr_send_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
-    {
-    };
-  nm_sr_recv_init(nm_bench_common.p_session, &rreq);
-  nm_sr_recv_unpack_contiguous(nm_bench_common.p_session, &rreq, buf, len);
-  nm_sr_request_set_completion_queue(nm_bench_common.p_session, &rreq);
-  nm_sr_recv_irecv(nm_bench_common.p_session, &rreq, nm_bench_common.p_gate, data_tag, NM_TAG_MASK_FULL);
-  while(nm_sr_recv_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
-    {
-    };
+  {
+    nm_sr_request_t sreq;
+    nm_sr_send_init(nm_bench_common.p_session, &sreq);
+    nm_sr_send_pack_contiguous(nm_bench_common.p_session, &sreq, buf, len);
+    nm_sr_request_set_completion_queue(nm_bench_common.p_session, &sreq);
+    nm_sr_send_isend(nm_bench_common.p_session, &sreq, nm_bench_common.p_gate, data_tag);
+    p_request = NULL;
+    while(nm_sr_send_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
+      {
+      };
+  }
+  {
+    nm_sr_request_t rreq;
+    nm_sr_recv_init(nm_bench_common.p_session, &rreq);
+    nm_sr_recv_unpack_contiguous(nm_bench_common.p_session, &rreq, buf, len);
+    nm_sr_request_set_completion_queue(nm_bench_common.p_session, &rreq);
+    nm_sr_recv_irecv(nm_bench_common.p_session, &rreq, nm_bench_common.p_gate, data_tag, NM_TAG_MASK_FULL);
+    p_request = NULL;
+    while(nm_sr_recv_success(nm_bench_common.p_session, &p_request) != NM_ESUCCESS)
+      {
+      };
+  }
 }
 
 const struct nm_bench_s nm_bench =

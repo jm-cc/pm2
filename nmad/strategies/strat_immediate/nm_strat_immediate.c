289f622855783500d824738c80e17367943639f3
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-09-08 11:38:22 +0000

    nmad:   -- do not return a vlue in a void function      (fixes build with clang)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26901 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/strategies/strat_immediate/nm_strat_immediate.c
+++ nmad/strategies/strat_immediate/nm_strat_immediate.c
@@ -125,7 +125,6 @@ static void strat_immediate_pack_chunk(void*_status, struct nm_req_s*p_pack, voi
       struct nm_pkt_wrap_s*p_pw = nm_pw_alloc_global_header();
       nm_so_pw_add_data_chunk(p_pw, p_pack, ptr, len, chunk_offset, NM_PW_GLOBAL_HEADER | NM_PW_DATA_USE_COPY);
       nm_core_post_send(p_gate, p_pw, NM_TRK_SMALL, p_drv);
-      return NM_ESUCCESS;
     }
   else
     {

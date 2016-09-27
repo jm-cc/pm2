aa64b36d98d7bfd866cef71548399a17492c9cba
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-09-27 13:55:19 +0000

    nmad:   -- doc for rpc interface   -- clarify semantics for get_size
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26937 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/interfaces/rpc/include/nm_rpc_interface.h
+++ nmad/interfaces/rpc/include/nm_rpc_interface.h
@@ -20,13 +20,57 @@
 #include <nm_sendrecv_interface.h>
 #include <nm_session_interface.h>
 
+/** a RPC token given to handlers */
 typedef struct nm_rpc_token_s*nm_rpc_token_t;
 
+/** a RPC registered service */
 typedef struct nm_rpc_service_s*nm_rpc_service_t;
 
+/** a RPC handler called upon request invocation */
 typedef void (*nm_rpc_handler_t)(nm_rpc_token_t p_token);
 
+/** a RPC finalizer, called when all data has been received */
 typedef void (*nm_rpc_finalizer_t)(nm_rpc_token_t p_token);
+
+/** register a new RPC service, listenning on the given tag/tag_mask */
+nm_rpc_service_t nm_rpc_register(nm_session_t p_session, nm_tag_t tag, nm_tag_t tag_mask, nm_len_t hlen,
+				 nm_rpc_handler_t p_handler, nm_rpc_finalizer_t p_finalizer,
+				 void*ref);
+
+/** stop listenning fro RPC requests on given service */
+void nm_rpc_unregister(nm_rpc_service_t p_service);
+
+/** send a RPC request; non-blocking, wait on request for completion */
+void nm_rpc_isend(nm_session_t p_session, nm_sr_request_t*p_request,
+		  nm_gate_t p_gate, nm_tag_t tag,
+		  void*hptr, nm_len_t hlen, struct nm_data_s*p_body);
+
+/** send a RPC request; blocking */
+static inline void nm_rpc_send(nm_session_t p_session, nm_gate_t p_gate, nm_tag_t tag,
+			       void*hptr, nm_len_t hlen, struct nm_data_s*p_body);
+
+/** get pointer to the received header; to be called from a handler */
+static inline void*nm_rpc_get_header(struct nm_rpc_token_s*p_token);
+
+/** get the source for the received request; to be called from a handler */
+static inline nm_gate_t nm_rpc_get_source(struct nm_rpc_token_s*p_token);
+
+/** get the tag for the received request; to be called from a handler */
+static inline nm_tag_t nm_rpc_get_tag(struct nm_rpc_token_s*p_token);
+
+/** get the size of the body for the received request; to be called from a handler */
+static inline nm_len_t nm_rpc_get_size(struct nm_rpc_token_s*p_token);
+
+/** posts the recv for the body of a received request; to be called from a handler;
+ *  data will be actually received in finalizer */
+static inline void nm_rpc_recv_data(struct nm_rpc_token_s*p_token, struct nm_data_s*p_body);
+
+
+/* ********************************************************* */
+/* ** inline */
+
+/** @internal */
+void nm_rpc_data_build(struct nm_data_s*p_rpc_data, void*hptr, nm_len_t hlen, const struct nm_data_s*p_body);
 
 struct nm_rpc_token_s
 {
@@ -47,35 +91,6 @@ struct nm_rpc_service_s
   void*ref;
   struct nm_rpc_token_s token;
 };
-
-
-nm_rpc_service_t nm_rpc_register(nm_session_t p_session, nm_tag_t tag, nm_tag_t tag_mask, nm_len_t hlen,
-				 nm_rpc_handler_t p_handler, nm_rpc_finalizer_t p_finalizer,
-				 void*ref);
-
-void nm_rpc_unregister(nm_rpc_service_t p_service);
-
-void nm_rpc_data_build(struct nm_data_s*p_rpc_data, void*hptr, nm_len_t hlen, const struct nm_data_s*p_body);
-
-void nm_rpc_isend(nm_session_t p_session, nm_sr_request_t*p_request,
-		  nm_gate_t p_gate, nm_tag_t tag,
-		  void*hptr, nm_len_t hlen, struct nm_data_s*p_body);
-
-static inline void nm_rpc_send(nm_session_t p_session, nm_gate_t p_gate, nm_tag_t tag,
-			       void*hptr, nm_len_t hlen, struct nm_data_s*p_body);
-
-static inline void*nm_rpc_get_header(struct nm_rpc_token_s*p_token);
-
-static inline nm_gate_t nm_rpc_get_source(struct nm_rpc_token_s*p_token);
-
-static inline nm_tag_t nm_rpc_get_tag(struct nm_rpc_token_s*p_token);
-
-static inline nm_len_t nm_rpc_get_size(struct nm_rpc_token_s*p_token);
-
-static inline void nm_rpc_recv_data(struct nm_rpc_token_s*p_token, struct nm_data_s*p_body);
-
-
-/* ********************************************************* */
 
 static inline void nm_rpc_send(nm_session_t p_session, nm_gate_t p_gate, nm_tag_t tag,
 			       void*hptr, nm_len_t hlen, struct nm_data_s*p_body)
@@ -108,7 +123,8 @@ static inline nm_len_t nm_rpc_get_size(struct nm_rpc_token_s*p_token)
 {
   nm_len_t size = NM_LEN_UNDEFINED;
   nm_sr_request_get_size(&p_token->request, &size);
-  return size;
+  assert(size >= p_token->p_service->hlen);
+  return size - p_token->p_service->hlen;
 }
 
 static inline void nm_rpc_recv_data(struct nm_rpc_token_s*p_token, struct nm_data_s*p_body)

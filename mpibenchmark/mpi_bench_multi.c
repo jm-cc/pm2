bb7f51308cd22666e517642e902b0b2f6a577ba6
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-10-17 09:27:20 +0000

    mpibenchmark:   -- make number of chunks a parameter in bench multi
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26983 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- mpibenchmark/mpi_bench_multi.c
+++ mpibenchmark/mpi_bench_multi.c
@@ -1,6 +1,6 @@
 /*
  * NewMadeleine
- * Copyright (C) 2015 (see AUTHORS file)
+ * Copyright (C) 2015-2016 (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,39 +15,58 @@
 
 #include "mpi_bench_generic.h"
 
-#define NB_PACKS 4
+static const struct mpi_bench_param_bounds_s param_bounds =
+  {
+    .min  = 2,
+    .max  = 1024,
+    .mult = 2,
+    .incr = 0
+  };
 
+static int nb_packs = 2;
 static int chunk = 0;
+
+static const struct mpi_bench_param_bounds_s*mpi_bench_multi_getparams(void)
+{
+  return &param_bounds;
+}
+
+static void mpi_bench_multi_setparam(int param)
+{
+  nb_packs = param;
+}
 
 static void mpi_bench_multi_init(void*buf, size_t len)
 {
-  chunk = len / NB_PACKS;
+  chunk = len / nb_packs;
 }
 
 static void mpi_bench_multi_server(void*buf, size_t len)
 {
   int i;
-  for(i = 0; i < NB_PACKS; i++)
+  for(i = 0; i < nb_packs; i++)
     MPI_Recv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  for(i = 0; i < NB_PACKS; i++)
+  for(i = 0; i < nb_packs; i++)
     MPI_Send(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
 }
 
 static void mpi_bench_multi_client(void*buf, size_t len)
 {
   int i;
-  for(i = 0; i < NB_PACKS; i++)
+  for(i = 0; i < nb_packs; i++)
     MPI_Send(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD);
-  for(i = 0; i < NB_PACKS; i++)
+  for(i = 0; i < nb_packs; i++)
     MPI_Recv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 }
 
 const struct mpi_bench_s mpi_bench_multi =
   {
-    .label   = "mpi_bench_multi",
-    .name    = "MPI multi ping",
-    .server  = &mpi_bench_multi_server,
-    .client  = &mpi_bench_multi_client,
-    .init    = &mpi_bench_multi_init
+    .label     = "mpi_bench_multi",
+    .name      = "MPI multi ping",
+    .init      = &mpi_bench_multi_init,
+    .server    = &mpi_bench_multi_server,
+    .client    = &mpi_bench_multi_client,
+    .setparam  = &mpi_bench_multi_setparam,
+    .getparams = &mpi_bench_multi_getparams
   };
 

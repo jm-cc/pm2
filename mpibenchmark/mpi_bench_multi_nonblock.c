55692d7580fffc2191fcae7dd92a42b741c2ec00
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2016-10-17 09:29:00 +0000

    mpibenchmark:   -- add a multi bench with non-blocking send/recv
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@26984 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ mpibenchmark/mpi_bench_multi_nonblock.c
@@ -0,0 +1,78 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015-2016 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+static const struct mpi_bench_param_bounds_s param_bounds =
+  {
+    .min  = 2,
+    .max  = 1024,
+    .mult = 2,
+    .incr = 0
+  };
+
+static int nb_packs = 2;
+static int chunk = 0;
+
+static const struct mpi_bench_param_bounds_s*mpi_bench_multi_nonblock_getparams(void)
+{
+  return &param_bounds;
+}
+
+static void mpi_bench_multi_nonblock_setparam(int param)
+{
+  nb_packs = param;
+}
+
+static void mpi_bench_multi_nonblock_init(void*buf, size_t len)
+{
+  chunk = len / nb_packs;
+}
+
+static void mpi_bench_multi_nonblock_server(void*buf, size_t len)
+{
+  MPI_Request reqs[nb_packs];
+  int i;
+  for(i = 0; i < nb_packs; i++)
+    MPI_Irecv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &reqs[i]);
+  MPI_Waitall(nb_packs, reqs, MPI_STATUS_IGNORE);
+  for(i = 0; i < nb_packs; i++)
+    MPI_Isend(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &reqs[i]);
+  MPI_Waitall(nb_packs, reqs, MPI_STATUS_IGNORE);
+}
+
+static void mpi_bench_multi_nonblock_client(void*buf, size_t len)
+{
+  MPI_Request reqs[nb_packs];
+  int i;
+  for(i = 0; i < nb_packs; i++)
+    MPI_Isend(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &reqs[i]);
+  MPI_Waitall(nb_packs, reqs, MPI_STATUS_IGNORE);
+  for(i = 0; i < nb_packs; i++)
+    MPI_Irecv(buf + i * chunk, chunk, MPI_CHAR, mpi_bench_common.peer, 0, MPI_COMM_WORLD, &reqs[i]);
+  MPI_Waitall(nb_packs, reqs, MPI_STATUS_IGNORE);
+}
+
+const struct mpi_bench_s mpi_bench_multi_nonblock =
+  {
+    .label     = "mpi_bench_multi_nonblock",
+    .name      = "MPI multi ping, non-blocking",
+    .init      = &mpi_bench_multi_nonblock_init,
+    .server    = &mpi_bench_multi_nonblock_server,
+    .client    = &mpi_bench_multi_nonblock_client,
+    .setparam  = &mpi_bench_multi_nonblock_setparam,
+    .getparams = &mpi_bench_multi_nonblock_getparams
+  };
+

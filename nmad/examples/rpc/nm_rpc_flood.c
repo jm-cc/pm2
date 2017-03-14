eeafcc3b3fe665f418e8cbf219dd5c7106b43429
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-03-14 10:50:08 +0000

    nmad:   -- move rpc examples into their own directory
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27050 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/rpc/nm_rpc_flood.c
+++ nmad/examples/rpc/nm_rpc_flood.c
@@ -17,8 +17,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "sr_examples_helper.h"
-
+#include <nm_launcher.h>
+#include <nm_coll_interface.h>
 #include <nm_rpc_interface.h>
 
 const char msg[] = "Hello, world!";
@@ -50,15 +50,26 @@ static void rpc_flood_finalizer(nm_rpc_token_t p_token)
 
 int main(int argc, char**argv)
 {
+  nm_launcher_init(&argc, argv);
+  nm_session_t p_session = NULL;
+  nm_launcher_get_session(&p_session);
+  int rank = -1;
+  nm_launcher_get_rank(&rank);
+  int peer = 1 - rank;
+  nm_gate_t p_gate = NULL;
+  nm_launcher_get_gate(peer, &p_gate);
+
+  nm_comm_t p_comm = nm_comm_dup(nm_comm_world());
+
   nm_len_t len = sizeof(nm_len_t) + strlen(msg) + 1;
   struct rpc_flood_header_s header = { .len = len };
   void*buf = malloc(len);
-  nm_examples_init(&argc, argv);
 
   nm_rpc_service_t p_service = nm_rpc_register(p_session, tag, NM_TAG_MASK_FULL, sizeof(struct rpc_flood_header_s),
 					       &rpc_flood_handler, &rpc_flood_finalizer, NULL);
 
-  nm_examples_barrier(0x03);
+  nm_coll_barrier(p_comm, 0xF1);
+
   struct nm_data_s data;
   nm_data_contiguous_build(&data, (void*)msg, len);
   int k;
@@ -77,11 +88,12 @@ int main(int argc, char**argv)
 	  nm_sr_swait(p_session, &reqs[i]);
 	}
     }
-  nm_examples_barrier(0x03);
+
+  nm_coll_barrier(p_comm, 0xF2);
 
   nm_rpc_unregister(p_service);
   free(buf);
-  nm_examples_exit();
+  nm_launcher_exit();
   return 0;
 
 }
--- nmad/examples/rpc/nm_rpc_hello.c
+++ nmad/examples/rpc/nm_rpc_hello.c
@@ -17,8 +17,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "sr_examples_helper.h"
-
+#include <nm_launcher.h>
+#include <nm_coll_interface.h>
 #include <nm_rpc_interface.h>
 
 const char msg[] = "Hello, world!";
@@ -26,7 +26,7 @@ const nm_tag_t tag = 0x02;
 struct rpc_hello_header_s
 {
   nm_len_t len;
-};
+}  __attribute__((packed));
 
 static void rpc_hello_handler(nm_rpc_token_t p_token)
 {
@@ -50,9 +50,19 @@ static void rpc_hello_finalizer(nm_rpc_token_t p_token)
 
 int main(int argc, char**argv)
 {
+  nm_launcher_init(&argc, argv);
+  nm_session_t p_session = NULL;
+  nm_launcher_get_session(&p_session);
+  int rank = -1;
+  nm_launcher_get_rank(&rank);
+  int peer = 1 - rank;
+  nm_gate_t p_gate = NULL;
+  nm_launcher_get_gate(peer, &p_gate);
+
+  nm_comm_t p_comm = nm_comm_dup(nm_comm_world());
+
   nm_len_t len = strlen(msg) + 1;
   struct rpc_hello_header_s header = { .len = len };
-  nm_examples_init(&argc, argv);
 
   nm_rpc_service_t p_service = nm_rpc_register(p_session, tag, NM_TAG_MASK_FULL, sizeof(struct rpc_hello_header_s),
 					       &rpc_hello_handler, &rpc_hello_finalizer, NULL);
@@ -60,10 +70,10 @@ int main(int argc, char**argv)
   struct nm_data_s data;
   nm_data_contiguous_build(&data, (void*)msg, len);
   nm_rpc_send(p_session, p_gate, tag, &header, sizeof(struct rpc_hello_header_s), &data);
-  nm_examples_barrier(0x03);
 
+  nm_coll_barrier(p_comm, 0xF2);
   nm_rpc_unregister(p_service);
-  nm_examples_exit();
+  nm_launcher_exit();
   return 0;
 
 }

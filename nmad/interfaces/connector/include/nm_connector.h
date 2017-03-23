a49ae078bfac76fa5ac1f643b1bcc39e2760312b
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-03-23 11:24:30 +0000

    nmad:   -- doc in the connector interface
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27089 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/interfaces/connector/include/nm_connector.h
+++ nmad/interfaces/connector/include/nm_connector.h
@@ -18,19 +18,27 @@
 #ifndef NM_CONNECTOR_H
 #define NM_CONNECTOR_H
 
+/** @defgroup connector_interface Connector interface
+ * This is the connector interface, an interface designed for drivers to exchange urls.
+ * End-users are not expected to use this interface designed only for nmad internal use.
+ * @{
+ */
+
+
 /** timeout to receive connection ACK after sending connect address to peer (in msec.) */
 #define NM_CONNECTOR_TIMEOUT_ACK   600
 
-/* ********************************************************* */
-/* ** connection management */
-
+/** create a connector object */
 struct nm_connector_s*nm_connector_create(int addr_len, const char**url);
 
+/** dynamically exchange addresses with peer node */
 int nm_connector_exchange(const char*local_connector_url, const char*remote_connector_url,
 			  const void*local_cnx_addr, void*remote_cnx_addr);
 
+/** destroy a connector object */
 void nm_connector_destroy(struct nm_connector_s*p_connector);
 
+/** @} */
 
 #endif /* NM_CONNECTOR_H */
 

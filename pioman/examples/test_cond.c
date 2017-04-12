20767cea79842e58c6911310fb594e3f2ab49ba2
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-04-12 20:59:01 +0000

    pioman:   -- update test_cond test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27139 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pioman/examples/test_cond.c
+++ pioman/examples/test_cond.c
@@ -1,5 +1,3 @@
-#define MARCEL_INTERNAL_INCLUDE
-
 #include <errno.h>
 #include <sys/time.h>
 #include <time.h>
@@ -8,9 +6,8 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#include "pm2_common.h"
-
-#ifdef MARCEL
+#include <pioman.h>
+#include <tbx.h>
 
 /* This program tests the behavior of piom_cond*
  * it performs a pingpong between 2 threads
@@ -19,92 +16,93 @@
 
 static unsigned loops = DEFAULT_LOOPS;
 
-static void usage(void) {
+static void usage(void)
+{
   fprintf(stderr, "-L loops - number of loops [%d]\n", DEFAULT_LOOPS);
 }
 
-piom_cond_t cond[DEFAULT_LOOPS];
-
-marcel_barrier_t barrier;
+static piom_cond_t cond1[DEFAULT_LOOPS], cond2[DEFAULT_LOOPS];
 
 /* compute for usec microsecondes  */
 static void work(unsigned usec)
 {
-	tbx_tick_t t1, t2;
-	TBX_GET_TICK(t1);
-	do{
-		TBX_GET_TICK(t2);
-	} while(TBX_TIMING_DELAY(t1, t2) < usec);
+  tbx_tick_t t1, t2;
+  TBX_GET_TICK(t1);
+  do
+    {
+      TBX_GET_TICK(t2);
+    }
+  while(TBX_TIMING_DELAY(t1, t2) < usec);
 }
 
-static void* th_func(void* arg)
+static void*th_func(void*arg)
 {
-	marcel_barrier_wait(&barrier);
-	int i;
-	for(i=0;i< loops; i++) {
-		piom_cond_wait(&cond[i], 1);
-		/* as soon as the thread is unblocked, the condition may be re-used,
-		 *  for instance, we could call piom_cond_init.
-		 */
-		memset(&cond[i], 0x42, sizeof(cond[i]));
-	}
-	printf("cond_wait ok\n");
-	marcel_barrier_wait(&barrier);
-	for(i=0;i< loops; i++) {
-		while(! piom_cond_test(&cond[i], 1));
-		memset(&cond[i], 0x42, sizeof(cond[i]));
-	}
-	printf("cond_test ok\n");
+  int i;
+  for(i = 0 ; i < loops ; i++)
+    {
+      piom_cond_wait(&cond1[i], 1);
+      /* as soon as the thread is unblocked, the condition may be re-used,
+       *  for instance, we could call piom_cond_init.
+       */
+      memset(&cond1[i], 0x42, sizeof(cond1[i]));
+    }
+  printf("cond_wait ok\n");
+
+  for(i = 0 ; i < loops ; i++)
+    {
+      while(!piom_cond_test(&cond2[i], 1))
+	;
+      memset(&cond2[i], 0x42, sizeof(cond2[i]));
+    }
+  printf("cond_test ok\n");
+  return NULL;
 }
 
-int main(int argc, char ** argv)
+int main(int argc, char**argv)
 {
-	common_init(&argc, argv, NULL);
-	int i;
-	
-	if (argc > 1 && !strcmp(argv[1], "--help")) {
-		usage();
-		goto out;
+  tbx_init(&argc, &argv);
+  pioman_init(&argc, argv);
+  int i;
+  
+  if (argc > 1 && !strcmp(argv[1], "--help"))
+    {
+      usage();
+      goto out;
+    }
+  for(i = 1 ; i < argc ; i += 2)
+    {
+      if (!strcmp(argv[i], "-L"))
+	{
+	  loops = atoi(argv[i+1]);
 	}
-	for(i=1 ; i<argc ; i+=2) {
-		if (!strcmp(argv[i], "-L")) {
-			loops = atoi(argv[i+1]);
-		}
-		else {
-			fprintf(stderr, "Illegal argument %s\n", argv[i]);
-			usage();
-			goto out;
-		}
+      else
+	{
+	  fprintf(stderr, "Illegal argument %s\n", argv[i]);
+	  usage();
+	  goto out;
 	}
+    }
+  for(i = 0 ; i < loops ; i++)
+    {
+      piom_cond_init(&cond1[i], 0);
+      piom_cond_init(&cond2[i], 0);
+    }
+  
+  piom_thread_t tid;
+  piom_thread_create(&tid, NULL, th_func, NULL);
+  
+  for(i = 0 ; i < loops ; i++)
+    {
+      piom_cond_signal(&cond1[i], 1);
+    }
 
-	for(i=0; i< loops; i++)
-		piom_cond_init(&cond[i], 0);
+  for(i = 0 ; i < loops ; i++)
+    {
+      piom_cond_signal(&cond2[i], 1);
+    }
 
-	marcel_barrier_init(&barrier, NULL, 2);
-	
-	marcel_t tid;
-	marcel_create(&tid, NULL, th_func, NULL);
-
-	/* Wait the consumer thread */
-	marcel_barrier_wait(&barrier);	
-	for(i=0;i< loops; i++) {
-		piom_cond_signal(&cond[i], 1);
-	}
-
-	marcel_barrier_wait(&barrier);
-	for(i=0; i< loops; i++)
-		piom_cond_init(&cond[i], 0);
-
-	for(i=0;i< loops; i++) {
-		piom_cond_signal(&cond[i], 1);
-	}
-	
-	marcel_join(tid, NULL);
-out:
-	return 0;
+  piom_thread_join(tid);
+ out:
+  return 0;
 }
 
-#else
-#warning test_cond is only available with Marcel
-int main(){ return 0;}
-#endif

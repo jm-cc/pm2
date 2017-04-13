6ffb54a81a4ba906b6b2d111095fc1733ca37d45
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-04-13 16:21:32 +0000

    pioman:   -- display dispersion in benchmark result
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27150 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pioman/examples/piom_bench.c
+++ pioman/examples/piom_bench.c
@@ -21,14 +21,61 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <math.h>
 
 #include <pioman.h>
 #include <tbx.h>
 
-#define SEM_LOOPS     50000
-#define SPIN_LOOPS    50000
-#define TASKLET_LOOPS 50000
-#define COND_LOOPS    50000
+#define ITERATIONS   1000000
+
+static double*lats = NULL;
+static int iterations = ITERATIONS;
+static int comp_double(const void*_a, const void*_b)
+{
+  const double*a = _a;
+  const double*b = _b;
+  if(*a < *b)
+    return -1;
+  else if(*a > *b)
+    return 1;
+  else
+    return 0;
+}
+static void lats_init(const char*label)
+{
+  int i;
+  if(lats == NULL)
+    {
+      lats = malloc(sizeof(double) * iterations);
+      printf("# %d loops\n", iterations);
+      printf("#  min   \t  med    \t  max    \t  q1    \t  q3    \t  d1    \t   d9   \t   p99  \t  avg\n");
+    }
+  for(i = 0; i < iterations; i++)
+    lats[i] = -1.0;
+  printf("\n%s\n", label);
+}
+static void lats_display(void)
+{
+  qsort(lats, iterations, sizeof(double), &comp_double);
+  const double min_lat = lats[0];
+  const double max_lat = lats[iterations - 1];
+  const double med_lat = lats[(iterations - 1) / 2];
+  const double q1_lat  = lats[(iterations - 1) / 4];
+  const double q3_lat  = lats[ 3 * (iterations - 1) / 4];
+  const double d1_lat  = lats[(iterations - 1) / 10];
+  const double d9_lat  = lats[ 9 * (iterations - 1) / 10];
+  const double p99_lat = lats[ 99 * (iterations - 1) / 100];
+  double avg_lat = 0.0;
+  int k;
+  for(k = 0; k < iterations; k++)
+    {
+      avg_lat += lats[k];
+    }
+  avg_lat /= iterations;
+  printf("%8.3lf\t%8.3f\t%8.3f\t%8.3lf\t%8.3lf\t%8.3lf\t%8.3lf\t%8.3lf\t%8.3f\n",
+	 min_lat, med_lat, max_lat, q1_lat, q3_lat, d1_lat, d9_lat, p99_lat, avg_lat);
+
+}
 
 static piom_cond_t ltask_cond;
 
@@ -53,34 +100,35 @@ int main(int argc, char**argv)
   tbx_tick_t t1, t2;
 
   /* cout d'un sem_V() sem_P() */
+  lats_init("piom_sem_V + P");
   piom_sem_t sem;
   piom_sem_init(&sem, 0);
-  printf("piom_sem_V + P (%d loops):", SEM_LOOPS);
-  TBX_GET_TICK(t1);
-  for(i = 0 ; i < SEM_LOOPS ; i++)
+  for(i = 0 ; i < iterations ; i++)
     {
+      TBX_GET_TICK(t1);
       piom_sem_V(&sem);
       piom_sem_P(&sem);
+      TBX_GET_TICK(t2);
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  TBX_GET_TICK(t2);
-  printf("\t\t\t%f usec.\n", TBX_TIMING_DELAY(t1, t2)/SEM_LOOPS);
-  
+  lats_display();
+
+  lats_init("piom_spin_lock + unlock");
   piom_spinlock_t lock;
   piom_spin_init(&lock);
-  printf("piom_spin_lock + unlock (%d loops):", SPIN_LOOPS);
-  TBX_GET_TICK(t1);
-  for(i = 0 ; i < SPIN_LOOPS ; i++)
+  for(i = 0 ; i < iterations ; i++)
     {
+      TBX_GET_TICK(t1);
       piom_spin_lock(&lock);
       piom_spin_unlock(&lock);
+      TBX_GET_TICK(t2);	
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  TBX_GET_TICK(t2);	
-  printf("\t\t%f usec.\n", TBX_TIMING_DELAY(t1, t2)/SPIN_LOOPS);
-  
+  lats_display();
+
+  lats_init("ltask submit");
   struct piom_ltask ltask;
-  double total = 0.0;
-  printf("ltask submit (%d loops):", TASKLET_LOOPS);
-  for(i = 0 ; i < TASKLET_LOOPS ; i++)
+  for(i = 0 ; i < iterations ; i++)
     {
       TBX_GET_TICK(t1);
       piom_ltask_create(&ltask, &test_ltask_empty, NULL, PIOM_LTASK_OPTION_ONESHOT);
@@ -88,12 +136,12 @@ int main(int argc, char**argv)
       TBX_GET_TICK(t2);
       piom_ltask_wait(&ltask);
       piom_ltask_destroy(&ltask);
-      total += TBX_TIMING_DELAY(t1, t2);
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  printf("\t\t\t%f usec.\n", total / TASKLET_LOOPS);
+  lats_display();
 
-  printf("ltask submit + ltask wait (%d loops):", TASKLET_LOOPS);
-  for(i = 0 ; i < TASKLET_LOOPS ; i++)
+  lats_init("ltask submit + ltask wait");
+  for(i = 0 ; i < iterations ; i++)
     {
       TBX_GET_TICK(t1);
       piom_ltask_create(&ltask, &test_ltask_empty, NULL, PIOM_LTASK_OPTION_ONESHOT);
@@ -101,12 +149,12 @@ int main(int argc, char**argv)
       piom_ltask_wait(&ltask);
       TBX_GET_TICK(t2);
       piom_ltask_destroy(&ltask);
-      total += TBX_TIMING_DELAY(t1, t2);
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  printf("\t%f usec.\n", total / TASKLET_LOOPS);
+  lats_display();
 
-  printf("ltask submit + cond wait (%d loops):", TASKLET_LOOPS);
-  for(i = 0 ; i < TASKLET_LOOPS ; i++)
+  lats_init("ltask submit + cond wait");
+  for(i = 0 ; i < iterations ; i++)
     {
       piom_cond_init(&ltask_cond, 0);
       TBX_GET_TICK(t1);
@@ -116,13 +164,13 @@ int main(int argc, char**argv)
       TBX_GET_TICK(t2);
       piom_ltask_wait(&ltask);
       piom_ltask_destroy(&ltask);
-      total += TBX_TIMING_DELAY(t1, t2);
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  printf("\t\t%f usec.\n", total / TASKLET_LOOPS);
+  lats_display();
 
+  lats_init("piom cond signal + cond wait");
   piom_cond_t cond;
-  printf("piom cond signal + cond wait (%d loops):", COND_LOOPS);
-  for(i = 0 ; i < COND_LOOPS ; i++)
+  for(i = 0 ; i < iterations ; i++)
     {
       piom_cond_init(&cond, 0);
       TBX_GET_TICK(t1);
@@ -130,10 +178,9 @@ int main(int argc, char**argv)
       piom_cond_wait(&cond, 1);
       TBX_GET_TICK(t2);
       piom_cond_destroy(&cond);
-      total += TBX_TIMING_DELAY(t1, t2);
+      lats[i] = TBX_TIMING_DELAY(t1, t2);
     }
-  printf("\t%f usec.\n", total / COND_LOOPS);
-  
+  lats_display();
   
   pioman_exit();
   tbx_exit();

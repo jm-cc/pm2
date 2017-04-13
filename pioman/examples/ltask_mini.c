6d8b21ebe2f29ce2cf04de73744fafc44dd2ced7
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-04-13 13:15:18 +0000

    pioman:   -- make ltask_mini example buildable with any thread library
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27143 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pioman/examples/ltask_mini.c
+++ pioman/examples/ltask_mini.c
@@ -2,21 +2,23 @@
  */
 
 #include <pioman.h>
-#include <marcel.h>
 #include <stdio.h>
 
 static struct piom_ltask ltask;
 static volatile int flag = 0;
+static long long int count = 0;
+static const int seconds = 5;
 
 void*worker(void*dummy)
 {
-  marcel_sleep(3);
+  sleep(seconds);
   flag = 1;
   return NULL;
 }
 
 int ltask_handler(void*arg)
 {
+  count++;
   if(flag)
     {
       fprintf(stderr, "poll success in handler!\n");
@@ -31,19 +33,21 @@ int main(int argc, char**argv)
   pioman_init(&argc, argv);
   
   /* create and submit an ltask that polls into memory */
-  piom_ltask_create(&ltask, &ltask_handler, NULL, PIOM_LTASK_OPTION_REPEAT, piom_vpset_full);
+  piom_ltask_create(&ltask, &ltask_handler, NULL, PIOM_LTASK_OPTION_REPEAT);
   piom_ltask_submit(&ltask);
 
-  /* create a thread that will generate an event in 3 seconds. */
-  marcel_t tid;
-  marcel_create(&tid, NULL, &worker, NULL);
-  marcel_detach(tid);
+  /* create a thread that will trigger an event in 5 seconds. */
+  piom_thread_t tid;
+  piom_thread_create(&tid, NULL, &worker, NULL);
 
   /* wait for the ltask completion */
-  fprintf(stderr, "waiting for ltask completion...\n");
+  fprintf(stderr, "waiting for ltask completion... (%d seconds)\n", seconds);
   piom_ltask_wait(&ltask);
-  fprintf(stderr, "task completed.\n");
+  fprintf(stderr, "task completed after count = %lld iterations (%lld/s; %f usec/iter).\n",
+	  count, count/seconds, 1000000.0 * (double)seconds/(double)count);
 
+  piom_thread_join(tid);
+  
   /* finalize pioman */
   pioman_exit();
   return 0;

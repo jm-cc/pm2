b6542847722b886cd11dd61f2ca72ff0a828236c
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-05-31 14:47:59 +0000

    nmad:   -- really send to self in self benchmark
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27307 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/benchmarks/nm_bench_self.c
+++ nmad/examples/benchmarks/nm_bench_self.c
@@ -15,14 +15,28 @@
 
 #include "nm_bench_generic.h"
 #include <nm_sendrecv_interface.h>
+#include <nm_launcher.h>
 
 static const nm_tag_t data_tag = 0x01;
+static nm_gate_t p_self = NULL;
+static void*discard_buffer = NULL;
+
+static void nm_self_init(void*buf, nm_len_t len)
+{
+  discard_buffer = realloc(discard_buffer, len);
+  if(p_self == NULL)
+    {
+      int rank;
+      nm_launcher_get_rank(&rank);
+      nm_launcher_get_gate(rank, &p_self);
+    }
+}
 
 static void nm_self_bench(void*buf, nm_len_t len)
 {
   nm_sr_request_t sreq, rreq;
-  nm_sr_irecv(nm_bench_common.p_session, nm_bench_common.p_gate, data_tag, buf, len, &rreq);
-  nm_sr_isend(nm_bench_common.p_session, nm_bench_common.p_gate, data_tag, buf, len, &sreq);
+  nm_sr_irecv(nm_bench_common.p_session, p_self, data_tag, discard_buffer, len, &rreq);
+  nm_sr_isend(nm_bench_common.p_session, p_self, data_tag, buf, len, &sreq);
   nm_sr_rwait(nm_bench_common.p_session, &rreq);
   nm_sr_swait(nm_bench_common.p_session, &sreq);
 }
@@ -31,6 +45,7 @@ static void nm_self_bench(void*buf, nm_len_t len)
 const struct nm_bench_s nm_bench =
   {
     .name = "send to self",
+    .init   = &nm_self_init,
     .server = &nm_self_bench,
     .client = &nm_self_bench,
   };

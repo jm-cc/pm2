cd3b6c49a75787d84f632da419d39ccb1043b317
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-05-31 14:43:16 +0000

    nmad:   -- add a benchmark for communication to self
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27306 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/benchmarks/nm_bench_self.c
@@ -0,0 +1,37 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2017 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "nm_bench_generic.h"
+#include <nm_sendrecv_interface.h>
+
+static const nm_tag_t data_tag = 0x01;
+
+static void nm_self_bench(void*buf, nm_len_t len)
+{
+  nm_sr_request_t sreq, rreq;
+  nm_sr_irecv(nm_bench_common.p_session, nm_bench_common.p_gate, data_tag, buf, len, &rreq);
+  nm_sr_isend(nm_bench_common.p_session, nm_bench_common.p_gate, data_tag, buf, len, &sreq);
+  nm_sr_rwait(nm_bench_common.p_session, &rreq);
+  nm_sr_swait(nm_bench_common.p_session, &sreq);
+}
+
+
+const struct nm_bench_s nm_bench =
+  {
+    .name = "send to self",
+    .server = &nm_self_bench,
+    .client = &nm_self_bench,
+  };
+

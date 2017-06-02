f95932d4ce02fbb7ef92bde075da53cc0248e163
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-06-02 09:35:45 +0000

    nmad:   -- raise the size of maximum unexpected packet
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27318 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/include/nm_parameters.h
+++ nmad/include/nm_parameters.h
@@ -19,7 +19,7 @@
 
 
 /** Maximum size of unexpected packets. Used for preallocated header zone within packet wrappers */
-#define NM_SO_MAX_UNEXPECTED  (16 * 1024)
+#define NM_SO_MAX_UNEXPECTED  (64 * 1024)
 
 /** size of the queue to submit requests */
 #define NM_REQ_CHUNK_QUEUE_SIZE 256

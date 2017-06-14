ca8a4f4f984e931182db069147bde0e579bc971a
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-06-14 14:21:45 +0000

    nmad:   -- fix pw v0 in case data have been mirrored
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27363 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/src/nm_so_schedule_in.c
+++ nmad/src/nm_so_schedule_in.c
@@ -896,6 +896,17 @@ void nm_pw_process_complete_recv(struct nm_core*p_core, struct nm_pkt_wrap_s*p_p
 	{
 	  const struct puk_receptacle_NewMad_minidriver_s*r = &p_pw->p_trk->receptacle;
 	  (*r->driver->buf_recv_release)(r->_status);
+	  if(p_pw->flags & NM_PW_BUF_MIRROR)
+	    {
+	      p_pw->v[0].iov_base = p_pw->buf;
+	    }
+	  else
+	    {
+#ifdef DEBUG
+	      p_pw->v[0].iov_base = NULL;
+	      p_pw->v[0].iov_len = 0;
+#endif /* DEBUG*/
+	    }
 	}
       /* refill recv on trk #0 */
       nm_drv_refill_recv(p_drv, p_gate);

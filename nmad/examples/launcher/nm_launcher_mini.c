113f26cdc06b97c55922a27921e28b64938d69ad
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-06-19 13:00:37 +0000

    nmad:   -- add a mini example for launcher
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27379 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/launcher/nm_launcher_mini.c
@@ -0,0 +1,42 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2017 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <nm_launcher.h>
+
+#include <stdio.h>
+
+
+int main(int argc, char**argv)
+{
+  int rc = nm_launcher_init(&argc, argv);
+  if(rc != NM_ESUCCESS)
+    {
+      fprintf(stderr, "error in nm_launcher_init()\n");
+    }
+  int rank = -1, size = -1;
+  nm_launcher_get_rank(&rank);
+  nm_launcher_get_size(&size);
+
+  printf("rank = %d; size = %d\n", rank, size);
+
+  nm_session_t p_session = NULL;
+  nm_launcher_session_open(&p_session, "launcher-mini");
+
+  nm_launcher_session_close(p_session);
+
+  nm_launcher_exit();
+
+  return 0;
+}

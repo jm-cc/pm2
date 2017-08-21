3d40b098274388a5ac6f2e9bec706dc31fefccb6
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-08-21 13:01:05 +0000

    pioman:   -- add a benchmark for various timers
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27409 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- pioman/examples/test_yield.c
+++ pioman/examples/test_yield.c
@@ -6,7 +6,9 @@
 
 #include <tbx.h>
 
-#define DEFAULT_LOOPS 100000
+#define DEFAULT_LOOPS 10000
+
+#define MAX_SLEEP 500
 
 static unsigned loops = DEFAULT_LOOPS;
 
@@ -23,7 +25,60 @@ int main(int argc, char**argv)
     }
   TBX_GET_TICK(t2);
   double t = TBX_TIMING_DELAY(t1, t2);
-  printf("# sched_yield = %fsec.\n", t/(double)loops);
+  printf("# sched_yield = %f usec.\n", t/(double)loops);
+
+  int s = 1;
+  for(s = 1; s < MAX_SLEEP ; s *= 2)
+    {
+      TBX_GET_TICK(t1);
+      for(i = 0 ; i < loops ; i++)
+	{
+	  usleep(s);
+	}
+      TBX_GET_TICK(t2);
+      t = TBX_TIMING_DELAY(t1, t2);
+      printf("# usleep(%d) = %f usec.\n", s, t/(double)loops);
+    }
+
+  for(s = 1; s < MAX_SLEEP ; s *= 2)
+    {
+      TBX_GET_TICK(t1);
+      for(i = 0 ; i < loops ; i++)
+	{
+	  struct timespec ts = { .tv_sec = 0, .tv_nsec = 1000 * s };
+	  nanosleep(&ts, NULL);
+	}
+      TBX_GET_TICK(t2);
+      t = TBX_TIMING_DELAY(t1, t2);
+      printf("# nanosleep(%d) = %f usec.\n", s, t/(double)loops);
+    }
+
+  for(s = 1; s < MAX_SLEEP ; s *= 2)
+    {
+      TBX_GET_TICK(t1);
+      for(i = 0 ; i < loops ; i++)
+	{
+	  struct timespec ts = { .tv_sec = 0, .tv_nsec = 1000 * s };
+	  clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, NULL);
+	}
+      TBX_GET_TICK(t2);
+      t = TBX_TIMING_DELAY(t1, t2);
+      printf("# clock_nanosleep(%d) = %f usec.\n", s, t/(double)loops);
+    }
+
+  for(s = 1; s < MAX_SLEEP ; s *= 2)
+    {
+      TBX_GET_TICK(t1);
+      for(i = 0 ; i < loops ; i++)
+	{
+	  struct timeval tv = { .tv_sec = 0, .tv_usec = s };
+	  select(0, NULL, NULL, NULL, &tv);
+	}
+      TBX_GET_TICK(t2);
+      t = TBX_TIMING_DELAY(t1, t2);
+      printf("# select(%d) = %f usec.\n", s, t/(double)loops);
+    }
+  
   return 0;
 }
 

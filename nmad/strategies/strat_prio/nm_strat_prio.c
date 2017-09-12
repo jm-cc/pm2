83c8428329f3c5ef45b2ea2b3389b4bda0c7946e
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-09-12 13:05:51 +0000

    nmad:   -- add a 'prio' strategy to schedule packets with priority   (only a skeleton for now, do not manage priority yet)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27430 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/strategies/strat_prio/nm_strat_prio.c
@@ -0,0 +1,161 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006-2017 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <stdint.h>
+#include <assert.h>
+
+#include <nm_private.h>
+#include <Padico/Module.h>
+
+
+/* ********************************************************* */
+
+static void strat_prio_try_and_commit(void*, nm_gate_t );
+static void strat_prio_rdv_accept(void*, nm_gate_t );
+
+static const struct nm_strategy_iface_s nm_strat_prio_driver =
+  {
+    .pack_data          = NULL,
+    .pack_ctrl          = NULL,
+    .try_and_commit     = &strat_prio_try_and_commit,
+    .rdv_accept         = &strat_prio_rdv_accept
+};
+
+static void*strat_prio_instantiate(puk_instance_t, puk_context_t);
+static void strat_prio_destroy(void*);
+
+static const struct puk_component_driver_s nm_strat_prio_component_driver =
+  {
+    .instantiate = &strat_prio_instantiate,
+    .destroy     = &strat_prio_destroy
+  };
+
+/* ********************************************************* */
+
+/** Component declaration */
+PADICO_MODULE_COMPONENT(NewMad_Strategy_prio,
+                        puk_component_declare("NewMad_Strategy_prio",
+                                              puk_component_provides("PadicoComponent", "component", &nm_strat_prio_component_driver),
+                                              puk_component_provides("NewMad_Strategy", "strat", &nm_strat_prio_driver),
+                                              puk_component_attr("nm_max_small", "16336"),
+                                              puk_component_attr("nm_copy_on_send_threshold", "4096"))
+                        );
+
+/* ********************************************************* */
+
+
+/** Per-gate status for strat prio instances
+ */
+struct nm_strat_prio_s
+{
+  int nm_max_small;
+  int nm_copy_on_send_threshold;
+};
+
+/** Initialize the gate storage for prio strategy.
+ */
+static void*strat_prio_instantiate(puk_instance_t ai, puk_context_t context)
+{
+  struct nm_strat_prio_s*p_status = malloc(sizeof(struct nm_strat_prio_s));
+  const char*nm_max_small = puk_instance_getattr(ai, "nm_max_small");
+  p_status->nm_max_small = atoi(nm_max_small);
+  const char*nm_copy_on_send_threshold = puk_instance_getattr(ai, "nm_copy_on_send_threshold");
+  p_status->nm_copy_on_send_threshold = atoi(nm_copy_on_send_threshold);
+  return (void*)p_status;
+}
+
+/** Cleanup the gate storage for prio strategy.
+ */
+static void strat_prio_destroy(void*_status)
+{
+  free(_status);
+}
+
+/** Compute and apply the best possible packet rearrangement, then
+ *  return next packet to send.
+ *
+ *  @param p_gate a pointer to the gate object.
+ */
+static void strat_prio_try_and_commit(void*_status, nm_gate_t p_gate)
+{
+  struct nm_strat_prio_s*p_status = _status;
+  struct nm_trk_s*p_trk_small = &p_gate->trks[NM_TRK_SMALL];
+  struct nm_drv_s*p_drv = p_trk_small->p_drv;
+  if((p_trk_small->p_pw_send == NULL) &&
+     !(nm_ctrl_chunk_list_empty(&p_gate->ctrl_chunk_list) &&
+       nm_req_chunk_list_empty(&p_gate->req_chunk_list)))
+    {
+      struct nm_pkt_wrap_s*p_pw = NULL;
+      const nm_len_t drv_max = ((p_drv->props.capabilities.max_msg_size == 0) ||
+				(p_drv->props.capabilities.max_msg_size > NM_SO_MAX_UNEXPECTED)) ?
+	NM_SO_MAX_UNEXPECTED : p_drv->props.capabilities.max_msg_size;
+      const nm_len_t max_small =  drv_max - NM_ALIGN_FRONTIER - sizeof(struct nm_header_global_s);
+      if(p_drv->props.capabilities.supports_buf_send)
+	{
+	  p_pw = nm_pw_alloc_driver_header(p_trk_small);
+	}
+      else
+	{
+	  p_pw = nm_pw_alloc_global_header();
+	  p_pw->max_len = max_small;
+	}
+      if(!nm_ctrl_chunk_list_empty(&p_gate->ctrl_chunk_list))
+	{
+	  /* post ctrl on trk #0 */
+	  struct nm_ctrl_chunk_s*p_ctrl_chunk = nm_ctrl_chunk_list_begin(&p_gate->ctrl_chunk_list);
+	  int rc __attribute__((unused));
+	  rc = nm_tactic_pack_ctrl(p_gate, p_drv, p_ctrl_chunk, p_pw);
+	  assert(rc == NM_ESUCCESS);
+	}
+      else if(!nm_req_chunk_list_empty(&p_gate->req_chunk_list))
+	{
+	  struct nm_req_chunk_s*p_req_chunk = nm_req_chunk_list_begin(&p_gate->req_chunk_list);
+	  struct nm_req_s*p_pack = p_req_chunk->p_req;
+	  const struct nm_data_properties_s*p_props = nm_data_properties_get(&p_pack->data);
+	  const nm_len_t max_header_len = NM_HEADER_DATA_SIZE + p_props->blocks * sizeof(struct nm_header_pkt_data_chunk_s);
+	  if(p_req_chunk->chunk_len + max_header_len <= p_status->nm_max_small)
+	    {
+	      /* post short data on trk #0 */
+	      nm_req_chunk_list_pop_front(&p_gate->req_chunk_list);
+	      nm_pw_add_req_chunk(p_pw, p_req_chunk, NM_REQ_FLAG_USE_COPY);
+	      assert(p_pw->length <= NM_SO_MAX_UNEXPECTED);
+	    }
+	  else
+	    {
+	      /* post RDV for large data */
+	      nm_tactic_pack_rdv(p_gate, p_drv, p_req_chunk, p_pw);
+	    }
+	}
+      nm_core_post_send(p_pw, p_gate, NM_TRK_SMALL);
+    }
+}
+
+/** Emit RTR for received RDV requests
+ */
+static void strat_prio_rdv_accept(void*_status, nm_gate_t p_gate)
+{
+  if(!nm_pkt_wrap_list_empty(&p_gate->pending_large_recv))
+    {
+      struct nm_trk_s*p_trk_large = &p_gate->trks[NM_TRK_LARGE];
+      if(p_trk_large->p_pw_recv == NULL)
+	{
+	  /* The large-packet track is available- post recv and RTR */
+	  struct nm_pkt_wrap_s*p_pw = nm_pkt_wrap_list_pop_front(&p_gate->pending_large_recv);
+	  struct nm_rdv_chunk chunk = 
+	    { .len = p_pw->length, .trk_id = NM_TRK_LARGE };
+	  nm_tactic_rtr_pack(p_pw, 1, &chunk);
+	}
+    }
+}

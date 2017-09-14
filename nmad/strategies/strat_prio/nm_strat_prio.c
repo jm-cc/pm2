548fcf802095af5f967dd9aade7a4da23d01ea4a
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-09-14 14:05:27 +0000

    nmad:   -- take priority into account in strat_prio (linear search)
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27435 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/strategies/strat_prio/nm_strat_prio.c
+++ nmad/strategies/strat_prio/nm_strat_prio.c
@@ -49,7 +49,6 @@ PADICO_MODULE_COMPONENT(NewMad_Strategy_prio,
                         puk_component_declare("NewMad_Strategy_prio",
                                               puk_component_provides("PadicoComponent", "component", &nm_strat_prio_component_driver),
                                               puk_component_provides("NewMad_Strategy", "strat", &nm_strat_prio_driver),
-                                              puk_component_attr("nm_max_small", "16336"),
                                               puk_component_attr("nm_copy_on_send_threshold", "4096"))
                         );
 
@@ -60,20 +59,19 @@ PADICO_MODULE_COMPONENT(NewMad_Strategy_prio,
  */
 struct nm_strat_prio_s
 {
-  int nm_max_small;
+  struct nm_req_chunk_list_s req_chunk_list;
   int nm_copy_on_send_threshold;
 };
 
 /** Initialize the gate storage for prio strategy.
  */
-static void*strat_prio_instantiate(puk_instance_t ai, puk_context_t context)
+static void*strat_prio_instantiate(puk_instance_t instance, puk_context_t context)
 {
   struct nm_strat_prio_s*p_status = malloc(sizeof(struct nm_strat_prio_s));
-  const char*nm_max_small = puk_instance_getattr(ai, "nm_max_small");
-  p_status->nm_max_small = atoi(nm_max_small);
-  const char*nm_copy_on_send_threshold = puk_instance_getattr(ai, "nm_copy_on_send_threshold");
+  const char*nm_copy_on_send_threshold = puk_instance_getattr(instance, "nm_copy_on_send_threshold");
   p_status->nm_copy_on_send_threshold = atoi(nm_copy_on_send_threshold);
-  return (void*)p_status;
+  nm_req_chunk_list_init(&p_status->req_chunk_list);
+  return p_status;
 }
 
 /** Cleanup the gate storage for prio strategy.
@@ -93,9 +91,29 @@ static void strat_prio_try_and_commit(void*_status, nm_gate_t p_gate)
   struct nm_strat_prio_s*p_status = _status;
   struct nm_trk_s*p_trk_small = &p_gate->trks[NM_TRK_SMALL];
   struct nm_drv_s*p_drv = p_trk_small->p_drv;
+  while(!nm_req_chunk_list_empty(&p_gate->req_chunk_list))
+    {
+      struct nm_req_chunk_s*p_req_chunk = nm_req_chunk_list_pop_front(&p_gate->req_chunk_list);
+      const int prio = p_req_chunk->p_req->pack.priority;
+      nm_req_chunk_itor_t i = nm_req_chunk_list_rend(&p_status->req_chunk_list);
+      while(i)
+	{
+	  if(prio > i->p_req->pack.priority)
+	    {
+	      i = nm_req_chunk_list_rnext(i);
+	    }
+	  else
+	    break;
+	}
+      if(i)
+	nm_req_chunk_list_insert_after(&p_status->req_chunk_list, i, p_req_chunk);
+      else
+	nm_req_chunk_list_push_front(&p_status->req_chunk_list, p_req_chunk);
+      fprintf(stderr, "insert req; size = %d\n", nm_req_chunk_list_size(&p_status->req_chunk_list));
+    }
   if((p_trk_small->p_pw_send == NULL) &&
      !(nm_ctrl_chunk_list_empty(&p_gate->ctrl_chunk_list) &&
-       nm_req_chunk_list_empty(&p_gate->req_chunk_list)))
+       nm_req_chunk_list_empty(&p_status->req_chunk_list)))
     {
       struct nm_pkt_wrap_s*p_pw = nm_pw_alloc_global_header(p_trk_small);
       if(!nm_ctrl_chunk_list_empty(&p_gate->ctrl_chunk_list))
@@ -106,16 +124,16 @@ static void strat_prio_try_and_commit(void*_status, nm_gate_t p_gate)
 	  rc = nm_tactic_pack_ctrl(p_gate, p_drv, p_ctrl_chunk, p_pw);
 	  assert(rc == NM_ESUCCESS);
 	}
-      else if(!nm_req_chunk_list_empty(&p_gate->req_chunk_list))
+      else if(!nm_req_chunk_list_empty(&p_status->req_chunk_list))
 	{
-	  struct nm_req_chunk_s*p_req_chunk = nm_req_chunk_list_begin(&p_gate->req_chunk_list);
+	  struct nm_req_chunk_s*p_req_chunk = nm_req_chunk_list_begin(&p_status->req_chunk_list);
 	  struct nm_req_s*p_pack = p_req_chunk->p_req;
 	  const struct nm_data_properties_s*p_props = nm_data_properties_get(&p_pack->data);
 	  const nm_len_t max_header_len = NM_HEADER_DATA_SIZE + p_props->blocks * sizeof(struct nm_header_pkt_data_chunk_s);
-	  if(p_req_chunk->chunk_len + max_header_len <= p_status->nm_max_small)
+	  if(p_req_chunk->chunk_len + max_header_len <= p_pw->max_len)
 	    {
 	      /* post short data on trk #0 */
-	      nm_req_chunk_list_pop_front(&p_gate->req_chunk_list);
+	      nm_req_chunk_list_pop_front(&p_status->req_chunk_list);
 	      nm_pw_add_req_chunk(p_pw, p_req_chunk, NM_REQ_FLAG_USE_COPY);
 	      assert(p_pw->length <= NM_SO_MAX_UNEXPECTED);
 	    }
@@ -123,8 +141,14 @@ static void strat_prio_try_and_commit(void*_status, nm_gate_t p_gate)
 	    {
 	      /* post RDV for large data */
 	      nm_tactic_pack_rdv(p_gate, p_drv, p_req_chunk, p_pw);
+
+	      abort(); /* TODO- fix nm_tactic_pack_rdv */
+	      
 	    }
 	}
+      fprintf(stderr, "nm_core_post_send(); status size = %d; gate size = %d\n",
+	      nm_req_chunk_list_size(&p_status->req_chunk_list),
+	      nm_req_chunk_list_size(&p_gate->req_chunk_list));
       nm_core_post_send(p_pw, p_gate, NM_TRK_SMALL);
     }
 }

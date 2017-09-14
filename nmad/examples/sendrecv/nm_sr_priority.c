55aad1c0ed9726c92fd670d4008d0f7ef44b3e63
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-09-14 14:05:47 +0000

    nmad:   -- add a test for priorities
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27436 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ nmad/examples/sendrecv/nm_sr_priority.c
@@ -0,0 +1,62 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2006-2017 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../common/nm_examples_helper.h"
+
+#define MAXMSG 5
+static int values[MAXMSG];
+static nm_sr_request_t requests[MAXMSG];
+
+int main(int argc, char**argv)
+{ 
+  nm_examples_init(&argc, argv);
+
+  int i;
+  for(i = 0; i < MAXMSG; i++)
+    {
+      if(is_server)
+	{
+	  values[i] = -1;
+	  nm_sr_irecv(p_session, NM_ANY_GATE, 0, &values[i], sizeof(int), &requests[i]);
+	}
+      else
+	{
+	  values[i] = i;
+	  nm_sr_isend(p_session, p_gate, 0, &values[i], sizeof(int), &requests[i]);
+	}
+    }
+  for(i = 0; i < MAXMSG; i++)
+    {
+      if(is_server)
+	{
+	  nm_sr_rwait(p_session, &requests[i]);
+	  printf("# %d: %d\n", i, values[i]);
+	}
+      else
+	{
+	  nm_sr_swait(p_session, &requests[i]);
+	}
+      }
+  
+  nm_examples_exit();
+  exit(0);
+}

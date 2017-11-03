bb926759e7711b29fa5fed17051af9dcd7d71416
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-11-03 16:03:08 +0000

    nmad:   -- add doc in minidriver interface
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27519 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/include/nm_minidriver.h
+++ nmad/include/nm_minidriver.h
@@ -48,7 +48,7 @@ struct nm_minidriver_capabilities_s
   int has_recv_any;      /**< driver accepts receive from NM_GATE_ANY */
   int min_period;        /**< minimum delay between poll (in microseconds) */
   int is_exportable;     /**< blocking calls may be exported by PIOMan */
-  int trk_rdv;           /**< trk needs a rdv for matched send/recv size; if 0, trk preserves boudaries */
+  int trk_rdv;           /**< trk needs a rdv for matched send/recv size; if 0, trk preserves boundaries */
 };
 
 struct nm_minidriver_properties_s
@@ -63,26 +63,59 @@ struct nm_minidriver_properties_s
  */
 struct nm_minidriver_iface_s
 {
+  /* ** driver init; applied to context before/after instantiation */
+
+  /** get driver properties (profiling and capabilities) */
   void (*getprops)(puk_context_t context, struct nm_minidriver_properties_s*props);
-  /* connection establishment */
+  /** init the driver and get its url */
   void (*init)(puk_context_t context, const void**drv_url, size_t*url_size);
-  void (*connect)(void*_status, const void*remote_url, size_t url_size);
+  /** close the driver */
   void (*close)(puk_context_t context);
-  /* sending primitives */
+  
+  /* ** connection establishment */
+
+  /** connect this instance to the remote url  */
+  void (*connect)(void*_status, const void*remote_url, size_t url_size);
+  /* TODO- disconnect */
+  /* TODO- register error handler for fault-tolerance */
+  
+  /* ** sending primitives */
+
+  /** post a send for data described by iovec */
   void (*send_post)(void*_status, const struct iovec*v, int n);
+  /** post a send for data described by nm_data iterator */
   void (*send_data)(void*_status, const struct nm_data_s*p_data, nm_len_t chunk_offset, nm_len_t chunk_len);
+  /** poll a send operation already posted */
   int  (*send_poll)(void*_status);
+  /** prefetch data in driver buffers before send_post 
+   * @note currently broken; obsoleted by buffer-based send (see below)
+   */
   void (*send_prefetch)(void*_status, const void*ptr, nm_len_t size);
-  /* receiving primitives */
-  void (*recv_init)(void*_status, struct iovec*v, int n);
-  void (*recv_data)(void*_status, const struct nm_data_s*p_data, nm_len_t chunk_offset, nm_len_t chunk_len);
-  int  (*poll_one)(void*_status);
-  int  (*cancel_recv)(void*_status);
-  /* buffer-based sending */
+
+  /* ** buffer-based sending */
+
+  /** get a buffer to prepare data for send */
   void (*buf_send_get)(void*_status, void**p_buffer, nm_len_t*p_len);
+  /** post a send after upper layers have filled-in the buffer */
   void (*buf_send_post)(void*_status, nm_len_t len);
-  /* buffer-based recv */
+  /* note: polling for buff_send is done through send_poll() */
+
+  /* ** receiving primitives */
+
+  /** post a recv for data described by iovec */
+  void (*recv_init)(void*_status, struct iovec*v, int n);
+  /** post a recv for data described by nm_data iterator */
+  void (*recv_data)(void*_status, const struct nm_data_s*p_data, nm_len_t chunk_offset, nm_len_t chunk_len);
+  /** poll a recv operation already posted */
+  int  (*poll_one)(void*_status);
+  /** cancel a posted recv */
+  int  (*cancel_recv)(void*_status);
+
+  /* ** buffer-based recv */
+
+  /** poll the driver for a pending recv; returns buffer */
   int  (*buf_recv_poll)(void*_status, void**p_buffer, nm_len_t*p_len);
+  /** release a buffer returned by buf_recv_poll() */
   void (*buf_recv_release)(void*_status);
 };
 PUK_IFACE_TYPE(NewMad_minidriver, struct nm_minidriver_iface_s);

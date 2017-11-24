02ee378a48615e54cc0dc4756229c1fbd7ff526d
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2017-11-24 10:11:47 +0000

    madmpi:   -- add persistent requests to base test suite   -- fix indexed datatype in persistent req test
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27607 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/examples/mpi/basics/nm_mpi_persistent2.c
+++ nmad/examples/mpi/basics/nm_mpi_persistent2.c
@@ -25,96 +25,109 @@
 void check_buffer(char *buffer, int *strides, int *blocklengths);
 void persistent_index(int ping_side, int rank_dst);
 
-int main(int argc, char **argv) {
+int main(int argc, char**argv)
+{
   int numtasks, rank;
   int rank_dst, ping_side;
-
+  
   // Initialise MPI
   MPI_Init(&argc,&argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-  if (numtasks % 2 != 0) {
-    printf("Need odd size of processes (%d)\n", numtasks);
-    MPI_Abort(MPI_COMM_WORLD, 1);
-    exit(1);
-  }
-
+  if(numtasks % 2 != 0)
+    {
+      printf("Need odd size of processes (%d)\n", numtasks);
+      MPI_Abort(MPI_COMM_WORLD, 1);
+      exit(1);
+    }
+  
   ping_side = !(rank & 1);
   rank_dst = ping_side?(rank | 1) : (rank & ~1);
-
+  
   persistent_index(ping_side, rank_dst);
 
   MPI_Finalize();
   exit(0);
 }
 
-void check_buffer(char *buffer, int *strides, int *blocklengths) {
-  int i=0, j=0, success=1;
+void check_buffer(char *buffer, int *strides, int *blocklengths)
+{
+  int i = 0, j = 0, success = 1;
   char value;
   int nb, count, blength;
 
-  for(nb=0 ; nb<3 ; nb++) {
-    j=0;
-    for(count=0 ; count<3 ; count++) {
-      value = 'a' + (nb * (strides[2] + blocklengths[2]));
-      value += strides[j];
-      for(blength=0 ; blength<blocklengths[j] ; blength++) {
-        if (buffer[i] != value) {
-          success=0;
-          break;
+  for(nb = 0 ; nb < 3 ; nb++)
+    {
+      j = 0;
+      for(count = 0 ; count < 3 ; count++)
+        {
+          value = 'a' + (nb * (strides[2] + blocklengths[2]));
+          value += strides[j];
+          for(blength = 0 ; blength < blocklengths[j] ; blength++)
+            {
+              if(buffer[i] != value)
+                {
+                  success=0;
+                  break;
+                }
+              i++;
+              value++;
+            }
+          j++;
         }
-        i++;
-        value++;
-      }
-      j++;
     }
-  }
-  if (success) {
-    printf("Index successfully received\n");
-  }
-  else {
-    printf("Incorrect index: [ ");
-    for(i=0 ; i<18 ; i++) printf("%c(%d) ", buffer[i], ((int) buffer[i])-97);
-    printf("]\n");
-  }
+  if(success)
+    {
+      printf("Index successfully received\n");
+    }
+  else
+    {
+      printf("Incorrect index: [ ");
+      for(i = 0 ; i < 18 ; i++)
+        printf("%c(%d) ", buffer[i], ((int) buffer[i])-97);
+      printf("]\n");
+    }
 }
 
-void persistent_index(int ping_side, int rank_dst) {
+void persistent_index(int ping_side, int rank_dst)
+{
   MPI_Datatype mytype;
   int blocklengths[3] = {1, 2, 3};
   int strides[3] = {0, 2, 3};
-
+  
   char buffer[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i' ,'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
   MPI_Type_indexed(3, blocklengths, strides, MPI_CHAR, &mytype);
   MPI_Type_commit(&mytype);
 
-  if (ping_side) {
-    MPI_Request send_request;
-
-    MPI_Send_init(buffer, 3, mytype, rank_dst, 10, MPI_COMM_WORLD, &send_request);
-    MPI_Start(&send_request);
-    MPI_Wait(&send_request, MPI_STATUS_IGNORE);
-
-    MPI_Start(&send_request);
-    MPI_Wait(&send_request, MPI_STATUS_IGNORE);
-
-    MPI_Cancel(&send_request);
-  }
-  else {
-    MPI_Request recv_request;
-    char buffer2[18];
-
-    MPI_Recv_init(buffer2, 3, mytype, rank_dst, 10, MPI_COMM_WORLD, &recv_request);
-    MPI_Start(&recv_request);
-    MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
-    check_buffer(buffer2, strides, blocklengths);
-
-    MPI_Start(&recv_request);
-    MPI_Cancel(&recv_request);
-    MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
-  }
+  if(ping_side)
+    {
+      MPI_Request send_request;
+      
+      MPI_Send_init(buffer, 3, mytype, rank_dst, 10, MPI_COMM_WORLD, &send_request);
+      MPI_Start(&send_request);
+      MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+      
+      MPI_Start(&send_request);
+      MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+      
+      MPI_Cancel(&send_request);
+    }
+  else
+    {
+      MPI_Request recv_request;
+      char buffer2[18];
+      
+      MPI_Recv_init(buffer2, 3, mytype, rank_dst, 10, MPI_COMM_WORLD, &recv_request);
+      MPI_Start(&recv_request);
+      MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+      check_buffer(buffer2, strides, blocklengths);
+      
+      MPI_Start(&recv_request);
+      MPI_Cancel(&recv_request);
+      MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+    }
 
   MPI_Type_free(&mytype);
 }

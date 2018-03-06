0cda138363c7ead9758e31810bc28ff56951f93b
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2018-03-06 09:52:03 +0000

    mpibenchmark:   -- add a benchmark for bcast
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27726 ee107e0b-c2fa-0310-842a-ec2723fb34f4

---
+++ mpibenchmark/mpi_bench_coll_bcast.c
@@ -0,0 +1,31 @@
+/*
+ * NewMadeleine
+ * Copyright (C) 2015-2018 (see AUTHORS file)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include "mpi_bench_generic.h"
+
+static void mpi_bench_coll_bcast_func(void*buf, size_t len)
+{
+  MPI_Bcast(buf, len, MPI_CHAR, 0, MPI_COMM_WORLD);
+}
+
+const struct mpi_bench_s mpi_bench_coll_bcast =
+  {
+    .label   = "mpi_bench_coll_bcast",
+    .name    = "MPI bcast",
+    .rtt     = MPI_BENCH_RTT_FULL,
+    .server  = &mpi_bench_coll_bcast_func,
+    .client  = &mpi_bench_coll_bcast_func
+  };
+

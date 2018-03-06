3b5f748c0de5efa9ce2ac74d2f2be5600e553396
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2018-03-06 16:46:01 +0000

    nmad:   -- add functions to compute binomial trees topology
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27734 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/interfaces/coll/include/nm_coll_private.h
+++ nmad/interfaces/coll/include/nm_coll_private.h
@@ -1,6 +1,6 @@
 /*
  * NewMadeleine
- * Copyright (C) 2014-2015 (see AUTHORS file)
+ * Copyright (C) 2014-2018 (see AUTHORS file)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,3 +32,39 @@
 #define NM_COLL_TAG_COMM_CREATE_1 ( NM_COLL_TAG_BASE | 0x04 )
 /** comm_create phase 2 */
 #define NM_COLL_TAG_COMM_CREATE_2 ( NM_COLL_TAG_BASE | 0x05 )
+
+
+/* ********************************************************* */
+
+/* ** trees */
+
+/** floor(log2(X)) */
+#define LOG2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
+
+/** number of communications steps in the tree (0 for a single node) */
+static inline int nm_coll_binomial_height(int n)
+{
+  assert(n > 0);
+  const int l = (LOG2(n)) ;
+  return l + (!!(n & (n - 1))); /* = ceil(log2(n)) */
+}
+
+/** walk to the leafs of the tree */
+static inline int nm_coll_binomial_child(int i, int n, int level)
+{
+  const int skip = (1 << level);
+  if((i % (2*skip) == 0) && (i + skip < n))
+    return (i + skip);
+  else
+    return -1;
+}
+
+/** walk to the root of the tree */
+static inline int nm_coll_binomial_parent(int i, int n, int level)
+{
+  const int skip = (1 << level);
+  if(i % (2*skip) == skip)
+    return (i - skip);
+  else
+    return -1;
+}

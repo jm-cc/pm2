237057608747dc40f41f8762847e160bafabd48f
adenis <adenis@ee107e0b-c2fa-0310-842a-ec2723fb34f4>
2018-03-07 14:56:33 +0000

    nmad:   -- make bcast use generic trees   -- switch to binomial tree by default
    git-svn-id: https://scm.gforge.inria.fr/authscm/jmcoutey/svn/pm2/trunk@27737 ee107e0b-c2fa-0310-842a-ec2723fb34f4

--- nmad/interfaces/coll/src/nm_coll.c
+++ nmad/interfaces/coll/src/nm_coll.c
@@ -25,35 +25,42 @@ void nm_coll_group_data_bcast(nm_session_t p_session, nm_group_t p_group, int ro
                               struct nm_data_s*p_data, nm_tag_t tag)
 {
   assert(nm_group_get_gate(p_group, self) == nm_launcher_self_gate());
-  if(self == root)
+  const int size = nm_group_size(p_group);
+  struct nm_coll_tree_info_s tree;
+  nm_coll_tree_init(&tree, NM_COLL_TREE_BINOMIAL, size, self, root);
+  nm_sr_request_t*requests = malloc(tree.max_arity * sizeof(nm_sr_request_t));
+  int*children = malloc(sizeof(int) * tree.max_arity);
+  int s;
+  for(s = 0; s < tree.height; s++)
     {
-      const int size = nm_group_size(p_group);
-      nm_sr_request_t*requests = malloc(size * sizeof(nm_sr_request_t));
-      int i;
-      for(i = 0; i < size; i++)
-	{
-	  if(i != self)
+      int parent = -1;
+      int n_children = 0;
+      nm_coll_tree_step(&tree, s, &parent, children, &n_children);
+      assert((parent == -1) || (n_children == 0) || (children[0] == -1));
+      if(parent != -1)
+        {
+          nm_gate_t p_parent_gate = nm_group_get_gate(p_group, parent);
+          nm_sr_request_t request;
+          nm_sr_irecv_data(p_session, p_parent_gate, tag, p_data, &request);
+          nm_sr_rwait(p_session, &request);
+        }
+      if(n_children > 0)
+        {
+          int i;
+          for(i = 0; i < n_children; i++)
             {
-              nm_gate_t p_gate = nm_group_get_gate(p_group, i);
+              assert(children[i] != -1);
+              nm_gate_t p_gate = nm_group_get_gate(p_group, children[i]);
               nm_sr_isend_data(p_session, p_gate, tag, p_data, &requests[i]);
             }
-	}
-      for(i = 0; i < size; i++)
-	{
-	  if(i != self)
+          for(i = 0; i < n_children; i++)
             {
               nm_sr_swait(p_session, &requests[i]);
             }
-	}
-      free(requests);
+        }
     }
-  else
-    {
-      nm_gate_t p_root_gate = nm_group_get_gate(p_group, root);
-      nm_sr_request_t request;
-      nm_sr_irecv_data(p_session, p_root_gate, tag, p_data, &request);
-      nm_sr_rwait(p_session, &request);
-    }
+  free(children);
+  free(requests);
 }
 
 /* ********************************************************* */
